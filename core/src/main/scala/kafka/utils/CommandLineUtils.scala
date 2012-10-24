4ef91667692101cec0e82bfb9f0d3c14cd915ef0
Jun Rao <junrao@apache.org>
2012-10-24 23:00:25 +0000

    trivial fix to add the missing Logging class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/kafka/branches/0.8@1401909 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/scala/kafka/utils/CommandLineUtils.scala
+++ core/src/main/scala/kafka/utils/CommandLineUtils.scala
@@ -5,7 +5,7 @@ import joptsimple.{OptionSpec, OptionSet, OptionParser}
 /**
  * Helper functions for dealing with command line utilities
  */
-object CommandLineUtils {
+object CommandLineUtils extends Logging {
 
     def checkRequiredArgs(parser: OptionParser, options: OptionSet, required: OptionSpec[_]*) {
       for(arg <- required) {
