f18ea91b4cd7e1389c0acb7cef957900f940aff7
Joe Stein <joe.stein@stealth.ly>
2013-12-22 09:17:28 -0500

    KAFKA-1180 WhiteList topic filter gets a NullPointerException on complex Regex

--- core/src/main/scala/kafka/utils/Utils.scala
+++ core/src/main/scala/kafka/utils/Utils.scala
@@ -476,6 +476,28 @@ object Utils extends Logging {
     builder.toString
   }
 
+  def quoteJsonLiteral (s : String) : String =
+    s.map {
+      case '"'  => "\\\""
+      case '\\' => "\\\\"
+      case '/'  => "\\/"
+      case '\b' => "\\b"
+      case '\f' => "\\f"
+      case '\n' => "\\n"
+      case '\r' => "\\r"
+      case '\t' => "\\t"
+      /* We'll unicode escape any control characters. These include:
+       * 0x0 -> 0x1f  : ASCII Control (C0 Control Codes)
+       * 0x7f         : ASCII DELETE
+       * 0x80 -> 0x9f : C1 Control Codes
+       *
+       * Per RFC4627, section 2.5, we're not technically required to
+       * encode the C1 codes, but we do to be safe.
+       */
+      case c if ((c >= '\u0000' && c <= '\u001f') || (c >= '\u007f' && c <= '\u009f')) => "\\u%04x".format(c: Int)
+      case c => c
+    }.mkString
+
  /**
    * Format a Map[String, String] as JSON object.
    */
