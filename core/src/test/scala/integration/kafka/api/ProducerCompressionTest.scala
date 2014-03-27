9bc47bc1365d17c34f8d43239846de28219a663c
Neha Narkhede <neha.narkhede@gmail.com>
2014-03-26 21:50:30 -0700

    KAFKA-1253 Compression in the new producer: follow up patch to push new files

---
+++ core/src/test/scala/integration/kafka/api/ProducerCompressionTest.scala
@@ -0,0 +1,126 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kafka.api.test
+
+import java.util.{Properties, Collection, ArrayList}
+
+import org.scalatest.junit.JUnit3Suite
+import org.junit.runners.Parameterized
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized.Parameters
+import org.junit.{After, Before, Test}
+import org.apache.kafka.clients.producer.{ProducerRecord, KafkaProducer, ProducerConfig}
+import org.junit.Assert._
+
+import kafka.api.FetchRequestBuilder
+import kafka.server.{KafkaConfig, KafkaServer}
+import kafka.consumer.SimpleConsumer
+import kafka.message.Message
+import kafka.zk.ZooKeeperTestHarness
+import kafka.utils.{Utils, TestUtils}
+
+import scala.Array
+
+
+@RunWith(value = classOf[Parameterized])
+class ProducerCompressionTest(compression: String) extends JUnit3Suite with ZooKeeperTestHarness {
+  private val brokerId = 0
+  private val port = TestUtils.choosePort
+  private var server: KafkaServer = null
+
+  private val props = TestUtils.createBrokerConfig(brokerId, port)
+  private val config = new KafkaConfig(props)
+
+  private val topic = "topic"
+  private val numRecords = 100
+
+  @Before
+  override def setUp() {
+    super.setUp()
+    server = TestUtils.createServer(config)
+  }
+
+  @After
+  override def tearDown() {
+    server.shutdown
+    Utils.rm(server.config.logDirs)
+    super.tearDown()
+  }
+
+  /**
+   * testCompression
+   *
+   * Compressed messages should be able to sent and consumed correctly
+   */
+  @Test
+  def testCompression() {
+
+    val props = new Properties()
+    props.put(ProducerConfig.BROKER_LIST_CONFIG, TestUtils.getBrokerListStrFromConfigs(Seq(config)))
+    props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, compression)
+    var producer = new KafkaProducer(props)
+
+    try {
+      // create topic
+      TestUtils.createTopic(zkClient, topic, 1, 1, List(server))
+      val partition = 0
+
+      // prepare the messages
+      val messages = for (i <-0 until numRecords)
+        yield ("value" + i).getBytes
+
+      // make sure the returned messages are correct
+      val responses = for (message <- messages)
+        yield producer.send(new ProducerRecord(topic, null, null, message))
+      val futures = responses.toList
+      for ((future, offset) <- futures zip (0 until numRecords)) {
+        assertEquals(offset.toLong, future.get.offset)
+      }
+
+      // make sure the fetched message count match
+      val consumer = new SimpleConsumer("localhost", port, 100, 1024*1024, "")
+      val fetchResponse = consumer.fetch(new FetchRequestBuilder().addFetch(topic, partition, 0, Int.MaxValue).build())
+      val messageSet = fetchResponse.messageSet(topic, partition).iterator.toBuffer
+      assertEquals("Should have fetched " + numRecords + " messages", numRecords, messageSet.size)
+
+      var index = 0
+      for (message <- messages) {
+        assertEquals(new Message(bytes = message), messageSet(index).message)
+        assertEquals(index.toLong, messageSet(index).offset)
+        index += 1
+      }
+    } finally {
+      if (producer != null) {
+        producer.close()
+        producer = null
+      }
+    }
+  }
+}
+
+object ProducerCompressionTest {
+
+  // NOTE: Must return collection of Array[AnyRef] (NOT Array[Any]).
+  @Parameters
+  def parameters: Collection[Array[String]] = {
+    val list = new ArrayList[Array[String]]()
+    list.add(Array("gzip"))
+    list.add(Array("snappy"))
+    list
+  }
+}
