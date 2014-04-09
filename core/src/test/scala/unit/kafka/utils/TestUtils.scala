47019a849e69209c16defa81001055aa9f57674d
Jun Rao <junrao@gmail.com>
2014-04-09 14:53:03 -0700

    kafka-1376; transient test failure in UncleanLeaderElectionTest; patched by Jun Rao; reviewed by Joel Koshy

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -163,7 +163,7 @@ object TestUtils extends Logging {
     props.put("group.id", groupId)
     props.put("consumer.id", consumerId)
     props.put("consumer.timeout.ms", consumerTimeout.toString)
-    props.put("zookeeper.session.timeout.ms", "400")
+    props.put("zookeeper.session.timeout.ms", "6000")
     props.put("zookeeper.sync.time.ms", "200")
     props.put("auto.commit.interval.ms", "1000")
     props.put("rebalance.max.retries", "4")
