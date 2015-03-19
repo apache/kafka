b65536b94705b0b890eb510df5610ab0e05df7e9
Seung-Yeoul Yang <syyang@uber.com>
2015-03-18 17:57:10 -0700

    [kafka] better error recovery in migrator tool (part 1)
    Summary:
    * Add a new gradle project for the migrator tool
    * Update .gitignore
    * KafkaMigrationTool is just copy-pasted.
    
    Test Plan:
      ./gradlew test
    
    Reviewers: grk, praveen, vinoth, csoman, norbert
    
    Reviewed By: norbert
    
    Subscribers: bigpunk, nharkins
    
    Maniphest Tasks: T67641
    
    Differential Revision: https://code.uberinternal.com/D83463

---
+++ migration/src/test/java/com/uber/kafka/tools/KafkaMigrationToolTest.java
@@ -0,0 +1,15 @@
+package com.uber.kafka.tools;
+
+import org.junit.Test;
+
+public class KafkaMigrationToolTest {
+
+    @Test
+    public void testFoo() throws Exception {
+        // InputStream is = getClass().getResourceAsStream("/offset_idx");
+        // byte[] encodedOffsets = ByteStreams.toByteArray(is);
+        // Assert.assertEquals(0, encodedOffsets.length);
+    }
+
+}
+
