f401990f13bddbd3d97e05756cf2f1abf367677e
Ismael Juma <ismael@juma.me.uk>
2015-04-21 01:23:39 +0100

    Minor clean-ups in `Json.encode`

--- core/src/main/scala/kafka/utils/Json.scala
+++ core/src/main/scala/kafka/utils/Json.scala
@@ -50,15 +50,14 @@ object Json {
       case n: Number => n.toString
       case m: Map[_, _] => 
         "{" + 
-          m.map(elem => 
-            elem match {
-            case t: Tuple2[_,_] => encode(t._1) + ":" + encode(t._2)
-            case _ => throw new IllegalArgumentException("Invalid map element (" + elem + ") in " + obj)
-          }).mkString(",") + 
-      "}"
+          m.map {
+            case (k, v) => encode(k) + ":" + encode(v)
+            case elem => throw new IllegalArgumentException("Invalid map element (" + elem + ") in " + obj)
+          }.mkString(",") +
+        "}"
       case a: Array[_] => encode(a.toSeq)
       case i: Iterable[_] => "[" + i.map(encode).mkString(",") + "]"
-      case other: AnyRef => throw new IllegalArgumentException("Unknown arguement of type " + other.getClass + ": " + other)
+      case other: AnyRef => throw new IllegalArgumentException("Unknown argument of type " + other.getClass + ": " + other)
     }
   }
 
