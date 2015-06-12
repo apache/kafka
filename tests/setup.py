7f7c3e0e68d9c3c50fb4b836b90887f39b43c466
Geoff Anderson <geoff@confluent.io>
2015-06-12 15:20:25 -0700

    Updated setup.py for kafkatest

--- tests/setup.py
+++ tests/setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(name="kafkatest",
-      version="0.1",
-      description="System tests for Apache Kafka",
-      author="Ewen Cheslack-Postava <ewen@confluent.io>, Geoff Anderson <geoff@confluent.io>",
+      version="0.8.3-SNAPSHOT",
+      description="Apache Kafka System Tests",
+      author="Apache Kafka",
       platforms=["any"], 
       license="apache2.0",
       packages=find_packages(),
+      requires=["ducktape(>=0.2.0)"]
       )
