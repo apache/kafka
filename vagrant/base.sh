e6d532f0a6e330f1734a4006e868efc1e760d661
Geoff Anderson <geoff@confluent.io>
2015-06-16 10:00:15 -0700

    java 7 -> java 6

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -24,16 +24,16 @@ if [ -z `which javac` ]; then
     apt-get -y update
 
     # Try to share cache. See Vagrantfile for details
-    mkdir -p /var/cache/oracle-jdk7-installer
-    if [ -e "/tmp/oracle-jdk7-installer-cache/" ]; then
-        find /tmp/oracle-jdk7-installer-cache/ -not -empty -exec cp '{}' /var/cache/oracle-jdk7-installer/ \;
+    mkdir -p /var/cache/oracle-jdk6-installer
+    if [ -e "/tmp/oracle-jdk6-installer-cache/" ]; then
+        find /tmp/oracle-jdk6-installer-cache/ -not -empty -exec cp '{}' /var/cache/oracle-jdk6-installer/ \;
     fi
 
     /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-    apt-get -y install oracle-java7-installer oracle-java7-set-default
+    apt-get -y install oracle-java6-installer oracle-java6-set-default
 
-    if [ -e "/tmp/oracle-jdk7-installer-cache/" ]; then
-        cp -R /var/cache/oracle-jdk7-installer/* /tmp/oracle-jdk7-installer-cache
+    if [ -e "/tmp/oracle-jdk6-installer-cache/" ]; then
+        cp -R /var/cache/oracle-jdk6-installer/* /tmp/oracle-jdk6-installer-cache
     fi
 fi
 
