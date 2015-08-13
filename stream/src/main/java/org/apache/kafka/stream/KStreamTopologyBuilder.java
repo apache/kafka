77c1ba24b384fa46d2045cacf8c58e86c4139980
Guozhang Wang <wangguoz@gmail.com>
2015-08-13 16:58:25 -0700

    Add two files

---
+++ stream/src/main/java/org/apache/kafka/stream/KStreamTopologyBuilder.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kafka.stream;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.stream.kstream.internals.KStreamImpl;
+import org.apache.kafka.stream.processor.KafkaSource;
+import org.apache.kafka.stream.processor.PTopologyBuilder;
+import org.apache.kafka.stream.kstream.internals.KStreamSource;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * KStreamTopologyBuilder is the class to create KStream instances.
+ */
+public class KStreamTopologyBuilder extends PTopologyBuilder {
+
+    public KStreamTopologyBuilder() {
+        super();
+    }
+
+    /**
+     * Creates a KStream instance for the specified topic. The stream is added to the default synchronization group.
+     *
+     * @param keyDeserializer key deserializer used to read this source KStream,
+     *                        if not specified the default deserializer defined in the configs will be used
+     * @param valDeserializer value deserializer used to read this source KStream,
+     *                        if not specified the default deserializer defined in the configs will be used
+     * @param topics          the topic names, if empty default to all the topics in the config
+     * @return KStream
+     */
+    @SuppressWarnings("unchecked")
+    public <K, V> KStream<K, V> from(Deserializer<? extends K> keyDeserializer, Deserializer<? extends V> valDeserializer, String... topics) {
+        KafkaSource source = addSource(KStreamImpl.SOURCE_NAME + KStreamImpl.INDEX.getAndIncrement(), keyDeserializer, valDeserializer, topics);
+        return new KStreamSource<>(this, source);
+    }
+}
