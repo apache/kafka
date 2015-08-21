c20a52c320f3652f672f60d7b86f38643a1ebf93
Ismael Juma <ismael@juma.me.uk>
2015-08-21 11:17:15 +0100

    Handle disconnections in `SelectorUtils`
    As suggested by Jun.

--- core/src/main/scala/kafka/utils/SelectorUtils.scala
+++ core/src/main/scala/kafka/utils/SelectorUtils.scala
@@ -17,6 +17,7 @@
 
 package kafka.utils
 
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters._
 
@@ -26,53 +27,64 @@ import org.apache.kafka.common.utils.{Time => JTime}
 object SelectorUtils {
 
   /**
-   * Invokes `selector.poll` until `id` is connected or the timeout expires. It returns `true` if the
-   * call completes normally or `false` if the timeout expires.
+   * Invokes `selector.poll` until `id` is connected, the timeout expires or a disconnection for `id` happens.
+   *
+   * It returns `true` if the call completes normally or `false` if the timeout expires. If `id` is disconnected,
+   * an `IOException` is thrown (an edge case, but it could happen).
    *
    * This method is useful for implementing blocking behaviour on top of the non-blocking `Selector`, use it with
    * care.
    */
   def pollUntilConnected(selector: Selector, id: String, timeout: Long)(implicit time: JTime): Boolean =
-    pollUntil(selector, timeout)(selector.connected.contains(id))
+    pollUntil(selector, id, timeout)(selector.connected.contains(id))
 
   /**
-   * Invokes `selector.poll` until a receive has been completed for `id` or the timeout expires. It returns the received
-   * value in a `Some` if the call completes normally or `None` if the timeout expires.
+   * Invokes `selector.poll` until a receive has been completed for `id`, the timeout expires or a disconnection for
+   * `id` happens.
+   *
+   * It returns the received value in a `Some` if the call completes normally or `None` if the timeout expires. If
+   * `id` is disconnected, an `IOException` is thrown instead.
    *
    * This method is useful for implementing blocking behaviour on top of the non-blocking `Selector`, use it with
    * care.
    */
   def pollUntilReceiveCompleted(selector: Selector, id: String, timeout: Long)(implicit time: JTime): Option[NetworkReceive] =
-    pollUntilFound(selector, timeout)(selector.completedReceives.asScala.find(_.source() == id))
+    pollUntilFound(selector, id, timeout)(selector.completedReceives.asScala.find(_.source() == id))
 
   /**
-   * Invokes `selector.poll` until `predicate` returns `true` or the timeout expires. It returns `true` if the call
-   * completes normally or `false` if the timeout expires.
+   * Invokes `selector.poll` until `predicate` returns `true`, the timeout expires or a disconnection for `id` happens.
+   *
+   * It returns `true` if the call completes normally or `false` if the timeout expires. If `id` is disconnected,
+   * an `IOException` is thrown instead.
    *
    * This method is useful for implementing blocking behaviour on top of the non-blocking `Selector`, use it with
    * care.
    */
-  private def pollUntil(selector: Selector, timeout: Long)(predicate: => Boolean)(implicit time: JTime): Boolean =
-    pollUntilFound(selector, timeout) {
+  private def pollUntil(selector: Selector, id: String, timeout: Long)(predicate: => Boolean)(implicit time: JTime): Boolean =
+    pollUntilFound(selector, id, timeout) {
       if (predicate) Some(true)
       else None
     }.fold(false)(_ => true)
 
   /**
-   * Invokes `selector.poll` until `find` returns `Some` or the timeout expires. It returns the result of `find` if
-   * the call completes normally or `None` if the timeout expires.
+   * Invokes `selector.poll` until `find` returns `Some`, the timeout expires or the connection with `id` is disconnected.
+   *
+   * It returns the result of `find` if the call completes normally or `None` if the timeout expires. If `id` `is
+   * disconnected, an `IOException` is thrown instead.
    *
    * This method is useful for implementing blocking behaviour on top of the non-blocking `Selector`, use it with
    * care.
    */
-  private def pollUntilFound[T](selector: Selector, timeout: Long)(find: => Option[T])(implicit time: JTime): Option[T] = {
+  private def pollUntilFound[T](selector: Selector, id: String, timeout: Long)(find: => Option[T])(implicit time: JTime): Option[T] = {
     var result: Option[T] = None
+    var isDisconnected = false
     // for consistency with `Selector.poll`
     if (timeout < 0) {
       do {
         selector.poll(timeout)
         result = find
-      } while (result.isEmpty)
+        isDisconnected = selector.disconnected.asScala.contains(id)
+      } while (result.isEmpty && !isDisconnected)
     }
     else {
       var timeRemaining = timeout
@@ -80,10 +92,13 @@ object SelectorUtils {
       do {
         selector.poll(timeRemaining)
         result = find
+        isDisconnected = selector.disconnected.asScala.contains(id)
         timeRemaining = endTime - TimeUnit.NANOSECONDS.toMillis(time.nanoseconds)
-      } while (result.isEmpty && timeRemaining > 0)
+      } while (result.isEmpty && !isDisconnected && timeRemaining > 0)
     }
-    result
+    if (result.isEmpty && isDisconnected)
+      throw new IOException(s"$id has been disconnected")
+    else result
   }
 
 }
