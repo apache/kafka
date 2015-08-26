446f8338927bfecf2aed46389661f9eacbf31cd4
David Jacot <david.jacot@gmail.com>
2015-08-26 19:59:11 +0200

    Address @ijuma comments.

--- clients/src/test/java/org/apache/kafka/common/requests/RequestResponseTest.java
+++ clients/src/test/java/org/apache/kafka/common/requests/RequestResponseTest.java
@@ -25,7 +25,12 @@ import org.junit.Test;
 
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
