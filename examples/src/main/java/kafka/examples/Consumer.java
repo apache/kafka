8241feb4d2d4ca6bc10b2bb727c92b10dc4d237b
Ashish Singh <asingh@cloudera.com>
2015-09-11 15:35:47 -0700

    KAFKA-2487: change kafka.examples.Consumer to use the new java consumer

--- examples/src/main/java/kafka/examples/Consumer.java
+++ examples/src/main/java/kafka/examples/Consumer.java
@@ -16,52 +16,40 @@
  */
 package kafka.examples;
 
-
-import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Collections;
 import java.util.Properties;
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.message.MessageAndMetadata;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class Consumer extends Thread
 {
-  private final ConsumerConnector consumer;
+  private final KafkaConsumer<Integer, String> consumer;
   private final String topic;
   
   public Consumer(String topic)
   {
-    consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
-            createConsumerConfig());
-    this.topic = topic;
-  }
-
-  private static ConsumerConfig createConsumerConfig()
-  {
     Properties props = new Properties();
-    props.put("zookeeper.connect", KafkaProperties.zkConnect);
-    props.put("group.id", KafkaProperties.groupId);
-    props.put("zookeeper.session.timeout.ms", "400");
-    props.put("zookeeper.sync.time.ms", "200");
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", "DemoConsumer");
+    props.put("enable.auto.commit", "true");
     props.put("auto.commit.interval.ms", "1000");
+    props.put("session.timeout.ms", "30000");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-    return new ConsumerConfig(props);
-
+    consumer = new KafkaConsumer<>(props);
+    this.topic = topic;
   }
  
   public void run() {
-    Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-    topicCountMap.put(topic, 1);
-    Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-    KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
-    for (MessageAndMetadata<byte[], byte[]> messageAndMetadata : stream) {
-      System.out.println("Received message: (" + ByteBuffer.wrap(messageAndMetadata.key()).getInt() +
-          ", " +
-          "" + new String(messageAndMetadata.message()) + ")");
+    consumer.subscribe(Collections.singletonList(this.topic));
+    while (true) {
+      ConsumerRecords<Integer, String> records = consumer.poll(1000);
+      for (ConsumerRecord<Integer, String> record: records) {
+        System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+      }
     }
   }
 }
