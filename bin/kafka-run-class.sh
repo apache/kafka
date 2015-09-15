547b6ee55584293b20890da2f8e16c96a5618ed3
Manikumar reddy O <manikumar.reddy@gmail.com>
2015-09-15 18:58:45 +0530

    KAFKA-2514; change default JVM options in kafka-run-class.sh. GC set to G1 collector.

--- bin/kafka-run-class.sh
+++ bin/kafka-run-class.sh
@@ -132,7 +132,7 @@ fi
 
 # JVM performance options
 if [ -z "$KAFKA_JVM_PERFORMANCE_OPTS" ]; then
-  KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:+CMSScavengeBeforeRemark -XX:+DisableExplicitGC -Djava.awt.headless=true"
+  KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
 fi
 
 
