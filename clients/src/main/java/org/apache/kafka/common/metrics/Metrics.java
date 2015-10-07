6bb0ddf87a9899cbf00d43f2e0d317705afef99c
Aditya Auradkar <aauradka@aauradka-mn1.(none)>
2015-10-07 08:57:03 -0700

    Addressing joels comments

--- clients/src/main/java/org/apache/kafka/common/metrics/Metrics.java
+++ clients/src/main/java/org/apache/kafka/common/metrics/Metrics.java
@@ -284,7 +284,7 @@ public class Metrics implements Closeable {
                 // removeSensor also locks the sensor object. This is fine because synchronized is reentrant
                 // There is however a minor race condition here. Assume we have a parent sensor P and child sensor C.
                 // Calling record on C would cause a record on P as well.
-                // So expiration time for P == C1. If the record on P happens via C just after P is removed,
+                // So expiration time for P == expiration time for C. If the record on P happens via C just after P is removed,
                 // that will cause C to also get removed.
                 // Since the expiration time is typically high it is not expected to be a significant concern
                 // and thus not necessary to optimize
--- core/src/main/scala/kafka/server/ClientQuotaManager.scala
+++ core/src/main/scala/kafka/server/ClientQuotaManager.scala
@@ -58,7 +58,7 @@ object ClientQuotaManagerConfig {
   val DefaultQuotaWindowSizeSeconds = 1
   val MaxThrottleTimeSeconds = 30
   // Purge sensors after 1 hour of inactivity
-  val inactiveSensorExpirationTimeSeconds  = 3600
+  val InactiveSensorExpirationTimeSeconds  = 3600
 }
 
 /**
@@ -200,7 +200,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
           // create the throttle time sensor also. Use default metric config
           throttleTimeSensor = metrics.sensor(throttleTimeSensorName,
                                               null,
-                                              ClientQuotaManagerConfig.inactiveSensorExpirationTimeSeconds)
+                                              ClientQuotaManagerConfig.InactiveSensorExpirationTimeSeconds)
           throttleTimeSensor.add(new MetricName("throttle-time",
                                                 apiKey,
                                                 "Tracking average throttle-time per client",
@@ -209,7 +209,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
 
           quotaSensor = metrics.sensor(quotaSensorName,
                                        getQuotaMetricConfig(quota(clientId)),
-                                       ClientQuotaManagerConfig.inactiveSensorExpirationTimeSeconds)
+                                       ClientQuotaManagerConfig.InactiveSensorExpirationTimeSeconds)
           quotaSensor.add(clientRateMetricName(clientId), new Rate())
         }
       } finally {
