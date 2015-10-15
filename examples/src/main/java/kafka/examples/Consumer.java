f6ea3c7d416f35244f6034b8fd7c82c218fc0b5d
Ashish Singh <asingh@cloudera.com>
2015-10-15 15:16:45 -0700

    Address review comments. 1. Extend ShutdownableThread instead of Thread. 2. Use conf name consts from ConsumerConfig

--- examples/src/main/java/kafka/examples/Consumer.java
+++ examples/src/main/java/kafka/examples/Consumer.java
@@ -19,37 +19,49 @@ package kafka.examples;
 import java.util.Collections;
 import java.util.Properties;
 
+import kafka.utils.ShutdownableThread;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
-public class Consumer extends Thread
+public class Consumer extends ShutdownableThread
 {
   private final KafkaConsumer<Integer, String> consumer;
   private final String topic;
-  
+
   public Consumer(String topic)
   {
+    super("KafkaConsumerExample", false);
     Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
-    props.put("group.id", "DemoConsumer");
-    props.put("enable.auto.commit", "true");
-    props.put("auto.commit.interval.ms", "1000");
-    props.put("session.timeout.ms", "30000");
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "DemoConsumer");
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
     consumer = new KafkaConsumer<>(props);
     this.topic = topic;
   }
- 
-  public void run() {
+
+  @Override
+  public void doWork() {
     consumer.subscribe(Collections.singletonList(this.topic));
-    while (true) {
-      ConsumerRecords<Integer, String> records = consumer.poll(1000);
-      for (ConsumerRecord<Integer, String> record: records) {
-        System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
-      }
+    ConsumerRecords<Integer, String> records = consumer.poll(1000);
+    for (ConsumerRecord<Integer, String> record : records) {
+      System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
     }
+  }
+
+  @Override
+  public String name() {
+    return null;
+  }
+
+  @Override
+  public boolean isInterruptible() {
+    return false;
   }
 }
