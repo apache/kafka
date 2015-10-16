25222bd494b371d3c5e9093492c2a97ac26d36b7
Flavio Junqueira <fpj@apache.org>
2015-10-16 23:14:40 +0100

    KAFKA-2640: Added license header and removed whitespaces.

--- core/src/test/scala/unit/kafka/security/auth/ZkAuthorizationTest.scala
+++ core/src/test/scala/unit/kafka/security/auth/ZkAuthorizationTest.scala
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package unit.kafka.security.auth
 
 import kafka.utils.{Logging, ZkUtils}
@@ -21,14 +38,14 @@ class ZkAuthorizationTest extends ZooKeeperTestHarness with Logging{
     System.setProperty(authProvider, "org.apache.zookeeper.server.auth.SASLAuthenticationProvider")
     super.setUp()
   }
-  
+
   @After
   override def tearDown() {
     super.tearDown()
   }
-  
+
   /**
-   * Tests the method in JaasUtils that checks whether to use 
+   * Tests the method in JaasUtils that checks whether to use
    * secure ACLs and authentication with ZooKeeper.
    */
   @Test
@@ -43,11 +60,11 @@ class ZkAuthorizationTest extends ZooKeeperTestHarness with Logging{
         // Expected
       }
       case e: Exception => {
-        throw e
+        fail(e.toString)
       }
     }
   }
-  
+
   /**
    * Tests ZkUtils. The goal is mainly to verify that the behavior of ZkUtils is
    * correct when isSecure is set to true.
@@ -71,7 +88,7 @@ class ZkAuthorizationTest extends ZooKeeperTestHarness with Logging{
               assertTrue(acl.getId.getScheme.equals("sasl"))
             }
             case _: Int => {
-             fail("Unrecognized ID scheme %d".format(acl.getPerms)) 
+             fail("Unrecognized ID scheme %d".format(acl.getPerms))
             }
           }
         }
