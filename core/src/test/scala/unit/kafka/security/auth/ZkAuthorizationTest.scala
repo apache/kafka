53ccead1f8300f18ccc46e1c959a87b4401fe07f
flavio junqueira <fpj@apache.org>
2015-10-16 11:17:13 +0100

    KAFKA-2640: Initial test passes.

--- core/src/test/scala/unit/kafka/security/auth/ZkAuthorizationTest.scala
+++ core/src/test/scala/unit/kafka/security/auth/ZkAuthorizationTest.scala
@@ -1,16 +1,34 @@
 package unit.kafka.security.auth
 
 import kafka.zk.ZooKeeperTestHarness
+import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.security.JaasUtils
 import org.junit.Assert._
 import org.junit.{Before, Test}
 
 class ZkAuthorizationTest extends ZooKeeperTestHarness {
-  val jaasFile: String = "./resources/zk-digest-jaas.conf"
+  val jaasFile: String = "zk-digest-jaas.conf"
+  
+  @Before
+  override def setUp() {
+    val classLoader = getClass.getClassLoader
+    val filePath = classLoader.getResource(jaasFile).getPath
+    System.setProperty(JaasUtils.JAVA_LOGIN_CONFIG_PARAM, filePath)
+  }
   
   @Test
   def testIsZkSecurityEnabled() {
-    assertTrue(JaasUtils.isZkSecurityEnabled(jaasFile))
-    assertFalse(JaasUtils.isZkSecurityEnabled(jaasFile))
+    assertTrue(JaasUtils.isZkSecurityEnabled(System.getProperty(JaasUtils.JAVA_LOGIN_CONFIG_PARAM)))
+    assertFalse(JaasUtils.isZkSecurityEnabled(""))
+    try {
+      assertFalse(JaasUtils.isZkSecurityEnabled("no-such-file-exists.conf"))
+    } catch {
+      case e: KafkaException => {
+        // Expected
+      }
+      case e: Exception => {
+        throw e
+      }
+    }
   }
 }
