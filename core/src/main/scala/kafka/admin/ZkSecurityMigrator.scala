1a8d6b702a6f0b4d35ba5c32499f094eab972c19
Flavio Junqueira <fpj@apache.org>
2015-10-19 12:03:10 +0100

    KAFKA-2641: Added functionality to set acls recursively.

--- core/src/main/scala/kafka/admin/ZkSecurityMigrator.scala
+++ core/src/main/scala/kafka/admin/ZkSecurityMigrator.scala
@@ -17,14 +17,103 @@
 
 package kafka.admin
 
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import javax.security.auth.login.LoginException
 import joptsimple.OptionParser
 import org.I0Itec.zkclient.ZkClient
-import kafka.utils.{ToolsUtils, Logging, ZKGroupTopicDirs, ZkUtils, CommandLineUtils}
+import kafka.utils.{ToolsUtils, Logging, ZKGroupTopicDirs, ZkUtils, CommandLineUtils, ZkPath}
 import org.apache.kafka.common.security.JaasUtils
 
-object ZkSecurityMigrator extends Logging {
+import org.apache.zookeeper.AsyncCallback.{ChildrenCallback, StatCallback}
+import org.apache.zookeeper.CreateMode
+import org.apache.zookeeper.data.Stat
+import org.apache.zookeeper.KeeperException
+import org.apache.zookeeper.KeeperException.Code
+import org.apache.zookeeper.ZooDefs.Ids
+import org.apache.zookeeper.ZooKeeper
 
+import scala.collection.JavaConverters._
+import scala.collection._
+import scala.concurrent._
+
+object ZkSecurityMigrator extends Logging {
+  val workQueue = new LinkedBlockingQueue[Runnable]
+  val threadExecutionContext = 
+    ExecutionContext.fromExecutor(new ThreadPoolExecutor(1,
+                                                         Runtime.getRuntime().availableProcessors(),
+                                                         5000,
+                                                         TimeUnit.MILLISECONDS,
+                                                         workQueue))
+
+  def setAclsRecursively(zkUtils: ZkUtils, path: String) {
+    zkUtils.zkConnection.getZookeeper.getChildren(path, false, GetChildrenCallback, zkUtils)
+  }
+
+  object GetChildrenCallback extends ChildrenCallback {
+      def processResult(rc: Int,
+                        path: String,
+                        ctx: Object,
+                        children: java.util.List[String]) {
+        val list = children.asScala.toList
+        val zkUtils: ZkUtils = ctx.asInstanceOf[ZkUtils]
+        val zkHandle = zkUtils.zkConnection.getZookeeper
+        
+        Code.get(rc) match {
+          case Code.OK =>
+            // Set ACL for each child
+            Future {
+              for(child <- list) {
+                zkHandle.setACL(child, ZkUtils.DefaultAcls(true), -1, SetACLCallback, ctx)
+                zkHandle.getChildren(child, false, GetChildrenCallback, ctx)
+              }
+            }(threadExecutionContext)
+          case Code.CONNECTIONLOSS =>
+            setAclsRecursively(zkUtils, path)
+          case Code.NONODE =>
+            warn("Node is gone, it could be have been legitimately deleted: %s".format(path))
+          case Code.SESSIONEXPIRED =>
+            // Starting a new session isn't really a problem, but it'd complicate
+            // the logic of the tool, so we quit and let the user re-run it.
+            error("ZooKeeper session expired while changing ACLs")
+            System.exit(1)
+          case _ =>
+            error("Unexpected return code: %d".format(rc))
+            System.exit(1)
+        }
+      }
+  }
+  
+  object SetACLCallback extends StatCallback {
+    def processResult(rc: Int,
+                      path: String,
+                      ctx: Object,
+                      stat: Stat) {
+      val zkUtils: ZkUtils = ctx.asInstanceOf[ZkUtils]
+      val zkHandle = zkUtils.zkConnection.getZookeeper
+        
+      Code.get(rc) match {
+          case Code.OK =>
+            info("Successfully set ACLs for %s".format(path))
+          case Code.CONNECTIONLOSS =>
+            Future {
+              zkHandle.setACL(path, ZkUtils.DefaultAcls(true), -1, SetACLCallback, ctx)
+            }(threadExecutionContext)
+          case Code.NONODE =>
+            warn("Node is gone, it could be have been legitimately deleted: %s".format(path))
+          case Code.SESSIONEXPIRED =>
+            // Starting a new session isn't really a problem, but it'd complicate
+            // the logic of the tool, so we quit and let the user re-run it.
+            error("ZooKeeper session expired while changing ACLs")
+            System.exit(1)
+          case _ =>
+            error("Unexpected return code: %d".format(rc))
+            System.exit(1)   
+      }
+    }
+  }
+  
   def main(args: Array[String]) {
     val parser = new OptionParser()
 
@@ -60,6 +149,7 @@ object ZkSecurityMigrator extends Logging {
     val zkUtils = ZkUtils.apply(zkUrl, zkSessionTimeout, zkConnectionTimeout, true)
     for (path <- zkUtils.persistentZkPaths) {
       zkUtils.makeSurePersistentPathExists(path)
-    }
+      setAclsRecursively(zkUtils, path)
+    } 
   }
 }
