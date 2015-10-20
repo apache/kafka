4578e502be64302a5040779845e7e56b1e314ca9
Guozhang Wang <wangguoz@gmail.com>
2015-10-19 19:31:45 -0700

    comment minor fix

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
@@ -513,7 +513,7 @@ public class Fetcher<K, V> {
                         this.records.add(new PartitionRecords<>(fetchOffset, tp, parsed));
                         this.sensors.recordsFetchLag.record(partition.highWatermark - record.offset());
                     } else if (buffer.limit() > 0) {
-                        // we did not read a single message from an non-empty buffer
+                        // we did not read a single message from a non-empty buffer
                         // because that message's size is larger than fetch size, in this case
                         // record this exception
                         this.recordTooLargePartitions.put(tp, fetchOffset);
