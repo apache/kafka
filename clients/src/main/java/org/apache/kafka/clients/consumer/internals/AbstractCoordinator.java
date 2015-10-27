0b05d3b939c5ed37a4253e7c3614d824e76ed664
Jason Gustafson <jason@confluent.io>
2015-10-27 16:24:10 -0700

    KAFKA-2677: ensure consumer sees coordinator disconnects
    Author: Jason Gustafson <jason@confluent.io>
    
    Reviewers: Ismael Juma, Guozhang Wang
    
    Closes #349 from hachikuji/KAFKA-2677

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
@@ -477,6 +477,8 @@ public abstract class AbstractCoordinator {
                         groupMetadataResponse.node().host(),
                         groupMetadataResponse.node().port());
 
+                client.tryConnect(coordinator);
+
                 // start sending heartbeats only if we have a valid generation
                 if (generation > 0)
                     heartbeatTask.reset();
@@ -488,11 +490,19 @@ public abstract class AbstractCoordinator {
     }
 
     /**
-     * Check if we know who the coordinator is.
+     * Check if we know who the coordinator is and we have an active connection
      * @return true if the coordinator is unknown
      */
     public boolean coordinatorUnknown() {
-        return this.coordinator == null;
+        if (coordinator == null)
+            return true;
+
+        if (client.connectionFailed(coordinator)) {
+            coordinatorDead();
+            return true;
+        }
+
+        return false;
     }
 
 
