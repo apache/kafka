da1a5d3efac71b32276405cc1b589672bbff5070
Anna Povzner <anna@confluent.io>
2015-11-06 16:10:04 -0800

    KAFKA-2769: More reliable sending of leave group request in the consumer on consumer close().

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
@@ -548,7 +548,6 @@ public abstract class AbstractCoordinator implements Closeable {
      */
     public void maybeLeaveGroup(boolean awaitResponse) {
         client.unschedule(heartbeatTask);
-        client.ensureFreshMetadata();
         if (!coordinatorUnknown() && generation > 0) {
             // this is a minimal effort attempt to leave the group. we do not
             // attempt any resending if the request fails or times out.
@@ -562,7 +561,7 @@ public abstract class AbstractCoordinator implements Closeable {
 
     private void sendLeaveGroupRequest(boolean awaitResponse) {
         LeaveGroupRequest request = new LeaveGroupRequest(groupId, memberId);
-        RequestFuture<Void> future = client.send(coordinator, ApiKeys.LEAVE_GROUP, request)
+        RequestFuture<Void> future = client.sendWithRetries(coordinator, ApiKeys.LEAVE_GROUP, request)
                 .compose(new LeaveGroupResponseHandler());
 
         future.addListener(new RequestFutureListener<Void>() {
