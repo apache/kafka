75db96e429de34373b92d8f4f5df508071b05127
Ewen Cheslack-Postava <me@ewencp.org>
2015-11-10 14:43:25 -0800

    KAFKA-2793: Use ByteArrayDeserializer instead of StringDeserializer for keys in ConsoleConsumer with new consumer.
    Author: Ewen Cheslack-Postava <me@ewencp.org>
    
    Reviewers: Gwen Shapira
    
    Closes #482 from ewencp/kafka-2793-console-consumer-new-consumer-deserializer
    
    (cherry picked from commit 64a1bfeb9b1b90c94cc62ee2587f9745c850ada3)
    Signed-off-by: Gwen Shapira <cshapi@gmail.com>

--- core/src/main/scala/kafka/tools/ConsoleConsumer.scala
+++ core/src/main/scala/kafka/tools/ConsoleConsumer.scala
@@ -162,7 +162,7 @@ object ConsoleConsumer extends Logging {
     props.putAll(config.consumerProps)
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, if (config.options.has(config.resetBeginningOpt)) "earliest" else "latest")
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServer)
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, if (config.keyDeserializer != null) config.keyDeserializer else "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, if (config.keyDeserializer != null) config.keyDeserializer else "org.apache.kafka.common.serialization.ByteArrayDeserializer")
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, if (config.valueDeserializer != null) config.valueDeserializer else "org.apache.kafka.common.serialization.ByteArrayDeserializer")
 
     props
