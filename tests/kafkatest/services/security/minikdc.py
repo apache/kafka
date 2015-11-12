0c71136a43df706d01111c11595a0d835fb26e47
Anna Povzner <anna@confluent.io>
2015-11-12 13:48:47 -0800

    Fix to make sure that temp dir for mini kcd files is removed after test finishes.

--- tests/kafkatest/services/security/minikdc.py
+++ tests/kafkatest/services/security/minikdc.py
@@ -50,7 +50,6 @@ class MiniKdc(Service):
         try:
             self.local_temp_dir = tempfile.mkdtemp(dir=self.local_temp_dir)
             self.logger.debug("Created temporary local directory %s" % (self.local_temp_dir))
-            # os.chmod(self.local_temp_dir, 0755)
         except OSError as e:
             raise Exception("Failed to create temporary tocal directory for $s and %s files: %s" % (self.LOCAL_KEYTAB_FILENAME, self.LOCAL_KRB5CONF_FILENAME, e.strerror))
 
@@ -77,6 +76,4 @@ class MiniKdc(Service):
     def clean_node(self, node):
         node.account.kill_process("apacheds", clean_shutdown=False, allow_fail=False)
         node.account.ssh("rm -rf " + MiniKdc.WORK_DIR, allow_fail=False)
-
-
 
