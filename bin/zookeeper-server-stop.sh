8775d9bae5c1abf490cbe0c256944544aeda7420
Michael G. Noll <michael@confluent.io>
2015-11-23 10:10:25 +0100

    KAFKA-2874: shutdown ZK process reliably

--- bin/zookeeper-server-stop.sh
+++ bin/zookeeper-server-stop.sh
@@ -13,4 +13,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ps ax | grep -i 'zookeeper' | grep -v grep | awk '{print $1}' | xargs kill -SIGINT
+ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print $1}' | xargs kill -SIGTERM
