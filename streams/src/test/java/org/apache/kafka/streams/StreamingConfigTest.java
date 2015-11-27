d4cc41cf24053ba800d5ff864e48342278cb3240
bbejeck <bbejeck@gmail.com>
2015-11-27 17:49:23 -0500

    KAKFA-2902 added license, checkstyle fixes

--- streams/src/test/java/org/apache/kafka/streams/StreamingConfigTest.java
+++ streams/src/test/java/org/apache/kafka/streams/StreamingConfigTest.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.kafka.streams;
 
 import org.apache.kafka.common.serialization.IntegerDeserializer;
@@ -11,9 +28,8 @@ import org.junit.Test;
 
 import java.util.Map;
 import java.util.Properties;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.hamcrest.CoreMatchers.is;
 
 
 /**
@@ -30,7 +46,7 @@ public class StreamingConfigTest {
     @Before
     public void setUp() {
         props.put(StreamingConfig.CLIENT_ID_CONFIG, "Example-Processor-Job");
-        props.put("group.id","test-consumer-group");
+        props.put("group.id", "test-consumer-group");
         props.put(StreamingConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamingConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(StreamingConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
@@ -44,14 +60,14 @@ public class StreamingConfigTest {
 
     @Test
     public void testGetConsumerConfigs() throws Exception {
-        Map<String,Object> returnedProps = streamingConfig.getConsumerConfigs(streamThreadPlaceHolder);
-        assertThat(returnedProps.get("group.id"),is("test-consumer-group"));
+        Map<String, Object> returnedProps = streamingConfig.getConsumerConfigs(streamThreadPlaceHolder);
+        assertEquals(returnedProps.get("group.id"), "test-consumer-group");
 
     }
 
     @Test
     public void testGetRestoreConsumerConfigs() throws Exception {
-        Map<String,Object> returnedProps = streamingConfig.getRestoreConsumerConfigs();
+        Map<String, Object> returnedProps = streamingConfig.getRestoreConsumerConfigs();
         assertNull(returnedProps.get("group.id"));
     }
 }
