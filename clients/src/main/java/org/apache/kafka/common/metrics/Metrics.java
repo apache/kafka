f90670621c8fab3a60ee62ec428fcd0452178f1c
Ismael Juma <ismael@juma.me.uk>
2015-12-09 09:52:17 -0800

    KAFKA-2973; Fix leak of child sensors on remove
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Gwen Shapira, Guozhang Wang
    
    Closes #650 from ijuma/kafka-2973-fix-leak-child-sensors-on-remove
    
    (cherry picked from commit 977ebbe9bafb6c1a6e1be69620f745712118fe80)
    Signed-off-by: Gwen Shapira <cshapi@gmail.com>

--- clients/src/main/java/org/apache/kafka/common/metrics/Metrics.java
+++ clients/src/main/java/org/apache/kafka/common/metrics/Metrics.java
@@ -190,7 +190,7 @@ public class Metrics implements Closeable {
             this.sensors.put(name, s);
             if (parents != null) {
                 for (Sensor parent : parents) {
-                    List<Sensor> children = childrenSensors.get(parent.name());
+                    List<Sensor> children = childrenSensors.get(parent);
                     if (children == null) {
                         children = new ArrayList<>();
                         childrenSensors.put(parent, children);
