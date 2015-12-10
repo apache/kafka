416210ec55fa0ca3dbda2b875feeb2db6ac38cc7
Jin Xing <jinxing@fenbi.com>
2015-12-10 10:49:02 -0800

    KAFKA-2893: Add a simple non-negative partition seek check
    Author: jinxing <jinxing@fenbi.com>
    Author: ZoneMayor <jinxing6042@126.com>
    
    Reviewers: Guozhang Wang
    
    Closes #628 from ZoneMayor/trunk-KAFKA-2893
    
    (cherry picked from commit 9d23b512ccb95940782f51c93bd6cdbed3e938db)
    Signed-off-by: Guozhang Wang <wangguoz@gmail.com>

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -1008,6 +1008,9 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
      */
     @Override
     public void seek(TopicPartition partition, long offset) {
+        if (offset < 0) {
+            throw new IllegalArgumentException("seek offset must not be a negative number");
+        }
         acquire();
         try {
             log.debug("Seeking to offset {} for partition {}", offset, partition);
