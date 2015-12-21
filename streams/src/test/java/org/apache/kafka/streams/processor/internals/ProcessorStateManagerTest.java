bd20d4b362dd872458559610920f10e15f2a1f0a
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-12-21 12:08:10 +0100

    fix checkstyle desiderata

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/ProcessorStateManagerTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/ProcessorStateManagerTest.java
@@ -40,7 +40,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
--- streams/src/test/java/org/apache/kafka/streams/processor/internals/ProcessorTopologyTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/ProcessorTopologyTest.java
@@ -45,7 +45,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Collection;
 import java.util.Properties;
 
 public class ProcessorTopologyTest {
