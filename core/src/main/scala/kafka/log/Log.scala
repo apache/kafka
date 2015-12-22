dc6002b59b8e0fbbc65bad9c8b4294eb9a2d981d
Mayuresh Gharat <mgharat@mgharat-ld1.linkedin.biz>
2015-12-21 16:00:50 -0800

    Removed a bug from earlier commit

--- core/src/main/scala/kafka/log/Log.scala
+++ core/src/main/scala/kafka/log/Log.scala
@@ -200,10 +200,8 @@ class Log(@volatile var dir: File,
           }
         }
         else {
-          if (!dir.getAbsolutePath.endsWith(Log.DeleteDirSuffix)) {
-            recoverLog()
-            activeSegment.index.resize(config.maxIndexSize)
-          }
+          error("Could not find index file corresponding to log file %s, rebuilding index...".format(segment.log.file.getAbsolutePath))
+          segment.recover(config.maxMessageSize)
         }
         segments.put(start, segment)
       }
@@ -242,9 +240,11 @@ class Log(@volatile var dir: File,
                                      initFileSize = this.initFileSize(),
                                      preallocate = config.preallocate))
     } else {
-      recoverLog()
-      // reset the index size of the currently active log segment to allow more entries
-      activeSegment.index.resize(config.maxIndexSize)
+      if (!dir.getAbsolutePath.endsWith(Log.DeleteDirSuffix)) {
+        recoverLog()
+        // reset the index size of the currently active log segment to allow more entries
+        activeSegment.index.resize(config.maxIndexSize)
+      }
     }
 
   }
