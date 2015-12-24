ea73356905ff4663d0d085c46b348fa4e9b9568d
Sasaki Toru <sasakitoa@nttdata.co.jp>
2015-12-23 20:55:01 -0800

    KAFKA-3002; Allow uppercase letters in hostnames
    Make available to specify hostname with Uppercase at broker list
    
    Author: Sasaki Toru <sasakitoa@nttdata.co.jp>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>, Grant Henke <granthenke@gmail.com>, Ewen Cheslack-Postava <ewen@confluent.io>
    
    Closes #685 from sasakitoa/hostname_uppercase

--- clients/src/main/java/org/apache/kafka/common/utils/Utils.java
+++ clients/src/main/java/org/apache/kafka/common/utils/Utils.java
@@ -47,7 +47,7 @@ public class Utils {
 
     // This matches URIs of formats: host:port and protocol:\\host:port
     // IPv6 is supported with [ip] pattern
-    private static final Pattern HOST_PORT_PATTERN = Pattern.compile(".*?\\[?([0-9a-z\\-.:]*)\\]?:([0-9]+)");
+    private static final Pattern HOST_PORT_PATTERN = Pattern.compile(".*?\\[?([0-9a-zA-Z\\-.:]*)\\]?:([0-9]+)");
 
     public static final String NL = System.getProperty("line.separator");
 
