f90863f255ac4583d7e47ea18585a222f7ead84f
Kim Christensen <kich@mvno.dk>
2016-01-28 22:08:48 +0100

    Remove unit part from package names

--- core/src/test/scala/unit/kafka/security/auth/AclTest.scala
+++ core/src/test/scala/unit/kafka/security/auth/AclTest.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package unit.kafka.security.auth
+package kafka.security.auth
 
 import kafka.security.auth._
 import kafka.utils.Json
