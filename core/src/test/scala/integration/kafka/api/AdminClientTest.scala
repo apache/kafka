a58b459bddd5b1dcab224b53cda0196e947a5b09
Kim Christensen <kich@mvno.dk>
2016-01-29 09:29:09 -0800

    KAFKA-2676: Fix incorrect package name in tests
    Also fixed a couple of other tests with the same issue.
    
    This is my original work and I license the work to the project under the project's open source license
    
    Author: Kim Christensen <kich@mvno.dk>
    
    Reviewers: Ismael Juma
    
    Closes #828 from kichristensen/KAFKA-2676

--- core/src/test/scala/integration/kafka/api/AdminClientTest.scala
+++ core/src/test/scala/integration/kafka/api/AdminClientTest.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package integration.kafka.api
+package kafka.api
 
 import kafka.admin.AdminClient
 import kafka.api.IntegrationTestHarness
--- core/src/test/scala/unit/kafka/admin/AclCommandTest.scala
+++ core/src/test/scala/unit/kafka/admin/AclCommandTest.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package unit.kafka.admin
+package kafka.admin
 
 import java.io.StringReader
 import java.util.Properties
