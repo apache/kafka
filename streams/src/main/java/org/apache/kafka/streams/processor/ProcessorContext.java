8189f9d58002ec0092737741bf6c74eebab4dc73
Yasuhiro Matsuda <yasuhiro@confluent.io>
2016-02-02 10:52:43 -0800

    MINOR: some javadocs for kstream public api
    guozhangwang
    
    Author: Yasuhiro Matsuda <yasuhiro@confluent.io>
    
    Reviewers: Guozhang Wang
    
    Closes #844 from ymatsuda/javadoc

--- streams/src/main/java/org/apache/kafka/streams/processor/ProcessorContext.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/ProcessorContext.java
@@ -83,19 +83,60 @@ public interface ProcessorContext {
 
     StateStore getStateStore(String name);
 
+    /**
+     * Schedules a periodic operation for processors. A processor may call this method during
+     * {@link Processor#init(ProcessorContext) initialization} to
+     * schedule a periodic call called a punctuation to {@link Processor#punctuate(long)}.
+     *
+     * @param interval the time interval between punctuations
+     */
     void schedule(long interval);
 
+    /**
+     * Forwards a key/value pair to the downstream processors
+     * @param key key
+     * @param value value
+     */
     <K, V> void forward(K key, V value);
 
+    /**
+     * Forwards a key/value pair to one of the downstream processors designated by childIndex
+     * @param key key
+     * @param value value
+     */
     <K, V> void forward(K key, V value, int childIndex);
 
+    /**
+     * Requests a commit
+     */
     void commit();
 
+    /**
+     * Returns the topic name of the current input record
+     *
+     * @return the topic name
+     */
     String topic();
 
+    /**
+     * Returns the partition id of the current input record
+     *
+     * @return the partition id
+     */
     int partition();
 
+    /**
+     * Returns the offset of the current input record
+     *
+     * @return the offset
+     */
     long offset();
 
+    /**
+     * Returns the timestamp of the current input record. The timestamp is extracted from
+     * {@link org.apache.kafka.clients.consumer.ConsumerRecord ConsumerRecord} by {@link TimestampExtractor}.
+     *
+     * @return the timestamp
+     */
     long timestamp();
 }
