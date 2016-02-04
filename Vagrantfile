apache/kafka

commit c8b60b6344e659b805ea04fc976abcae2bf9fcf8

MINOR: Fixed undefined method `update_guest'
Author: Piotr Szwed <pszwed@gmail.com>

Reviewers: Ewen Cheslack-Postava <ewen@confluent.io>

Closes #802 from szwed/trunk

Vagrantfile
@@ -63,12 +63,12 @@ end
 # stopped, skipping the update in that case since it's impossible to update
 # nodes in that state.
 Object.const_get("VagrantPlugins").const_get("HostManager").const_get("HostsFile").class_eval do
-  alias_method :old_update_guest, :update_guest
   def update_guest(machine)
     state_id = machine.state.id
     return if state_id == :not_created || state_id == :stopped
     old_update_guest(machine)
   end
+  alias_method :old_update_guest, :update_guest
 end
 
 # TODO(ksweeney): RAM requirements are not empirical and can probably be significantly lowered.

commit 287e45ab40ad744405ee2008b51caa7922f1b7b2

MINOR: Removed unnecessary Vagrantfile hack
The hack here is no longer necessary with up-to-date versions of Vagrant, vagrant-hostmanager, and vagrant-aws. What's more, the change in c8b60b63 caused a chain of infinite recursion on OSX, preventing bringup of VMs on a typical laptop.

Author: Geoff Anderson <geoff@confluent.io>

Reviewers: Grant Henke <granthenke@gmail.com>, Ewen Cheslack-Postava <ewen@confluent.io>

Closes #867 from granders/remove-vagrantfile-hack and squashes the following commits:

14f4395 [Geoff Anderson] Removed uneccessary references to version 1.5.0 of vagrant-hostmanager
8799afe [Geoff Anderson] Removed Vagrantfile hack which is no longer necessary with up-to-date versions of Vagrant, vagrant-hostmanager, and vagrant-aws

Vagrantfile
@@ -53,24 +53,6 @@ if File.exists?(local_config_file) then
   eval(File.read(local_config_file), binding, "Vagrantfile.local")
 end
 
-# This is a horrible hack to work around bad interactions between
-# vagrant-hostmanager and vagrant-aws/vagrant's implementation. Hostmanager
-# wants to update the /etc/hosts entries, but tries to do so even on nodes that
-# aren't up (e.g. even when all nodes are stopped and you run vagrant
-# destroy). Because of the way the underlying code in vagrant works, it still
-# tries to communicate with the node and has to wait for a very long
-# timeout. This modifies the update to check for hosts that are not created or
-# stopped, skipping the update in that case since it's impossible to update
-# nodes in that state.
-Object.const_get("VagrantPlugins").const_get("HostManager").const_get("HostsFile").class_eval do
-  def update_guest(machine)
-    state_id = machine.state.id
-    return if state_id == :not_created || state_id == :stopped
-    old_update_guest(machine)
-  end
-  alias_method :old_update_guest, :update_guest
-end
-
 # TODO(ksweeney): RAM requirements are not empirical and can probably be significantly lowered.
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.hostmanager.enabled = true

tests/bootstrap-test-env.sh
@@ -46,17 +46,9 @@ if [ "x$bad_vagrant" == "xtrue" -o "x$bad_vb" == "xtrue" ]; then
 fi
 
 echo "Checking for necessary Vagrant plugins..."
-install_hostmanager=false
 hostmanager_version=`vagrant plugin list | grep vagrant-hostmanager | egrep -o "[0-9]+\.[0-9]+\.[0-9]+"`
 if [ -z "$hostmanager_version"  ]; then
-    install_hostmanager=true
-elif [ "$hostmanager_version" != "1.5.0" ]; then
-    echo "You have the wrong version of vagrant plugin vagrant-hostmanager. Uninstalling..."
-    vagrant plugin uninstall vagrant-hostmanager
-    install_hostmanager=true
-fi
-if [ "x$install_hostmanager" == "xtrue" ]; then
-    vagrant plugin install vagrant-hostmanager --plugin-version 1.5.0
+    vagrant plugin install vagrant-hostmanager
 fi
 
 echo "Creating and packaging a reusable base box for Vagrant..."