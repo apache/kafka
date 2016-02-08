99c62e46fef6771909e7fdf6d1c839fb501e3996
Geoff Anderson <geoff@confluent.io>
2016-02-08 14:07:59 -0800

    Match timeout in trunk in producer check

--- tests/kafkatest/tests/produce_consume_validate.py
+++ tests/kafkatest/tests/produce_consume_validate.py
@@ -51,7 +51,7 @@ class ProduceConsumeValidateTest(Test):
 
         # Check that producer is still successfully producing
         currently_acked = self.producer.num_acked
-        wait_until(lambda: self.producer.num_acked > currently_acked + 5, timeout_sec=10,
+        wait_until(lambda: self.producer.num_acked > currently_acked + 5, timeout_sec=30,
              err_msg="Expected producer to still be producing.")
 
         self.producer.stop()
