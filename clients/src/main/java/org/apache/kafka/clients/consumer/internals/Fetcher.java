9a2d1c39179987978456eb77c8e6b7dc1cb69014
Yifan Ying <yying@fitbit.com>
2016-02-19 17:36:33 -0800

    KAFKA-3233: prefix consumer per-topic metrics with 'topic' string

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
@@ -704,7 +704,7 @@ public class Fetcher<K, V> {
 
         public void recordTopicFetchMetrics(String topic, int bytes, int records) {
             Map<String, String> metricTags = new HashMap<>();
-            metricTags.put("topic", topic);
+            metricTags.put("topic", "topic-" + topic.replace(".", "_"));
 
             // record bytes fetched
             String name = "topic." + topic + ".bytes-fetched";
