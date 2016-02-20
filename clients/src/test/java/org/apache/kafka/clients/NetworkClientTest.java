4d082ad9157aa9ca013b10ed7ee145695e41ce14
Frank Scholten <frank@frankscholten.nl>
2016-02-20 18:45:36 +0100

    Added unit tests for NetworkClient.connectionDelay(Node node, long now)

--- clients/src/test/java/org/apache/kafka/clients/NetworkClientTest.java
+++ clients/src/test/java/org/apache/kafka/clients/NetworkClientTest.java
@@ -175,6 +175,39 @@ public class NetworkClientTest {
         assertEquals("There should be NO leastloadednode", leastNode, null);
         
     }
+
+    @Test
+    public void testConnectionDelay() {
+        long now = time.milliseconds();
+        long delay = client.connectionDelay(node, now);
+
+        assertEquals(0, delay);
+    }
+
+    @Test
+    public void testConnectionDelay_connected() {
+        client.ready(node, time.milliseconds());
+        awaitReady(client, node);
+
+        long now = time.milliseconds();
+        long delay = client.connectionDelay(node, now);
+
+        assertEquals(Long.MAX_VALUE, delay);
+    }
+
+    @Test
+    public void testConnectionDelay_disconnected() {
+        client.ready(node, time.milliseconds());
+        awaitReady(client, node);
+
+        selector.close(node.idString());
+
+        client.poll(requestTimeoutMs, time.milliseconds());
+
+        long delay = client.connectionDelay(node, time.milliseconds());
+
+        assertEquals(reconnectBackoffMsTest, delay);
+    }
     
     private static class TestCallbackHandler implements RequestCompletionHandler {
         public boolean executed = false;
