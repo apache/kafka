e3ab96b2f0b429e0fe5991a185cd980b0d490e25
Ismael Juma <ismael@juma.me.uk>
2016-02-22 11:40:33 -0800

    MINOR: Example style improvements
    These are minor, but no reason to make our example code look worse than it has to.
    
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Grant Henke
    
    Closes #940 from ijuma/example-style-improvements

--- examples/src/main/java/kafka/examples/KafkaProperties.java
+++ examples/src/main/java/kafka/examples/KafkaProperties.java
@@ -16,16 +16,15 @@
  */
 package kafka.examples;
 
-public interface KafkaProperties {
-    String ZK_CONNECT = "127.0.0.1:2181";
-    String GROUP_ID = "group1";
-    String TOPIC = "topic1";
-    String KAFKA_SERVER_URL = "localhost";
-    int KAFKA_SERVER_PORT = 9092;
-    int KAFKA_PRODUCER_BUFFER_SIZE = 64 * 1024;
-    int CONNECTION_TIMEOUT = 100000;
-    int RECONNECT_INTERVAL = 10000;
-    String TOPIC2 = "topic2";
-    String TOPIC3 = "topic3";
-    String CLIENT_ID = "SimpleConsumerDemoClient";
+public class KafkaProperties {
+    public static final String TOPIC = "topic1";
+    public static final String KAFKA_SERVER_URL = "localhost";
+    public static final int KAFKA_SERVER_PORT = 9092;
+    public static final int KAFKA_PRODUCER_BUFFER_SIZE = 64 * 1024;
+    public static final int CONNECTION_TIMEOUT = 100000;
+    public static final String TOPIC2 = "topic2";
+    public static final String TOPIC3 = "topic3";
+    public static final String CLIENT_ID = "SimpleConsumerDemoClient";
+
+    private KafkaProperties() {}
 }
