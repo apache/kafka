d459d199f01b0ddf75363b423f859882b366005a
Sasaki Toru <sasakitoa@nttdata.co.jp>
2016-02-25 17:43:13 +0900

    Improve stop script's message when no processes are running.

--- bin/zookeeper-server-stop.sh
+++ bin/zookeeper-server-stop.sh
@@ -13,4 +13,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print $1}' | xargs kill -SIGTERM
+PIDS=$(ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print $1}')
+
+if [ -z "$PIDS" ]; then
+  echo "No zookeeper server to stop"
+  exit 1
+else
+  kill -SIGTERM $PIDS
+fi
+
