2f24e1604730097a0d0968722bf99dcf170ec74e
Anna Povzner <anna@confluent.io>
2016-03-03 19:01:46 -0800

    cleanup

--- tests/kafkatest/tests/upgrade_test.py
+++ tests/kafkatest/tests/upgrade_test.py
@@ -22,7 +22,6 @@ from kafkatest.services.console_consumer import ConsoleConsumer
 from kafkatest.services.kafka import config_property
 from kafkatest.tests.produce_consume_validate import ProduceConsumeValidateTest
 from kafkatest.utils import is_int
-import time
 
 
 class TestUpgrade(ProduceConsumeValidateTest):
@@ -41,7 +40,6 @@ class TestUpgrade(ProduceConsumeValidateTest):
         self.num_consumers = 1
 
     def perform_upgrade(self, from_kafka_version, to_message_format_version=None):
-        time.sleep(20)
         self.logger.info("First pass bounce - rolling upgrade")
         for node in self.kafka.nodes:
             self.kafka.stop_node(node)
@@ -49,8 +47,6 @@ class TestUpgrade(ProduceConsumeValidateTest):
             node.config[config_property.INTER_BROKER_PROTOCOL_VERSION] = from_kafka_version
             node.config[config_property.MESSAGE_FORMAT_VERSION] = from_kafka_version
             self.kafka.start_node(node)
-
-        time.sleep(10)
 
         self.logger.info("Second pass bounce - remove inter.broker.protocol.version config")
         for node in self.kafka.nodes:
@@ -62,7 +58,6 @@ class TestUpgrade(ProduceConsumeValidateTest):
                 node.config[config_property.MESSAGE_FORMAT_VERSION] = to_message_format_version
             self.kafka.start_node(node)
 
-        time.sleep(30)
 
     @parametrize(from_kafka_version=str(LATEST_0_9), to_message_format_version=None, compression_types=["none"])
     @parametrize(from_kafka_version=str(LATEST_0_9), to_message_format_version=None, compression_types=["snappy"], new_consumer=True)
