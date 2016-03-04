9e38416c78b5eace1909da712ccc3e4d9d986b15
stephen powis <spowis@salesforce.com>
2016-03-04 11:52:01 -0500

    [KAFKA-3333] - fix checkstyle violations

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/RoundRobinPartitionerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/RoundRobinPartitionerTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.clients.producer.internals;
 
 import org.apache.kafka.clients.producer.Partitioner;
@@ -10,7 +26,8 @@ import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test the RoundRobinPartitioner.
@@ -76,7 +93,7 @@ public class RoundRobinPartitionerTest {
 
         // When there are some unavailable partitions, we want to make sure that (1) we always pick an available partition,
         // and (2) the available partitions are selected in a round robin way.
-        int nodeCounts[] = { 0, 0, 0, 0 };
+        int[] nodeCounts = {0, 0, 0, 0};
         for (int i = 1; i <= 100; i++) {
             int part = partitioner.partition("test", null, null, null, null, cluster);
             assertTrue("Node should exist", part >= 0 && part <= 3);
