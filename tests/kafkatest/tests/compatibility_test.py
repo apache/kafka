b2d320daf2e81cc19d24862ebaa06cdc67ef32cf
Eno Thereska <eno.thereska@gmail.com>
2016-03-14 10:36:03 +0000

    Use version 0.8 and 0.9 for this test as originally intended

--- tests/kafkatest/tests/compatibility_test.py
+++ tests/kafkatest/tests/compatibility_test.py
@@ -16,7 +16,7 @@ from ducktape.tests.test import Test
 
 from kafkatest.services.zookeeper import ZookeeperService
 from kafkatest.services.kafka import KafkaService
-from kafkatest.services.kafka.version import LATEST_0_8_2, TRUNK
+from kafkatest.services.kafka.version import LATEST_0_8_2, LATEST_0_9
 from kafkatest.services.verifiable_producer import VerifiableProducer
 from kafkatest.services.console_consumer import ConsoleConsumer
 from kafkatest.utils import is_int
@@ -50,7 +50,7 @@ class ClientCompatibilityTest(Test):
         """
         self.producer = VerifiableProducer(
             self.test_context, self.num_producers, self.kafka, self.topic, max_messages=100,
-            throughput=self.producer_throughput, version=TRUNK)
+            throughput=self.producer_throughput, version=LATEST_0_9)
 
         node = self.producer.nodes[0]
         try:
@@ -64,11 +64,11 @@ class ClientCompatibilityTest(Test):
             self.producer.kill_node(node, clean_shutdown=False)
 
         self.logger.info("Grepping producer log for expected error type")
-        node.account.ssh("egrep -m 1 %s %s" % ("\"org\.apache\.kafka\.common\.protocol\.types\.SchemaException.*: java\.nio\.BufferUnderflowException\"", self.producer.LOG_FILE), allow_fail=False)
+        node.account.ssh("egrep -m 1 %s %s" % ("\"org\.apache\.kafka\.common\.protocol\.types\.SchemaException.*throttle_time_ms.*: java\.nio\.BufferUnderflowException\"", self.producer.LOG_FILE), allow_fail=False)
 
     def test_consumer_back_compatibility(self):
-        """Run the scala 0.8.X consumer against an 0.9.X cluster.
-        Expect 0.8.X scala consumer to fail with buffer underflow. This error is the same as when an 0.9.X producer
+        """Run the scala 0.9.X consumer against an 0.8.X cluster.
+        Expect 0.9.X scala consumer to fail with buffer underflow. This error is the same as when an 0.9.X producer
         is run against an 0.8.X broker: the broker responds to a V1 fetch request with a V0 fetch response; the
         client then tries to parse this V0 fetch response as a V1 fetch response, resulting in a BufferUnderflowException
         """
@@ -79,7 +79,7 @@ class ClientCompatibilityTest(Test):
 
         self.consumer = ConsoleConsumer(
             self.test_context, self.num_consumers, self.kafka, self.topic, group_id="consumer-09X",
-            consumer_timeout_ms=10000, message_validator=is_int, version=TRUNK)
+            consumer_timeout_ms=10000, message_validator=is_int, version=LATEST_0_9)
 
         self.old_consumer = ConsoleConsumer(
             self.test_context, self.num_consumers, self.kafka, self.topic, group_id="consumer-08X",
