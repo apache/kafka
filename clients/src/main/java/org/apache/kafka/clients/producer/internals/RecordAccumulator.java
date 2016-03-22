2ac0cf62a0ed8d2c507e3e2a618a133d20d7e509
Jiangjie Qin <becket.qin@gmail.com>
2016-03-21 17:43:15 -0700

    Addressed Jun's comments.

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
@@ -226,7 +226,14 @@ public final class RecordAccumulator {
             // 1. The leader is unknown.
             // 2. The leader broker is disconnected.
             Node leader = cluster.leaderFor(entry.getKey());
-            if (leader == null || client.connectionFailed(leader)) {
+            boolean leaderNotConnected = true;
+            try {
+                leaderNotConnected = client.connectionFailed(leader);
+            } catch (IllegalArgumentException e) {
+                // This means the client does not know the leader node. So it is not connected.
+            }
+
+            if (leader == null || leaderNotConnected) {
                 synchronized (dq) {
                     // iterate over the batches and expire them if they have stayed in accumulator for more than requestTimeOut
                     RecordBatch lastBatch = dq.peekLast();
