319c6e7195143c1fa4a1e5b34ad923c2b73f373d
Ismael Juma <ismael@juma.me.uk>
2016-04-12 11:34:46 -0700

    MINOR: Add missing `@Override` to `KStreamImpl.through`
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>
    
    Closes #1216 from ijuma/add-missing-override-to-through

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamImpl.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamImpl.java
@@ -243,6 +243,8 @@ public class KStreamImpl<K, V> extends AbstractStream<K> implements KStream<K, V
 
         topology.addProcessor(name, new KStreamForeach<>(action), this.name);
     }
+
+    @Override
     public KStream<K, V> through(Serde<K> keySerde, Serde<V> valSerde, String topic) {
         return through(keySerde, valSerde, null, topic);
     }
