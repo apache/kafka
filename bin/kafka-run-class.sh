a380c156d0613110fbc2bb9aeae3cab530b194c0
Liquan Pei <liquanpei@gmail.com>
2016-04-29 10:30:37 -0700

    Remove leftover debug message

--- bin/kafka-run-class.sh
+++ bin/kafka-run-class.sh
@@ -133,8 +133,6 @@ do
 done
 shopt -u nullglob
 
-echo $CLASSPATH
-
 # JMX settings
 if [ -z "$KAFKA_JMX_OPTS" ]; then
   KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false "
