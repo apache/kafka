0a89a7e3a35780d02b37e49e688d4985daec0407
Eno Thereska <eno.thereska@gmail.com>
2016-05-03 13:26:22 -0700

    MINOR: Added more integration tests
    Author: Eno Thereska <eno.thereska@gmail.com>
    
    Reviewers: Ismael Juma, Michael G. Noll, Guozhang Wang
    
    Closes #1285 from enothereska/more-integration-tests

--- streams/src/test/java/org/apache/kafka/streams/integration/WordCountIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/WordCountIntegrationTest.java
@@ -1,27 +1,26 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements.  See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.  You may obtain a
+ * copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package org.apache.kafka.streams.integration;
 
-
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
@@ -30,11 +29,12 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.ValueMapper;
-import org.apache.kafka.streams.processor.internals.ProcessorStateManager;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -42,24 +42,17 @@ import java.util.Properties;
 
 import org.apache.kafka.streams.integration.utils.EmbeddedSingleNodeKafkaCluster;
 import org.apache.kafka.streams.integration.utils.IntegrationTestUtils;
-import kafka.admin.AdminUtils;
-import kafka.log.LogConfig;
-import kafka.utils.ZKStringSerializer$;
-import kafka.utils.ZkUtils;
-import scala.Tuple2;
-import scala.collection.Iterator;
-import scala.collection.Map;
+
 
 /**
- * Tests related to internal topics in streams
+ * End-to-end integration test based on a simple word count example, using an embedded Kafka
+ * cluster.
  */
-public class InternalTopicIntegrationTest {
+public class WordCountIntegrationTest {
     @ClassRule
     public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
     private static final String DEFAULT_INPUT_TOPIC = "inputTopic";
     private static final String DEFAULT_OUTPUT_TOPIC = "outputTopic";
-    private static final int DEFAULT_ZK_SESSION_TIMEOUT_MS = 10 * 1000;
-    private static final int DEFAULT_ZK_CONNECTION_TIMEOUT_MS = 8 * 1000;
 
     @BeforeClass
     public static void startKafkaCluster() throws Exception {
@@ -67,61 +60,34 @@ public class InternalTopicIntegrationTest {
         CLUSTER.createTopic(DEFAULT_OUTPUT_TOPIC);
     }
 
-    /**
-     * Validates that any state changelog topics are compacted
-     * @return true if topics have a valid config, false otherwise
-     */
-    private boolean isUsingCompactionForStateChangelogTopics() {
-        boolean valid = true;
-
-        // Note: You must initialize the ZkClient with ZKStringSerializer.  If you don't, then
-        // createTopic() will only seem to work (it will return without error).  The topic will exist in
-        // only ZooKeeper and will be returned when listing topics, but Kafka itself does not create the
-        // topic.
-        ZkClient zkClient = new ZkClient(
-            CLUSTER.zKConnectString(),
-            DEFAULT_ZK_SESSION_TIMEOUT_MS,
-            DEFAULT_ZK_CONNECTION_TIMEOUT_MS,
-            ZKStringSerializer$.MODULE$);
-        boolean isSecure = false;
-        ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(CLUSTER.zKConnectString()), isSecure);
-
-        Map<String, Properties> topicConfigs = AdminUtils.fetchAllTopicConfigs(zkUtils);
-        Iterator it = topicConfigs.iterator();
-        while (it.hasNext()) {
-            Tuple2<String, Properties> topicConfig = (Tuple2<String, Properties>) it.next();
-            String topic = topicConfig._1;
-            Properties prop = topicConfig._2;
-
-            // state changelogs should be compacted
-            if (topic.endsWith(ProcessorStateManager.STATE_CHANGELOG_TOPIC_SUFFIX)) {
-                if (!prop.containsKey(LogConfig.CleanupPolicyProp()) ||
-                    !prop.getProperty(LogConfig.CleanupPolicyProp()).equals(LogConfig.Compact())) {
-                    valid = false;
-                    break;
-                }
-            }
-        }
-        zkClient.close();
-        return valid;
-    }
-
     @Test
-    public void shouldCompactTopicsForStateChangelogs() throws Exception {
+    public void shouldCountWords() throws Exception {
         List<String> inputValues = Arrays.asList("hello", "world", "world", "hello world");
+        List<KeyValue<String, Long>> expectedWordCounts = Arrays.asList(
+            new KeyValue<>("hello", 1L),
+            new KeyValue<>("world", 1L),
+            new KeyValue<>("world", 2L),
+            new KeyValue<>("hello", 2L),
+            new KeyValue<>("world", 3L)
+        );
 
         //
-        // Step 1: Configure and start a simple word count topology
+        // Step 1: Configure and start the processor topology.
         //
         final Serde<String> stringSerde = Serdes.String();
         final Serde<Long> longSerde = Serdes.Long();
 
         Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "compact-topics-integration-test");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-integration-test");
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
         streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, CLUSTER.zKConnectString());
         streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        // Explicitly place the state directory under /tmp so that we can remove it via
+        // `purgeLocalStreamsState` below.  Once Streams is updated to expose the effective
+        // StreamsConfig configuration (so we can retrieve whatever state directory Streams came up
+        // with automatically) we don't need to set this anymore and can update `purgeLocalStreamsState`
+        // accordingly.
         streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams");
 
         KStreamBuilder builder = new KStreamBuilder();
@@ -139,7 +105,8 @@ public class InternalTopicIntegrationTest {
                 public KeyValue<String, String> apply(String key, String value) {
                     return new KeyValue<String, String>(value, value);
                 }
-            }).countByKey("Counts").toStream();
+            }).countByKey("Counts")
+            .toStream();
 
         wordCounts.to(stringSerde, longSerde, DEFAULT_OUTPUT_TOPIC);
 
@@ -148,6 +115,10 @@ public class InternalTopicIntegrationTest {
 
         KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
         streams.start();
+
+        // Wait briefly for the topology to be fully up and running (otherwise it might miss some or all
+        // of the input data we produce below).
+        Thread.sleep(5000);
 
         //
         // Step 2: Produce some input data to the input topic.
@@ -161,9 +132,18 @@ public class InternalTopicIntegrationTest {
         IntegrationTestUtils.produceValuesSynchronously(DEFAULT_INPUT_TOPIC, inputValues, producerConfig);
 
         //
-        // Step 3: Verify the state changelog topics are compact
+        // Step 3: Verify the application's output data.
         //
+        Thread.sleep(5000);
         streams.close();
-        assertEquals(isUsingCompactionForStateChangelogTopics(), true);
+        Properties consumerConfig = new Properties();
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "wordcount-integration-test-standard-consumer");
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        List<KeyValue<String, Long>> actualWordCounts = IntegrationTestUtils.readKeyValues(DEFAULT_OUTPUT_TOPIC, consumerConfig);
+        assertThat(actualWordCounts, equalTo(expectedWordCounts));
     }
+
 }
