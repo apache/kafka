1a5d75c13bf10694a576382e120a38ff62f7ab29
Ismael Juma <ismael@juma.me.uk>
2016-05-12 20:36:47 +0100

    MINOR: Fix checkstyle failure in `StreamsConfigTest`
    I removed the hamcrest matcher to unbreak the build, but we probably want to tweak the `import-control.xml` as it currently only allows it for `<subpackage name="integration">`, which is weird.
    
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>
    
    Closes #1380 from ijuma/fix-streams-config-test-checkstyle

--- streams/src/test/java/org/apache/kafka/streams/StreamsConfigTest.java
+++ streams/src/test/java/org/apache/kafka/streams/StreamsConfigTest.java
@@ -30,10 +30,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 
 public class StreamsConfigTest {
 
@@ -115,7 +113,7 @@ public class StreamsConfigTest {
         StreamsConfig config = new StreamsConfig(props);
 
         List<String> actualBootstrapServers = config.getList(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG);
-        assertThat(actualBootstrapServers, equalTo(expectedBootstrapServers));
+        assertEquals(expectedBootstrapServers, actualBootstrapServers);
     }
 
 }
