1acdf3d1b487ebf77c83ccc9bb58b0b8b3986d2d
Eno Thereska <eno.thereska@gmail.com>
2016-05-25 13:08:57 +0100

    MINOR: Removed 1/2 of the hardcoded sleeps in Streams
    Author: Eno Thereska <eno.thereska@gmail.com>
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>, Ismael Juma <ismael@juma.me.uk>
    
    Closes #1422 from enothereska/minor-integration-timeout2

--- streams/src/test/java/org/apache/kafka/streams/integration/utils/IntegrationTestUtils.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/utils/IntegrationTestUtils.java
@@ -44,7 +44,8 @@ import java.util.concurrent.Future;
  */
 public class IntegrationTestUtils {
 
-    private static final int UNLIMITED_MESSAGES = -1;
+    public static final int UNLIMITED_MESSAGES = -1;
+    public static final long DEFAULT_TIMEOUT = 30 * 1000L;
 
     /**
      * Returns up to `maxMessages` message-values from the topic.
@@ -54,10 +55,10 @@ public class IntegrationTestUtils {
      * @param maxMessages    Maximum number of messages to read via the consumer.
      * @return The values retrieved via the consumer.
      */
-    public static <K, V> List<V> readValues(String topic, Properties consumerConfig, int maxMessages) {
+    public static <V> List<V> readValues(String topic, Properties consumerConfig, int maxMessages) {
         List<V> returnList = new ArrayList<>();
-        List<KeyValue<K, V>> kvs = readKeyValues(topic, consumerConfig, maxMessages);
-        for (KeyValue<K, V> kv : kvs) {
+        List<KeyValue<Object, V>> kvs = readKeyValues(topic, consumerConfig, maxMessages);
+        for (KeyValue<?, V> kv : kvs) {
             returnList.add(kv.value);
         }
         return returnList;
@@ -154,4 +155,75 @@ public class IntegrationTestUtils {
         produceKeyValuesSynchronously(topic, keyedRecords, producerConfig);
     }
 
+    public static <K, V> List<KeyValue<K, V>> waitUntilMinKeyValueRecordsReceived(Properties consumerConfig,
+                                                                                  String topic,
+                                                                                  int expectedNumRecords) throws InterruptedException {
+
+        return waitUntilMinKeyValueRecordsReceived(consumerConfig, topic, expectedNumRecords, DEFAULT_TIMEOUT);
+    }
+
+    /**
+     * Wait until enough data (key-value records) has been consumed.
+     * @param consumerConfig Kafka Consumer configuration
+     * @param topic          Topic to consume from
+     * @param expectedNumRecords Minimum number of expected records
+     * @param waitTime       Upper bound in waiting time in milliseconds
+     * @return All the records consumed, or null if no records are consumed
+     * @throws InterruptedException
+     * @throws AssertionError if the given wait time elapses
+     */
+    public static <K, V> List<KeyValue<K, V>> waitUntilMinKeyValueRecordsReceived(Properties consumerConfig,
+                                                                                  String topic,
+                                                                                  int expectedNumRecords,
+                                                                                  long waitTime) throws InterruptedException {
+        List<KeyValue<K, V>> accumData = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
+        while (true) {
+            List<KeyValue<K, V>> readData = readKeyValues(topic, consumerConfig);
+            accumData.addAll(readData);
+            if (accumData.size() >= expectedNumRecords)
+                return accumData;
+            if (System.currentTimeMillis() > startTime + waitTime)
+                throw new AssertionError("Expected " +  expectedNumRecords +
+                    " but received only " + accumData.size() +
+                    " records before timeout " + waitTime + " ms");
+            Thread.sleep(Math.min(waitTime, 100L));
+        }
+    }
+
+    public static <V> List<V> waitUntilMinValuesRecordsReceived(Properties consumerConfig,
+                                                                String topic,
+                                                                int expectedNumRecords) throws InterruptedException {
+
+        return waitUntilMinValuesRecordsReceived(consumerConfig, topic, expectedNumRecords, DEFAULT_TIMEOUT);
+    }
+
+    /**
+     * Wait until enough data (value records) has been consumed.
+     * @param consumerConfig Kafka Consumer configuration
+     * @param topic          Topic to consume from
+     * @param expectedNumRecords Minimum number of expected records
+     * @param waitTime       Upper bound in waiting time in milliseconds
+     * @return All the records consumed, or null if no records are consumed
+     * @throws InterruptedException
+     * @throws AssertionError if the given wait time elapses
+     */
+    public static <V> List<V> waitUntilMinValuesRecordsReceived(Properties consumerConfig,
+                                                                String topic,
+                                                                int expectedNumRecords,
+                                                                long waitTime) throws InterruptedException {
+        List<V> accumData = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
+        while (true) {
+            List<V> readData = readValues(topic, consumerConfig, expectedNumRecords);
+            accumData.addAll(readData);
+            if (accumData.size() >= expectedNumRecords)
+                return accumData;
+            if (System.currentTimeMillis() > startTime + waitTime)
+                throw new AssertionError("Expected " +  expectedNumRecords +
+                    " but received only " + accumData.size() +
+                    " records before timeout " + waitTime + " ms");
+            Thread.sleep(Math.min(waitTime, 100L));
+        }
+    }
 }
