a1155b449cb6e40ae7efc3a85b22950f2ffe80e2
Mickael Maison <mickael.maison@gmail.com>
2016-05-27 18:53:57 +0100

    MINOR: Fix tracing in KafkaApis.handle()
    requestObj() returns null for the new Java request objects so instead use requestDesc(true)

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -71,7 +71,7 @@ class KafkaApis(val requestChannel: RequestChannel,
   def handle(request: RequestChannel.Request) {
     try {
       trace("Handling request:%s from connection %s;securityProtocol:%s,principal:%s".
-        format(request.requestDesc(false), request.connectionId, request.securityProtocol, request.session.principal))
+        format(request.requestDesc(true), request.connectionId, request.securityProtocol, request.session.principal))
       ApiKeys.forId(request.requestId) match {
         case ApiKeys.PRODUCE => handleProducerRequest(request)
         case ApiKeys.FETCH => handleFetchRequest(request)
