430bf56cdfa32baffad21bb37dd50c080491dc03
Joshi <rekhajoshm@gmail.com>
2016-06-07 00:05:33 +0100

    KAFKA-724; Allow automatic socket.send.buffer from operating system in SocketServer
    If socket.receive.buffer.bytes/socket.send.buffer.bytes are set to -1, use the OS defaults.
    
    Author: Joshi <rekhajoshm@gmail.com>
    Author: Rekha Joshi <rekhajoshm@gmail.com>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    
    Closes #1469 from rekhajoshm/KAFKA-724-rebased

--- core/src/main/scala/kafka/server/KafkaConfig.scala
+++ core/src/main/scala/kafka/server/KafkaConfig.scala
@@ -396,8 +396,8 @@ object KafkaConfig {
   val AdvertisedListenersDoc = "Listeners to publish to ZooKeeper for clients to use, if different than the listeners above." +
   " In IaaS environments, this may need to be different from the interface to which the broker binds." +
   " If this is not set, the value for `listeners` will be used."
-  val SocketSendBufferBytesDoc = "The SO_SNDBUF buffer of the socket sever sockets"
-  val SocketReceiveBufferBytesDoc = "The SO_RCVBUF buffer of the socket sever sockets"
+  val SocketSendBufferBytesDoc = "The SO_SNDBUF buffer of the socket sever sockets. If the value is -1, the OS default will be used."
+  val SocketReceiveBufferBytesDoc = "The SO_RCVBUF buffer of the socket sever sockets. If the value is -1, the OS default will be used."
   val SocketRequestMaxBytesDoc = "The maximum number of bytes in a socket request"
   val MaxConnectionsPerIpDoc = "The maximum number of connections we allow from each ip address"
   val MaxConnectionsPerIpOverridesDoc = "Per-ip or hostname overrides to the default maximum number of connections"
