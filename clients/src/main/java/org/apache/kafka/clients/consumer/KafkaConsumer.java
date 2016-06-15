dc976b7905867d1fa1f15414a7ed8dea968d9c03
Bharat Viswanadham <bharatv@us.ibm.com>
2016-06-14 21:26:03 -0700

    Report the thread id of the blocking thread when throwing ConcurrentModificationException

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -1427,10 +1427,10 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
         ensureNotClosed();
         long threadId = Thread.currentThread().getId();
         if (threadId != currentThread.get() && !currentThread.compareAndSet(NO_CURRENT_THREAD, threadId))
-            throw new ConcurrentModificationException("KafkaConsumer is not safe for multi-threaded access. Request accessing thread is " +threadId + " and it is already being accessed by "+currentThread.get());
+            throw new ConcurrentModificationException("KafkaConsumer is not safe for multi-threaded access. Request accessing thread is " + threadId + " and it is already being accessed by " + currentThread.get());
         refcount.incrementAndGet();
     }
-
+\
     /**
      * Release the light lock protecting the consumer from multi-threaded access.
      */
