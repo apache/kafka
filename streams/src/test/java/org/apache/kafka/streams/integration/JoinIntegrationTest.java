44c8308ab1a6b950c5b12386d7864881b5d052a0
Damian Guy <damian.guy@gmail.com>
2016-06-20 14:37:43 -0700

    MINOR: speed up streams integration tests
    ... by
    a) merging some for startup/shutdown efficiency.
    b) use independent state dirs.
    c) remove some tests that are covered elsewhere
    
    guozhangwang ewencp - tests are running much quicker now, i.e, down to about 1 minute on my laptop (from about 2 - 3 minutes). There were some issues with state-dirs in some of the integration tests that was causing the shutdown of the streams apps to take a long time.
    
    Author: Damian Guy <damian.guy@gmail.com>
    
    Reviewers: Ismael Juma, Guozhang Wang
    
    Closes #1525 from dguy/integration-tests

--- streams/src/test/java/org/apache/kafka/streams/integration/JoinIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/JoinIntegrationTest.java
@@ -32,6 +32,7 @@ import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.Reducer;
 import org.apache.kafka.streams.kstream.ValueJoiner;
+import org.apache.kafka.streams.state.StateTestUtils;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -147,7 +148,8 @@ public class JoinIntegrationTest {
         // StreamsConfig configuration (so we can retrieve whatever state directory Streams came up
         // with automatically) we don't need to set this anymore and can update `purgeLocalStreamsState`
         // accordingly.
-        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams");
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG,
+                                 StateTestUtils.tempDir().getPath());
 
         // Remove any state from previous test runs
         IntegrationTestUtils.purgeLocalStreamsState(streamsConfiguration);
