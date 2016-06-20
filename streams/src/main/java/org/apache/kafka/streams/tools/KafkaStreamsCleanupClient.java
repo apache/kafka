836e1b0539875c1171c1f06a686df863889c5685
Matthias J. Sax <matthias@confluent.io>
2016-06-20 22:43:43 +0200

    checkstyle

--- streams/src/main/java/org/apache/kafka/streams/tools/KafkaStreamsCleanupClient.java
+++ streams/src/main/java/org/apache/kafka/streams/tools/KafkaStreamsCleanupClient.java
@@ -36,21 +36,21 @@ import java.util.Properties;
  * TODO
  */
 public class KafkaStreamsCleanupClient {
-    private static OptionSpec<String> bootstrap_server_option;
-    private static OptionSpec<String> zookeeper_option;
-    private static OptionSpec<String> application_id_option;
-    private static OptionSpec<String> source_topics_option;
-    private static OptionSpec<String> intermediate_topics_option;
-    private static OptionSpec<String> operator_option;
-    private static OptionSpec<String> window_option;
-    private static OptionSpec<String> state_dir_option;
+    private static OptionSpec<String> bootstrapServerOption;
+    private static OptionSpec<String> zookeeperOption;
+    private static OptionSpec<String> applicationIdOption;
+    private static OptionSpec<String> sourceTopicsOption;
+    private static OptionSpec<String> intermediateTopicsOption;
+    private static OptionSpec<String> operatorOption;
+    private static OptionSpec<String> windowOption;
+    private static OptionSpec<String> stateDirOption;
 
     private OptionSet options = null;
 
     private void run(String[] args) throws IOException {
         parseArguments(args);
 
-        ZkUtils zkUtils = ZkUtils.apply(options.valueOf(zookeeper_option),
+        ZkUtils zkUtils = ZkUtils.apply(options.valueOf(zookeeperOption),
                 30000,
                 30000,
                 JaasUtils.isZkSecurityEnabled());
@@ -65,40 +65,40 @@ public class KafkaStreamsCleanupClient {
 
     private void parseArguments(String[] args) throws IOException {
         OptionParser optionParser = new OptionParser();
-        application_id_option = optionParser.accepts("application-id", "The Kafka Streams application ID (application.id)")
+        applicationIdOption = optionParser.accepts("application-id", "The Kafka Streams application ID (application.id)")
                 .withRequiredArg()
                 .ofType(String.class)
                 .required();
-        bootstrap_server_option = optionParser.accepts("bootstrap-server", "Format: <host:port>")
+        bootstrapServerOption = optionParser.accepts("bootstrap-server", "Format: <host:port>")
                 .withRequiredArg()
                 .defaultsTo("localhost:9092")
                 .describedAs("url")
                 .ofType(String.class);
-        zookeeper_option = optionParser.accepts("zookeeper", "Format: <host:port>")
+        zookeeperOption = optionParser.accepts("zookeeper", "Format: <host:port>")
                 .withRequiredArg()
                 .defaultsTo("localhost:2181")
                 .describedAs("url")
                 .ofType(String.class);
-        source_topics_option = optionParser.accepts("source-topics", "Comma separated list of user source topics")
+        sourceTopicsOption = optionParser.accepts("source-topics", "Comma separated list of user source topics")
                 .withRequiredArg()
                 .ofType(String.class);
-        intermediate_topics_option = optionParser.accepts("intermediate-topics", "Comma separated list of intermediate user topics")
+        intermediateTopicsOption = optionParser.accepts("intermediate-topics", "Comma separated list of intermediate user topics")
                 .withRequiredArg()
                 .ofType(String.class);
-        operator_option = optionParser.accepts("operators", "Comma separted list of operator names")
+        operatorOption = optionParser.accepts("operators", "Comma separted list of operator names")
                 .withRequiredArg()
                 .ofType(String.class);
-        window_option = optionParser.accepts("windows", "Comma separted list of window names")
+        windowOption = optionParser.accepts("windows", "Comma separted list of window names")
                 .withRequiredArg()
                 .ofType(String.class);
-        state_dir_option = optionParser.accepts("state-dir", "Local state store directory (state.dir)")
+        stateDirOption = optionParser.accepts("state-dir", "Local state store directory (state.dir)")
                 .withRequiredArg()
                 .defaultsTo("/tmp/kafka-streams")
                 .ofType(String.class);
 
         try {
             options = optionParser.parse(args);
-        } catch(OptionException e) {
+        } catch (OptionException e) {
             System.err.println(e.getMessage());
             optionParser.printHelpOn(System.err);
             System.exit(-1);
@@ -106,11 +106,11 @@ public class KafkaStreamsCleanupClient {
     }
 
     private void resetSourceTopicOffsets() {
-        List<String> topics = new LinkedList< >();
-        topics.addAll(options.valuesOf(source_topics_option));
-        topics.addAll(options.valuesOf(intermediate_topics_option));
+        List<String> topics = new LinkedList<>();
+        topics.addAll(options.valuesOf(sourceTopicsOption));
+        topics.addAll(options.valuesOf(intermediateTopicsOption));
 
-        if(topics.size() == 0) {
+        if (topics.size() == 0) {
             System.out.println("No source or intermediate topics specified.");
             return;
         }
@@ -118,11 +118,11 @@ public class KafkaStreamsCleanupClient {
         System.out.println("Resetting offests to zero for topics " + topics);
 
         Properties config = new Properties();
-        config.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, options.valueOf(bootstrap_server_option));
-        config.setProperty(ConsumerConfig.GROUP_ID_CONFIG, options.valueOf(application_id_option));
+        config.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, options.valueOf(bootstrapServerOption));
+        config.setProperty(ConsumerConfig.GROUP_ID_CONFIG, options.valueOf(applicationIdOption));
         config.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
-        KafkaConsumer<byte[],byte[]> client = new KafkaConsumer<>(config, new ByteArrayDeserializer(), new ByteArrayDeserializer());
+        KafkaConsumer<byte[], byte[]> client = new KafkaConsumer<>(config, new ByteArrayDeserializer(), new ByteArrayDeserializer());
         try {
             client.subscribe(topics);
             client.poll(1);
@@ -131,7 +131,7 @@ public class KafkaStreamsCleanupClient {
                 client.seek(partition, 0);
             }
             client.commitSync();
-        } catch(RuntimeException e) {
+        } catch (RuntimeException e) {
             System.err.println("Resetting source topic offsets failed");
             throw e;
         } finally {
@@ -140,11 +140,11 @@ public class KafkaStreamsCleanupClient {
     }
 
     private void clearIntermediateTopics(ZkUtils zkUtils, List<String> allTopics) {
-        if (options.has(intermediate_topics_option)) {
+        if (options.has(intermediateTopicsOption)) {
             System.out.println("Clearing intermediate user topics.");
 
             HashMap<String, String> topicRetentionTime = new HashMap<>();
-            for (String topic : options.valuesOf(intermediate_topics_option)) {
+            for (String topic : options.valuesOf(intermediateTopicsOption)) {
                 if (allTopics.contains(topic)) {
                     Properties configs = AdminUtils.fetchEntityConfig(zkUtils, "topics", topic);
                     topicRetentionTime.put(topic, configs.getProperty("retention.ms"));
@@ -157,14 +157,14 @@ public class KafkaStreamsCleanupClient {
             // TODO fix -- simply sleep is not good enough
             try {
                 Thread.sleep(120 * 1000); // default clean-up interval is 60 seconds
-            } catch(Exception e) {}
+            } catch (Exception e) { }
 
-            for (String topic : options.valuesOf(intermediate_topics_option)) {
+            for (String topic : options.valuesOf(intermediateTopicsOption)) {
                 if (allTopics.contains(topic)) {
                     Properties configs = AdminUtils.fetchEntityConfig(zkUtils, "topics", topic);
 
                     String retentionTime = topicRetentionTime.get(topic);
-                    if(retentionTime == null) {
+                    if (retentionTime == null) {
                         configs.remove("retention.ms");
                     } else {
                         configs.setProperty("retention.ms", retentionTime);
@@ -182,26 +182,28 @@ public class KafkaStreamsCleanupClient {
     }
 
     private void deleteInternalTopics(List<String> allTopics) {
-        if (options.has(operator_option) || options.has(window_option)) {
+        if (options.has(operatorOption) || options.has(windowOption)) {
             System.out.println("Deleting internal topics.");
 
-            String applicationId = options.valueOf(application_id_option);
+            String applicationId = options.valueOf(applicationIdOption);
 
-            for (String operator : options.valuesOf(operator_option)) {
+            for (String operator : options.valuesOf(operatorOption)) {
                 String changelog = applicationId + "-" + operator + "-changelog";
-                if(allTopics.contains(changelog)) {
-                    TopicCommand.main(new String[]{"--zookeeper", options.valueOf(zookeeper_option),
-                            "--delete", "--topic", changelog});
+                if (allTopics.contains(changelog)) {
+                    TopicCommand.main(new String[] {
+                        "--zookeeper", options.valueOf(zookeeperOption),
+                        "--delete", "--topic", changelog});
                 } else {
                     System.err.println("No topic found for operator " + operator);
                 }
             }
 
-            for (String window : options.valuesOf(window_option)) {
+            for (String window : options.valuesOf(windowOption)) {
                 String changelog = applicationId + "-" + window + "-changelog";
-                if(allTopics.contains(changelog)) {
-                    TopicCommand.main(new String[]{"--zookeeper", options.valueOf(zookeeper_option),
-                            "--delete", "--topic", applicationId + "-" + window + "-changelog"});
+                if (allTopics.contains(changelog)) {
+                    TopicCommand.main(new String[] {
+                        "--zookeeper", options.valueOf(zookeeperOption),
+                        "--delete", "--topic", applicationId + "-" + window + "-changelog"});
                 } else {
                     System.err.println("No topic found for window " + window);
                 }
@@ -214,12 +216,12 @@ public class KafkaStreamsCleanupClient {
     }
 
     private void removeLocalStateStore() {
-        File stateStore = new File(options.valueOf(state_dir_option) + File.separator + options.valueOf(application_id_option));
-        if(!stateStore.exists()) {
+        File stateStore = new File(options.valueOf(stateDirOption) + File.separator + options.valueOf(applicationIdOption));
+        if (!stateStore.exists()) {
             System.out.println("Nothing to clear. Local state store directory does not exist.");
             return;
         }
-        if(!stateStore.isDirectory()) {
+        if (!stateStore.isDirectory()) {
             System.err.println("ERROR: " + stateStore.getAbsolutePath() + " is not a directory.");
             return;
         }
@@ -230,14 +232,14 @@ public class KafkaStreamsCleanupClient {
     }
 
     private void deleteDirectory(File directory) {
-        for(File f : directory.listFiles()) {
-            if(f.isDirectory()) {
+        for (File f : directory.listFiles()) {
+            if (f.isDirectory()) {
                 deleteDirectory(f);
-            } else if(!f.delete()) {
+            } else if (!f.delete()) {
                 System.err.println("ERROR: could not delete file " + f.getName() + " within " + directory.getAbsolutePath());
             }
         }
-        if(!directory.delete()) {
+        if (!directory.delete()) {
             System.err.println("ERROR: could not delete directory " + directory.getAbsolutePath());
         }
     }
