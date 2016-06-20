5fae56123fabc0568a99c27ba5b2e792b9f6a685
Guozhang Wang <wangguoz@gmail.com>
2016-06-20 16:29:47 -0700

    remove unused imports

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
@@ -55,7 +55,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
