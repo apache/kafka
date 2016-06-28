c49604110138d78f647198e2729a1637faa6b448
Ishita Mandhan <imandha@us.ibm.com>
2016-06-28 13:13:51 -0700

    KAFKA-2857 ConsumerGroupCommand throws GroupCoordinatorNotAvailableException when describing a non-existent group before the offset topic is created

--- core/src/main/scala/kafka/admin/AdminClient.scala
+++ core/src/main/scala/kafka/admin/AdminClient.scala
@@ -138,7 +138,7 @@ class AdminClient(val time: Time,
   def describeConsumerGroup(groupId: String): ConsumerGroupSummary = {
     findCoordinator(groupId) match {
       case None => 
-        throw new KafkaException(s"Could not find coordinator for group %s, which implies that one of the consumer offsets partitions may be offline or in the process of being created if this is a new cluster.".format(groupId))
+        throw new KafkaException(s"Could not find coordinator for group $groupId, which implies that one of the consumer offsets partitions may be offline or in the process of being created if this is a new cluster.")
       case Some(coordinator) =>
         val responseBody = send(coordinator, ApiKeys.DESCRIBE_GROUPS, new DescribeGroupsRequest(Collections.singletonList(groupId)))
         val response = responseBody.asInstanceOf[DescribeGroupsResponse]
