4814678bb048c99a13e06827c5ac49e58af9e8fe
James Cheng <jylcheng@yahoo.com>
2016-06-29 17:34:59 -0700

    Get rid of statically defined MetricNameTemplates.

--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerMetrics.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerMetrics.java
@@ -12,6 +12,7 @@
  */
 package org.apache.kafka.clients.consumer;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.kafka.clients.consumer.internals.FetcherMetricsRegistry;
@@ -19,66 +20,6 @@ import org.apache.kafka.common.MetricNameTemplate;
 import org.apache.kafka.common.metrics.SpecificMetrics;
 
 public class ConsumerMetrics {
-
-    // XXX group name is no longer configurable at runtime. Is that okay?
-    public static final MetricNameTemplate FETCH_SIZE_AVG = new MetricNameTemplate("fetch-size-avg", "consumer-fetch-manager-metrics", 
-            "The average number of bytes fetched per request", "client-id");
-    public static final MetricNameTemplate FETCH_SIZE_MAX = new MetricNameTemplate("fetch-size-max", "consumer-fetch-manager-metrics", 
-            "The maximum number of bytes fetched per request", "client-id");
-    public static final MetricNameTemplate BYTES_CONSUMED_RATE = new MetricNameTemplate("bytes-consumed-rate", "consumer-fetch-manager-metrics", 
-            "The average number of bytes consumed per second", "client-id");
-
-    public static final MetricNameTemplate RECORDS_PER_REQUEST_AVG = new MetricNameTemplate("records-per-request-avg", "consumer-fetch-manager-metrics", 
-            "The average number of records in each request", "client-id");
-    public static final MetricNameTemplate RECORDS_CONSUMED_RATE = new MetricNameTemplate("records-consumed-rate", "consumer-fetch-manager-metrics", 
-            "The average number of records consumed per second", "client-id");
-
-    public static final MetricNameTemplate FETCH_LATENCY_AVG = new MetricNameTemplate("fetch-latency-avg", "consumer-fetch-manager-metrics", 
-            "The average time taken for a fetch request.", "client-id");
-    public static final MetricNameTemplate FETCH_LATENCY_MAX = new MetricNameTemplate("fetch-latency-max", "consumer-fetch-manager-metrics", 
-            "The max time taken for any fetch request.", "client-id");
-    public static final MetricNameTemplate FETCH_RATE = new MetricNameTemplate("fetch-rate", "consumer-fetch-manager-metrics", 
-            "The number of fetch requests per second.", "client-id");
-
-    public static final MetricNameTemplate RECORDS_LAG_MAX = new MetricNameTemplate("records-lag-max", "consumer-fetch-manager-metrics", 
-            "The maximum lag in terms of number of records for any partition in this window", "client-id");
-
-    public static final MetricNameTemplate FETCH_THROTTLE_TIME_AVG = new MetricNameTemplate("fetch-throttle-time-avg", "consumer-fetch-manager-metrics", 
-            "The average throttle time in ms", "client-id");
-    public static final MetricNameTemplate FETCH_THROTTLE_TIME_MAX = new MetricNameTemplate("fetch-throttle-time-max", "consumer-fetch-manager-metrics", 
-            "The maximum throttle time in ms", "client-id");
-
-    // XXX can I put the topic name in the JMX description?
-    public static final MetricNameTemplate TOPIC_FETCH_SIZE_AVG = new MetricNameTemplate("fetch-size-avg", "consumer-fetch-manager-metrics", 
-            "The average number of bytes fetched per request for topic {topic}", "client-id", "topic");
-    public static final MetricNameTemplate TOPIC_FETCH_SIZE_MAX = new MetricNameTemplate("fetch-size-max", "consumer-fetch-manager-metrics", 
-            "The maximum number of bytes fetched per request for topic {topic}", "client-id", "topic");
-    public static final MetricNameTemplate TOPIC_BYTES_CONSUMED_RATE = new MetricNameTemplate("bytes-consumed-rate", "consumer-fetch-manager-metrics", 
-            "The average number of bytes consumed per second for topic {topic}", "client-id", "topic");
-
-    public static final MetricNameTemplate TOPIC_RECORDS_PER_REQUEST_AVG = new MetricNameTemplate("records-per-request-avg", "consumer-fetch-manager-metrics", 
-            "The average number of records in each request for topic {topic}", "client-id", "topic");
-    public static final MetricNameTemplate TOPIC_RECORDS_CONSUMED_RATE = new MetricNameTemplate("records-consumed-rate", "consumer-fetch-manager-metrics", 
-            "The average number of records consumed per second for topic {topic}", "client-id", "topic");
-
-    private static final MetricNameTemplate[] ALL_METRICS = {
-        FETCH_SIZE_AVG,
-        FETCH_SIZE_MAX,
-        BYTES_CONSUMED_RATE,
-        RECORDS_PER_REQUEST_AVG,
-        RECORDS_CONSUMED_RATE,
-        FETCH_LATENCY_AVG,
-        FETCH_LATENCY_MAX,
-        FETCH_RATE,
-        RECORDS_LAG_MAX,
-        FETCH_THROTTLE_TIME_AVG,
-        FETCH_THROTTLE_TIME_MAX,
-        TOPIC_FETCH_SIZE_AVG,
-        TOPIC_FETCH_SIZE_MAX,
-        TOPIC_BYTES_CONSUMED_RATE,
-        TOPIC_RECORDS_PER_REQUEST_AVG,
-        TOPIC_RECORDS_CONSUMED_RATE,
-    };
     
     public FetcherMetricsRegistry fetcherMetrics;
     
@@ -87,8 +28,17 @@ public class ConsumerMetrics {
         this.fetcherMetrics = new FetcherMetricsRegistry(metricsTags);
     }
 
-    public static void main(String[] args) {
-        System.out.println(SpecificMetrics.toHtmlTable("kafka.consumer", ALL_METRICS));
+    private MetricNameTemplate[] getAllTemplates() {
+        return this.fetcherMetrics.getAllTemplates();
     }
+
+    public static void main(String[] args) {
+        Set<String> tags = new HashSet<>();
+        tags.add("client-id");
+        ConsumerMetrics metrics = new ConsumerMetrics(tags, "consumer");
+        MetricNameTemplate[] allMetrics = metrics.getAllTemplates();
+        System.out.println(SpecificMetrics.toHtmlTable("kafka.consumer", allMetrics));
+    }
+
 
 }
