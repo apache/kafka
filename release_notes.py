39243b4c9023fb1b1cc43e2169bc00574949175c
Ewen Cheslack-Postava <me@ewencp.org>
2016-07-27 14:56:03 -0700

    Address review comments

--- release_notes.py
+++ release_notes.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+"""Usage: release_notes.py <version> > RELEASE_NOTES.html
+
+Generates release notes for a Kafka release by generating an HTML doc containing some introductory information about the
+ release with links to the Kafka docs followed by a list of issues resolved in the release. The script will fail if it finds
+ any unresolved issues still marked with the target release. You should run this script after either resolving all issues or
+ moving outstanding issues to a later release.
+
+"""
+
 from jira import JIRA
 import itertools, sys
 
@@ -39,13 +48,21 @@ if __name__ == "__main__":
 
     unresolved_issues = [issue for issue in issues if issue.fields.resolution is None]
     if unresolved_issues:
+        print >>sys.stderr, "The release is not completed since unresolved issues were found still tagged with this release as the fix version:"
         for issue in unresolved_issues:
             print >>sys.stderr, "Unresolved issue: %s %s" % (issue.key, issue_link(issue))
         sys.exit(1)
 
-    # Get list of (issue type, [issues]) sorted by the issue ID type's , with each subset of issues sorted by their key so they are in
-    # increasing order of bug #
-    by_group = [(k,sorted(g, key=lambda issue: issue.id)) for k,g in itertools.groupby(sorted(issues, key=lambda issue: issue.fields.issuetype.id), lambda issue: issue.fields.issuetype.name)]
+    # Get list of (issue type, [issues]) sorted by the issue ID type, with each subset of issues sorted by their key so they
+    # are in increasing order of bug #. To get a nice ordering of the issue types we customize the key used to sort by issue
+    # type a bit to ensure features and improvements end up first.
+    def issue_type_key(issue):
+        if issue.fields.issuetype.name == 'New Feature':
+            return -2
+        if issue.fields.issuetype.name == 'Improvement':
+            return -1
+        return issue.fields.issuetype.id
+    by_group = [(k,sorted(g, key=lambda issue: issue.id)) for k,g in itertools.groupby(sorted(issues, key=issue_type_key), lambda issue: issue.fields.issuetype.name)]
 
     print "<h1>Release Notes - Kafka - Version %s</h1>" % version
     print """<p>Below is a summary of the JIRA issues addressed in the %(version)s release of Kafka. For full documentation of the
