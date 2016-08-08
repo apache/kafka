f1704a6c6cea0f44fa3620b748cd84cd010f1fc8
Geoff Anderson <geoff@confluent.io>
2016-08-08 16:08:16 -0700

    More informative error message

--- tests/kafkatest/services/security/security_config.py
+++ tests/kafkatest/services/security/security_config.py
@@ -80,9 +80,10 @@ class SslStores(object):
 
     def runcmd(self, cmd):
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        proc.communicate()
+        stdout, stderr = proc.communicate()
+
         if proc.returncode != 0:
-            raise subprocess.CalledProcessError(proc.returncode, cmd)
+            raise RuntimeError("Command '%s' returned non-zero exit status %d: %s" % (cmd, proc.returncode, stderr))
 
 
 class SecurityConfig(TemplateRenderer):
