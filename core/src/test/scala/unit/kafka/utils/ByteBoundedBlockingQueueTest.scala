f997ffd1926f043bc7352f1bad251ef538bb53d7
radai-rosenblatt <radai.rosenblatt@gmail.com>
2016-08-08 14:59:24 -0700

    KAFKA-4011 - fix issues and beef up tests around ByteBoundedBlockingQueue, as discussed under KIP-72

--- core/src/test/scala/unit/kafka/utils/ByteBoundedBlockingQueueTest.scala
+++ core/src/test/scala/unit/kafka/utils/ByteBoundedBlockingQueueTest.scala
@@ -17,14 +17,191 @@
 
 package kafka.utils
 
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.{CountDownLatch, TimeUnit}
 
 import org.junit.Assert._
 import org.junit.Test
 
+import scala.util.Random
+
 class ByteBoundedBlockingQueueTest {
-  val sizeFunction = (a: String) => a.length
-  val queue = new ByteBoundedBlockingQueue[String](5, 15, Some(sizeFunction))
+  val sizeFunction: String => Long = (a: String) => a.length
+  val queue = new ByteBoundedBlockingQueue[String](5, 15, sizeFunction)
+
+  @Test(expected = classOf[IllegalArgumentException])
+  def testNullSizeFunctionOption(): Unit = {
+    new ByteBoundedBlockingQueue[String](1, 1, null)
+  }
+
+  @Test
+  def testEmptyInvariants(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[String](1000, 1000, sizeFunction)
+    assertEquals(0, queue.size())
+    assertEquals(1000, queue.remainingSize)
+    assertEquals(0, queue.byteSize())
+    assertEquals(1000, queue.remainingByteSize)
+  }
+
+  @Test
+  def testCountBound(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[String](3, Long.MaxValue, sizeFunction)
+    assertTrue(queue.offer("one", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("two", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("three", 10, TimeUnit.MILLISECONDS))
+    assertEquals(3, queue.size())
+    assertEquals(0, queue.remainingSize)
+    assertEquals(11, queue.byteSize())
+    assertEquals(Long.MaxValue - 11, queue.remainingByteSize)
+    assertFalse(queue.offer("four", 10, TimeUnit.MILLISECONDS)) //msg count cap reached
+    assertEquals(3, queue.size())
+    assertEquals(0, queue.remainingSize)
+    assertEquals(11, queue.byteSize())
+    assertEquals(Long.MaxValue - 11, queue.remainingByteSize)
+    assertEquals("one", queue.poll())
+    assertTrue(queue.offer("four", 10, TimeUnit.MILLISECONDS))
+    assertEquals(3, queue.size())
+    assertEquals(0, queue.remainingSize)
+    assertEquals(12, queue.byteSize())
+    assertEquals(Long.MaxValue - 12, queue.remainingByteSize)
+  }
+
+  @Test
+  def testSizeBound(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[String](1000, 10, sizeFunction)
+    assertTrue(queue.offer("one", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("two", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("three", 10, TimeUnit.MILLISECONDS))
+    assertEquals(3, queue.size())
+    assertEquals(1000 - 3, queue.remainingSize)
+    assertEquals(11, queue.byteSize())
+    assertEquals(0, queue.remainingByteSize) //negatives are returned as 0
+    assertFalse(queue.offer("four", 10, TimeUnit.MILLISECONDS)) //msg size cap reached
+    assertEquals(3, queue.size())
+    assertEquals(1000 - 3, queue.remainingSize)
+    assertEquals(11, queue.byteSize())
+    assertEquals(0, queue.remainingByteSize) //negatives returned as 0
+    assertEquals("one", queue.poll())
+    assertEquals(2, queue.size())
+    assertEquals(1000 - 2, queue.remainingSize)
+    assertEquals(8, queue.byteSize())
+    assertEquals(10 - 8, queue.remainingByteSize)
+    assertTrue(queue.offer("something big", 10, TimeUnit.MILLISECONDS)) //will be accepted because there's _some_ cap available.
+    assertEquals(21, queue.byteSize())
+    assertEquals(0, queue.remainingByteSize)
+    assertEquals("two", queue.poll())
+    assertFalse(queue.offer("four", 10, TimeUnit.MILLISECONDS)) //still over cap
+    assertEquals(2, queue.size())
+    assertEquals(18, queue.byteSize())
+  }
+
+  @Test
+  def testOfferBlocksUntilMsgCountAvailable(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[String](3, 1000, sizeFunction)
+    val started = new CountDownLatch(1)
+    val finished = new CountDownLatch(1)
+    val success = new AtomicReference[Boolean](false)
+    val putter1 = new Thread(new Runnable {
+      override def run(): Unit = {
+        started.countDown()
+        success.set(queue.offer("value", 10, TimeUnit.SECONDS))
+        finished.countDown()
+      }
+    })
+    assertTrue(queue.offer("one", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("two", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("three", 10, TimeUnit.MILLISECONDS))
+    putter1.start()
+    assertTrue(started.await(10, TimeUnit.SECONDS)) //thread has started
+    Thread.sleep(100)
+    assertEquals(1, finished.getCount) //thread still blocked
+    assertEquals("one", queue.poll(10, TimeUnit.MILLISECONDS))
+    assertTrue(finished.await(10, TimeUnit.SECONDS))
+    assertTrue(success.get())
+  }
+
+  @Test
+  def testOfferBlocksUntilBytesAvailable(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[String](1000, 10, sizeFunction)
+    val started = new CountDownLatch(1)
+    val finished = new CountDownLatch(1)
+    val success = new AtomicReference[Boolean](false)
+    val putter1 = new Thread(new Runnable {
+      override def run(): Unit = {
+        started.countDown()
+        success.set(queue.offer("value", 10, TimeUnit.SECONDS))
+        finished.countDown()
+      }
+    })
+    assertTrue(queue.offer("one", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("two", 10, TimeUnit.MILLISECONDS))
+    assertTrue(queue.offer("three", 10, TimeUnit.MILLISECONDS))
+    putter1.start()
+    assertTrue(started.await(10, TimeUnit.SECONDS)) //thread has started
+    Thread.sleep(100)
+    assertEquals(1, finished.getCount) //thread still blocked
+    assertEquals("one", queue.poll(10, TimeUnit.MILLISECONDS))
+    assertTrue(finished.await(10, TimeUnit.SECONDS))
+    assertTrue(success.get())
+  }
+
+  @Test
+  def testInvariantsMT(): Unit = {
+    val queue = new ByteBoundedBlockingQueue[Long](20, 100, (l: Long) => l)
+    val rand = new Random()
+    val bound = 10L
+    val numThreads = 3
+    val allDone = new CountDownLatch(2 * numThreads)
+    @volatile var die = false
+    @volatile var error: Option[Throwable] = None
+
+    def validateInvariants(): Unit = {
+      val size = queue.size()
+      val bytes = queue.byteSize()
+      try {
+        assertTrue(size >= 0)
+        assertTrue(size <= 20)
+        assertTrue(bytes >= 0)
+        assertTrue(bytes <= 110)
+      } catch {
+        case t: Throwable => error = Some(t)
+      }
+    }
+
+    class Producer extends Runnable {
+      override def run(): Unit = {
+        while (!die) {
+          val element: Long = 1L + ((bound - 1) * rand.nextDouble()).toLong //[1, 10] inclusive
+          queue.offer(element, 10, TimeUnit.MILLISECONDS)
+          validateInvariants()
+        }
+        allDone.countDown()
+      }
+    }
+
+    class Consumer extends Runnable {
+      override def run(): Unit = {
+        while (!die) {
+          val element = queue.poll(10, TimeUnit.MILLISECONDS)
+          validateInvariants()
+        }
+        allDone.countDown()
+      }
+    }
+
+
+    for (i <- 1 to numThreads) {
+      new Thread(new Producer()).start()
+      new Thread(new Consumer()).start()
+    }
+
+    Thread.sleep(TimeUnit.SECONDS.toMillis(3))
+    die = true
+    assertTrue(allDone.await(100, TimeUnit.MILLISECONDS))
+    if (error.nonEmpty) {
+      throw new AssertionError(error.get)
+    }
+  }
 
   @Test
   def testByteBoundedBlockingQueue() {
@@ -64,7 +241,7 @@ class ByteBoundedBlockingQueueTest {
 
     // add 5 small elements into the queue, first 4 should succeed, the 5th one should fail
     // test put()
-    assertEquals(true, queue.put("a"))
+    queue.put("a")
     assertEquals(true, queue.offer("b"))
     assertEquals(true, queue.offer("c"))
     assertEquals(4, queue.size())
