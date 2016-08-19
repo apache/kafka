b3e26666794861985701a42a32b17a54ae3b5561
Jason Gustafson <jason@confluent.io>
2016-08-19 13:06:04 -0700

    Reuse subscription set from SubscriptionState since it is immutable

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
@@ -139,14 +139,13 @@ public final class ConsumerCoordinator extends AbstractCoordinator {
 
     @Override
     public List<ProtocolMetadata> metadata() {
-        Set<String> subscribedTopics = new HashSet<>(subscriptions.subscription());
+        this.joinedSubscription = subscriptions.subscription();
         List<ProtocolMetadata> metadataList = new ArrayList<>();
         for (PartitionAssignor assignor : assignors) {
-            Subscription subscription = assignor.subscription(subscribedTopics);
+            Subscription subscription = assignor.subscription(joinedSubscription);
             ByteBuffer metadata = ConsumerProtocol.serializeSubscription(subscription);
             metadataList.add(new ProtocolMetadata(assignor.name(), metadata));
         }
-        this.joinedSubscription = subscribedTopics;
         return metadataList;
     }
 
