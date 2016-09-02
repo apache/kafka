01582a2057775c63227f4111aa104d3c558cb8b5
Sumant Tambe <sutambe@yahoo.com>
2016-09-01 17:28:20 -0700

    refactored batch expiry logic

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
@@ -224,9 +224,8 @@ public final class RecordAccumulator {
      * Abort the batches that have been sitting in RecordAccumulator for more than the configured requestTimeout
      * due to metadata being unavailable
      */
-    public List<RecordBatch> abortExpiredBatches(int requestTimeout, boolean staleMetadataNow, Metadata metadata, long now) {
+    public List<RecordBatch> abortExpiredBatches(int requestTimeout, boolean isMetadataStale, Cluster cluster, long now) {
         List<RecordBatch> expiredBatches = new ArrayList<>();
-        Cluster cluster = metadata.fetch();
         int count = 0;
         for (Map.Entry<TopicPartition, Deque<RecordBatch>> entry : this.batches.entrySet()) {
             Deque<RecordBatch> dq = entry.getValue();
@@ -235,7 +234,7 @@ public final class RecordAccumulator {
             // This is to prevent later batches from being expired while an earlier batch is still in progress.
             // Note that `muted` is only ever populated if `max.in.flight.request.per.connection=1` so this protection
             // is only active in this case. Otherwise the expiration order is not guaranteed.
-            if (!muted.contains(tp) && (staleMetadataNow || cluster.leaderFor(tp) == null)) {
+            if (!muted.contains(tp) && (isMetadataStale || cluster.leaderFor(tp) == null)) {
                 synchronized (dq) {
                     // iterate over the batches and expire them if they have been in the accumulator for more than requestTimeOut
                     RecordBatch lastBatch = dq.peekLast();
