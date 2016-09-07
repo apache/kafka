12d7b850fd0c2e074b041492f8b1d46b0c587bf3
Dustin Cote <dustin@confluent.io>
2016-09-07 09:31:59 -0400

    don't encourage acks=-1 (we want acks=all to be used)

--- clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
@@ -87,9 +87,9 @@ public class ProducerConfig extends AbstractConfig {
                                            + " <li><code>acks=1</code> This will mean the leader will write the record to its local log but will respond"
                                            + " without awaiting full acknowledgement from all followers. In this case should the leader fail immediately after"
                                            + " acknowledging the record but before the followers have replicated it then the record will be lost."
-                                           + " <li><code>acks=[-1|all]</code> This means the leader will wait for the full set of in-sync replicas to"
+                                           + " <li><code>acks=all</code> This means the leader will wait for the full set of in-sync replicas to"
                                            + " acknowledge the record. This guarantees that the record will not be lost as long as at least one in-sync replica"
-                                           + " remains alive. This is the strongest available guarantee.";
+                                           + " remains alive. This is the strongest available guarantee. This is equivalent to the acks=-1 setting.";
 
     /** <code>timeout.ms</code> */
 
