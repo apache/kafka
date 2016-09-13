28a6ad51f157705c600757b37703620d0207bae9
arrawatia <sumit.arrawatia@gmail.com>
2016-09-13 15:39:49 -0700

    Add Javadocs to ClusterResourceListener interface.

--- clients/src/main/java/org/apache/kafka/common/ClusterResourceListener.java
+++ clients/src/main/java/org/apache/kafka/common/ClusterResourceListener.java
@@ -13,6 +13,30 @@
 
 package org.apache.kafka.common;
 
+/**
+ * A callback interface that users can implement when they wish to get notified about changes in the
+ * Cluster metadata.
+ * <p>
+ * Users who need access to cluster metadata in interceptors, metric reporters, serializers and deserializers
+ * can implement this interface. The order of method calls for each of these types is described below
+ * <p>
+ * {@link org.apache.kafka.clients.producer.ProducerInterceptor} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called after {@link org.apache.kafka.clients.producer.ProducerInterceptor#onSend(ProducerRecord)}
+ * but before {@link org.apache.kafka.clients.producer.ProducerInterceptor#onAcknowledgement(RecordMetadata, Exception)} .
+ * <p>
+ * {@link org.apache.kafka.clients.consumer.ConsumerInterceptor} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called before {@link org.apache.kafka.clients.consumer.ConsumerInterceptor#onConsume(ConsumerRecords)}
+ * <p>
+ * {@link org.apache.kafka.common.serialization.Serializer} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called before {@link org.apache.kafka.common.serialization.Serializer#serialize(String, Object)}
+ * <p>
+ * {@link org.apache.kafka.common.serialization.Deserializer} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called before {@link org.apache.kafka.common.serialization.Deserializer#deserialize(String, byte[])}
+ * <p>
+ * {@link org.apache.kafka.common.metrics.MetricsReporter} : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called during the bootup of the Kafka broker. The reporter may receive metric events from the network layer before this method is called.
+ * <p>
+ * KafkaMetricsReporter : The {@link ClusterResourceListener#onUpdate(ClusterResource)} method will be called during the bootup of the Kafka broker. The reporter may receive metric events from the network layer before this method is called.
+ */
 public interface ClusterResourceListener {
+    /**
+     * A callback method that a user can implement to get updates for {@link ClusterResource}.
+     * @param clusterResource cluster metadata
+     */
     void onUpdate(ClusterResource clusterResource);
 }
