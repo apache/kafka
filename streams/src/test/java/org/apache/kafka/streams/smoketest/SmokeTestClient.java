70afd5f9dd2eddc784a24fa2518992ef3371f0a4
Damian Guy <damian.guy@gmail.com>
2016-09-19 10:30:58 -0700

    KAFKA-4175: Can't have StandbyTasks in KafkaStreams where NUM_STREAM_THREADS_CONFIG > 1
    standby tasks should be assigned per consumer not per process
    
    Author: Damian Guy <damian.guy@gmail.com>
    
    Reviewers: Eno Thereska, Guozhang Wang
    
    Closes #1862 from dguy/kafka-4175

--- streams/src/test/java/org/apache/kafka/streams/smoketest/SmokeTestClient.java
+++ streams/src/test/java/org/apache/kafka/streams/smoketest/SmokeTestClient.java
@@ -85,7 +85,7 @@ public class SmokeTestClient extends SmokeTestUtil {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
         props.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, zookeeper);
         props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, TestTimestampExtractor.class.getName());
-        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 3);
         props.put(StreamsConfig.NUM_STANDBY_REPLICAS_CONFIG, 2);
         props.put(StreamsConfig.BUFFERED_RECORDS_PER_PARTITION_CONFIG, 100);
         props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 2);
