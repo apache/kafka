44965568916dc82077a7b696d91cd73f73f69e71
Apurva Mehta <apurva.1618@gmail.com>
2016-09-27 18:06:35 -0700

    Enable the bounce brokers test for reassign partitions as well, since the underlying bug is now fixed

--- tests/kafkatest/tests/core/reassign_partitions_test.py
+++ tests/kafkatest/tests/core/reassign_partitions_test.py
@@ -86,6 +86,7 @@ class ReassignPartitionsTest(ProduceConsumeValidateTest):
         # Wait until finished or timeout
         wait_until(lambda: self.kafka.verify_reassign_partitions(partition_info), timeout_sec=self.timeout_sec, backoff_sec=.5)
 
+    @parametrize(security_protocol="PLAINTEXT", bounce_brokers=True)
     @parametrize(security_protocol="PLAINTEXT", bounce_brokers=False)
     def test_reassign_partitions(self, bounce_brokers, security_protocol):
         """Reassign partitions tests.
