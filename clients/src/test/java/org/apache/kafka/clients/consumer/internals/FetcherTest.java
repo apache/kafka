cbf16eac7b8db0d7335989c2ecc4a057f1debfe8
Jiangjie Qin <becket.qin@gmail.com>
2016-10-07 18:43:24 -0700

    KAFKA-4274; Consumer `offsetForTimes` times out on empty map
    Author: Jiangjie Qin <becket.qin@gmail.com>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>, Jason Gustafson <jason@confluent.io>
    
    Closes #1993 from becketqin/KAFKA-4274
    
    (cherry picked from commit 6b91f83fbaef26fefb9ec221529c29872a04c004)
    Signed-off-by: Jason Gustafson <jason@confluent.io>

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetcherTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetcherTest.java
@@ -648,6 +648,8 @@ public class FetcherTest {
 
     @Test
     public void testGetOffsetsForTimes() {
+        // Empty map
+        assertTrue(fetcher.getOffsetsByTimes(new HashMap<TopicPartition, Long>(), 100L).isEmpty());
         // Error code none with unknown offset
         testGetOffsetsForTimesWithError(Errors.NONE, Errors.NONE, -1L, 100L, null, 100L);
         // Error code none with known offset
