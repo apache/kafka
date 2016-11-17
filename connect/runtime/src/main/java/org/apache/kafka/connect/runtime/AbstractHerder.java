f62f4c651ae2e781ea7744213700335dba14672c
Ryan P <ryan.n.pridgeon@gmail.com>
2016-11-17 01:28:44 -0500

    restore herder to former glory

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/AbstractHerder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/AbstractHerder.java
@@ -40,7 +40,6 @@ import org.apache.kafka.connect.tools.VerifiableSinkConnector;
 import org.apache.kafka.connect.tools.VerifiableSourceConnector;
 import org.apache.kafka.connect.util.ConnectorTaskId;
 import org.apache.kafka.connect.util.ReflectionsUtil;
-import org.eclipse.jetty.util.log.Log;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
@@ -273,8 +272,6 @@ public abstract class AbstractHerder implements Herder, TaskStatus.Listener, Con
             }
             ReflectionsUtil.registerUrlTypes();
             ConfigurationBuilder builder = new ConfigurationBuilder().setUrls(ClasspathHelper.forJavaClassPath());
-
-            builder.getUrls().remove(ReflectionsUtil.stringtoURL("/"));
 
             Reflections reflections = new Reflections(builder);
             Set<Class<? extends Connector>> connectorClasses = reflections.getSubTypesOf(Connector.class);
