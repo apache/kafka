bd89d4253a13c50ef75e75a993eeb7bd2a49e0ae
Jason Guo <habren@163.com>
2016-11-27 10:54:18 +0800

    KAFKA-4449: Add Serializer/Deserializer for POJO -- format code

--- clients/src/main/java/org/apache/kafka/common/serialization/GenericDeserializer.java
+++ clients/src/main/java/org/apache/kafka/common/serialization/GenericDeserializer.java
@@ -1,10 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.apache.kafka.common.serialization;
 
 import java.io.IOException;
 import java.util.Map;
 
 import org.apache.kafka.common.errors.SerializationException;
-import org.apache.kafka.common.serialization.Deserializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -21,45 +33,46 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GenericDeserializer<T> implements Deserializer<T> {
 
-	private Class<T> type;
-	private ObjectMapper objectMapper = new ObjectMapper();
+    private Class<T> type;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-	public GenericDeserializer() {}
-	
-	public GenericDeserializer(Class<T> type) {
-		this.type = type;
-	}
+    public GenericDeserializer() {
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void configure(Map<String, ?> configs, boolean isKey) {
-		if(type != null) {
-			return;
-		}
-		
-		String typeProp = isKey ? "key.deserializer.type" : "value.deserializer.type";
-		String typeName = (String)configs.get(typeProp);
-		try {
-			type = (Class<T>)Class.forName(typeName);
-		} catch (Exception ex) {
-			throw new SerializationException("Failed to initialize GenericDeserializer for " + typeName, ex);
-		}
-	}
+    public GenericDeserializer(Class<T> type) {
+        this.type = type;
+    }
 
-	@Override
-	public T deserialize(String topic, byte[] data) {
-		if (data == null) {
-			return null;
-		}
-		try {
-			return this.objectMapper.readValue(data, type);
-		} catch (IOException ex) {
-			throw new SerializationException(ex);
-		}
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        if (type != null) {
+            return;
+        }
 
-	@Override
-	public void close() {
-	}
+        String typeProp = isKey ? "key.deserializer.type" : "value.deserializer.type";
+        String typeName = (String) configs.get(typeProp);
+        try {
+            type = (Class<T>) Class.forName(typeName);
+        } catch (Exception ex) {
+            throw new SerializationException("Failed to initialize GenericDeserializer for " + typeName, ex);
+        }
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        try {
+            return this.objectMapper.readValue(data, type);
+        } catch (IOException ex) {
+            throw new SerializationException(ex);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
 
 }
--- clients/src/main/java/org/apache/kafka/common/serialization/GenericSerializer.java
+++ clients/src/main/java/org/apache/kafka/common/serialization/GenericSerializer.java
@@ -1,10 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.apache.kafka.common.serialization;
 
 import java.io.IOException;
 import java.util.Map;
 
 import org.apache.kafka.common.errors.SerializationException;
-import org.apache.kafka.common.serialization.Serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -21,44 +33,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GenericSerializer<T> implements Serializer<T> {
 
-	private Class<T> type;
-	private ObjectMapper objectMapper = new ObjectMapper();
+    private Class<T> type;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-	public GenericSerializer() {}
-	
-	public GenericSerializer(Class<T> type) {
-		this.type = type;
-	}
+    public GenericSerializer() {
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void configure(Map<String, ?> configs, boolean isKey) {
-		if(type != null) {
-			return;
-		}
-		String typeProp = isKey ? "key.serializer.type" : "value.serializer.type";
-		String typeName = (String)configs.get(typeProp);
-		try {
-			type = (Class<T>)Class.forName(typeName);
-		} catch (Exception ex) {
-			throw new SerializationException("Failed to initialize GenericSerializer for " + typeName, ex);
-		}
-	}
+    public GenericSerializer(Class<T> type) {
+        this.type = type;
+    }
 
-	@Override
-	public byte[] serialize(String topic, T object) {
-		if (object == null) {
-			return null;
-		}
-		try {
-			return this.objectMapper.writerFor(type).writeValueAsBytes(object);
-		} catch (IOException ex) {
-			throw new SerializationException(ex);
-		}
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        if (type != null) {
+            return;
+        }
+        String typeProp = isKey ? "key.serializer.type" : "value.serializer.type";
+        String typeName = (String) configs.get(typeProp);
+        try {
+            type = (Class<T>) Class.forName(typeName);
+        } catch (Exception ex) {
+            throw new SerializationException("Failed to initialize GenericSerializer for " + typeName, ex);
+        }
+    }
 
-	@Override
-	public void close() {
-	}
+    @Override
+    public byte[] serialize(String topic, T object) {
+        if (object == null) {
+            return null;
+        }
+        try {
+            return this.objectMapper.writerFor(type).writeValueAsBytes(object);
+        } catch (IOException ex) {
+            throw new SerializationException(ex);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
 
 }
