818f526ad0186b1e9f15e12b114b93bef65d6aa6
Ismael Juma <ismael@juma.me.uk>
2016-11-29 09:11:21 -0800

    Revert "KAFKA-4345; Run decktape test for each pull request"
    This reverts commit e035fc039598127e88f31739458f705290b1fdba for the
    following reasons:
    
    1. License files are missing causing local builds to fail during the
    rat task (rat is not being run in Jenkins for some reason, filed
    KAFKA-4459 for that)
    2. It renames a number of system test files when there's a better
    way to achieve the goal of running a subset of system tests to stay
    under the Travis limit.
    3. It adds the gradle wrapper binary even though this was removed
    intentionally a while back.
    
    A new PR will be submitted for KAFKA-4345 without the undesired
    changes.
    
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Jason Gustafson <jason@confluent.io>, Ewen Cheslack-Postava <ewen@confluent.io>
    
    Closes #2187 from ijuma/kafka-4345-revert

--- tests/kafkatest/tests/produce_consume_validate.py
+++ tests/kafkatest/tests/produce_consume_validate.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import traceback
 
 from ducktape.tests.test import Test
 from ducktape.utils.util import wait_until
@@ -103,7 +102,7 @@ class ProduceConsumeValidateTest(Test):
         except BaseException as e:
             for s in self.test_context.services:
                 self.mark_for_collect(s)
-            raise Exception(traceback.format_exc(e))
+            raise
 
     @staticmethod
     def annotate_missing_msgs(missing, acked, consumed, msg):
