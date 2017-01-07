e5c2d3320e9be0394eb4ed147939b2ae615caf61
Matthias J. Sax <matthias@confluent.io>
2017-01-07 10:40:47 -0800

    MINOR: rework JavaDoc for windowing related public API

--- streams/src/main/java/org/apache/kafka/streams/kstream/Window.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/Window.java
@@ -5,33 +5,45 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.kafka.streams.kstream;
+
+import org.apache.kafka.common.annotation.InterfaceStability;
 
 /**
  * A single window instance, defined by its start and end timestamp.
+ * {@link Window} is agnostic if start/end boundaries are inclusive or exclusive; this is defined by concrete
+ * window implementations.
+ * <p>
+ * To specify how {@link Window} boundaries are defined use {@link Windows}.
+ * For time semantics, compare {@link org.apache.kafka.streams.processor.TimestampExtractor TimestampExtractor}.
+ *
+ * @see Windows
+ * @see org.apache.kafka.streams.kstream.internals.TimeWindow
+ * @see org.apache.kafka.streams.kstream.internals.SessionWindow
+ * @see org.apache.kafka.streams.kstream.internals.UnlimitedWindow
+ * @see org.apache.kafka.streams.processor.TimestampExtractor
  */
+@InterfaceStability.Unstable
 public abstract class Window {
 
     protected final long startMs;
     protected final long endMs;
 
     /**
-     * Create a new window for the given start time (inclusive) and end time (exclusive).
+     * Create a new window for the given start and end time.
      *
-     * @param start  the start timestamp of the window (inclusive)
-     * @param end    the end timestamp of the window (exclusive)
-     * @throws IllegalArgumentException if {@code start} or {@code end} is negative or if {@code end} is smaller than
-     * {@code start}
+     * @param startMs the start timestamp of the window
+     * @param endMs   the end timestamp of the window
+     * @throws IllegalArgumentException if {@code startMs} is negative or if {@code endMs} is smaller than {@code startMs}
      */
     public Window(long startMs, long endMs) throws IllegalArgumentException {
         if (startMs < 0) {
@@ -45,14 +57,14 @@ public abstract class Window {
     }
 
     /**
-     * Return the start timestamp of this window, inclusive
+     * Return the start timestamp of this window.
      */
     public long start() {
         return startMs;
     }
 
     /**
-     * Return the end timestamp of this window, exclusive
+     * Return the end timestamp of this window.
      */
     public long end() {
         return endMs;
@@ -60,9 +72,11 @@ public abstract class Window {
 
     /**
      * Check if the given window overlaps with this window.
+     * Should throw an {@link IllegalArgumentException} if the {@code other} window has a different type than {@code
+     * this} window.
      *
-     * @param other  another window
-     * @return       {@code true} if {@code other} overlaps with this window&mdash;{@code false} otherwise
+     * @param other another window of the same type
+     * @return {@code true} if {@code other} overlaps with this window&mdash;{@code false} otherwise
      */
     public abstract boolean overlap(final Window other);
 
