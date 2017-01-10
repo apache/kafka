672ca3bd8111000a84cf30dc5bfa45021ee9e079
Shikhar Bhushan <shikhar@confluent.io>
2017-01-10 11:32:55 -0800

    unused imports

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/AbstractHerder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/AbstractHerder.java
@@ -38,16 +38,13 @@ import org.apache.kafka.connect.util.ConnectorTaskId;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
