d2f6b934bcbd8feca561e050ff8e18d0a4db7d61
Guozhang Wang <wangguoz@gmail.com>
2017-01-15 09:55:11 +0000

    MINOR: replace remove() with delete() after 5.0.1 RocksDB upgrade
    Author: Guozhang Wang <wangguoz@gmail.com>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    
    Closes #2380 from guozhangwang/KMinor-remove-deprecated-rocksDB

--- streams/src/main/java/org/apache/kafka/streams/state/internals/RocksDBStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/RocksDBStore.java
@@ -253,7 +253,7 @@ public class RocksDBStore<K, V> implements KeyValueStore<K, V> {
     private void putInternal(byte[] rawKey, byte[] rawValue) {
         if (rawValue == null) {
             try {
-                db.remove(wOptions, rawKey);
+                db.delete(wOptions, rawKey);
             } catch (RocksDBException e) {
                 throw new ProcessorStateException("Error while removing key " + serdes.keyFrom(rawKey) +
                         " from store " + this.name, e);
@@ -274,7 +274,7 @@ public class RocksDBStore<K, V> implements KeyValueStore<K, V> {
             for (KeyValue<K, V> entry : entries) {
                 final byte[] rawKey = serdes.rawKey(entry.key);
                 if (entry.value == null) {
-                    db.remove(rawKey);
+                    db.delete(rawKey);
                 } else {
                     final byte[] value = serdes.rawValue(entry.value);
                     batch.put(rawKey, value);
