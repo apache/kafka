90b2e24f46e34e9fcee29bea708c824dfb12d81a
Colin P. Mccabe <cmccabe@confluent.io>
2017-01-20 15:55:49 -0800

    NodeApiVersionsTest: fix checkstyle

--- clients/src/test/java/org/apache/kafka/clients/NodeApiVersionsTest.java
+++ clients/src/test/java/org/apache/kafka/clients/NodeApiVersionsTest.java
@@ -25,7 +25,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
