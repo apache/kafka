740abdb7bf6dff841a09baf84f1bacd88401f50e
Matthias J. Sax <matthias@confluent.io>
2017-01-24 11:02:34 -0800

    MINOR: rework JavaDoc for windowing related public API
     - also some code refactoring and bug fixes
    
    Author: Matthias J. Sax <matthias@confluent.io>
    
    Reviewers: Damian Guy, Eno Thereska, Guozhang Wang
    
    Closes #2337 from mjsax/javaDocImprovements4
    
    (cherry picked from commit b938c03b0aa7a0cebdaad0cbb22689cc719c6ebc)
    Signed-off-by: Guozhang Wang <wangguoz@gmail.com>

--- streams/src/main/java/org/apache/kafka/streams/kstream/UnlimitedWindows.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/UnlimitedWindows.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,14 +17,33 @@
 
 package org.apache.kafka.streams.kstream;
 
+import org.apache.kafka.common.annotation.InterfaceStability;
 import org.apache.kafka.streams.kstream.internals.UnlimitedWindow;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The unlimited window specifications.
+ * The unlimited window specifications used for aggregations.
+ * <p>
+ * An unlimited time window is also called landmark window.
+ * It has a fixed starting point while its window end is defined as infinite.
+ * With this regard, it is a fixed-size window with infinite window size.
+ * <p>
+ * For time semantics, see {@link org.apache.kafka.streams.processor.TimestampExtractor TimestampExtractor}.
+ *
+ * @see TimeWindows
+ * @see SessionWindows
+ * @see JoinWindows
+ * @see KGroupedStream#count(Windows, String)
+ * @see KGroupedStream#count(Windows, org.apache.kafka.streams.processor.StateStoreSupplier)
+ * @see KGroupedStream#reduce(Reducer, Windows, String)
+ * @see KGroupedStream#reduce(Reducer, Windows, org.apache.kafka.streams.processor.StateStoreSupplier)
+ * @see KGroupedStream#aggregate(Initializer, Aggregator, Windows, org.apache.kafka.common.serialization.Serde, String)
+ * @see KGroupedStream#aggregate(Initializer, Aggregator, Windows, org.apache.kafka.streams.processor.StateStoreSupplier)
+ * @see org.apache.kafka.streams.processor.TimestampExtractor
  */
+@InterfaceStability.Unstable
 public class UnlimitedWindows extends Windows<UnlimitedWindow> {
 
     private static final long DEFAULT_START_TIMESTAMP_MS = 0L;
@@ -32,10 +51,7 @@ public class UnlimitedWindows extends Windows<UnlimitedWindow> {
     /** The start timestamp of the window. */
     public final long startMs;
 
-    private UnlimitedWindows(final long startMs) throws IllegalArgumentException {
-        if (startMs < 0) {
-            throw new IllegalArgumentException("startMs must be > 0 (you provided " + startMs + ")");
-        }
+    private UnlimitedWindows(final long startMs) {
         this.startMs = startMs;
     }
 
@@ -49,11 +65,15 @@ public class UnlimitedWindows extends Windows<UnlimitedWindow> {
     /**
      * Return a new unlimited window for the specified start timestamp.
      *
-     * @param start  the window start time
-     * @return       a new unlimited window that starts at {@code start}
+     * @param startMs the window start time
+     * @return a new unlimited window that starts at {@code startMs}
+     * @throws IllegalArgumentException if the start time is negative
      */
-    public UnlimitedWindows startOn(final long start) throws IllegalArgumentException {
-        return new UnlimitedWindows(start);
+    public UnlimitedWindows startOn(final long startMs) throws IllegalArgumentException {
+        if (startMs < 0) {
+            throw new IllegalArgumentException("Window start time (startMs) cannot be negative.");
+        }
+        return new UnlimitedWindows(startMs);
     }
 
     @Override
@@ -68,8 +88,36 @@ public class UnlimitedWindows extends Windows<UnlimitedWindow> {
         return windows;
     }
 
+    /**
+     * {@inheritDoc}
+     * As unlimited windows have conceptually infinite size, this methods just returns {@link Long#MAX_VALUE}.
+     *
+     * @return the size of the specified windows which is {@link Long#MAX_VALUE}
+     */
     @Override
     public long size() {
+        return Long.MAX_VALUE;
+    }
+
+    /**
+     * Throws an {@link IllegalArgumentException} because the retention time for unlimited windows is always infinite
+     * and cannot be changed.
+     *
+     * @throws IllegalArgumentException on every invocation
+     */
+    @Override
+    public UnlimitedWindows until(final long durationMs) {
+        throw new IllegalArgumentException("Window retention time (durationMs) cannot be set for UnlimitedWindows.");
+    }
+
+    /**
+     * {@inheritDoc}
+     * The retention time for unlimited windows in infinite and thus represented as {@link Long#MAX_VALUE}.
+     *
+     * @return the window retention time that is {@link Long#MAX_VALUE}
+     */
+    @Override
+    public long maintainMs() {
         return Long.MAX_VALUE;
     }
 
@@ -92,13 +140,4 @@ public class UnlimitedWindows extends Windows<UnlimitedWindow> {
         return (int) (startMs ^ (startMs >>> 32));
     }
 
-    @Override
-    public UnlimitedWindows until(final long durationMs) {
-        throw new IllegalArgumentException("Window retention time (durationMs) cannot be set for UnlimitedWindows.");
-    }
-
-    @Override
-    public long maintainMs() {
-        return Long.MAX_VALUE;
-    }
 }
--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/UnlimitedWindow.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/UnlimitedWindow.java
@@ -14,22 +14,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.kafka.streams.kstream.internals;
 
+import org.apache.kafka.common.annotation.InterfaceStability;
 import org.apache.kafka.streams.kstream.Window;
 
+/**
+ * {@link UnlimitedWindow} is an "infinite" large window with a fixed (inclusive) start time.
+ * All windows of the same {@link org.apache.kafka.streams.kstream.UnlimitedWindows window specification} will have the
+ * same start time.
+ * To make the window size "infinite" end time is set to {@link Long#MAX_VALUE}.
+ * <p>
+ * For time semantics, see {@link org.apache.kafka.streams.processor.TimestampExtractor TimestampExtractor}.
+ *
+ * @see TimeWindow
+ * @see SessionWindow
+ * @see org.apache.kafka.streams.kstream.UnlimitedWindows
+ * @see org.apache.kafka.streams.processor.TimestampExtractor
+ */
+@InterfaceStability.Unstable
 public class UnlimitedWindow extends Window {
 
+    /**
+     * Create a new window for the given start time (inclusive).
+     *
+     * @param startMs the start timestamp of the window (inclusive)
+     * @throws IllegalArgumentException if {@code start} is negative
+     */
     public UnlimitedWindow(final long startMs) {
         super(startMs, Long.MAX_VALUE);
     }
 
+    /**
+     * Returns {@code true} if the given window is of the same type, because all unlimited windows overlap with each
+     * other due to their infinite size.
+     *
+     * @param other another window
+     * @return {@code true}
+     * @throws IllegalArgumentException if the {@code other} window has a different type than {@link this} window
+     */
     @Override
     public boolean overlap(final Window other) {
         if (getClass() != other.getClass()) {
             throw new IllegalArgumentException("Cannot compare windows of different type. Other window has type "
-                + other.getClass());
+                + other.getClass() + ".");
         }
         return true;
     }
