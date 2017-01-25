ec3975978582dfec63986b67c38d6e3bb7742f81
Guozhang Wang <wangguoz@gmail.com>
2017-01-25 11:11:21 -0800

    KAFKA-3502: KStreamTestDriver needs to be closed after the test case
    Found a few recently added unit tests did not close KStreamTestDriver after the test itself is closed; this can cause RocksDB virtual function called if the contained topology has some persistent store since they will be initialized but not closed in time.
    
    MINOR fix: found that when closing KStreamTestDriver, we need to first flushing all stores before closing any of them; this is triggered from the `KTableKTableLeftJoin.shouldNotThrowIllegalStateExceptionWhenMultiCacheEvictions`.
    
    MINOR fix: in CachingXXXStore, the `name` field is actually used as the cache's namespace, not really the store name or its corresponding topic name. Fixed it by renaming it to `cacheName` and use `this.name()` elsewhere which will call the underlying store's name.
    
    Author: Guozhang Wang <wangguoz@gmail.com>
    
    Reviewers: Ewen Cheslack-Postava, Eno Thereska, Damian Guy
    
    Closes #2432 from guozhangwang/K3502-kstream-builder-test
    
    (cherry picked from commit 2ca3e59bb6054cc8ecf6926a74859efe3df000ba)
    Signed-off-by: Guozhang Wang <wangguoz@gmail.com>

--- streams/src/main/java/org/apache/kafka/streams/state/internals/RocksDBStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/RocksDBStore.java
@@ -352,6 +352,7 @@ public class RocksDBStore<K, V> implements KeyValueStore<K, V> {
         if (!open) {
             return;
         }
+
         open = false;
         closeOpenIterators();
         options.close();
