dc4d1bb81b37908725e3bcefd21bade9c2297ebd
Jon Freedman <jon.freedman@zoho.com>
2017-01-26 14:17:09 -0800

    MINOR: Update kafka-run-class.bat to handle spaces in classpath
    handle existing classpath with spaces
    list each lib dependency individually
    
    Author: Jon Freedman <jon.freedman@zoho.com>
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>
    
    Closes #2433 from jonfreedman/trunk

--- bin/windows/kafka-run-class.bat
+++ bin/windows/kafka-run-class.bat
@@ -90,7 +90,9 @@ for %%p in (api runtime file json tools) do (
 )
 
 rem Classpath addition for release
-call :concat %BASE_DIR%\libs\*
+for %%i in (%BASE_DIR%\libs\*) do (
+	call :concat %%i
+)
 
 rem Classpath addition for core
 for %%i in (%BASE_DIR%\core\build\libs\kafka_%SCALA_BINARY_VERSION%*.jar) do (
@@ -174,7 +176,7 @@ IF ["%CLASSPATH%"] EQU [""] (
 	EXIT /B 2
 )
 
-set COMMAND=%JAVA% %KAFKA_HEAP_OPTS% %KAFKA_JVM_PERFORMANCE_OPTS% %KAFKA_JMX_OPTS% %KAFKA_LOG4J_OPTS% -cp %CLASSPATH% %KAFKA_OPTS% %*
+set COMMAND=%JAVA% %KAFKA_HEAP_OPTS% %KAFKA_JVM_PERFORMANCE_OPTS% %KAFKA_JMX_OPTS% %KAFKA_LOG4J_OPTS% -cp "%CLASSPATH%" %KAFKA_OPTS% %*
 rem echo.
 rem echo %COMMAND%
 rem echo.
@@ -184,7 +186,7 @@ rem echo.
 goto :eof
 :concat
 IF ["%CLASSPATH%"] EQU [""] (
-  set CLASSPATH="%1"
+  set "CLASSPATH=%1"
 ) ELSE (
-  set CLASSPATH=%CLASSPATH%;"%1"
+  set "CLASSPATH=%CLASSPATH%;%1"
 )
