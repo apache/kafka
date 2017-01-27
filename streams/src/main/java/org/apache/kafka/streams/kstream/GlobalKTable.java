1bd11b23ab55b25d5669813f47ad1d5670c6ec9c
Matthias J. Sax <matthias@confluent.io>
2017-01-27 08:40:53 -0800

    MINOR: Update KTable JavaDoc
    Author: Matthias J. Sax <matthias@confluent.io>
    
    Reviewers: Damian Guy <damian.guy@gmail.com>
    
    Closes #2438 from mjsax/ktableJavaDocs

--- streams/src/main/java/org/apache/kafka/streams/kstream/GlobalKTable.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/GlobalKTable.java
@@ -19,40 +19,49 @@ package org.apache.kafka.streams.kstream;
 
 import org.apache.kafka.common.annotation.InterfaceStability;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 
 /**
  * {@link GlobalKTable} is an abstraction of a <i>changelog stream</i> from a primary-keyed table.
- * Each record in this stream is an update on the primary-keyed table with the record key as the primary key.
+ * Each record in this changelog stream is an update on the primary-keyed table with the record key as the primary key.
  * <p>
- * A {@link GlobalKTable} is fully replicated per {@link KafkaStreams} instance. Every partition of the underlying topic
- * is consumed by each {@link GlobalKTable}, such that the full set of data is available in every {@link KafkaStreams} instance.
- * This provides the ability to perform joins with {@link KStream}, and {@link KTable},
- * without having to repartition the input streams. All joins with the {@link GlobalKTable} require that a {@link KeyValueMapper}
- * is provided that can map from the (key, value) of the left hand side to the key of the right hand side {@link GlobalKTable}
+ * {@link GlobalKTable} can only be used as right-hand side input for {@link KStream stream}-table joins.
+ * <p>
+ * In contrast to a {@link KTable} that is partitioned over all {@link KafkaStreams} instances, a {@link GlobalKTable}
+ * is fully replicated per {@link KafkaStreams} instance.
+ * Every partition of the underlying topic is consumed by each {@link GlobalKTable}, such that the full set of data is
+ * available in every {@link KafkaStreams} instance.
+ * This provides the ability to perform joins with {@link KStream} without having to repartition the input stream.
+ * All joins with the {@link GlobalKTable} require that a {@link KeyValueMapper} is provided that can map from the
+ * {@link KeyValue} of the left hand side {@link KStream} to the key of the right hand side {@link GlobalKTable}.
  * <p>
  * A {@link GlobalKTable} is created via a {@link KStreamBuilder}. For example:
- * <pre>
- *     builder.globalTable("topic-name", "queryable-store-name");
- * </pre>
- * all {@link GlobalKTable}s are backed by a {@link org.apache.kafka.streams.state.ReadOnlyKeyValueStore ReadOnlyKeyValueStore}
- * and are therefore queryable via the interactive queries API.
+ * <pre>{@code
+ * builder.globalTable("topic-name", "queryable-store-name");
+ * }</pre>
+ * all {@link GlobalKTable}s are backed by a {@link ReadOnlyKeyValueStore} and are therefore queryable via the
+ * interactive queries API.
  * For example:
  * <pre>{@code
- *     final GlobalKTable globalOne = builder.globalTable("g1", "g1-store");
- *     final GlobalKTable globalTwo = builder.globalTable("g2", "g2-store");
- *     ...
- *     final KafkaStreams streams = ...;
- *     streams.start()
- *     ...
- *     ReadOnlyKeyValueStore view = streams.store("g1-store", QueryableStoreTypes.keyValueStore());
- *     view.get(key);
+ * final GlobalKTable globalOne = builder.globalTable("g1", "g1-store");
+ * final GlobalKTable globalTwo = builder.globalTable("g2", "g2-store");
+ * ...
+ * final KafkaStreams streams = ...;
+ * streams.start()
+ * ...
+ * ReadOnlyKeyValueStore view = streams.store("g1-store", QueryableStoreTypes.keyValueStore());
+ * view.get(key); // can be done on any key, as all keys are present
  *}</pre>
- *
+ * Note that in contrast to {@link KTable} a {@link GlobalKTable}'s state holds a full copy if the underlying topic and
+ * thus, all keys can be queried locally.
  *
  * @param <K> Type of primary keys
  * @param <V> Type of value changes
- *
  * @see KTable
+ * @see KStreamBuilder#globalTable(String, String)
+ * @see KStream#join(GlobalKTable, KeyValueMapper, ValueJoiner)
+ * @see KStream#leftJoin(GlobalKTable, KeyValueMapper, ValueJoiner)
  */
 @InterfaceStability.Unstable
 public interface GlobalKTable<K, V> {
