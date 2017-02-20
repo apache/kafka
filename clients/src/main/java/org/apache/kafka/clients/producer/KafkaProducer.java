d10417802d58d4282fe4a39377e38df1a6d4a7db
huxi <huxi@zhenrongbao.com>
2017-02-20 10:27:27 +0800

    kafka-4767: KafkaProducer is not joining its IO thread properly
    KafkaProducer#close swallows the InterruptedException which might be acceptable when it's invoked from within the main thread or user is extending Thread and therefore control all the code higher up on the call stack. For other cases, it'd better retstore the interupted status after capturing the exception.

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -702,6 +702,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
                     try {
                         this.ioThread.join(timeUnit.toMillis(timeout));
                     } catch (InterruptedException t) {
+                        Thread.currentThread().interrupt();
                         firstException.compareAndSet(null, t);
                         log.error("Interrupted while joining ioThread", t);
                     }
@@ -718,6 +719,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
                 try {
                     this.ioThread.join();
                 } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                     firstException.compareAndSet(null, e);
                 }
             }
