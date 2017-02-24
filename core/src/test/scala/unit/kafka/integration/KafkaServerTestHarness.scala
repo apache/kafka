11b637ec7d2b8dbd2ac57ca2f2b360b2a0707ab9
Armin Braun <me@obrown.io>
2017-02-24 11:44:31 +0000

    KAFKA-4198; Fix race condition in KafkaServer.shutdown()
    It contained this step:
    
          val canShutdown = isShuttingDown.compareAndSet(false, true)
          if (canShutdown && shutdownLatch.getCount > 0) {
    
    without any fallback for the case of `shutdownLatch.getCount == 0`. So in the case
    of `shutdownLatch.getCount == 0` (when a previous call to the shutdown method
    was right about to finish) you would set `isShuttingDown` to true again without any
    possibility of ever getting the server started (since `startup` will check
    `isShuttingDown` before setting up a new latch with count 1).
    
    Long story short: concurrent calls to shutdown can get the server locked in a broken state.
    
    This fixes the reported error:
    
    java.lang.IllegalStateException: Kafka server is still shutting down, cannot re-start!
    	at kafka.server.KafkaServer.startup(KafkaServer.scala:184)
    	at kafka.integration.KafkaServerTestHarness$$anonfun$restartDeadBrokers$2.apply$mcVI$sp(KafkaServerTestHarness.scala:117)
    	at kafka.integration.KafkaServerTestHarness$$anonfun$restartDeadBrokers$2.apply(KafkaServerTestHarness.scala:116)
    	at kafka.integration.KafkaServerTestHarness$$anonfun$restartDeadBrokers$2.apply(KafkaServerTestHarness.scala:116)
    	at scala.collection.TraversableLike$WithFilter$$anonfun$foreach$1.apply(TraversableLike.scala:733)
    	at scala.collection.immutable.Range.foreach(Range.scala:160)
    	at scala.collection.TraversableLike$WithFilter.foreach(TraversableLike.scala:732)
    	at kafka.integration.KafkaServerTestHarness$class.restartDeadBrokers(KafkaServerTestHarness.scala:116)
    	at kafka.api.ConsumerBounceTest.restartDeadBrokers(ConsumerBounceTest.scala:34)
    	at kafka.api.ConsumerBounceTest$BounceBrokerScheduler.doWork(ConsumerBounceTest.scala:158)
    
    Author: Armin Braun <me@obrown.io>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    
    Closes #2568 from original-brownbear/KAFKA-4198
    
    (cherry picked from commit 1ed4b48d5a43af743229fafc22f78846c9c3134d)
    Signed-off-by: Ismael Juma <ismael@juma.me.uk>

--- core/src/test/scala/unit/kafka/integration/KafkaServerTestHarness.scala
+++ core/src/test/scala/unit/kafka/integration/KafkaServerTestHarness.scala
@@ -5,7 +5,7 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -102,7 +102,7 @@ trait KafkaServerTestHarness extends ZooKeeperTestHarness {
     }
     super.tearDown
   }
-  
+
   /**
    * Pick a broker at random and kill it if it isn't already dead
    * Return the id of the broker killed
@@ -120,7 +120,7 @@ trait KafkaServerTestHarness extends ZooKeeperTestHarness {
       alive(index) = false
     }
   }
-  
+
   /**
    * Restart any dead brokers
    */
