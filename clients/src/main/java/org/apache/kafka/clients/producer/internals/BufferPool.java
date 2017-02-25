8fdbed4d7f85c25c72ec648fb3b40be507b38ee0
Ismael Juma <ismael@juma.me.uk>
2017-02-24 16:41:51 -0800

    MINOR: Fix potential integer overflow and String.format issue
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Apurva Mehta <apurva.1618@gmail.com>, Jason Gustafson <jason@confluent.io>
    
    Closes #2585 from ijuma/overflow-and-format-fixes

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/BufferPool.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/BufferPool.java
@@ -41,7 +41,7 @@ import org.apache.kafka.common.utils.Time;
  * buffers are deallocated.
  * </ol>
  */
-public final class BufferPool {
+public class BufferPool {
 
     private final long totalMemory;
     private final int poolableSize;
@@ -65,8 +65,8 @@ public final class BufferPool {
     public BufferPool(long memory, int poolableSize, Metrics metrics, Time time, String metricGrpName) {
         this.poolableSize = poolableSize;
         this.lock = new ReentrantLock();
-        this.free = new ArrayDeque<ByteBuffer>();
-        this.waiters = new ArrayDeque<Condition>();
+        this.free = new ArrayDeque<>();
+        this.waiters = new ArrayDeque<>();
         this.totalMemory = memory;
         this.availableMemory = memory;
         this.metrics = metrics;
@@ -104,14 +104,14 @@ public final class BufferPool {
 
             // now check if the request is immediately satisfiable with the
             // memory on hand or if we need to block
-            int freeListSize = this.free.size() * this.poolableSize;
+            int freeListSize = freeSize() * this.poolableSize;
             if (this.availableMemory + freeListSize >= size) {
                 // we have enough unallocated or pooled memory to immediately
                 // satisfy the request
                 freeUp(size);
                 this.availableMemory -= size;
                 lock.unlock();
-                return ByteBuffer.allocate(size);
+                return allocateByteBuffer(size);
             } else {
                 // we are out of memory and will have to block
                 int accumulated = 0;
@@ -174,7 +174,7 @@ public final class BufferPool {
                 // unlock and return the buffer
                 lock.unlock();
                 if (buffer == null)
-                    return ByteBuffer.allocate(size);
+                    return allocateByteBuffer(size);
                 else
                     return buffer;
             }
@@ -182,6 +182,11 @@ public final class BufferPool {
             if (lock.isHeldByCurrentThread())
                 lock.unlock();
         }
+    }
+
+    // Protected for testing.
+    protected ByteBuffer allocateByteBuffer(int size) {
+        return ByteBuffer.allocate(size);
     }
 
     /**
@@ -228,10 +233,15 @@ public final class BufferPool {
     public long availableMemory() {
         lock.lock();
         try {
-            return this.availableMemory + this.free.size() * this.poolableSize;
+            return this.availableMemory + freeSize() * (long) this.poolableSize;
         } finally {
             lock.unlock();
         }
+    }
+
+    // Protected for testing.
+    protected int freeSize() {
+        return this.free.size();
     }
 
     /**
