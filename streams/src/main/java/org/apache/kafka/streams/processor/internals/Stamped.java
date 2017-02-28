d0e436c471ba4122ddcc0f7a1624546f97c4a517
Matthias J. Sax <matthias@confluent.io>
2017-02-28 12:35:04 -0800

    MINOR: improve license header check by providing head file instead of (prefix) header regex
    Author: Matthias J. Sax <matthias@confluent.io>
    
    Reviewers: Jason Gustafson <jason@confluent.io>, Ewen Cheslack-Postava <ewen@confluent.io>
    
    Closes #2303 from mjsax/licenseHeader

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/Stamped.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/Stamped.java
@@ -1,10 +1,10 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.kafka.streams.processor.internals;
 
 public class Stamped<V> implements Comparable {
--- tools/src/main/java/org/apache/kafka/tools/VerifiableConsumer.java
+++ tools/src/main/java/org/apache/kafka/tools/VerifiableConsumer.java
@@ -1,10 +1,10 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -57,7 +57,6 @@ import java.util.concurrent.CountDownLatch;
 
 import static net.sourceforge.argparse4j.impl.Arguments.store;
 import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
-
 
 /**
  * Command line consumer designed for system testing. It outputs consumer events to STDOUT as JSON
