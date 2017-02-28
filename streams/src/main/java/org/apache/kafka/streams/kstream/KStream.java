806e982cc3e8b0dcfcddd2563ce3daeb8bc9b08e
Michael G. Noll <michael@confluent.io>
2017-02-28 13:28:11 +0100

    Fix return type of `flatMap` code example

--- streams/src/main/java/org/apache/kafka/streams/kstream/KStream.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/KStream.java
@@ -192,7 +192,7 @@ public interface KStream<K, V> {
      * and emit a record {@code <word:1>} for each word.
      * <pre>{@code
      * KStream<byte[], String> inputStream = builder.stream("topic");
-     * KStream<Integer, String> outputStream = inputStream.flatMap(new KeyValueMapper<byte[], String, Iterable<KeyValue<String, Integer>>> {
+     * KStream<String, Integer> outputStream = inputStream.flatMap(new KeyValueMapper<byte[], String, Iterable<KeyValue<String, Integer>>> {
      *     Iterable<KeyValue<String, Integer>> apply(byte[] key, String value) {
      *         String[] tokens = value.split(" ");
      *         List<KeyValue<String, Integer>> result = new ArrayList<>(tokens.length);
