b9875ce73f20578f6562a2c85aa738e48be4dc64
huxi <huxi@zhenrongbao.com>
2017-03-02 14:53:00 +0800

    kafka4811: ReplicaFetchThread may fail to create due to existing metric
    Removed unnecessary BrokerIdAndFetcher instance creation.

--- core/src/main/scala/kafka/server/AbstractFetcherManager.scala
+++ core/src/main/scala/kafka/server/AbstractFetcherManager.scala
@@ -84,7 +84,7 @@ abstract class AbstractFetcherManager(protected val name: String, clientId: Stri
 
       for ((brokerAndFetcherId, partitionAndOffsets) <- partitionsPerFetcher) {
         val brokerIdAndFetcherId = BrokerIdAndFetcherId(brokerAndFetcherId.broker.id, brokerAndFetcherId.fetcherId)
-        fetcherThreadMap.get(BrokerIdAndFetcherId(brokerAndFetcherId.broker.id, brokerAndFetcherId.fetcherId)) match {
+        fetcherThreadMap.get(brokerIdAndFetcherId) match {
           case Some(f) if f.sourceBroker.host == brokerAndFetcherId.broker.host && f.sourceBroker.port == brokerAndFetcherId.broker.port =>
             // reuse the fetcher thread
           case Some(f) =>
