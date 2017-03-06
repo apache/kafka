6a2a7b5d608adbc29b81b0f016de3b27c2295317
Armin Braun <me@obrown.io>
2017-03-06 18:17:13 +0100

    KAFKA-3155: added tests for RecordBatch#maybeExpire with past now parameter

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordBatchTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordBatchTest.java
@@ -29,6 +29,22 @@ import static org.junit.Assert.assertFalse;
 public class RecordBatchTest {
 
     /**
+     * Arbitrary constant timestamp used as reproducible "now" reference.
+     */
+    private static final long REFERENCE_NOW = 1488748346917L;
+
+    /**
+     * Arbitrary constant MemoryRecordsBuilder for instantiating RecordBatch,
+     * when the specifics of its MemoryRecordsBuilder are not relevant.
+     */
+    private static final MemoryRecordsBuilder MEMORY_RECORDS_BUILDER =
+        new MemoryRecordsBuilder(
+            ByteBuffer.allocate(0), Record.CURRENT_MAGIC_VALUE,
+            CompressionType.NONE,
+            TimestampType.CREATE_TIME, 0L, Record.NO_TIMESTAMP, 0
+        );
+
+    /**
      * A RecordBatch configured using very large linger value, in combination
      * with a timestamp preceding the create time of a RecordBatch, is
      * interpreted correctly as not expired when the linger time is larger than
@@ -36,18 +52,48 @@ public class RecordBatchTest {
      */
     @Test
     public void testLargeLingerOldNowExpire() {
-        final long now = 1488748346917L;
+        final long now = REFERENCE_NOW;
         final RecordBatch batch = new RecordBatch(
-            new TopicPartition("topic", 1),
-            new MemoryRecordsBuilder(
-                ByteBuffer.allocate(0), Record.CURRENT_MAGIC_VALUE,
-                CompressionType.NONE,
-                TimestampType.CREATE_TIME, 0L, Record.NO_TIMESTAMP, 0
-            ), now
+            new TopicPartition("topic", 1), MEMORY_RECORDS_BUILDER, now
         );
         // Use `now` 2ms before the create time.
         assertFalse(
             batch.maybeExpire(10240, 100L, now - 2L, Long.MAX_VALUE, false)
+        );
+    }
+
+    /**
+     * A RecordBatch configured using very large retryBackoff value, in combination
+     * with a timestamp preceding the create time of a RecordBatch, is
+     * interpreted correctly as not expired when the retryBackoff time is larger than
+     * the difference between now and create time by RecordBatch#maybeExpire.
+     */
+    @Test
+    public void testLargeRetryBackoffOldNowExpire() {
+        final long now = REFERENCE_NOW;
+        final RecordBatch batch = new RecordBatch(
+            new TopicPartition("topic", 1), MEMORY_RECORDS_BUILDER, now
+        );
+        // Use `now` 2ms before the create time.
+        assertFalse(
+            batch.maybeExpire(10240, Long.MAX_VALUE, now - 2L, 10240L, false)
+        );
+    }
+
+    /**
+     * A RecordBatch#maybeExpire call with a now value before the create time of
+     * the RecordBatch as correctly recognized as not expired when invoked with
+     * parameter {@code isFull} equal to {@code true}.
+     */
+    @Test
+    public void testLargeFullOldNowExpire() {
+        final long now = REFERENCE_NOW;
+        final RecordBatch batch = new RecordBatch(
+            new TopicPartition("topic", 1), MEMORY_RECORDS_BUILDER, now
+        );
+        // Use `now` 2ms before the create time.
+        assertFalse(
+            batch.maybeExpire(10240, 10240L, now - 2L, 10240L, true)
         );
     }
 }
