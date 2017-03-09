b0f0174332d2d5c23a3fc88e8c606641562f96bd
Ismael Juma <ismael@juma.me.uk>
2017-03-09 09:27:33 -0800

    MINOR: Introduce NetworkClient.hasInFlightRequests
    It’s a minor optimisation, but simple enough.
    
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Apurva Mehta <apurva.1618@gmail.com>, Jason Gustafson <jason@confluent.io>
    
    Closes #2658 from ijuma/has-in-flight-requests

--- clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
+++ clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
@@ -386,7 +386,12 @@ public class NetworkClient implements KafkaClient {
      */
     @Override
     public int inFlightRequestCount() {
-        return this.inFlightRequests.inFlightRequestCount();
+        return this.inFlightRequests.count();
+    }
+
+    @Override
+    public boolean hasInFlightRequests() {
+        return !this.inFlightRequests.isEmpty();
     }
 
     /**
@@ -394,7 +399,12 @@ public class NetworkClient implements KafkaClient {
      */
     @Override
     public int inFlightRequestCount(String node) {
-        return this.inFlightRequests.inFlightRequestCount(node);
+        return this.inFlightRequests.count(node);
+    }
+
+    @Override
+    public boolean hasInFlightRequests(String node) {
+        return this.inFlightRequests.isEmpty(node);
     }
 
     /**
@@ -431,7 +441,7 @@ public class NetworkClient implements KafkaClient {
         for (int i = 0; i < nodes.size(); i++) {
             int idx = (offset + i) % nodes.size();
             Node node = nodes.get(idx);
-            int currInflight = this.inFlightRequests.inFlightRequestCount(node.idString());
+            int currInflight = this.inFlightRequests.count(node.idString());
             if (currInflight == 0 && this.connectionStates.isReady(node.idString())) {
                 // if we find an established connection with no in-flight requests we can stop right away
                 log.trace("Found least loaded node {} connected with no in-flight requests", node);
