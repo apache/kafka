apache/kafka

commit fc1cfe475e8ae8458d8ddf119ce18d0c64653a70

MINOR: Remove Struct from Request/Response classes
More details:
* Replaced `struct` field in Request/Response with a `toStruct` method. This
makes the performance model (including memory usage) easier to understand.
Note that requests have `toStruct()` while responses have `toStruct(version)`.
* Replaced mutable `version` field in `Request.Builder` with an immutable
field `desiredVersion` and a `version` parameter passed to the `build` method.
* Optimised `handleFetchRequest` to avoid unnecessary creation of `Struct`
instances (from 4 to 2 in the worst case and 2 to 1 in the best case).
* Various clean-ups in request/response classes and their test. In particular,
it is now clear what we are testing. Previously, it looked like we were testing
more than we really were.

With this in place, we could remove `AbstractRequest.Builder` in the future by
doing the following:
* Change `AbstractRequest.toStruct` to accept a version (like responses).
* Change `AbstractRequest.version` to be `desiredVersion` (like `Builder`).
* Change `ClientRequest` to take `AbstractRequest`.
* Move validation from the `build` methods to the request constructors or
static factory methods.
* Anything else required for the code to compile again.

Author: Ismael Juma <ismael@juma.me.uk>

Reviewers: Apurva Mehta <apurva.1618@gmail.com>, Jason Gustafson <jason@confluent.io>

Closes #2513 from ijuma/separate-struct

clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
@@ -25,40 +25,39 @@ import org.apache.kafka.common.protocol.types.Struct;
 import java.nio.ByteBuffer;
 
 public abstract class AbstractRequest extends AbstractRequestResponse {
-    private final short version;
 
     public static abstract class Builder<T extends AbstractRequest> {
         private final ApiKeys apiKey;
-        private short version;
+        private final Short desiredVersion;
 
         public Builder(ApiKeys apiKey) {
+            this(apiKey, null);
+        }
+
+        public Builder(ApiKeys apiKey, Short desiredVersion) {
             this.apiKey = apiKey;
-            this.version = ProtoUtils.latestVersion(apiKey.id);
+            this.desiredVersion = desiredVersion;
         }
 
         public ApiKeys apiKey() {
             return apiKey;
         }
 
-        public Builder<T> setVersion(short version) {
-            this.version = version;
-            return this;
+        public short desiredOrLatestVersion() {
+            return desiredVersion == null ? ProtoUtils.latestVersion(apiKey.id) : desiredVersion;
         }
 
-        public short version() {
-            return version;
+        public T build() {
+            return build(desiredOrLatestVersion());
         }
 
-        public abstract T build();
+        public abstract T build(short version);
     }
 
-    public AbstractRequest(Struct struct, short version) {
-        super(struct);
+    private final short version;
+
+    public AbstractRequest(short version) {
         this.version = version;
-    }
-
-    public Send toSend(String destination, RequestHeader header) {
-        return new NetworkSend(destination, serialize(header, this));
     }
 
     /**
@@ -68,6 +67,19 @@ public abstract class AbstractRequest extends AbstractRequestResponse {
         return version;
     }
 
+    public Send toSend(String destination, RequestHeader header) {
+        return new NetworkSend(destination, serialize(header));
+    }
+
+    /**
+     * Use with care, typically {@link #toSend(String, RequestHeader)} should be used instead.
+     */
+    public ByteBuffer serialize(RequestHeader header) {
+        return serialize(header.toStruct(), toStruct());
+    }
+
+    protected abstract Struct toStruct();
+
     /**
      * Get an error response for a request
      */
@@ -76,54 +88,78 @@ public abstract class AbstractRequest extends AbstractRequestResponse {
     /**
      * Factory method for getting a request object based on ApiKey ID and a buffer
      */
-    public static AbstractRequest getRequest(int requestId, short versionId, ByteBuffer buffer) {
+    public static RequestAndSize getRequest(int requestId, short version, ByteBuffer buffer) {
         ApiKeys apiKey = ApiKeys.forId(requestId);
+        Struct struct = ProtoUtils.parseRequest(apiKey.id, version, buffer);
+        AbstractRequest request;
         switch (apiKey) {
             case PRODUCE:
-                return ProduceRequest.parse(buffer, versionId);
+                request = new ProduceRequest(struct, version);
+                break;
             case FETCH:
-                return FetchRequest.parse(buffer, versionId);
+                request = new FetchRequest(struct, version);
+                break;
             case LIST_OFFSETS:
-                return ListOffsetRequest.parse(buffer, versionId);
+                request = new ListOffsetRequest(struct, version);
+                break;
             case METADATA:
-                return MetadataRequest.parse(buffer, versionId);
+                request = new MetadataRequest(struct, version);
+                break;
             case OFFSET_COMMIT:
-                return OffsetCommitRequest.parse(buffer, versionId);
+                request = new OffsetCommitRequest(struct, version);
+                break;
             case OFFSET_FETCH:
-                return OffsetFetchRequest.parse(buffer, versionId);
+                request = new OffsetFetchRequest(struct, version);
+                break;
             case GROUP_COORDINATOR:
-                return GroupCoordinatorRequest.parse(buffer, versionId);
+                request = new GroupCoordinatorRequest(struct, version);
+                break;
             case JOIN_GROUP:
-                return JoinGroupRequest.parse(buffer, versionId);
+                request = new JoinGroupRequest(struct, version);
+                break;
             case HEARTBEAT:
-                return HeartbeatRequest.parse(buffer, versionId);
+                request = new HeartbeatRequest(struct, version);
+                break;
             case LEAVE_GROUP:
-                return LeaveGroupRequest.parse(buffer, versionId);
+                request = new LeaveGroupRequest(struct, version);
+                break;
             case SYNC_GROUP:
-                return SyncGroupRequest.parse(buffer, versionId);
+                request = new SyncGroupRequest(struct, version);
+                break;
             case STOP_REPLICA:
-                return StopReplicaRequest.parse(buffer, versionId);
+                request = new StopReplicaRequest(struct, version);
+                break;
             case CONTROLLED_SHUTDOWN_KEY:
-                return ControlledShutdownRequest.parse(buffer, versionId);
+                request = new ControlledShutdownRequest(struct, version);
+                break;
             case UPDATE_METADATA_KEY:
-                return UpdateMetadataRequest.parse(buffer, versionId);
+                request = new UpdateMetadataRequest(struct, version);
+                break;
             case LEADER_AND_ISR:
-                return LeaderAndIsrRequest.parse(buffer, versionId);
+                request = new LeaderAndIsrRequest(struct, version);
+                break;
             case DESCRIBE_GROUPS:
-                return DescribeGroupsRequest.parse(buffer, versionId);
+                request = new DescribeGroupsRequest(struct, version);
+                break;
             case LIST_GROUPS:
-                return ListGroupsRequest.parse(buffer, versionId);
+                request = new ListGroupsRequest(struct, version);
+                break;
             case SASL_HANDSHAKE:
-                return SaslHandshakeRequest.parse(buffer, versionId);
+                request = new SaslHandshakeRequest(struct, version);
+                break;
             case API_VERSIONS:
-                return ApiVersionsRequest.parse(buffer, versionId);
+                request = new ApiVersionsRequest(struct, version);
+                break;
             case CREATE_TOPICS:
-                return CreateTopicsRequest.parse(buffer, versionId);
+                request = new CreateTopicsRequest(struct, version);
+                break;
             case DELETE_TOPICS:
-                return DeleteTopicsRequest.parse(buffer, versionId);
+                request = new DeleteTopicsRequest(struct, version);
+                break;
             default:
                 throw new AssertionError(String.format("ApiKey %s is not currently handled in `getRequest`, the " +
                         "code should be updated to do so.", apiKey));
         }
+        return new RequestAndSize(request, struct.sizeOf());
     }
 }

clients/src/main/java/org/apache/kafka/common/requests/RequestHeader.java
@@ -35,8 +35,7 @@ public class RequestHeader extends AbstractRequestResponse {
     private final String clientId;
     private final int correlationId;
 
-    public RequestHeader(Struct header) {
-        super(header);
+    public RequestHeader(Struct struct) {
         apiKey = struct.getShort(API_KEY_FIELD);
         apiVersion = struct.getShort(API_VERSION_FIELD);
         clientId = struct.getString(CLIENT_ID_FIELD);
@@ -44,15 +43,19 @@ public class RequestHeader extends AbstractRequestResponse {
     }
 
     public RequestHeader(short apiKey, short version, String client, int correlation) {
-        super(new Struct(Protocol.REQUEST_HEADER));
-        struct.set(API_KEY_FIELD, apiKey);
-        struct.set(API_VERSION_FIELD, version);
-        struct.set(CLIENT_ID_FIELD, client);
-        struct.set(CORRELATION_ID_FIELD, correlation);
         this.apiKey = apiKey;
         this.apiVersion = version;
         this.clientId = client;
         this.correlationId = correlation;
+    }
+
+    public Struct toStruct() {
+        Struct struct = new Struct(Protocol.REQUEST_HEADER);
+        struct.set(API_KEY_FIELD, apiKey);
+        struct.set(API_VERSION_FIELD, apiVersion);
+        struct.set(CLIENT_ID_FIELD, clientId);
+        struct.set(CORRELATION_ID_FIELD, correlationId);
+        return struct;
     }
 
     public short apiKey() {
@@ -69,6 +72,10 @@ public class RequestHeader extends AbstractRequestResponse {
 
     public int correlationId() {
         return correlationId;
+    }
+
+    public ResponseHeader toResponseHeader() {
+        return new ResponseHeader(correlationId);
     }
 
     public static RequestHeader parse(ByteBuffer buffer) {

commit 2a9c18a87d4374d03f8a07b37a4e66d74325f1c2

KAFKA-4891; kafka.request.logger TRACE regression
Both the headers and requests have regressed to just show object ids instead of their contents from their underlying structs. I'm guessing this regression came from commit [fc1cfe475e8ae8458d8ddf119ce18d0c64653a70](https://github.com/apache/kafka/commit/fc1cfe475e8ae8458d8ddf119ce18d0c64653a70)

Author: Onur Karaman <okaraman@linkedin.com>

Reviewers: Jiangjie Qin <becket.qin@gmail.com>

Closes #2678 from onurkaraman/KAFKA-4891

clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
@@ -79,6 +79,11 @@ public abstract class AbstractRequest extends AbstractRequestResponse {
 
     protected abstract Struct toStruct();
 
+    @Override
+    public String toString() {
+        return toStruct().toString();
+    }
+
     /**
      * Get an error response for a request
      */

clients/src/main/java/org/apache/kafka/common/requests/RequestHeader.java
@@ -85,4 +85,9 @@ public class RequestHeader extends AbstractRequestResponse {
     public static RequestHeader parse(ByteBuffer buffer) {
         return new RequestHeader(Protocol.REQUEST_HEADER.read(buffer));
     }
+
+    @Override
+    public String toString() {
+        return toStruct().toString();
+    }
 }