0cdd0d691fd67800674833a5809b357ee6dc4afa
Wladimir Schmidt <wlsc.dev@gmail.com>
2017-03-16 21:45:54 +0100

    * fixed memory pollution due to unnecessary creation of additional HashMap

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -762,7 +762,7 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
     public Set<TopicPartition> assignment() {
         acquire();
         try {
-            return Collections.unmodifiableSet(new HashSet<>(this.subscriptions.assignedPartitions()));
+            return Collections.unmodifiableSet(this.subscriptions.assignedPartitions());
         } finally {
             release();
         }
@@ -776,7 +776,7 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
     public Set<String> subscription() {
         acquire();
         try {
-            return Collections.unmodifiableSet(new HashSet<>(this.subscriptions.subscription()));
+            return Collections.unmodifiableSet(this.subscriptions.subscription());
         } finally {
             release();
         }
