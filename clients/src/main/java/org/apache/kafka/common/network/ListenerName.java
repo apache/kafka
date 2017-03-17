01768e3321cfce19bf558ad0fe7bf6ca84ada8f6
Kamal C <kamal.chandraprakash@gmail.com>
2017-03-17 13:49:19 +0000

    MINOR: Fix non-null check in the constructor of ListenerName
    Author: Kamal C <kamal.chandraprakash@gmail.com>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    
    Closes #2700 from Kamal15/precondition

--- clients/src/main/java/org/apache/kafka/common/network/ListenerName.java
+++ clients/src/main/java/org/apache/kafka/common/network/ListenerName.java
@@ -42,7 +42,7 @@ public final class ListenerName {
     private final String value;
 
     public ListenerName(String value) {
-        Objects.requireNonNull("value should not be null");
+        Objects.requireNonNull(value, "value should not be null");
         this.value = value;
     }
 
