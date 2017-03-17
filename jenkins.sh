f97a6f1a147eff056f606da101aa37baa10e3692
Damian Guy <damian.guy@gmail.com>
2017-03-17 12:26:46 +0000

    add common jenkins build arguments

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./gradlew clean compileJava compileScala compileTestJava compileTestScala checkstyleMain checkstyleTest unitTest integrationTest $@
+./gradlew clean compileJava compileScala compileTestJava compileTestScala checkstyleMain checkstyleTest unitTest integrationTest --no-daemon -Dorg.gradle.project.testLoggingEvents=started,passed,skipped,failed $@
