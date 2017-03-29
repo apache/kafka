71dbf02e45c0573af4bdda9f5864fbd749b56031
Dong Lin <lindong28@gmail.com>
2017-03-29 10:00:59 -0700

    KAFKA-4973; Fix transient failure of AdminClientTest.testDeleteRecordsWithException

--- core/src/test/scala/integration/kafka/api/AdminClientTest.scala
+++ core/src/test/scala/integration/kafka/api/AdminClientTest.scala
@@ -178,7 +178,7 @@ class AdminClientTest extends IntegrationTestHarness with Logging {
     // OffsetOutOfRangeException if offset > high_watermark
     assertEquals(DeleteRecordsResult(-1L, Errors.OFFSET_OUT_OF_RANGE.exception()), client.deleteRecordsBefore(Map((tp, 20))).get()(tp))
     // TimeoutException if response is not available within user-specified timeout
-    assertEquals(DeleteRecordsResult(-1L, Errors.REQUEST_TIMED_OUT.exception()), client.deleteRecordsBefore(Map((tp, 5L))).get(0, TimeUnit.MILLISECONDS)(tp))
+    assertEquals(DeleteRecordsResult(-1L, Errors.REQUEST_TIMED_OUT.exception()), client.deleteRecordsBefore(Map((tp, 8L))).get(0, TimeUnit.MILLISECONDS)(tp))
 
     val nonExistPartition = new TopicPartition(topic, 3)
     // UnknownTopicOrPartitionException if user tries to delete records of a non-existent partition
