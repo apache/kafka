3dbfb18eb79065489efc0c0dd172ef41fff8402a
Colin P. Mccabe <cmccabe@confluent.io>
2017-03-29 16:07:15 +0100

    KAFKA-4903; Remove dead code in Shell
    Author: Colin P. Mccabe <cmccabe@confluent.io>
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    
    Closes #2692 from cmccabe/KAFKA-4903

--- clients/src/main/java/org/apache/kafka/common/Os.java
+++ clients/src/main/java/org/apache/kafka/common/Os.java
@@ -1,11 +1,11 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
+ * the License. You may obtain a copy of the License at
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,12 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.kafka.common;
 
-package kafka.utils
+import java.util.Locale;
 
-import java.util.Locale
+public final class Os {
+    public static final String NAME;
 
-object Os {
-  val name = System.getProperty("os.name").toLowerCase(Locale.ROOT)
-  val isWindows = name.startsWith("windows")
+    public static final boolean IS_WINDOWS;
+
+    static {
+        NAME = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        IS_WINDOWS = NAME.startsWith("windows");
+    }
 }
