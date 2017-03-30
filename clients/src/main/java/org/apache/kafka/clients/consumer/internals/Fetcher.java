f21617bebdc6897a11750d2f67cfb741e297697b
Jason Gustafson <jason@confluent.io>
2017-03-30 13:12:35 -0700

    Fix checkstyle

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/Fetcher.java
@@ -62,7 +62,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
