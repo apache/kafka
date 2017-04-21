23afe4c0c2cd060f0b3615e27796b0699dbef301
Matthias J. Sax <matthias@confluent.io>
2017-04-21 10:53:29 -0700

    KAFKA-4564: Add system test for pre-0.10 brokers
    Author: Matthias J. Sax <matthias@confluent.io>
    
    Reviewers: Ismael Juma, Eno Thereska, Matthias J. Sax, Guozhang Wang
    
    Closes #2837 from mjsax/kafka-4564-fail-fast-test-stream-compatibility

--- tests/kafkatest/tests/streams/streams_broker_compatibility_test.py
+++ tests/kafkatest/tests/streams/streams_broker_compatibility_test.py
@@ -21,13 +21,14 @@ from kafkatest.services.kafka import KafkaService
 from kafkatest.services.streams import StreamsBrokerCompatibilityService
 from kafkatest.services.verifiable_consumer import VerifiableConsumer
 from kafkatest.services.zookeeper import ZookeeperService
-from kafkatest.version import DEV_BRANCH, LATEST_0_10_1, LATEST_0_10_0, KafkaVersion
+from kafkatest.version import DEV_BRANCH, LATEST_0_10_1, LATEST_0_10_0, LATEST_0_9, LATEST_0_8_2, KafkaVersion
 
 
 class StreamsBrokerCompatibility(Test):
     """
     These tests validate that Streams v0.10.2+ can connect to older brokers v0.10.1+
-    and that Streams fails fast for pre-0.10.0 brokers
+    and that Streams fails fast for 0.10.0 brokers
+    and that Streams times-out for pre-0.10.0 brokers
     """
 
     input = "brokerCompatibilitySourceTopic"
@@ -84,5 +85,21 @@ class StreamsBrokerCompatibility(Test):
             monitor.wait_until('Exception in thread "main" org.apache.kafka.streams.errors.StreamsException: Kafka Streams requires broker version 0.10.1.x or higher.',
                         timeout_sec=60,
                         err_msg="Never saw 'incompatible broker' error message " + str(self.processor.node.account))
+
+        self.kafka.stop()
+
+    @parametrize(broker_version=str(LATEST_0_9))
+    @parametrize(broker_version=str(LATEST_0_8_2))
+    def test_timeout_on_pre_010_brokers(self, broker_version):
+        self.kafka.set_version(KafkaVersion(broker_version))
+        self.kafka.start()
+
+        self.processor.start()
+
+        self.processor.node.account.ssh(self.processor.start_cmd(self.processor.node))
+        with self.processor.node.account.monitor_log(self.processor.STDERR_FILE) as monitor:
+            monitor.wait_until('Exception in thread "main" org.apache.kafka.streams.errors.StreamsException: Could not find any available broker.',
+                               timeout_sec=60,
+                               err_msg="Never saw 'no available broker' error message " + str(self.processor.node.account))
 
         self.kafka.stop()
