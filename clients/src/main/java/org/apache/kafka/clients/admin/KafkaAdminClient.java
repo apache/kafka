60746657e0f6b00c673220388bac72eadab11823
Colin P. Mccabe <cmccabe@confluent.io>
2017-05-01 09:33:24 -0700

    Fix unused import

--- clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
@@ -69,7 +69,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
