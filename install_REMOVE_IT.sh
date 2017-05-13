e2b467c882a89bd31747e80557761c3dbd774584
Arseniy Tashoyan <tashoyan@gmail.com>
2017-05-13 16:55:12 +0300

    Add an utility to install on my machine - remove it later

---
+++ install_REMOVE_IT.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+install_dir=/work/install/kafka
+
+rm -r $install_dir/*
+cp core/build/distributions/kafka_2.10-0.11.0.0-SNAPSHOT.tgz $install_dir
+cd $install_dir
+tar -xzvf kafka_2.10-0.11.0.0-SNAPSHOT.tgz
+ln -s kafka_2.10-0.11.0.0-SNAPSHOT latest
+cd -
+
