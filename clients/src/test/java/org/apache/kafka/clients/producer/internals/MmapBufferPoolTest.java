b58118c6413a5e900f5c1ebee112bd24e8d4b119
Brandon Bradley <bradleytastic@gmail.com>
2017-05-17 18:34:04 -0500

    simple file buffer test

---
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/MmapBufferPoolTest.java
@@ -0,0 +1,26 @@
+package org.apache.kafka.clients.producer.internals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(PowerMockRunner.class)
+public class MmapBufferPoolTest {
+    private final long maxBlockTimeMs = 2000;
+
+    @Test
+    public void testSimple() throws Exception {
+        long totalMemory = 64 * 1024;
+        int size = 1024;
+        File storefile = new File(System.getProperty("java.io.tmpdir"), "kafka-producer-data-" + new Random().nextInt(Integer.MAX_VALUE) + ".dat");
+        BufferPool pool = new MmapBufferPool(storefile, totalMemory, size);
+        ByteBuffer buffer = pool.allocate(size, maxBlockTimeMs);
+        assertEquals("Buffer size should equal requested size.", size, buffer.limit());
+    }
+}
