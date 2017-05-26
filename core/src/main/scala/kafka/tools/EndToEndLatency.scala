b004b2e568ed2765997d45c919e5aee41c357c2f
John Doe <residentevil293@gmail.com>
2017-05-26 19:04:49 +0530

    Revert to 0f7d6f58452811c0fa89f833e235115519232cd7

--- core/src/main/scala/kafka/tools/EndToEndLatency.scala
+++ core/src/main/scala/kafka/tools/EndToEndLatency.scala
@@ -1,4 +1,4 @@
-/**
+**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -25,6 +25,10 @@ import org.apache.kafka.common.utils.Utils
 
 import scala.collection.JavaConverters._
 import scala.util.Random
+import org.{ HdrHistogram => hdr }
+import java.io._
+
+
 
 
 /**
@@ -38,11 +42,14 @@ import scala.util.Random
  * e.g. [localhost:9092 test 10000 1 20]
  */
 
+
+
 object EndToEndLatency {
-  private val timeout: Long = 60000
+  private val timeout: Long = 5000
+  val histogram = new hdr.AtomicHistogram(10000000000L, 3)
 
   def main(args: Array[String]) {
-    if (args.length != 5 && args.length != 6) {
+    if (args.length != 7 && args.length != 8) {
       System.err.println("USAGE: java " + getClass.getName + " broker_list topic num_messages producer_acks message_size_bytes [optional] properties_file")
       System.exit(1)
     }
@@ -52,7 +59,10 @@ object EndToEndLatency {
     val numMessages = args(2).toInt
     val producerAcks = args(3)
     val messageLen = args(4).toInt
-    val propsFile = if (args.length > 5) Some(args(5)).filter(_.nonEmpty) else None
+    val maxTime = args(5).toInt
+    val threads = args(6).toInt
+    val warmupTime = args(7).toInt
+    val propsFile = if (args.length > 8) Some(args(8)).filter(_.nonEmpty) else None
 
     if (!List("1", "all").contains(producerAcks))
       throw new IllegalArgumentException("Latency testing requires synchronous acknowledgement. Please use 1 or all")
@@ -68,8 +78,6 @@ object EndToEndLatency {
     consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer")
     consumerProps.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "0") //ensure we have no temporal batching
 
-    val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](consumerProps)
-    consumer.subscribe(Collections.singletonList(topic))
 
     val producerProps = loadProps
     producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
@@ -78,70 +86,108 @@ object EndToEndLatency {
     producerProps.put(ProducerConfig.ACKS_CONFIG, producerAcks.toString)
     producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
     producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
-    val producer = new KafkaProducer[Array[Byte], Array[Byte]](producerProps)
 
     def finalise() {
-      consumer.commitSync()
-      producer.close()
-      consumer.close()
+      val out_file = new java.io.FileOutputStream("latency_histogram.log")
+      val out_stream = new java.io.PrintStream(out_file)
+      histogram.outputPercentileDistribution(out_stream, 1000.0)
     }
 
-    //Ensure we are at latest offset. seekToEnd evaluates lazily, that is to say actually performs the seek only when
-    //a poll() or position() request is issued. Hence we need to poll after we seek to ensure we see our first write.
-    consumer.seekToEnd(Collections.emptyList())
-    consumer.poll(0)
+    val producer_threads = (1 to threads).map{e => new Thread(new ThrottledKafkaProducer(producerProps, numMessages, topic, maxTime, messageLen)) }
+    producer_threads.map{e => e.start() }
 
-    var totalTime = 0.0
-    val latencies = new Array[Long](numMessages)
-    val random = new Random(0)
+    val consumer_threads = (1 to threads).map{e => new Thread(new ThrottledKafkaConsumer(consumerProps, histogram, timeout, numMessages, topic, warmupTime))}
+    consumer_threads.map{e => e.start() }
 
-    for (i <- 0 until numMessages) {
-      val message = randomBytesOfLen(random, messageLen)
-      val begin = System.nanoTime
+    producer_threads.map{e => e.join() }
+    println("Produced all the messages")
 
-      //Send message (of random bytes) synchronously then immediately poll for it
-      producer.send(new ProducerRecord[Array[Byte], Array[Byte]](topic, message)).get()
-      val recordIter = consumer.poll(timeout).iterator
+    consumer_threads.map{e => e.join() }
+    println("Consumed all the messages")
 
-      val elapsed = System.nanoTime - begin
-
-      //Check we got results
-      if (!recordIter.hasNext) {
-        finalise()
-        throw new RuntimeException(s"poll() timed out before finding a result (timeout:[$timeout])")
-      }
-
-      //Check result matches the original record
-      val sent = new String(message)
-      val read = new String(recordIter.next().value())
-      if (!read.equals(sent)) {
-        finalise()
-        throw new RuntimeException(s"The message read [$read] did not match the message sent [$sent]")
-      }
-
-      //Check we only got the one message
-      if (recordIter.hasNext) {
-        val count = 1 + recordIter.asScala.size
-        throw new RuntimeException(s"Only one result was expected during this test. We found [$count]")
-      }
-
-      //Report progress
-      if (i % 1000 == 0)
-        println(i + "\t" + elapsed / 1000.0 / 1000.0)
-      totalTime += elapsed
-      latencies(i) = elapsed / 1000 / 1000
-    }
 
     //Results
-    println("Avg latency: %.4f ms\n".format(totalTime / numMessages / 1000.0 / 1000.0))
-    Arrays.sort(latencies)
-    val p50 = latencies((latencies.length * 0.5).toInt)
-    val p99 = latencies((latencies.length * 0.99).toInt)
-    val p999 = latencies((latencies.length * 0.999).toInt)
-    println("Percentiles: 50th = %d, 99th = %d, 99.9th = %d".format(p50, p99, p999))
-
     finalise()
   }
+
+  // TODO: Throttling yet to be done
+  class ThrottledKafkaProducer(producerProps: Properties, numMessages: Int, topic: String, maxTime: Int, messageLen: Int) extends Runnable {
+
+    def run() {
+          // This will block until a connection comes in.
+      val producer = new KafkaProducer[Array[Byte], Array[Byte]](producerProps)
+
+      val startTimeMS = System.currentTimeMillis
+      var timeDiff = true
+      var i = 0
+      val spaceBytes = Array.fill(messageLen)(' '.toByte)
+      while(timeDiff){
+
+        val begin = System.nanoTime
+        val message = begin.toString.getBytes() ++ spaceBytes
+
+        val currentTimeMS = System.currentTimeMillis
+
+        if((currentTimeMS - startTimeMS)< (maxTime*60*1000)){
+          producer.send(new ProducerRecord[Array[Byte], Array[Byte]](topic, message)).get()
+          i += 1
+          //Report progress
+          if (i % 10000 == 0)
+            println(i + "\t messages produced")
+        } else {
+          timeDiff = false
+        }
+        //Send message (of random bytes) synchronously then immediately poll for it
+
+        
+      }
+      producer.close()
+    }
+  }
+
+  // TODO: Throttling yet to be done
+  class ThrottledKafkaConsumer(consumerProps: Properties, histogram: hdr.Histogram, timeout: Long, numMessages: Int, topic: String, warmupTime: Int) extends Runnable {
+
+    def run() {
+      val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](consumerProps)
+      // This will block until a connection comes in.
+      consumer.subscribe(Collections.singletonList(topic))
+      consumer.seekToEnd(Collections.emptyList())
+      consumer.poll(0)
+
+      var done = true
+      var counter = 0
+      val max_count_value = 15
+      val startTimeMS = System.currentTimeMillis
+      while(done) {
+        val recordIter = consumer.poll(timeout).iterator
+      //Check we got results
+        if (recordIter.hasNext) {
+          val read = new String(recordIter.next().value())
+          val elapsed = System.nanoTime - read.trim.toLong
+          val currentTimeMS = System.currentTimeMillis
+          if((currentTimeMS - startTimeMS)>warmupTime*60*1000){
+            histogram.recordValue(elapsed)
+          }
+          // println(elapsed)
+        } else {
+          counter += 1
+          if(counter > max_count_value){
+            done = false
+          }
+        }
+
+      }
+      consumer.commitSync()
+      consumer.close()
+
+
+      println("After break")
+
+    }
+  }
+
+
 
   def randomBytesOfLen(random: Random, len: Int): Array[Byte] = {
     Array.fill(len)((random.nextInt(26) + 65).toByte)
