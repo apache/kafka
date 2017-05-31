3bc1734362c6e28a3f35fbe04df087e0eb3f39ed
Mario Molina <mmolimar@gmail.com>
2017-05-31 15:09:58 -0700

    KAFKA-5218; New Short serializer, deserializer, serde
    Author: Mario Molina <mmolimar@gmail.com>
    
    Reviewers: Matthias J. Sax <matthias@confluent.io>, Damian Guy <damian.guy@gmail.com>, Michael G. Noll <michael@confluent.io>, Guozhang Wang <wangguoz@gmail.com>
    
    Closes #3017 from mmolimar/KAFKA-5218
    
    (cherry picked from commit dc5bf4bd453495af050ea7c0ec7a66b8d2b2e8d4)
    Signed-off-by: Guozhang Wang <wangguoz@gmail.com>

--- clients/src/main/java/org/apache/kafka/common/serialization/ShortSerializer.java
+++ clients/src/main/java/org/apache/kafka/common/serialization/ShortSerializer.java
@@ -16,29 +16,22 @@
  */
 package org.apache.kafka.common.serialization;
 
-import org.apache.kafka.common.errors.SerializationException;
-
 import java.util.Map;
 
-public class IntegerDeserializer implements Deserializer<Integer> {
+public class ShortSerializer implements Serializer<Short> {
 
     public void configure(Map<String, ?> configs, boolean isKey) {
         // nothing to do
     }
 
-    public Integer deserialize(String topic, byte[] data) {
+    public byte[] serialize(String topic, Short data) {
         if (data == null)
             return null;
-        if (data.length != 4) {
-            throw new SerializationException("Size of data received by IntegerDeserializer is not 4");
-        }
 
-        int value = 0;
-        for (byte b : data) {
-            value <<= 8;
-            value |= b & 0xFF;
-        }
-        return value;
+        return new byte[] {
+            (byte) (data >>> 8),
+            data.byteValue()
+        };
     }
 
     public void close() {
