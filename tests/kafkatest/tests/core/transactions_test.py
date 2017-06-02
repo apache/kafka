311aa67c7af557ab6da521bf7dd8c5098d8f56de
Apurva Mehta <apurva@confluent.io>
2017-06-02 15:25:00 -0700

    Add a concurrent consumer in the transactions system tests. This will exercise the abort index

--- tests/kafkatest/tests/core/transactions_test.py
+++ tests/kafkatest/tests/core/transactions_test.py
@@ -84,7 +84,6 @@ class TransactionsTest(Test):
                                            message_validator=is_int,
                                            max_messages=self.num_seed_messages,
                                            enable_idempotence=True)
-
         seed_producer.start()
         wait_until(lambda: seed_producer.num_acked >= self.num_seed_messages,
                    timeout_sec=seed_timeout_sec,
@@ -93,28 +92,8 @@ class TransactionsTest(Test):
         return seed_producer.acked
 
     def get_messages_from_output_topic(self):
-        consumer = ConsoleConsumer(context=self.test_context,
-                                   num_nodes=1,
-                                   kafka=self.kafka,
-                                   topic=self.output_topic,
-                                   new_consumer=True,
-                                   message_validator=is_int,
-                                   from_beginning=True,
-                                   consumer_timeout_ms=5000,
-                                   isolation_level="read_committed")
-        consumer.start()
-        # ensure that the consumer is up.
-        wait_until(lambda: consumer.alive(consumer.nodes[0]) == True,
-                   timeout_sec=60,
-                   err_msg="Consumer failed to start for %ds" %\
-                   60)
-        # wait until the consumer closes, which will be 5 seconds after
-        # receiving the last message.
-        wait_until(lambda: consumer.alive(consumer.nodes[0]) == False,
-                   timeout_sec=60,
-                   err_msg="Consumer failed to consume %d messages in %ds" %\
-                   (self.num_seed_messages, 60))
-        return consumer.messages_consumed[1]
+        consumer = self.start_consumer(self.output_topic, group_id="verifying_consumer")
+        return self.drain_consumer(consumer)
 
     def bounce_brokers(self, clean_shutdown):
        for node in self.kafka.nodes:
@@ -170,8 +149,47 @@ class TransactionsTest(Test):
         ))
         return copiers
 
+    def start_consumer(self, topic_to_read, group_id):
+        consumer = ConsoleConsumer(context=self.test_context,
+                                   num_nodes=1,
+                                   kafka=self.kafka,
+                                   topic=topic_to_read,
+                                   group_id=group_id,
+                                   new_consumer=True,
+                                   message_validator=is_int,
+                                   from_beginning=True,
+                                   consumer_timeout_ms=5000,
+                                   isolation_level="read_committed")
+        consumer.start()
+        # ensure that the consumer is up.
+        wait_until(lambda: consumer.alive(consumer.nodes[0]) == True,
+                   timeout_sec=60,
+                   err_msg="Consumer failed to start for %ds" %\
+                   60)
+        return consumer
+
+    def drain_consumer(self, consumer):
+        # wait until the consumer closes, which will be 5 seconds after
+        # receiving the last message.
+        wait_until(lambda: consumer.alive(consumer.nodes[0]) == False,
+                   timeout_sec=60,
+                   err_msg="Consumer failed to consume %d messages in %ds" %\
+                   (self.num_seed_messages, 60))
+        return consumer.messages_consumed[1]
+
     def copy_messages_transactionally(self, failure_mode, bounce_target):
+        """Copies messages transactionally from the seeded input topic to the
+        output topic, either bouncing brokers or clients in a hard and soft
+        way as it goes.
+
+        This method also consumes messages in read_committed mode from the
+        output topic while the bounces and copy is going on.
+
+        It returns the concurrently consumed messages.
+        """
         copiers = self.create_and_start_copiers()
+        concurrent_consumer = self.start_consumer(self.output_topic,
+                                                  group_id="concurrent_consumer")
         clean_shutdown = False
         if failure_mode == "clean_bounce":
             clean_shutdown = True
@@ -187,21 +205,32 @@ class TransactionsTest(Test):
                        err_msg="%s - Failed to copy all messages in  %ds." %\
                        (copier.transactional_id, 60))
         self.logger.info("finished copying messages")
+        return self.drain_consumer(concurrent_consumer)
 
-    @cluster(num_nodes=8)
+    @cluster(num_nodes=9)
     @matrix(failure_mode=["clean_bounce", "hard_bounce"],
-            bounce_target=["brokers", "clients"])
+            bounce_target=["clients"])
     def test_transactions(self, failure_mode, bounce_target):
         security_protocol = 'PLAINTEXT'
         self.kafka.security_protocol = security_protocol
         self.kafka.interbroker_security_protocol = security_protocol
         self.kafka.start()
         input_messages = self.seed_messages()
-        self.copy_messages_transactionally(failure_mode, bounce_target)
+        concurrently_consumed_messages = self.copy_messages_transactionally(failure_mode, bounce_target)
         output_messages = self.get_messages_from_output_topic()
+
+        concurrently_consumed_message_set = set(concurrently_consumed_messages)
         output_message_set = set(output_messages)
         input_message_set = set(input_messages)
+
         num_dups = abs(len(output_messages) - len(output_message_set))
+        num_dups_in_concurrent_consumer = abs(len(concurrently_consumed_messages)
+                                              - len(concurrently_consumed_message_set))
         assert num_dups == 0, "Detected %d duplicates in the output stream" % num_dups
         assert input_message_set == output_message_set, "Input and output message sets are not equal. Num input messages %d. Num output messages %d" %\
             (len(input_message_set), len(output_message_set))
+
+        assert num_dups_in_concurrent_consumer == 0, "Detected %d dups in concurrently consumed messages" % num_dups_in_concurrent_consumer
+        assert input_message_set == concurrently_consumed_message_set, \
+            "Input and concurrently consumed output message sets are not equal. Num input messages: %d. Num concurrently_consumed_messages: %d" %\
+            (len(input_message_set), len(concurrently_consumed_message_set))
