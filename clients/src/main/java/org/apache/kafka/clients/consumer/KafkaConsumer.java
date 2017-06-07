2f5b652a3531b2f5c4e570d25359269a750c90b7
Rajini Sivaram <rajinisivaram@googlemail.com>
2017-06-07 12:51:53 +0100

    KAFKA-5380: Fix transient failure in KafkaConsumerTest, close consumers
    1. Fix ordering of metadata update request for regex subscription to avoid timing issue when heartbeat thread updates metadata
    2. Override metadata cluster in MockClient for `KafkaConsumer#testChangingRegexSubscription` to avoid timing issues during update
    3. Close consumer in all KafkaConsumer tests since they leave behind heartbeat threads.
    
    Author: Rajini Sivaram <rajinisivaram@googlemail.com>
    
    Reviewers: Jason Gustafson <jason@confluent.io>, Ismael Juma <ismael@juma.me.uk>
    
    Closes #3238 from rajinisivaram/KAFKA-5380
    
    (cherry picked from commit bb914a0445549b41fb0b667ea2f5f15a90060133)
    Signed-off-by: Rajini Sivaram <rajinisivaram@googlemail.com>

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -903,8 +903,8 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
             log.debug("Subscribed to pattern: {}", pattern);
             this.subscriptions.subscribe(pattern, listener);
             this.metadata.needMetadataForAllTopics(true);
-            this.metadata.requestUpdate();
             this.coordinator.updatePatternSubscription(metadata.fetch());
+            this.metadata.requestUpdate();
         } finally {
             release();
         }
