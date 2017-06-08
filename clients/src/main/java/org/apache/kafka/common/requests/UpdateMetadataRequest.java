eb2aa184a07abeec97fcda96d86d44c47ab35557
Ismael Juma <ismael@juma.me.uk>
2017-06-08 03:26:43 +0100

    KAFKA-5329; Fix order of replica list in metadata cache
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Colin P. Mccabe <cmccabe@confluent.io>, Jun Rao <junrao@gmail.com>
    
    Closes #3257 from ijuma/kafka-5329-fix-order-of-replica-list-in-metadata-cache
    
    (cherry picked from commit 12612e82971b8dc85941c771b7fe87ff95a07ab5)
    Signed-off-by: Ismael Juma <ismael@juma.me.uk>

--- clients/src/main/java/org/apache/kafka/common/requests/UpdateMetadataRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/UpdateMetadataRequest.java
@@ -174,7 +174,7 @@ public class UpdateMetadataRequest extends AbstractRequest {
             int zkVersion = partitionStateData.getInt(ZK_VERSION_KEY_NAME);
 
             Object[] replicasArray = partitionStateData.getArray(REPLICAS_KEY_NAME);
-            Set<Integer> replicas = new HashSet<>(replicasArray.length);
+            List<Integer> replicas = new ArrayList<>(replicasArray.length);
             for (Object r : replicasArray)
                 replicas.add((Integer) r);
 
