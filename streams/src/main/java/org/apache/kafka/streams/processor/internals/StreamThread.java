e60d547eff4e918b50998a42123ab3a942ba76e9
Matthias J. Sax <matthias@confluent.io>
2017-06-14 09:20:48 -0700

    HOTFIX: Introduce max wait time for retry-and-backoff while creating tasks
    Author: Matthias J. Sax <matthias@confluent.io>
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>
    
    Closes #3327 from mjsax/hotfix-backoff-retry
    
    (cherry picked from commit 65e36895f5cf91afcf785857ebff4c0fce8d1d9c)
    Signed-off-by: Guozhang Wang <wangguoz@gmail.com>

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
@@ -238,6 +238,7 @@ public class StreamThread extends Thread {
     }
 
     abstract class AbstractTaskCreator {
+        final static long MAX_BACKOFF_TIME_MS = 1000L;
         void retryWithBackoff(final Map<TaskId, Set<TopicPartition>> tasksToBeCreated, final long start) {
             long backoffTimeMs = 50L;
             final Set<TaskId> retryingTasks = new HashSet<>();
@@ -272,6 +273,7 @@ public class StreamThread extends Thread {
                 try {
                     Thread.sleep(backoffTimeMs);
                     backoffTimeMs <<= 1;
+                    backoffTimeMs = Math.min(backoffTimeMs, MAX_BACKOFF_TIME_MS);
                 } catch (final InterruptedException e) {
                     // ignore
                 }
