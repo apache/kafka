a4be2a15ab99326afc19504266bbbca42459a645
Jason Gustafson <jason@confluent.io>
2017-06-14 21:50:05 -0700

    Pull IncompleteBatches out of RecordAccumulator

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/RecordAccumulator.java
@@ -685,41 +685,4 @@ public final class RecordAccumulator {
         }
     }
 
-    /*
-     * A threadsafe helper class to hold batches that haven't been ack'd yet
-     */
-    private final static class IncompleteBatches {
-        private final Set<ProducerBatch> incomplete;
-
-        public IncompleteBatches() {
-            this.incomplete = new HashSet<>();
-        }
-
-        public void add(ProducerBatch batch) {
-            synchronized (incomplete) {
-                this.incomplete.add(batch);
-            }
-        }
-
-        public void remove(ProducerBatch batch) {
-            synchronized (incomplete) {
-                boolean removed = this.incomplete.remove(batch);
-                if (!removed)
-                    throw new IllegalStateException("Remove from the incomplete set failed. This should be impossible.");
-            }
-        }
-
-        public Iterable<ProducerBatch> all() {
-            synchronized (incomplete) {
-                return new ArrayList<>(this.incomplete);
-            }
-        }
-
-        public boolean isEmpty() {
-            synchronized (incomplete) {
-                return incomplete.isEmpty();
-            }
-        }
-    }
-
 }
