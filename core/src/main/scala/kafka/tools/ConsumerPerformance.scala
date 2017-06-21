edf1d0888728bc5c39b6ee59d23d3eed108243c6
huxihx <huxi_2b@hotmail.com>
2017-06-21 09:24:28 +0800

    returned back to the original output format for new consumer

--- core/src/main/scala/kafka/tools/ConsumerPerformance.scala
+++ core/src/main/scala/kafka/tools/ConsumerPerformance.scala
@@ -59,8 +59,9 @@ object ConsumerPerformance {
 
     if (!config.hideHeader) {
       if (!config.showDetailedStats) {
-        if (config.useOldConsumer)
-          println("start.time, end.time, data.consumed.in.MB, MB.sec, data.consumed.in.nMsg, nMsg.sec")
+        println("start.time, end.time, data.consumed.in.MB, MB.sec, data.consumed.in.nMsg, nMsg.sec" +
+          s"${if (!config.useOldConsumer) ", rebalance.time.ms, fetch.time.ms, fetch.MB.sec, fetch.nMsg.sec" else ""}"
+        )
       } else
         println("time, data.consumed.in.MB, MB.sec, data.consumed.in.nMsg, nMsg.sec")
     }
@@ -105,22 +106,21 @@ object ConsumerPerformance {
     fetchTimeInMs.set((endMs - startMs) - joinGroupTimeInMs.get)
     if (!config.showDetailedStats) {
       val totalMBRead = (totalBytesRead.get * 1.0) / (1024 * 1024)
-      if (config.useOldConsumer) {
-        println(s"%s, %s, %.4f, %.4f, %d, %.4f${if (!config.useOldConsumer) ", %s" else ""}".format(
-          config.dateFormat.format(startMs), config.dateFormat.format(endMs), totalMBRead,
-          totalMBRead / elapsedSecs, totalMessagesRead.get, totalMessagesRead.get / elapsedSecs))
-      } else {
-        println("---- performance test result ----")
-        println(s"Start time:${"\t" * 5}" + config.dateFormat.format(startMs))
-        println(s"End time:${"\t" * 5}" + config.dateFormat.format(startMs))
-        println(s"Total time:${"\t" * 5}%.4fs [%dms(rebalance time) + %dms(fetch time)]".format(
-          elapsedSecs, joinGroupTimeInMs.get, fetchTimeInMs.get))
-        println(s"Total MB read:${"\t" * 4}%.4f".format(totalMBRead))
-        println(s"Total throughput(MB/s):${"\t" * 2}%.4f".format(totalMBRead / elapsedSecs))
-        println(s"Fetch throughput(MB/s):${"\t" * 2}%.4f".format(totalMBRead / (fetchTimeInMs.get / 1000.0)))
-        println(s"Total messages read:${"\t" * 2}%d".format(totalMessagesRead.get))
-        println(s"Total message rate(msg/s):${"\t"}%.4f".format(totalMessagesRead.get / elapsedSecs))
-        println(s"Fetch message rate(msg/s):\t%.4f".format(totalMessagesRead.get / (fetchTimeInMs.get / 1000.0)))
+      print("%s, %s, %.4f, %.4f, %d, %.4f".format(
+        config.dateFormat.format(startMs),
+        config.dateFormat.format(endMs),
+        totalMBRead,
+        totalMBRead / elapsedSecs,
+        totalMessagesRead.get,
+        totalMessagesRead.get / elapsedSecs
+      ))
+      if (!config.useOldConsumer) {
+        print(", %d, %d, %.4f, %.4f".format(
+          joinGroupTimeInMs.get,
+          fetchTimeInMs.get,
+          totalMBRead / (fetchTimeInMs.get / 1000.0),
+          totalMessagesRead.get / (fetchTimeInMs.get / 1000.0)
+        ))
       }
     }
 
