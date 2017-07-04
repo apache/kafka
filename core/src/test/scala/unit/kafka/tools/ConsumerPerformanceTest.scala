6f15bf9c45abc579f6ef25b5882c548b57aa36da
Xianyang Liu <xianyang.liu@intel.com>
2017-07-04 10:40:38 +0800

    fix unit errors

--- core/src/test/scala/unit/kafka/tools/ConsumerPerformanceTest.scala
+++ core/src/test/scala/unit/kafka/tools/ConsumerPerformanceTest.scala
@@ -17,12 +17,12 @@
 
 package unit.kafka.tools
 
-import java.io.{ByteArrayOutputStream, PrintStream}
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 
 import kafka.tools.ConsumerPerformance
 import org.junit.Assert.assertEquals
-import org.junit.{After, Before, Test}
+import org.junit.Test
 
 
 class ConsumerPerformanceTest {
@@ -33,10 +33,10 @@ class ConsumerPerformanceTest {
   def testPrintProgressMessage(): Unit = {
     Console.withOut(outContent) {
       ConsumerPerformance.printProgressMessage(1, 1024 * 1024, 0, 1, 0, 0, 1,
-        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS")
+        new SimpleDateFormat("yyyy")
       )
     }
-    val expected = "1970-01-01 08:00:00:001, 1, 1.0000, 1000.0000, 1, 1000.0000"
+    val expected = "1970, 1, 1.0000, 1000.0000, 1, 1000.0000"
     assertEquals(expected, outContent.toString.trim)
   }
 
@@ -56,6 +56,4 @@ class ConsumerPerformanceTest {
 
     assertEquals(header.split(",").length, body.split(",").length)
   }
-
-
 }
