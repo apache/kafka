4ba20618a13dfd6c16b8228b319a21ae1915e857
Gregor Uhlenheuer <kongo2002@googlemail.com>
2017-07-12 12:07:49 +0200

    KAFKA-5584: fix integer overflow in Log.size

--- core/src/main/scala/kafka/log/Log.scala
+++ core/src/main/scala/kafka/log/Log.scala
@@ -1144,7 +1144,7 @@ class Log(@volatile var dir: File,
   /**
    * The size of the log in bytes
    */
-  def size: Long = logSegments.map(_.size).sum
+  def size: Long = logSegments.map(_.size).foldLeft(0L)(_ + _)
 
   /**
    * The offset metadata of the next message that will be appended to the log
