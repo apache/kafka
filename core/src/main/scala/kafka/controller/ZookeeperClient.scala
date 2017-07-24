1d4bfb253888f81d6c08fd2a9f761fd0cd704b61
Onur Karaman <okaraman@linkedin.com>
2017-07-24 14:13:13 -0700

    add logIdent for ZookeeperClient

--- core/src/main/scala/kafka/controller/ZookeeperClient.scala
+++ core/src/main/scala/kafka/controller/ZookeeperClient.scala
@@ -37,6 +37,7 @@ import org.apache.zookeeper.{CreateMode, WatchedEvent, Watcher, ZooKeeper}
   * @param stateChangeHandler state change handler callbacks called by the underlying zookeeper client's EventThread.
   */
 class ZookeeperClient(connectString: String, sessionTimeoutMs: Int, connectionTimeoutMs: Int, stateChangeHandler: StateChangeHandler) extends Logging {
+  this.logIdent = "[ZookeeperClient]: "
   private val initializationLock = new ReentrantReadWriteLock()
   private val isConnectedOrExpiredLock = new ReentrantLock()
   private val isConnectedOrExpiredCondition = isConnectedOrExpiredLock.newCondition()
