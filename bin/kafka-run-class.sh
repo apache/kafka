69d3d4aad6ce0678f592140501485893b68b20b1
Ismael Juma <ismael@juma.me.uk>
2017-08-09 23:38:57 +0100

    KAFKA-5470; Replace -XX:+DisableExplicitGC with -XX:+ExplicitGCInvokesConcurrent in kafka-run-class
    This is important because Bits.reserveMemory calls System.gc() hoping to free native
    memory in order to avoid throwing an OutOfMemoryException. This call is currently
    a no-op due to -XX:+DisableExplicitGC.
    
    It's worth mentioning that -XX:MaxDirectMemorySize can be used to increase the
    amount of native memory available for allocation of direct byte buffers.
    
    Author: Ismael Juma <ismael@juma.me.uk>
    
    Reviewers: Apurva Mehta <apurva@confluent.io>, Manikumar Reddy <manikumar.reddy@gmail.com>, Jason Gustafson <jason@confluent.io>
    
    Closes #3371 from ijuma/kafka-5470-explicit-gc-invokes-concurrent

--- bin/kafka-run-class.sh
+++ bin/kafka-run-class.sh
@@ -212,7 +212,7 @@ fi
 
 # JVM performance options
 if [ -z "$KAFKA_JVM_PERFORMANCE_OPTS" ]; then
-  KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
+  KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent -Djava.awt.headless=true"
 fi
 
 
