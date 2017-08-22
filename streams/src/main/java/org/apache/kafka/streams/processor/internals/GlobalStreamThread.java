a5703325d9b711eedf814d0c84c45d6fc6c82781
Guozhang Wang <wangguoz@gmail.com>
2017-08-22 13:04:01 -0700

    minor fix

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/GlobalStreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/GlobalStreamThread.java
@@ -146,7 +146,7 @@ public class GlobalStreamThread extends Thread {
                 // when the state is already in NOT_RUNNING, all its transitions
                 // will be refused but we do not throw exception here
                 return false;
-            }else if (!state.isValidTransition(newState)) {
+            } else if (!state.isValidTransition(newState)) {
                 throw new StreamsException(logPrefix + " Unexpected state transition from " + oldState + " to " + newState);
             } else {
                 log.info("{} State transition from {} to {}", logPrefix, oldState, newState);
