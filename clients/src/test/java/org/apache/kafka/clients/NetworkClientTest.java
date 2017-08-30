dabf6648ccde5e8d6f4e97b08e6a33a6b77a3589
Andrey Dyachkov <andrey.dyachkov@gmail.com>
2017-08-30 23:04:12 +0200

    kafka-5763: reverted imports change

--- clients/src/test/java/org/apache/kafka/clients/NetworkClientTest.java
+++ clients/src/test/java/org/apache/kafka/clients/NetworkClientTest.java
@@ -41,7 +41,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class NetworkClientTest {
 
