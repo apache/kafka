63ca7932727b704eb2f9f52fdc1241c8c7238aa2
Rajini Sivaram <rajinisivaram@googlemail.com>
2017-08-31 15:07:15 -0400

    MINOR: Fix unit test failures from last commit and checkstyle fixes

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
@@ -966,7 +966,7 @@ public abstract class AbstractCoordinator implements Closeable {
                 log.error("Unexpected interrupt received in heartbeat thread for group {}", groupId, e);
                 this.failed.set(new RuntimeException(e));
             } catch (RuntimeException e) {
-                log.error("Heartbeat thread for group {} failed due to unexpected error" , groupId, e);
+                log.error("Heartbeat thread for group {} failed due to unexpected error", groupId, e);
                 this.failed.set(e);
             } finally {
                 log.debug("Heartbeat thread for group {} has closed", groupId);
