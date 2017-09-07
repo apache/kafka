c24d98180fea045fecb213abb582710b915c025e
Damian Guy <damian.guy@gmail.com>
2017-09-07 11:05:50 +0100

    address comments

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KTableImpl.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KTableImpl.java
@@ -622,7 +622,7 @@ public class KTableImpl<K, S, V> extends AbstractStream<K> implements KTable<K, 
     public <K1, V1> KGroupedTable<K1, V1> groupBy(final KeyValueMapper<? super K, ? super V, KeyValue<K1, V1>> selector,
                                                   final Serialized<K1, V1> serialized) {
         Objects.requireNonNull(selector, "selector can't be null");
-        Objects.requireNonNull(selector, "serialized can't be null");
+        Objects.requireNonNull(serialized, "serialized can't be null");
         String selectName = builder.newName(SELECT_NAME);
 
         KTableProcessorSupplier<K, V, KeyValue<K1, V1>> selectSupplier = new KTableRepartitionMap<>(this, selector);
