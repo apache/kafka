9bc3ef6b8d8b72331efe8c3a8fe9ff670e5698ca
Apurva Mehta <apurva@confluent.io>
2017-09-13 14:35:36 -0700

    Fix checkstyle failures

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/SenderTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/SenderTest.java
@@ -80,7 +80,6 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
