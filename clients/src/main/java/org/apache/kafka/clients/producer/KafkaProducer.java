a0ad9f156a122e3e3a073cba0e2b0c39c93c7db5
Vahid Hashemian <vahidhashemian@us.ibm.com>
2017-09-14 11:18:07 +0100

    MINOR: Fix LogContext message format in KafkaProducer
    Author: Vahid Hashemian <vahidhashemian@us.ibm.com>
    
    Reviewers: Manikumar Reddy <manikumar.reddy@gmail.com>, Ismael Juma <ismael@juma.me.uk>
    
    Closes #3854 from vahidhashemian/minor/fix_log_context_message_format

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -316,7 +316,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
 
             String transactionalId = userProvidedConfigs.containsKey(ProducerConfig.TRANSACTIONAL_ID_CONFIG) ?
                     (String) userProvidedConfigs.get(ProducerConfig.TRANSACTIONAL_ID_CONFIG) : null;
-            LogContext logContext = new LogContext(String.format("[Producer clientId=%s, transactionalId=%s", clientId, transactionalId));
+            LogContext logContext = new LogContext(String.format("[Producer clientId=%s, transactionalId=%s] ", clientId, transactionalId));
             log = logContext.logger(KafkaProducer.class);
             log.trace("Starting the Kafka producer");
 
