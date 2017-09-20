44dede0df1c4e9b50f76525b730631e30c4ce44d
Apurva Mehta <apurva@confluent.io>
2017-09-20 14:39:05 -0700

    Add Javadoc for the UnknownProducerException

--- clients/src/main/java/org/apache/kafka/common/errors/UnknownProducerException.java
+++ clients/src/main/java/org/apache/kafka/common/errors/UnknownProducerException.java
@@ -17,6 +17,12 @@
 
 package org.apache.kafka.common.errors;
 
+/**
+ * This exception is raised by the broker if it could not locate the producer metadata associated with the producerId
+ * in question. This could happen if, for instance, the producer's records were deleted because their retention time
+ * had elapsed. Once the last records of the producerId are removed, the producer's metadata is removed from the broker,
+ * and future appends by the producer will return this exception.
+ */
 public class UnknownProducerException extends ApiException {
 
     public UnknownProducerException(String message) {
