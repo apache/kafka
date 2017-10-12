64fcbfc1e3cbf5949431f8c7ca4011f19fa3ac3d
Tom Bentley <tbentley@redhat.com>
2017-10-12 12:30:58 +0100

    Add errorCounts() method

--- clients/src/main/java/org/apache/kafka/common/requests/ElectPreferredLeadersResponse.java
+++ clients/src/main/java/org/apache/kafka/common/requests/ElectPreferredLeadersResponse.java
@@ -19,6 +19,7 @@ package org.apache.kafka.common.requests;
 
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.Errors;
 import org.apache.kafka.common.protocol.types.ArrayOf;
 import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.protocol.types.Schema;
@@ -88,6 +89,11 @@ public class ElectPreferredLeadersResponse extends AbstractResponse {
 
     public int throttleTimeMs() {
         return throttleTimeMs;
+    }
+
+    @Override
+    public Map<Errors, Integer> errorCounts() {
+        return apiErrorCounts(errors);
     }
 
     @Override
