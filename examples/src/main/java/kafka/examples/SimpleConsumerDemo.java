114ac44e8068a7cccc5cf3bd942f565c3660b918
yezhu <yezhuozhu@gmail.com>
2017-10-18 14:46:42 -0700

    Test branch

--- examples/src/main/java/kafka/examples/SimpleConsumerDemo.java
+++ examples/src/main/java/kafka/examples/SimpleConsumerDemo.java
@@ -14,6 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+* Modify for test
+*/
 package kafka.examples;
 
 import kafka.api.FetchRequest;
