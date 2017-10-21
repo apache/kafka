a0bdb7e673b71e5cc332d390f7fd4d015542c34d
Per Steffensen <steff@designware.dk>
2017-10-21 16:40:12 +0200

    Ups. Making check-style happy.

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerSourceTask.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerSourceTask.java
@@ -34,7 +34,10 @@ import org.apache.kafka.connect.util.ConnectorTaskId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
