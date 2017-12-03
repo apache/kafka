9204197abf2e13d345ad6ee6a7f827268a49ebca
Bill Bejeck <bill@confluent.io>
2017-12-03 09:05:18 -0800

    MINOR: Fix broker compatibility tests
    Updated the System test `stream_broker_compatibility_test.py` to address system test failures as we have removed explicit broker version checking
    
    - Ignore the `0.8.2.2` and `0.9.0.0` tests because the `NetworkClient` only logs `UnsupportedVersionException`s that occur and will continue to retry connecting.  Once issue https://issues.apache.org/jira/browse/KAFKA-6297 is addressed, we may re-enable these tests.
    - Updated existing tests expected error messages
    - Updated Streams code in test for to make sure we fail fast for incompatible brokers
    
    Author: Bill Bejeck <bill@confluent.io>
    
    Reviewers: Matthias J. Sax <matthias@confluent.io>, Guozhang Wang <wangguoz@gmail.com>
    
    Closes #4286 from bbejeck/MINOR_fix_broker_compatibility_tests

--- tests/kafkatest/tests/streams/streams_broker_compatibility_test.py
+++ tests/kafkatest/tests/streams/streams_broker_compatibility_test.py
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from ducktape.mark import ignore
 from ducktape.mark import parametrize
 from ducktape.tests.test import Test
 from ducktape.utils.util import wait_until
-
 from kafkatest.services.kafka import KafkaService
 from kafkatest.services.streams import StreamsBrokerCompatibilityService
 from kafkatest.services.verifiable_consumer import VerifiableConsumer
@@ -67,9 +67,9 @@ class StreamsBrokerCompatibility(Test):
 
         processor.node.account.ssh(processor.start_cmd(processor.node))
         with processor.node.account.monitor_log(processor.STDERR_FILE) as monitor:
-            monitor.wait_until('Exception in thread "main" org.apache.kafka.streams.errors.StreamsException: Setting processing.guarantee=exactly_once requires broker version 0.11.0.x or higher.',
+            monitor.wait_until('FATAL: An unexpected exception org.apache.kafka.common.errors.UnsupportedVersionException: The broker does not support LIST_OFFSETS ',
                                timeout_sec=60,
-                               err_msg="Never saw 'EOS requires broker version 0.11+' error message " + str(processor.node.account))
+                               err_msg="Never saw 'FATAL: An unexpected exception org.apache.kafka.common.errors.UnsupportedVersionException: The broker does not support LIST_OFFSETS ' error message " + str(processor.node.account))
 
         self.kafka.stop()
 
@@ -102,12 +102,13 @@ class StreamsBrokerCompatibility(Test):
 
         processor.node.account.ssh(processor.start_cmd(processor.node))
         with processor.node.account.monitor_log(processor.STDERR_FILE) as monitor:
-            monitor.wait_until('Exception in thread "main" org.apache.kafka.streams.errors.StreamsException: Kafka Streams requires broker version 0.10.1.x or higher.',
+            monitor.wait_until('FATAL: An unexpected exception org.apache.kafka.streams.errors.StreamsException: Could not create internal topics.',
                         timeout_sec=60,
-                        err_msg="Never saw 'Streams requires broker verion 0.10.1+' error message " + str(processor.node.account))
+                        err_msg="Never saw 'FATAL: An unexpected exception org.apache.kafka.streams.errors.StreamsException: Could not create internal topics.' error message " + str(processor.node.account))
 
         self.kafka.stop()
 
+    @ignore
     @parametrize(broker_version=str(LATEST_0_9))
     @parametrize(broker_version=str(LATEST_0_8_2))
     def test_timeout_on_pre_010_brokers(self, broker_version):
