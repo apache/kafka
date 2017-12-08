4e5a3a5540ae46ccaa27f448800a7c46eedf85f0
umesh chaudhary <umesh9794@gmail.com>
2017-12-08 10:12:30 +0530

    Added tests for new Json encode methods

--- core/src/test/scala/unit/kafka/utils/JsonTest.scala
+++ core/src/test/scala/unit/kafka/utils/JsonTest.scala
@@ -16,11 +16,15 @@
  */
 package kafka.utils
 
+import java.nio.charset.StandardCharsets
+import java.util
+
 import org.junit.Assert._
 import org.junit.Test
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node._
 import kafka.utils.json.JsonValue
+
 import scala.collection.JavaConverters._
 
 class JsonTest {
@@ -45,8 +49,8 @@ class JsonTest {
   }
 
   @Test
-  def testJsonEncoding() {
-    assertEquals("null", Json.encodeAsString(null))
+  def testLegacyEncodeAsString() {
+    assertEquals("null", Json.legacyEncodeAsString(null))
     assertEquals("1", Json.legacyEncodeAsString(1))
     assertEquals("1", Json.legacyEncodeAsString(1L))
     assertEquals("1", Json.legacyEncodeAsString(1.toByte))
@@ -61,6 +65,48 @@ class JsonTest {
     assertEquals("{}", Json.legacyEncodeAsString(Map()))
     assertEquals("{\"a\":1,\"b\":2}", Json.legacyEncodeAsString(Map("a" -> 1, "b" -> 2)))
     assertEquals("{\"a\":[1,2],\"c\":[3,4]}", Json.legacyEncodeAsString(Map("a" -> Seq(1,2), "c" -> Seq(3,4))))
+  }
+
+  @Test
+  def testEncodeAsString() {
+    assertEquals("null", Json.encodeAsString(null))
+    assertEquals("1", Json.encodeAsString(1))
+    assertEquals("1", Json.encodeAsString(1L))
+    assertEquals("1", Json.encodeAsString(1.toByte))
+    assertEquals("1", Json.encodeAsString(1.toShort))
+    assertEquals("1.0", Json.encodeAsString(1.0))
+    assertEquals("\"str\"", Json.encodeAsString("str"))
+    assertEquals("true", Json.encodeAsString(true))
+    assertEquals("false", Json.encodeAsString(false))
+    assertEquals("[]", Json.encodeAsString(Seq().asJava))
+    assertEquals("[null]", Json.encodeAsString(Seq(null).asJava))
+    assertEquals("[1,2,3]", Json.encodeAsString(Seq(1,2,3).asJava))
+    assertEquals("[1,\"2\",[3],null]", Json.encodeAsString(Seq(1,"2",Seq(3).asJava,null).asJava))
+    assertEquals("{}", Json.encodeAsString(Map().asJava))
+    assertEquals("{\"a\":1,\"b\":2,\"c\":null}", Json.encodeAsString(Map("a" -> 1, "b" -> 2, "c" -> null).asJava))
+    assertEquals("{\"a\":[1,2],\"c\":[3,4]}", Json.encodeAsString(Map("a" -> Seq(1,2).asJava, "c" -> Seq(3,4).asJava).asJava))
+    assertEquals("{\"a\":[1,2],\"b\":[3,4],\"c\":null}", Json.encodeAsString(Map("a" -> Seq(1,2).asJava, "b" -> Seq(3,4).asJava, "c" -> null).asJava))
+  }
+
+  @Test
+  def testEncodeAsBytes() {
+    assertArrayEquals("null".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(null))
+    assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(1))
+    assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(1L))
+    assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(1.toByte))
+    assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(1.toShort))
+    assertArrayEquals("1.0".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(1.0))
+    assertArrayEquals("\"str\"".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes("str"))
+    assertArrayEquals("true".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(true))
+    assertArrayEquals("false".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(false))
+    assertArrayEquals("[]".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Seq().asJava))
+    assertArrayEquals("[null]".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Seq(null).asJava))
+    assertArrayEquals("[1,2,3]".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Seq(1,2,3).asJava))
+    assertArrayEquals("[1,\"2\",[3],null]".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Seq(1,"2",Seq(3).asJava,null).asJava))
+    assertArrayEquals("{}".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Map().asJava))
+    assertArrayEquals("{\"a\":1,\"b\":2,\"c\":null}".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Map("a" -> 1, "b" -> 2, "c" -> null).asJava))
+    assertArrayEquals("{\"a\":[1,2],\"c\":[3,4]}".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Map("a" -> Seq(1,2).asJava, "c" -> Seq(3,4).asJava).asJava))
+    assertArrayEquals("{\"a\":[1,2],\"b\":[3,4],\"c\":null}".getBytes(StandardCharsets.UTF_8), Json.encodeAsBytes(Map("a" -> Seq(1,2).asJava, "b" -> Seq(3,4).asJava, "c" -> null).asJava))
   }
   
 }
