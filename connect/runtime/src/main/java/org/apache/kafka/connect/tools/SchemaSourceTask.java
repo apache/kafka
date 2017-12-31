04803244a1b76dec0d8d317b5b0ad619f26a8f26
KoenDG <kdg.dev@gmail.com>
2017-12-31 15:26:41 +0100

    Removed unused imports.

--- connect/runtime/src/main/java/org/apache/kafka/connect/tools/SchemaSourceTask.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/tools/SchemaSourceTask.java
@@ -27,7 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
--- connect/runtime/src/main/java/org/apache/kafka/connect/tools/VerifiableSourceTask.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/tools/VerifiableSourceTask.java
@@ -26,7 +26,6 @@ import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
