c838a703f2a86694f69bc66c0a17ed971d17fff2
Guozhang Wang <wangguoz@gmail.com>
2018-01-16 16:39:46 -0800

    remove imports

--- streams/src/test/java/org/apache/kafka/streams/StreamsBuilderTest.java
+++ streams/src/test/java/org/apache/kafka/streams/StreamsBuilderTest.java
@@ -46,7 +46,6 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
