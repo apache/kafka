5ad55c1f48637b8b76b125caf6713c60d5dc7bd7
Matthias J. Sax <mjsax@apache.org>
2018-01-17 14:01:43 -0800

    KAFKA-6302: Improve AdmintClient JavaDocs
    Reviewers: Colin P. McCabe <cmccabe@confluent.io>, Ismael Juma <ismael@juma.me.uk>
    
    Closes #4332

--- clients/src/main/java/org/apache/kafka/clients/admin/AdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/AdminClient.java
@@ -104,7 +104,7 @@ public abstract class AdminClient implements AutoCloseable {
      *
      * This operation is not transactional so it may succeed for some topics while fail for others.
      *
-     * It may take several seconds after this method returns
+     * It may take several seconds after {@code CreateTopicsResult} returns
      * success for all the brokers to become aware that the topics have been created.
      * During this time, {@link AdminClient#listTopics()} and {@link AdminClient#describeTopics(Collection)}
      * may not return information about the new topics.
@@ -137,7 +137,7 @@ public abstract class AdminClient implements AutoCloseable {
      *
      * This operation is not transactional so it may succeed for some topics while fail for others.
      *
-     * It may take several seconds after AdminClient#deleteTopics returns
+     * It may take several seconds after the {@code DeleteTopicsResult} returns
      * success for all the brokers to become aware that the topics are gone.
      * During this time, AdminClient#listTopics and AdminClient#describeTopics
      * may continue to return information about the deleted topics.
