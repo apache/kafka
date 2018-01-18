70f25b95192bb7d56c62c8b8bc80b478b8e08ef9
Guozhang Wang <wangguoz@gmail.com>
2018-01-18 13:54:59 -0800

    MINOR: Improve on reset integration test (#4436)
    * Parameterize abstract reset integration test
    
    Reviewers: Damian Guy <damian.guy@gmail.com>, Matthias J. Sax <mjsax@apache.org>

--- streams/src/test/java/org/apache/kafka/streams/integration/ResetIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/ResetIntegrationTest.java
@@ -18,20 +18,15 @@ package org.apache.kafka.streams.integration;
 
 import kafka.server.KafkaConfig$;
 import org.apache.kafka.streams.integration.utils.EmbeddedKafkaCluster;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.test.IntegrationTest;
-import org.junit.AfterClass;
-import org.junit.Assert;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import kafka.tools.StreamsResetter;
 
 import java.util.Properties;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -42,31 +37,29 @@ public class ResetIntegrationTest extends AbstractResetIntegrationTest {
 
     @ClassRule
     public static final EmbeddedKafkaCluster CLUSTER;
-    private static final long CLEANUP_CONSUMER_TIMEOUT = 2000L;
-    private static final String APP_ID = "Integration-test";
-    private static final String NON_EXISTING_TOPIC = "nonExistingTopic";
-    private static int testNo = 1;
+
+    private static final String TEST_ID = "reset-integration-test";
 
     static {
-        final Properties props = new Properties();
+        final Properties brokerProps = new Properties();
         // we double the value passed to `time.sleep` in each iteration in one of the map functions, so we disable
         // expiration of connections by the brokers to avoid errors when `AdminClient` sends requests after potentially
         // very long sleep times
-        props.put(KafkaConfig$.MODULE$.ConnectionsMaxIdleMsProp(), -1L);
-        CLUSTER = new EmbeddedKafkaCluster(NUM_BROKERS, props);
-        cluster = CLUSTER;
-    }
-
-    @AfterClass
-    public static void globalCleanup() {
-        afterClassGlobalCleanup();
+        brokerProps.put(KafkaConfig$.MODULE$.ConnectionsMaxIdleMsProp(), -1L);
+        CLUSTER = new EmbeddedKafkaCluster(1, brokerProps);
     }
 
     @Before
     public void before() throws Exception {
-        beforePrepareTest();
+        testId = TEST_ID;
+        cluster = CLUSTER;
+        prepareTest();
     }
 
+    @After
+    public void after() throws Exception {
+        cleanupTest();
+    }
 
     @Test
     public void testReprocessingFromScratchAfterResetWithoutIntermediateUserTopic() throws Exception {
@@ -100,36 +93,11 @@ public class ResetIntegrationTest extends AbstractResetIntegrationTest {
 
     @Test
     public void shouldNotAllowToResetWhenInputTopicAbsent() throws Exception {
-
-        final List<String> parameterList = new ArrayList<>(
-                Arrays.asList("--application-id", APP_ID + testNo,
-                        "--bootstrap-servers", bootstrapServers,
-                        "--input-topics", NON_EXISTING_TOPIC));
-
-        final String[] parameters = parameterList.toArray(new String[parameterList.size()]);
-        final Properties cleanUpConfig = new Properties();
-        cleanUpConfig.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 100);
-        cleanUpConfig.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "" + CLEANUP_CONSUMER_TIMEOUT);
-
-        final int exitCode = new StreamsResetter().run(parameters, cleanUpConfig);
-        Assert.assertEquals(1, exitCode);
+        super.shouldNotAllowToResetWhenInputTopicAbsent();
     }
 
     @Test
     public void shouldNotAllowToResetWhenIntermediateTopicAbsent() throws Exception {
-
-        final List<String> parameterList = new ArrayList<>(
-                Arrays.asList("--application-id", APP_ID + testNo,
-                        "--bootstrap-servers", bootstrapServers,
-                        "--intermediate-topics", NON_EXISTING_TOPIC));
-
-        final String[] parameters = parameterList.toArray(new String[parameterList.size()]);
-        final Properties cleanUpConfig = new Properties();
-        cleanUpConfig.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 100);
-        cleanUpConfig.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "" + CLEANUP_CONSUMER_TIMEOUT);
-
-        final int exitCode = new StreamsResetter().run(parameters, cleanUpConfig);
-        Assert.assertEquals(1, exitCode);
+        super.shouldNotAllowToResetWhenIntermediateTopicAbsent();
     }
-
 }
