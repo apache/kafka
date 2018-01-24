281f50807ab8e31f20e9c6c85b8775064c9b11e9
umesh chaudhary <umesh9794@gmail.com>
2018-01-24 10:18:17 +0530

    Changed the AclJson to have the incorrect format for principals

--- core/src/test/scala/unit/kafka/security/auth/AclTest.scala
+++ core/src/test/scala/unit/kafka/security/auth/AclTest.scala
@@ -30,8 +30,8 @@ class AclTest extends JUnitSuite {
   val AclJson = "{\"version\": 1, \"acls\": [{\"host\": \"host1\",\"permissionType\": \"Deny\",\"operation\": \"READ\", \"principal\": \"User:alice\"  },  " +
     "{  \"host\":  \"*\" ,  \"permissionType\": \"Allow\",  \"operation\":  \"Read\", \"principal\": \"User:bob\"  },  " +
     "{  \"host\": \"host1\",  \"permissionType\": \"Deny\",  \"operation\":   \"Read\" ,  \"principal\": \"User:bob\"},  " +
-    "{  \"host\": \"host1\",  \"permissionType\": \"Deny\",  \"operation\":   \"Read\" ,  \"principal\": \"User:\\\\bob\"}, " +
-    "{  \"host\": \"host1\",  \"permissionType\": \"Deny\",  \"operation\":   \"Read\" ,  \"principal\": \"User:bob1\\\\bob2\"}]}"
+    "{  \"host\": \"host1\",  \"permissionType\": \"Deny\",  \"operation\":   \"Read\" ,  \"principal\": \"User:\\bob\"}, " +
+    "{  \"host\": \"host1\",  \"permissionType\": \"Deny\",  \"operation\":   \"Read\" ,  \"principal\": \"User:bob1\\bob2\"}]}"
 
   @Test
   def testAclJsonConversion(): Unit = {
