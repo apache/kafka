3ce567bac4e66a9851ea82bc6ab11e0913b86a84
RichardYuSTUG <yohan.richard.yu2@gmail.com>
2018-01-27 20:06:24 -0800

    Resolving ambiguous error

--- core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
+++ core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
@@ -368,7 +368,9 @@ class DynamicBrokerReconfigurationTest extends ZooKeeperTestHarness with SaslSet
     )
     invalidProps.foreach { case (k, v) =>
       val newProps = new Properties
-      newProps.putAll(props)
+      props.asScala.foreach { case (k,v) => 
+        newProps.put(k, v)
+      }
       props.put(k, v)
       reconfigureServers(props, perBrokerConfig = false, (k, props.getProperty(k)), expectFailure = true)
     }
