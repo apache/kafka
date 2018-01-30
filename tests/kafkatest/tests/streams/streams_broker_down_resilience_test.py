6bcb0324e6440a59c919e7f61a873357b1b097d5
Bill Bejeck <bill@confluent.io>
2018-01-30 16:58:05 -0500

    MINOR: adding system tests for how streams functions with broker failures
    trying to grab bootstrap servers without verifying port open for test
    
    override start_cmd to not verify port open
    
    revert method
    
    fix pep warning about blank lines
    
    adding timeouts and some debugging
    
    starting service, then stopping node for leader
    
    fix string concatenation
    
    change application id, go back to just starting kafka later
    
    increase timeout
    
    try with first test ignored
    
    start the service, but try stopping the leader node first
    
    added new test, expand tests for multi-node stream applications
    
    added debugging, change 3rd test to correct format
    
    removed logging, improved wait for grepping log file, added confirmation each node read messages
    
    add regex into wait_until for logging
    
    clean up formatting
    
    fixed wrong method call, increased timeout for parsing log files
    
    verifying via received messages

--- tests/kafkatest/tests/streams/streams_broker_down_resilience_test.py
+++ tests/kafkatest/tests/streams/streams_broker_down_resilience_test.py
@@ -40,40 +40,53 @@ class StreamsBrokerDownResilience(Test):
                                   num_nodes=1,
                                   zk=self.zk,
                                   topics={
-                                      self.inputTopic: {'partitions': 1, 'replication-factor': 1},
+                                      self.inputTopic: {'partitions': 3, 'replication-factor': 1},
                                       self.outputTopic: {'partitions': 1, 'replication-factor': 1}
                                   })
 
-    def get_consumer(self):
+    def get_consumer(self, num_messages):
         return VerifiableConsumer(self.test_context,
                                   1,
                                   self.kafka,
                                   self.outputTopic,
                                   "stream-broker-resilience-verify-consumer",
-                                  max_messages=self.num_messages)
+                                  max_messages=num_messages)
 
-    def get_producer(self):
+    def get_producer(self, num_messages):
         return VerifiableProducer(self.test_context,
                                   1,
                                   self.kafka,
                                   self.inputTopic,
-                                  max_messages=self.num_messages,
+                                  max_messages=num_messages,
                                   acks=1)
 
-    def assert_produce_consume(self, test_state):
-        producer = self.get_producer()
+    def assert_produce_consume(self, test_state, num_messages=5):
+        producer = self.get_producer(num_messages)
         producer.start()
 
-        wait_until(lambda: producer.num_acked > 0,
+        wait_until(lambda: producer.num_acked >= num_messages,
                    timeout_sec=30,
                    err_msg="At %s failed to send messages " % test_state)
 
-        consumer = self.get_consumer()
+        consumer = self.get_consumer(num_messages)
         consumer.start()
 
-        wait_until(lambda: consumer.total_consumed() > 0,
+        wait_until(lambda: consumer.total_consumed() >= num_messages,
                    timeout_sec=60,
                    err_msg="At %s streams did not process messages in 60 seconds " % test_state)
+
+    @staticmethod
+    def get_configs(extra_configs=""):
+        # Consumer max.poll.interval > min(max.block.ms, ((retries + 1) * request.timeout)
+        consumer_poll_ms = "consumer.max.poll.interval.ms=50000"
+        retries_config = "producer.retries=2"
+        request_timeout = "producer.request.timeout.ms=15000"
+        max_block_ms = "producer.max.block.ms=30000"
+
+        # java code expects configs in key=value,key=value format
+        updated_configs = consumer_poll_ms + "," + retries_config + "," + request_timeout + "," + max_block_ms + extra_configs
+
+        return updated_configs
 
     def setUp(self):
         self.zk.start()
@@ -81,20 +94,11 @@ class StreamsBrokerDownResilience(Test):
     def test_streams_resilient_to_broker_down(self):
         self.kafka.start()
 
-        # Consumer max.poll.interval > min(max.block.ms, ((retries + 1) * request.timeout)
-        consumer_poll_ms = "consumer.max.poll.interval.ms=50000"
-        retries_config = "producer.retries=2"
-        request_timeout = "producer.request.timeout.ms=15000"
-        max_block_ms = "producer.max.block.ms=30000"
-
         # Broker should be down over 2x of retries * timeout ms
         # So with (2 * 15000) = 30 seconds, we'll set downtime to 70 seconds
         broker_down_time_in_seconds = 70
 
-        # java code expects configs in key=value,key=value format
-        updated_configs = consumer_poll_ms + "," + retries_config + "," + request_timeout + "," + max_block_ms
-
-        processor = StreamsBrokerDownResilienceService(self.test_context, self.kafka, updated_configs)
+        processor = StreamsBrokerDownResilienceService(self.test_context, self.kafka, self.get_configs())
         processor.start()
 
         # until KIP-91 is merged we'll only send 5 messages to assert Kafka Streams is running before taking the broker down
@@ -110,5 +114,70 @@ class StreamsBrokerDownResilience(Test):
         self.kafka.start_node(node)
 
         self.assert_produce_consume("after_broker_stop")
+
+        self.kafka.stop()
+
+    def test_streams_runs_with_broker_down_initially(self):
+        self.kafka.start()
+        node = self.kafka.leader(self.inputTopic)
+        self.kafka.stop_node(node)
+
+        broker_down_initially_in_seconds = 60
+
+        configs = self.get_configs(extra_configs=",application.id=starting_wo_broker_id")
+
+        # start streams with broker down initially
+        processor = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor.start()
+
+        processor_2 = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor_2.start()
+
+        processor_3 = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor_3.start()
+
+        time.sleep(broker_down_initially_in_seconds)
+
+        # now start broker
+        self.kafka.start_node(node)
+        # give broker time to start up
+        time.sleep(20)
+
+        # assert streams can process when starting with broker down
+        self.assert_produce_consume("running_with_broker_down_initially", num_messages=9)
+
+        self.kafka.stop()
+
+    def test_streams_shuts_down_clean_with_broker_down(self):
+        self.kafka.start()
+
+        configs = self.get_configs(extra_configs=",application.id=shutdown_with_broker_down")
+
+        # start streams with broker down initially
+        processor = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor.start()
+
+        processor_2 = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor_2.start()
+
+        processor_3 = StreamsBrokerDownResilienceService(self.test_context, self.kafka, configs)
+        processor_3.start()
+
+        # give time for rebalance to finish
+        time.sleep(30)
+
+        node = self.kafka.leader(self.inputTopic)
+        self.kafka.stop_node(node)
+
+        processor.stop()
+        processor_2.stop()
+
+        time.sleep(20)
+
+        self.kafka.start_node(node)
+
+        time.sleep(20)
+
+        self.assert_produce_consume("sending_message_after_stopping_streams_instance_bouncing_broker", num_messages=9)
 
         self.kafka.stop()
