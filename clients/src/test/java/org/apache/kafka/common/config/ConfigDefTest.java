1eb4c13a7a66eb860f6a9f3814fc4672d266cd70
Ewen Cheslack-Postava <me@ewencp.org>
2018-01-30 20:38:40 -0800

    Don't rely on UTF-8 encoding of source code files

--- clients/src/test/java/org/apache/kafka/common/config/ConfigDefTest.java
+++ clients/src/test/java/org/apache/kafka/common/config/ConfigDefTest.java
@@ -161,7 +161,7 @@ public class ConfigDefTest {
         testValidators(Type.STRING, new ConfigDef.NonNullValidator(), "a", new Object[]{"abb"}, new Object[] {null});
         testValidators(Type.STRING, ConfigDef.CompositeValidator.of(new ConfigDef.NonNullValidator(), ValidString.in("a", "b")), "a", new Object[]{"a", "b"}, new Object[] {null, -1, "c"});
         testValidators(Type.STRING, new ConfigDef.NonEmptyStringWithoutControlChars(), "defaultname",
-                new Object[]{"test", "name", "test/test", "testሴ", "\u1324name\\", "/+%>&):??<&()?-", "+1", "\uD83D\uDE01", "🎱", "     test   \n\r", "\n  hello \t"},
+                new Object[]{"test", "name", "test/test", "test\u1234", "\u1324name\\", "/+%>&):??<&()?-", "+1", "\uD83D\uDE01", "\uF3B1", "     test   \n\r", "\n  hello \t"},
                 new Object[]{"nontrailing\nnotallowed", "as\u0001cii control char", "tes\rt", "test\btest", "1\t2", ""});
     }
 
