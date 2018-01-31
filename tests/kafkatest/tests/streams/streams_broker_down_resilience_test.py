d6173fbff2b8e5760c45a1707a95282f851698ae
Bill Bejeck <bill@confluent.io>
2018-01-30 19:06:08 -0500

    override start_cmd to not verify port open

--- tests/kafkatest/tests/streams/streams_broker_down_resilience_test.py
+++ tests/kafkatest/tests/streams/streams_broker_down_resilience_test.py
@@ -123,7 +123,7 @@ class StreamsBrokerDownResilience(Test):
 
         # start streams with broker down initially
         processor = StreamsBrokerDownResilienceService(self.test_context, self.kafka, self.get_configs())
-        processor.start(validate=False)
+        processor.start()
 
         time.sleep(broker_down_initially_in_seconds)
 
