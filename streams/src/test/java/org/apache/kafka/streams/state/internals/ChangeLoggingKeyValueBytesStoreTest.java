a0da29ca3e689e62863132c9e98dd6fc9c5dcb9d
Bart De Vylder <bartdevylder@gmail.com>
2018-01-31 14:00:06 +0100

    KAFKA-6487: ChangeLoggingKeyValueBytesStore does not propagate delete

--- streams/src/test/java/org/apache/kafka/streams/state/internals/ChangeLoggingKeyValueBytesStoreTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/ChangeLoggingKeyValueBytesStoreTest.java
@@ -109,9 +109,10 @@ public class ChangeLoggingKeyValueBytesStoreTest {
     }
 
     @Test
-    public void shouldPutNullOnDelete() {
+    public void shouldPropagateDelete() {
         store.put(hi, there);
         store.delete(hi);
+        assertThat(inner.approximateNumEntries(), equalTo(0L));
         assertThat(inner.get(hi), nullValue());
     }
 
@@ -125,6 +126,7 @@ public class ChangeLoggingKeyValueBytesStoreTest {
     public void shouldLogKeyNullOnDelete() {
         store.put(hi, there);
         store.delete(hi);
+        assertThat(sent.containsKey(hi), is(true));
         assertThat(sent.get(hi), nullValue());
     }
 
@@ -175,4 +177,5 @@ public class ChangeLoggingKeyValueBytesStoreTest {
     public void shouldReturnNullOnGetWhenDoesntExist() {
         assertThat(store.get(hello), is(nullValue()));
     }
+
 }
