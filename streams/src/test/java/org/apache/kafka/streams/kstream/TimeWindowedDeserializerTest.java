3ee8ad1fa8dce44533a6218ac946189c86b30aa1
Guozhang Wang <wangguoz@gmail.com>
2018-03-01 14:38:06 -0800

    remove more imports

--- streams/src/test/java/org/apache/kafka/streams/kstream/TimeWindowedDeserializerTest.java
+++ streams/src/test/java/org/apache/kafka/streams/kstream/TimeWindowedDeserializerTest.java
@@ -27,7 +27,6 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
