14dc2fc5e4a7c8d538f289e55075e0f9d7d14d99
Matthias J. Sax <matthias@confluent.io>
2018-03-17 23:02:48 -0700

    KAFKA-6054: Fix upgrade path from Kafka Streams v0.10.0
    Update upgrade docs
    Github comments
    John's review
    Guozhang's follow up

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -61,6 +61,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
+DEV_VERSION = KafkaVersion("1.1.0-SNAPSHOT")
 
 # 0.8.2.X versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
@@ -91,5 +92,7 @@ LATEST_0_10 = LATEST_0_10_2
 
 # 0.11.0.0 versions
 V_0_11_0_0 = KafkaVersion("0.11.0.0")
-LATEST_0_11_0 = V_0_11_0_0
+V_0_11_0_1 = KafkaVersion("0.11.0.1")
+V_0_11_0_2 = KafkaVersion("0.11.0.2")
+LATEST_0_11_0 = V_0_11_0_2
 LATEST_0_11 = LATEST_0_11_0
