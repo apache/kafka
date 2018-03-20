b71e1535eb39e69cc7aee931ec3bceb72cdadae2
Rajini Sivaram <rajinisivaram@googlemail.com>
2018-03-20 22:17:01 +0000

    Fix thread count in test and verify acceptor threads

--- core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
+++ core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
@@ -420,7 +420,7 @@ class DynamicBrokerReconfigurationTest extends ZooKeeperTestHarness with SaslSet
     }
 
     def increasePoolSize(propName: String, currentSize: => Int, threadPrefix: String): Int = {
-      val newSize = currentSize * 2 - 1
+      val newSize = if (currentSize == 1) currentSize * 2 else currentSize * 2 - 1
       resizeThreadPool(propName, newSize, threadPrefix)
       newSize
     }
@@ -459,6 +459,7 @@ class DynamicBrokerReconfigurationTest extends ZooKeeperTestHarness with SaslSet
       "", mayReceiveDuplicates = false)
     verifyThreadPoolResize(KafkaConfig.NumNetworkThreadsProp, config.numNetworkThreads,
       networkThreadPrefix, mayReceiveDuplicates = true)
+    verifyThreads("kafka-socket-acceptor-", config.listeners.size)
 
     verifyProcessorMetrics()
     verifyMarkPartitionsForTruncation()
