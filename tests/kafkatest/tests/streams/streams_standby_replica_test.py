639d0d164e31726a0b815c830be9da2bac68fff9
Bill Bejeck <bill@confluent.io>
2018-03-26 15:06:06 -0400

    reset previous conditions to stricter output, only have last case with multiple rebalances looser to account for some non-determinism.

--- tests/kafkatest/tests/streams/streams_standby_replica_test.py
+++ tests/kafkatest/tests/streams/streams_standby_replica_test.py
@@ -61,19 +61,19 @@ class StreamsStandbyTask(BaseStreamsTest):
 
         processor_2.start()
 
-        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_1.STDOUT_FILE)
-        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_2.STDOUT_FILE)
+        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_1.STDOUT_FILE)
+        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_2.STDOUT_FILE)
 
         processor_3.start()
 
-        self.wait_for_verification(processor_1, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_1.STDOUT_FILE)
-        self.wait_for_verification(processor_2, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_2.STDOUT_FILE)
-        self.wait_for_verification(processor_3, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_3.STDOUT_FILE)
+        self.wait_for_verification(processor_1, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_1.STDOUT_FILE)
+        self.wait_for_verification(processor_2, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_2.STDOUT_FILE)
+        self.wait_for_verification(processor_3, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_3.STDOUT_FILE)
 
         processor_1.stop()
 
-        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_2.STDOUT_FILE, num_lines=2)
-        self.wait_for_verification(processor_3, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_3.STDOUT_FILE)
+        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_2.STDOUT_FILE, num_lines=2)
+        self.wait_for_verification(processor_3, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_3.STDOUT_FILE)
 
         processor_2.stop()
 
@@ -81,19 +81,19 @@ class StreamsStandbyTask(BaseStreamsTest):
 
         processor_1.start()
 
-        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_1.STDOUT_FILE)
-        self.wait_for_verification(processor_3, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_3.STDOUT_FILE, num_lines=2)
+        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_1.STDOUT_FILE)
+        self.wait_for_verification(processor_3, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_3.STDOUT_FILE, num_lines=2)
 
         processor_2.start()
 
-        self.wait_for_verification(processor_1, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_1.STDOUT_FILE)
-        self.wait_for_verification(processor_2, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_2.STDOUT_FILE)
-        self.wait_for_verification(processor_3, "ACTIVE_TASKS:2 STANDBY_TASKS:[^0]", processor_3.STDOUT_FILE, num_lines=2)
+        self.wait_for_verification(processor_1, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_1.STDOUT_FILE)
+        self.wait_for_verification(processor_2, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_2.STDOUT_FILE)
+        self.wait_for_verification(processor_3, "ACTIVE_TASKS:2 STANDBY_TASKS:2", processor_3.STDOUT_FILE, num_lines=2)
 
         processor_3.stop()
 
-        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_1.STDOUT_FILE, num_lines=2)
-        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:[^0]", processor_2.STDOUT_FILE)
+        self.wait_for_verification(processor_1, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_1.STDOUT_FILE, num_lines=2)
+        self.wait_for_verification(processor_2, "ACTIVE_TASKS:3 STANDBY_TASKS:3", processor_2.STDOUT_FILE)
 
         processor_1.stop()
 
