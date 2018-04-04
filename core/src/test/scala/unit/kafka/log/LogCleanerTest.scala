6c416019fadfe08c130e2f36aee9cb40a9ae9480
Cabral Luis <Luis.Cabral@avaloq.com>
2018-04-04 17:22:57 +0200

    Dummy change to re-trigger jenkins jobs

--- core/src/test/scala/unit/kafka/log/LogCleanerTest.scala
+++ core/src/test/scala/unit/kafka/log/LogCleanerTest.scala
@@ -1429,7 +1429,6 @@ class LogCleanerTest extends JUnitSuite {
     TestUtils.singletonRecords(value = value.toString.getBytes)
 
   private def tombstoneRecord(key: Int): MemoryRecords = record(key, null)
-
 }
 
 class FakeCleanerCache(val slots: Int) extends CleanerCache {
