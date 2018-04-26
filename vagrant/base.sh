8332258be33d6cffcf64e7cbebfc7668b6305dac
Jarek Rudzinski <jarek@confluent.io>
2018-04-26 10:34:51 -0700

    update cached jdk to 171, current version used by debian jdk8 installer

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -31,9 +31,9 @@ if [ -z `which javac` ]; then
     if [ -e "/tmp/oracle-jdk8-installer-cache/" ]; then
         find /tmp/oracle-jdk8-installer-cache/ -not -empty -exec cp '{}' /var/cache/oracle-jdk8-installer/ \;
     fi
-    if [ ! -e "/var/cache/oracle-jdk8-installer/jdk-8u161-linux-x64.tar.gz" ]; then
+    if [ ! -e "/var/cache/oracle-jdk8-installer/jdk-8u171-linux-x64.tar.gz" ]; then
       # Grab a copy of the JDK since it has moved and original downloader won't work
-      curl -s -L "https://s3-us-west-2.amazonaws.com/kafka-packages/jdk-8u161-linux-x64.tar.gz" -o /var/cache/oracle-jdk8-installer/jdk-8u161-linux-x64.tar.gz
+      curl -s -L "https://s3-us-west-2.amazonaws.com/kafka-packages/jdk-8u171-linux-x64.tar.gz" -o /var/cache/oracle-jdk8-installer/jdk-8u171-linux-x64.tar.gz
     fi
 
     /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
