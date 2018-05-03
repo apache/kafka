12898188f0410029df870dd3be05580c1249a6c7
Dhruvil Shah <dhruvil@confluent.io>
2018-05-03 11:56:49 -0700

    Added some comments.

--- clients/src/main/java/org/apache/kafka/common/record/LazyDownConversionRecords.java
+++ clients/src/main/java/org/apache/kafka/common/record/LazyDownConversionRecords.java
@@ -19,12 +19,14 @@ package org.apache.kafka.common.record;
 import org.apache.kafka.common.RecordsProcessingStats;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.TopicPartitionRecordsStats;
+import org.apache.kafka.common.requests.RecordsSend;
 import org.apache.kafka.common.utils.AbstractIterator;
 import org.apache.kafka.common.utils.SystemTime;
 import org.apache.kafka.common.utils.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.GatheringByteChannel;
@@ -34,12 +36,12 @@ import java.util.List;
 
 /**
  * Encapsulation for holding down-converted records with implementation for being able to "lazily" down-convert records.
- * Records are down-converted in batches and on-demand when {@link #writeTo} method is called.
- * <p>
- * Because we do not have a full view of the underlying down-converted records, lot of methods typically associated with
- * {@link Records} are unsupported and not implemented. Specifically, this class provides implementations for
- * {@link #sizeInBytes() sizeInBytes} and {@link #writeTo(GatheringByteChannel, long, int) writeTo} methods only.
- * </p>
+ * Records are down-converted in batches and on-demand when {@link #writeTo} method is called. The implementation ensures
+ * that we are able to send out at least one full batch of messages after down-conversion if {@link #writeTo} is used in
+ * conjunction with {@link #sizeInBytes} - see {@link RecordsSend#writeTo} for an example.
+ * <p> Because we do not have a full view of the underlying down-converted records, lot of methods typically associated
+ * with {@link Records} are unsupported and not implemented. Specifically, this class provides implementations for
+ * {@link #sizeInBytes() sizeInBytes} and {@link #writeTo(GatheringByteChannel, long, int) writeTo} methods only. </p>
  */
 public class LazyDownConversionRecords implements SerializableRecords {
     private static final Logger log = LoggerFactory.getLogger(LazyDownConversionRecords.class);
@@ -72,18 +74,34 @@ public class LazyDownConversionRecords implements SerializableRecords {
 
     /**
      * {@inheritDoc}
-     * Note that this method returns the size of records before down-conversion, because we do not have a full view
-     * of the down-converted records.
+     * Note that we do not have a way to return the exact size of down-converted messages, so we return the size of the
+     * pre-down-converted messages. The consumer however expects at least one full batch of messages to be sent out so
+     * we also factor in the down-converted size of the first batch.
      */
     @Override
     public int sizeInBytes() {
         return Math.max(records.sizeInBytes(), minimumSize);
     }
 
+    /**
+     * {@inheritDoc}
+     * Note that this is a stateful method. The expected use case is to start writing to the underlying channel starting
+     * with (position = 0, length = sizeInBytes()), with position being advanced by the bytes written and the length being
+     * reduced by the same amount. Example usage is the following:
+     * <pre>
+     *     LazyDownConversionRecords records = ...
+     *     int length = records.sizeInBytes();
+     *     int remaining = length;
+     *
+     *     while (remaining > 0)
+     *         remaining -= records.writeTo(channel, length - remaining, remaining);
+     * </pre>
+     * Also see {@link RecordsSend#writeTo} for example usage.
+     */
     @Override
     public long writeTo(GatheringByteChannel channel, long position, int length) throws IOException {
         if (position == 0) {
-            log.debug("Initializing lazy down-conversion for {" + topicPartition + "} with length=" + length);
+            log.info("Initializing lazy down-conversion for {" + topicPartition + "} with length=" + length);
             convertedRecordsIterator = lazyDownConversionRecordsIterator(MAX_READ_SIZE);
             processingStats = new RecordsProcessingStats(0, 0, 0);
         }
@@ -96,16 +114,19 @@ public class LazyDownConversionRecords implements SerializableRecords {
                 ConvertedRecords recordsAndStats = convertedRecordsIterator.next();
                 convertedRecords = recordsAndStats.records();
 
-                if (position == 0 && convertedRecords.batchIterator().peek().sizeInBytes() > length)
-                    throw new IllegalStateException("Not able to send first batch completely." +
+                if ((position == 0) && (convertedRecords.batchIterator().peek().sizeInBytes() > length))
+                    throw new EOFException("Unable to send first batch completely." +
                             " batch_size: " + convertedRecords.batchIterator().peek().sizeInBytes() +
                             " required_size: " + length +
                             " minimum_size: " + minimumSize +
                             " records_size: " + records.sizeInBytes());
 
                 processingStats.addToProcessingStats(recordsAndStats.recordsProcessingStats());
-                log.debug("Got lazy converted records for {" + topicPartition + "} with length=" + convertedRecords.sizeInBytes());
+                log.info("Got lazy converted records for {" + topicPartition + "} with length=" + convertedRecords.sizeInBytes());
             } else {
+                if (position == 0)
+                    throw new EOFException("Unable to get the first batch of down-converted records");
+
                 // We do not have any records left to down-convert. Construct a "fake" message for the length remaining.
                 // This message will be ignored by the consumer because its length will be past the length of maximum
                 // possible response size.
@@ -113,9 +134,10 @@ public class LazyDownConversionRecords implements SerializableRecords {
                 //      BaseOffset => Int64
                 //      Length => Int32
                 //      ...
-                log.error("Constructing fake message batch for topic-partition {" + topicPartition + "} for remaining length " + length);
-                int minLength = 8 + 4;
-                ByteBuffer fakeMessageBatch = ByteBuffer.allocate(length + 1 + minLength);
+                // TODO: check if there is a better way to encapsulate this logic, perhaps in DefaultRecordBatch
+                log.info("Constructing fake message batch for topic-partition {" + topicPartition + "} for remaining length " + length);
+                int minLength = (Long.SIZE / Byte.SIZE) + (Integer.SIZE / Byte.SIZE);
+                ByteBuffer fakeMessageBatch = ByteBuffer.allocate(Math.max(minLength, length + 1));
                 fakeMessageBatch.putLong(-1L);
                 fakeMessageBatch.putInt(length + 1);
                 convertedRecords = MemoryRecords.readableRecords(fakeMessageBatch);
