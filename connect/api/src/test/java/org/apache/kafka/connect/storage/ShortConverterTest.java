04f7a9074912cb188893696baa189b50d0b6b62f
Randall Hauch <rhauch@gmail.com>
2018-05-17 17:33:24 -0500

    KAFKA-6913: Add Connect converters and header converters for short, integer, long, float, and double
    Added converters and header converters for the primitive number types for which Kafka already had serializers and deserializers.

---
+++ connect/api/src/test/java/org/apache/kafka/connect/storage/ShortConverterTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.connect.storage;
+
+import org.apache.kafka.common.serialization.ShortSerializer;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class ShortConverterTest {
+    private static final String TOPIC = "topic";
+    private static final Short SAMPLE_VALUE = 123;
+    private static final byte[] SAMPLE_VALUE_BYTES = new ShortSerializer().serialize(TOPIC, SAMPLE_VALUE);
+    private static final Schema SAMPLE_SCHEMA = Schema.OPTIONAL_INT16_SCHEMA;
+    private static final ShortConverter CONVERTER = new ShortConverter();
+
+    @Test
+    public void testNumberToBytes() throws UnsupportedEncodingException {
+        assertArrayEquals(SAMPLE_VALUE_BYTES, CONVERTER.fromConnectData(TOPIC, SAMPLE_SCHEMA, SAMPLE_VALUE));
+    }
+
+    @Test
+    public void testNullToBytes() {
+        assertEquals(null, CONVERTER.fromConnectData(TOPIC, SAMPLE_SCHEMA, null));
+    }
+
+    @Test
+    public void testBytesToNumber() {
+        SchemaAndValue data = CONVERTER.toConnectData(TOPIC, SAMPLE_VALUE_BYTES);
+        assertEquals(SAMPLE_SCHEMA, data.schema());
+        assertEquals(SAMPLE_VALUE, data.value());
+    }
+
+    @Test
+    public void testBytesNullToNumber() {
+        SchemaAndValue data = CONVERTER.toConnectData(TOPIC, null);
+        assertEquals(SAMPLE_SCHEMA, data.schema());
+        assertEquals(null, data.value());
+    }
+
+    // Note: the header conversion methods delegates to the data conversion methods, which are tested above.
+    // The following simply verify that the delegation works.
+
+    @Test
+    public void testHeaderValueToBytes() throws UnsupportedEncodingException {
+        assertArrayEquals(SAMPLE_VALUE_BYTES, CONVERTER.fromConnectHeader(TOPIC, "hdr", SAMPLE_SCHEMA, SAMPLE_VALUE));
+    }
+
+    @Test
+    public void testNullHeaderValueToBytes() {
+        assertEquals(null, CONVERTER.fromConnectHeader(TOPIC, "hdr", SAMPLE_SCHEMA, null));
+    }
+}
