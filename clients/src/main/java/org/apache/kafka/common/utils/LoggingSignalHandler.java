34ea0893ffed84ecaaa1a412a5dfc1f4e5686fbb
Ismael Juma <ismael@juma.me.uk>
2018-05-19 22:51:13 -0700

    Fix checkstyle error

--- clients/src/main/java/org/apache/kafka/common/utils/LoggingSignalHandler.java
+++ clients/src/main/java/org/apache/kafka/common/utils/LoggingSignalHandler.java
@@ -85,7 +85,7 @@ public class LoggingSignalHandler {
                 return null;
             }
         };
-        return Proxy.newProxyInstance(Utils.getContextOrKafkaClassLoader(), new Class[] { signalHandlerClass },
+        return Proxy.newProxyInstance(Utils.getContextOrKafkaClassLoader(), new Class[] {signalHandlerClass},
                 invocationHandler);
     }
 
