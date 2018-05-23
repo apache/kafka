ca77b6515333c35f111016cd346f789b77ce0592
maytals <maytal@alooma.io>
2018-05-23 09:02:15 +0300

    Added ZkDataTest for testing ConsumerOffset.path change

---
+++ core/src/test/scala/unit/kafka/zk/ZkDataTest.scala
@@ -0,0 +1,25 @@
+package unit.kafka.zk
+
+import kafka.zk.ConsumerOffset
+import org.junit.{Assert, Test}
+
+class ZkDataTest {
+
+
+  @Test
+  def testConsumerOffsetPath(): Unit = {
+    def getConsumersOffsetsZkPath(consumerGroup: String, topic: String, partition: Int): String = {
+      s"/consumers/$consumerGroup/offsets/$topic/$partition"
+    }
+
+    val consumerGroup = "test-group"
+    val topic = "test-topic"
+    val partition = 2
+
+    val expectedConsumerGroupOffsetsPath = getConsumersOffsetsZkPath(consumerGroup, topic, partition)
+    val actualConsumerGroupOffsetsPath = ConsumerOffset.path(consumerGroup, topic, partition)
+
+    Assert.assertEquals(expectedConsumerGroupOffsetsPath, actualConsumerGroupOffsetsPath)
+  }
+
+}
