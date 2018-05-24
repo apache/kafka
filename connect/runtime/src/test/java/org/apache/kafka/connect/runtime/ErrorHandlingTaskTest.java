16ec1d804579d909f70489f3a3b068e4926445ef
Arjun Satish <arjun@confluent.io>
2018-05-24 00:13:23 -0700

    KAFKA-6728: Add error handling tests for worker source task
    Signed-off-by: Arjun Satish <arjun@confluent.io>

--- connect/runtime/src/test/java/org/apache/kafka/connect/runtime/ErrorHandlingTaskTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/ErrorHandlingTaskTest.java
@@ -20,11 +20,15 @@ import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.utils.MockTime;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.json.JsonConverter;
 import org.apache.kafka.connect.runtime.errors.ErrorHandlingMetrics;
@@ -37,7 +41,12 @@ import org.apache.kafka.connect.runtime.standalone.StandaloneConfig;
 import org.apache.kafka.connect.sink.SinkConnector;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.source.SourceTask;
+import org.apache.kafka.connect.storage.Converter;
 import org.apache.kafka.connect.storage.HeaderConverter;
+import org.apache.kafka.connect.storage.OffsetStorageReader;
+import org.apache.kafka.connect.storage.OffsetStorageWriter;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.SimpleConfig;
 import org.apache.kafka.connect.util.ConnectorTaskId;
@@ -58,12 +67,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(WorkerSinkTask.class)
+@PrepareForTest({WorkerSinkTask.class, WorkerSourceTask.class})
 @PowerMockIgnore("javax.management.*")
-public class WorkerTaskWithErrorHandlingTest {
+public class ErrorHandlingTaskTest {
 
     private static final String TOPIC = "test";
     private static final int PARTITION1 = 12;
@@ -96,6 +107,9 @@ public class WorkerTaskWithErrorHandlingTest {
     @SuppressWarnings("unused")
     @Mock
     private SinkTask sinkTask;
+    @SuppressWarnings("unused")
+    @Mock
+    private SourceTask sourceTask;
     private Capture<WorkerSinkTaskContext> sinkTaskContext = EasyMock.newCapture();
     private WorkerConfig workerConfig;
     @Mock
@@ -103,10 +117,20 @@ public class WorkerTaskWithErrorHandlingTest {
     @SuppressWarnings("unused")
     @Mock
     private HeaderConverter headerConverter;
-    private WorkerSinkTask workerTask;
+    private WorkerSinkTask workerSinkTask;
+    private WorkerSourceTask workerSourceTask;
     @SuppressWarnings("unused")
     @Mock
     private KafkaConsumer<byte[], byte[]> consumer;
+    @SuppressWarnings("unused")
+    @Mock
+    private KafkaProducer<byte[], byte[]> producer;
+
+    @Mock
+    OffsetStorageReader offsetReader;
+    @Mock
+    OffsetStorageWriter offsetWriter;
+
     private Capture<ConsumerRebalanceListener> rebalanceListener = EasyMock.newCapture();
     @SuppressWarnings("unused")
     @Mock
@@ -143,7 +167,7 @@ public class WorkerTaskWithErrorHandlingTest {
         RetryWithToleranceExecutor executor = new RetryWithToleranceExecutor(time);
         executor.configure(OPERATION_EXECUTOR_PROPS);
         executor.setMetrics(errorHandlingMetrics);
-        createTask(initialState, new ProcessingContext(Collections.singletonList(reporter)), executor);
+        createSinkTask(initialState, new ProcessingContext(singletonList(reporter)), executor);
 
         expectInitializeTask();
 
@@ -160,11 +184,11 @@ public class WorkerTaskWithErrorHandlingTest {
 
         PowerMock.replayAll();
 
-        workerTask.initialize(TASK_CONFIG);
-        workerTask.initializeAndStart();
-        workerTask.iteration();
+        workerSinkTask.initialize(TASK_CONFIG);
+        workerSinkTask.initializeAndStart();
+        workerSinkTask.iteration();
 
-        workerTask.iteration();
+        workerSinkTask.iteration();
 
         // two records were consumed from Kafka
         assertSinkMetricValue("sink-record-read-total", 2.0);
@@ -180,8 +204,130 @@ public class WorkerTaskWithErrorHandlingTest {
         PowerMock.verifyAll();
     }
 
+    @Test
+    public void testErrorHandlingInSourceTasks() throws Exception {
+        LogReporter reporter = new LogReporter();
+        Map<String, Object> reportProps = new HashMap<>();
+        reportProps.put(LogReporter.LOG_ENABLE, "true");
+        reportProps.put(LogReporter.LOG_INCLUDE_MESSAGES, "true");
+        reporter.configure(reportProps);
+        reporter.setMetrics(errorHandlingMetrics);
+
+        RetryWithToleranceExecutor executor = new RetryWithToleranceExecutor(time);
+        executor.configure(OPERATION_EXECUTOR_PROPS);
+        executor.setMetrics(errorHandlingMetrics);
+        createSourceTask(initialState, new ProcessingContext(singletonList(reporter)), executor);
+
+        // valid json
+        Schema valSchema = SchemaBuilder.struct().field("val", Schema.INT32_SCHEMA).build();
+        Struct struct1 = new Struct(valSchema).put("val", 1234);
+        SourceRecord record1 = new SourceRecord(emptyMap(), emptyMap(), TOPIC, PARTITION1, valSchema, struct1);
+        Struct struct2 = new Struct(valSchema).put("val", 6789);
+        SourceRecord record2 = new SourceRecord(emptyMap(), emptyMap(), TOPIC, PARTITION1, valSchema, struct2);
+
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(false);
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(false);
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(true);
+
+        EasyMock.expect(workerSourceTask.commitOffsets()).andReturn(true);
+
+        offsetWriter.offset(EasyMock.anyObject(), EasyMock.anyObject());
+        EasyMock.expectLastCall().times(2);
+        sourceTask.initialize(EasyMock.anyObject());
+        EasyMock.expectLastCall();
+
+        sourceTask.start(EasyMock.anyObject());
+        EasyMock.expectLastCall();
+
+        EasyMock.expect(sourceTask.poll()).andReturn(singletonList(record1));
+        EasyMock.expect(sourceTask.poll()).andReturn(singletonList(record2));
+        EasyMock.expect(producer.send(EasyMock.anyObject(), EasyMock.anyObject())).andReturn(null).times(2);
+
+        PowerMock.replayAll();
+
+        workerSourceTask.initialize(TASK_CONFIG);
+        workerSourceTask.execute();
+
+        // two records were consumed from Kafka
+        assertSourceMetricValue("source-record-poll-total", 2.0);
+        // only one was written to the task
+        assertSourceMetricValue("source-record-write-total", 0.0);
+        // one record completely failed (converter issues)
+        assertErrorHandlingMetricValue("processing-errors", 0.0);
+        // 2 failures in the transformation, and 1 in the converter
+        assertErrorHandlingMetricValue("processing-failures", 4.0);
+        // one record completely failed (converter issues), and thus was skipped
+        assertErrorHandlingMetricValue("record-skipped", 0.0);
+
+        PowerMock.verifyAll();
+    }
+
+    @Test
+    public void testErrorHandlingInSourceTasksWthBadConverter() throws Exception {
+        LogReporter reporter = new LogReporter();
+        Map<String, Object> reportProps = new HashMap<>();
+        reportProps.put(LogReporter.LOG_ENABLE, "true");
+        reportProps.put(LogReporter.LOG_INCLUDE_MESSAGES, "true");
+        reporter.configure(reportProps);
+        reporter.setMetrics(errorHandlingMetrics);
+
+        RetryWithToleranceExecutor executor = new RetryWithToleranceExecutor(time);
+        executor.configure(OPERATION_EXECUTOR_PROPS);
+        executor.setMetrics(errorHandlingMetrics);
+        createSourceTask(initialState, new ProcessingContext(singletonList(reporter)), executor, badConverter());
+
+        // valid json
+        Schema valSchema = SchemaBuilder.struct().field("val", Schema.INT32_SCHEMA).build();
+        Struct struct1 = new Struct(valSchema).put("val", 1234);
+        SourceRecord record1 = new SourceRecord(emptyMap(), emptyMap(), TOPIC, PARTITION1, valSchema, struct1);
+        Struct struct2 = new Struct(valSchema).put("val", 6789);
+        SourceRecord record2 = new SourceRecord(emptyMap(), emptyMap(), TOPIC, PARTITION1, valSchema, struct2);
+
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(false);
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(false);
+        EasyMock.expect(workerSourceTask.isStopping()).andReturn(true);
+
+        EasyMock.expect(workerSourceTask.commitOffsets()).andReturn(true);
+
+        offsetWriter.offset(EasyMock.anyObject(), EasyMock.anyObject());
+        EasyMock.expectLastCall().times(2);
+        sourceTask.initialize(EasyMock.anyObject());
+        EasyMock.expectLastCall();
+
+        sourceTask.start(EasyMock.anyObject());
+        EasyMock.expectLastCall();
+
+        EasyMock.expect(sourceTask.poll()).andReturn(singletonList(record1));
+        EasyMock.expect(sourceTask.poll()).andReturn(singletonList(record2));
+        EasyMock.expect(producer.send(EasyMock.anyObject(), EasyMock.anyObject())).andReturn(null).times(2);
+
+        PowerMock.replayAll();
+
+        workerSourceTask.initialize(TASK_CONFIG);
+        workerSourceTask.execute();
+
+        // two records were consumed from Kafka
+        assertSourceMetricValue("source-record-poll-total", 2.0);
+        // only one was written to the task
+        assertSourceMetricValue("source-record-write-total", 0.0);
+        // one record completely failed (converter issues)
+        assertErrorHandlingMetricValue("processing-errors", 0.0);
+        // 2 failures in the transformation, and 1 in the converter
+        assertErrorHandlingMetricValue("processing-failures", 8.0);
+        // one record completely failed (converter issues), and thus was skipped
+        assertErrorHandlingMetricValue("record-skipped", 0.0);
+
+        PowerMock.verifyAll();
+    }
+
     private void assertSinkMetricValue(String name, double expected) {
-        ConnectMetrics.MetricGroup sinkTaskGroup = workerTask.sinkTaskMetricsGroup().metricGroup();
+        ConnectMetrics.MetricGroup sinkTaskGroup = workerSinkTask.sinkTaskMetricsGroup().metricGroup();
+        double measured = metrics.currentMetricValueAsDouble(sinkTaskGroup, name);
+        assertEquals(expected, measured, 0.001d);
+    }
+
+    private void assertSourceMetricValue(String name, double expected) {
+        ConnectMetrics.MetricGroup sinkTaskGroup = workerSourceTask.sourceTaskMetricsGroup().metricGroup();
         double measured = metrics.currentMetricValueAsDouble(sinkTaskGroup, name);
         assertEquals(expected, measured, 0.001d);
     }
@@ -193,8 +339,8 @@ public class WorkerTaskWithErrorHandlingTest {
     }
 
     private void expectInitializeTask() throws Exception {
-        PowerMock.expectPrivate(workerTask, "createConsumer").andReturn(consumer);
-        consumer.subscribe(EasyMock.eq(Collections.singletonList(TOPIC)), EasyMock.capture(rebalanceListener));
+        PowerMock.expectPrivate(workerSinkTask, "createConsumer").andReturn(consumer);
+        consumer.subscribe(EasyMock.eq(singletonList(TOPIC)), EasyMock.capture(rebalanceListener));
         PowerMock.expectLastCall();
 
         sinkTask.initialize(EasyMock.capture(sinkTaskContext));
@@ -203,28 +349,76 @@ public class WorkerTaskWithErrorHandlingTest {
         PowerMock.expectLastCall();
     }
 
-    private void createTask(TargetState initialState, ProcessingContext context, OperationExecutor executor) {
+    private void createSinkTask(TargetState initialState, ProcessingContext context, OperationExecutor executor) {
         JsonConverter converter = new JsonConverter();
         Map<String, Object> oo = workerConfig.originalsWithPrefix("value.converter.");
         oo.put("converter.type", "value");
         oo.put("schemas.enable", "false");
         converter.configure(oo);
 
-        TransformationChain<SinkRecord> sinkTransforms = new TransformationChain<>(Collections.singletonList(new FaultyPassthrough<SinkRecord>()));
+        TransformationChain<SinkRecord> sinkTransforms = new TransformationChain<>(singletonList(new FaultyPassthrough<SinkRecord>()));
         sinkTransforms.initialize(executor, context);
 
-        workerTask = PowerMock.createPartialMock(
+        workerSinkTask = PowerMock.createPartialMock(
                 WorkerSinkTask.class, new String[]{"createConsumer"},
                 taskId, sinkTask, statusListener, initialState, workerConfig, metrics, converter, converter,
                 headerConverter, sinkTransforms, pluginLoader, time, context, executor);
     }
 
+    private void createSourceTask(TargetState initialState, ProcessingContext context, OperationExecutor executor) {
+        JsonConverter converter = new JsonConverter();
+        Map<String, Object> oo = workerConfig.originalsWithPrefix("value.converter.");
+        oo.put("converter.type", "value");
+        oo.put("schemas.enable", "false");
+        converter.configure(oo);
+
+        createSourceTask(initialState, context, executor, converter);
+    }
+
+    private Converter badConverter() {
+        FaultyConverter converter = new FaultyConverter();
+        Map<String, Object> oo = workerConfig.originalsWithPrefix("value.converter.");
+        oo.put("converter.type", "value");
+        oo.put("schemas.enable", "false");
+        converter.configure(oo);
+        return converter;
+    }
+
+    private void createSourceTask(TargetState initialState, ProcessingContext context, OperationExecutor executor, Converter converter) {
+        TransformationChain<SourceRecord> sourceTransforms = new TransformationChain<>(singletonList(new FaultyPassthrough<SourceRecord>()));
+        sourceTransforms.initialize(executor, context);
+
+        workerSourceTask = PowerMock.createPartialMock(
+                WorkerSourceTask.class, new String[]{"commitOffsets", "isStopping"},
+                taskId, sourceTask, statusListener, initialState, converter, converter, headerConverter, sourceTransforms,
+                producer, offsetReader, offsetWriter, workerConfig, metrics, pluginLoader, time, context, executor);
+    }
+
     private ConsumerRecords<byte[], byte[]> records(ConsumerRecord<byte[], byte[]> record) {
         return new ConsumerRecords<>(Collections.singletonMap(
-                new TopicPartition(record.topic(), record.partition()), Collections.singletonList(record)));
+                new TopicPartition(record.topic(), record.partition()), singletonList(record)));
     }
 
     private abstract static class TestSinkTask extends SinkTask {
+    }
+
+    static class FaultyConverter extends JsonConverter {
+        private static final Logger log = LoggerFactory.getLogger(FaultyConverter.class);
+        private int invocations = 0;
+
+        public byte[] fromConnectData(String topic, Schema schema, Object value) {
+            if (value == null) {
+                return super.fromConnectData(topic, schema, null);
+            }
+            invocations++;
+            if (invocations % 3 == 0) {
+                log.debug("Succeeding record: {} where invocations={}", value, invocations);
+                return super.fromConnectData(topic, schema, value);
+            } else {
+                log.debug("Failing record: {} at invocations={}", value, invocations);
+                throw new RetriableException("Bad invocations " + invocations + " for mod 3");
+            }
+        }
     }
 
     static class FaultyPassthrough<R extends ConnectRecord<R>> implements Transformation<R> {
