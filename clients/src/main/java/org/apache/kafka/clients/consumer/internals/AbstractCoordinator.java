98f70e4e97c1a0044f2b9785a2f3a6dc005f6dba
John Roesler <john@confluent.io>
2018-05-25 18:36:50 -0500

    javadoc update

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
@@ -259,7 +259,9 @@ public abstract class AbstractCoordinator implements Closeable {
     }
 
     /**
-     * Check whether the group should be rejoined (e.g. if metadata changes)
+     * Check whether the group should be rejoined (e.g. if metadata changes) or whether a
+     * rejoin request is already in flight and needs to be completed.
+     * 
      * @return true if it should, false otherwise
      */
     protected synchronized boolean rejoinNeededOrPending() {
