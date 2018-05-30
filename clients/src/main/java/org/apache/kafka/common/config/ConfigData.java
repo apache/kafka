998b1f9c4ba49a613b5993d11eb9a52167405ae0
Robert Yokota <rayokota@gmail.com>
2018-05-29 17:18:12 -0700

    Mention TTL can be null for ConfigData in JavaDoc

--- clients/src/main/java/org/apache/kafka/common/config/ConfigData.java
+++ clients/src/main/java/org/apache/kafka/common/config/ConfigData.java
@@ -30,7 +30,7 @@ public class ConfigData {
      * Creates a new ConfigData with the given data and TTL (in milliseconds).
      *
      * @param data a Map of key-value pairs
-     * @param ttl the time-to-live of the data in milliseconds
+     * @param ttl the time-to-live of the data in milliseconds, or null if there is no TTL
      */
     public ConfigData(Map<String, String> data, Long ttl) {
         this.data = data;
@@ -58,7 +58,7 @@ public class ConfigData {
     /**
      * Returns the TTL (in milliseconds).
      *
-     * @return ttl the time-to-live (in milliseconds) of the data.
+     * @return ttl the time-to-live (in milliseconds) of the data, or null if there is no TTL
      */
     public Long ttl() {
         return ttl;
