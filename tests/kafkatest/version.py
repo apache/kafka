8edf17573e74c63f2ca8ced7e3038214cd7e04e3
Matthias J. Sax <matthias@confluent.io>
2018-06-06 11:42:24 -0700

    MINOR: need to update system test version after version bumb

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -61,7 +61,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("2.0.0-SNAPSHOT")
+DEV_VERSION = KafkaVersion("2.1.0-SNAPSHOT")
 
 # 0.8.2.x versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
