bf09e38f16a0efbe1dc194cb5e0c8914d9a94794
Robert Yokota <rayokota@gmail.com>
2018-06-11 16:57:02 -0700

    KIP-297: MINOR: move FileConfigProvider to provider subpackage

--- clients/src/main/java/org/apache/kafka/common/config/ConfigTransformer.java
+++ clients/src/main/java/org/apache/kafka/common/config/ConfigTransformer.java
@@ -16,6 +16,8 @@
  */
 package org.apache.kafka.common.config;
 
+import org.apache.kafka.common.config.provider.FileConfigProvider;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
