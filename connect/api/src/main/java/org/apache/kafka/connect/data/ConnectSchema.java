1ae768ca71d8edac37aad2b536b18e563f4fac11
Gunnar Morling <gunnar.morling@googlemail.com>
2018-06-14 12:15:06 +0200

    KAFKA-7058 Comparing schema default values using Objects#deepEquals()

--- connect/api/src/main/java/org/apache/kafka/connect/data/ConnectSchema.java
+++ connect/api/src/main/java/org/apache/kafka/connect/data/ConnectSchema.java
@@ -291,7 +291,7 @@ public class ConnectSchema implements Schema {
                 Objects.equals(name, schema.name) &&
                 Objects.equals(doc, schema.doc) &&
                 Objects.equals(type, schema.type) &&
-                Objects.equals(defaultValue, schema.defaultValue) &&
+                Objects.deepEquals(defaultValue, schema.defaultValue) &&
                 Objects.equals(fields, schema.fields) &&
                 Objects.equals(keySchema, schema.keySchema) &&
                 Objects.equals(valueSchema, schema.valueSchema) &&
