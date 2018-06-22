e2d05cca70d4671888a2a735e7316f927583d62d
tedyu <yuzhihong@gmail.com>
2018-06-22 15:48:08 -0700

    Minor: add exception to debug log for Sender#maybeSendTransactionalRequest

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
@@ -364,7 +364,7 @@ public class Sender implements Runnable {
                 }
             } catch (IOException e) {
                 log.debug("Disconnect from {} while trying to send request {}. Going " +
-                        "to back off and retry", targetNode, requestBuilder);
+                        "to back off and retry. Encountered {}", targetNode, requestBuilder, e);
                 if (nextRequestHandler.needsCoordinator()) {
                     // We break here so that we pick up the FindCoordinator request immediately.
                     transactionManager.lookupCoordinator(nextRequestHandler);
