38e4be686ccd8906f936c0c010d628956c2844a7
Ismael Juma <ismael@juma.me.uk>
2018-07-06 14:35:25 -0700

    MINOR: Use FetchRequest v8 and ListOffsetRequest v3 in ReplicaFetcherThread (#5342)
    If inter.broker.protocol.version is 2.0-IV1 or newer. Also fixed ListOffsetRequest
    so that v2 is used, if applicable.
    
    Added a unit test which verifies that we use the latest version of the various
    requests by default. Included a few minor tweaks to make testing easier.
    
    Reviewers: Rajini Sivaram <rajinisivaram@googlemail.com>

--- core/src/main/scala/kafka/api/ApiVersion.scala
+++ core/src/main/scala/kafka/api/ApiVersion.scala
@@ -70,9 +70,9 @@ object ApiVersion {
     // Introduced DeleteGroupsRequest V0 via KIP-229, plus KIP-227 incremental fetch requests,
     // and KafkaStorageException for fetch requests.
     KAFKA_1_1_IV0,
-    // Introduced OffsetsForLeaderEpochRequest V1 via KIP-279
+    // Introduced OffsetsForLeaderEpochRequest V1 via KIP-279 (Fix log divergence between leader and follower after fast leader fail over)
     KAFKA_2_0_IV0,
-    // Introduced ApiVersionsRequest V2 via KIP-219
+    // Several request versions were bumped due to KIP-219 (Improve quota communication)
     KAFKA_2_0_IV1
   )
 
