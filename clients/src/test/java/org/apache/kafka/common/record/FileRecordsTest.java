9c54eb5d461779100e1c9c236b10e36a18dabac8
Jason Gustafson <jason@confluent.io>
2018-07-10 11:19:07 -0700

    Remove unused import

--- clients/src/test/java/org/apache/kafka/common/record/FileRecordsTest.java
+++ clients/src/test/java/org/apache/kafka/common/record/FileRecordsTest.java
@@ -30,7 +30,6 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
