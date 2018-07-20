e248fed6a2b5393bc4ef80f2b7756329fe3a77f1
teivah <teiva.harsanyi@gmail.com>
2018-07-20 12:21:28 +0200

    Implicit conversion from Long to SessionWindows

--- streams/streams-scala/src/main/scala/org/apache/kafka/streams/scala/ImplicitConversions.scala
+++ streams/streams-scala/src/main/scala/org/apache/kafka/streams/scala/ImplicitConversions.scala
@@ -81,4 +81,6 @@ object ImplicitConversions {
                                                       valueSerde: Serde[V],
                                                       otherValueSerde: Serde[VO]): Joined[K, V, VO] =
     Joined.`with`(keySerde, valueSerde, otherValueSerde)
+
+  implicit def longToSessionWindows(x: Long): SessionWindows = SessionWindows.`with`(x)
 }
