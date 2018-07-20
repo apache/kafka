bdb08a4221b4b36873a5ceeb060cd155d5f8d053
Bill Bejeck <bill@confluent.io>
2018-07-20 14:37:09 -0400

    MINOR: increase dev version from 1.1.1-SNAPSHOT to 1.1.2-SNAPSHOT

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -14,9 +14,8 @@
 # limitations under the License.
 
 
-from kafkatest.utils import kafkatest_version
-
 from distutils.version import LooseVersion
+from kafkatest.utils import kafkatest_version
 
 
 class KafkaVersion(LooseVersion):
@@ -61,7 +60,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("1.1.1-SNAPSHOT")
+DEV_VERSION = KafkaVersion("1.1.2-SNAPSHOT")
 
 # 0.8.2.X versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
