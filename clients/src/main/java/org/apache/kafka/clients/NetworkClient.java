0417183da0c32174a1f55d990117403d91f85fff
Jason Gustafson <jason@confluent.io>
2018-08-02 11:02:38 -0700

    KAFKA-7231; Ensure NetworkClient uses overridden request timeout (#5444)
    Fixed incorrect use of default timeout instead of the argument explicitly passed to `newClientRequest`.
    
    Reviewers: Ron Dagostino <rndgstn@gmail.com>, Ismael Juma <ismael@juma.me.uk>

--- clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
+++ clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
@@ -1060,7 +1060,7 @@ public class NetworkClient implements KafkaClient {
                                           int requestTimeoutMs,
                                           RequestCompletionHandler callback) {
         return new ClientRequest(nodeId, requestBuilder, correlation++, clientId, createdTimeMs, expectResponse,
-                defaultRequestTimeoutMs, callback);
+                requestTimeoutMs, callback);
     }
 
     public boolean discoverBrokerVersions() {
