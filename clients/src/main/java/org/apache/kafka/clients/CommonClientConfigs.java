014a0a7aee1d90f5cd1fb1095dc662bbda29d8a3
sowhynot <30879636+sowhynot@users.noreply.github.com>
2018-08-14 22:36:55 +0100

    KAFKA-6195 change setting documentation

--- clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
+++ clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
@@ -42,7 +42,7 @@ public class CommonClientConfigs {
                                                        + "servers (you may want more than one, though, in case a server is down).";
 
     public static final String CLIENT_DNS_LOOKUP = "client.dns.lookup";
-    public static final String CLIENT_DNS_LOOKUP_DOC = "Drives whether the client performs DNS lookups on entries of bootstrap.servers";
+    public static final String CLIENT_DNS_LOOKUP_DOC = "To enable client DNS lookups on bootstrap.servers";
 
     public static final String METADATA_MAX_AGE_CONFIG = "metadata.max.age.ms";
     public static final String METADATA_MAX_AGE_DOC = "The period of time in milliseconds after which we force a refresh of metadata even if we haven't seen any partition leadership changes to proactively discover any new brokers or partitions.";
