bfbd7e4e5630308d54f0cc77b349ec777ed00957
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-09-03 16:42:09 +0200

    removed null check because .close() call has been removed as well

--- core/src/main/scala/kafka/log/AbstractIndex.scala
+++ core/src/main/scala/kafka/log/AbstractIndex.scala
@@ -334,10 +334,8 @@ abstract class AbstractIndex[K, V](@volatile var file: File, val baseOffset: Lon
    * Forcefully free the buffer's mmap.
    */
   protected[log] def forceUnmap() {
-    if (mmap != null) {
-      try MappedByteBuffers.unmap(file.getAbsolutePath, mmap)
-      finally mmap = null // Accessing unmapped mmap crashes JVM by SEGV so we null it out to be safe
-    }
+    try MappedByteBuffers.unmap(file.getAbsolutePath, mmap)
+    finally mmap = null // Accessing unmapped mmap crashes JVM by SEGV so we null it out to be safe
   }
 
   /**
