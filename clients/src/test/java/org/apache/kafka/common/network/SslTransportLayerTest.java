f256dfdaad90545f9e1f57c678c7247568110bee
Rajini Sivaram <rajinisivaram@googlemail.com>
2018-09-29 07:09:10 +0100

    KAFKA-7454: Use lazy allocation for SslTransportLayer buffers and null them on close (#5713)
    Lazy allocation helps when there are a large number of connections
    that have been accepted, but where no data has been received from
    the clients. Each buffer is often around 16k (max TLS record size).
    
    Nulling the buffers should not make a difference in the current
    implementation since we release the reference to the channel
    and transport layer after we close them, but it's a good practice
    to release medium/large buffers after `close` is called.
    
    Reviewers: Jun Rao <junrao@gmail.com>, Ismael Juma <ismael@juma.me.uk>

--- clients/src/test/java/org/apache/kafka/common/network/SslTransportLayerTest.java
+++ clients/src/test/java/org/apache/kafka/common/network/SslTransportLayerTest.java
@@ -1071,7 +1071,6 @@ public class SslTransportLayerTest {
             SocketChannel socketChannel = (SocketChannel) key.channel();
             SSLEngine sslEngine = sslFactory.createSslEngine(host, socketChannel.socket().getPort());
             TestSslTransportLayer transportLayer = newTransportLayer(id, key, sslEngine);
-            transportLayer.startHandshake();
             return transportLayer;
         }
 
@@ -1144,6 +1143,12 @@ public class SslTransportLayerTest {
                     return false;
                 resetDelayedFlush();
                 return super.flush(buf);
+            }
+
+            @Override
+            protected void startHandshake() throws IOException {
+                assertTrue("SSL handshake initialized too early", socketChannel().isConnected());
+                super.startHandshake();
             }
 
             private void resetDelayedFlush() {
