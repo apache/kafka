de06eb1ab373f53cc74c513043d737ba164c303e
Bill Bejeck <bill@confluent.io>
2018-10-25 12:46:33 -0400

    MINOR: Increase low timeouts to help with test flakiness

--- streams/src/test/java/org/apache/kafka/streams/integration/GlobalThreadShutDownOrderTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/GlobalThreadShutDownOrderTest.java
@@ -146,7 +146,7 @@ public class GlobalThreadShutDownOrderTest {
             public boolean conditionMet() {
                 return firstRecordProcessed;
             }
-        }, 5000L, "Has not processed record within 5 seconds");
+        }, "Has not processed record within 5 seconds");
 
         kafkaStreams.close(30, TimeUnit.SECONDS);
 
