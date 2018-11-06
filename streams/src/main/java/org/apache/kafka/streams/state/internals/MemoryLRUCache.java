2b7b0a9ae60e002917543fd52c6d3412d4572b97
Charles Feduke <charles.feduke@gmail.com>
2018-11-06 01:31:57 -0500

    MINOR: updated documentation where RocksDBStore was being used as the sample class for byte[] versus Bytes examples

--- streams/src/main/java/org/apache/kafka/streams/state/internals/MemoryLRUCache.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/MemoryLRUCache.java
@@ -36,7 +36,7 @@ import java.util.Objects;
  *
  *  * Note that the use of array-typed keys is discouraged because they result in incorrect ordering behavior.
  * If you intend to work on byte arrays as key, for example, you may want to wrap them with the {@code Bytes} class,
- * i.e. use {@code RocksDBStore<Bytes, ...>} rather than {@code RocksDBStore<byte[], ...>}.
+ * i.e. use {@code MemoryLRUCache<Bytes, ...>} rather than {@code MemoryLRUCache<byte[], ...>}.
  *
  * @param <K> The key type
  * @param <V> The value type
