3af72cd328f5afcc2696571ce10cd895ee024a38
Arjun Satish <arjun@confluent.io>
2018-11-05 16:12:55 -0800

    MINOR: Find free ports using ServerSocket API
    Signed-off-by: Arjun Satish <arjun@confluent.io>

--- connect/runtime/src/test/java/org/apache/kafka/connect/util/clusters/EmbeddedConnectCluster.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/util/clusters/EmbeddedConnectCluster.java
@@ -29,7 +29,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.ServerSocket;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -106,7 +108,6 @@ public class EmbeddedConnectCluster {
 
         workerProps.put(BOOTSTRAP_SERVERS_CONFIG, kafka().bootstrapServers());
         workerProps.put(REST_HOST_NAME_CONFIG, REST_HOST_NAME);
-        workerProps.put(REST_PORT_CONFIG, "0"); // set this to zero so we pick a random port
 
         putIfAbsent(workerProps, GROUP_ID_CONFIG, "connect-integration-test-" + connectClusterName);
         putIfAbsent(workerProps, OFFSET_STORAGE_TOPIC_CONFIG, "connect-offset-topic-" + connectClusterName);
@@ -118,7 +119,13 @@ public class EmbeddedConnectCluster {
         putIfAbsent(workerProps, KEY_CONVERTER_CLASS_CONFIG, "org.apache.kafka.connect.storage.StringConverter");
         putIfAbsent(workerProps, VALUE_CONVERTER_CLASS_CONFIG, "org.apache.kafka.connect.storage.StringConverter");
 
+        int[] ports = new int[connectCluster.length];
+        for (int i = 0; i < ports.length; i++) {
+            ports[i] = findFreePort();
+        }
+        log.debug("Found available ports: {}", Arrays.toString(ports));
         for (int i = 0; i < connectCluster.length; i++) {
+            workerProps.put(REST_PORT_CONFIG, String.valueOf(ports[i])); // set this to zero so we pick a random port
             connectCluster[i] = new ConnectDistributed().startConnect(workerProps);
         }
     }
@@ -231,6 +238,30 @@ public class EmbeddedConnectCluster {
         httpCon.setRequestMethod("DELETE");
         httpCon.connect();
         return httpCon.getResponseCode();
+    }
+
+
+    private int findFreePort() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            // make this port available to bind during timeout state after connection is released
+            socket.setReuseAddress(true);
+            int port = socket.getLocalPort();
+            try {
+                socket.close();
+            } catch (IOException ignored) {
+            }
+            return port;
+        } catch (Exception e) {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+        throw new IllegalStateException("Could not find a free TCP/IP port");
     }
 
     public static class Builder {
