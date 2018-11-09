ca54441c0b3f0db664b1ce462e69c29e1dcaabea
Andras Katona <akatona@cloudera.com>
2018-11-09 20:34:38 +0100

    Reverting RecordSendTest changes

--- clients/src/test/java/org/apache/kafka/clients/producer/RecordSendTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/RecordSendTest.java
@@ -16,22 +16,23 @@
  */
 package org.apache.kafka.clients.producer;
 
-import org.apache.kafka.clients.producer.internals.FutureRecordMetadata;
-import org.apache.kafka.clients.producer.internals.ProduceRequestResult;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.errors.CorruptRecordException;
-import org.apache.kafka.common.record.RecordBatch;
-import org.apache.kafka.common.utils.MockTime;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
+import org.apache.kafka.clients.producer.internals.FutureRecordMetadata;
+import org.apache.kafka.clients.producer.internals.ProduceRequestResult;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.CorruptRecordException;
+import org.apache.kafka.common.record.RecordBatch;
+import org.apache.kafka.common.utils.Time;
+import org.junit.Test;
 
 public class RecordSendTest {
 
@@ -46,7 +47,7 @@ public class RecordSendTest {
     public void testTimeout() throws Exception {
         ProduceRequestResult request = new ProduceRequestResult(topicPartition);
         FutureRecordMetadata future = new FutureRecordMetadata(request, relOffset,
-                RecordBatch.NO_TIMESTAMP, 0L, 0, 0, new MockTime());
+                RecordBatch.NO_TIMESTAMP, 0L, 0, 0, Time.SYSTEM);
         assertFalse("Request is not completed", future.isDone());
         try {
             future.get(5, TimeUnit.MILLISECONDS);
@@ -61,30 +62,38 @@ public class RecordSendTest {
     }
 
     /**
-     * Test that a request will throw the right exception
+     * Test that an asynchronous request will eventually throw the right exception
      */
     @Test(expected = ExecutionException.class)
     public void testError() throws Exception {
-        FutureRecordMetadata future = new FutureRecordMetadata(produceRequestResult(baseOffset, new CorruptRecordException()),
-                relOffset, RecordBatch.NO_TIMESTAMP, 0L, 0, 0, new MockTime());
+        FutureRecordMetadata future = new FutureRecordMetadata(asyncRequest(baseOffset, new CorruptRecordException(), 50L),
+                relOffset, RecordBatch.NO_TIMESTAMP, 0L, 0, 0, Time.SYSTEM);
         future.get();
     }
 
     /**
-     * Test that a request will return the right offset
+     * Test that an asynchronous request will eventually return the right offset
      */
     @Test
     public void testBlocking() throws Exception {
-        FutureRecordMetadata future = new FutureRecordMetadata(produceRequestResult(baseOffset, null),
-                relOffset, RecordBatch.NO_TIMESTAMP, 0L, 0, 0, new MockTime());
+        FutureRecordMetadata future = new FutureRecordMetadata(asyncRequest(baseOffset, null, 50L),
+                relOffset, RecordBatch.NO_TIMESTAMP, 0L, 0, 0, Time.SYSTEM);
         assertEquals(baseOffset + relOffset, future.get().offset());
     }
 
-    /* create a new completed request result */
-    private ProduceRequestResult produceRequestResult(final long baseOffset, final RuntimeException error) {
+    /* create a new request result that will be completed after the given timeout */
+    public ProduceRequestResult asyncRequest(final long baseOffset, final RuntimeException error, final long timeout) {
         final ProduceRequestResult request = new ProduceRequestResult(topicPartition);
-        request.set(baseOffset, RecordBatch.NO_TIMESTAMP, error);
-        request.done();
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(timeout);
+                    request.set(baseOffset, RecordBatch.NO_TIMESTAMP, error);
+                    request.done();
+                } catch (InterruptedException e) { }
+            }
+        };
+        thread.start();
         return request;
     }
 
