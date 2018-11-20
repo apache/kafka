46e7b136e22430340337d3540627a1d685dfa31e
Guozhang Wang <wangguoz@gmail.com>
2018-11-20 14:39:12 -0800

    KAFKA-7536: Initialize TopologyTestDriver with non-null topic (#5923)
    In TopologyTestDriver constructor set non-null topic; and in unit test intentionally turn on caching to verify this case.
    
    Reviewers: Bill Bejeck <bill@confluent.io>, Matthias J. Sax <matthias@confluent.io>

--- streams/test-utils/src/test/java/org/apache/kafka/streams/TopologyTestDriverTest.java
+++ streams/test-utils/src/test/java/org/apache/kafka/streams/TopologyTestDriverTest.java
@@ -890,6 +890,23 @@ public class TopologyTestDriverTest {
     }
 
     @Test
+    public void shouldAllowPrePopulatingStatesStoresWithCachingEnabled() {
+        final Topology topology = new Topology();
+        topology.addSource("sourceProcessor", "input-topic");
+        topology.addProcessor("aggregator", new CustomMaxAggregatorSupplier(), "sourceProcessor");
+        topology.addStateStore(Stores.keyValueStoreBuilder(
+            Stores.inMemoryKeyValueStore("aggStore"),
+            Serdes.String(),
+            Serdes.Long()).withCachingEnabled(), // intentionally turn on caching to achieve better test coverage
+            "aggregator");
+
+        testDriver = new TopologyTestDriver(topology, config);
+
+        store = testDriver.getKeyValueStore("aggStore");
+        store.put("a", 21L);
+    }
+
+    @Test
     public void shouldCleanUpPersistentStateStoresOnClose() {
         final Topology topology = new Topology();
         topology.addSource("sourceProcessor", "input-topic");
