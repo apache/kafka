35589944550469c9cb8d5f03160d9d109d340623
John Roesler <john@confluent.io>
2018-11-20 19:24:35 -0600

    MINOR: increase system test kafka start timeout

--- tests/kafkatest/services/kafka/kafka.py
+++ tests/kafkatest/services/kafka/kafka.py
@@ -271,7 +271,7 @@ class KafkaService(KafkaPathResolverMixin, JmxMixin, Service):
         with node.account.monitor_log(KafkaService.STDOUT_STDERR_CAPTURE) as monitor:
             node.account.ssh(cmd)
             # Kafka 1.0.0 and higher don't have a space between "Kafka" and "Server"
-            monitor.wait_until("Kafka\s*Server.*started", timeout_sec=30, backoff_sec=.25, err_msg="Kafka server didn't finish startup")
+            monitor.wait_until("Kafka\s*Server.*started", timeout_sec=60, backoff_sec=.25, err_msg="Kafka server didn't finish startup")
 
         # Credentials for inter-broker communication are created before starting Kafka.
         # Client credentials are created after starting Kafka so that both loading of
