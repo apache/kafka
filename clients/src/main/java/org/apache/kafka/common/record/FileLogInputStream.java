d831a1e9258de7968643f6dadc655bd2743b658f
Flavien Raynaud <flavr@yelp.com>
2018-12-28 18:36:02 +0000

    Fix FileChannelRecordBatch.hashCode

--- clients/src/main/java/org/apache/kafka/common/record/FileLogInputStream.java
+++ clients/src/main/java/org/apache/kafka/common/record/FileLogInputStream.java
@@ -238,8 +238,10 @@ public class FileLogInputStream implements LogInputStream<FileLogInputStream.Fil
 
         @Override
         public int hashCode() {
+            FileChannel channel = fileRecords == null ? null : fileRecords.channel();
+
             int result = (int) (offset ^ (offset >>> 32));
-            result = 31 * result + (fileRecords != null ? fileRecords.hashCode() : 0);
+            result = 31 * result + (channel != null ? channel.hashCode() : 0);
             result = 31 * result + position;
             result = 31 * result + batchSize;
             return result;
