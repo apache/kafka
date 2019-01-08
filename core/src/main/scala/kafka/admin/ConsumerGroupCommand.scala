3c97939b973122034b9f2f65c3f5eff657a91a47
alex.dunayevsky <a.a.dunayevsky@gmail.com>
2019-01-08 22:16:23 +0300

    Support both new and old CSV file formats to export/import offsets: * Single group CSV format: "topic,partition,offset" * Multiple group CSV format: "group,topic,partition,offset"

--- core/src/main/scala/kafka/admin/ConsumerGroupCommand.scala
+++ core/src/main/scala/kafka/admin/ConsumerGroupCommand.scala
@@ -18,13 +18,15 @@
 package kafka.admin
 
 import java.text.{ParseException, SimpleDateFormat}
+import java.util
 import java.util.{Date, Properties}
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import javax.xml.datatype.DatatypeFactory
-import joptsimple.OptionSpec
+import joptsimple.{OptionParser, OptionSpec}
+import kafka.admin.ConsumerGroupCommand.{CsvRecordNoGroup, CsvRecordWithGroup, CsvUtils}
 import kafka.utils._
 import org.apache.kafka.clients.admin._
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, OffsetAndMetadata}
@@ -34,11 +36,13 @@ import org.apache.kafka.common.utils.Utils
 import org.apache.kafka.common.{KafkaException, Node, TopicPartition}
 
 import scala.collection.JavaConverters._
-import scala.collection.immutable.TreeMap
 import scala.collection.mutable.ListBuffer
 import scala.collection.{Seq, Set, mutable}
-import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
+import com.fasterxml.jackson.databind.{ObjectReader, ObjectWriter}
+import joptsimple.OptionSpec
+import scala.collection.immutable.TreeMap
+import scala.reflect.ClassTag
 
 object ConsumerGroupCommand extends Logging {
 
