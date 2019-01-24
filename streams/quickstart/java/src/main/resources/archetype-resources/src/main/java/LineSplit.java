e94ea8ee3023428eec1c2ec3139c024a51ac5985
Kristian Aurlien <kristian.aurlien@finn.no>
2019-01-24 14:21:51 +0100

    Specify type of the stream to make the archetype-generated LineSplit-application compile

--- streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
+++ streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
@@ -44,9 +44,9 @@ public class LineSplit {
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        builder.stream("streams-plaintext-input")
-               .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-               .to("streams-linesplit-output");
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+            .to("streams-linesplit-output");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
