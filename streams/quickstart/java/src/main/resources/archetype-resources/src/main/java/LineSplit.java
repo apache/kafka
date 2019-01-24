c18f63f4733677db7c72e65a5bd5818c450803d5
Kristian Aurlien <kristian.aurlien@finn.no>
2019-01-24 16:13:56 +0100

    Fix formatting

--- streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
+++ streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
@@ -45,8 +45,9 @@ public class LineSplit {
         final StreamsBuilder builder = new StreamsBuilder();
 
         KStream<String, String> source = builder.stream("streams-plaintext-input");
+
         source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-            .to("streams-linesplit-output");
+                .to("streams-linesplit-output");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
