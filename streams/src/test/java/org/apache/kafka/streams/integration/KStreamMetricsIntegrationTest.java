0a4e3dfcbaf973394ee35330dd3380e52a90543a
khairy <khaireddine.rezgui@machinestlak.com>
2019-01-29 14:15:56 +0100

    add processor metric test

--- streams/src/test/java/org/apache/kafka/streams/integration/KStreamMetricsIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/KStreamMetricsIntegrationTest.java
@@ -37,7 +37,6 @@ import org.junit.experimental.categories.Category;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
@@ -49,6 +48,25 @@ public class KStreamMetricsIntegrationTest {
     @ClassRule
     public static final EmbeddedKafkaCluster CLUSTER =
         new EmbeddedKafkaCluster(NUM_BROKERS);
+    public static final String PROCESS_LATENCY_AVG = "process-latency-avg";
+    public static final String STREAM_PROCESSOR_NODE_METRICS = "stream-processor-node-metrics";
+    public static final String PROCESS_LATENCY_MAX = "process-latency-max";
+    public static final String PUNCTUATE_LATENCY_AVG = "punctuate-latency-avg";
+    public static final String PUNCTUATE_LATENCY_MAX = "punctuate-latency-max";
+    public static final String CREATE_LATENCY_AVG = "create-latency-avg";
+    public static final String CREATE_LATENCY_MAX = "create-latency-max";
+    public static final String DESTROY_LATENCY_AVG = "destroy-latency-avg";
+    public static final String DESTROY_LATENCY_MAX = "destroy-latency-max";
+    public static final String PROCESS_RATE = "process-rate";
+    public static final String PROCESS_TOTAL = "process-total";
+    public static final String PUNCTUATE_RATE = "punctuate-rate";
+    public static final String PUNCTUATE_TOTAL = "punctuate-total";
+    public static final String CREATE_RATE = "create-rate";
+    public static final String CREATE_TOTAL = "create-total";
+    public static final String DESTROY_RATE = "destroy-rate";
+    public static final String DESTROY_TOTAL = "destroy-total";
+    public static final String FORWARD_TOTAL = "forward-total";
+    public static final String STREAM_STRING = "stream";
 
     private StreamsBuilder builder;
     private Properties streamsConfiguration;
@@ -69,7 +87,6 @@ public class KStreamMetricsIntegrationTest {
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         streamsConfiguration.put(StreamsConfig.METRICS_RECORDING_LEVEL_CONFIG, Sensor.RecordingLevel.DEBUG.name);
-        stream = builder.stream(streamInput, Consumed.with(Serdes.Integer(), Serdes.String()));
     }
 
 
@@ -79,31 +96,56 @@ public class KStreamMetricsIntegrationTest {
 
         Thread.sleep(10000);
 
-        final List<Metric> listMetricAfterStartingApp = new ArrayList<Metric>(kafkaStreams.metrics().values()).stream().filter(m -> m.metricName().group().contains("stream")).collect(Collectors.toList());
-        Assert.assertTrue(listMetricAfterStartingApp.size() > 0);
-        for (final Metric metric : listMetricAfterStartingApp) {
-            Assert.assertTrue(!Objects.isNull(metric.metricValue()));
-        }
+        testProcessorMetric();
 
         closeApplication();
 
         Thread.sleep(10000);
 
-        final List<Metric> listMetricAfterClosingApp = new ArrayList<Metric>(kafkaStreams.metrics().values()).stream().filter(m -> m.metricName().group().contains("stream")).collect(Collectors.toList());
+        final List<Metric> listMetricAfterClosingApp = new ArrayList<Metric>(kafkaStreams.metrics().values()).stream().filter(m -> m.metricName().group().contains(STREAM_STRING)).collect(Collectors.toList());
         Assert.assertEquals(0, listMetricAfterClosingApp.size());
 
+    }
+
+    private void testProcessorMetric() {
+        final List<Metric> listMetricProcessor = new ArrayList<Metric>(kafkaStreams.metrics().values()).stream().filter(m -> m.metricName().group().equals(STREAM_PROCESSOR_NODE_METRICS)).collect(Collectors.toList());
+        testMetricByName(listMetricProcessor, PROCESS_LATENCY_AVG, 3);
+        testMetricByName(listMetricProcessor, PROCESS_LATENCY_MAX, 3);
+        testMetricByName(listMetricProcessor, PUNCTUATE_LATENCY_AVG, 3);
+        testMetricByName(listMetricProcessor, PUNCTUATE_LATENCY_MAX, 3);
+        testMetricByName(listMetricProcessor, CREATE_LATENCY_AVG, 3);
+        testMetricByName(listMetricProcessor, CREATE_LATENCY_MAX, 3);
+        testMetricByName(listMetricProcessor, DESTROY_LATENCY_AVG, 3);
+        testMetricByName(listMetricProcessor, DESTROY_LATENCY_MAX, 3);
+        testMetricByName(listMetricProcessor, PROCESS_RATE, 3);
+        testMetricByName(listMetricProcessor, PROCESS_TOTAL, 3);
+        testMetricByName(listMetricProcessor, PUNCTUATE_RATE, 3);
+        testMetricByName(listMetricProcessor, PUNCTUATE_TOTAL, 3);
+        testMetricByName(listMetricProcessor, CREATE_RATE, 3);
+        testMetricByName(listMetricProcessor, CREATE_TOTAL, 3);
+        testMetricByName(listMetricProcessor, DESTROY_RATE, 3);
+        testMetricByName(listMetricProcessor, DESTROY_TOTAL, 3);
+        testMetricByName(listMetricProcessor, FORWARD_TOTAL, 3);
+    }
+
+    private void testMetricByName(List<Metric> listMetric, String metricName, int numMetric) {
+        List<Metric> metrics = listMetric.stream().filter(m -> m.metricName().name().equals(metricName)).collect(Collectors.toList());
+        Assert.assertEquals(numMetric, metrics.size());
+        for (Metric m : metrics) {
+            Assert.assertNotNull(m.metricValue());
+        }
     }
 
     private void createTopics() throws InterruptedException {
         streamInput = "streamInput";
         metricOutput = "metricOutput";
-        CLUSTER.createTopic(streamInput, 3, 1);
+        CLUSTER.createTopic(streamInput, 1, 1);
         CLUSTER.createTopics(metricOutput);
     }
 
     private void startApplication() {
+        stream = builder.stream(streamInput, Consumed.with(Serdes.Integer(), Serdes.String()));
         stream.to(metricOutput, Produced.with(Serdes.Integer(), Serdes.String()));
-
         kafkaStreams = new KafkaStreams(builder.build(), streamsConfiguration);
         kafkaStreams.start();
     }
