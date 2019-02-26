4fa27ab9fc7447e4e7c7762eca2632d1812119ae
Konstantine Karantasis <konstantine@confluent.io>
2019-02-26 12:39:30 -0800

    KIP-415: Add test for task load balancing

---
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/distributed/ConnectAssignorTest.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.connect.runtime.distributed;
+
+import org.apache.kafka.common.utils.LogContext;
+import org.apache.kafka.connect.util.ConnectorTaskId;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.apache.kafka.connect.runtime.distributed.WorkerCoordinator.WorkerLoad;
+import static org.junit.Assert.assertEquals;
+
+public class ConnectAssignorTest {
+    private static final String LEADER_URL = "leaderUrl:8083";
+
+    private Logger log;
+    private ConnectAssignor assignor;
+
+    @Before
+    public void setup() {
+        LogContext loggerFactory = new LogContext();
+        log = loggerFactory.logger(ConnectAssignor.class);
+        assignor = new ConnectAssignor(loggerFactory);
+    }
+
+    @After
+    public void teardown() {
+    }
+
+    @Test
+    public void testAssignConnectorsWhenBalanced() {
+        int num = 2;
+        List<WorkerLoad> existingAssignment = IntStream.range(0, 3)
+                .mapToObj(i -> workerLoad("worker" + i, i * num, num, i * num, num))
+                .collect(Collectors.toList());
+
+        List<WorkerLoad> expectedAssignment = existingAssignment.stream()
+                .map(wl -> WorkerLoad.copy(wl.worker(), wl.connectors(), wl.tasks()))
+                .collect(Collectors.toList());
+        expectedAssignment.get(0).connectors().addAll(Arrays.asList("connector6", "connector9"));
+        expectedAssignment.get(1).connectors().addAll(Arrays.asList("connector7", "connector10"));
+        expectedAssignment.get(2).connectors().addAll(Arrays.asList("connector8"));
+
+        List<String> newConnectors = newConnectors(6, 11);
+        ConnectAssignor.assignConnectors(existingAssignment, newConnectors, log);
+        assertEquals(expectedAssignment, existingAssignment);
+    }
+
+    @Test
+    public void testAssignTasksWhenBalanced() {
+        int num = 2;
+        List<WorkerLoad> existingAssignment = IntStream.range(0, 3)
+                .mapToObj(i -> workerLoad("worker" + i, i * num, num, i * num, num))
+                .collect(Collectors.toList());
+
+        List<WorkerLoad> expectedAssignment = existingAssignment.stream()
+                .map(wl -> WorkerLoad.copy(wl.worker(), wl.connectors(), wl.tasks()))
+                .collect(Collectors.toList());
+
+        expectedAssignment.get(0).connectors().addAll(Arrays.asList("connector6", "connector9"));
+        expectedAssignment.get(1).connectors().addAll(Arrays.asList("connector7", "connector10"));
+        expectedAssignment.get(2).connectors().addAll(Arrays.asList("connector8"));
+
+        expectedAssignment.get(0).tasks().addAll(Arrays.asList(new ConnectorTaskId("task", 6), new ConnectorTaskId("task", 9)));
+        expectedAssignment.get(1).tasks().addAll(Arrays.asList(new ConnectorTaskId("task", 7), new ConnectorTaskId("task", 10)));
+        expectedAssignment.get(2).tasks().addAll(Arrays.asList(new ConnectorTaskId("task", 8)));
+
+        List<String> newConnectors = newConnectors(6, 11);
+        ConnectAssignor.assignConnectors(existingAssignment, newConnectors, log);
+        List<ConnectorTaskId> newTasks = newTasks(6, 11);
+        ConnectAssignor.assignTasks(existingAssignment, newTasks, log);
+        assertEquals(expectedAssignment, existingAssignment);
+    }
+
+    @Test
+    public void testAssignConnectorsWhenImbalanced() {
+        int num = 2;
+        IntUnaryOperator arithmeticProgression = i -> i * ((i - 1) + 2 * num) / 2;
+        List<WorkerLoad> existingAssignment = IntStream.range(0, 3)
+                .mapToObj(i -> workerLoad("worker" + i,
+                        arithmeticProgression.applyAsInt(i), num + i,
+                        arithmeticProgression.applyAsInt(i), num + i))
+                .collect(Collectors.toList());
+        existingAssignment.add(emptyWorkerLoad("worker4"));
+
+        List<String> newConnectors = newConnectors(9, 24);
+        List<ConnectorTaskId> newTasks = newTasks(9, 24);
+        ConnectAssignor.assignConnectors(existingAssignment, newConnectors, log);
+        ConnectAssignor.assignTasks(existingAssignment, newTasks, log);
+        for (WorkerLoad worker : existingAssignment) {
+            assertEquals(6, worker.connectorsSize());
+            assertEquals(6, worker.tasksSize());
+        }
+    }
+
+    private WorkerLoad emptyWorkerLoad(String worker) {
+        return WorkerLoad.embed(worker, new ArrayList<>(), new ArrayList<>());
+    }
+
+    private WorkerLoad workerLoad(String worker, int connectorStart, int connectorNum,
+                                  int taskStart, int taskNum) {
+        return WorkerLoad.embed(worker,
+                newConnectors(connectorStart, connectorStart + connectorNum),
+                newTasks(taskStart, taskStart + taskNum));
+    }
+
+    private static List<String> newConnectors(int start, int end) {
+        return IntStream.range(start, end)
+                .mapToObj(i -> "connector" + i)
+                .collect(Collectors.toList());
+    }
+
+    private static List<ConnectorTaskId> newTasks(int start, int end) {
+        return IntStream.range(start, end)
+                .mapToObj(i -> new ConnectorTaskId("task", i))
+                .collect(Collectors.toList());
+    }
+
+}
