20f626d5e58cf324be4d552d74d9292c92cc6212
Manikumar Reddy <manikumar.reddy@gmail.com>
2019-03-04 14:10:52 +0530

    update comment

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -71,7 +71,8 @@ object TestUtils extends Logging {
   /* 0 gives a random port; you can then retrieve the assigned port from the Socket object. */
   val RandomPort = 0
 
-  /* Incorrect broker port which can used by kafka clients in tests. */
+  /* Incorrect broker port which can used by kafka clients in tests. This port should not be used
+   by any other service and hence we use a reserved port(225) */
   val incorrectBrokerPort = 225
 
   /** Port to use for unit tests that mock/don't require a real ZK server. */
