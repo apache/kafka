ae618528442157bd7e966407a1f9bd828f2be994
John Roesler <john@confluent.io>
2019-03-13 13:10:39 -0500

    checkstyle

--- streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedKeyValueBufferTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedKeyValueBufferTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.streams.state.internals;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -45,14 +61,14 @@ public class TimeOrderedKeyValueBufferTest<B extends TimeOrderedKeyValueBuffer> 
         return asList(
             new Object[] {
                 InMemoryTimeOrderedKeyValueBuffer.class.getSimpleName() + "_" + new Random().nextInt(Integer.MAX_VALUE),
-                (Function<String, InMemoryTimeOrderedKeyValueBuffer>) (name) ->
+                (Function<String, InMemoryTimeOrderedKeyValueBuffer>) name ->
                     (InMemoryTimeOrderedKeyValueBuffer) new InMemoryTimeOrderedKeyValueBuffer
                         .Builder(name)
                         .withLoggingDisabled().build()
             },
             new Object[] {
                 RocksDBTimeOrderedKeyValueBuffer.class.getSimpleName() + "_" + new Random().nextInt(Integer.MAX_VALUE),
-                (Function<String, RocksDBTimeOrderedKeyValueBuffer>) (name) ->
+                (Function<String, RocksDBTimeOrderedKeyValueBuffer>) name ->
                     (RocksDBTimeOrderedKeyValueBuffer) new RocksDBTimeOrderedKeyValueBuffer
                         .Builder(name, new StrictBufferConfigImpl(-1L,
                                                                   32_000L,
@@ -119,7 +135,7 @@ public class TimeOrderedKeyValueBufferTest<B extends TimeOrderedKeyValueBuffer> 
         buffer.init(context, buffer);
         buffer.put(0, getBytes("asdf"), getRecord("qwer"));
         assertThat(buffer.numRecords(), is(1));
-        buffer.evictWhile(() -> true, kv -> {});
+        buffer.evictWhile(() -> true, kv -> { });
         assertThat(buffer.numRecords(), is(0));
         cleanup(context, buffer);
     }
@@ -198,7 +214,7 @@ public class TimeOrderedKeyValueBufferTest<B extends TimeOrderedKeyValueBuffer> 
         assertThat(buffer.minTimestamp(), is(0L));
 
         final AtomicInteger callbackCount = new AtomicInteger(0);
-        buffer.evictWhile(() -> true, (kv) -> {
+        buffer.evictWhile(() -> true, kv -> {
             switch (callbackCount.incrementAndGet()) {
                 case 1: {
                     assertThat(new String(kv.key.get(), UTF_8), is("asdf"));
@@ -261,7 +277,7 @@ public class TimeOrderedKeyValueBufferTest<B extends TimeOrderedKeyValueBuffer> 
         ));
 
         // the buffer metadata is correct after adding both records
-        
+
         assertThat(buffer.numRecords(), is(2));
         assertThat(buffer.bufferSize(), is(83L));
         assertThat(buffer.minTimestamp(), is(0L));
