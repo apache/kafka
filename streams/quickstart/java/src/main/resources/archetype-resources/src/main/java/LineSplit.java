94e125201db6d022a85a69e012e49565904a5caa
Kristian Aurlien <kristian.aurlien@finn.no>
2019-03-15 09:54:50 +0100

    Specify stream types in builder

--- streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
+++ streams/quickstart/java/src/main/resources/archetype-resources/src/main/java/LineSplit.java
@@ -44,10 +44,9 @@ public class LineSplit {
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<String, String> source = builder.stream("streams-plaintext-input");
-
-        source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-                .to("streams-linesplit-output");
+        builder.<String, String>stream("streams-plaintext-input")
+            .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+            .to("streams-linesplit-output");
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
