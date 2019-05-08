bfa9d3b1ceb2ef5ecf78911aca449a523d430c7b
chenlin <chenlin@kuaishou.com>
2019-05-08 11:03:09 +0800

    Kafka smooth expansion fix5

--- clients/src/main/java/org/apache/kafka/common/protocol/Errors.java
+++ clients/src/main/java/org/apache/kafka/common/protocol/Errors.java
@@ -66,6 +66,7 @@ import org.apache.kafka.common.errors.UnsupportedVersionException;
 import org.apache.kafka.common.errors.UnknownMemberIdException;
 import org.apache.kafka.common.errors.UnknownServerException;
 import org.apache.kafka.common.errors.UnknownTopicOrPartitionException;
+import org.apache.kafka.common.errors.GetStartOffsetRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -167,8 +168,8 @@ public enum Errors {
     UNSUPPORTED_FOR_MESSAGE_FORMAT(43,
         new UnsupportedForMessageFormatException("The message format version on the broker does not support the request.")),
     POLICY_VIOLATION(44, new PolicyViolationException("Request parameters do not satisfy the configured policy.")),
-    OFFSET_LEO(45, null),
-    OFFSET_HW(46, null);
+    OFFSET_LEO(45, new GetStartOffsetRequestException("Follower log end offset is start offset")),
+    OFFSET_HW(46, new GetStartOffsetRequestException("HW is start offset"));
 
     private static final Logger log = LoggerFactory.getLogger(Errors.class);
 
