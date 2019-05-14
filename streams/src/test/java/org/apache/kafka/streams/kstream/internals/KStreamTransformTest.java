b9f3849483616d3d7163bbdc64288135b3ec621a
John Roesler <vvcephei@users.noreply.github.com>
2019-05-14 15:04:33 -0500

    Update streams/src/test/java/org/apache/kafka/streams/kstream/internals/KStreamTransformTest.java

--- streams/src/test/java/org/apache/kafka/streams/kstream/internals/KStreamTransformTest.java
+++ streams/src/test/java/org/apache/kafka/streams/kstream/internals/KStreamTransformTest.java
@@ -71,7 +71,7 @@ public class KStreamTransformTest {
                 }
 
                 @Override
-                public void close() {
+                public void close() {}
                 }
             };
 
