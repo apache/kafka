4b636d05c955d50845337ae36a624e0c9868e660
John Roesler <john@confluent.io>
2019-05-29 15:24:25 -0500

    unused import

--- streams/src/test/java/org/apache/kafka/streams/state/internals/MaybeTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/MaybeTest.java
@@ -21,7 +21,6 @@ import org.junit.Test;
 import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.fail;
