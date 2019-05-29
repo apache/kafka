c46804e88d30ec74bcdde97c46733328fdb9952e
John Roesler <john@confluent.io>
2019-05-29 15:23:49 -0500

    license

--- streams/src/test/java/org/apache/kafka/streams/state/internals/MaybeTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/MaybeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.streams.state.internals;
 
 import org.junit.Test;
@@ -56,7 +72,7 @@ public class MaybeTest {
     public void shouldUpholdHashCodeCorrectness() {
         // This specifies the current implementation, which is simpler to write than an exhaustive test.
         // As long as this implementation doesn't change, then the equals/hashcode contract is upheld.
-        
+
         assertThat(Maybe.undefined().hashCode(), is(-1));
         assertThat(Maybe.defined(null).hashCode(), is(0));
         assertThat(Maybe.defined("a").hashCode(), is("a".hashCode()));
