4e2c9892f752e47aa5072127a58a86c85f628e9a
Hai-Dang Dam <hai-dang.dam@Hai-Dang-Dams-MBP15.local>
2019-06-03 10:50:47 -0700

    Minor change in String regex for task request pattern

--- connect/basic-auth-extension/src/main/java/org/apache/kafka/connect/rest/basic/auth/extension/JaasBasicAuthFilter.java
+++ connect/basic-auth-extension/src/main/java/org/apache/kafka/connect/rest/basic/auth/extension/JaasBasicAuthFilter.java
@@ -39,7 +39,7 @@ import javax.ws.rs.core.Response;
 public class JaasBasicAuthFilter implements ContainerRequestFilter {
     private static final String CONNECT_LOGIN_MODULE = "KafkaConnect";
     static final String AUTHORIZATION = "Authorization";
-    private static final String TASK_REQUEST_PATTERN = "\\/?connectors\\/([^\\/]*)\\/tasks\\/?"; //\/?connectors\/([^\/]*)\/tasks\/?
+    private static final String TASK_REQUEST_PATTERN = "\\/?connectors\\/([^\\/]+)\\/tasks\\/?";
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         try {
