e3201ff6ec8bf69e87d8ba60d5e0c15146bfa3c5
David Arthur <mumrah@gmail.com>
2019-06-11 08:48:27 -0400

    Cleanup

--- tools/src/main/java/org/apache/kafka/trogdor/fault/NetworkDegradeFaultWorker.java
+++ tools/src/main/java/org/apache/kafka/trogdor/fault/NetworkDegradeFaultWorker.java
@@ -34,7 +34,7 @@ import java.util.Map;
  */
 public class NetworkDegradeFaultWorker implements TaskWorker {
 
-    private static final Logger log = LoggerFactory.getLogger(NetworkPartitionFaultWorker.class);
+    private static final Logger log = LoggerFactory.getLogger(NetworkDegradeFaultWorker.class);
 
     private final String id;
     private final Map<String, NetworkDegradeFaultSpec.NodeDegradeSpec> nodeSpecs;
@@ -51,30 +51,27 @@ public class NetworkDegradeFaultWorker implements TaskWorker {
         this.status = status;
         this.status.update(new TextNode("enabling traffic control " + id));
         Node curNode = platform.curNode();
-        for (Map.Entry<String, NetworkDegradeFaultSpec.NodeDegradeSpec> entry : nodeSpecs.entrySet()) {
-            if (entry.getKey().equals(curNode.name())) {
-                enableTrafficControl(platform, entry.getValue().getNetworkDevice(), entry.getValue().getLatency());
-            }
+        NetworkDegradeFaultSpec.NodeDegradeSpec nodeSpec = nodeSpecs.get(curNode.name());
+        if (nodeSpec != null) {
+            enableTrafficControl(platform, nodeSpec.getNetworkDevice(), nodeSpec.getLatency());
         }
         this.status.update(new TextNode("enabled traffic control " + id));
     }
-
 
     @Override
     public void stop(Platform platform) throws Exception {
         log.info("Deactivating NetworkDegradeFaultWorker {}.", id);
         this.status.update(new TextNode("disabling traffic control " + id));
         Node curNode = platform.curNode();
-        for (Map.Entry<String, NetworkDegradeFaultSpec.NodeDegradeSpec> entry : nodeSpecs.entrySet()) {
-            if (entry.getKey().equals(curNode.name())) {
-                disableTrafficControl(platform, entry.getValue().getNetworkDevice());
-            }
+        NetworkDegradeFaultSpec.NodeDegradeSpec nodeSpec = nodeSpecs.get(curNode.name());
+        if (nodeSpec != null) {
+            disableTrafficControl(platform, nodeSpec.getNetworkDevice());
         }
         this.status.update(new TextNode("disabled traffic control " + id));
     }
 
     private void enableTrafficControl(Platform platform, String networkDevice, int delayMs) throws IOException {
-        int deviationMs = Math.max(1, (int)Math.sqrt(delayMs));
+        int deviationMs = Math.max(1, (int) Math.sqrt(delayMs));
         platform.runCommand(new String[] {
             "sudo", "tc", "qdisc", "add", "dev", networkDevice, "root", "netem",
             "delay", String.format("%dms", delayMs), String.format("%dms", deviationMs), "distribution", "normal"
@@ -83,7 +80,7 @@ public class NetworkDegradeFaultWorker implements TaskWorker {
 
     private void disableTrafficControl(Platform platform, String networkDevice) throws IOException {
         platform.runCommand(new String[] {
-                "sudo", "tc", "qdisc", "del", "dev", networkDevice, "root"
+            "sudo", "tc", "qdisc", "del", "dev", networkDevice, "root"
         });
     }
 }
