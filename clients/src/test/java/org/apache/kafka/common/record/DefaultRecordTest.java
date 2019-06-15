8014557a34d3fb425ba41edba603730fc0c06733
Guozhang Wang <wangguoz@gmail.com>
2019-06-14 19:13:04 -0700

    minor fix

--- clients/src/test/java/org/apache/kafka/common/record/DefaultRecordTest.java
+++ clients/src/test/java/org/apache/kafka/common/record/DefaultRecordTest.java
@@ -270,7 +270,7 @@ public class DefaultRecordTest {
         DefaultRecord.readPartiallyFrom(inputStream, skipBuffer, 0L, 0L, RecordBatch.NO_SEQUENCE, null);
     }
 
-    @Test(expected = InvalidRecordException.class)
+    @Test(expected = StringIndexOutOfBoundsException.class)
     public void testInvalidHeaderKey() {
         byte attributes = 0;
         long timestampDelta = 2;
