9c79a969c89db2a22d00136d12758a7e9d6afd52
abbccdda <boyang@confluent.io>
2019-06-17 13:35:52 -0700

    fix optional import

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
@@ -64,6 +64,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
