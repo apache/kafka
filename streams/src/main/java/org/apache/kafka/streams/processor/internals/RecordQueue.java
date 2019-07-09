ac606c5cdecb873efeef002501b150d686b039fc
ableegoldman <sophie@confluent.io>
2019-07-08 18:50:16 -0700

    move method

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/RecordQueue.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/RecordQueue.java
@@ -113,10 +113,9 @@ public class RecordQueue {
      */
     public StampedRecord poll() {
         final StampedRecord recordToReturn = headRecord;
+        headRecord = null;
 
         updateHead();
-
-        headRecord = null;
 
         return recordToReturn;
     }
