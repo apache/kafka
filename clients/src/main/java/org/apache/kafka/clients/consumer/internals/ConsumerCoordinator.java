c6ed3b7276332b19d756150aadef4a47a79c11da
ableegoldman <sophie@confluent.io>
2019-07-15 17:38:53 -0700

    add method for List copy

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
@@ -202,7 +202,7 @@ public final class ConsumerCoordinator extends AbstractCoordinator {
         JoinGroupRequestData.JoinGroupRequestProtocolCollection protocolSet = new JoinGroupRequestData.JoinGroupRequestProtocolCollection();
 
         for (PartitionAssignor assignor : assignors) {
-            Subscription subscription = assignor.subscription(joinedSubscription, new ArrayList<>(subscriptions.assignedPartitions()));
+            Subscription subscription = assignor.subscription(joinedSubscription, subscriptions.assignedPartitionList());
             ByteBuffer metadata = ConsumerProtocol.serializeSubscription(subscription);
 
             protocolSet.add(new JoinGroupRequestData.JoinGroupRequestProtocol()
