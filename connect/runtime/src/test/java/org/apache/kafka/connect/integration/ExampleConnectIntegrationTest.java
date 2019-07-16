6b8a21dbf561398ca4ef37d559d7fad847639e3b
Jason Gustafson <jason@confluent.io>
2019-07-16 08:30:49 -0700

    Increase timeout to 30s

--- connect/runtime/src/test/java/org/apache/kafka/connect/integration/ExampleConnectIntegrationTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/integration/ExampleConnectIntegrationTest.java
@@ -55,7 +55,7 @@ public class ExampleConnectIntegrationTest {
 
     private static final int NUM_RECORDS_PRODUCED = 2000;
     private static final int NUM_TOPIC_PARTITIONS = 3;
-    private static final int RECORD_TRANSFER_DURATION_MS = 15000;
+    private static final int RECORD_TRANSFER_DURATION_MS = 30000;
     private static final int CONNECTOR_SETUP_DURATION_MS = 15000;
     private static final int NUM_TASKS = 3;
     private static final int NUM_WORKERS = 3;
