457d824d1715b67acd6ffbfc75bb335fb1e19b93
Levani Kokhreidze <levani.codes@gmail.com>
2019-08-03 14:05:38 +0300

    KAFKA-8611 / fix Repartitioned class checkstyle violations

--- streams/src/main/java/org/apache/kafka/streams/kstream/Repartitioned.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/Repartitioned.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.streams.kstream;
 
 import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
@@ -26,11 +42,11 @@ public class Repartitioned<K, V> implements NamedOperation<Repartitioned<K, V>> 
 
     protected final StreamPartitioner<? super K, ? super V> partitioner;
 
-    private Repartitioned(String name,
-                          Serde<K> keySerde,
-                          Serde<V> valueSerde,
-                          Integer numberOfPartitions,
-                          StreamPartitioner<? super K, ? super V> partitioner) {
+    private Repartitioned(final String name,
+                          final Serde<K> keySerde,
+                          final Serde<V> valueSerde,
+                          final Integer numberOfPartitions,
+                          final StreamPartitioner<? super K, ? super V> partitioner) {
         this.name = name;
         this.keySerde = keySerde;
         this.valueSerde = valueSerde;
