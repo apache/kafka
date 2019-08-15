8f24311d2247ce7b66f525ef6e0fdaaa6ae176ad
John Roesler <john@confluent.io>
2019-08-15 00:38:30 -0500

    layered store benchmarks

--- jmh-benchmarks/src/main/java/org/apache/kafka/streams/processor/internals/StreamsStoreBenchmark.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/streams/processor/internals/StreamsStoreBenchmark.java
@@ -22,6 +22,7 @@ import org.apache.kafka.common.metrics.Metrics;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.common.utils.LogContext;
+import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.internals.MaterializedInternal;
@@ -29,8 +30,7 @@ import org.apache.kafka.streams.kstream.internals.TimestampedKeyValueStoreMateri
 import org.apache.kafka.streams.processor.TaskId;
 import org.apache.kafka.streams.processor.internals.metrics.StreamsMetricsImpl;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.StoreBuilder;
-import org.apache.kafka.streams.state.TimestampedKeyValueStore;
+import org.apache.kafka.streams.state.Stores;
 import org.apache.kafka.streams.state.ValueAndTimestamp;
 import org.apache.kafka.streams.state.internals.ThreadCache;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -40,7 +40,8 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import static org.apache.kafka.common.utils.Utils.mkEntry;
@@ -48,7 +49,7 @@ import static org.apache.kafka.common.utils.Utils.mkMap;
 
 /**
  * This is a simple example of a JMH benchmark.
- *
+ * <p>
  * The sample code provided by the JMH project is a great place to start learning how to write correct benchmarks:
  * http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
  */
@@ -70,7 +71,10 @@ public class StreamsStoreBenchmark {
 
     private int counter;
 
-    private KeyValueStore<String, ValueAndTimestamp<String>> store;
+    private KeyValueStore<String, ValueAndTimestamp<String>> persistentTimestampedStore;
+    private KeyValueStore<String, ValueAndTimestamp<String>> persistentTimestampedStoreNoCache;
+    private KeyValueStore<String, ValueAndTimestamp<String>> inMemoryTimestampedStore;
+    private KeyValueStore<String, ValueAndTimestamp<String>> inMemoryTimestampedStoreNoCache;
 
     @Setup(Level.Trial)
     public void setUp() {
@@ -79,11 +83,55 @@ public class StreamsStoreBenchmark {
             values[i] = ValueAndTimestamp.make(VALUE + i, i);
         }
 
-        final TimestampedKeyValueStoreMaterializer<String, String> materializer =
+        persistentTimestampedStore =
             new TimestampedKeyValueStoreMaterializer<>(
-                new MaterializedInternal<>(Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("store").withKeySerde(Serdes.String()).withValueSerde(Serdes.String())));
-        final StoreBuilder<TimestampedKeyValueStore<String, String>> builder = materializer.materialize();
-        store = builder.build();
+                new MaterializedInternal<>(
+                    Materialized
+                        .<String, String, KeyValueStore<Bytes, byte[]>>as("rocks")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String())
+                        .withLoggingDisabled()
+                )
+            ).materialize()
+             .build();
+
+        persistentTimestampedStoreNoCache =
+            new TimestampedKeyValueStoreMaterializer<>(
+                new MaterializedInternal<>(
+                    Materialized
+                        .<String, String, KeyValueStore<Bytes, byte[]>>as("rocksnc")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String())
+                        .withCachingDisabled()
+                        .withLoggingDisabled()
+                )
+            ).materialize()
+             .build();
+
+        inMemoryTimestampedStore =
+            new TimestampedKeyValueStoreMaterializer<>(
+                new MaterializedInternal<>(
+                    Materialized
+                        .<String, String>as(Stores.inMemoryKeyValueStore("mem"))
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String())
+                        .withLoggingDisabled()
+                )
+            ).materialize()
+             .build();
+
+        inMemoryTimestampedStoreNoCache =
+            new TimestampedKeyValueStoreMaterializer<>(
+                new MaterializedInternal<>(
+                    Materialized
+                        .<String, String>as(Stores.inMemoryKeyValueStore("memnc"))
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String())
+                        .withCachingDisabled()
+                        .withLoggingDisabled()
+                )
+            ).materialize()
+             .build();
 
         final StreamsMetricsImpl metrics = new StreamsMetricsImpl(new Metrics(), "test-metrics");
         final ThreadCache cache = new ThreadCache(new LogContext("testCache "), 1_000_000_000, metrics);
@@ -91,34 +139,89 @@ public class StreamsStoreBenchmark {
             mkEntry(StreamsConfig.APPLICATION_ID_CONFIG, "test"),
             mkEntry(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "test")
         ));
+        final TaskId id = new TaskId(0, 0);
+        final ProcessorStateManager stateMgr;
+        try {
+            stateMgr = new ProcessorStateManager(
+                id,
+                Collections.emptySet(),
+                false,
+                new StateDirectory(config, Time.SYSTEM, true),
+                Collections.emptyMap(),
+                null,
+                false,
+                new LogContext("jmh")
+            );
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
         final ProcessorContextImpl context = new ProcessorContextImpl(
-            new TaskId(0, 0),
+            id,
             null,
             config,
             null,
-            new ProcessorStateManager(),
+            stateMgr,
             metrics,
             cache
         );
+
         context.setRecordContext(new ProcessorRecordContext(0, 0, 0, "topic", new RecordHeaders()));
 
-        this.store.init(context, null);
+        persistentTimestampedStore.init(context, persistentTimestampedStore);
+        persistentTimestampedStoreNoCache.init(context, persistentTimestampedStoreNoCache);
+        inMemoryTimestampedStore.init(context, inMemoryTimestampedStore);
+        inMemoryTimestampedStoreNoCache.init(context, inMemoryTimestampedStoreNoCache);
     }
 
     @Benchmark
-    public ValueAndTimestamp<String> testCachePerformance() {
+    public ValueAndTimestamp<String> testPersistentPutGetPerformance() {
         counter++;
         final int index = counter % DISTINCT_KEYS;
         final String key = keys[index];
-        store.put(key, values[index]);
+        persistentTimestampedStore.put(key, values[index]);
 
-        return store.get(key);
+        return persistentTimestampedStore.get(key);
+    }
+
+
+    @Benchmark
+    public ValueAndTimestamp<String> testPersistentPutGetPerformanceNoCache() {
+        counter++;
+        final int index = counter % DISTINCT_KEYS;
+        final String key = keys[index];
+        persistentTimestampedStoreNoCache.put(key, values[index]);
+
+        return persistentTimestampedStoreNoCache.get(key);
+    }
+
+    @Benchmark
+    public ValueAndTimestamp<String> testMemoryPutGetPerformance() {
+        counter++;
+        final int index = counter % DISTINCT_KEYS;
+        final String key = keys[index];
+        inMemoryTimestampedStore.put(key, values[index]);
+
+        return inMemoryTimestampedStore.get(key);
+    }
+
+    @Benchmark
+    public ValueAndTimestamp<String> testMemoryPutGetPerformanceNoCache() {
+        counter++;
+        final int index = counter % DISTINCT_KEYS;
+        final String key = keys[index];
+        inMemoryTimestampedStoreNoCache.put(key, values[index]);
+
+        return inMemoryTimestampedStoreNoCache.get(key);
     }
 
     public static void main(final String[] args) {
         final StreamsStoreBenchmark streamsCacheBenchmark = new StreamsStoreBenchmark();
         streamsCacheBenchmark.setUp();
-        System.out.println(streamsCacheBenchmark.testCachePerformance());
+        System.out.println(streamsCacheBenchmark.testPersistentPutGetPerformance());
+        System.out.println(streamsCacheBenchmark.testPersistentPutGetPerformanceNoCache());
+        System.out.println(streamsCacheBenchmark.testMemoryPutGetPerformance());
+        System.out.println(streamsCacheBenchmark.testMemoryPutGetPerformanceNoCache());
     }
 
 }
