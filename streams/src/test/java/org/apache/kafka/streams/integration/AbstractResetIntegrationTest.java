52e65d804f7a9e501a8c50955669dc8e921ef966
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2019-09-13 14:41:46 -0500

    KAFKA-8894: Bump streams test topic deletion assertion timeout from 30s to 60s We have seen rare flakiness in this assertion - all of streams' internal topics would not get deleted within the 30 second window. Increasing to 60 seconds should reduce the occurrence.

--- streams/src/test/java/org/apache/kafka/streams/integration/AbstractResetIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/AbstractResetIntegrationTest.java
@@ -583,10 +583,10 @@ public abstract class AbstractResetIntegrationTest {
     private void assertInternalTopicsGotDeleted(final String intermediateUserTopic) throws Exception {
         // do not use list topics request, but read from the embedded cluster's zookeeper path directly to confirm
         if (intermediateUserTopic != null) {
-            cluster.waitForRemainingTopics(30000, INPUT_TOPIC, OUTPUT_TOPIC, OUTPUT_TOPIC_2, OUTPUT_TOPIC_2_RERUN,
+            cluster.waitForRemainingTopics(60000, INPUT_TOPIC, OUTPUT_TOPIC, OUTPUT_TOPIC_2, OUTPUT_TOPIC_2_RERUN,
                     Topic.GROUP_METADATA_TOPIC_NAME, intermediateUserTopic);
         } else {
-            cluster.waitForRemainingTopics(30000, INPUT_TOPIC, OUTPUT_TOPIC, OUTPUT_TOPIC_2, OUTPUT_TOPIC_2_RERUN,
+            cluster.waitForRemainingTopics(60000, INPUT_TOPIC, OUTPUT_TOPIC, OUTPUT_TOPIC_2, OUTPUT_TOPIC_2_RERUN,
                     Topic.GROUP_METADATA_TOPIC_NAME);
         }
     }
