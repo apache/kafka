e6e353c0e5d2b29e55fe9ae69df52a33967e6403
Adam Bellemare <adam.bellemare@wishabi.com>
2019-09-19 09:54:19 -0400

    Fixed some :clients:checkstyleTest failures

--- clients/src/test/java/org/apache/kafka/common/utils/Murmur3Test.java
+++ clients/src/test/java/org/apache/kafka/common/utils/Murmur3Test.java
@@ -20,11 +20,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Random;
 
 /**
  * This class was taken from Hive org.apache.hive.common.util;
@@ -48,20 +44,20 @@ public class Murmur3Test {
 
         int seed = 123;
         for (Map.Entry c : cases.entrySet()) {
-            byte[] b = (byte[])c.getKey();
-            assertEquals( c.getValue(), Murmur3.hash32(b, b.length, seed));
+            byte[] b = (byte[]) c.getKey();
+            assertEquals(c.getValue(), Murmur3.hash32(b, b.length, seed));
         }
     }
 
     @Test
     public void testMurmur3_128() {
         Map<byte[], long[]> cases = new java.util.HashMap<>();
-        cases.put("21".getBytes(), new long[]{5857341059704281894L,-5288187638297930763L});
-        cases.put("foobar".getBytes(), new long[]{-351361463397418609L,8959716011862540668L});
-        cases.put("a-little-bit-long-string".getBytes(), new long[]{8836256500583638442L,-198172363548498523L});
-        cases.put("a-little-bit-longer-string".getBytes(), new long[]{1838346159335108511L,8794688210320490705L});
-        cases.put("lkjh234lh9fiuh90y23oiuhsafujhadof229phr9h19h89h8".getBytes(), new long[]{-4024021876037397259L,-1482317706335141238L});
-        cases.put(new byte[]{'a', 'b', 'c'}, new long[]{1489494923063836066L,-5440978547625122829L});
+        cases.put("21".getBytes(), new long[]{5857341059704281894L, -5288187638297930763L});
+        cases.put("foobar".getBytes(), new long[]{-351361463397418609L, 8959716011862540668L});
+        cases.put("a-little-bit-long-string".getBytes(), new long[]{8836256500583638442L, -198172363548498523L});
+        cases.put("a-little-bit-longer-string".getBytes(), new long[]{1838346159335108511L, 8794688210320490705L});
+        cases.put("lkjh234lh9fiuh90y23oiuhsafujhadof229phr9h19h89h8".getBytes(), new long[]{-4024021876037397259L, -1482317706335141238L});
+        cases.put(new byte[]{'a', 'b', 'c'}, new long[]{1489494923063836066L, -5440978547625122829L});
 
         int seed = 123;
 
