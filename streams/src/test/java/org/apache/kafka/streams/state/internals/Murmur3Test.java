a0470726c44a26a1cbd0cb7f6c29b4e3e28a3af1
Guozhang Wang <wangguoz@gmail.com>
2019-09-19 16:38:18 -0700

    MINOR: Move Murmur3 to Streams

--- streams/src/test/java/org/apache/kafka/streams/state/internals/Murmur3Test.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/Murmur3Test.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.kafka.common.utils;
+package org.apache.kafka.streams.state.internals;
 
 import static org.junit.Assert.*;
 
