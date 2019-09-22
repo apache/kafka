6f39cbda286d5a704e97c5c93c372a92c7aa89f1
abbccdda <boyang@confluent.io>
2019-09-21 22:11:58 -0700

    add logic to check group dead

--- core/src/main/scala/kafka/coordinator/group/GroupCoordinator.scala
+++ core/src/main/scala/kafka/coordinator/group/GroupCoordinator.scala
@@ -1023,18 +1023,29 @@ class GroupCoordinator(val brokerId: Int,
 
   def tryCompleteHeartbeat(group: GroupMetadata, memberId: String, isPending: Boolean, heartbeatDeadline: Long, forceComplete: () => Boolean) = {
     group.inLock {
-      if (isPending) {
-        // complete the heartbeat if the member has joined the group
+      // The group has been unloaded and invalid, we should complete the heartbeat.
+      if (group.is(Dead)) {
+        forceComplete()
+      } else if (isPending) {
         if (group.has(memberId)) {
           forceComplete()
         } else false
-      }
-      else {
-        val member = group.get(memberId)
-        if (member.shouldKeepAlive(heartbeatDeadline) || member.isLeaving) {
+      } else {
+        if (shouldCompleteNonPendingHeartbeat(group, memberId, heartbeatDeadline)) {
           forceComplete()
         } else false
       }
+    }
+  }
+
+  def shouldCompleteNonPendingHeartbeat(group: GroupMetadata, memberId: String, heartbeatDeadline: Long): Boolean = {
+    if (group.has(memberId)) {
+      val member = group.get(memberId)
+      member.shouldKeepAlive(heartbeatDeadline) || member.isLeaving
+    } else {
+      error(s"Member id $memberId was not found in ${group.groupId} during heartbeat expiration, " +
+        s"this could be potentially related to KAFKA-8896")
+      false
     }
   }
 
