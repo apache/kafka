3e135da80bf0108e060599ca4d9aefacb87982cf
Satish Duggana <satishd@uber.com>
2019-09-23 21:49:56 +0530

    MINOR Fixed checkstyle issues in remote-storage-managers:hdfs module

--- remote-storage-managers/hdfs/src/main/java/org/apache/kafka/rsm/hdfs/HDFSRemoteStorageManager.java
+++ remote-storage-managers/hdfs/src/main/java/org/apache/kafka/rsm/hdfs/HDFSRemoteStorageManager.java
@@ -37,7 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
