0e8996680607f22f9227c9a98a726dd5ce7aa8e1
Ismael Juma <ismael@juma.me.uk>
2019-09-25 10:08:14 -0700

    Fix checkstyle and copyright check

--- clients/src/main/java/org/apache/kafka/common/requests/UpdateMetadataRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/UpdateMetadataRequest.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.singletonList;
 
