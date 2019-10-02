d3bf3cd3d16945c17d45cf3b2bc4ef0d699cf418
A. Sophie Blee-Goldman <sophie@confluent.io>
2019-10-02 16:01:54 -0700

    KAFKA-8649: send latest commonly supported version in assignment (#7426)
    PR 7423 but targeted at 2.2.
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>

--- streams/src/test/java/org/apache/kafka/streams/tests/StreamsUpgradeTest.java
+++ streams/src/test/java/org/apache/kafka/streams/tests/StreamsUpgradeTest.java
@@ -163,6 +163,11 @@ public class StreamsUpgradeTest {
             final AssignmentInfo info = AssignmentInfo.decode(
                 assignment.userData().putInt(0, AssignmentInfo.LATEST_SUPPORTED_VERSION));
 
+            if (super.maybeUpdateSubscriptionVersion(usedVersion, info.commonlySupportedVersion())) {
+                setAssignmentErrorCode(Error.VERSION_PROBING.code());
+                return;
+            }
+
             final List<TopicPartition> partitions = new ArrayList<>(assignment.partitions());
             partitions.sort(PARTITION_COMPARATOR);
 
@@ -292,6 +297,7 @@ public class StreamsUpgradeTest {
         private FutureAssignmentInfo(final boolean bumpUsedVersion,
                                      final boolean bumpSupportedVersion,
                                      final ByteBuffer bytes) {
+            super(LATEST_SUPPORTED_VERSION, LATEST_SUPPORTED_VERSION);
             this.bumpUsedVersion = bumpUsedVersion;
             this.bumpSupportedVersion = bumpSupportedVersion;
             originalUserMetadata = bytes;
