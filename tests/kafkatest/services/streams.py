23ef3b5bd7eef11b0cf5942b4195d3202753b879
abbccdda <boyang@confluent.io>
2019-10-22 12:24:18 -0700

    debug level change

--- tests/kafkatest/services/streams.py
+++ tests/kafkatest/services/streams.py
@@ -205,7 +205,7 @@ class StreamsTestBaseService(KafkaPathResolverMixin, JmxMixin, Service):
                      'user_test_args2': user_test_args2,
                      'user_test_args3': user_test_args3,
                      'user_test_args4': user_test_args4}
-        self.log_level = "DEBUG"
+        self.log_level = "TRACE"
 
     @property
     def node(self):
