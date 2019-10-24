6794d64055aca325c54d5ffca2fbd8cf60905024
abbccdda <boyang@confluent.io>
2019-10-23 17:25:34 -0700

    flush state

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
@@ -776,6 +776,8 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator 
                       final boolean isZombie) {
         log.debug("Closing");
 
+        flushState();
+
         RuntimeException firstException = null;
         try {
             suspend(clean, isZombie);
