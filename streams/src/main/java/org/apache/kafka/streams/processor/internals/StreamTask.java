1bdf5cf22677bebab206c070889d709efcc7aa1e
abbccdda <boyang@confluent.io>
2019-10-24 10:00:41 -0700

    no flush state

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
@@ -672,8 +672,6 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator 
                 throw taskMigratedException;
             }
         } else {
-            flushState();
-
             maybeAbortTransactionAndCloseRecordCollector(isZombie);
         }
     }
