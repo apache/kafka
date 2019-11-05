5b7afab9cc5d04e63efcdf950fe2f2f9c3efacd6
Stanislav Kozlovski <stanislav_kozlovski@outlook.com>
2019-11-05 18:34:11 +0000

    MINOR: Rework NewPartitionReassignment public API (#7638)
    This patch removes the NewPartitionReassignment#of() method in favor of a simple constructor. Said method was confusing due to breaking two conventions - always returning a non-empty Optional and thus not being used as a static factory method.
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>, Colin P. McCabe <cmccabe@apache.org>
    (cherry picked from commit be58580e14be93618f11e609389ff6bb16317702)

--- core/src/test/scala/unit/kafka/admin/ReassignPartitionsClusterTest.scala
+++ core/src/test/scala/unit/kafka/admin/ReassignPartitionsClusterTest.scala
@@ -28,7 +28,7 @@ import scala.collection.JavaConverters._
 import scala.collection.{Map, Seq}
 import scala.util.Random
 import java.io.File
-import java.util.{Collections, Properties}
+import java.util.{Collections, Optional, Properties}
 import java.util.concurrent.ExecutionException
 
 import kafka.controller.ReplicaAssignment
@@ -1261,7 +1261,7 @@ class ReassignPartitionsClusterTest extends ZooKeeperTestHarness with Logging {
     }.mkString(",")
 
   def reassignmentEntry(tp: TopicPartition, replicas: Seq[Int]): (TopicPartition, java.util.Optional[NewPartitionReassignment]) =
-    tp -> NewPartitionReassignment.of(replicas.map(_.asInstanceOf[Integer]).asJava)
+    tp -> Optional.of(new NewPartitionReassignment((replicas.map(_.asInstanceOf[Integer]).asJava)))
 
   def cancelReassignmentEntry(tp: TopicPartition): (TopicPartition, java.util.Optional[NewPartitionReassignment]) =
     tp -> java.util.Optional.empty()
