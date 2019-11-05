c92cdb91bdc866b2d248c5eb1aa84cc744a362da
Ismael Juma <ismael@juma.me.uk>
2019-11-04 17:06:57 -0800

    Reduce transferSize to 32k

--- clients/src/main/java/org/apache/kafka/common/network/SslTransportLayer.java
+++ clients/src/main/java/org/apache/kafka/common/network/SslTransportLayer.java
@@ -917,8 +917,8 @@ public class SslTransportLayer implements TransportLayer {
         if (fileChannelBuffer == null) {
             // Pick a size that allows for reasonably efficient disk reads, keeps the memory overhead per connection
             // manageable and can typically be drained in a single `write` call. The `netWriteBuffer` is typically 16k
-            // and the socket send buffer is 100k by default, so 64k is a good number given the mentioned trade-offs.
-            int transferSize = 65536;
+            // and the socket send buffer is 100k by default, so 32k is a good number given the mentioned trade-offs.
+            int transferSize = 32768;
             // Allocate a direct buffer to avoid one heap to heap buffer copy. SSLEngine copies the source
             // buffer (fileChannelBuffer) to the destination buffer (netWriteBuffer) and then encrypts in-place.
             // FileChannel.read() to a heap buffer requires a copy from a direct buffer to a heap buffer, which is not
