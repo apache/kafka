bcd3e97c765797f8450bdb1c72bb0bd884b8a925
Colin P. Mccabe <cmccabe@confluent.io>
2019-11-08 12:46:28 -0800

    KAFKA-9165: Fix jersey warnings in Trogdor

---
+++ tools/src/main/java/org/apache/kafka/trogdor/agent/AgentResourceBinder.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kafka.trogdor.agent;
+
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+
+/**
+ * This class implements a workaround for the Jersey bug which leads to
+ * warning messages when registering a Resource object.
+ * See https://github.com/eclipse-ee4j/jersey/issues/3700
+ *
+ * Unfortunately, this can't be common code shared with the Coordinator, because hk2/jersey
+ * doesn't add new bindings for the same class.
+ */
+public class AgentResourceBinder extends AbstractBinder {
+    private Object object;
+    private Class clazz;
+
+    public AgentResourceBinder(Object object, Class clazz) {
+        this.object = object;
+        this.clazz = clazz;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void configure() {
+        bind(object).to(clazz);
+    }
+}
