b3a36b4c1af16d228a456ee0c138000f6c2c635c
gleb-kom <gkomissarov@YULC02PLBHYG8WN.sea.corp.expecn.com>
2019-11-08 12:43:32 -0500

    add missing @throws annotation to waitOnMetadata in KafkaProducer

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -995,6 +995,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
      * @param nowMs The current time in ms
      * @param maxWaitMs The maximum time in ms for waiting on the metadata
      * @return The cluster containing topic metadata and the amount of time we waited in ms
+     * @throws TimeoutException if metadata could not be refreshed within {@code max.block.ms}
      * @throws KafkaException for all Kafka-related exceptions, including the case where this method is called after producer close
      */
     private ClusterAndWaitTime waitOnMetadata(String topic, Integer partition, long nowMs, long maxWaitMs) throws InterruptedException {
