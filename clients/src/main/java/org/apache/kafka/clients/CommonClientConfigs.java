dafda14b836899161bcecda6cb40565f326b915b
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2019-11-12 14:17:20 -0500

    Make more descriptive docs
    Fix typo
    
    Complete fixing typo

--- clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
+++ clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
@@ -119,18 +119,22 @@ public class CommonClientConfigs {
                                                          + "retries are exhausted.";
 
     public static final String DEFAULT_LIST_KEY_SERDE_INNER_CLASS = "default.list.key.serde.inner";
-    public static final String DEFAULT_LIST_KEY_SERDE_INNER_CLASS_DOC = "Default inner class of list serde for key that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface.";
+    public static final String DEFAULT_LIST_KEY_SERDE_INNER_CLASS_DOC = "Default inner class of list serde for key that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface. "
+            + "This configuration will be read if and only if <code>default.key.serde</code> configuration is set to <code>org.apache.kafka.common.serialization.Serdes.ListSerde</code>";
 
     public static final String DEFAULT_LIST_VALUE_SERDE_INNER_CLASS = "default.list.value.serde.inner";
-    public static final String DEFAULT_LIST_VALUE_SERDE_INNER_CLASS_DOC = "Default inner class of list serde for value that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface.";
+    public static final String DEFAULT_LIST_VALUE_SERDE_INNER_CLASS_DOC = "Default inner class of list serde for value that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface. "
+            + "This configuration will be read if and only if <code>default.value.serde</code> configuration is set to <code>org.apache.kafka.common.serialization.Serdes.ListSerde</code>";
 
     public static final String DEFAULT_LIST_KEY_SERDE_TYPE_CLASS = "default.list.key.serde.type";
     public static final String DEFAULT_LIST_KEY_SERDE_TYPE_CLASS_DOC = "Default class for key that implements the <code>java.util.List</code> interface. "
+            + "This configuration will be read if and only if <code>default.key.serde</code> configuration is set to <code>org.apache.kafka.common.serialization.Serdes.ListSerde</code> "
             + "Note when list serde class is used, one needs to set the inner serde class that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface via '"
             + DEFAULT_LIST_KEY_SERDE_INNER_CLASS + "'";
 
     public static final String DEFAULT_LIST_VALUE_SERDE_TYPE_CLASS = "default.list.value.serde.type";
     public static final String DEFAULT_LIST_VALUE_SERDE_TYPE_CLASS_DOC = "Default class for value that implements the <code>java.util.List</code> interface. "
+            + "This configuration will be read if and only if <code>default.value.serde</code> configuration is set to <code>org.apache.kafka.common.serialization.Serdes.ListSerde</code> "
             + "Note when list serde class is used, one needs to set the inner serde class that implements the <code>org.apache.kafka.common.serialization.Serde</code> interface via '"
             + DEFAULT_LIST_VALUE_SERDE_INNER_CLASS + "'";
 
--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerConfig.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerConfig.java
@@ -227,7 +227,7 @@ public class ConsumerConfig extends AbstractConfig {
     /**
      * <code>list.key.serializer.inner</code>
      */
-    public static final String LIST_KEY_DEERIALIZER_INNER_CLASS_CONFIG = "list.key.deserializer.inner";
+    public static final String LIST_KEY_DESERIALIZER_INNER_CLASS_CONFIG = "list.key.deserializer.inner";
     public static final String LIST_KEY_DESERIALIZER_INNER_CLASS_DOC = "Inner deserializer class for key that implements the <code>java.util.List</code> interface. "
             + "This configuration will be read if and only if <code>key.deserializer</code> configuration is set to <code>org.apache.kafka.common.serialization.ListDeserializer</code>";
 
@@ -574,7 +574,7 @@ public class ConsumerConfig extends AbstractConfig {
                                         null,
                                         Importance.MEDIUM,
                                         LIST_KEY_DESERIALIZER_TYPE_CLASS_DOC)
-                                .define(LIST_KEY_DEERIALIZER_INNER_CLASS_CONFIG,
+                                .define(LIST_KEY_DESERIALIZER_INNER_CLASS_CONFIG,
                                         Type.CLASS,
                                         null,
                                         Importance.MEDIUM,
