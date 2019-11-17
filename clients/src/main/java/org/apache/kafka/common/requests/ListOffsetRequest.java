9ada5e7fedfa1f0f26aa4550bb06136981d05937
Lucas Bradstreet <lucas@confluent.io>
2019-11-17 14:36:34 -0800

    Switch approach back to specific handling of version 5. Add tests.

--- clients/src/main/java/org/apache/kafka/common/requests/ListOffsetRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/ListOffsetRequest.java
@@ -269,7 +269,18 @@ public class ListOffsetRequest extends AbstractRequest {
             responseData.put(partition, partitionError);
         }
 
-        return new ListOffsetResponse(throttleTimeMs, responseData);
+        switch (versionId) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                return new ListOffsetResponse(throttleTimeMs, responseData);
+            default:
+                throw new IllegalArgumentException(String.format("Version %d is not valid. Valid versions for %s are 0 to %d",
+                        versionId, this.getClass().getSimpleName(), ApiKeys.LIST_OFFSETS.latestVersion()));
+        }
     }
 
     public int replicaId() {
