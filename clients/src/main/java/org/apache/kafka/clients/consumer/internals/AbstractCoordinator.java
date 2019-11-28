a9f5b174623b3ae0613ac19f996f41e39dad1423
mbl <307499405@qq.com>
2019-11-28 18:34:07 +0800

    MINOR: Correct the wrong comment which confuse me.

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinator.java
@@ -1216,8 +1216,7 @@ public abstract class AbstractCoordinator implements Closeable {
                                 // brokers are available to connect to.
                                 AbstractCoordinator.this.wait(rebalanceConfig.retryBackoffMs);
                         } else if (heartbeat.sessionTimeoutExpired(now)) {
-                            // the session timeout has expired without seeing a successful heartbeat, so we should
-                            // probably make sure the coordinator is still healthy.
+                            // the session timeout has expired, so we should mark coordinator unknown.
                             markCoordinatorUnknown();
                         } else if (heartbeat.pollTimeoutExpired(now)) {
                             // the poll timeout has expired, which means that the foreground thread has stalled
