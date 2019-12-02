ef25d2301f9206d335eaaaf71e19c24b6fb4ab22
Cyrus Vafadari <cyrus@confluent.io>
2019-12-02 13:24:11 -0800

    KAFKA-9258 Check Connect Metrics non-null in task stop

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Worker.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Worker.java
@@ -726,12 +726,14 @@ public class Worker {
     private void awaitStopTask(ConnectorTaskId taskId, long timeout) {
         try (LoggingContext loggingContext = LoggingContext.forTask(taskId)) {
             WorkerTask task = tasks.remove(taskId);
-            connectorStatusMetricsGroup.recordTaskRemoved(taskId);
             if (task == null) {
                 log.warn("Ignoring await stop request for non-present task {}", taskId);
                 return;
             }
 
+            if (connectorStatusMetricsGroup != null) {
+                connectorStatusMetricsGroup.recordTaskRemoved(taskId);
+            }
             if (!task.awaitStop(timeout)) {
                 log.error("Graceful stop of task {} failed.", task.id());
                 task.cancel();
