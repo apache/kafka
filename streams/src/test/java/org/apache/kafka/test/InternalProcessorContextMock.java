38dca1e5ac1531c39a4298e18a025a40549b4d54
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2019-12-08 18:02:01 +0100

    Mock applicationId() method

---
+++ streams/src/test/java/org/apache/kafka/test/InternalProcessorContextMock.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.test;
+
+import org.apache.kafka.streams.processor.MockProcessorContext;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.internals.InternalProcessorContext;
+import org.easymock.EasyMock;
+
+public class InternalProcessorContextMock {
+
+    public static Builder builder() {
+        return new Builder(processorContext());
+    }
+
+    public static class Builder {
+
+        private String applicationId;
+
+        private InternalProcessorContext mock;
+
+        Builder(final ProcessorContext processorContext) {
+            mock = EasyMock.mock(InternalProcessorContext.class);
+
+            applicationId = processorContext.applicationId();
+        }
+
+        public InternalProcessorContext build() {
+            applicationId();
+            EasyMock.replay(mock);
+            return mock;
+        }
+
+        private void applicationId() {
+            EasyMock.expect(mock.applicationId()).andReturn(applicationId);
+        }
+    }
+
+    private static ProcessorContext processorContext() {
+        return new MockProcessorContext();
+    }
+}
