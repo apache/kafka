b306756963fba34ce37f1c317105f7f374987e53
ableegoldman <sophie@confluent.io>
2019-12-11 16:45:03 -0800

    refactor NamedCache to only flush evicted

--- streams/src/main/java/org/apache/kafka/streams/state/internals/NamedCache.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/NamedCache.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kafka.streams.state.internals;
 
+import java.util.Collections;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -106,19 +107,11 @@ class NamedCache {
     }
 
     synchronized void flush() {
-        flush(null);
-    }
-
-    private void flush(final LRUNode evicted) {
         numFlushes++;
 
         if (log.isTraceEnabled()) {
             log.trace("Named cache {} stats on flush: #hits={}, #misses={}, #overwrites={}, #flushes={}",
                 name, hits(), misses(), overwrites(), flushes());
-        }
-
-        if (listener == null) {
-            throw new IllegalArgumentException("No listener for namespace " + name + " registered with cache");
         }
 
         if (dirtyKeys.isEmpty()) {
@@ -127,13 +120,6 @@ class NamedCache {
 
         final List<ThreadCache.DirtyEntry> entries = new ArrayList<>();
         final List<Bytes> deleted = new ArrayList<>();
-
-        // evicted already been removed from the cache so add it to the list of
-        // flushed entries and remove from dirtyKeys.
-        if (evicted != null) {
-            entries.add(new ThreadCache.DirtyEntry(evicted.key, evicted.entry.value(), evicted.entry));
-            dirtyKeys.remove(evicted.key);
-        }
 
         for (final Bytes key : dirtyKeys) {
             final LRUNode node = getInternal(key);
@@ -146,12 +132,21 @@ class NamedCache {
                 deleted.add(node.key);
             }
         }
-        // clear dirtyKeys before the listener is applied as it may be re-entrant.
+
         dirtyKeys.clear();
-        listener.apply(entries);
+        flush(entries);
         for (final Bytes key : deleted) {
             delete(key);
         }
+    }
+
+    // Entries should be removed from dirtyKeys before the listener is applied as it may be re-entrant
+    private void flush(final List<ThreadCache.DirtyEntry> entries) {
+        if (listener == null) {
+            throw new IllegalArgumentException("No listener for namespace " + name + " registered with cache");
+        }
+
+        listener.apply(entries);
     }
 
     synchronized void put(final Bytes key, final LRUCacheEntry value) {
@@ -241,7 +236,9 @@ class NamedCache {
         remove(eldest);
         cache.remove(eldest.key);
         if (eldest.entry.isDirty()) {
-            flush(eldest);
+            dirtyKeys.remove(eldest.key);
+            flush(Collections.singletonList(
+                new ThreadCache.DirtyEntry(eldest.key,eldest.entry.value(), eldest.entry)));
         }
     }
 
