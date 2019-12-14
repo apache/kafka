c37fe3c671833eff552eb581e880a54cbf9bacec
Manikumar Reddy <manikumar@confluent.io>
2019-12-14 07:49:05 +0530

    Bump version to 2.4.1-SNAPSHOT

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "24",
     "dotVersion": "2.4",
-    "fullDotVersion": "2.4.0",
+    "fullDotVersion": "2.4.1",
     "scalaVersion": "2.12"
 };
