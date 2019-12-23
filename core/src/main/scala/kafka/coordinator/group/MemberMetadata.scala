7c3018b25dd5b74c0fc7f5d703c2e6e18d1db249
A. Sophie Blee-Goldman <sophie@confluent.io>
2019-12-23 18:55:48 -0500

    KAFKA-9232; Coordinator new member timeout does not work for JoinGroup v3 and below (#7753)
    The v3 JoinGroup logic does not properly complete the initial heartbeat for new members, which then expires after the static 5 minute timeout if the member does not rejoin. The core issue is in the `shouldKeepAlive` method, which returns false when it should return true because of an inconsistent timeout check.
    
    Reviewers: Jason Gustafson <jason@confluent.io>

--- core/src/main/scala/kafka/coordinator/group/MemberMetadata.scala
+++ core/src/main/scala/kafka/coordinator/group/MemberMetadata.scala
@@ -86,10 +86,16 @@ private[group] class MemberMetadata(var memberId: String,
   }
 
   def shouldKeepAlive(deadlineMs: Long): Boolean = {
-    if (isAwaitingJoin)
-      !isNew || latestHeartbeat + GroupCoordinator.NewMemberJoinTimeoutMs > deadlineMs
-    else awaitingSyncCallback != null ||
+    if (isNew) {
+      // New members are expired after the static join timeout
+      latestHeartbeat + GroupCoordinator.NewMemberJoinTimeoutMs > deadlineMs
+    } else if (isAwaitingJoin || isAwaitingSync) {
+      // Don't remove members as long as they have a request in purgatory
+      true
+    } else {
+      // Otherwise check for session expiration
       latestHeartbeat + sessionTimeoutMs > deadlineMs
+    }
   }
 
   /**
