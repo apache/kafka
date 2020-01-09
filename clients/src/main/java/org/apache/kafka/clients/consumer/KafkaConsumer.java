ae7da12430e2759d754bbf1c3bf2612c057b5fed
abbccdda <boyang@confluent.io>
2020-01-09 11:23:46 -0800

    remove unthrown exceptions

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -1209,8 +1209,6 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
      * @throws org.apache.kafka.common.errors.InvalidTopicException if the current subscription contains any invalid
      *             topic (per {@link org.apache.kafka.common.internals.Topic#validate(String)})
      * @throws org.apache.kafka.common.errors.FencedInstanceIdException if this consumer instance gets fenced by broker.
-     * @throws org.apache.kafka.common.errors.PendingTransactionException if there are other pending transactional commits
-     *             and the isolation level is {@code read_committed}
      */
     @Override
     public ConsumerRecords<K, V> poll(final Duration timeout) {
@@ -1715,8 +1713,6 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
      * @throws org.apache.kafka.common.errors.AuthenticationException if authentication fails. See the exception for more details
      * @throws org.apache.kafka.common.errors.AuthorizationException if not authorized to the topic or to the
      *             configured groupId. See the exception for more details
-     * @throws org.apache.kafka.common.errors.PendingTransactionException if there are other pending transactional commits
-     *             and the isolation level is {@code read_committed}
      * @throws org.apache.kafka.common.KafkaException for any other unrecoverable errors
      * @throws org.apache.kafka.common.errors.TimeoutException if the position cannot be determined before the
      *             timeout specified by {@code default.api.timeout.ms} expires
@@ -1793,8 +1789,6 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
      * @throws org.apache.kafka.common.errors.AuthenticationException if authentication fails. See the exception for more details
      * @throws org.apache.kafka.common.errors.AuthorizationException if not authorized to the topic or to the
      *             configured groupId. See the exception for more details
-     * @throws org.apache.kafka.common.errors.PendingTransactionException if there are other pending transactional commits
-     *             and the isolation level is {@code read_committed}
      * @throws org.apache.kafka.common.KafkaException for any other unrecoverable errors
      * @throws org.apache.kafka.common.errors.TimeoutException if the committed offset cannot be found before
      *             the timeout specified by {@code default.api.timeout.ms} expires.
