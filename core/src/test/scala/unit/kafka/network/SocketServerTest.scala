4f71c9d6c7e59c3b587645efd6cd9da0398c3ff0
David Mollitor <dmollitor@apache.org>
2020-01-13 12:42:51 -0500

    Add StandardCharset to Test classes as well

--- core/src/test/scala/unit/kafka/network/SocketServerTest.scala
+++ core/src/test/scala/unit/kafka/network/SocketServerTest.scala
@@ -21,6 +21,7 @@ import java.io._
 import java.net._
 import java.nio.ByteBuffer
 import java.nio.channels.SocketChannel
+import java.nio.charset.StandardCharsets
 import java.util.concurrent.{CompletableFuture, Executors}
 import java.util.{HashMap, Properties, Random}
 
@@ -823,7 +824,7 @@ class SocketServerTest {
       receiveResponse(socket)
 
       // now send credentials
-      val authBytes = "admin\u0000admin\u0000admin-secret".getBytes("UTF-8")
+      val authBytes = "admin\u0000admin\u0000admin-secret".getBytes(StandardCharsets.UTF_8)
       if (leverageKip152SaslAuthenticateRequest) {
         // send credentials within a SaslAuthenticateRequest
         val saslAuthenticateRequest = new SaslAuthenticateRequest.Builder(new SaslAuthenticateRequestData()
