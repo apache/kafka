d92192192d4e18ff32325e12c02b7b0226eb2a98
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2020-01-21 22:52:40 +0200

    KAFKA-9143: Log task reconfiguration error only when it happened (#7648)
    This commit makes `DistributedHerder` log that some error has happened during task reconfiguration only when it actually has happened.
    
    Author: Ivan Yurchenko <ivan0yurchenko@gmail.com>
    Reviewer: Randall Hauch <rhauch@gmail.com>

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/DistributedHerder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/DistributedHerder.java
@@ -1137,8 +1137,10 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
                             }, new Callback<Void>() {
                                 @Override
                                 public void onCompletion(Throwable error, Void result) {
-                                    log.error("Unexpected error during connector task reconfiguration: ", error);
-                                    log.error("Task reconfiguration for {} failed unexpectedly, this connector will not be properly reconfigured unless manually triggered.", connName);
+                                    if (error != null) {
+                                        log.error("Unexpected error during connector task reconfiguration: ", error);
+                                        log.error("Task reconfiguration for {} failed unexpectedly, this connector will not be properly reconfigured unless manually triggered.", connName);
+                                    }
                                 }
                             }
                     );
