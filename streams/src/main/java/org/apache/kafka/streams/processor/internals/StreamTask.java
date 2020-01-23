a67b953642c3597737d43a00b9c09c0ee840b7cd
Guozhang Wang <wangguoz@gmail.com>
2020-01-22 17:24:08 -0800

    fix build

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
@@ -33,7 +33,6 @@ import org.apache.kafka.streams.errors.TaskMigratedException;
 import org.apache.kafka.streams.processor.Cancellable;
 import org.apache.kafka.streams.processor.PunctuationType;
 import org.apache.kafka.streams.processor.Punctuator;
-import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.processor.TaskId;
 import org.apache.kafka.streams.processor.TimestampExtractor;
 import org.apache.kafka.streams.processor.internals.metrics.StreamsMetricsImpl;
