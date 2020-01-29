832a6dbb2e0bd31de4045370e862ad9cd37db219
David Jacot <djacot@confluent.io>
2020-01-28 21:52:17 -0500

    typo

--- clients/src/main/java/org/apache/kafka/common/requests/SyncGroupRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/SyncGroupRequest.java
@@ -83,7 +83,7 @@ public class SyncGroupRequest extends AbstractRequest {
 
     /**
      * ProtocolType and ProtocolName are mandatory since version 5. This methods verifies that
-     * they are defined for version % or higher, or returns true otherwise for older versions.
+     * they are defined for version 5 or higher, or returns true otherwise for older versions.
      */
     public boolean areMandatoryProtocolTypeAndNamePresent() {
         if (version() >= 5)
