7748fc2fc6cab504403276696df9ae8cf193601f
Alexandra Rodoni <arodoni@confluent.io>
2020-02-04 09:44:12 -0800

    KAFKA-9477 Document RoundRobinAssignor as an option for partition.assignment.strategy (#8007)
    Reviewers: Colin P. McCabe <cmccabe@apache.org>

--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerConfig.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerConfig.java
@@ -103,7 +103,18 @@ public class ConsumerConfig extends AbstractConfig {
      * <code>partition.assignment.strategy</code>
      */
     public static final String PARTITION_ASSIGNMENT_STRATEGY_CONFIG = "partition.assignment.strategy";
-    private static final String PARTITION_ASSIGNMENT_STRATEGY_DOC = "A list of class names or class types, ordered by preference, of supported assignors responsible for the partition assignment strategy that the client will use to distribute partition ownership amongst consumer instances when group management is used. Implementing the <code>org.apache.kafka.clients.consumer.ConsumerPartitionAssignor</code> interface allows you to plug in a custom assignment strategy.";
+    private static final String PARTITION_ASSIGNMENT_STRATEGY_DOC = "A list of class names or class types, "+
+            "ordered by preference, of supported partition assignment "+
+            "strategies that the client will use to distribute partition "+
+            "ownership amongst consumer instances when group management is "+
+            "used.<p>In addition to the default class specified below, "+
+            "you can use the " + 
+            "<code>org.apache.kafka.clients.consumer.RoundRobinAssignor</code>"+
+            "class for round robin assignments of partitions to consumers. "+
+            "</p><p>Implementing the " +
+            "<code>org.apache.kafka.clients.consumer.ConsumerPartitionAssignor"+
+            "</code> interface allows you to plug in a custom assignment"+
+            "strategy.";
 
     /**
      * <code>auto.offset.reset</code>
