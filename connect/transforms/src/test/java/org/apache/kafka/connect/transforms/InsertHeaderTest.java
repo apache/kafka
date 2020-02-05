dfb45c14e9aaf2a0b63b140e46768775f11c083c
alozano3 <alozano@silk.es>
2020-02-05 16:02:01 +0100

    add tests for the rest of header types

--- connect/transforms/src/test/java/org/apache/kafka/connect/transforms/InsertHeaderTest.java
+++ connect/transforms/src/test/java/org/apache/kafka/connect/transforms/InsertHeaderTest.java
@@ -19,7 +19,9 @@ package org.apache.kafka.connect.transforms;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.data.Time;
+import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Timestamp;
+import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.source.SourceRecord;
@@ -28,7 +30,9 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -68,7 +72,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().add("AAA", null);
@@ -117,7 +121,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addString("dummy header", "dummy value");
@@ -137,11 +141,11 @@ public class InsertHeaderTest {
         Headers existentHeaders = new ConnectHeaders().addString("BBB", "existent value");
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, existentHeaders);
+                0, null, null, null, null, null, existentHeaders);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addString("BBB", "existent value")
-            .addString("AAA", "dummy value");
+                .addString("AAA", "dummy value");
 
         assertEquals(2, transformedRecord.headers().size());
         assertEquals(expected, transformedRecord.headers());
@@ -157,15 +161,15 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         Headers existentHeaders = new ConnectHeaders().addString("AAA", "existent value")
-            .addString("BBB", "existent value");
+                .addString("BBB", "existent value");
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, existentHeaders);
+                0, null, null, null, null, null, existentHeaders);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addString("AAA", "existent value")
-            .addString("BBB", "existent value")
-            .addString("AAA", "dummy value");
+                .addString("BBB", "existent value")
+                .addString("AAA", "dummy value");
 
 
         assertEquals(3, transformedRecord.headers().size());
@@ -181,7 +185,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addByte("AAA", (byte) 2);
@@ -199,7 +203,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addShort("AAA", (short) 18000);
@@ -217,7 +221,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addInt("AAA", 50000);
@@ -235,7 +239,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addLong("AAA", 87474836647L);
@@ -253,7 +257,7 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         Headers expected = new ConnectHeaders().addDouble("AAA", 2353245343456.435435);
@@ -271,11 +275,11 @@ public class InsertHeaderTest {
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         SchemaAndValue schemaAndValue = new SchemaAndValue(org.apache.kafka.connect.data.Date.SCHEMA,
-            new SimpleDateFormat("yyyy-MM-dd").parse("2019-08-23"));
+                new SimpleDateFormat("yyyy-MM-dd").parse("2019-08-23"));
 
         Headers expected = new ConnectHeaders().add("AAA", schemaAndValue);
 
@@ -287,15 +291,15 @@ public class InsertHeaderTest {
     public void insertHeaderWithTimeValue() throws ParseException {
         final Map<String, Object> props = new HashMap<>();
         props.put("header", "AAA");
-        props.put("literal.value", "14\\:34\\:54.346Z");
+        props.put("literal.value", "14:34:54.346Z");
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         SchemaAndValue schemaAndValue = new SchemaAndValue(Time.SCHEMA,
-            new SimpleDateFormat("HH:mm:ss.SSS'Z'").parse("14:34:54.346Z"));
+                new SimpleDateFormat("HH:mm:ss.SSS'Z'").parse("14:34:54.346Z"));
 
         Headers expected = new ConnectHeaders().add("AAA", schemaAndValue);
 
@@ -307,18 +311,80 @@ public class InsertHeaderTest {
     public void insertHeaderWithTimestampValue() throws ParseException {
         final Map<String, Object> props = new HashMap<>();
         props.put("header", "AAA");
-        props.put("literal.value", "2019-08-23T14\\:34\\:54.346Z");
+        props.put("literal.value", "2019-08-23T14:34:54.346Z");
 
         xform.configure(props);
 
         final SourceRecord record = new SourceRecord(null, null, "test",
-            0, null, null, null, null, null, null);
+                0, null, null, null, null, null, null);
         final SourceRecord transformedRecord = xform.apply(record);
 
         SchemaAndValue schemaAndValue = new SchemaAndValue(Timestamp.SCHEMA,
-            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse("2019-08-23T14:34:54.346Z"));
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse("2019-08-23T14:34:54.346Z"));
 
         Headers expected = new ConnectHeaders().add("AAA", schemaAndValue);
+
+        assertEquals(1, transformedRecord.headers().size());
+        assertEquals(expected, transformedRecord.headers());
+    }
+
+    @Test
+    public void insertHeaderWithBooleanValue() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put("header", "dummy header");
+        props.put("literal.value", "true");
+
+        xform.configure(props);
+
+        final SourceRecord record = new SourceRecord(null, null, "test",
+                0, null, null, null, null, null, null);
+        final SourceRecord transformedRecord = xform.apply(record);
+
+        Headers expected = new ConnectHeaders().addBoolean("dummy header", true);
+
+        assertEquals(1, transformedRecord.headers().size());
+        assertEquals(expected, transformedRecord.headers());
+    }
+
+    @Test
+    public void insertHeaderWithMapValue() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put("header", "dummy header");
+        props.put("literal.value", "{\"foo\":\"abc\",\"bar\":\"def\"}");
+
+        xform.configure(props);
+
+        final SourceRecord record = new SourceRecord(null, null, "test",
+                0, null, null, null, null, null, null);
+        final SourceRecord transformedRecord = xform.apply(record);
+
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("foo", "abc");
+        map.put("bar", "def");
+        Headers expected = new ConnectHeaders().addMap("dummy header", map, SchemaBuilder.map(
+                Schema.STRING_SCHEMA, Schema.STRING_SCHEMA).build());
+
+        assertEquals(1, transformedRecord.headers().size());
+        assertEquals(expected, transformedRecord.headers());
+    }
+
+    @Test
+    public void insertHeaderWithListValue() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put("header", "dummy header");
+        props.put("literal.value", "[\"one\",\"two\",\"three\"]");
+
+        xform.configure(props);
+
+        final SourceRecord record = new SourceRecord(null, null, "test",
+                0, null, null, null, null, null, null);
+        final SourceRecord transformedRecord = xform.apply(record);
+
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("foo", "abc");
+        map.put("bar", "def");
+        Headers expected = new ConnectHeaders().addList("dummy header", Arrays.asList(
+                "one", "two", "three"), SchemaBuilder.array(Schema.STRING_SCHEMA).build());
 
         assertEquals(1, transformedRecord.headers().size());
         assertEquals(expected, transformedRecord.headers());
