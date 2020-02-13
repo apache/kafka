61906c8bfc9a8447161bbf7b9a7322e23e1bfcb6
Nikolay Izhikov <nizhikov@apache.org>
2020-02-13 17:49:30 +0300

    KAFKA-9319: Fix generation of CA certificate for system tests.

--- clients/src/main/java/org/apache/kafka/common/security/ssl/SslFactory.java
+++ clients/src/main/java/org/apache/kafka/common/security/ssl/SslFactory.java
@@ -97,6 +97,8 @@ public class SslFactory implements Reconfigurable {
             try {
                 SslEngineValidator.validate(builder, builder);
             } catch (Exception e) {
+                log.error("SSL validation failed", e);
+
                 throw new ConfigException("A client SSLEngine created with the provided settings " +
                         "can't connect to a server SSLEngine created with those settings.", e);
             }
