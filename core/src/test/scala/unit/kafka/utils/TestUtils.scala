04afb784231807cdaaf43135d4dde529f381358a
senthilm-ms <55106539+senthilm-ms@users.noreply.github.com>
2020-02-12 16:47:11 -0800

    KIP-280 Enhanced log compaction (#1)
    * Config update
    
    * Draft for KIP 280
    
    * Unit test for Offset Map with compact strategy
    
    * More unit test cases to cover all scenarios on the compact strategies
    
    * Added the compact strategy at the broker level as well with unit tests
    
    * Config update
    
    * Draft for KIP 280
    
    * Unit test for Offset Map with compact strategy
    
    * More unit test cases to cover all scenarios on the compact strategies
    
    * Added the compact strategy at the broker level as well with unit tests
    
    * Addressed design review comments and fixed the unit test failures
    
    * initial unit test
    
    * detailed unit test coverage for timestamp & header based compact strategy
    
    * more unit test coverage
    
    * Full unit test coverage for the timestamp & header based compaction strategy
    
    * Address review comments
    
    * Fixed the build warning & test failure
    
    * Added config validation for the header key if the strategy is header
    
    * Addressed review comments
    
    * Addressed review comment and merge conflicts

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -58,7 +58,7 @@ import org.apache.kafka.common.record._
 import org.apache.kafka.common.resource.ResourcePattern
 import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, Deserializer, IntegerSerializer, Serializer}
-import org.apache.kafka.common.utils.Time
+import org.apache.kafka.common.utils.{ByteUtils, Time}
 import org.apache.kafka.common.utils.Utils._
 import org.apache.kafka.common.{KafkaFuture, TopicPartition}
 import org.apache.kafka.server.authorizer.{Authorizer => JAuthorizer}
@@ -414,10 +414,11 @@ object TestUtils extends Logging {
    */
   def singletonRecords(value: Array[Byte],
                        key: Array[Byte] = null,
+                       headers: Array[Header] = Record.EMPTY_HEADERS,
                        codec: CompressionType = CompressionType.NONE,
                        timestamp: Long = RecordBatch.NO_TIMESTAMP,
                        magicValue: Byte = RecordBatch.CURRENT_MAGIC_VALUE): MemoryRecords = {
-    records(Seq(new SimpleRecord(timestamp, key, value)), magicValue = magicValue, codec = codec)
+    records(Seq(new SimpleRecord(timestamp, key, value, headers)), magicValue = magicValue, codec = codec)
   }
 
   def recordsWithValues(magicValue: Byte,
@@ -1756,4 +1757,10 @@ object TestUtils extends Logging {
       authorizer, resource)
   }
 
+  def longToByte(value: Long): Array[Byte] = {
+    val buffer = ByteBuffer.allocate(16)
+    ByteUtils.writeVarlong(value, buffer)
+    buffer.flip
+    buffer.array
+  }
 }
