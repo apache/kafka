0f612ad1850f9c25eefb01dc779347a6926b5bd6
Ruslan Gibaiev <ruslan.gibaiev@bolt.eu>
2020-02-17 16:24:42 +0200

    Do not override NULL values with default values

--- connect/json/src/main/java/org/apache/kafka/connect/json/JsonConverter.java
+++ connect/json/src/main/java/org/apache/kafka/connect/json/JsonConverter.java
@@ -593,10 +593,10 @@ public class JsonConverter implements Converter, HeaderConverter {
         if (value == null) {
             if (schema == null) // Any schema is valid and we don't have a default, so treat this as an optional schema
                 return null;
-            if (schema.defaultValue() != null)
-                return convertToJson(schema, schema.defaultValue());
             if (schema.isOptional())
                 return JsonNodeFactory.instance.nullNode();
+            if (schema.defaultValue() != null)
+                return convertToJson(schema, schema.defaultValue());
             throw new DataException("Conversion error: null value for field that is required and has no default value");
         }
 
