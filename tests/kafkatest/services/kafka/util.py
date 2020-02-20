0a2a70317f4807e4503b5e4eec3f07a17c46ab48
Nikolay Izhikov <nizhikov@apache.org>
2020-02-20 13:19:01 +0300

    KAFKA-9573: Version to fix options added.

--- tests/kafkatest/services/kafka/util.py
+++ tests/kafkatest/services/kafka/util.py
@@ -14,22 +14,37 @@
 # limitations under the License.
 
 from collections import namedtuple
+from kafkatest.version import DEV_BRANCH, LATEST_0_8_2, LATEST_0_9, LATEST_0_10_0, LATEST_0_10_1, LATEST_0_10_2, LATEST_0_11, LATEST_1_0
 
 TopicPartition = namedtuple('TopicPartition', ['topic', 'partition'])
 
-def fix_opts_for_new_jvm(self, node):
+def fix_opts_for_new_jvm(node):
     # Startup scripts for early versions of Kafka contains options
     # that not supported on latest versions of JVM like -XX:+PrintGCDateStamps or -XX:UseParNewGC.
     # When system test run on JVM that doesn't support these options
     # we should setup environment variables with correct options.
-    java_version = node.account.ssh_capture("java -version 2>&1 | sed -E -n 's/.* version \"([0-9]*).*$/\1/p'")
+    java_version = java_version(node)
 
-    if java_version <= 9
+    if int(java_version) <= 9:
         return ""
 
     cmd = ""
-    if node.version == LATEST_0_9 or node.version == LATEST_0_8_2:
-        cmd += "export KAFKA_GC_LOG_OPTS=\"-verbose:gc\"; "
+    if node.version == LATEST_0_8_2 or node.version == LATEST_0_9 or node.version == LATEST_0_10_0 or node.version == LATEST_0_10_1 or node.version == LATEST_0_10_2 or node.version == LATEST_0_11 or node.version == LATEST_1_0:
+        cmd += "export KAFKA_GC_LOG_OPTS=\"-Xlog:gc*:file=/home/ducker/gc.log:time,tags:filecount=10,filesize=102400\"; "
     if node.version == LATEST_0_8_2:
-        cmd += "export KAFKA_JVM_PERFORMANCE_OPTS=\"-server -XX:+CMSClassUnloadingEnabled -XX:+CMSScavengeBeforeRemark -XX:+DisableExplicitGC -Djava.awt.headless=true\"; "
+        cmd += "export KAFKA_JVM_PERFORMANCE_OPTS=\"-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent -XX:MaxInlineLevel=15 -Djava.awt.headless=true\"; "
+
     return cmd
+
+def java_version(node):
+    # Determine java version on the node
+    version = -1
+
+    for line in node.account.ssh_capture("java -version"):
+        print(line)
+        if line.find("version") != -1
+            print(line)
+
+    return version
+
+
