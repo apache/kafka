76e12b299a3db98b2cdf2d1cd7a8fac264d69d33
ableegoldman <sophie@confluent.io>
2020-03-16 17:32:26 -0700

    remove unused import

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/StateDirectoryTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/StateDirectoryTest.java
@@ -49,7 +49,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class StateDirectoryTest {
 
