e7564337cf56338735a5e4e3c18c3c0b7751e1db
Tom Bentley <tbentley@redhat.com>
2020-03-19 15:21:13 +0000

    Review comments

--- clients/src/main/java/org/apache/kafka/common/requests/DescribeLogDirsRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/DescribeLogDirsRequest.java
@@ -20,28 +20,11 @@ package org.apache.kafka.common.requests;
 import org.apache.kafka.common.message.DescribeLogDirsRequestData;
 import org.apache.kafka.common.message.DescribeLogDirsResponseData;
 import org.apache.kafka.common.protocol.ApiKeys;
-import org.apache.kafka.common.protocol.types.ArrayOf;
-import org.apache.kafka.common.protocol.types.Field;
-import org.apache.kafka.common.protocol.types.Schema;
 import org.apache.kafka.common.protocol.types.Struct;
 
 import java.nio.ByteBuffer;
 
-import static org.apache.kafka.common.protocol.CommonFields.TOPIC_NAME;
-import static org.apache.kafka.common.protocol.types.Type.INT32;
-
 public class DescribeLogDirsRequest extends AbstractRequest {
-
-    // request level key names
-    private static final String TOPICS_KEY_NAME = "topics";
-
-    // topic level key names
-    private static final String PARTITIONS_KEY_NAME = "partitions";
-
-    private static final Schema DESCRIBE_LOG_DIRS_REQUEST_V0 = new Schema(
-            new Field(TOPICS_KEY_NAME, ArrayOf.nullable(new Schema(
-                    TOPIC_NAME,
-                    new Field(PARTITIONS_KEY_NAME, new ArrayOf(INT32), "List of partition ids of the topic.")))));
 
     private final DescribeLogDirsRequestData data;
 
@@ -69,7 +52,6 @@ public class DescribeLogDirsRequest extends AbstractRequest {
         this.data = new DescribeLogDirsRequestData(struct, version);
     }
 
-    // topicPartitions == null indicates requesting all partitions, and an empty list indicates requesting no partitions.
     public DescribeLogDirsRequest(DescribeLogDirsRequestData data, short version) {
         super(ApiKeys.DESCRIBE_LOG_DIRS, version);
         this.data = data;
