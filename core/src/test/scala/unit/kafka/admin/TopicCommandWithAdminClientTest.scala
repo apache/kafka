eb0bc802928ad0d4a44966a42a0abacf69d9358c
José Armando García Sancio <jsancio@gmail.com>
2020-03-26 08:25:52 -0700

    MINOR: Parition is under reassignment when adding and removing
    A partition is under reassignment if the either the set of adding
    replicas or set removing replicas is non-empty.
    
    Fix the test assertion such that it prints stdout on failure.

--- core/src/test/scala/unit/kafka/admin/TopicCommandWithAdminClientTest.scala
+++ core/src/test/scala/unit/kafka/admin/TopicCommandWithAdminClientTest.scala
@@ -682,7 +682,7 @@ class TopicCommandWithAdminClientTest extends KafkaServerTestHarness with Loggin
 
     val underReplicatedOutput = TestUtils.grabConsoleOutput(
       topicService.describeTopic(new TopicCommandOptions(Array("--under-replicated-partitions"))))
-    assertEquals("--under-replicated-partitions shouldn't return anything", "", underReplicatedOutput)
+    assertEquals(s"--under-replicated-partitions shouldn't return anything: '$underReplicatedOutput'", "", underReplicatedOutput)
 
     TestUtils.removeReplicationThrottleForPartitions(adminClient, brokerIds, Set(tp))
     TestUtils.waitForAllReassignmentsToComplete(adminClient)
