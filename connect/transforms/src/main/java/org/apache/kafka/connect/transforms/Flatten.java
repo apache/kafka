f534da078eaf3a3b0b5471522b22b99acc9e4837
Greg Harris <gregh@confluent.io>
2020-03-30 15:09:27 -0700

    KAFKA-9706: Handle null in keys or values when Flatten transformation is used (#8279)
    * Fixed DataException thrown when handling tombstone events with null value
    * Passes through original record when finding a null key when it's configured for keys or a null value when it's configured for values. 
    * Added unit tests for schema and schemaless data

--- connect/transforms/src/main/java/org/apache/kafka/connect/transforms/Flatten.java
+++ connect/transforms/src/main/java/org/apache/kafka/connect/transforms/Flatten.java
@@ -69,7 +69,9 @@ public abstract class Flatten<R extends ConnectRecord<R>> implements Transformat
 
     @Override
     public R apply(R record) {
-        if (operatingSchema(record) == null) {
+        if (operatingValue(record) == null) {
+            return record;
+        } else if (operatingSchema(record) == null) {
             return applySchemaless(record);
         } else {
             return applyWithSchema(record);
