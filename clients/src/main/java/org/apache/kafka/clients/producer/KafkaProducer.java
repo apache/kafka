a81cdc482bb8068dd2dd04cb70b2e54d32315d8e
abbccdda <boyang@confluent.io>
2020-03-30 22:49:37 -0700

    comment revers

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -256,7 +256,6 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
     private final ApiVersions apiVersions;
     private final TransactionManager transactionManager;
 
-
     /**
      * A producer is instantiated by providing a set of key-value pairs as configuration. Valid configuration strings
      * are documented <a href="http://kafka.apache.org/documentation.html#producerconfigs">here</a>. Values can be
@@ -651,8 +650,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
      * {@code groupMetadata} should be extracted from the used {@link KafkaConsumer consumer} via
      * {@link KafkaConsumer#groupMetadata()} to leverage consumer group metadata for stronger fencing than
      * {@link #sendOffsetsToTransaction(Map, String)} which only sends with consumer group id.
-     * If broker doesn't support this new transactional API (i.e. if its version is lower than 2.5.0),
-     * this call will silently downgrade to the equivalent of {@link #sendOffsetsToTransaction(Map, String)}.
+     *
      * <p>
      * Note, that the consumer should have {@code enable.auto.commit=false} and should
      * also not commit offsets manually (via {@link KafkaConsumer#commitSync(Map) sync} or
@@ -661,7 +659,9 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
      * @throws IllegalStateException if no transactional.id has been configured or no transaction has been started.
      * @throws ProducerFencedException fatal error indicating another producer with the same transactional.id is active
      * @throws org.apache.kafka.common.errors.UnsupportedVersionException fatal error indicating the broker
-     *         does not support transactions (i.e. if its version is lower than 0.11.0.0).
+     *         does not support transactions (i.e. if its version is lower than 0.11.0.0) or
+     *         the broker doesn't support latest version of transactional API with consumer group metadata (i.e. if its version is
+     *         lower than 2.5.0).
      * @throws org.apache.kafka.common.errors.UnsupportedForMessageFormatException fatal error indicating the message
      *         format used for the offsets topic on the broker does not support transactions
      * @throws org.apache.kafka.common.errors.AuthorizationException fatal error indicating that the configured
