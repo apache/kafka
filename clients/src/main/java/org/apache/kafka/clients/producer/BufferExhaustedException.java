928b45d326e9e5bedc0efed48cb2fa905adef8d8
Sönke Liebau <soenke.liebau@opencore.com>
2020-04-01 10:32:28 +0200

    Added comment on why BufferExhaustedException subclasses TimeoutException.

--- clients/src/main/java/org/apache/kafka/clients/producer/BufferExhaustedException.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/BufferExhaustedException.java
@@ -19,8 +19,12 @@ package org.apache.kafka.clients.producer;
 import org.apache.kafka.common.errors.TimeoutException;
 
 /**
- * This exception is thrown if the producer is in non-blocking mode and the rate of data production exceeds the rate at
- * which data can be sent for long enough for the allocated buffer to be exhausted.
+ * This exception is thrown if the producer cannot allocate memory for a record within max.block.ms due to the buffer
+ * being too full.
+ *
+ * In earlier versions a TimeoutException was thrown instead of this. To keep existing catch-clauses working
+ * this class extends TimeoutException.
+ *
  */
 public class BufferExhaustedException extends TimeoutException {
 
