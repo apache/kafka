04b3032507b550aa2dd76f51fa5f91a84faa4a9f
Tom Bentley <tombentley@users.noreply.github.com>
2020-04-03 09:42:30 +0100

    Review comment
    Co-Authored-By: Manikumar Reddy <manikumar.reddy@gmail.com>

--- core/src/test/scala/kafka/utils/ToolsUtilsTest.scala
+++ core/src/test/scala/kafka/utils/ToolsUtilsTest.scala
@@ -26,7 +26,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.slf4j.LoggerFactory
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 
 class ToolsUtilsTest {
   private val log = LoggerFactory.getLogger(classOf[ToolsUtilsTest])
