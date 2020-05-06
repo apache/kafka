23d2054b28e2b92bae2a51a3cc25950bab900fff
Viktor Somogyi <viktorsomogyi@gmail.com>
2020-05-06 21:39:34 +0200

    KAFKA-6342; Remove unused workaround for JSON parsing of non-escaped strings (#8591)
    Previously we had fallback logic when parsing ACLs to handle older entries which may contain non-escaped characters. This code became dead after 1.1 since it was no longer used in the parsing of ACLs. This patch removes the fallback logic.
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>, Jason Gustafson <jason@confluent.io>

--- core/src/main/scala/kafka/utils/Json.scala
+++ core/src/main/scala/kafka/utils/Json.scala
@@ -35,16 +35,7 @@ object Json {
    */
   def parseFull(input: String): Option[JsonValue] =
     try Option(mapper.readTree(input)).map(JsonValue(_))
-    catch {
-      case _: JsonProcessingException =>
-        // Before 1.0.1, Json#encode did not escape backslash or any other special characters. SSL principals
-        // stored in ACLs may contain backslash as an escape char, making the JSON generated in earlier versions invalid.
-        // Escape backslash and retry to handle these strings which may have been persisted in ZK.
-        // Note that this does not handle all special characters (e.g. non-escaped double quotes are not supported)
-        val escapedInput = input.replaceAll("\\\\", "\\\\\\\\")
-        try Option(mapper.readTree(escapedInput)).map(JsonValue(_))
-        catch { case _: JsonProcessingException => None }
-    }
+    catch { case _: JsonProcessingException => None }
 
   /**
    * Parse a JSON string into either a generic type T, or a JsonProcessingException in the case of
