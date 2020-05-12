2a3d93dbcd9324ad3a4393541a86258df61722de
John Roesler <vvcephei@users.noreply.github.com>
2020-05-12 13:10:56 -0500

    minor test fix

--- streams/src/test/java/org/apache/kafka/streams/state/internals/MeteredTimestampedKeyValueStoreTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/MeteredTimestampedKeyValueStoreTest.java
@@ -199,7 +199,6 @@ public class MeteredTimestampedKeyValueStoreTest {
     @SuppressWarnings("resource")
     @Test
     public void shouldNotPutIfSameValuesAndGreaterTimestamp() {
-        expectLastCall();
         init();
 
         metered.put(key, valueAndTimestamp);
