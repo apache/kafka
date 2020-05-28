af17aa6c4c510bf6792b98e461ea493448639f50
Ismael Juma <ismael@juma.me.uk>
2020-05-27 21:14:16 -0700

    use_first_dns_ips -> use_first_dns_ip

--- clients/src/main/java/org/apache/kafka/clients/ClientUtils.java
+++ clients/src/main/java/org/apache/kafka/clients/ClientUtils.java
@@ -108,7 +108,7 @@ public final class ClientUtils {
 
     static List<InetAddress> resolve(String host, ClientDnsLookup clientDnsLookup) throws UnknownHostException {
         InetAddress[] addresses = InetAddress.getAllByName(host);
-        if (ClientDnsLookup.USE_FIRST_DNS_IPS == clientDnsLookup) {
+        if (ClientDnsLookup.USE_FIRST_DNS_IP == clientDnsLookup) {
             return Collections.singletonList(addresses[0]);
         } else {
             // ClientDnsLookup.USE_ALL_DNS_IPS == clientDnsLookup || ClientDnsLookup.DEFAULT == clientDnsLookup
