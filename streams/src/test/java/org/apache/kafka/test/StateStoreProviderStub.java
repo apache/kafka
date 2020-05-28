74dd371196c105603d049d4cc5d4a42f8796dca5
dima.reznik <dima.r@fiverr.com>
2020-05-28 12:04:06 +0300

    [TESTS] provide StateStoreProviderStub to query partitioned store

--- streams/src/test/java/org/apache/kafka/test/StateStoreProviderStub.java
+++ streams/src/test/java/org/apache/kafka/test/StateStoreProviderStub.java
@@ -22,16 +22,17 @@ import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.state.QueryableStoreType;
 import org.apache.kafka.streams.state.internals.StreamThreadStateStoreProvider;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.AbstractMap;
+import java.util.Map.Entry;
 
 public class StateStoreProviderStub extends StreamThreadStateStoreProvider {
 
     //<store name : partition> -> state store
-    private final Map<Map.Entry<String, Integer>, StateStore> stores = new HashMap<>();
+    private final Map<Entry<String, Integer>, StateStore> stores = new HashMap<>();
     private final boolean throwException;
 
     private final int defaultStorePartition = 0;
@@ -46,7 +47,7 @@ public class StateStoreProviderStub extends StreamThreadStateStoreProvider {
     public <T> List<T> stores(final StoreQueryParameters storeQueryParameters) {
         final String storeName = storeQueryParameters.storeName();
         final int storePartition = storeQueryParameters.partition() != null ? storeQueryParameters.partition() : 0;
-        final Map.Entry<String, Integer> stateStoreKey = new AbstractMap.SimpleEntry<>(storeName, storePartition);
+        final Entry<String, Integer> stateStoreKey = new SimpleEntry<>(storeName, storePartition);
         final QueryableStoreType<T> queryableStoreType = storeQueryParameters.queryableStoreType();
         if (throwException) {
             throw new InvalidStateStoreException("store is unavailable");
@@ -65,7 +66,6 @@ public class StateStoreProviderStub extends StreamThreadStateStoreProvider {
     public void addStore(final String storeName,
                          final int partition,
                          final StateStore store) {
-        stores.put(new AbstractMap.SimpleEntry<>(storeName, partition), store);
+        stores.put(new SimpleEntry<>(storeName, partition), store);
     }
-
 }
