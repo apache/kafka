bce24581523f955b02eb52b9e337742ee4b7a459
ableegoldman <sophie@confluent.io>
2020-06-10 10:34:31 -0700

    only catch Streams/Timeout Exceptions

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamsPartitionAssignor.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamsPartitionAssignor.java
@@ -32,9 +32,11 @@ import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.TimeoutException;
 import org.apache.kafka.common.utils.LogContext;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.common.utils.Utils;
+import org.apache.kafka.streams.errors.StreamsException;
 import org.apache.kafka.streams.errors.TaskAssignmentException;
 import org.apache.kafka.streams.processor.TaskId;
 import org.apache.kafka.streams.processor.internals.InternalTopologyBuilder.TopicsInfo;
@@ -798,7 +800,7 @@ public class StreamsPartitionAssignor implements ConsumerPartitionAssignor, Conf
                 sourceChangelogEndOffsets,
                 newlyCreatedChangelogPartitions);
             fetchEndOffsetsSuccessful = true;
-        } catch (final RuntimeException e) {
+        } catch (final StreamsException | TimeoutException e) {
             allTaskEndOffsetSums = changelogsByStatefulTask.keySet().stream().collect(Collectors.toMap(t -> t, t -> UNKNOWN_OFFSET_SUM));
             fetchEndOffsetsSuccessful = false;
         }
