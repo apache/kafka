bfb1bc04832b342e614e891d6da5af1f8ebe30a5
Chris Egerton <chrise@confluent.io>
2020-06-10 21:43:46 -0700

    KAFKA-9374: Fix race condition in failing unit test

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerConnector.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerConnector.java
@@ -273,7 +273,6 @@ public class WorkerConnector implements Runnable {
             statusListener.onFailure(connName, t);
         } finally {
             ctx.close();
-            shutdownLatch.countDown();
             metrics.close();
         }
     }
