10f1e90d0288747c579dd760701e6c0f29052ba4
David Arthur <mumrah@gmail.com>
2020-06-17 10:29:39 -0400

    Checkstyle

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/SubscriptionStateTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/SubscriptionStateTest.java
@@ -42,7 +42,6 @@ import java.util.regex.Pattern;
 import static java.util.Collections.singleton;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
