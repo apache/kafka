c73e10b84fa9e2055b1d03097b3f5a5df67c14be
admin <uuu>
2020-06-29 15:31:36 +0800

    "ADD finally to close consumerThread If exception occur"

--- examples/src/main/java/kafka/examples/KafkaConsumerProducerDemo.java
+++ examples/src/main/java/kafka/examples/KafkaConsumerProducerDemo.java
@@ -23,7 +23,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class KafkaConsumerProducerDemo {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         boolean isAsync = args.length == 0 || !args[0].trim().equalsIgnoreCase("sync");
         CountDownLatch latch = new CountDownLatch(2);
         Producer producerThread = new Producer(KafkaProperties.TOPIC, isAsync, null, false, 10000, -1, latch);
@@ -32,8 +32,14 @@ public class KafkaConsumerProducerDemo {
         Consumer consumerThread = new Consumer(KafkaProperties.TOPIC, "DemoConsumer", Optional.empty(), false, 10000, latch);
         consumerThread.start();
 
-        if (!latch.await(5, TimeUnit.MINUTES)) {
-            throw new TimeoutException("Timeout after 5 minutes waiting for demo producer and consumer to finish");
+        try {
+            if (!latch.await(1, TimeUnit.MINUTES)) {
+                throw new TimeoutException("Timeout after 5 minutes waiting for demo producer and consumer to finish");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            consumerThread.shutdown();
         }
 
         consumerThread.shutdown();
