e0233072724259e4578c7956ef73a77d13a1591c
David Jacot <djacot@confluent.io>
2020-07-16 10:43:06 +0200

    WIP

--- clients/src/test/java/org/apache/kafka/common/metrics/stats/TokenBucketComparaison.java
+++ clients/src/test/java/org/apache/kafka/common/metrics/stats/TokenBucketComparaison.java
@@ -27,7 +27,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TokenBucketComparaison {
-    Time time;
+    private Time time;
 
     @Before
     public void setup() {
@@ -35,113 +35,217 @@ public class TokenBucketComparaison {
     }
 
     @Test
-    public void compare() {
+    public void compareOneBurstSmallerThanMaxBurst() {
+        // Max Burst = 5 * 1 * 10 = 50
         MetricConfig config = new MetricConfig()
             .quota(Quota.upperBound(5))
             .timeWindow(1, TimeUnit.SECONDS)
             .samples(11);
 
-        SampledTokenBucket sampledTokenBucket = new SampledTokenBucket();
-        BackfillTokenBucket backfillTokenBucket = new BackfillTokenBucket();
-        RealTokenBucket realTokenBucket = new RealTokenBucket();
+        RealTokenBucket real = new RealTokenBucket();
+        SampledTokenBucket sampled = new SampledTokenBucket();
+        BackfillTokenBucket backfill = new BackfillTokenBucket();
 
-        sampledTokenBucket.record(config, 50, time.milliseconds());
-        backfillTokenBucket.record(config, 50, time.milliseconds());
-        realTokenBucket.record(config, 50, time.milliseconds());
+        // Record 15 at T = 0s
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
 
-        assertEquals(50, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, realTokenBucket.measure(config, time.milliseconds()), 0.1);
 
-        time.sleep(5000);
+        // Expect 15 at T = 0s
+        assertEquals(15, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(15, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(15, backfill.measure(config, time.milliseconds()), 0.1);
 
-        assertEquals(25, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(25, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(25, realTokenBucket.measure(config, time.milliseconds()), 0.1);
+        // Expect 0 at T = 3s
+        time.sleep(3000);
+        assertEquals(0, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(0, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(0, backfill.measure(config, time.milliseconds()), 0.1);
+    }
 
-        time.sleep(5000);
+    @Test
+    public void compareOneBurstEqualToMaxBurst() {
+        // Max Burst = 5 * 1 * 10 = 50
+        MetricConfig config = new MetricConfig()
+            .quota(Quota.upperBound(5))
+            .timeWindow(1, TimeUnit.SECONDS)
+            .samples(11);
 
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, realTokenBucket.measure(config, time.milliseconds()), 0.1);
+        RealTokenBucket real = new RealTokenBucket();
+        SampledTokenBucket sampled = new SampledTokenBucket();
+        BackfillTokenBucket backfill = new BackfillTokenBucket();
 
-        time.sleep(5000);
+        // Record 50 at T = 0s
+        real.record(config, 50, time.milliseconds());
+        sampled.record(config, 50, time.milliseconds());
+        backfill.record(config, 50, time.milliseconds());
 
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, realTokenBucket.measure(config, time.milliseconds()), 0.1);
+        // Expect 50 at T = 0s
+        assertEquals(50, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(50, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(50, backfill.measure(config, time.milliseconds()), 0.1);
 
-        sampledTokenBucket.record(config, 50, time.milliseconds());
-        backfillTokenBucket.record(config, 50, time.milliseconds());
-        realTokenBucket.record(config, 50, time.milliseconds());
+        // Expect 0 at T = 10s
+        time.sleep(10000);
+        assertEquals(0, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(0, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(0, backfill.measure(config, time.milliseconds()), 0.1);
+    }
 
-        assertEquals(50, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, realTokenBucket.measure(config, time.milliseconds()), 0.1);
+    @Test
+    public void compareOneBurstLargerThanMaxBurst() {
+        // Max Burst = 5 * 1 * 10 = 50
+        MetricConfig config = new MetricConfig()
+            .quota(Quota.upperBound(5))
+            .timeWindow(1, TimeUnit.SECONDS)
+            .samples(11);
 
-        time.sleep(5000);
+        RealTokenBucket real = new RealTokenBucket();
+        SampledTokenBucket sampled = new SampledTokenBucket();
+        BackfillTokenBucket backfill = new BackfillTokenBucket();
 
-        sampledTokenBucket.record(config, 50, time.milliseconds());
-        backfillTokenBucket.record(config, 50, time.milliseconds());
-        realTokenBucket.record(config, 50, time.milliseconds());
+        // Record 200 at T = 0s
+        real.record(config, 200, time.milliseconds());
+        sampled.record(config, 200, time.milliseconds());
+        backfill.record(config, 200, time.milliseconds());
 
-        assertEquals(75, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(75, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(75, realTokenBucket.measure(config, time.milliseconds()), 0.1);
+        // Expect 200 at T = 0s
+        assertEquals(200, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(200, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(200, backfill.measure(config, time.milliseconds()), 0.1);
 
-        time.sleep(5000);
+        // Expect 50 at T = 10s
+        time.sleep(10000);
+        assertEquals(150, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(150, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(150, backfill.measure(config, time.milliseconds()), 0.1);
 
-        sampledTokenBucket.record(config, 50, time.milliseconds());
-        backfillTokenBucket.record(config, 50, time.milliseconds());
-        realTokenBucket.record(config, 50, time.milliseconds());
-
-        assertEquals(100, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(100, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(100, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-
-        time.sleep(5000);
-
-        // Drop to 50 because first sample is dropped
-        assertEquals(50, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(75, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(75, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-
-        time.sleep(5000);
-
-        // Drop to 0 because second sample is dropped
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(50, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-
-        time.sleep(5000);
-
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        // Drop to 0 because last sample is dropped
-        assertEquals(0, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(25, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-
-        time.sleep(5000);
-
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-
-        sampledTokenBucket.record(config, 500, time.milliseconds());
-        backfillTokenBucket.record(config, 500, time.milliseconds());
-        realTokenBucket.record(config, 500, time.milliseconds());
-
-        for (int i = 1; i <= 10; i++) {
-            time.sleep(1000);
-            double expected = 500 - i * 5;
-            assertEquals(expected, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-            assertEquals(expected, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-            assertEquals(expected, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
-        }
-
+        // Expect 45 at T = 11s
         time.sleep(1000);
-        assertEquals(445, realTokenBucket.measure(config, time.milliseconds()), 0.1);
-        // Both drop to 0 because samples are expired
-        assertEquals(0, backfillTokenBucket.measure(config, time.milliseconds()), 0.1);
-        assertEquals(0, sampledTokenBucket.measure(config, time.milliseconds()), 0.1);
+        assertEquals(145, real.measure(config, time.milliseconds()), 0.1);
+
+        // /!\ /!\ /!\
+        // Both implementations drop to 0 because all the samples have been
+        // purged after 10s whereas the real implementation yields the correct
+        // value.
+        assertEquals(0, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(0, backfill.measure(config, time.milliseconds()), 0.1);
+    }
+
+    @Test
+    public void compareMultipleBurstsSmallerThanMaxBurst() {
+        // Max Burst = 5 * 1 * 10 = 50
+        MetricConfig config = new MetricConfig()
+            .quota(Quota.upperBound(5))
+            .timeWindow(1, TimeUnit.SECONDS)
+            .samples(11);
+
+        RealTokenBucket real = new RealTokenBucket();
+        SampledTokenBucket sampled = new SampledTokenBucket();
+        BackfillTokenBucket backfill = new BackfillTokenBucket();
+
+        // Record 15 at T = 0s
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 15 at T = 0s
+        assertEquals(15, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(15, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(15, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 2s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 20 at T = 2s
+        assertEquals(20, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(20, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(20, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 4s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 25 at T = 4s
+        assertEquals(25, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(25, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(25, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 6s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 30 at T = 6s
+        assertEquals(30, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(30, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(30, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 8s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 35 at T = 8s
+        assertEquals(35, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(35, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(35, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 10s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 40 at T = 10s
+        assertEquals(40, real.measure(config, time.milliseconds()), 0.1);
+        assertEquals(40, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(40, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 12s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 45 at T = 12s
+        assertEquals(45, real.measure(config, time.milliseconds()), 0.1);
+        // /!\ /!\ /!\
+        // The "sampled" implementation starts to differ from this point one because the
+        // oldest sample has been purged.
+        assertEquals(40, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(45, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 14s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 50 at T = 14s
+        assertEquals(50, real.measure(config, time.milliseconds()), 0.1);
+        // /!\ /!\ /!\
+        assertEquals(40, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(50, backfill.measure(config, time.milliseconds()), 0.1);
+
+        // Record 15 at T = 16s
+        time.sleep(2000);
+        real.record(config, 15, time.milliseconds());
+        sampled.record(config, 15, time.milliseconds());
+        backfill.record(config, 15, time.milliseconds());
+
+        // Expect 55 at T = 16s
+        assertEquals(55, real.measure(config, time.milliseconds()), 0.1);
+        // /!\ /!\ /!\
+        assertEquals(40, sampled.measure(config, time.milliseconds()), 0.1);
+        assertEquals(55, backfill.measure(config, time.milliseconds()), 0.1);
     }
 }
