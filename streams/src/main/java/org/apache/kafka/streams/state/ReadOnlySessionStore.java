026e762953a6ae9ec1e24917981df96c1a7f80d3
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-07-23 14:36:14 +0100

    default new methods

--- streams/src/main/java/org/apache/kafka/streams/state/ReadOnlySessionStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/ReadOnlySessionStore.java
@@ -41,41 +41,57 @@ public interface ReadOnlySessionStore<K, AGG> {
      * @return iterator of sessions with the matching key and aggregated values
      * @throws NullPointerException If null is used for key.
      */
-    KeyValueIterator<Windowed<K>, AGG> findSessions(final K key, final long earliestSessionEndTime, final long latestSessionStartTime);
+    default KeyValueIterator<Windowed<K>, AGG> findSessions(final K key,
+                                                            final long earliestSessionEndTime,
+                                                            final long latestSessionStartTime) {
+        throw new UnsupportedOperationException("Operation moved from SessionStore");
+    }
 
-    default KeyValueIterator<Windowed<K>, AGG> backwardFindSessions(final K key, final long earliestSessionEndTime, final long latestSessionStartTime) {
+    default KeyValueIterator<Windowed<K>, AGG> backwardFindSessions(final K key,
+                                                                    final long earliestSessionEndTime,
+                                                                    final long latestSessionStartTime) {
         throw new UnsupportedOperationException();
     }
 
     /**
      * Fetch any sessions in the given range of keys and the sessions end is &ge; earliestSessionEndTime and the sessions
      * start is &le; latestSessionStartTime
-     *
+     * <p>
      * This iterator must be closed after use.
      *
-     * @param keyFrom The first key that could be in the range
-     * @param keyTo The last key that could be in the range
+     * @param keyFrom                The first key that could be in the range
+     * @param keyTo                  The last key that could be in the range
      * @param earliestSessionEndTime the end timestamp of the earliest session to search for
      * @param latestSessionStartTime the end timestamp of the latest session to search for
      * @return iterator of sessions with the matching keys and aggregated values
      * @throws NullPointerException If null is used for any key.
      */
-    KeyValueIterator<Windowed<K>, AGG> findSessions(final K keyFrom, final K keyTo, final long earliestSessionEndTime, final long latestSessionStartTime);
+    default KeyValueIterator<Windowed<K>, AGG> findSessions(final K keyFrom,
+                                                            final K keyTo,
+                                                            final long earliestSessionEndTime,
+                                                            final long latestSessionStartTime) {
+        throw new UnsupportedOperationException("Operation moved from SessionStore");
+    }
 
-    default KeyValueIterator<Windowed<K>, AGG> backwardFindSessions(final K keyFrom, final K keyTo, final long earliestSessionEndTime, final long latestSessionStartTime) {
+    default KeyValueIterator<Windowed<K>, AGG> backwardFindSessions(final K keyFrom,
+                                                                    final K keyTo,
+                                                                    final long earliestSessionEndTime,
+                                                                    final long latestSessionStartTime) {
         throw new UnsupportedOperationException();
     }
 
     /**
      * Get the value of key from a single session.
      *
-     * @param key            the key to fetch
-     * @param startTime      start timestamp of the session
-     * @param endTime        end timestamp of the session
+     * @param key       the key to fetch
+     * @param startTime start timestamp of the session
+     * @param endTime   end timestamp of the session
      * @return The value or {@code null} if no session associated with the key can be found
      * @throws NullPointerException If {@code null} is used for any key.
      */
-    AGG fetchSession(final K key, final long startTime, final long endTime);
+    default AGG fetchSession(final K key, final long startTime, final long endTime) {
+        throw new UnsupportedOperationException("Operation moved from SessionStore");
+    }
 
     /**
      * Retrieve all aggregated sessions for the provided key.
