89978e1dcfcc8873f2898ae47107b897c2813196
Randall Hauch <rhauch@gmail.com>
2020-08-12 17:42:12 -0500

    KAFKA-9066: Retain metrics for failed tasks (#8502) (#8854)
    Author: Chris Egerton <chrise@confluent.io>
    Reviewers: Nigel Liang <nigel@nigelliang.com>, Konstantine Karantasis <konstantine@confluent.io>, Randall Hauch <rhauch@gmail.com>

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Worker.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Worker.java
@@ -733,12 +733,17 @@ public class Worker {
                 return;
             }
 
-            connectorStatusMetricsGroup.recordTaskRemoved(taskId);
             if (!task.awaitStop(timeout)) {
                 log.error("Graceful stop of task {} failed.", task.id());
                 task.cancel();
             } else {
                 log.debug("Graceful stop of task {} succeeded.", task.id());
+            }
+
+            try {
+                task.removeMetrics();
+            } finally {
+                connectorStatusMetricsGroup.recordTaskRemoved(taskId);
             }
         }
     }
