e9616c64dfdc33481d0b831f80ecd0385801c761
Joel Wee <joel.wee@outlook.com>
2020-08-26 11:55:01 +0800

    KAFKA-10277: refactor join processor

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamKTableJoinProcessor.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamKTableJoinProcessor.java
@@ -25,8 +25,6 @@ import org.apache.kafka.streams.processor.internals.metrics.StreamsMetricsImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-
 import static org.apache.kafka.streams.processor.internals.metrics.TaskMetrics.droppedRecordsSensorOrSkippedRecordsSensor;
 import static org.apache.kafka.streams.state.ValueAndTimestamp.getValueOrNull;
 
@@ -67,37 +65,19 @@ class KStreamKTableJoinProcessor<K1, K2, V1, V2, R> extends AbstractProcessor<K1
         // an empty message (ie, there is nothing to be joined) -- this contrast SQL NULL semantics
         // furthermore, on left/outer joins 'null' in ValueJoiner#apply() indicates a missing record --
         // thus, to be consistent and to avoid ambiguous null semantics, null values are ignored
-        final Optional<K2> maybeMappedKey = maybeExtractMappedKey(key, value);
-        if (!maybeMappedKey.isPresent()) {
+        final K2 mappedKey = keyMapper.apply(key, value);
+        if (mappedKey == null || value == null) {
             LOG.warn(
                 "Skipping record due to null key or value. key=[{}] value=[{}] topic=[{}] partition=[{}] offset=[{}]",
                 key, value, context().topic(), context().partition(), context().offset()
             );
             droppedRecordsSensor.record();
         } else {
-            final K2 mappedKey = maybeMappedKey.get();
             final V2 value2 = getValueOrNull(valueGetter.get(mappedKey));
             if (leftJoin || value2 != null) {
                 context().forward(key, joiner.apply(value, value2));
             }
         }
-    }
-
-    private Optional<K2> maybeExtractMappedKey(final K1 key, final V1 value) {
-        if (value == null) {
-            return Optional.empty();
-        }
-
-        // we allow the case where the key is null but mappedKey is not null and thus
-        // we need to guard against nullPointerExceptions. This may happen for GlobalKTables.
-        // For KTables, the keyMapper simply returns the key, so this will never happen
-        Optional<K2> maybeMappedKey;
-        try {
-            maybeMappedKey = Optional.ofNullable(keyMapper.apply(key, value));
-        } catch (final NullPointerException e) {
-            maybeMappedKey = Optional.empty();
-        }
-        return maybeMappedKey;
     }
 
     @Override
