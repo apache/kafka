29694d06d3a5e4deb2cb04daf908fa7c8c94c5a7
Nikolay Izhikov <nizhikov@apache.org>
2020-09-21 11:58:49 +0300

    KAFKA-10505: Regexp fix.

--- tests/kafkatest/tests/streams/utils/util.py
+++ tests/kafkatest/tests/streams/utils/util.py
@@ -41,5 +41,5 @@ def extract_generation_id(generation):
     # Generation string looks like
     # "Generation{generationId=5,memberId='consumer-A-3-72d7be15-bcdd-4032-b247-784e648d4dd8',protocol='stream'} "
     # Extracting generationId from it.
-    m = re.search(r'Generation{generationId=(\d),.*', generation)
+    m = re.search(r'Generation{generationId=(\d+),.*', generation)
     return int(m.group(1))
