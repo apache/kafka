7a94ebbf829a394e10986aabbfe8e28687a2d6c3
Ron Dagostino <rdagostino@confluent.io>
2020-09-30 12:42:01 -0400

    Fix SaslServerAuthenticator as well

--- clients/src/main/java/org/apache/kafka/common/security/authenticator/SaslServerAuthenticator.java
+++ clients/src/main/java/org/apache/kafka/common/security/authenticator/SaslServerAuthenticator.java
@@ -190,11 +190,15 @@ public class SaslServerAuthenticator implements Authenticator {
         if (mechanism.equals(SaslConfigs.GSSAPI_MECHANISM)) {
             saslServer = createSaslKerberosServer(callbackHandler, configs, subject);
         } else {
+            String failureErrorMessage = "Kafka Server failed to create a SaslServer to interact with a client during session authentication";
             try {
                 saslServer = Subject.doAs(subject, (PrivilegedExceptionAction<SaslServer>) () ->
                     Sasl.createSaslServer(saslMechanism, "kafka", serverAddress().getHostName(), configs, callbackHandler));
+                if (saslServer == null) {
+                    throw new SaslException(failureErrorMessage);
+                }
             } catch (PrivilegedActionException e) {
-                throw new SaslException("Kafka Server failed to create a SaslServer to interact with a client during session authentication", e.getCause());
+                throw new SaslException(failureErrorMessage, e.getCause());
             }
         }
     }
