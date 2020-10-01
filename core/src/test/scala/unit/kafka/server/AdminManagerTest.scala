492f31fcc21b997c4c0057a3d5ba4c90f93112f9
Edoardo Comar <ecomar@uk.ibm.com>
2020-10-01 12:23:20 +0100

    KAFKA-10557: Missing docs when describing topic configs including documentation
    Co-authored-by: Edoardo Comar <ecomar@uk.ibm.com>
    Co-authored-by: Mickael Maison <mickael.maison@gmail.com>

--- core/src/test/scala/unit/kafka/server/AdminManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/AdminManagerTest.scala
@@ -29,6 +29,7 @@ import org.apache.kafka.common.protocol.Errors
 import org.junit.{After, Test}
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
 
 class AdminManagerTest {
 
@@ -79,5 +80,31 @@ class AdminManagerTest {
     val results: List[DescribeConfigsResponseData.DescribeConfigsResult] = adminManager.describeConfigs(resources, true, true)
     assertEquals(Errors.NONE.code, results.head.errorCode())
     assertFalse("Should return configs", results.head.configs().isEmpty)
+  }
+
+  @Test
+  def testDescribeConfigsWithDocumentation(): Unit = {
+    EasyMock.expect(zkClient.getEntityConfigs(ConfigType.Topic, topic)).andReturn(new java.util.Properties)
+    EasyMock.expect(zkClient.getEntityConfigs(ConfigType.Broker, brokerId.toString)).andReturn(TestUtils.createBrokerConfig(brokerId,"zk"))
+    EasyMock.expect(metadataCache.contains(topic)).andReturn(true)
+    EasyMock.replay(zkClient, metadataCache)
+
+    val adminManager = createAdminManager()
+
+    val resources = List(
+      new DescribeConfigsRequestData.DescribeConfigsResource()
+        .setResourceName(topic)
+        .setResourceType(ConfigResource.Type.TOPIC.id),
+      new DescribeConfigsRequestData.DescribeConfigsResource()
+        .setResourceName(brokerId.toString)
+        .setResourceType(ConfigResource.Type.BROKER.id))
+
+    val results: List[DescribeConfigsResponseData.DescribeConfigsResult] = adminManager.describeConfigs(resources, true, true)
+    assertEquals(2, results.size)
+    results.foreach(r => {
+      assertEquals(Errors.NONE.code, r.errorCode)
+      assertFalse("Should return configs", r.configs.isEmpty)
+      r.configs.forEach(c => assertNotNull(s"Config ${c.name} should have documentation", c.documentation))
+    })
   }
 }
