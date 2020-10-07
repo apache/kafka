d6e4d060bc0cb446ef1f493fae3d0a9426227e02
Samuel Cantero <scanterog@gmail.com>
2020-10-07 18:38:00 -0300

    [mm2] Add tests for {source,target}AdminConfig

--- connect/mirror/src/test/java/org/apache/kafka/connect/mirror/MirrorConnectorConfigTest.java
+++ connect/mirror/src/test/java/org/apache/kafka/connect/mirror/MirrorConnectorConfigTest.java
@@ -184,4 +184,56 @@ public class MirrorConnectorConfigTest {
         assertEquals(expectedProducerProps, connectorProducerProps);
     }
 
+    @Test
+    public void testSourceAdminConfig() {
+        Map<String, String> connectorProps = makeProps(
+                MirrorConnectorConfig.ADMIN_CLIENT_PREFIX +
+                        "connections.max.idle.ms", "10000"
+        );
+        MirrorConnectorConfig config = new MirrorConnectorConfig(connectorProps);
+        Map<String, Object> connectorAdminProps = config.sourceAdminConfig();
+        Map<String, Object> expectedAdminProps = new HashMap<>();
+        expectedAdminProps.put("connections.max.idle.ms", "10000");
+        assertEquals(expectedAdminProps, connectorAdminProps);
+    }
+
+    @Test
+    public void testSourceAdminConfigWithSourcePrefix() {
+        Map<String, String> connectorProps = makeProps(
+                MirrorConnectorConfig.SOURCE_ADMIN_CLIENT_PREFIX +
+                        "connections.max.idle.ms", "10000"
+        );
+        MirrorConnectorConfig config = new MirrorConnectorConfig(connectorProps);
+        Map<String, Object> connectorAdminProps = config.sourceAdminConfig();
+        Map<String, Object> expectedAdminProps = new HashMap<>();
+        expectedAdminProps.put("connections.max.idle.ms", "10000");
+        assertEquals(expectedAdminProps, connectorAdminProps);
+    }
+
+    @Test
+    public void testTargetAdminConfig() {
+        Map<String, String> connectorProps = makeProps(
+                MirrorConnectorConfig.ADMIN_CLIENT_PREFIX +
+                        "connections.max.idle.ms", "10000"
+        );
+        MirrorConnectorConfig config = new MirrorConnectorConfig(connectorProps);
+        Map<String, Object> connectorAdminProps = config.targetAdminConfig();
+        Map<String, Object> expectedAdminProps = new HashMap<>();
+        expectedAdminProps.put("connections.max.idle.ms", "10000");
+        assertEquals(expectedAdminProps, connectorAdminProps);
+    }
+
+    @Test
+    public void testTargetAdminConfigWithSourcePrefix() {
+        Map<String, String> connectorProps = makeProps(
+                MirrorConnectorConfig.TARGET_ADMIN_CLIENT_PREFIX +
+                        "connections.max.idle.ms", "10000"
+        );
+        MirrorConnectorConfig config = new MirrorConnectorConfig(connectorProps);
+        Map<String, Object> connectorAdminProps = config.targetAdminConfig();
+        Map<String, Object> expectedAdminProps = new HashMap<>();
+        expectedAdminProps.put("connections.max.idle.ms", "10000");
+        assertEquals(expectedAdminProps, connectorAdminProps);
+    }
+
 }
