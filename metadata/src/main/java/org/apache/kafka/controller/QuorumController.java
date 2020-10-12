aa1ee354317536efedb24b2326f49af278dc98dc
Colin P. Mccabe <cmccabe@confluent.io>
2020-10-12 16:38:18 -0700

    More controller improvements
    QuorumController: add ControlEvent.  Reorganize event end handling.
    Add MetaLogListener.  Add renounce().
    
    SnapshotRegistry: add logging.  Add deleteSnapshotsUpTo.

--- metadata/src/main/java/org/apache/kafka/controller/QuorumController.java
+++ metadata/src/main/java/org/apache/kafka/controller/QuorumController.java
@@ -25,6 +25,10 @@ import org.apache.kafka.common.errors.ApiException;
 import org.apache.kafka.common.errors.NotControllerException;
 import org.apache.kafka.common.errors.UnknownServerException;
 import org.apache.kafka.common.errors.UnsupportedVersionException;
+import org.apache.kafka.common.metadata.ConfigRecord;
+import org.apache.kafka.common.metadata.MetadataRecordType;
+import org.apache.kafka.common.protocol.ApiMessage;
+import org.apache.kafka.common.protocol.ApiMessageAndVersion;
 import org.apache.kafka.common.protocol.Errors;
 import org.apache.kafka.common.requests.ApiError;
 import org.apache.kafka.common.utils.EventQueue;
@@ -36,12 +40,14 @@ import org.apache.kafka.timeline.SnapshotRegistry;
 import org.slf4j.Logger;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public final class QuorumController implements Controller {
     /**
@@ -106,8 +112,63 @@ public final class QuorumController implements Controller {
         }
     }
 
+    private void handleEventEnd(String name, long startProcessingTimeNs) {
+        long endProcessingTime = time.nanoseconds();
+        long deltaNs = endProcessingTime - startProcessingTimeNs;
+        log.debug("Processed {} in {} ns", name, deltaNs);
+    }
+
+    private Throwable handleEventException(String name, long startProcessingTimeNs,
+                                           Throwable exception) {
+        long endProcessingTime = time.nanoseconds();
+        long deltaNs = endProcessingTime - startProcessingTimeNs;
+        log.info("{}: failed with {} in ns", name,
+            exception.getClass().getSimpleName(), deltaNs);
+        if (exception instanceof ApiException) {
+            return exception;
+        } else {
+            log.info("Renouncing the leadership at epoch {} due to an unknown server " +
+                "exception. Reverting to last committed offset {}.", curClaimEpoch,
+                lastCommittedOffset);
+            renounce();
+            return new UnknownServerException(exception);
+        }
+    }
+
     /**
-     * A controller event that reads the committed internal state.
+     * A controller event for handling internal state changes, such as Raft inputs.
+     */
+    class ControlEvent<T> implements EventQueue.Event {
+        private final String name;
+        private final Runnable handler;
+        private long startProcessingTimeNs;
+
+        ControlEvent(String name, Runnable handler) {
+            this.name = name;
+            this.handler = handler;
+        }
+
+        @Override
+        public void run() throws Exception {
+            startProcessingTimeNs = time.nanoseconds();
+            handler.run();
+            handleEventEnd(name, startProcessingTimeNs);
+        }
+
+        @Override
+        public void handleException(Throwable exception) {
+            handleEventException(name, startProcessingTimeNs, exception);
+        }
+    }
+
+    private void appendControlEvent(String name, Runnable handler) {
+        ControlEvent event = new ControlEvent(name, handler);
+        queue.append(event);
+    }
+
+    /**
+     * A controller event that reads the committed internal state in order to expose it
+     * to an API.
      */
     class ControllerReadEvent<T> implements EventQueue.Event {
         private final String name;
@@ -129,29 +190,15 @@ public final class QuorumController implements Controller {
         @Override
         public void run() throws Exception {
             startProcessingTimeNs = time.nanoseconds();
-            complete(handler.get(), null);
+            T value = handler.get();
+            handleEventEnd(name, startProcessingTimeNs);
+            future.complete(value);
         }
 
         @Override
         public void handleException(Throwable exception) {
-            complete(null, exception);
-        }
-
-        private void complete(T result, Throwable exception) {
-            long endProcessingTime = time.nanoseconds();
-            long deltaNs = endProcessingTime - startProcessingTimeNs;
-            if (exception != null) {
-                log.info("{}: failed with {} in ns", name,
-                    exception.getClass().getSimpleName(), deltaNs);
-                if (exception instanceof ApiException) {
-                    future.completeExceptionally(exception);
-                } else {
-                    future.completeExceptionally(new UnknownServerException(exception));
-                }
-            } else {
-                log.info("Processed {} in {} ns", name, deltaNs);
-                future.complete(result);
-            }
+            future.completeExceptionally(
+                handleEventException(name, startProcessingTimeNs, exception));
         }
     }
 
@@ -185,7 +232,8 @@ public final class QuorumController implements Controller {
 
         @Override
         public void run() throws Exception {
-            if (activeState == null) {
+            long controllerEpoch = curClaimEpoch;
+            if (controllerEpoch == -1) {
                 throw new NotControllerException("Node " + nodeId + " is not the " +
                     "current controller.");
             }
@@ -214,9 +262,13 @@ public final class QuorumController implements Controller {
                 // written before we can return our result to the user.  Here, we hand off
                 // the batch of records to the metadata log manager.  They will be written
                 // out asynchronously.
-                long offset = logManager.scheduleWrite(activeState.epoch(), result.records());
+                long offset = logManager.scheduleWrite(controllerEpoch, result.records());
                 this.resultAndOffset = new ControllerResultAndOffset<>(offset,
                     result.records(), result.response());
+                for (ApiMessageAndVersion message : result.records()) {
+                    replay(message.message());
+                }
+                snapshotRegistry.createSnapshot(offset);
             }
             purgatory.add(resultAndOffset.offset(), this);
         }
@@ -252,27 +304,161 @@ public final class QuorumController implements Controller {
         return event.future();
     }
 
-    static class ActiveState {
-        private final long epoch;
+    class MetaLogListener implements MetaLogManager.Listener {
+        @Override
+        public void handleCommits(long offset, List<ApiMessage> messages) {
+            appendControlEvent("handleCommits", () -> {
+                if (curClaimEpoch == -1) {
+                    if (log.isDebugEnabled()) {
+                        if (log.isTraceEnabled()) {
+                            log.trace("Replaying commits from the active node up to " +
+                                "offset {}: {}.", offset, messages.stream().
+                                map(m -> m.toString()).collect(Collectors.joining(", ")));
+                        } else {
+                            log.debug("Replaying commits from the active node up to " +
+                                "offset {}.", offset);
+                        }
+                    }
+                    for (ApiMessage message : messages) {
+                        replay(message);
+                    }
+                } else {
+                    log.debug("Completing purgatory items up to offset {}.", offset);
 
-        ActiveState(long epoch) {
-            this.epoch = epoch;
+                    // Complete any events in the purgatory that were waiting for this offset.
+                    purgatory.completeUpTo(offset);
+
+                    // Delete all snapshots older than the offset.
+                    // TODO: add an exception here for when we're writing out a log snapshot
+                    snapshotRegistry.deleteSnapshotsUpTo(offset);
+                }
+                lastCommittedOffset = offset;
+            });
         }
 
-        long epoch() {
-            return epoch;
+        @Override
+        public void handleClaim(long epoch) {
+            appendControlEvent("handleClaim", () -> {
+                long curEpoch = curClaimEpoch;
+                if (curEpoch == -1) {
+                    throw new RuntimeException("Tried to claim controller epoch " +
+                        curEpoch + ", but we never renounced controller epoch " +
+                        curEpoch);
+                }
+                log.info("Becoming active at controller epoch {}.", epoch);
+                curClaimEpoch = epoch;
+            });
+        }
+
+        @Override
+        public void handleRenounce(long epoch) {
+            appendControlEvent("handleClaim", () -> {
+                if (curClaimEpoch == epoch) {
+                    log.info("Renouncing the leadership at epoch {} due to a metadata " +
+                            "log event. Reverting to last committed offset {}.", curClaimEpoch,
+                        lastCommittedOffset);
+                    renounce();
+                }
+            });
+        }
+
+        @Override
+        public void beginShutdown() {
+            log.info("Shutting down the controller because the metadata log requested it.");
+            QuorumController.this.beginShutdown();
+        }
+
+        @Override
+        public long currentClaimEpoch() {
+            return curClaimEpoch;
+        }
+    }
+
+    private void renounce() {
+        curClaimEpoch = -1;
+        purgatory.failAll(new NotControllerException("Node " + nodeId +
+            " is no longer the controller."));
+        snapshotRegistry.revertToSnapshot(lastCommittedOffset);
+        snapshotRegistry.deleteSnapshotsUpTo(lastCommittedOffset);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void replay(ApiMessage message) {
+        MetadataRecordType type = MetadataRecordType.fromId(message.apiKey());
+        switch (type) {
+            case BROKER_RECORD:
+                throw new RuntimeException("Unhandled record type " + type);
+            case TOPIC_RECORD:
+                throw new RuntimeException("Unhandled record type " + type);
+            case PARTITION_RECORD:
+                throw new RuntimeException("Unhandled record type " + type);
+            case CONFIG_RECORD:
+                configurationControlManager.replay((ConfigRecord) message);
+                break;
+            case ISR_CHANGE_RECORD:
+                throw new RuntimeException("Unhandled record type " + type);
+            case ACCESS_CONTROL_RECORD:
+                throw new RuntimeException("Unhandled record type " + type);
         }
     }
 
     private final Logger log;
+
+    /**
+     * The ID of this controller node.
+     */
     private final int nodeId;
+
+    /**
+     * The single-threaded queue that processes all of our events.
+     * It also processes timeouts.
+     */
     private final KafkaEventQueue queue;
+
+    /**
+     * The Kafka clock object to use.
+     */
     private final Time time;
+
+    /**
+     * A registry for snapshot data.  This must be accessed only by the event queue thread.
+     */
     private final SnapshotRegistry snapshotRegistry;
+
+    /**
+     * The purgatory which holds deferred operations which are waiting for the metadata
+     * log's high water mark to advance.  This must be accessed only by the event queue thread.
+     */
     private final ControllerPurgatory purgatory;
+
+    /**
+     * An object which stores the controller's dynamic configuration.
+     * This must be accessed only by the event queue thread.
+     */
     private final ConfigurationControlManager configurationControlManager;
+
+    /**
+     * The interface that we use to mutate the Raft log.
+     */
     private final MetaLogManager logManager;
-    private ActiveState activeState;
+
+    /**
+     * The interface that receives callbacks from the Raft log.  These callbacks are
+     * invoked from the Raft thread(s), not from the controller thread.
+     */
+    private final MetaLogListener metaLogListener;
+
+    /**
+     * If this controller is active, this is the non-negative controller epoch.
+     * Otherwise, this is -1.  This variable must be modified only from the controller
+     * thread, but it can be read from other threads.
+     */
+    private volatile long curClaimEpoch;
+
+    /**
+     * The last offset we have committed, or -1 if we have not committed any offsets.
+     */
+    private long lastCommittedOffset;
 
     private QuorumController(LogContext logContext,
                              int nodeId,
@@ -284,13 +470,16 @@ public final class QuorumController implements Controller {
         this.nodeId = nodeId;
         this.queue = queue;
         this.time = time;
-        this.snapshotRegistry = new SnapshotRegistry(-1);
+        this.snapshotRegistry = new SnapshotRegistry(logContext, -1);
         snapshotRegistry.createSnapshot(-1);
         this.purgatory = new ControllerPurgatory();
         this.configurationControlManager =
             new ConfigurationControlManager(snapshotRegistry, configDefs);
         this.logManager = logManager;
-        this.activeState = null;
+        this.metaLogListener = new MetaLogListener();
+        this.curClaimEpoch = -1L;
+        this.lastCommittedOffset = -1L;
+        this.logManager.initialize(metaLogListener);
     }
 
     @Override
