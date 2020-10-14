20b5c20926292c3396c5be6bd845f01bff526fec
Colin P. Mccabe <cmccabe@confluent.io>
2020-10-14 15:08:29 -0700

    Add BrokerState enum

---
+++ metadata/src/main/java/org/apache/kafka/metadata/BrokerState.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kafka.metadata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The broker state.
+ *
+ * The numeric values used here are part of Kafka's public API.  They appear in metrics,
+ * and are also sent over the wire in some cases.
+ */
+public enum BrokerState {
+    /**
+     * The state the broker is in when it first starts up, and hasn't acquired a
+     * lease.
+     */
+    NOT_RUNNING((byte) 0),
+
+    /**
+     * The state the broker is in when it has a lease, but isn't ready to start
+     * handling client requests.  Note: in pre-500-brokers this state was
+     * previously known as Starting.
+     */
+    FENCED((byte) 1),
+
+    /**
+     * The state the broker is in when it has a lease, but is still recovering
+     * from an unclean shutdown.
+     */
+    RECOVERING_FROM_UNCLEAN_SHUTDOWN((byte) 2),
+
+    /**
+     * The state the broker is in when it is running and accepting client
+     * requests.
+     */
+    RUNNING((byte) 3),
+
+    /**
+     * The state the broker is in when it is attempting to perform a controlled
+     * shutdown.
+     */
+    PENDING_CONTROLLED_SHUTDOWN((byte) 6),
+
+    /**
+     * The state the broker is in when it is shutting down.
+     */
+    SHUTTING_DOWN((byte) 7),
+
+    /**
+     * The broker is in an unknown state.
+     */
+    UNKNOWN((byte) 127);
+
+    private final static Map<Byte, BrokerState> VALUES_TO_ENUMS = new HashMap<>();
+
+    static {
+        for (BrokerState state : BrokerState.values()) {
+            VALUES_TO_ENUMS.put(state.value(), state);
+        }
+    }
+
+    private final byte value;
+
+    BrokerState(byte value) {
+        this.value = value;
+    }
+
+    public static BrokerState fromValue(byte value) {
+        BrokerState state = VALUES_TO_ENUMS.get(value);
+        if (state == null) {
+            return UNKNOWN;
+        }
+        return state;
+    }
+
+    public byte value() {
+        return value;
+    }
+}
