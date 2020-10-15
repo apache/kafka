10808ca01c3c2de974803b1bdf848a27a057f3de
Guozhang Wang <wangguoz@gmail.com>
2020-10-14 22:22:40 -0700

    github comments

--- core/src/main/scala/kafka/coordinator/group/GroupCoordinator.scala
+++ core/src/main/scala/kafka/coordinator/group/GroupCoordinator.scala
@@ -908,7 +908,7 @@ class GroupCoordinator(val brokerId: Int,
    * @param offsetTopicPartitionId The partition we are now leading
    */
   def onElection(offsetTopicPartitionId: Int): Unit = {
-    info(s"Becoming the group coordinator for partition $offsetTopicPartitionId")
+    info(s"Electing as the group coordinator for partition $offsetTopicPartitionId")
     groupManager.scheduleLoadGroupAndOffsets(offsetTopicPartitionId, onGroupLoaded)
   }
 
@@ -918,7 +918,7 @@ class GroupCoordinator(val brokerId: Int,
    * @param offsetTopicPartitionId The partition we are no longer leading
    */
   def onResignation(offsetTopicPartitionId: Int): Unit = {
-    info(s"Resigning the group coordinator for partition $offsetTopicPartitionId")
+    info(s"Resigning as the group coordinator for partition $offsetTopicPartitionId")
     groupManager.removeGroupsForPartition(offsetTopicPartitionId, onGroupUnloaded)
   }
 
