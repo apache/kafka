98233b7821cfb4bf618be1628e58e9a332d6c247
Colin P. Mccabe <cmccabe@confluent.io>
2020-10-26 16:30:52 -0700

    Kip500Controller: implement API_VERSIONS

--- core/src/main/scala/kafka/server/ControllerApis.scala
+++ core/src/main/scala/kafka/server/ControllerApis.scala
@@ -24,12 +24,15 @@ import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.acl.AclOperation.CLUSTER_ACTION
 import org.apache.kafka.common.errors.ApiException
 import org.apache.kafka.common.internals.FatalExitError
-import org.apache.kafka.common.protocol.ApiKeys
-import org.apache.kafka.common.requests.AlterIsrRequest
+import org.apache.kafka.common.message.ApiVersionsResponseData
+import org.apache.kafka.common.message.ApiVersionsResponseData.{ApiVersionsResponseKey, FinalizedFeatureKey, SupportedFeatureKey}
+import org.apache.kafka.common.protocol.{ApiKeys, Errors}
+import org.apache.kafka.common.requests.{AlterIsrRequest, ApiVersionsRequest, ApiVersionsResponse}
 import org.apache.kafka.common.resource.Resource.CLUSTER_NAME
 import org.apache.kafka.common.resource.ResourceType.CLUSTER
 import org.apache.kafka.common.utils.Time
 import org.apache.kafka.controller.{Controller, LeaderAndIsr}
+import org.apache.kafka.metadata.FeatureManager
 import org.apache.kafka.server.authorizer.Authorizer
 
 import scala.collection.mutable
@@ -42,13 +45,20 @@ class ControllerApis(val requestChannel: RequestChannel,
                      val authorizer: Option[Authorizer],
                      val quotas: QuotaManagers,
                      val time: Time,
+                     val featureManager: FeatureManager,
                      val controller: Controller) extends ApiRequestHandler with Logging {
 
   val apisUtil = new ApisUtils(requestChannel, authorizer, quotas, time)
 
+  val supportedApiKeys = Set(
+    ApiKeys.API_VERSIONS,
+    ApiKeys.ALTER_ISR
+  )
+
   override def handle(request: RequestChannel.Request): Unit = {
     try {
       request.header.apiKey match {
+        case ApiKeys.API_VERSIONS => handleApiVersionsRequest(request)
         case ApiKeys.ALTER_ISR => handleAlterIsrRequest(request)
           // TODO other APIs
         case _ => throw new ApiException(s"Unsupported ApiKey ${request.context.header.apiKey()}")
@@ -59,6 +69,47 @@ class ControllerApis(val requestChannel: RequestChannel,
     } finally {
 
     }
+  }
+
+  def handleApiVersionsRequest(request: RequestChannel.Request): Unit = {
+    // Note that broker returns its full list of supported ApiKeys and versions regardless of current
+    // authentication state (e.g., before SASL authentication on an SASL listener, do note that no
+    // Kafka protocol requests may take place on an SSL listener before the SSL handshake is finished).
+    // If this is considered to leak information about the broker version a workaround is to use SSL
+    // with client authentication which is performed at an earlier stage of the connection where the
+    // ApiVersionRequest is not available.
+    def createResponseCallback(requestThrottleMs: Int): ApiVersionsResponse = {
+      val apiVersionRequest = request.body[ApiVersionsRequest]
+      if (apiVersionRequest.hasUnsupportedRequestVersion)
+        apiVersionRequest.getErrorResponse(requestThrottleMs, Errors.UNSUPPORTED_VERSION.exception)
+      else if (!apiVersionRequest.isValid)
+        apiVersionRequest.getErrorResponse(requestThrottleMs, Errors.INVALID_REQUEST.exception)
+      else {
+        val finalized = featureManager.finalizedFeatures()
+        val data = new ApiVersionsResponseData().
+          setErrorCode(0.toShort).
+          setThrottleTimeMs(requestThrottleMs).
+          setFinalizedFeaturesEpoch(finalized.epoch())
+        featureManager.supportedFeatures().asScala.foreach {
+          case (k, v) => data.supportedFeatures().add(new SupportedFeatureKey().
+            setName(k).setMaxVersion(v.max()).setMinVersion(v.min()))
+        }
+        finalized.finalizedFeatures().asScala.foreach {
+          case (k, v) => data.finalizedFeatures().add(new FinalizedFeatureKey().
+            setName(k).setMaxVersionLevel(v.max()).setMinVersionLevel(v.min()))
+        }
+        ApiKeys.enabledApis().asScala.foreach {
+          case key => if (supportedApiKeys.contains(key)) {
+            data.apiKeys().add(new ApiVersionsResponseKey().
+              setApiKey(key.id).
+              setMaxVersion(key.latestVersion()).
+              setMinVersion(key.oldestVersion()))
+          }
+        }
+        new ApiVersionsResponse(data)
+      }
+    }
+    apisUtil.sendResponseMaybeThrottle(request, createResponseCallback)
   }
 
   def handleAlterIsrRequest(request: RequestChannel.Request): Unit = {
