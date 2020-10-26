3bfbe682a2246313d126045f2c5e1a0c2577bbd2
Andrey Falko <afalko@users.noreply.github.com>
2020-10-25 22:06:56 -0700

    KAFKA-10092: Remove unnecessary contructor and exception in NioEchoServer (#8794)
    Reviewers: notifygd, Andrew Choi <andrew.choi@uwaterloo.ca>, Jakob Homan, Chia-Ping Tsai <chia7712@gmail.com>

--- clients/src/test/java/org/apache/kafka/common/network/NioEchoServer.java
+++ clients/src/test/java/org/apache/kafka/common/network/NioEchoServer.java
@@ -60,11 +60,7 @@ public class NioEchoServer extends Thread {
     public enum MetricType {
         TOTAL, RATE, AVG, MAX;
 
-        private final String metricNameSuffix;
-
-        private MetricType() {
-            metricNameSuffix = "-" + name().toLowerCase(Locale.ROOT);
-        }
+        private final String metricNameSuffix = "-" + name().toLowerCase(Locale.ROOT);
 
         public String metricNameSuffix() {
             return metricNameSuffix;
@@ -316,7 +312,7 @@ public class NioEchoServer extends Thread {
     }
 
     private class AcceptorThread extends Thread {
-        public AcceptorThread() throws IOException {
+        public AcceptorThread() {
             setName("acceptor");
         }
         @Override
