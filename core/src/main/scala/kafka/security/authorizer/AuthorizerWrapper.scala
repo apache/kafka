5c9feff96744804c543ecec370e60c4bd07708b0
Cheng Tan <chengtan97@hotmail.com>
2020-11-03 12:40:38 -0800

    Implement the new API for AuthorizerWrapper

--- core/src/main/scala/kafka/security/authorizer/AuthorizerWrapper.scala
+++ core/src/main/scala/kafka/security/authorizer/AuthorizerWrapper.scala
@@ -21,14 +21,14 @@ import java.util.concurrent.{CompletableFuture, CompletionStage}
 import java.{lang, util}
 
 import kafka.network.RequestChannel.Session
-import kafka.security.auth.{Acl, Operation, PermissionType, Resource, ResourceType}
+import kafka.security.auth.{Acl, Operation, PermissionType, Resource, ResourceType => ResourceTypeLegacy}
 import kafka.security.authorizer.AuthorizerWrapper._
 import org.apache.kafka.common.Endpoint
-import org.apache.kafka.common.acl.{AccessControlEntry, AclBinding, AclBindingFilter}
+import org.apache.kafka.common.acl.{AccessControlEntry, AccessControlEntryFilter, AclBinding, AclBindingFilter, AclOperation, AclPermissionType}
 import org.apache.kafka.common.errors.{ApiException, InvalidRequestException}
 import org.apache.kafka.common.protocol.Errors
 import org.apache.kafka.common.requests.ApiError
-import org.apache.kafka.common.resource.ResourcePattern
+import org.apache.kafka.common.resource.{PatternType, ResourcePattern, ResourcePatternFilter, ResourceType}
 import org.apache.kafka.common.utils.SecurityUtils.parseKafkaPrincipal
 import org.apache.kafka.server.authorizer.AclDeleteResult.AclBindingDeleteResult
 import org.apache.kafka.server.authorizer.{AuthorizableRequestContext, AuthorizerServerInfo, _}
@@ -43,7 +43,7 @@ object AuthorizerWrapper {
 
   def convertToResourceAndAcl(filter: AclBindingFilter): Either[ApiError, (Resource, Acl)] = {
     (for {
-      resourceType <- Try(ResourceType.fromJava(filter.patternFilter.resourceType))
+      resourceType <- Try(ResourceTypeLegacy.fromJava(filter.patternFilter.resourceType))
       principal <- Try(parseKafkaPrincipal(filter.entryFilter.principal))
       operation <- Try(Operation.fromJava(filter.entryFilter.operation))
       permissionType <- Try(PermissionType.fromJava(filter.entryFilter.permissionType))
@@ -71,15 +71,19 @@ object AuthorizerWrapper {
   }
 
   def convertToResource(resourcePattern: ResourcePattern): Resource = {
-    Resource(ResourceType.fromJava(resourcePattern.resourceType), resourcePattern.name, resourcePattern.patternType)
+    Resource(ResourceTypeLegacy.fromJava(resourcePattern.resourceType), resourcePattern.name, resourcePattern.patternType)
   }
 }
 
 @deprecated("Use kafka.security.authorizer.AclAuthorizer", "Since 2.5")
 class AuthorizerWrapper(private[kafka] val baseAuthorizer: kafka.security.auth.Authorizer) extends Authorizer {
 
+  var shouldAllowEveryoneIfNoAclIsFound = false
+
   override def configure(configs: util.Map[String, _]): Unit = {
     baseAuthorizer.configure(configs)
+    shouldAllowEveryoneIfNoAclIsFound = configs.asScala.get(
+      AclAuthorizer.AllowEveryoneIfNoAclIsFoundProp).exists(_.toString.toBoolean)
   }
 
   override def start(serverInfo: AuthorizerServerInfo): util.Map[Endpoint, _ <: CompletionStage[Void]] = {
@@ -175,4 +179,74 @@ class AuthorizerWrapper(private[kafka] val baseAuthorizer: kafka.security.auth.A
   override def close(): Unit = {
     baseAuthorizer.close()
   }
+
+  override def authorizeAny(requestContext: AuthorizableRequestContext,
+                            op: AclOperation,
+                            resourceType: ResourceType): AuthorizationResult = {
+    if (resourceType == ResourceType.ANY)
+      throw new IllegalArgumentException("Must specify a non-filter resource type for authorizeAny")
+
+    if (resourceType == ResourceType.UNKNOWN)
+      throw new IllegalArgumentException("Unknown resource type")
+
+    if (op == AclOperation.ANY)
+      throw new IllegalArgumentException("Must specify a non-filter operation type for authorizeAny")
+
+    if (op == AclOperation.UNKNOWN)
+      throw new IllegalArgumentException("Unknown operation type")
+
+    if (shouldAllowEveryoneIfNoAclIsFound && !denyAllResource(requestContext, op, resourceType)) {
+      AuthorizationResult.ALLOWED
+    } else {
+      super.authorizeAny(requestContext, op, resourceType)
+    }
+  }
+
+  def denyAllResource(requestContext: AuthorizableRequestContext,
+                      op: AclOperation,
+                      resourceType: ResourceType): Boolean = {
+    val resourceTypeFilter = new ResourcePatternFilter(
+      resourceType, null, PatternType.ANY)
+    val accessControlEntry = new AccessControlEntryFilter(
+      null, null, null, AclPermissionType.DENY)
+    val aclFilter = new AclBindingFilter(resourceTypeFilter, accessControlEntry)
+
+    for (binding <- acls(aclFilter).asScala) {
+      if (aceMatched(requestContext, op, binding) && canDenyAll(binding.pattern()))
+        return true
+    }
+    false
+  }
+
+  def aceMatched(requestContext: AuthorizableRequestContext,
+                 op: AclOperation,
+                 binding: AclBinding): Boolean = {
+    (hostMatched(requestContext, binding) && principleMatched(requestContext, binding)
+      && operationMatched(op, binding))
+  }
+
+  def hostMatched(requestContext: AuthorizableRequestContext,
+                  binding: AclBinding): Boolean =
+    (binding.entry().host().equals(requestContext.clientAddress().getHostAddress)
+      || binding.entry().host().equals(AclEntry.WildcardHost))
+
+  def principleMatched(requestContext: AuthorizableRequestContext,
+                  binding: AclBinding): Boolean =
+    (binding.entry().principal().equals(requestContext.principal().toString)
+      || binding.entry().principal().equals(AclEntry.WildcardPrincipal.toString))
+
+
+  def operationMatched(op: AclOperation,
+                       binding: AclBinding): Boolean =
+    (binding.entry().operation() == op
+      || binding.entry().operation() == AclOperation.ALL)
+
+  def canDenyAll(pattern: ResourcePattern): Boolean = {
+    pattern.patternType() match {
+      case PatternType.LITERAL => pattern.name().equals(ResourcePattern.WILDCARD_RESOURCE)
+      case PatternType.PREFIXED => pattern.name().isEmpty
+      case _ => false
+    }
+  }
+
 }
