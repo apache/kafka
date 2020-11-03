730eb2fb70642ad81f4d30a5bd88c55f1a735be5
Michael Carter <michael.carter@instaclustr.com>
2020-11-03 13:00:50 +1100

    Fixed unit test that was not using the initializeAndStart() method

--- connect/runtime/src/test/java/org/apache/kafka/connect/runtime/WorkerSinkTaskTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/WorkerSinkTaskTest.java
@@ -633,6 +633,7 @@ public class WorkerSinkTaskTest {
         PowerMock.replayAll();
 
         workerTask.initialize(TASK_CONFIG);
+        workerTask.initializeAndStart();
         workerTask.execute();
 
         assertEquals(0, workerTask.commitFailures());
