052839ae4886bac1a2f2c3ee92d7bb9d437953ec
Mickael Maison <mickael.maison@gmail.com>
2020-11-05 10:57:36 +0000

    MINOR: Move upgraded docs from site to Kafka docs
    A number of doc updates have been applied to the website. For the 2.6.1 release, resync Kafka's docs folder with the website.

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "26",
     "dotVersion": "2.6",
-    "fullDotVersion": "2.6.1",
+    "fullDotVersion": "2.6.0",
     "scalaVersion": "2.13"
 };
