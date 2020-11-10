396097dc3423b7871fb55d00fc8f43b86ff23c50
prithvi <prithvi@cloudera.com>
2020-11-10 12:28:21 +0530

    nit changes

--- core/src/main/scala/kafka/tools/DumpLogSegments.scala
+++ core/src/main/scala/kafka/tools/DumpLogSegments.scala
@@ -258,11 +258,12 @@ object DumpLogSegments {
             }
             lastOffset = record.offset
 
-            print(s"$RecordIndent offset: ${record.offset} baseOffset: ${batch.baseOffset} lastOffset: ${batch.lastOffset} baseSequence: ${} ${batch.baseSequence}" +
-             s" lastSequence: ${batch.lastSequence} producerEpoch: ${batch.producerEpoch} partitionLeaderEpoch: ${batch.partitionLeaderEpoch} position: ${validBytes} " +
-             s" ${batch.timestampType}: ${record.timestamp} isvalid: ${record.isValid}"+
-             s" keysize: ${record.keySize} valuesize: ${record.valueSize} size: ${batch.sizeInBytes} magic: ${batch.magic} " +
-             s"compresscodec: ${batch.compressionType} crc: ${record.checksumOrNull}")
+            print(s"$RecordIndent offset: ${record.offset} isValid: ${record.isValid} crc: ${record.checksumOrNull} "+
+                s" keySize: ${record.keySize} valueSize: ${record.valueSize} ${batch.timestampType}: ${record.timestamp} "+
+                s" baseOffset: ${batch.baseOffset} lastOffset: ${batch.lastOffset} baseSequence: ${batch.baseSequence} " +
+                s" lastSequence: ${batch.lastSequence} producerEpoch: ${batch.producerEpoch} partitionLeaderEpoch: ${batch.partitionLeaderEpoch}  " +
+                s" size: ${batch.sizeInBytes} magic: ${batch.magic} compressType: ${batch.compressionType} position: ${validBytes} ")
+
 
             if (batch.magic >= RecordBatch.MAGIC_VALUE_V2) {
               print(" sequence: " + record.sequence + " headerKeys: " + record.headers.map(_.key).mkString("[", ",", "]"))
