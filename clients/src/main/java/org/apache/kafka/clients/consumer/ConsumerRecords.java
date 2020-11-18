bd990abb0137ef02fcdb7bf210ec18ff2fe7fcb8
John Roesler <vvcephei@apache.org>
2020-11-18 13:30:37 -0600

    KAFKA-10866: Add fetched metadata to ConsumerRecords
    Exposed the fetched metadata via the ConsumerRecords
    object as described in KIP-695

--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
@@ -16,11 +16,13 @@
  */
 package org.apache.kafka.clients.consumer;
 
+import org.apache.kafka.clients.consumer.internals.FetchedRecords;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.utils.AbstractIterator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -32,14 +34,99 @@ import java.util.Set;
  * partition returned by a {@link Consumer#poll(java.time.Duration)} operation.
  */
 public class ConsumerRecords<K, V> implements Iterable<ConsumerRecord<K, V>> {
-
-    @SuppressWarnings("unchecked")
-    public static final ConsumerRecords<Object, Object> EMPTY = new ConsumerRecords<>(Collections.EMPTY_MAP);
+    public static final ConsumerRecords<Object, Object> EMPTY = new ConsumerRecords<>(
+        Collections.emptyMap(),
+        Collections.emptyMap()
+    );
 
     private final Map<TopicPartition, List<ConsumerRecord<K, V>>> records;
+    private final Map<TopicPartition, Metadata> metadata;
 
-    public ConsumerRecords(Map<TopicPartition, List<ConsumerRecord<K, V>>> records) {
+    public static final class Metadata {
+
+        private final long receivedTimestamp;
+        private final long position;
+        private final long beginningOffset;
+        private final long endOffset;
+
+        public Metadata(final long receivedTimestamp,
+                        final long position,
+                        final long beginningOffset,
+                        final long endOffset) {
+            this.receivedTimestamp = receivedTimestamp;
+            this.position = position;
+            this.beginningOffset = beginningOffset;
+            this.endOffset = endOffset;
+        }
+
+        /**
+         * @return The timestamp of the broker response that contained this metadata
+         */
+        public long receivedTimestamp() {
+            return receivedTimestamp;
+        }
+
+        /**
+         * @return The next position the consumer will fetch
+         */
+        public long position() {
+            return position;
+        }
+
+        /**
+         * @return The lag between the next position to fetch and the current end of the partition
+         */
+        public long lag() {
+            return endOffset - position;
+        }
+
+        /**
+         * @return The current first offset in the partition.
+         */
+        public long beginningOffset() {
+            return beginningOffset;
+        }
+
+        /**
+         * @return The current last offset in the partition. The determination of the "last" offset
+         * depends on the Consumer's isolation level. Under "read_uncommitted," this is the last successfully
+         * replicated offset plus one. Under "read_committed," this is the minimum of the last successfully
+         * replicated offset plus one or the smallest offset of any open transaction.
+         */
+        public long endOffset() {
+            return endOffset;
+        }
+    }
+
+    private static <K, V> Map<TopicPartition, Metadata> extractMetadata(final FetchedRecords<K, V> fetchedRecords) {
+        final Map<TopicPartition, Metadata> metadata = new HashMap<>();
+        for (final Map.Entry<TopicPartition, FetchedRecords.FetchMetadata> entry : fetchedRecords.metadata().entrySet()) {
+            metadata.put(
+                entry.getKey(),
+                new Metadata(
+                    entry.getValue().receivedTimestamp(),
+                    entry.getValue().position().offset,
+                    entry.getValue().beginningOffset(),
+                    entry.getValue().endOffset()
+                )
+            );
+        }
+        return metadata;
+    }
+
+    public ConsumerRecords(final Map<TopicPartition, List<ConsumerRecord<K, V>>> records) {
         this.records = records;
+        this.metadata = new HashMap<>();
+    }
+
+    public ConsumerRecords(final Map<TopicPartition, List<ConsumerRecord<K, V>>> records,
+                           final Map<TopicPartition, Metadata> metadata) {
+        this.records = records;
+        this.metadata = metadata;
+    }
+
+    public ConsumerRecords(final FetchedRecords<K, V> fetchedRecords) {
+        this(fetchedRecords.records(), extractMetadata(fetchedRecords));
     }
 
     /**
@@ -53,6 +140,16 @@ public class ConsumerRecords<K, V> implements Iterable<ConsumerRecord<K, V>> {
             return Collections.emptyList();
         else
             return Collections.unmodifiableList(recs);
+    }
+
+    /**
+     * Get the updated metadata returned by the brokers along with this record set.
+     * May be empty or partial depending on the responses from the broker during this particular poll.
+     * May also include metadata for additional partitions than the ones for which there are records
+     * in this {@code ConsumerRecords} object.
+     */
+    public Map<TopicPartition, Metadata> metadata() {
+        return Collections.unmodifiableMap(metadata);
     }
 
     /**
