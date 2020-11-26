acf7db00a5306805d01c0891bf0e45ed6de6ebcf
Tom Bentley <tbentley@redhat.com>
2020-11-26 17:52:36 +0000

    Review comment

--- connect/api/src/main/java/org/apache/kafka/connect/transforms/Transformation.java
+++ connect/api/src/main/java/org/apache/kafka/connect/transforms/Transformation.java
@@ -34,7 +34,7 @@ public interface Transformation<R extends ConnectRecord<R>> extends Configurable
      * corresponding to a map or filter operation respectively.
      *
      * A transformation must not mutate objects reachable from the given {@code record}
-     * (including, but not limited to, {@link org.apache.kafka.connect.header.Headers},
+     * (including, but not limited to, {@link org.apache.kafka.connect.header.Headers Headers},
      * {@link org.apache.kafka.connect.data.Struct Structs}, {@code Lists}, and {@code Maps}).
      * If such objects need to be changed, a new ConnectRecord should be created and returned.
      *
