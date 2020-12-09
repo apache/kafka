305affe23ef10f45cab33cdf7db84c274dd33091
Chia-Ping Tsai <chia7712@gmail.com>
2020-12-09 23:49:07 +0800

    remove older discussion

--- clients/src/main/java/org/apache/kafka/common/security/authenticator/SaslClientAuthenticator.java
+++ clients/src/main/java/org/apache/kafka/common/security/authenticator/SaslClientAuthenticator.java
@@ -626,7 +626,6 @@ public class SaslClientAuthenticator implements Authenticator {
         Set<Principal> principals = subject.getPrincipals();
         // getPrincipals() returns a SynchronizedSet and iteration over SynchronizedSet is not thread safe.
         // Hence, we have to add synchronization on local variable "principals".
-        // see https://github.com/apache/kafka/pull/3208#discussion_r120130706 for discussion.
         synchronized (principals) {
             Iterator<Principal> iterator = principals.iterator();
             if (iterator.hasNext())
