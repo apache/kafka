df907b3d0b1620b4ce81ef0dd53b4b7c9ccf8947
Ismael Juma <ismael@juma.me.uk>
2020-12-09 06:33:43 -0800

    Revert "KAFKA-10713: Stricter protocol parsing in hostnames (#9593)"
    This reverts commit 8a59a228817df221ac9acc7cce4f6c9dd702e9ef since it breaks
    client configurations like `bootstrap.servers=SASL_PLAINTEXT://localhost:49767`.
    A KIP will be submitted to discuss the details and an adjusted change will
    be submitted depending on the outcome of that.

--- clients/src/test/java/org/apache/kafka/clients/ClientUtilsTest.java
+++ clients/src/test/java/org/apache/kafka/clients/ClientUtilsTest.java
@@ -25,7 +25,6 @@ import java.util.stream.Collectors;
 import org.apache.kafka.common.config.ConfigException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -43,8 +42,6 @@ public class ClientUtilsTest {
         InetSocketAddress onlyAddress = validatedAddresses.get(0);
         assertEquals("localhost", onlyAddress.getHostName());
         assertEquals(10000, onlyAddress.getPort());
-        assertThrows(ConfigException.class, () -> ClientUtils.parseAndValidateAddresses(
-                Arrays.asList("localhost:8000;localhost:9000"), ClientDnsLookup.USE_ALL_DNS_IPS));
     }
 
     @Test
