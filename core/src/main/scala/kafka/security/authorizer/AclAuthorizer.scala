0920b7936eaa43c5d0cc184fc19afb3739196385
Manikumar Reddy <manikumar.reddy@gmail.com>
2021-01-07 16:32:22 +0530

    MINOR: Add a log to print acl change notification details
    This is similar to config change logs for topic/broker. This will be useful for debugging any acl related issues.
    
    Author: Manikumar Reddy <manikumar.reddy@gmail.com>
    
    Reviewers: Rajini Sivaram <rajinisivaram@googlemail.com>
    
    Closes #9841 from omkreddy/acl-change-log
    
    (cherry picked from commit f0ef5785e3964eab3844a36dc95c4c33fcf98ef5)
    Signed-off-by: Manikumar Reddy <manikumar.reddy@gmail.com>

--- core/src/main/scala/kafka/security/authorizer/AclAuthorizer.scala
+++ core/src/main/scala/kafka/security/authorizer/AclAuthorizer.scala
@@ -575,6 +575,7 @@ class AclAuthorizer extends Authorizer with Logging {
   private[authorizer] def processAclChangeNotification(resource: ResourcePattern): Unit = {
     lock synchronized {
       val versionedAcls = getAclsFromZk(resource)
+      info(s"Processing Acl change notification for $resource, versionedAcls : ${versionedAcls.acls}, zkVersion : ${versionedAcls.zkVersion}")
       updateCache(resource, versionedAcls)
     }
   }
