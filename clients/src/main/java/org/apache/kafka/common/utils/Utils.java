74ecd23614435ff6e96f84224747c087f76996b4
Shao Yang Hong <hongsy2006@gmail.com>
2021-01-21 11:20:18 +0800

    Update Utils.java

--- clients/src/main/java/org/apache/kafka/common/utils/Utils.java
+++ clients/src/main/java/org/apache/kafka/common/utils/Utils.java
@@ -982,7 +982,7 @@ public final class Utils {
     /**
      * A cheap way to deterministically convert a number to a positive value. When the input is
      * positive, the original value is returned. When the input number is negative, the returned
-     * positive value is the original value bit AND against 0x7fffffff which is not its absolutely
+     * positive value is the original value bit AND against 0x7fffffff which is not its absolute
      * value.
      *
      * Note: changing this method in the future will possibly cause partition selection not to be
