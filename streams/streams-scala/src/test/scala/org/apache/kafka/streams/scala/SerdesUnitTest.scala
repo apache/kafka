4ecf5f9ac8b451045266c752448d731fc6354d4b
Leah Thomas <lthomas@confluent.io>
2021-01-26 15:10:24 -0600

    scala fix

--- streams/streams-scala/src/test/scala/org/apache/kafka/streams/scala/SerdesUnitTest.scala
+++ streams/streams-scala/src/test/scala/org/apache/kafka/streams/scala/SerdesUnitTest.scala
@@ -1,10 +1,24 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.streams.scala
 
-import org.apache.log4j.AppenderSkeleton
 import kafka.utils.LogCaptureAppender
 import org.apache.kafka.streams.kstream.WindowedSerdes.TimeWindowedSerde
-import org.hamcrest.CoreMatchers.hasItems
-import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class SerdesUnitTest {
@@ -14,12 +28,8 @@ class SerdesUnitTest {
 
     Serdes.timeWindowedSerde(new TimeWindowedSerde[String]())
     val appender = LogCaptureAppender.createAndRegister()
-    assertThat(
-      appender,
-      hasItems(
-        "Implicit `timeWindowedSerde` produces incorrect end times on deserialization. Explicitly declare a new TimeWindowedDeserializer instead."
-      )
-    )
+    val message = appender.getMessages.find(e => e.getLevel == Level.WARN && e.getThrowableInformation != null)
+    assertTrue("There should be a warning about TimeWindowedDeserializer", message.isDefined)
   }
 
 }
