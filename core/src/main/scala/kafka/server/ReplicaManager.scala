c3a0451140093b7a6e889f76c1231f2f841ba0bd
Ron Dagostino <rdagostino@confluent.io>
2021-02-01 14:22:19 -0500

    renames (e.g. changesDeferrable => deferringMetadataChanges)

--- core/src/main/scala/kafka/server/ReplicaManager.scala
+++ core/src/main/scala/kafka/server/ReplicaManager.scala
@@ -264,25 +264,34 @@ class ReplicaManager(val config: KafkaConfig,
     }
   }
 
-  // Changes are initially deferrable when using a Raft-based metadata quorum, and may flip-flop thereafter;
-  // changes are never deferrable when using ZooKeeper.  When true, this indicates that we should transition online
-  // partitions to the deferred state if we see metadata with a different leader epoch.
-  @volatile private var changesDeferrable: Boolean = !config.requiresZookeeper
-  stateChangeLogger.info(s"Metadata changes deferrable=$changesDeferrable")
+  // Changes are initially deferred when using a Raft-based metadata quorum, and they may flip-flop to not
+  // being deferred and being deferred again thereafter as the broker (re)acquires/loses its lease.
+  // Changes are never deferred when using ZooKeeper.  When true, this indicates that we should transition
+  // online partitions to the deferred state if we see a metadata update for that partition.
+  @volatile private var deferringMetadataChanges: Boolean = !config.requiresZookeeper
+  stateChangeLogger.info(s"Metadata changes deferred=$deferringMetadataChanges")
 
-  private def confirmChangesNotDeferrableWithZooKeeper(): Unit = {
-    if (changesDeferrable) {
-      throw new IllegalStateException("Partition metadata changes should never be deferrable when using ZooKeeper")
+  private def confirmNotDeferringMetadataUpdatesWithZooKeeper(): Unit = {
+    if (deferringMetadataChanges) {
+      throw new IllegalStateException("Partition metadata changes should never be deferred when using ZooKeeper")
     }
   }
 
-  def deferrableMetadataChanges(): Unit = {
+  def beginMetadataChangeDeferral(): Unit = {
     replicaStateChangeLock synchronized {
       if (config.requiresZookeeper) {
         throw new IllegalStateException("Partition metadata changes can never be deferred when using ZooKeeper")
       }
-      changesDeferrable = true
-      stateChangeLogger.info(s"Metadata changes are now deferrable")
+      deferringMetadataChanges = true
+      stateChangeLogger.info(s"Metadata changes are now being deferred")
+    }
+  }
+
+  def endMetadataChangeDeferral(): Unit = {
+    replicaStateChangeLock synchronized {
+      // TODO: implement for Raft-based metadata log messages
+      deferringMetadataChanges = false
+      stateChangeLogger.info(s"Metadata changes are no longer being deferred")
     }
   }
 
@@ -364,7 +373,7 @@ class ReplicaManager(val config: KafkaConfig,
                    partitionStates: Map[TopicPartition, StopReplicaPartitionState]
                   ): (mutable.Map[TopicPartition, Errors], Errors) = {
     replicaStateChangeLock synchronized {
-      confirmChangesNotDeferrableWithZooKeeper()
+      confirmNotDeferringMetadataUpdatesWithZooKeeper()
       stateChangeLogger.info(s"Handling StopReplica request correlationId $correlationId from controller " +
         s"$controllerId for ${partitionStates.size} partitions")
       if (stateChangeLogger.isTraceEnabled)
@@ -1295,7 +1304,7 @@ class ReplicaManager(val config: KafkaConfig,
 
   def maybeUpdateMetadataCache(correlationId: Int, updateMetadataRequest: UpdateMetadataRequest) : Seq[TopicPartition] =  {
     replicaStateChangeLock synchronized {
-      confirmChangesNotDeferrableWithZooKeeper()
+      confirmNotDeferringMetadataUpdatesWithZooKeeper()
       if (updateMetadataRequest.controllerEpoch < controllerEpoch) {
         val stateControllerEpochErrorMessage = s"Received update metadata request with correlation id $correlationId " +
           s"from an old controller ${updateMetadataRequest.controllerId} with epoch ${updateMetadataRequest.controllerEpoch}. " +
@@ -1315,7 +1324,7 @@ class ReplicaManager(val config: KafkaConfig,
                              onLeadershipChange: (Iterable[Partition], Iterable[Partition]) => Unit): LeaderAndIsrResponse = {
     val startMs = time.milliseconds()
     replicaStateChangeLock synchronized {
-      confirmChangesNotDeferrableWithZooKeeper()
+      confirmNotDeferringMetadataUpdatesWithZooKeeper()
       val controllerId = leaderAndIsrRequest.controllerId
       val requestPartitionStates = leaderAndIsrRequest.partitionStates.asScala
       stateChangeLogger.info(s"Handling LeaderAndIsr request correlationId $correlationId from controller " +
