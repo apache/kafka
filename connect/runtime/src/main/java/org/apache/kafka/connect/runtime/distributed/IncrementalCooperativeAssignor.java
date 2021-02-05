56b7313e0a9f5fcf6bf79617af969f51b74e1cdf
Luke Chen <showuon@gmail.com>
2021-02-05 16:28:52 +0800

    again

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/IncrementalCooperativeAssignor.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/IncrementalCooperativeAssignor.java
@@ -461,7 +461,7 @@ public class IncrementalCooperativeAssignor implements ConnectAssignor {
             }
 
             if (!candidateWorkerLoad.isEmpty()) {
-                System.out.println("Assigning " + lostAssignments.tasks().size() + " tasks to " + candidateWorkerLoad.size() + " worker");
+                System.out.println("Assigning:" + lostAssignments.tasks().size() + " tasks to " + candidateWorkerLoad.size() + " worker");
                 Iterator<WorkerLoad> candidateWorkerIterator = candidateWorkerLoad.iterator();
                 for (String connector : lostAssignments.connectors()) {
                     // Loop over the the candidate workers as many times as it takes
