42a9355e606bd2bbdb7fd0dd348805e6666dc189
Ron Dagostino <rdagostino@confluent.io>
2021-02-07 18:46:41 -0500

    MINOR: Defer log recovery until LogManager startup (#10039)
    Currently log recovery begins as soon as we instantiate `LogManager`, but when using a
    Raft-based metadata quorum we won't have configs until after we catch up on the metadata
    log.  We therefore defer log recovery until we actually invoke `startup()` on the `LogManager`
    instance.  This timing difference has no effect when using ZooKeeper because we
    immediately invoke `startup()` on the instantiated instance, but it gives us the necessary
    flexibility for accurate log recovery with updated configs when using a Raft-based metadata
    quorum.
    
    The `LogCleaner` is currently instantiated during construction just after log recovery
    completes, and then it is started in `startup()`.  As an extra precaution, since we are
    no longer performing recovery during construction, we both instantiate and start the
    log cleaner in `startup()` after log recovery completes.
    
    We also convert `LogManager` to use a `ConfigRepository` to load topic configs
    (which can override the default log configs) instead of having a hard-coded
    dependency on ZooKeeper.  We retrieve the topic configs when we invoke `startup()`
    -- which again is effectively no different from a timing perspective than what we do
    today for the ZooKeeper case.
    
    One subtlety is that currently we create the log configs for every topic at this point
    -- if a topic has no config overrides then we associate a copy of the default
    configuration with the topic inside a map, and we retrieve the log configs for that
    topic's partitions from from that map during recovery.  This PR makes a change to
    this series of events as follows.  We do not associate a copy of the the default
    configuration with a topic in the map if the topic has no configs set when we query
    for them.  This saves some memory -- we don't unnecessarily copy the default
    config many times -- but it also means we have use the default log configs for
    that topic later on when recovery for each of its partitions begins.
    
    The difference is that the default configs are dynamically reconfigurable, and they
    could potentially change between the time when we invoke `startup()` and when
    log recovery begins (log recovery can begin quite some time after `startup()` is
    invoked if shutdown was unclean).  Prior to this patch such a change would not
    be used; with this patch they could be if they happen before recovery begins.
    This actually is better -- we are performing log recovery with the most recent
    known defaults when a topic had no overrides at all. Also, `Partition.createLog`
    has logic to handle missed config updates, so the behavior is eventually the same.
    
    The transition of the broker state from `STARTING` to `RECOVERY` currently
    happens within the `LogManager`, and it only occurs if the shutdown was
    unclean.  We move this transition into the broker as it avoids passing a
    reference to the broker state into the `LogManager`.  We also now always
    transition the broker into the `RECOVERY` state as dictated by [the KIP-631 broker state machine](https://cwiki.apache.org/confluence/display/KAFKA/KIP-631%3A+The+Quorumbased+Kafka+Controller#KIP631:TheQuorumbasedKafkaController-TheBrokerStateMachine).
    
    Finally, a few clean-ups were included. One worth highlighting is that `Partition`
    no longer requires a `ConfigRepository`.
    
    Reviewers: David Arthur <david.arthur@confluent.io>, Ismael Juma <ismael@juma.me.uk>

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/fetcher/ReplicaFetcherThreadBenchmark.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/fetcher/ReplicaFetcherThreadBenchmark.java
@@ -56,7 +56,6 @@ import org.apache.kafka.common.requests.FetchRequest;
 import org.apache.kafka.common.requests.FetchResponse;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.common.utils.Utils;
-import org.apache.kafka.metadata.BrokerState;
 import org.mockito.Mockito;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -89,7 +88,6 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 @State(Scope.Benchmark)
 @Fork(value = 1)
@@ -124,7 +122,7 @@ public class ReplicaFetcherThreadBenchmark {
         LogDirFailureChannel logDirFailureChannel = Mockito.mock(LogDirFailureChannel.class);
         logManager = new LogManager(JavaConverters.asScalaIteratorConverter(logDirs.iterator()).asScala().toSeq(),
                 JavaConverters.asScalaIteratorConverter(new ArrayList<File>().iterator()).asScala().toSeq(),
-                new scala.collection.mutable.HashMap<>(),
+                new CachedConfigRepository(),
                 logConfig,
                 new CleanerConfig(0, 0, 0, 0, 0, 0.0, 0, false, "MD5"),
                 1,
@@ -134,7 +132,6 @@ public class ReplicaFetcherThreadBenchmark {
                 1000L,
                 60000,
                 scheduler,
-                new AtomicReference<>(BrokerState.NOT_RUNNING),
                 brokerTopicStats,
                 logDirFailureChannel,
                 Time.SYSTEM);
@@ -159,7 +156,7 @@ public class ReplicaFetcherThreadBenchmark {
             Mockito.when(offsetCheckpoints.fetch(logDir.getAbsolutePath(), tp)).thenReturn(Option.apply(0L));
             AlterIsrManager isrChannelManager = Mockito.mock(AlterIsrManager.class);
             Partition partition = new Partition(tp, 100, ApiVersion$.MODULE$.latestVersion(),
-                    0, Time.SYSTEM, new CachedConfigRepository(), isrChangeListener, new DelayedOperationsMock(tp),
+                    0, Time.SYSTEM, isrChangeListener, new DelayedOperationsMock(tp),
                     Mockito.mock(MetadataCache.class), logManager, isrChannelManager);
 
             partition.makeFollower(partitionState, offsetCheckpoints);
