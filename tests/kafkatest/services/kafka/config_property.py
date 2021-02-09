b63985f6cbe91d9a6edab3a7be488dc52d8f7df5
Ron Dagostino <rdagostino@confluent.io>
2021-02-09 17:57:25 -0500

    MINOR: Support Raft-based metadata quorums in system tests

--- tests/kafkatest/services/kafka/config_property.py
+++ tests/kafkatest/services/kafka/config_property.py
@@ -18,6 +18,11 @@ Define Kafka configuration property names here.
 """
 
 BROKER_ID = "broker.id"
+NODE_ID = "node.id"
+FIRST_BROKER_PORT = 9092
+FIRST_CONTROLLER_PORT = FIRST_BROKER_PORT + 500
+FIRST_CONTROLLER_ID = 3001
+CLUSTER_ID = "6bd37820-6745-4790-ae98-620300e1f61b"
 PORT = "port"
 ADVERTISED_HOSTNAME = "advertised.host.name"
 
