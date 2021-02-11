58b0e7296049c778ae088deb60ec032d3934271e
Luke Chen <showuon@gmail.com>
2021-02-11 15:55:35 +0800

    indent

--- connect/runtime/src/test/java/org/apache/kafka/connect/integration/BlockingConnectorTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/integration/BlockingConnectorTest.java
@@ -156,7 +156,7 @@ public class BlockingConnectorTest {
             fail("failed twice");
         }
         if (fail) {
-          fail("failed 1st");
+            fail("failed 1st");
         }
     }
 
