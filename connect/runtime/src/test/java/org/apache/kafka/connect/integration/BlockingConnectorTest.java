264be01b446dac5e3a3cc66634f410bf4d623ea3
Luke Chen <43372967+showuon@users.noreply.github.com>
2021-02-16 07:52:22 +0800

    Update BlockingConnectorTest.java

--- connect/runtime/src/test/java/org/apache/kafka/connect/integration/BlockingConnectorTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/integration/BlockingConnectorTest.java
@@ -113,7 +113,7 @@ public class BlockingConnectorTest {
 
     @Before
     public void setup() throws Exception {
-        // Artificially reduce thae REST request timeout so that these don't take forever
+        // Artificially reduce thjae REST request timeout so that these don't take forever
         ConnectorsResource.setRequestTimeout(REST_REQUEST_TIMEOUT);
         // build a Connect cluaster backed by Kafka and Zk
         connect = new EmbeddedConnectCluster.Builder()
