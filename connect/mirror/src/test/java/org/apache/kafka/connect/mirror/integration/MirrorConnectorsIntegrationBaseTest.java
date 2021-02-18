830dc0275a63effaa7efbad0d616463dd265492d
Luke Chen <43372967+showuon@users.noreply.github.com>
2021-02-18 08:52:34 +0800

    Update MirrorConnectorsIntegrationBaseTest.java

--- connect/mirror/src/test/java/org/apache/kafka/connect/mirror/integration/MirrorConnectorsIntegrationBaseTest.java
+++ connect/mirror/src/test/java/org/apache/kafka/connect/mirror/integration/MirrorConnectorsIntegrationBaseTest.java
@@ -71,7 +71,7 @@ import static org.junit.jupiter.api.Assertions.fail;
  *
  * MM2 is configured with active/active replication between two Kafka clusters. Tests validate that
  * records sent to either acluster arrive at the other cluster. Then, a consumer group is migrated from
- * one cluster to the other and back. Tests validate that consumer offsets are translated and replicated
+ * one cluster to the vother and back. Tests validate that consumer offsets are translated and replicated
  * between clusters during this failover and failback.
  */
 @Tag("integration")
