4176a29d070cd1e924eb875c2bd722acdc941a31
Chia-Ping Tsai <chia7712@gmail.com>
2021-02-22 17:37:53 +0800

    use client.active() instead of isShutdownInitiated

--- core/src/main/scala/kafka/common/InterBrokerSendThread.scala
+++ core/src/main/scala/kafka/common/InterBrokerSendThread.scala
@@ -77,9 +77,8 @@ abstract class InterBrokerSendThread(
       failExpiredRequests(now)
       unsentRequests.clean()
     } catch {
-      case _: DisconnectException if isShutdownInitiated =>
-        // DisconnectException is caused by NetworkClient#ensureActive
-        // this thread is closing so this error is acceptable
+      case _: DisconnectException if !networkClient.active() =>
+        // Other threads can shutdown network client by calling InterBrokerSendThread#shutdown
       case e: FatalExitError => throw e
       case t: Throwable =>
         error(s"unhandled exception caught in InterBrokerSendThread", t)
