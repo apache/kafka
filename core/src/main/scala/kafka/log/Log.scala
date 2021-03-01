a2cdbc5da14ceb0a2c27141a121884a0b1d1fde0
Lucas Bradstreet <lucasbradstreet@gmail.com>
2021-03-01 22:36:26 +0800

    Segment recovery

--- core/src/main/scala/kafka/log/Log.scala
+++ core/src/main/scala/kafka/log/Log.scala
@@ -964,8 +964,9 @@ class Log(@volatile private var _dir: File,
     } else {
       info(s"Reloading from producer snapshot and rebuilding producer state from $lastOffset")
       val isEmptyBeforeTruncation = producerStateManager.isEmpty && producerStateManager.mapEndOffset >= lastOffset
+      val producerStateLoadStart = time.milliseconds()
       producerStateManager.truncateAndReload(logStartOffset, lastOffset, time.milliseconds())
-      val recoveryStart = time.milliseconds()
+      val segmentRecoveryStart = time.milliseconds()
 
       // Only do the potentially expensive reloading if the last snapshot offset is lower than the log end
       // offset (which would be the case on first startup) and there were active producers prior to truncation
@@ -1001,7 +1002,8 @@ class Log(@volatile private var _dir: File,
       }
       producerStateManager.updateMapEndOffset(lastOffset)
       producerStateManager.takeSnapshot()
-      info(s"Producer state recovery from log segment data after $lastOffset took ${time.milliseconds() - recoveryStart}ms")
+      info(s"Producer state recovery took ${producerStateLoadStart - segmentRecoveryStart}ms for snapshot load " +
+        s"and ${time.milliseconds() - segmentRecoveryStart}ms for segment recovery from offset $lastOffset")
     }
   }
 
