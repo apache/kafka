c12f808ba4989f8b838413c1e7de5dc1cfc66049
Ron Dagostino <rdagostino@confluent.io>
2021-03-02 12:57:12 -0500

    MINOR: Disable transactional/idempotent system tests for Raft quorums (#10224)

--- tests/kafkatest/tests/streams/streams_smoke_test.py
+++ tests/kafkatest/tests/streams/streams_smoke_test.py
@@ -47,7 +47,8 @@ class StreamsSmokeTest(KafkaTest):
         self.driver = StreamsSmokeTestDriverService(test_context, self.kafka)
 
     @cluster(num_nodes=8)
-    @matrix(processing_guarantee=['at_least_once', 'exactly_once', 'exactly_once_beta'], crash=[True, False], metadata_quorum=quorum.all_non_upgrade)
+    @matrix(processing_guarantee=['at_least_once'], crash=[True, False], metadata_quorum=quorum.all_non_upgrade)
+    @matrix(processing_guarantee=['exactly_once', 'exactly_once_beta'], crash=[True, False])
     def test_streams(self, processing_guarantee, crash, metadata_quorum=quorum.zk):
         processor1 = StreamsSmokeTestJobRunnerService(self.test_context, self.kafka, processing_guarantee)
         processor2 = StreamsSmokeTestJobRunnerService(self.test_context, self.kafka, processing_guarantee)
