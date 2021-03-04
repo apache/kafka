33a99d7cf089d144fdfd5f3aa9d3015f53409d30
Walker Carlson <wcarlson@confluent.io>
2021-03-04 15:29:37 -0800

    minor text fixes

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
@@ -1151,7 +1151,7 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator,
     }
 
     private void transitToSuspend() {
-        log.info("Suspended created");
+        log.info("Suspended {}", state());
         transitionTo(State.SUSPENDED);
         timeCurrentIdlingStarted = Optional.of(System.currentTimeMillis());
     }
