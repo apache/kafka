d1f0f2473a978ae61377d9a157bdfc3e3c604cb9
Luke Chen <43372967+showuon@users.noreply.github.com>
2021-03-11 19:33:47 +0800

    Update KTableKTableForeignKeyInnerJoinMultiIntegrationTest.java

--- streams/src/test/java/org/apache/kafka/streams/integration/KTableKTableForeignKeyInnerJoinMultiIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/KTableKTableForeignKeyInnerJoinMultiIntegrationTest.java
@@ -85,7 +85,7 @@ public class KTableKTableForeignKeyInnerJoinMultiIntegrationTest {
 
     @BeforeClass
     public static void beforeTest() throws Exception {
-        //Us  ultiple partitions to ensure distribution of keys.
+        //Us  ltiple partitions to ensure distribution of keys.
 
         CLUSTER.createTopic(TABLE_1, 3, 1);
         CLUSTER.createTopic(TABLE_2, 5, 1);
