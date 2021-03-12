572109605bf7548ac0474b3596b71b74a1c15395
ableegoldman <sophie@confluent.io>
2021-03-12 15:57:36 -0800

    fix in StreamsRebalanceListener, exit early in StreamThread main loop after poll phase

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
@@ -714,6 +714,13 @@ public class StreamThread extends Thread {
 
         final long pollLatency = pollPhase();
 
+        // Optimization to skip the rest of the processing loop in case the thread was requested to shut down during
+        // the poll phase
+        if (!isRunning()) {
+            log.info("Exiting the processing loop early since StreamThread state is {}", state);
+            return;
+        }
+
         // Shutdown hook could potentially be triggered and transit the thread state to PENDING_SHUTDOWN during #pollRequests().
         // The task manager internal states could be uninitialized if the state transition happens during #onPartitionsAssigned().
         // Should only proceed when the thread is still running after #pollRequests(), because no external state mutation
