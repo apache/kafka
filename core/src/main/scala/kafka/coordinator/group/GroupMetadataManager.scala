1e54f2f89b4635240b60c2c8638307974871c966
Luke Chen <43372967+showuon@users.noreply.github.com>
2021-03-14 07:21:02 +0800

    Update GroupMetadataManager.scala

--- core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala
+++ core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala
@@ -66,7 +66,7 @@ class GroupMetadataManager(brokerId: Int,
   /* lock protecting access to loading and owned partition sets */
   private val partitionLock = new ReentrantLock()
 
-  /* partitions of consumer groups that are being loaded, its lock should be always called BEFORE the group lock if needed */
+  /* partitions of conasumer groups that are being loaded, its lock should be always called BEFORE the group lock if needed */
   private val loadingPartitions: mutable.Set[Int] = mutable.Set()
 
   /* partitions of consumer groups that are assigned, using the same loading partition lock */
