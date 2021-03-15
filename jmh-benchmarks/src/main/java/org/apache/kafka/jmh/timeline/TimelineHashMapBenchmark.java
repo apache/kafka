c7f35d237042182e96c293f8d9e6b4b3188a88f8
José Armando García Sancio <jsancio@gmail.com>
2021-03-15 16:53:51 -0700

    MINOR: Add a few more benchmark for the timeline map

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/timeline/TimelineHashMapBenchmark.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/timeline/TimelineHashMapBenchmark.java
@@ -23,14 +23,19 @@ import org.apache.kafka.timeline.TimelineHashMap;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -44,33 +49,96 @@ import java.util.concurrent.TimeUnit;
 public class TimelineHashMapBenchmark {
     private final static int NUM_ENTRIES = 1_000_000;
 
+    @State(Scope.Thread)
+    public static class HashMapInput {
+        public HashMap<Integer, String> map;
+        public final List<Integer> keys = createKeys(NUM_ENTRIES);
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            map = new HashMap<>(keys.size());
+            for (Integer key : keys) {
+                map.put(key, String.valueOf(key));
+            }
+
+            Collections.shuffle(keys);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class ImmutableMapInput {
+        scala.collection.immutable.HashMap<Integer, String> map;
+        public final List<Integer> keys = createKeys(NUM_ENTRIES);
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            map = new scala.collection.immutable.HashMap<>();
+            for (Integer key : keys) {
+                map = map.updated(key, String.valueOf(key));
+            }
+
+            Collections.shuffle(keys);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class TimelineMapInput {
+        public SnapshotRegistry snapshotRegistry;
+        public TimelineHashMap<Integer, String> map;
+        public final List<Integer> keys = createKeys(NUM_ENTRIES);
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            snapshotRegistry = new SnapshotRegistry(new LogContext());
+            map = new TimelineHashMap<>(snapshotRegistry, keys.size());
+
+            for (Integer key : keys) {
+                map.put(key, String.valueOf(key));
+            }
+
+            Collections.shuffle(keys);
+        }
+    }
+
     @Benchmark
     public Map<Integer, String> testAddEntriesInHashMap() {
-        HashMap<Integer, String> map = new HashMap<>(NUM_ENTRIES);
+        HashMap<Integer, String> map = new HashMap<>();
         for (int i = 0; i < NUM_ENTRIES; i++) {
             int key = (int) (0xffffffff & ((i * 2862933555777941757L) + 3037000493L));
             map.put(key, String.valueOf(key));
         }
+
+        return map;
+    }
+
+    @Benchmark
+    public scala.collection.immutable.HashMap<Integer, String> testAddEntriesInImmutableMap() {
+        scala.collection.immutable.HashMap<Integer, String> map = new scala.collection.immutable.HashMap<>();
+        for (int i = 0; i < NUM_ENTRIES; i++) {
+            int key = (int) (0xffffffff & ((i * 2862933555777941757L) + 3037000493L));
+            map = map.updated(key, String.valueOf(key));
+        }
+
         return map;
     }
 
     @Benchmark
     public Map<Integer, String> testAddEntriesInTimelineMap() {
         SnapshotRegistry snapshotRegistry = new SnapshotRegistry(new LogContext());
-        TimelineHashMap<Integer, String> map =
-            new TimelineHashMap<>(snapshotRegistry, NUM_ENTRIES);
+        TimelineHashMap<Integer, String> map = new TimelineHashMap<>(snapshotRegistry, 16);
         for (int i = 0; i < NUM_ENTRIES; i++) {
             int key = (int) (0xffffffff & ((i * 2862933555777941757L) + 3037000493L));
             map.put(key, String.valueOf(key));
         }
+
         return map;
     }
 
     @Benchmark
     public Map<Integer, String> testAddEntriesWithSnapshots() {
         SnapshotRegistry snapshotRegistry = new SnapshotRegistry(new LogContext());
-        TimelineHashMap<Integer, String> map =
-            new TimelineHashMap<>(snapshotRegistry, NUM_ENTRIES);
+        TimelineHashMap<Integer, String> map = new TimelineHashMap<>(snapshotRegistry, 16);
+
         long epoch = 0;
         int j = 0;
         for (int i = 0; i < NUM_ENTRIES; i++) {
@@ -86,5 +154,101 @@ public class TimelineHashMapBenchmark {
             epoch++;
         }
         return map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testUpdateEntriesInHashMap(HashMapInput input) {
+        for (Integer key : input.keys) {
+            input.map.put(key, String.valueOf(key));
+        }
+        return input.map;
+    }
+
+    @Benchmark
+    public scala.collection.Map testUpdateEntriesInImmutableMap(ImmutableMapInput input) {
+        scala.collection.immutable.HashMap<Integer, String> map = input.map;
+        for (Integer key : input.keys) {
+            map = map.updated(key, String.valueOf(key));
+        }
+        return map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testUpdateEntriesInTimelineMap(TimelineMapInput input) {
+        for (Integer key : input.keys) {
+            input.map.put(key, String.valueOf(key));
+        }
+        return input.map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testUpdateEntriesWithSnapshots(TimelineMapInput input) {
+        long epoch = 0;
+        int j = 0;
+        for (Integer key : input.keys) {
+            if (j > 1_000) {
+                input.snapshotRegistry.deleteSnapshotsUpTo(epoch - 10_000);
+                input.snapshotRegistry.createSnapshot(epoch);
+                j = 0;
+            } else {
+                j++;
+            }
+            input.map.put(key, String.valueOf(key));
+            epoch++;
+        }
+        return input.map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testRemoveEntriesInHashMap(HashMapInput input) {
+        for (Integer key : input.keys) {
+            input.map.remove(key);
+        }
+        return input.map;
+    }
+
+    @Benchmark
+    public scala.collection.Map testRemoveEntriesInImmutableMap(ImmutableMapInput input) {
+        scala.collection.immutable.HashMap<Integer, String> map = input.map;
+        for (Integer key : input.keys) {
+            map = map.removed(key);
+        }
+        return map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testRemoveEntriesInTimelineMap(TimelineMapInput input) {
+        for (Integer key : input.keys) {
+            input.map.remove(key);
+        }
+        return input.map;
+    }
+
+    @Benchmark
+    public Map<Integer, String> testRemoveEntriesWithSnapshots(TimelineMapInput input) {
+        long epoch = 0;
+        int j = 0;
+        for (Integer key : input.keys) {
+            if (j > 1_000) {
+                input.snapshotRegistry.deleteSnapshotsUpTo(epoch - 10_000);
+                input.snapshotRegistry.createSnapshot(epoch);
+                j = 0;
+            } else {
+                j++;
+            }
+            input.map.remove(key, String.valueOf(key));
+            epoch++;
+        }
+        return input.map;
+    }
+
+    static List<Integer> createKeys(int numberOfEntries) {
+        List<Integer> keys = new ArrayList<>(numberOfEntries);
+        for (int i = 0; i < numberOfEntries; i++) {
+            int key = (int) (0xffffffff & ((i * 2862933555777941757L) + 3037000493L));
+            keys.add(key);
+        }
+
+        return keys;
     }
 }
