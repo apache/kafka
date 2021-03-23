430964fd524abd8b59710c29c6d1464f7dabc7e5
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-03-23 12:25:29 +0000

    migrate kstream peek to new processor

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamPeek.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamPeek.java
@@ -17,11 +17,12 @@
 package org.apache.kafka.streams.kstream.internals;
 
 import org.apache.kafka.streams.kstream.ForeachAction;
-import org.apache.kafka.streams.processor.AbstractProcessor;
-import org.apache.kafka.streams.processor.Processor;
-import org.apache.kafka.streams.processor.ProcessorSupplier;
+import org.apache.kafka.streams.processor.api.AbstractProcessor;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorSupplier;
+import org.apache.kafka.streams.processor.api.Record;
 
-class KStreamPeek<K, V> implements ProcessorSupplier<K, V> {
+class KStreamPeek<K, V> implements ProcessorSupplier<K, V, K, V> {
 
     private final boolean forwardDownStream;
     private final ForeachAction<K, V> action;
@@ -32,16 +33,16 @@ class KStreamPeek<K, V> implements ProcessorSupplier<K, V> {
     }
 
     @Override
-    public Processor<K, V> get() {
+    public Processor<K, V, K, V> get() {
         return new KStreamPeekProcessor();
     }
 
-    private class KStreamPeekProcessor extends AbstractProcessor<K, V> {
+    private class KStreamPeekProcessor extends AbstractProcessor<K, V, K, V> {
         @Override
-        public void process(final K key, final V value) {
-            action.apply(key, value);
+        public void process(final Record<K, V> record) {
+            action.apply(record.key(), record.value());
             if (forwardDownStream) {
-                context().forward(key, value);
+                context().forward(record);
             }
         }
     }
