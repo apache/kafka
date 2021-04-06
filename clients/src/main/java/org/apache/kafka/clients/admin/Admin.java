2f3600198722dd5a01a210bc78b7d43b33967c7f
Ismael Juma <ismael@juma.me.uk>
2021-04-06 08:54:53 -0700

    KAFKA-12579: Remove various deprecated clients classes/methods for 3.0 (#10438)
    * Remove `ExtendedSerializer` and `ExtendedDeserializer`, deprecated since 2.1.
    The extra functionality was also made available in `Serializer` and `Deserializer`.
    * Remove `close(long, TimeUnit)` from the producer, consumer and admin client,
    deprecated since 2.0 for the consumer and 2.2 for the rest. The replacement is `close(Duration)`.
    * Remove `ConsumerConfig.addDeserializerToConfig` and `ProducerConfig.addSerializerToConfig`,
    deprecated since 2.7 with no replacement. These methods were not intended to be public API
    and are likely not used much (if at all).
    * Remove `NoOffsetForPartitionException.partition()`, deprecated since 0.11. `partitions()`
    should be used instead.
    * Remove `MessageFormatter.init(Properties)`, deprecated since 2.7. The `configure(Map)`
    method should be used instead.
    * Remove `kafka.common.MessageFormatter`, deprecated since 2.7.
    `org.apache.kafka.common.MessageFormatter` should be used instead.
    
    Reviewers: Chia-Ping Tsai <chia7712@gmail.com>, David Jacot <djacot@confluent.io>

--- clients/src/main/java/org/apache/kafka/clients/admin/Admin.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/Admin.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.ElectionType;
@@ -147,28 +146,11 @@ public interface Admin extends AutoCloseable {
     /**
      * Close the Admin and release all associated resources.
      * <p>
-     * See {@link #close(long, TimeUnit)}
+     * See {@link #close(Duration)}
      */
     @Override
     default void close() {
-        close(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * Close the Admin and release all associated resources.
-     * <p>
-     * The close operation has a grace period during which current operations will be allowed to
-     * complete, specified by the given duration and time unit.
-     * New operations will not be accepted during the grace period. Once the grace period is over,
-     * all operations that have not yet been completed will be aborted with a {@link org.apache.kafka.common.errors.TimeoutException}.
-     *
-     * @param duration The duration to use for the wait time.
-     * @param unit     The time unit to use for the wait time.
-     * @deprecated Since 2.2. Use {@link #close(Duration)} or {@link #close()}.
-     */
-    @Deprecated
-    default void close(long duration, TimeUnit unit) {
-        close(Duration.ofMillis(unit.toMillis(duration)));
+        close(Duration.ofMillis(Long.MAX_VALUE));
     }
 
     /**
