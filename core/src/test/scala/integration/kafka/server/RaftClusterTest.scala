a7b73448c192aa275cc13136bad21a89007142f3
Luke Chen <showuon@gmail.com>
2021-04-13 14:28:28 +0800

    temp

--- core/src/test/scala/integration/kafka/server/RaftClusterTest.scala
+++ core/src/test/scala/integration/kafka/server/RaftClusterTest.scala
@@ -97,10 +97,11 @@ class RaftClusterTest {
       try {
         // Create a test topic
         val newTopic = Collections.singletonList(new NewTopic("test-topic", 1, 3.toShort))
+        System.err.println("!!! before create topic")
         val createTopicResult = admin.createTopics(newTopic)
         createTopicResult.all().get()
 
-        System.err.println("after create topic")
+        System.err.println("!!! after create topic")
 
         // List created topic
         TestUtils.waitUntilTrue(() => {
@@ -167,10 +168,12 @@ class RaftClusterTest {
         newTopic.add(new NewTopic("test-topic-1", 1, 3.toShort))
         newTopic.add(new NewTopic("test-topic-2", 1, 3.toShort))
         newTopic.add(new NewTopic("test-topic-3", 1, 3.toShort))
+
+        System.err.println("!!! before create topic")
         val createTopicResult = admin.createTopics(newTopic)
         createTopicResult.all().get()
 
-        System.err.println("after create topic")
+        System.err.println("!!! after create topic")
 
         // List created topic
         TestUtils.waitUntilTrue(() => {
