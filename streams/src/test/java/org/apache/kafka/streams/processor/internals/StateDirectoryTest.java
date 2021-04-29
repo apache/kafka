149f1b30a904de9a3ef8f3d7685d1395a26b480a
A. Sophie Blee-Goldman <sophie@confluent.io>
2021-04-28 20:36:49 -0700

    checkstyle

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/StateDirectoryTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/StateDirectoryTest.java
@@ -39,7 +39,6 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
-import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
