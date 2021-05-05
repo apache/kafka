66240b337fb8cdbb426efa66ae77cc9d2b2e3343
Walker Carlson <wcarlson@confluent.io>
2021-05-05 14:06:56 -0700

    checkstyle

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamThread.java
@@ -54,7 +54,6 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
