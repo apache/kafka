5ea0c0747494ff043a4157c577b894d049144272
Mickael Maison <mickael.maison@gmail.com>
2021-05-10 11:16:40 +0100

    MINOR: Update 2.7 branch version to 2.7.2-SNAPSHOT

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "27",
     "dotVersion": "2.7",
-    "fullDotVersion": "2.7.1",
+    "fullDotVersion": "2.7.2-SNAPSHOT",
     "scalaVersion": "2.13"
 };
