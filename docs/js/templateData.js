933b197cbe63fc7b679b263bbd914a2ef1182dda
Konstantine Karantasis <konstantine@confluent.io>
2021-07-06 14:28:13 -0700

    Bump trunk to 3.1.0-SNAPSHOT (#10981)
    Typical version bumps on trunk following the creation of the 3.0 release branch.
    
    Reviewer: Randall Hauch <rhauch@gmail.com>

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -17,8 +17,8 @@ limitations under the License.
 
 // Define variables for doc templates
 var context={
-    "version": "30",
-    "dotVersion": "3.0",
-    "fullDotVersion": "3.0.0",
+    "version": "31",
+    "dotVersion": "3.1",
+    "fullDotVersion": "3.1.0",
     "scalaVersion": "2.13"
 };
