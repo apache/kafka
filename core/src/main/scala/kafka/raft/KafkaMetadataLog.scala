8643ca767e6799d90381732c553950181bc63c88
Zhao Haiyuan <76048421@qq.com>
2021-07-10 16:20:37 +0800

    KAFKA-12333:KafkaMetadataLog and MockLog should validate that appended epochs are monotonically

--- core/src/main/scala/kafka/raft/KafkaMetadataLog.scala
+++ core/src/main/scala/kafka/raft/KafkaMetadataLog.scala
@@ -75,6 +75,9 @@ final class KafkaMetadataLog private (
     if (records.sizeInBytes == 0)
       throw new IllegalArgumentException("Attempt to append an empty record set")
 
+    if (epoch < lastFetchedEpoch())
+      throw new IllegalArgumentException("Attempt to append records with stale epoch")
+
     handleAndConvertLogAppendInfo(
       log.appendAsLeader(records.asInstanceOf[MemoryRecords],
         leaderEpoch = epoch,
