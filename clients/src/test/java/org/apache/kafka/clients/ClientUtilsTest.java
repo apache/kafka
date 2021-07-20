ea47c9972cd47402473d99fd6702a481876853a9
Ismael Juma <ismael@juma.me.uk>
2021-07-20 06:29:20 -0700

    Remove unused import

--- clients/src/test/java/org/apache/kafka/clients/ClientUtilsTest.java
+++ clients/src/test/java/org/apache/kafka/clients/ClientUtilsTest.java
@@ -19,7 +19,6 @@ package org.apache.kafka.clients;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
