01dd5e156bf6b39ba383b03e7c77917bc3d8c18a
Ismael Juma <ismael@juma.me.uk>
2021-07-20 13:42:47 -0700

    MINOR: Fix testTlsDefaults failure due to TLS 1.0/1.1 being disabled (#11092)
    The latest JDKs no longer support TLS 1.0/1.1 causing the test to fail.
    We have already fixed this in trunk and 3.0, so this is for 2.8 and older
    branches.
    
    The relevant trunk commit is 530224e4fe. We had another test with
    the same issue and it was fixed for all branches via #10922.
    
    Reviewers: Rajini Sivaram <rajinisivaram@googlemail.com>

--- clients/src/test/java/org/apache/kafka/common/network/SslTransportLayerTest.java
+++ clients/src/test/java/org/apache/kafka/common/network/SslTransportLayerTest.java
@@ -550,7 +550,7 @@ public class SslTransportLayerTest {
     }
 
     /**
-     * Tests that connection success with the default TLS version.
+     * Tests that connection succeeds with the default TLS version.
      */
     @Test
     public void testTLSDefaults() throws Exception {
@@ -569,12 +569,6 @@ public class SslTransportLayerTest {
         NetworkTestUtils.checkClientConnection(selector, "0", 10, 100);
         server.verifyAuthenticationMetrics(1, 0);
         selector.close();
-
-        checkAuthentiationFailed("1", "TLSv1.1");
-        server.verifyAuthenticationMetrics(1, 1);
-
-        checkAuthentiationFailed("2", "TLSv1");
-        server.verifyAuthenticationMetrics(1, 2);
     }
 
     /** Checks connection failed using the specified {@code tlsVersion}. */
