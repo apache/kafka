32218dde27bed80365cbe61eec772b2d661418ed
A. Sophie Blee-Goldman <sophie@confluent.io>
2021-07-23 16:14:37 -0700

    KAFKA-13021: clarify KIP-633 javadocs and address remaining feedback (#11114)
    There were a few followup things to address from #10926, most importantly a number of updates to the javadocs. Also includes a few missing verification checks.
    
    Reviewers: Guozhang Wang <guozhang@confluent.io>, Matthias J. Sax <mjsax@apache.org>, Israel Ekpo

--- streams/src/main/java/org/apache/kafka/streams/kstream/TimeWindows.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/TimeWindows.java
@@ -78,6 +78,11 @@ public final class TimeWindows extends Windows<TimeWindow> {
             throw new IllegalArgumentException("Window size (sizeMs) must be larger than zero.");
         }
 
+        if (advanceMs <= 0 || advanceMs > sizeMs) {
+            throw new IllegalArgumentException(String.format("Window advancement interval should be more than zero " +
+                "and less than window duration which is %d ms, but given advancement interval is: %d ms", sizeMs, advanceMs));
+        }
+
         if (graceMs < 0) {
             throw new IllegalArgumentException("Grace period must not be negative.");
         }
@@ -90,11 +95,12 @@ public final class TimeWindows extends Windows<TimeWindow> {
      * <p>
      * This provides the semantics of tumbling windows, which are fixed-sized, gap-less, non-overlapping windows.
      * Tumbling windows are a special case of hopping windows with {@code advance == size}.
-     * Using the method implicitly sets the grace period to zero which means
-     * that out of order records arriving after the window end will be dropped
+     * <p>
+     * CAUTION: Using this method implicitly sets the grace period to zero, which means that any out-of-order
+     * records arriving after the window ends are considered late and will be dropped.
      *
      * @param size The size of the window
-     * @return a new window definition with default no grace period. Note that this means out of order records arriving after the window end will be dropped
+     * @return a new window definition with default no grace period. Note that this means out-of-order records arriving after the window end will be dropped
      * @throws IllegalArgumentException if the specified window size is zero or negative or can't be represented as {@code long milliseconds}
      */
     public static TimeWindows ofSizeWithNoGrace(final Duration size) throws IllegalArgumentException {
@@ -108,22 +114,23 @@ public final class TimeWindows extends Windows<TimeWindow> {
      * <p>
      * This provides the semantics of tumbling windows, which are fixed-sized, gap-less, non-overlapping windows.
      * Tumbling windows are a special case of hopping windows with {@code advance == size}.
-     * Using the method explicitly sets the grace period to the duration specified by {@code afterWindowEnd} which means
-     * that out of order records arriving after the window end will be dropped.
-     *
      * <p>
-     * Delay is defined as (stream_time - record_timestamp).
+     * Using this method explicitly sets the grace period to the duration specified by {@code afterWindowEnd}, which
+     * means that only out-of-order records arriving more than the grace period after the window end will be dropped.
+     * The window close, after which any incoming records are considered late and will be rejected, is defined as
+     * {@code windowEnd + afterWindowEnd}
      *
      * @param size The size of the window. Must be larger than zero
      * @param afterWindowEnd The grace period to admit out-of-order events to a window. Must be non-negative.
      * @return a TimeWindows object with the specified size and the specified grace period
      * @throws IllegalArgumentException if {@code afterWindowEnd} is negative or can't be represented as {@code long milliseconds}
      */
-    public static TimeWindows ofSizeAndGrace(final Duration size, final Duration afterWindowEnd)
-            throws IllegalArgumentException {
+    public static TimeWindows ofSizeAndGrace(final Duration size, final Duration afterWindowEnd) throws IllegalArgumentException {
+        final String sizeMsgPrefix = prepareMillisCheckFailMsgPrefix(size, "size");
+        final long sizeMs = validateMillisecondDuration(size, sizeMsgPrefix);
 
-        final long sizeMs = size.toMillis();
-        final long afterWindowEndMs = afterWindowEnd.toMillis();
+        final String afterWindowEndMsgPrefix = prepareMillisCheckFailMsgPrefix(afterWindowEnd, "afterWindowEnd");
+        final long afterWindowEndMs = validateMillisecondDuration(afterWindowEnd, afterWindowEndMsgPrefix);
 
         return new TimeWindows(sizeMs, sizeMs, afterWindowEndMs);
     }
@@ -139,14 +146,14 @@ public final class TimeWindows extends Windows<TimeWindow> {
      * @param size The size of the window
      * @return a new window definition without specifying the grace period (default to 24 hours minus window {@code size})
      * @throws IllegalArgumentException if the specified window size is zero or negative or can't be represented as {@code long milliseconds}
-     * @deprecated since 3.0 Use {@link #ofSizeWithNoGrace(Duration)} } instead
+     * @deprecated since 3.0. Use {@link #ofSizeWithNoGrace(Duration)} } instead
      */
     @Deprecated
     public static TimeWindows of(final Duration size) throws IllegalArgumentException {
         final String msgPrefix = prepareMillisCheckFailMsgPrefix(size, "size");
         final long sizeMs = validateMillisecondDuration(size, msgPrefix);
 
-        return new TimeWindows(sizeMs, sizeMs, Math.max(DEPRECATED_OLD_24_HR_GRACE_PERIOD - sizeMs, 0));
+        return new TimeWindows(sizeMs, sizeMs, Math.max(DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD - sizeMs, 0));
     }
 
     /**
@@ -163,10 +170,6 @@ public final class TimeWindows extends Windows<TimeWindow> {
     public TimeWindows advanceBy(final Duration advance) {
         final String msgPrefix = prepareMillisCheckFailMsgPrefix(advance, "advance");
         final long advanceMs = validateMillisecondDuration(advance, msgPrefix);
-        if (advanceMs <= 0 || advanceMs > sizeMs) {
-            throw new IllegalArgumentException(String.format("Window advancement interval should be more than zero " +
-                    "and less than window duration which is %d ms, but given advancement interval is: %d ms", sizeMs, advanceMs));
-        }
         return new TimeWindows(sizeMs, advanceMs, graceMs);
     }
 
@@ -196,15 +199,13 @@ public final class TimeWindows extends Windows<TimeWindow> {
      * @param afterWindowEnd The grace period to admit out-of-order events to a window.
      * @return this updated builder
      * @throws IllegalArgumentException if {@code afterWindowEnd} is negative or can't be represented as {@code long milliseconds}
-     * @deprecated since 3.0 Use {@link #ofSizeAndGrace(Duration, Duration)} instead
+     * @deprecated since 3.0. Use {@link #ofSizeAndGrace(Duration, Duration)} instead
      */
     @Deprecated
     public TimeWindows grace(final Duration afterWindowEnd) throws IllegalArgumentException {
+        //TODO KAFKA-13021: disallow calling grace() if it was already set via ofTimeDifferenceAndGrace/WithNoGrace()
         final String msgPrefix = prepareMillisCheckFailMsgPrefix(afterWindowEnd, "afterWindowEnd");
         final long afterWindowEndMs = validateMillisecondDuration(afterWindowEnd, msgPrefix);
-        if (afterWindowEndMs < 0) {
-            throw new IllegalArgumentException("Grace period must not be negative.");
-        }
 
         return new TimeWindows(sizeMs, advanceMs, afterWindowEndMs);
     }
--- streams/src/test/java/org/apache/kafka/streams/kstream/JoinWindowsTest.java
+++ streams/src/test/java/org/apache/kafka/streams/kstream/JoinWindowsTest.java
@@ -24,7 +24,7 @@ import static java.time.Duration.ofMillis;
 import static java.time.Duration.ofSeconds;
 import static org.apache.kafka.streams.EqualityCheck.verifyEquality;
 import static org.apache.kafka.streams.EqualityCheck.verifyInEquality;
-import static org.apache.kafka.streams.kstream.Windows.DEPRECATED_OLD_24_HR_GRACE_PERIOD;
+import static org.apache.kafka.streams.kstream.Windows.DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
@@ -122,10 +122,10 @@ public class JoinWindowsTest {
 
     @Test
     public void oldAPIShouldSetDefaultGracePeriod() {
-        assertEquals(Duration.ofDays(1).toMillis(), DEPRECATED_OLD_24_HR_GRACE_PERIOD);
-        assertEquals(DEPRECATED_OLD_24_HR_GRACE_PERIOD - 6L, JoinWindows.of(ofMillis(3L)).gracePeriodMs());
-        assertEquals(0L, JoinWindows.of(ofMillis(DEPRECATED_OLD_24_HR_GRACE_PERIOD)).gracePeriodMs());
-        assertEquals(0L, JoinWindows.of(ofMillis(DEPRECATED_OLD_24_HR_GRACE_PERIOD + 1L)).gracePeriodMs());
+        assertEquals(Duration.ofDays(1).toMillis(), DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD);
+        assertEquals(DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD - 6L, JoinWindows.of(ofMillis(3L)).gracePeriodMs());
+        assertEquals(0L, JoinWindows.of(ofMillis(DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD)).gracePeriodMs());
+        assertEquals(0L, JoinWindows.of(ofMillis(DEPRECATED_DEFAULT_24_HR_GRACE_PERIOD + 1L)).gracePeriodMs());
     }
 
     @Test
