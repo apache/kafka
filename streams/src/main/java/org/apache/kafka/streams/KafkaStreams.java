7cc2b9925bdf1563a32a9347f797bebc8fc3839a
Phil Hardwick <philipchardwick@gmail.com>
2021-08-23 21:21:38 +0100

    HOTFIX: Fix null pointer when getting metric value in MetricsReporter (#11248)
    The alive stream threads metric relies on the threads field as a monitor object for
    its synchronized block. When the alive stream threads metric is registered it isn't
    initialised so any call to get the metric value before it is initialised will result
    in a null pointer exception.
    
    Reviewers: Anna Sophie Blee-Goldman <ableegoldman@apache.org>, Walker Carlson <wcarlson@confluent.io>

--- streams/src/main/java/org/apache/kafka/streams/KafkaStreams.java
+++ streams/src/main/java/org/apache/kafka/streams/KafkaStreams.java
@@ -861,6 +861,7 @@ public class KafkaStreams implements AutoCloseable {
         ClientMetrics.addApplicationIdMetric(streamsMetrics, config.getString(StreamsConfig.APPLICATION_ID_CONFIG));
         ClientMetrics.addTopologyDescriptionMetric(streamsMetrics, internalTopologyBuilder.describe().toString());
         ClientMetrics.addStateMetric(streamsMetrics, (metricsConfig, now) -> state);
+        threads = Collections.synchronizedList(new LinkedList<>());
         ClientMetrics.addNumAliveStreamThreadMetric(streamsMetrics, (metricsConfig, now) -> getNumLiveStreamThreads());
 
         streamsMetadataState = new StreamsMetadataState(
@@ -893,7 +894,6 @@ public class KafkaStreams implements AutoCloseable {
             globalThreadState = globalStreamThread.state();
         }
 
-        threads = Collections.synchronizedList(new LinkedList<>());
         threadState = new HashMap<>(numStreamThreads);
         streamStateListener = new StreamStateListener(threadState, globalThreadState);
 
