ff4d6f54ce24bb5820ef688da1a329faf5252a74
Rohan <desai.p.rohan@gmail.com>
2021-08-27 08:37:08 -0700

    review feedback

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/KafkaConsumerMetricsTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/KafkaConsumerMetricsTest.java
@@ -22,11 +22,14 @@ import org.apache.kafka.common.metrics.Metrics;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class KafkaConsumerMetricsTest {
   private static final long METRIC_VALUE = 123L;
   private static final String CONSUMER_GROUP_PREFIX = "consumer";
   private static final String CONSUMER_METRIC_GROUP = "consumer-metrics";
+  private static final String COMMIT_SYNC_TIME_TOTAL = "commit-sync-time-total";
+  private static final String COMMITTED_TIME_TOTAL = "committed-time-total";
 
   private final Metrics metrics = new Metrics();
   private final KafkaConsumerMetrics consumerMetrics
@@ -38,7 +41,7 @@ class KafkaConsumerMetricsTest {
     consumerMetrics.recordCommitSync(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("commit-sync-time-total");
+    assertMetricValue(COMMIT_SYNC_TIME_TOTAL);
   }
 
   @Test
@@ -47,7 +50,21 @@ class KafkaConsumerMetricsTest {
     consumerMetrics.recordCommitted(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("committed-time-total");
+    assertMetricValue(COMMITTED_TIME_TOTAL);
+  }
+
+  @Test
+  public void shouldRemoveMetricsOnClose() {
+    // When:
+    consumerMetrics.close();
+
+    // Then:
+    assertMetricRemoved(COMMIT_SYNC_TIME_TOTAL);
+    assertMetricRemoved(COMMITTED_TIME_TOTAL);
+  }
+
+  private void assertMetricRemoved(final String name) {
+    assertNull(metrics.metric(metrics.metricName(name, CONSUMER_METRIC_GROUP)));
   }
 
   private void assertMetricValue(final String name) {
--- clients/src/test/java/org/apache/kafka/clients/producer/internals/KafkaProducerMetricsTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/KafkaProducerMetricsTest.java
@@ -22,9 +22,16 @@ import org.apache.kafka.common.metrics.Metrics;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class KafkaProducerMetricsTest {
   private static final long METRIC_VALUE = 123L;
+  private static final String FLUSH_TIME_TOTAL = "flush-time-total";
+  private static final String TXN_INIT_TIME_TOTAL = "txn-init-time-total";
+  private static final String TXN_BEGIN_TIME_TOTAL = "txn-begin-time-total";
+  private static final String TXN_COMMIT_TIME_TOTAL = "txn-commit-time-total";
+  private static final String TXN_ABORT_TIME_TOTAL = "txn-abort-time-total";
+  private static final String TXN_SEND_OFFSETS_TIME_TOTAL = "txn-send-offsets-time-total";
 
   private final Metrics metrics = new Metrics();
   private final KafkaProducerMetrics producerMetrics = new KafkaProducerMetrics(metrics);
@@ -35,7 +42,7 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordFlush(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("flush-time-total");
+    assertMetricValue(FLUSH_TIME_TOTAL);
   }
 
   @Test
@@ -44,7 +51,7 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordInit(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("txn-init-time-total");
+    assertMetricValue(TXN_INIT_TIME_TOTAL);
   }
 
   @Test
@@ -53,7 +60,7 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordBeginTxn(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("txn-begin-time-total");
+    assertMetricValue(TXN_BEGIN_TIME_TOTAL);
   }
 
   @Test
@@ -62,7 +69,7 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordCommitTxn(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("txn-commit-time-total");
+    assertMetricValue(TXN_COMMIT_TIME_TOTAL);
   }
 
   @Test
@@ -71,7 +78,7 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordAbortTxn(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("txn-abort-time-total");
+    assertMetricValue(TXN_ABORT_TIME_TOTAL);
   }
 
   @Test
@@ -80,7 +87,25 @@ class KafkaProducerMetricsTest {
     producerMetrics.recordSendOffsets(METRIC_VALUE);
 
     // Then:
-    assertMetricValue("txn-send-offsets-time-total");
+    assertMetricValue(TXN_SEND_OFFSETS_TIME_TOTAL);
+  }
+
+  @Test
+  public void shouldRemoveMetricsOnClose() {
+    // When:
+    producerMetrics.close();
+
+    // Then:
+    assertMetricRemoved(FLUSH_TIME_TOTAL);
+    assertMetricRemoved(TXN_INIT_TIME_TOTAL);
+    assertMetricRemoved(TXN_BEGIN_TIME_TOTAL);
+    assertMetricRemoved(TXN_COMMIT_TIME_TOTAL);
+    assertMetricRemoved(TXN_ABORT_TIME_TOTAL);
+    assertMetricRemoved(TXN_SEND_OFFSETS_TIME_TOTAL);
+  }
+
+  private void assertMetricRemoved(final String name) {
+    assertNull(metrics.metric(metrics.metricName(name, KafkaProducerMetrics.GROUP)));
   }
 
   private void assertMetricValue(final String name) {
