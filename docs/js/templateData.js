0de260c14678807a17f556c14e0b794af371abbc
David Jacot <djacot@confluent.io>
2021-09-14 15:01:30 +0200

    MINOR: Update version in documentation to 2.8.1 (#11325)
    Reviewers: Manikumar Reddy <manikumar.reddy@gmail.com>, Randall Hauch <rhauch@gmail.com>

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "28",
     "dotVersion": "2.8",
-    "fullDotVersion": "2.8.0",
+    "fullDotVersion": "2.8.1",
     "scalaVersion": "2.13"
 };
