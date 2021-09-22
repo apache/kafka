46b64bf44d5835dff17b9e0c895303145edf4713
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-09-22 20:25:41 +0100

    KAFKA-10544: Migrate KTable aggregate and reduce (#11316)
    As part of the migration of KStream/KTable operations to the new Processor API https://issues.apache.org/jira/browse/KAFKA-8410, this PR includes the migration of KTable aggregate/reduce operations.
    
    Reviewers: John Roesler <vvcephei@apache.org>

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KTableReduce.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KTableReduce.java
@@ -18,13 +18,15 @@ package org.apache.kafka.streams.kstream.internals;
 
 import org.apache.kafka.streams.errors.StreamsException;
 import org.apache.kafka.streams.kstream.Reducer;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
 import org.apache.kafka.streams.state.TimestampedKeyValueStore;
 import org.apache.kafka.streams.state.ValueAndTimestamp;
 
 import static org.apache.kafka.streams.state.ValueAndTimestamp.getValueOrNull;
 
-@SuppressWarnings("deprecation") // Old PAPI. Needs to be migrated.
-public class KTableReduce<K, V> implements KTableProcessorSupplier<K, V, V> {
+public class KTableReduce<K, V> implements KTableNewProcessorSupplier<K, V, K, V> {
 
     private final String storeName;
     private final Reducer<V> addReducer;
@@ -46,19 +48,18 @@ public class KTableReduce<K, V> implements KTableProcessorSupplier<K, V, V> {
     }
 
     @Override
-    public org.apache.kafka.streams.processor.Processor<K, Change<V>> get() {
+    public Processor<K, Change<V>, K, Change<V>> get() {
         return new KTableReduceProcessor();
     }
 
-    private class KTableReduceProcessor extends org.apache.kafka.streams.processor.AbstractProcessor<K, Change<V>> {
+    private class KTableReduceProcessor implements Processor<K, Change<V>, K, Change<V>> {
 
         private TimestampedKeyValueStore<K, V> store;
         private TimestampedTupleForwarder<K, V> tupleForwarder;
 
         @SuppressWarnings("unchecked")
         @Override
-        public void init(final org.apache.kafka.streams.processor.ProcessorContext context) {
-            super.init(context);
+        public void init(final ProcessorContext<K, Change<V>> context) {
             store = (TimestampedKeyValueStore<K, V>) context.getStateStore(storeName);
             tupleForwarder = new TimestampedTupleForwarder<>(
                 store,
@@ -71,42 +72,42 @@ public class KTableReduce<K, V> implements KTableProcessorSupplier<K, V, V> {
          * @throws StreamsException if key is null
          */
         @Override
-        public void process(final K key, final Change<V> value) {
+        public void process(final Record<K, Change<V>> record) {
             // the keys should never be null
-            if (key == null) {
+            if (record.key() == null) {
                 throw new StreamsException("Record key for KTable reduce operator with state " + storeName + " should not be null.");
             }
 
-            final ValueAndTimestamp<V> oldAggAndTimestamp = store.get(key);
+            final ValueAndTimestamp<V> oldAggAndTimestamp = store.get(record.key());
             final V oldAgg = getValueOrNull(oldAggAndTimestamp);
             final V intermediateAgg;
             long newTimestamp;
 
             // first try to remove the old value
-            if (value.oldValue != null && oldAgg != null) {
-                intermediateAgg = removeReducer.apply(oldAgg, value.oldValue);
-                newTimestamp = Math.max(context().timestamp(), oldAggAndTimestamp.timestamp());
+            if (record.value().oldValue != null && oldAgg != null) {
+                intermediateAgg = removeReducer.apply(oldAgg, record.value().oldValue);
+                newTimestamp = Math.max(record.timestamp(), oldAggAndTimestamp.timestamp());
             } else {
                 intermediateAgg = oldAgg;
-                newTimestamp = context().timestamp();
+                newTimestamp = record.timestamp();
             }
 
             // then try to add the new value
             final V newAgg;
-            if (value.newValue != null) {
+            if (record.value().newValue != null) {
                 if (intermediateAgg == null) {
-                    newAgg = value.newValue;
+                    newAgg = record.value().newValue;
                 } else {
-                    newAgg = addReducer.apply(intermediateAgg, value.newValue);
-                    newTimestamp = Math.max(context().timestamp(), oldAggAndTimestamp.timestamp());
+                    newAgg = addReducer.apply(intermediateAgg, record.value().newValue);
+                    newTimestamp = Math.max(record.timestamp(), oldAggAndTimestamp.timestamp());
                 }
             } else {
                 newAgg = intermediateAgg;
             }
 
             // update the store with the new value
-            store.put(key, ValueAndTimestamp.make(newAgg, newTimestamp));
-            tupleForwarder.maybeForward(key, newAgg, sendOldValues ? oldAgg : null, newTimestamp);
+            store.put(record.key(), ValueAndTimestamp.make(newAgg, newTimestamp));
+            tupleForwarder.maybeForward(record.key(), newAgg, sendOldValues ? oldAgg : null, newTimestamp);
         }
     }
 
