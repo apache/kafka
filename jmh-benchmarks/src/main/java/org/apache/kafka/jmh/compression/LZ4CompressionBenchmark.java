71e23c673bc93c22a1da7d1d53afb333ac4df689
Lee Dongjin <dongjin@apache.org>
2021-10-05 18:12:26 +0900

    Add compression benchmarks: gzip, snappy, lz4, zstd.

---
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/compression/LZ4CompressionBenchmark.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.jmh.compression;
+
+import org.apache.kafka.common.record.CompressionConfig;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 15)
+public class LZ4CompressionBenchmark extends AbstractCompressionBenchmark {
+    @Param(value = {"4", "5", "6", "7"})
+    private int blockSize = 4;
+
+    @Param(value = {"1", "5", "9", "13", "17"})
+    private int level = 6;
+
+    @Override
+    CompressionConfig compressionConfig() {
+        return CompressionConfig.lz4().setBlockSize(this.blockSize).build();
+    }
+}
