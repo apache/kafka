2b96da1986928e7d77a6ee0118336d155607d26d
Mickael Maison <mickael.maison@gmail.com>
2021-11-12 15:51:32 +0100

    MINOR: Update 2.6 branch version to 2.6.4-SNAPSHOT

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "26",
     "dotVersion": "2.6",
-    "fullDotVersion": "2.6.3-SNAPSHOT",
+    "fullDotVersion": "2.6.4-SNAPSHOT",
     "scalaVersion": "2.13"
 };
