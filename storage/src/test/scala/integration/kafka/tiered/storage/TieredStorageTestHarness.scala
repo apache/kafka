98d617498556a6003a1ac86e0620978e9a8e5c07
Kamal Chandraprakash <kchandraprakash@uber.com>
2021-12-09 20:24:58 +0530

    DKAFC-1588: Remove acquiring write lock when closing the Remote Log Manager.
    Summary:
    RLM needs a write lock to close itself. But, the read lock is held by other threads which are under the infinite loop:
      # Read lock held by ConsumerManager won't be returned when it's in `waitTillConsumptionCatchesUp` method.
      # Read lock held by ProducerManager won't be released when it's in `publishMessage` method.
    
    Delivery timeout is set to 2 minutes by default. So, once the timeout exceeds the read lock held by the producer manager gets released.
    
    Test Plan:
    Prior to this patch, the TransactionsTestWithTieredStore test takes 20+ minutes to run, now it takes 8 minutes. The normal variant takes 5+ minutes to run the test.
    
    Out of 19 tests, 4 of them each wait for around 35 seconds to get the log segments uploaded to remote storage and removed from the local-log storage physically . This constraint is due to the initial task delay to run the log cleaner thread being set to 30 seconds. (Compared to normal one, extra time required is 35 x 4 = 140 seconds).
    
    The wait timeout is increased to 40 seconds for reliability.
    
    Reviewers: satishd, prateeka, kjayaraman
    
    Reviewed By: satishd
    
    JIRA Issues: DKAFC-1588
    
    Differential Revision: https://code.uberinternal.com/D6928497

--- storage/src/test/scala/integration/kafka/tiered/storage/TieredStorageTestHarness.scala
+++ storage/src/test/scala/integration/kafka/tiered/storage/TieredStorageTestHarness.scala
@@ -66,10 +66,8 @@ abstract class TieredStorageTestHarness extends IntegrationTestHarness {
       metadataConfigPrefix(TopicBasedRemoteLogMetadataManagerConfig.REMOTE_LOG_METADATA_TOPIC_PARTITIONS_PROP), numMetadataPartitions.toString)
     overridingProps.setProperty(
       metadataConfigPrefix(TopicBasedRemoteLogMetadataManagerConfig.REMOTE_LOG_METADATA_TOPIC_REPLICATION_FACTOR_PROP), brokerCount.toString)
-
-    // Set secondary consumer subscription interval as 2 secs.
-    overridingProps.setProperty(metadataConfigPrefix(TopicBasedRemoteLogMetadataManagerConfig.REMOTE_LOG_METADATA_SECONDARY_CONSUMER_SUBSCRIPTION_INTERVAL_MS_PROP),
-      2000.toString);
+    overridingProps.setProperty(
+      metadataConfigPrefix(TopicBasedRemoteLogMetadataManagerConfig.REMOTE_LOG_METADATA_SECONDARY_CONSUMER_SUBSCRIPTION_INTERVAL_MS_PROP), 2000.toString)
 
     //
     // This configuration ensures inactive log segments are deleted fast enough so that
