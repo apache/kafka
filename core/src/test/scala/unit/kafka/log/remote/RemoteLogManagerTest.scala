3b57fb7fac489eae6fbdc8dc4ca90f92406309cb
Satish Duggana <satishd@apache.org>
2021-12-22 09:21:56 +0530

    - Fixed a typo in a method.  - Code cleanup in RemoteLogManagerTest. Those fields will be part of future tests to be added in upcoming PRs.

--- core/src/test/scala/unit/kafka/log/remote/RemoteLogManagerTest.scala
+++ core/src/test/scala/unit/kafka/log/remote/RemoteLogManagerTest.scala
@@ -16,42 +16,14 @@
  */
 package kafka.log.remote
 
-import kafka.server._
-import kafka.server.checkpoints.LeaderEpochCheckpoint
-import kafka.server.epoch.{EpochEntry, LeaderEpochFileCache}
-import kafka.utils.MockTime
 import org.apache.kafka.common.config.AbstractConfig
-import org.apache.kafka.common.utils.Utils
-import org.apache.kafka.common.{TopicIdPartition, TopicPartition, Uuid}
 import org.apache.kafka.server.log.remote.storage._
 import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse}
-import org.junit.jupiter.api.{AfterEach, Test}
+import org.junit.jupiter.api.Test
 
-import java.nio.file.{Files, Paths}
 import java.util.Properties
-import scala.collection.Seq
 
 class RemoteLogManagerTest {
-
-  val clusterId = "test-cluster-id"
-  val brokerId = 0
-  val topicPartition = new TopicPartition("test-topic", 0)
-  val topicIdPartition = new TopicIdPartition(Uuid.randomUuid(), topicPartition)
-  val time = new MockTime()
-  val brokerTopicStats = new BrokerTopicStats
-  val logsDir: String = Files.createTempDirectory("kafka-").toString
-  val checkpoint: LeaderEpochCheckpoint = new LeaderEpochCheckpoint {
-    private var epochs: Seq[EpochEntry] = Seq()
-    override def write(epochs: Iterable[EpochEntry]): Unit = this.epochs = epochs.toSeq
-    override def read(): Seq[EpochEntry] = this.epochs
-  }
-  val cache = new LeaderEpochFileCache(topicPartition, checkpoint)
-  val rlmConfig: RemoteLogManagerConfig = createRLMConfig()
-
-  @AfterEach
-  def afterEach(): Unit = {
-    Utils.delete(Paths.get(logsDir).toFile)
-  }
 
   @Test
   def testRLMConfig(): Unit = {
@@ -67,7 +39,7 @@ class RemoteLogManagerTest {
     assertFalse(rlmmConfig.containsKey("remote.log.metadata.y"))
   }
 
-  private def createRLMConfig(props: Properties = new Properties): RemoteLogManagerConfig = {
+  private def createRLMConfig(props: Properties): RemoteLogManagerConfig = {
     props.put(RemoteLogManagerConfig.REMOTE_LOG_STORAGE_SYSTEM_ENABLE_PROP, true.toString)
     props.put(RemoteLogManagerConfig.REMOTE_STORAGE_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteStorageManager].getName)
     props.put(RemoteLogManagerConfig.REMOTE_LOG_METADATA_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteLogMetadataManager].getName)
