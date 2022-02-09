2ffa2f9321e626fc6d38ac2593b0e528a7451c5b
Hao Geng <hgengcmu@gmail.com>
2022-02-09 14:26:19 -0800

    [LI-HOTFIX]  Add tag for brokerId on metric maxRequestAge (#276)
    TICKET = LIKAFKA-41601
    LI_DESCRIPTION =
    In the precious commit 26a24fd we add a metric called "MaxRequestAge". Before create a sensor for it, we realized that we didn't attach a tag with broker id to id. This commit is to add brokerId as a tag for the metric.
    EXIT_CRITERIA =
    
    
    Co-authored-by: Hao Geng <hgeng@hgeng-mn1.linkedin.biz>

--- core/src/main/scala/kafka/controller/ControllerChannelManager.scala
+++ core/src/main/scala/kafka/controller/ControllerChannelManager.scala
@@ -270,8 +270,11 @@ extends ShutdownableThread(name = name) with KafkaMetricsGroup {
       def value: Long =
         if (latestRequestStatus.isInFlight || latestRequestStatus.isInQueue) time.milliseconds() - latestRequestStatus.enqueueTimeMs
         else 0
-    }
+    },
+    brokerMetricTags(brokerNode.id())
   )
+
+  private def brokerMetricTags(brokerId: Int) = Map("broker-id" -> brokerId.toString)
 
   def backoff(): Unit = pause(100, TimeUnit.MILLISECONDS)
 
@@ -435,7 +438,7 @@ extends ShutdownableThread(name = name) with KafkaMetricsGroup {
   override def initiateShutdown(): Boolean = {
     if (super.initiateShutdown()) {
       networkClient.initiateClose()
-      removeMetric(MaxRequestAgeMetricName)
+      removeMetric(MaxRequestAgeMetricName, brokerMetricTags(brokerNode.id()))
       true
     } else
       false
