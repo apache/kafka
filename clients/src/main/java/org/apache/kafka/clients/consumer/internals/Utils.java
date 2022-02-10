1a9fb88891713e20ee9cf6a0a499e47d71cc1104
Matthias J. Sax <matthias@confluent.io>
2022-02-10 15:37:49 -0800

    checkstyle

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/Utils.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/Utils.java
@@ -24,39 +24,39 @@ import org.apache.kafka.common.TopicPartition;
 
 final class Utils {
 
-  final static class PartitionComparator implements Comparator<TopicPartition>, Serializable {
-      private static final long serialVersionUID = 1L;
-      private Map<String, List<String>> map;
+    final static class PartitionComparator implements Comparator<TopicPartition>, Serializable {
+        private static final long serialVersionUID = 1L;
+        private Map<String, List<String>> map;
 
-      PartitionComparator(Map<String, List<String>> map) {
-          this.map = map;
-      }
+        PartitionComparator(Map<String, List<String>> map) {
+            this.map = map;
+        }
 
-      @Override
-      public int compare(TopicPartition o1, TopicPartition o2) {
-          int ret = map.get(o1.topic()).size() - map.get(o2.topic()).size();
-          if (ret == 0) {
-              ret = o1.topic().compareTo(o2.topic());
-              if (ret == 0)
-                  ret = o1.partition() - o2.partition();
-          }
-          return ret;
-      }
-  }
+        @Override
+        public int compare(TopicPartition o1, TopicPartition o2) {
+            int ret = map.get(o1.topic()).size() - map.get(o2.topic()).size();
+            if (ret == 0) {
+                ret = o1.topic().compareTo(o2.topic());
+                if (ret == 0)
+                    ret = o1.partition() - o2.partition();
+            }
+            return ret;
+        }
+    }
 
-  final static class TopicPartitionComparator implements Comparator<TopicPartition>, Serializable {
-      private static final long serialVersionUID = 1L;
+    final static class TopicPartitionComparator implements Comparator<TopicPartition>, Serializable {
+        private static final long serialVersionUID = 1L;
 
-      @Override
-      public int compare(TopicPartition topicPartition1, TopicPartition topicPartition2) {
-          String topic1 = topicPartition1.topic();
-          String topic2 = topicPartition2.topic();
+        @Override
+        public int compare(TopicPartition topicPartition1, TopicPartition topicPartition2) {
+            String topic1 = topicPartition1.topic();
+            String topic2 = topicPartition2.topic();
 
-          if (topic1.equals(topic2)) {
-              return topicPartition1.partition() - topicPartition2.partition();
-          } else {
-              return topic1.compareTo(topic2);
-          }
-      }
-  }
+            if (topic1.equals(topic2)) {
+                return topicPartition1.partition() - topicPartition2.partition();
+            } else {
+                return topic1.compareTo(topic2);
+            }
+        }
+    }
 }
