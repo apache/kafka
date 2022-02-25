ec74c841e11b7d0a5ccd7fdc74478962fd73ef5a
ecararus <cararuseugeniu@gmail.com>
2022-02-25 10:51:36 +0000

    KAFKA-13691: Rename target topic to custom name

--- connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/RenameTopicReplicationPolicy.java
+++ connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/RenameTopicReplicationPolicy.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.connect.mirror;
 
 import org.slf4j.Logger;
@@ -25,7 +41,7 @@ public class RenameTopicReplicationPolicy extends DefaultReplicationPolicy {
                 String[] topicsArray = topicAssignment.split(",");
                 if (topicsArray.length == 2) {
                     topicMap.put(topicsArray[0], topicsArray[1]);
-                }else{
+                } else {
                     log.info("Malformed arguments were passed in '{}'", topicAssignment);
                 }
             }
@@ -34,7 +50,7 @@ public class RenameTopicReplicationPolicy extends DefaultReplicationPolicy {
 
     @Override
     public String formatRemoteTopic(String sourceClusterAlias, String topic) {
-        String targetTopic = topicMap.containsKey(topic) ?  topicMap.get(topic) : topic ;
+        String targetTopic = topicMap.containsKey(topic) ?  topicMap.get(topic) : topic;
         return super.formatRemoteTopic(sourceClusterAlias,targetTopic);
     }
 
