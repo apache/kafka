5776367f800bc701eab574daccf4197650c67b71
ecararus <cararuseugeniu@gmail.com>
2022-02-28 10:35:39 +0000

    KAFKA-13691: Rename target topic to custom name

--- connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/MirrorClientConfig.java
+++ connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/MirrorClientConfig.java
@@ -50,6 +50,7 @@ public class MirrorClientConfig extends AbstractConfig {
     public static final Class<?> REPLICATION_POLICY_CLASS_DEFAULT = DefaultReplicationPolicy.class;
     public static final String REPLICATION_POLICY_SEPARATOR = "replication.policy.separator";
     public static final String REPLICATION_POLICY_TOPICS_MAP = "replication.policy.topic.map";
+
     private static final String REPLICATION_POLICY_SEPARATOR_DOC = "Separator used in remote topic naming convention.";
     public static final String REPLICATION_POLICY_SEPARATOR_DEFAULT =
         DefaultReplicationPolicy.SEPARATOR_DEFAULT;
