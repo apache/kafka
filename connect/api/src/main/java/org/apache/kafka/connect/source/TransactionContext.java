c2a192bc2e8ecf3528d7a6bee9996d862605c902
Chris Egerton <fearthecellos@gmail.com>
2022-03-01 21:17:51 -0500

    Update connect/api/src/main/java/org/apache/kafka/connect/source/TransactionContext.java
    Co-authored-by: Tom Bentley <tombentley@users.noreply.github.com>

--- connect/api/src/main/java/org/apache/kafka/connect/source/TransactionContext.java
+++ connect/api/src/main/java/org/apache/kafka/connect/source/TransactionContext.java
@@ -38,7 +38,7 @@ public interface TransactionContext {
     /**
      * Requests a transaction abort the next batch of records from {@link SourceTask#poll()}. All of
      * the records in that transaction will be discarded and will not appear in a committed transaction.
-     * However, offsets for that transaction will still be committed. If the data should be reprocessed,
+     * However, offsets for that transaction will still be committed so than the records in that transaction are not reprocessed. If the data should instead be reprocessed,
      * the task should not invoke this method and should instead throw an exception.
      */
     void abortTransaction();
