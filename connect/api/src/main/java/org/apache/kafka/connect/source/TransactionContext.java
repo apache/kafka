ea9ae469c06d85e4e9baf46a54e5e0338dd9a430
Chris Egerton <fearthecellos@gmail.com>
2022-03-07 12:12:47 -0500

    Fix Jacadoc on TransactionContext::abortTransaction

--- connect/api/src/main/java/org/apache/kafka/connect/source/TransactionContext.java
+++ connect/api/src/main/java/org/apache/kafka/connect/source/TransactionContext.java
@@ -36,7 +36,7 @@ public interface TransactionContext {
     void commitTransaction(SourceRecord record);
 
     /**
-     * Requests a transaction abort the next batch of records from {@link SourceTask#poll()}. All of
+     * Requests a transaction abort after the next batch of records from {@link SourceTask#poll()}. All of
      * the records in that transaction will be discarded and will not appear in a committed transaction.
      * However, offsets for that transaction will still be committed so than the records in that transaction
      * are not reprocessed. If the data should instead be reprocessed, the task should not invoke this method
