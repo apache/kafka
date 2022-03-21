4c8685e701e89e1c65acea720cb3facf63389791
Bruno Cadonna <cadonna@apache.org>
2022-03-21 21:37:05 +0100

    MINOR: Bump trunk to 3.3.0-SNAPSHOT (#11925)
    Version bumps on trunk following the creation of the 3.2 release branch.
    
    Reviewer: David Jacot <djacot@confluent.io>

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -17,8 +17,8 @@ limitations under the License.
 
 // Define variables for doc templates
 var context={
-    "version": "32",
-    "dotVersion": "3.2",
-    "fullDotVersion": "3.2.0",
+    "version": "33",
+    "dotVersion": "3.3",
+    "fullDotVersion": "3.3.0",
     "scalaVersion": "2.13"
 };
