bee2ee839640929bd0fc5d74c251b8386bb98a01
Ismael Juma <ismael@juma.me.uk>
2022-03-29 07:37:05 -0700

    Fix checkstyle

--- clients/src/test/java/org/apache/kafka/common/network/DefaultTlsSelectorTest.java
+++ clients/src/test/java/org/apache/kafka/common/network/DefaultTlsSelectorTest.java
@@ -37,52 +37,52 @@ import org.junit.jupiter.api.Test;
  */
 public class DefaultTlsSelectorTest extends SslSelectorTest {
 
-  @Override
-  protected Map<String, Object> createSslClientConfigs(File trustStoreFile) throws GeneralSecurityException, IOException {
-      return TestSslUtils.createSslConfig(false, false, Mode.CLIENT, trustStoreFile, "client");
-  }
+    @Override
+    protected Map<String, Object> createSslClientConfigs(File trustStoreFile) throws GeneralSecurityException, IOException {
+        return TestSslUtils.createSslConfig(false, false, Mode.CLIENT, trustStoreFile, "client");
+    }
 
-  /**
-   * TLS 1.3 has a post-handshake key and IV update, which will update the sending and receiving keys
-   * for one side of the connection.
-   *
-   * Key Usage Limits will trigger an update when the algorithm limits are reached, but the default
-   * value is too large (2^37 bytes of plaintext data) for a unit test. This value can be overridden
-   * via the security property `jdk.tls.keyLimits`, but that's also difficult to achieve in a unit
-   * test.
-   *
-   * Applications can also trigger an update by calling `SSLSocket.startHandshake()` or
-   * `SSLEngine.beginHandshake()` (this would trigger `renegotiation` with TLS 1.2) and that's the
-   * approach we take here.
-   */
-  @Test
-  public void testKeyUpdate() throws Exception {
-      String node = "0";
-      // create connections
-      InetSocketAddress addr = new InetSocketAddress("localhost", server.port);
-      selector.connect(node, addr, BUFFER_SIZE, BUFFER_SIZE);
-      // send echo requests and receive responses
-      while (!selector.isChannelReady(node)) {
-          selector.poll(1000L);
-      }
-      selector.send(createSend(node, node + "-" + 0));
-      selector.poll(0L);
-      server.renegotiate();
-      selector.send(createSend(node,  node + "-" + 1));
-      List<NetworkReceive> received = new ArrayList<>();
-      TestUtils.waitForCondition(() -> {
-        try {
+    /**
+     * TLS 1.3 has a post-handshake key and IV update, which will update the sending and receiving keys
+     * for one side of the connection.
+     *
+     * Key Usage Limits will trigger an update when the algorithm limits are reached, but the default
+     * value is too large (2^37 bytes of plaintext data) for a unit test. This value can be overridden
+     * via the security property `jdk.tls.keyLimits`, but that's also difficult to achieve in a unit
+     * test.
+     *
+     * Applications can also trigger an update by calling `SSLSocket.startHandshake()` or
+     * `SSLEngine.beginHandshake()` (this would trigger `renegotiation` with TLS 1.2) and that's the
+     * approach we take here.
+     */
+    @Test
+    public void testKeyUpdate() throws Exception {
+        String node = "0";
+        // create connections
+        InetSocketAddress addr = new InetSocketAddress("localhost", server.port);
+        selector.connect(node, addr, BUFFER_SIZE, BUFFER_SIZE);
+        // send echo requests and receive responses
+        while (!selector.isChannelReady(node)) {
             selector.poll(1000L);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-        for (NetworkReceive receive : selector.completedReceives()) {
-            if (receive.source().equals(node))
-                received.add(receive);
-        }
-        return received.size() == 2;
-      }, "Expected two receives, got " + received.size());
+        selector.send(createSend(node, node + "-" + 0));
+        selector.poll(0L);
+        server.renegotiate();
+        selector.send(createSend(node,  node + "-" + 1));
+        List<NetworkReceive> received = new ArrayList<>();
+        TestUtils.waitForCondition(() -> {
+            try {
+                selector.poll(1000L);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            for (NetworkReceive receive : selector.completedReceives()) {
+                if (receive.source().equals(node))
+                    received.add(receive);
+            }
+            return received.size() == 2;
+        }, "Expected two receives, got " + received.size());
 
-      assertEquals(asList("0-0", "0-1"), received.stream().map(this::asString).collect(Collectors.toList()));
-  }
+        assertEquals(asList("0-0", "0-1"), received.stream().map(this::asString).collect(Collectors.toList()));
+    }
 }
