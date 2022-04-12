37ad318d378f6ee4da7eaa6ddb94df8c90d34bb5
Philip Nee <philipnee@gmail.com>
2022-04-12 16:42:45 -0700

    Update ConsumerBounceTest.scala

--- core/src/test/scala/integration/kafka/api/ConsumerBounceTest.scala
+++ core/src/test/scala/integration/kafka/api/ConsumerBounceTest.scala
@@ -292,7 +292,6 @@ class ConsumerBounceTest extends AbstractConsumerTest with Logging {
     * Then, 1 consumer should be left out of the group.
     */
   @Test
-  @Disabled // TODO: To be re-enabled once we can make it less flaky (KAFKA-13421)
   def testRollingBrokerRestartsWithSmallerMaxGroupSizeConfigDisruptsBigGroup(): Unit = {
     val group = "group-max-size-test"
     val topic = "group-max-size-test"
