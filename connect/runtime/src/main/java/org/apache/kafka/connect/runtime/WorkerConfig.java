6e0723c498e75adaf45103ed4a13ef760dc5fd36
RivenSun2 <riven.sun@zoom.us>
2022-04-20 23:01:00 +0800

    fix checkstyle issue

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerConfig.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/WorkerConfig.java
@@ -36,7 +36,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
