0463700b49504c64d7fffe43f3bc3c28916277de
zhaobo <zhaobo@kuaishou.com>
2022-05-01 08:08:57 +0800

    MINOR:Fix test in ReplicaManagerTest.scala

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -2352,7 +2352,7 @@ class ReplicaManagerTest {
     val (rm0, rm1) = prepareDifferentReplicaManagers(mock(classOf[BrokerTopicStats]), mockTopicStats1)
 
     try {
-      // make broker 1 the leader of partition 0 and
+      // make broker 0 the leader of partition 0 and
       // make broker 1 the leader of partition 1
       val tp0 = new TopicPartition(topic, 0)
       val tp1 = new TopicPartition(topic, 1)
@@ -2367,7 +2367,7 @@ class ReplicaManagerTest {
             .setTopicName(tp0.topic)
             .setPartitionIndex(tp0.partition)
             .setControllerEpoch(controllerEpoch)
-            .setLeader(1)
+            .setLeader(0)
             .setLeaderEpoch(leaderEpoch)
             .setIsr(partition0Replicas)
             .setPartitionEpoch(0)
@@ -2427,7 +2427,6 @@ class ReplicaManagerTest {
 
     // verify that broker 1 did remove its metrics when no longer being the leader of partition 1
     verify(mockTopicStats1).removeOldLeaderMetrics(topic)
-    verify(mockTopicStats1).removeOldFollowerMetrics(topic)
   }
 
   private def prepareDifferentReplicaManagers(brokerTopicStats1: BrokerTopicStats,
