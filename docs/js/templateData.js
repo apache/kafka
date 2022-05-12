1c28928b9502bd1bc28b4b1dc058fa033d75b558
Tom Bentley <tbentley@redhat.com>
2022-05-12 11:44:18 +0100

    MINOR: Update 3.1 branch version to 3.1.2-SNAPSHOT

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "31",
     "dotVersion": "3.1",
-    "fullDotVersion": "3.1.1-SNAPSHOT",
+    "fullDotVersion": "3.1.2-SNAPSHOT",
     "scalaVersion": "2.13"
 };
