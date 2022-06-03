771c296c6d96cd9b2da63aaced9db3bf72e7df1c
Jim Hughes <jhughes@confluent.io>
2022-06-03 15:43:18 -0400

    Removing most comments.

--- streams/src/test/java/org/apache/kafka/streams/integration/PauseResumeIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/PauseResumeIntegrationTest.java
@@ -30,7 +30,6 @@ import static org.junit.Assert.assertTrue;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -97,7 +96,6 @@ public class PauseResumeIntegrationTest {
     @BeforeClass
     public static void startCluster() throws Exception {
         CLUSTER.start();
-
         producerConfig = TestUtils.producerConfig(CLUSTER.bootstrapServers(),
             StringSerializer.class, LongSerializer.class);
         consumerConfig = TestUtils.consumerConfig(CLUSTER.bootstrapServers(),
@@ -159,18 +157,12 @@ public class PauseResumeIntegrationTest {
     }
 
     private static void produceToInputTopics(final String topic, final Collection<KeyValue<String, Long>> records) {
-        IntegrationTestUtils.produceKeyValuesSynchronously(
-            topic,
-            records,
-            producerConfig,
-            CLUSTER.time
-        );
+        IntegrationTestUtils.produceKeyValuesSynchronously(topic, records, producerConfig, CLUSTER.time);
     }
 
 
     @Test
     public void shouldPauseAndResumeKafkaStreams() throws Exception {
-        // Create KafkaStreams instance
         final StreamsBuilder builder = new StreamsBuilder();
         builder.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
 
@@ -178,78 +170,56 @@ public class PauseResumeIntegrationTest {
         kafkaStreams.start();
         waitForApplicationState(singletonList(kafkaStreams), State.RUNNING, STARTUP_TIMEOUT);
 
-        // Write data
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
-
-        // Verify output
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
 
-        // Pause KafkaStreams/topology
         kafkaStreams.pause();
         assertTrue(kafkaStreams.isPaused());
 
-        // Write more data
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
 
-        // Verify that consumers read new data -- AKA, there is no lag.
         final Map<String, Map<Integer, LagInfo>> lagMap = kafkaStreams.allLocalStorePartitionLags();
         assertNoLag(lagMap);
 
         // Verify no output somehow?
-        // Is there a better way to show this?
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
 
-        // Resume KafkaStreams/topology
         kafkaStreams.resume();
         assertFalse(kafkaStreams.isPaused());
 
-        // Verify that the input written during the pause is processed.
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA2));
     }
 
     @Test
     public void shouldAllowForTopologiesToStartPaused() throws Exception {
-        // Create KafkaStreams instance
         final StreamsBuilder builder = new StreamsBuilder();
         builder.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
 
         kafkaStreams = new KafkaStreams(builder.build(props()), props());
-
-        // Start KafkaStream with paused processing.
         kafkaStreams.pause();
         kafkaStreams.start();
-        // Check for rebalancing instead?
         waitForApplicationState(singletonList(kafkaStreams), State.RUNNING, STARTUP_TIMEOUT);
-
         assertTrue(kafkaStreams.isPaused());
 
-        // Write data
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
-
-        // Verify that consumers read new data -- AKA, there is no lag.
-        final Map<String, Map<Integer, LagInfo>> lagMap =
-            kafkaStreams.allLocalStorePartitionLags();
+        final Map<String, Map<Integer, LagInfo>> lagMap = kafkaStreams.allLocalStorePartitionLags();
         assertNoLag(lagMap);
 
         // Verify no output
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
 
-        // Resume KafkaStreams/topology
         kafkaStreams.resume();
         assertFalse(kafkaStreams.isPaused());
-
-        // Verify that the input written during the pause is processed.
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
     }
 
     @Test
     public void shouldPauseAndResumeKafkaStreamsWithNamedTopologies() throws Exception {
-        // Create KafkaStream / NamedTopologyBuilders
         streamsNamedTopologyWrapper = new KafkaStreamsNamedTopologyWrapper(props());
         final NamedTopologyBuilder builder1 = streamsNamedTopologyWrapper.newNamedTopologyBuilder(TOPOLOGY1);
         builder1.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
@@ -261,33 +231,25 @@ public class PauseResumeIntegrationTest {
             .toStream()
             .to(OUTPUT_STREAM_2);
 
-        // Start KafkaStreams instance
         streamsNamedTopologyWrapper.start(asList(builder1.build(), builder2.build()));
         waitForApplicationState(singletonList(streamsNamedTopologyWrapper), State.RUNNING, STARTUP_TIMEOUT);
 
-        // Write data for topology1
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
 
-        // Verify output
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
 
-        // Pause topology1
         streamsNamedTopologyWrapper.pauseNamedTopology(TOPOLOGY1);
-
-        // Assert the topology1 is paused and topology2 is not paused
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
         assertFalse(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
         assertFalse(streamsNamedTopologyWrapper.isPaused());
 
-        // Write more data for topology 1 and topology 2
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
 
-        // Verify that consumers read new data -- AKA, there is no lag.
         final Map<String, Map<Integer, LagInfo>> lagMap = streamsNamedTopologyWrapper.allLocalStorePartitionLags();
         assertNoLag(lagMap);
 
@@ -300,18 +262,14 @@ public class PauseResumeIntegrationTest {
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA2));
 
 
-        // Resume topology1 and show that it processes data.
         streamsNamedTopologyWrapper.resumeNamedTopology(TOPOLOGY1);
         assertFalse(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
-
-        // Verify that the input written during the pause is processed.
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA2));
     }
 
     @Test
     public void shouldPauseAndResumeAllKafkaStreamsWithNamedTopologies() throws Exception {
-        // Create KafkaStreams instance / NamedTopologyBuilders
         streamsNamedTopologyWrapper = new KafkaStreamsNamedTopologyWrapper(props());
         final NamedTopologyBuilder builder1 = streamsNamedTopologyWrapper.newNamedTopologyBuilder(TOPOLOGY1);
         builder1.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
@@ -323,31 +281,24 @@ public class PauseResumeIntegrationTest {
             .toStream()
             .to(OUTPUT_STREAM_2);
 
-        // Start KafkaStream
         streamsNamedTopologyWrapper.start(asList(builder1.build(), builder2.build()));
         waitForApplicationState(singletonList(streamsNamedTopologyWrapper), State.RUNNING, STARTUP_TIMEOUT);
 
-        // Write data for topology1
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
-
-        // Verify output
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
 
-        // Pause KafkaStreams instance.  This will pause all named/module topologies.
         streamsNamedTopologyWrapper.pause();
         assertTrue(streamsNamedTopologyWrapper.isPaused());
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
 
-        // Write more data for topology 1 and topology 2
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
 
-        // Verify that consumers read new data -- AKA, there is no lag.
         final Map<String, Map<Integer, LagInfo>> lagMap = streamsNamedTopologyWrapper.allLocalStorePartitionLags();
         assertNoLag(lagMap);
 
@@ -359,21 +310,16 @@ public class PauseResumeIntegrationTest {
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
 
-        // Resume topology1.  This will cause us to see that the instance is not paused since there
-        // is at least one topology being processed.
         streamsNamedTopologyWrapper.resumeNamedTopology(TOPOLOGY1);
         assertFalse(streamsNamedTopologyWrapper.isPaused());
         assertFalse(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
-        assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));  // Still paused
-
-        // Verify that the input written during the pause is processed.
+        assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA2));
     }
 
     @Test
     public void shouldAllowForNamedTopologiesToStartPaused() throws Exception {
-        // Create KafkaStreams instance / NamedTopologyBuilders
         streamsNamedTopologyWrapper = new KafkaStreamsNamedTopologyWrapper(props());
         final NamedTopologyBuilder builder1 = streamsNamedTopologyWrapper.newNamedTopologyBuilder(TOPOLOGY1);
         builder1.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
@@ -385,7 +331,6 @@ public class PauseResumeIntegrationTest {
             .toStream()
             .to(OUTPUT_STREAM_2);
 
-        // Start KafkaStream with topology1 paused.
         streamsNamedTopologyWrapper.pauseNamedTopology(TOPOLOGY1);
         streamsNamedTopologyWrapper.start(asList(builder1.build(), builder2.build()));
         waitForApplicationState(singletonList(streamsNamedTopologyWrapper), State.RUNNING, STARTUP_TIMEOUT);
@@ -394,28 +339,22 @@ public class PauseResumeIntegrationTest {
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
         assertFalse(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
 
-        // Write data for topology1
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
 
-        // Verify output
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
 
-        // Pause KafkaStreams instance
         streamsNamedTopologyWrapper.pause();
-
         assertTrue(streamsNamedTopologyWrapper.isPaused());
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
         assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
 
-        // Write more data for topology 1 and topology 2
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
         produceToInputTopics(INPUT_STREAM_2, STANDARD_INPUT_DATA);
 
-        // Verify that consumers read new data -- AKA, there is no lag.
         final Map<String, Map<Integer, LagInfo>> lagMap = streamsNamedTopologyWrapper.allLocalStorePartitionLags();
         assertNoLag(lagMap);
 
@@ -427,70 +366,47 @@ public class PauseResumeIntegrationTest {
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_2, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
 
-        // Resume topology 1; assert that it is running.
         streamsNamedTopologyWrapper.resumeNamedTopology(TOPOLOGY1);
-        assertFalse(streamsNamedTopologyWrapper.isPaused()); // Are all paused?  No.
+        assertFalse(streamsNamedTopologyWrapper.isPaused());
         assertFalse(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY1));
-        assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));  // Still paused
+        assertTrue(streamsNamedTopologyWrapper.isNamedTopologyPaused(TOPOLOGY2));
 
-        // Verify that the input written during the pause is processed.
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA2));
     }
 
     @Test
-    public void shouldWorkAcrossInstances() throws Exception {
-        // Create data on input topics (would need at least two partitions)
+    public void pauseResumehouldWorkAcrossInstances() throws Exception {
         produceToInputTopics(INPUT_STREAM_1, STANDARD_INPUT_DATA);
 
-        // Start two instances paused
-        // Create KafkaStreams instance
         final StreamsBuilder builder = new StreamsBuilder();
         builder.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_1);
-
         kafkaStreams = new KafkaStreams(builder.build(props()), props());
-
-        // Start KafkaStream with paused processing.
         kafkaStreams.pause();
         kafkaStreams.start();
-        // Check for rebalancing instead?
+
         waitForApplicationState(singletonList(kafkaStreams), State.RUNNING, STARTUP_TIMEOUT);
         assertTrue(kafkaStreams.isPaused());
 
-        // Create KafkaStreams instance
         final StreamsBuilder builder2 = new StreamsBuilder();
         builder2.stream(INPUT_STREAM_1).groupByKey().count().toStream().to(OUTPUT_STREAM_2);
 
         kafkaStreams2 = new KafkaStreams(builder2.build(props()), props());
-
-        // Start KafkaStream with paused processing.
         kafkaStreams2.pause();
         kafkaStreams2.start();
-        // Check for rebalancing instead?
-        //waitForApplicationState(singletonList(kafkaStreams2), State.RUNNING, STARTUP_TIMEOUT);
         assertTrue(kafkaStreams2.isPaused());
 
         // Verify no data?
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 0),
             CoreMatchers.equalTo(Collections.emptyList()));
 
-        // -- Verify that each instance is in rebalancing (after change to pause active task restoration)
-
-        System.out.println("JNH: calling close: " + kafkaStreams.state());
-        // Close the other -- this causes a rebalance
         kafkaStreams2.close();
         kafkaStreams2.cleanUp();
         waitForApplicationState(singletonList(kafkaStreams2), State.NOT_RUNNING, STARTUP_TIMEOUT);
 
-        System.out.println("JNH: called close: " + kafkaStreams.state());
-
-        // Resume paused instance
         kafkaStreams.resume();
-        System.out.println("JNH: called resume " + kafkaStreams.state());
         waitForApplicationState(singletonList(kafkaStreams), State.RUNNING, STARTUP_TIMEOUT);
-        System.out.println("JNH: streams is running again " + new Date());
 
-        // Observe all data processed
         assertThat(waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_STREAM_1, 3),
             CoreMatchers.equalTo(COUNT_OUTPUT_DATA));
     }
