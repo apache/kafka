681400061790f642ad3c6418053304a7dbd8ddbf
A. Sophie Blee-Goldman <sophie@confluent.io>
2022-06-03 13:19:52 -0700

    close partition group in closeCleanAndRecycleState

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask.java
@@ -553,6 +553,7 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator,
         switch (state()) {
             case SUSPENDED:
                 stateMgr.recycle();
+                partitionGroup.close();
                 recordCollector.closeClean();
 
                 break;
@@ -614,6 +615,13 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator,
     private void close(final boolean clean) {
         switch (state()) {
             case SUSPENDED:
+                TaskManager.executeAndMaybeSwallow(
+                    clean,
+                    partitionGroup::close,
+                    "partition group close",
+                    log
+                );
+
                 // first close state manager (which is idempotent) then close the record collector
                 // if the latter throws and we re-close dirty which would close the state manager again.
                 TaskManager.executeAndMaybeSwallow(
@@ -637,13 +645,6 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator,
                     log
                 );
 
-                TaskManager.executeAndMaybeSwallow(
-                    clean,
-                    partitionGroup::close,
-                    "partition group close",
-                    log
-                );
-
                 break;
 
             case CLOSED:
@@ -660,7 +661,6 @@ public class StreamTask extends AbstractTask implements ProcessorNodePunctuator,
         }
 
         record = null;
-        partitionGroup.clear();
         closeTaskSensor.record();
 
         transitionTo(State.CLOSED);
