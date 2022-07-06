63d6e01e2285a2c1cbc592dd202cfc1801a88ebd
Stanislav Vodetskyi <stan@confluent.io>
2022-07-06 16:57:23 -0700

    try using kraft

--- tests/kafkatest/tests/core/mini_test.py
+++ tests/kafkatest/tests/core/mini_test.py
@@ -1,18 +1,22 @@
 from ducktape.tests.test import Test
+from ducktape.mark import matrix
 from ducktape.mark.resource import cluster
 
 from kafkatest.services.zookeeper import ZookeeperService
-from kafkatest.services.kafka import KafkaService
+from kafkatest.services.kafka import KafkaService, quorum
 
 
 class MiniTest(Test):
     def __init__(self, test_context):
-        super(MiniTest, self).__init__(test_context=test_context)
+        super().__init__(test_context=test_context)
 
-        self.zk = ZookeeperService(test_context, 1)
+        self.zk = ZookeeperService(test_context, 1) if quorum.for_test(test_context) == quorum.zk else None
         self.kafka = KafkaService(test_context, 1, self.zk)
 
     @cluster(num_nodes=2)
-    def test(self):
-        self.zk.start()
+    @matrix(metadata_quorum=quorum.all)
+    def test(self, metadata_quorum):
+        # zk will be none if using kraft
+        if self.zk:
+            self.zk.start()
         self.kafka.start()
