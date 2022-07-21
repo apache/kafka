1118c7a5fe6563fbf2789ce4f730fd97a26ccc7e
Colin P. McCabe <cmccabe@apache.org>
2022-07-21 01:14:31 -0700

    change test default to kraft

--- core/src/test/scala/kafka/utils/TestInfoUtils.scala
+++ core/src/test/scala/kafka/utils/TestInfoUtils.scala
@@ -36,6 +36,116 @@ class EmptyTestInfo extends TestInfo {
 }
 
 object TestInfoUtils {
+  val legacyTestClassNames = Set(
+    "integration.kafka.network.DynamicNumNetworkThreadsTest",
+    "integration.kafka.server.FetchRequestBetweenDifferentIbpTest",
+    "integration.kafka.server.FetchRequestTestDowngrade",
+    "kafka.admin.AclCommandTest",
+    "kafka.admin.AddPartitionsTest",
+    "kafka.admin.AdminZkClientTest",
+    "kafka.admin.BrokerApiVersionsCommandTest",
+    "kafka.admin.DelegationTokenCommandTest",
+    "kafka.admin.DeleteConsumerGroupsTest",
+    "kafka.admin.DeleteOffsetsConsumerGroupCommandIntegrationTest",
+    "kafka.admin.DeleteTopicTest",
+    "kafka.admin.DescribeConsumerGroupTest",
+    "kafka.admin.FeatureCommandTest",
+    "kafka.admin.ListConsumerGroupTest",
+    "kafka.admin.ListOffsetsIntegrationTest",
+    "kafka.admin.LogDirsCommandTest",
+    "kafka.admin.ResetConsumerGroupOffsetTest",
+    "kafka.admin.UserScramCredentialsCommandTest",
+    "kafka.api.ConsumerWithLegacyMessageFormatIntegrationTest",
+    "kafka.api.CustomQuotaCallbackTest",
+    "kafka.api.DelegationTokenEndToEndAuthorizationTest",
+    "kafka.api.DelegationTokenEndToEndAuthorizationWithOwnerTest",
+    "kafka.api.DescribeAuthorizedOperationsTest",
+    "kafka.api.GroupCoordinatorIntegrationTest",
+    "kafka.api.GroupEndToEndAuthorizationTest",
+    "kafka.api.MetricsTest",
+    "kafka.api.PlaintextAdminIntegrationTest",
+    "kafka.api.PlaintextConsumerTest",
+    "kafka.api.PlaintextProducerSendTest",
+    "kafka.api.ProducerFailureHandlingTest",
+    "kafka.api.ProducerSendWhileDeletionTest",
+    "kafka.api.RackAwareAutoTopicCreationTest",
+    "kafka.api.SaslClientsWithInvalidCredentialsTest",
+    "kafka.api.SaslGssapiSslEndToEndAuthorizationTest",
+    "kafka.api.SaslOAuthBearerSslEndToEndAuthorizationTest",
+    "kafka.api.SaslPlainPlaintextConsumerTest",
+    "kafka.api.SaslPlainSslEndToEndAuthorizationTest",
+    "kafka.api.SaslPlaintextConsumerTest",
+    "kafka.api.SaslSslAdminIntegrationTest",
+    "kafka.api.SaslSslConsumerTest",
+    "kafka.api.SslAdminIntegrationTest",
+    "kafka.api.SslConsumerTest",
+    "kafka.api.SslEndToEndAuthorizationTest",
+    "kafka.api.SslProducerSendTest",
+    "kafka.api.TransactionsBounceTest",
+    "kafka.api.TransactionsWithMaxInFlightOneTest",
+    "kafka.api.test.ProducerCompressionTest",
+    "kafka.common.ZkNodeChangeNotificationListenerTest",
+    "kafka.controller.ControllerFailoverTest",
+    "kafka.controller.ControllerIntegrationTest",
+    "kafka.integration.MetricsDuringTopicCreationDeletionTest",
+    "kafka.integration.UncleanLeaderElectionTest",
+    "kafka.network.DynamicConnectionQuotaTest",
+    "kafka.security.auth.ZkAuthorizationTest",
+    "kafka.security.authorizer.AclAuthorizerTest",
+    "kafka.security.authorizer.AclAuthorizerWithZkSaslTest",
+    "kafka.security.authorizer.AuthorizerInterfaceDefaultTest",
+    "kafka.security.token.delegation.DelegationTokenManagerTest",
+    "kafka.server.AdvertiseBrokerTest",
+    "kafka.server.AlterReplicaLogDirsRequestTest",
+    "kafka.server.AlterUserScramCredentialsRequestTest",
+    "kafka.server.BrokerEpochIntegrationTest",
+    "kafka.server.ControllerMutationQuotaTest",
+    "kafka.server.DelegationTokenRequestsOnPlainTextTest",
+    "kafka.server.DelegationTokenRequestsTest",
+    "kafka.server.DelegationTokenRequestsWithDisableTokenFeatureTest",
+    "kafka.server.DeleteTopicsRequestWithDeletionDisabledTest",
+    "kafka.server.DescribeClusterRequestTest",
+    "kafka.server.DescribeLogDirsRequestTest",
+    "kafka.server.DescribeUserScramCredentialsRequestNotAuthorizedTest",
+    "kafka.server.DescribeUserScramCredentialsRequestTest",
+    "kafka.server.DynamicBrokerReconfigurationTest",
+    "kafka.server.DynamicConfigTest",
+    "kafka.server.EdgeCaseRequestTest",
+    "kafka.server.FetchRequestDownConversionConfigTest",
+    "kafka.server.FetchRequestMaxBytesTest",
+    "kafka.server.FetchRequestTest",
+    "kafka.server.FinalizedFeatureChangeListenerTest",
+    "kafka.server.GssapiAuthenticationTest",
+    "kafka.server.KafkaMetricReporterExceptionHandlingTest",
+    "kafka.server.KafkaMetricsReporterTest",
+    "kafka.server.LeaderElectionTest",
+    "kafka.server.ListOffsetsRequestTest",
+    "kafka.server.LogDirFailureTest",
+    "kafka.server.LogRecoveryTest",
+    "kafka.server.MetadataRequestBetweenDifferentIbpTest",
+    "kafka.server.MultipleListenersWithAdditionalJaasContextTest",
+    "kafka.server.MultipleListenersWithDefaultJaasContextTest",
+    "kafka.server.OffsetsForLeaderEpochRequestTest",
+    "kafka.server.ProduceRequestTest",
+    "kafka.server.ReplicationQuotasTest",
+    "kafka.server.RequestQuotaTest",
+    "kafka.server.ScramServerStartupTest",
+    "kafka.server.ServerGenerateClusterIdTest",
+    "kafka.server.ServerShutdownTest",
+    "kafka.server.ServerStartupTest",
+    "kafka.server.StopReplicaRequestTest",
+    "kafka.server.TopicIdWithOldInterBrokerProtocolTest",
+    "kafka.server.UpdateFeaturesTest",
+    "kafka.server.epoch.EpochDrivenReplicationProtocolAcceptanceTest",
+    "kafka.server.epoch.EpochDrivenReplicationProtocolAcceptanceWithIbp26Test",
+    "kafka.server.epoch.LeaderEpochIntegrationTest",
+    "kafka.tools.GetOffsetShellTest",
+    "kafka.tools.MirrorMakerIntegrationTest",
+    "kafka.utils.ReplicationUtilsTest",
+    "kafka.zk.KafkaZkClientTest",
+    "kafka.zookeeper.ZooKeeperClientTest",
+  )
+
   def isKRaft(testInfo: TestInfo): Boolean = {
     if (testInfo.getDisplayName().contains("quorum=")) {
       if (testInfo.getDisplayName().contains("quorum=kraft")) {
@@ -46,12 +156,18 @@ object TestInfoUtils {
         throw new RuntimeException(s"Unknown quorum value")
       }
     } else {
+      // If there is no quorum parameter, we check if any class in our current stack trace is a legacy test class.
+      // If it is, we default to ZK mode. Otherwise, we default to KRaft mode.
       testInfo.getTestClass().asScala match {
         case None => false
-        case Some(testClass) =>
-          Files.write(Paths.get("/Users/cmccabe/all_classes.txt"), (testClass.getCanonicalName + "\n").getBytes(), StandardOpenOption.APPEND)
-          false
+        case Some(testClass) => !legacyTestClassNames.contains(testClass.getCanonicalName)
       }
+//      testInfo.getTestClass().asScala match {
+//        case None => false
+//        case Some(testClass) =>
+//          Files.write(Paths.get("/Users/cmccabe/all_classes.txt"), (testClass.getCanonicalName + "\n").getBytes(), StandardOpenOption.APPEND)
+//          false
+//      }
     }
   }
 
