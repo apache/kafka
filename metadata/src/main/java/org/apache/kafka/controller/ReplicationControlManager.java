bbd659325a60c8923afaa0bf099ad0536e297914
Colin Patrick McCabe <cmccabe@apache.org>
2022-08-02 15:39:47 -0700

    KAFKA-14129: KRaft must check manual assignments for createTopics are contiguous (#12467)
    KRaft should validate that manual assignments given to createTopics are contiguous. In other words,
    they must start with partition 0, and progress through 1, 2, 3, etc. ZK mode does this, but KRaft
    mode previously did not. Also fix a null pointer exception when the placement for partition 0
    was not specified.
    
    Convert over AddPartitionsTest to use KRaft. This PR converts all of the test except for some of
    the placement logic tests, which will need to be redone for KRaft mode in a future change.
    
    Fix null pointer exception in KRaftMetadataCache#getPartitionInfo.  Specifically, we should not
    assume that the partition will be found in the hash map. This is another case where we had
    "Some(x)" but it should be "Option(x)."
    
    Fix a potential null pointer exception in BrokerServer#state.
    
    Reviewers: dengziming <dengziming1993@gmail.com>, Jason Gustafson <jason@confluent.io>

--- metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
+++ metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
@@ -666,6 +666,12 @@ public class ReplicationControlManager {
                     Replicas.toArray(assignment.brokerIds()), Replicas.toArray(isr),
                     Replicas.NONE, Replicas.NONE, isr.get(0), LeaderRecoveryState.RECOVERED, 0, 0));
             }
+            for (int i = 0; i < newParts.size(); i++) {
+                if (!newParts.containsKey(i)) {
+                    return new ApiError(Errors.INVALID_REPLICA_ASSIGNMENT,
+                            "partitions should be a consecutive 0-based integer sequence");
+                }
+            }
             ApiError error = maybeCheckCreateTopicPolicy(() -> {
                 Map<Integer, List<Integer>> assignments = new HashMap<>();
                 newParts.entrySet().forEach(e -> assignments.put(e.getKey(),
@@ -744,7 +750,7 @@ public class ReplicationControlManager {
                     setIsSensitive(entry.isSensitive()));
             }
             result.setNumPartitions(newParts.size());
-            result.setReplicationFactor((short) newParts.get(0).replicas.length);
+            result.setReplicationFactor((short) newParts.values().iterator().next().replicas.length);
             result.setTopicConfigErrorCode(NONE.code());
         } else {
             result.setTopicConfigErrorCode(TOPIC_AUTHORIZATION_FAILED.code());
