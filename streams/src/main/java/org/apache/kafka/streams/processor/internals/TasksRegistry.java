f191126550f155c50069a75a82fd44923997642a
Bruno Cadonna <cadonna@apache.org>
2022-08-24 08:19:40 +0200

    KAFKA-10199: Introduce task registry (#12549)
    Currently the task manager stores the tasks it manages in an
    internally. We recently extracted the code to store and retrieve
    tasks into its own class Tasks. However, the task manager creates
    the Tasks object internally and during testing of the task
    manager we do not have access to it which makes testing the task
    manager quite complex.
    
    This commit externalizes the data structure that the task manager
    uses to store and rerieve tasks. It introduces the TasksRegistry
    interface and lets the Tasks object implementing TasksRegistry.
    The Tasks object is passed into the task manager via its
    constructor. Passing the TasksRegistry dependency to the task
    manager from outside faciliates simpler testing of the task
    manager.
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>, Walker Carlson <wcarlson@confluent.io>

---
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/TasksRegistry.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.streams.processor.internals;
+
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.streams.processor.TaskId;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public interface TasksRegistry {
+
+    Map<TaskId, Set<TopicPartition>> drainPendingActiveTasksForTopologies(final Set<String> currentTopologies);
+
+    Map<TaskId, Set<TopicPartition>> drainPendingStandbyTasksForTopologies(final Set<String> currentTopologies);
+
+    void addPendingActiveTasksToCreate(final Map<TaskId, Set<TopicPartition>> pendingTasks);
+
+    void addPendingStandbyTasksToCreate(final Map<TaskId, Set<TopicPartition>> pendingTasks);
+
+    void clearPendingTasksToCreate();
+
+    Set<TopicPartition> removePendingTaskToRecycle(final TaskId taskId);
+
+    void addPendingTaskToRecycle(final TaskId taskId, final Set<TopicPartition> inputPartitions);
+
+    Set<TopicPartition> removePendingTaskToUpdateInputPartitions(final TaskId taskId);
+
+    void addPendingTaskToUpdateInputPartitions(final TaskId taskId, final Set<TopicPartition> inputPartitions);
+
+    boolean removePendingTaskToCloseDirty(final TaskId taskId);
+
+    void addPendingTaskToCloseDirty(final TaskId taskId);
+
+    boolean removePendingTaskToCloseClean(final TaskId taskId);
+
+    void addPendingTaskToCloseClean(final TaskId taskId);
+
+    Set<Task> drainPendingTaskToInit();
+
+    void addPendingTaskToInit(final Collection<Task> tasks);
+
+    void addNewActiveTasks(final Collection<Task> newTasks);
+
+    void addNewStandbyTasks(final Collection<Task> newTasks);
+
+    void removeTask(final Task taskToRemove);
+
+    void replaceActiveWithStandby(final StandbyTask standbyTask);
+
+    void replaceStandbyWithActive(final StreamTask activeTask);
+
+    boolean updateActiveTaskInputPartitions(final Task task, final Set<TopicPartition> topicPartitions);
+
+    void clear();
+
+    Task activeTasksForInputPartition(final TopicPartition partition);
+
+    Task task(final TaskId taskId);
+
+    Collection<Task> tasks(final Collection<TaskId> taskIds);
+
+    Collection<Task> activeTasks();
+
+    Set<Task> allTasks();
+
+    Map<TaskId, Task> allTasksPerId();
+
+    Set<TaskId> allTaskIds();
+
+    boolean contains(final TaskId taskId);
+
+    void addTask(final Task task);
+}
