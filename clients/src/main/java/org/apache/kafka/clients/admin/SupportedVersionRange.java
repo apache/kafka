a48dce7c78f99594ad483b6f5e570c8e78b60c34
Colin P. McCabe <cmccabe@apache.org>
2022-08-29 13:05:40 -0700

    KAFKA-14187: kafka-featuressh: add support for --metadata
    This PR adds support to kafka-features.sh for the --metadata flag, as specified in KIP-778.  This
    flag makes it possible to upgrade to a new metadata version without consulting a table mapping
    version names to short integers.
    
    FeatureCommandTest.scala: make most tests here true unit tests (that don't start brokers) in order
    to improve test run time, and allow us to test more cases. For the integration test part, test both
    KRaft and ZK-based clusters. Add support for mocking feature operations in MockAdminClient.java.
    
    upgrade.html: add a section describing how the metadata.version should be upgraded in KRaft
    clusters.

--- clients/src/main/java/org/apache/kafka/clients/admin/SupportedVersionRange.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/SupportedVersionRange.java
@@ -36,10 +36,10 @@ public class SupportedVersionRange {
      * @throws IllegalArgumentException   Raised when the condition described above is not met.
      */
     SupportedVersionRange(final short minVersion, final short maxVersion) {
-        if (minVersion < 1 || maxVersion < 1 || maxVersion < minVersion) {
+        if (minVersion < 0 || maxVersion < 0 || maxVersion < minVersion) {
             throw new IllegalArgumentException(
                 String.format(
-                    "Expected 1 <= minVersion <= maxVersion but received minVersion:%d, maxVersion:%d.",
+                    "Expected 0 <= minVersion <= maxVersion but received minVersion:%d, maxVersion:%d.",
                     minVersion,
                     maxVersion));
         }
