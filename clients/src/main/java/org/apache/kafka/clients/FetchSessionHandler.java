9c1b68e0794d60f4ab8a4d86d09abc75d5724264
Dom Twitty <dtwitty@linkedin.com>
2022-08-30 10:38:33 -0700

    [LI-HOTFIX] Downgrade FETCH_SESSION_ID_NOT_FOUND logging from info to debug

--- clients/src/main/java/org/apache/kafka/clients/FetchSessionHandler.java
+++ clients/src/main/java/org/apache/kafka/clients/FetchSessionHandler.java
@@ -382,11 +382,15 @@ public class FetchSessionHandler {
      */
     public boolean handleResponse(FetchResponse<?> response) {
         if (response.error() != Errors.NONE) {
-            log.info("Node {} was unable to process the fetch request with {}: {}.",
-                node, nextMetadata, response.error());
             if (response.error() == Errors.FETCH_SESSION_ID_NOT_FOUND) {
+                log.debug("Node {} was unable to process the fetch request with {} due to a cache miss. " +
+                    "This client will reset the fetch session. " +
+                    "This is not an error, bit may indicate a sub-optimal broker configuration.",
+                    node, nextMetadata);
                 nextMetadata = FetchMetadata.INITIAL;
             } else {
+                log.info("Node {} was unable to process the fetch request with {}: {}.",
+                    node, nextMetadata, response.error());
                 nextMetadata = nextMetadata.nextCloseExisting();
             }
             return false;
