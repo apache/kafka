50029d3ed8ba576fd605df6a67f4229303cc944d
Tom Bentley <tbentley@redhat.com>
2022-09-13 09:12:22 +0100

    Bump version to 3.2.3

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -116,7 +116,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("3.2.2-SNAPSHOT")
+DEV_VERSION = KafkaVersion("3.2.3-SNAPSHOT")
 
 # 0.8.2.x versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
