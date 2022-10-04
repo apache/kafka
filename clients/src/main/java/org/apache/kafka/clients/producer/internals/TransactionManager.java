0c8ee2c9f2fdf20e48f1c4e0e1ba93caba3e3cd9
Lucas Brutschy <lbrutschy@confluent.io>
2022-10-04 13:58:50 +0200

    Minor: Resolve UNSUPPORTED_FOR_MESSAGE_FORMAT inconsistency
    According to API documentation, UNSUPPORTED_FOR_MESSAGE_FORMAT is not possible for TxnOffsetCommit, but it looks like it is (and correctly being handled here).
    
    Also inline trivial `isFatalException`.

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/TransactionManager.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/TransactionManager.java
@@ -1659,7 +1659,8 @@ public class TransactionManager {
                     abortableError(new CommitFailedException("Transaction offset Commit failed " +
                         "due to consumer group metadata mismatch: " + error.exception().getMessage()));
                     break;
-                } else if (isFatalException(error)) {
+                } else if (error == Errors.TRANSACTIONAL_ID_AUTHORIZATION_FAILED
+                        || error == Errors.UNSUPPORTED_FOR_MESSAGE_FORMAT) {
                     fatalError(error.exception());
                     break;
                 } else {
@@ -1677,11 +1678,6 @@ public class TransactionManager {
                 reenqueue();
             }
         }
-    }
-
-    private boolean isFatalException(Errors error) {
-        return error == Errors.TRANSACTIONAL_ID_AUTHORIZATION_FAILED
-                   || error == Errors.UNSUPPORTED_FOR_MESSAGE_FORMAT;
     }
 
     private static final class PendingStateTransition {
