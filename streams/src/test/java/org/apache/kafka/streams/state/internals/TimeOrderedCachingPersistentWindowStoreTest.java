b8ceffab832bb86e59f0d2ab7b205120a1fe2368
SHEKHAR PRASAD RAJAK <shekhar_rajak@apple.com>
2022-10-14 12:21:22 +0530

    minor change

--- streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedCachingPersistentWindowStoreTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedCachingPersistentWindowStoreTest.java
@@ -56,7 +56,6 @@ import org.apache.kafka.test.TestUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
