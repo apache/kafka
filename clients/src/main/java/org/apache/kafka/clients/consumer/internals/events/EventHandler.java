f602eda3378fab4d4b186b7a5c07ddf1f44e9fcc
Philip Nee <pnee@confluent.io>
2022-10-19 19:02:55 -0700

    KAFKA-14247: Consumer background thread base implementation (#12672)
    Adds skeleton of the background thread.
    
    1-pager: https://cwiki.apache.org/confluence/display/KAFKA/Proposal%3A+Consumer+Threading+Model+Refactor
    Continuation of #12663
    
    Reviewers: Guozhang Wang <guozhang@apache.org>, Kirk True <kirk@mustardgrain.com>, John Roesler <vvcephei@apache.org>

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/events/EventHandler.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/events/EventHandler.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.kafka.clients.consumer.internals.events;
 
 import java.util.Optional;
