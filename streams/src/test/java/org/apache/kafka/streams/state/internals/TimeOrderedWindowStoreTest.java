ecabd3d5206b210d1f9e9a2bc6eaee70924478ba
SHEKHAR PRASAD RAJAK <shekhar_rajak@apple.com>
2022-10-21 11:30:07 +0530

    minor change: imports update

--- streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedWindowStoreTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/TimeOrderedWindowStoreTest.java
@@ -89,7 +89,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
 
 @RunWith(Parameterized.class)
 public class TimeOrderedWindowStoreTest {
