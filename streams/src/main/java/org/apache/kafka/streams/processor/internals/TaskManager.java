1c8b5e3faac662736ed4c6b94766d1d9a2e97262
Lucas Brutschy <lbrutschy@confluent.io>
2022-11-22 18:40:08 +0100

    Revert "KAFKA-14299: Initialize tasks in state updater (#12795)"
    This reverts commit a55071a99fabc9a706afa0e9acddf898c7cd05c4.

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
@@ -400,8 +400,8 @@ public class TaskManager {
             tasks.addActiveTasks(newActiveTasks);
             tasks.addStandbyTasks(newStandbyTask);
         } else {
-            Stream.concat(newActiveTasks.stream(), newStandbyTask.stream())
-                    .forEach(stateUpdater::add);
+            tasks.addPendingTaskToInit(newActiveTasks);
+            tasks.addPendingTaskToInit(newStandbyTask);
         }
     }
 
@@ -735,6 +735,7 @@ public class TaskManager {
 
     public boolean checkStateUpdater(final long now,
                                      final java.util.function.Consumer<Set<TopicPartition>> offsetResetter) {
+        addTasksToStateUpdater();
         if (stateUpdater.hasExceptionsAndFailedTasks()) {
             handleExceptionsFromStateUpdater();
         }
@@ -757,6 +758,7 @@ public class TaskManager {
             newTask = task.isActive() ?
                 convertActiveToStandby((StreamTask) task, inputPartitions) :
                 convertStandbyToActive((StandbyTask) task, inputPartitions);
+            newTask.initializeIfNeeded();
             stateUpdater.add(newTask);
         } catch (final RuntimeException e) {
             final TaskId taskId = task.id();
@@ -814,6 +816,22 @@ public class TaskManager {
                 timeoutException
             );
         }
+    }
+
+    private void addTasksToStateUpdater() {
+        final Map<TaskId, RuntimeException> taskExceptions = new LinkedHashMap<>();
+        for (final Task task : tasks.drainPendingTaskToInit()) {
+            try {
+                task.initializeIfNeeded();
+                stateUpdater.add(task);
+            } catch (final RuntimeException e) {
+                // need to add task back to the bookkeeping to be handled by the stream thread
+                tasks.addTask(task);
+                taskExceptions.put(task.id(), e);
+            }
+        }
+
+        maybeThrowTaskExceptions(taskExceptions);
     }
 
     public void handleExceptionsFromStateUpdater() {
