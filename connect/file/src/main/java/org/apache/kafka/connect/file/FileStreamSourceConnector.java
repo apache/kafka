apache/kafka

commit e11b515ef35475e8940cf2655a6d3a5d2acecbe3

KAFKA-14017: Implement new KIP-618 APIs in FileStreamSourceConnector (#12355)
Reviewers: Yash Mayya <yash.mayya@gmail.com>,  Maison <mickael.maison@gmail.com>

connect/file/src/main/java/org/apache/kafka/connect/file/FileStreamSourceConnector.java
@@ -22,6 +22,7 @@ import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.utils.AppInfoParser;
 import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.ExactlyOnceSupport;
 import org.apache.kafka.connect.source.SourceConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -86,4 +87,18 @@ public class FileStreamSourceConnector extends SourceConnector {
     public ConfigDef config() {
         return CONFIG_DEF;
     }
+
+    @Override
+    public ExactlyOnceSupport exactlyOnceSupport(Map<String, String> props) {
+        AbstractConfig parsedConfig = new AbstractConfig(CONFIG_DEF, props);
+        filename = parsedConfig.getString(FILE_CONFIG);
+        // We can provide exactly-once guarantees if reading from a "real" file
+        // (as long as the file is only appended to over the lifetime of the connector)
+        // If we're reading from stdin, we can't provide exactly-once guarantees
+        // since we don't even track offsets
+        return filename != null && !filename.isEmpty()
+                ? ExactlyOnceSupport.SUPPORTED
+                : ExactlyOnceSupport.UNSUPPORTED;
+    }
+
 }

connect/file/src/test/java/org/apache/kafka/connect/file/FileStreamSourceConnectorTest.java
@@ -20,6 +20,8 @@ import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.config.ConfigValue;
 import org.apache.kafka.connect.connector.ConnectorContext;
+import org.apache.kafka.connect.source.ConnectorTransactionBoundaries;
+import org.apache.kafka.connect.source.ExactlyOnceSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -57,6 +59,23 @@ public class FileStreamSourceConnectorTest {
         for (ConfigValue val : configValues) {
             assertEquals(0, val.errorMessages().size(), "Config property errors: " + val.errorMessages());
         }
+    }
+
+    @Test
+    public void testExactlyOnceSupport() {
+        sourceProperties.put(FileStreamSourceConnector.FILE_CONFIG, FILENAME);
+        assertEquals(ExactlyOnceSupport.SUPPORTED, connector.exactlyOnceSupport(sourceProperties));
+
+        sourceProperties.put(FileStreamSourceConnector.FILE_CONFIG, " ");
+        assertEquals(ExactlyOnceSupport.UNSUPPORTED, connector.exactlyOnceSupport(sourceProperties));
+
+        sourceProperties.remove(FileStreamSourceConnector.FILE_CONFIG);
+        assertEquals(ExactlyOnceSupport.UNSUPPORTED, connector.exactlyOnceSupport(sourceProperties));
+    }
+
+    @Test
+    public void testTransactionBoundaryDefinition() {
+        assertEquals(ConnectorTransactionBoundaries.UNSUPPORTED, connector.canDefineTransactionBoundaries(sourceProperties));
     }
 
     @Test

commit 8bb89c4beb6c21558bff6207dfa5f68729ac51bc

MINOR: Fix compilation issue in FileStreamSourceConnector (#12938)
Fix compilation failure introduced in https://github.com/apache/kafka/pull/12355.

Reviewers: Mickael Maison <mickael.maison@gmail.com>

connect/file/src/main/java/org/apache/kafka/connect/file/FileStreamSourceConnector.java
@@ -91,7 +91,7 @@ public class FileStreamSourceConnector extends SourceConnector {
     @Override
     public ExactlyOnceSupport exactlyOnceSupport(Map<String, String> props) {
         AbstractConfig parsedConfig = new AbstractConfig(CONFIG_DEF, props);
-        filename = parsedConfig.getString(FILE_CONFIG);
+        String filename = parsedConfig.getString(FILE_CONFIG);
         // We can provide exactly-once guarantees if reading from a "real" file
         // (as long as the file is only appended to over the lifetime of the connector)
         // If we're reading from stdin, we can't provide exactly-once guarantees