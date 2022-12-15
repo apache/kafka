55942812fbfe694416254d82c36be6592f40e29e
Colin P. McCabe <cmccabe@apache.org>
2022-12-15 13:26:58 -0800

    fix

--- metadata/src/test/java/org/apache/kafka/image/loader/MetadataLoaderTest.java
+++ metadata/src/test/java/org/apache/kafka/image/loader/MetadataLoaderTest.java
@@ -37,7 +37,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Iterator;
