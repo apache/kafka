64b13b74e04b49724d7f7fca6c8ea6dea571075f
bill <bbejeck@gmail.com>
2022-12-15 19:28:12 -0500

    Add license and format fix

--- streams/src/test/java/org/apache/kafka/streams/query/StateQueryResultTest.java
+++ streams/src/test/java/org/apache/kafka/streams/query/StateQueryResultTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.streams.query;
 
 import org.apache.kafka.streams.query.internals.SucceededQueryResult;
@@ -43,6 +59,6 @@ class StateQueryResultTest {
     void getOnlyPartitionResultMultipleResults() {
         stringStateQueryResult.addResult(0, validResult);
         stringStateQueryResult.addResult(1, validResult);
-        assertThrows(IllegalArgumentException.class, () ->stringStateQueryResult.getOnlyPartitionResult());
+        assertThrows(IllegalArgumentException.class, () -> stringStateQueryResult.getOnlyPartitionResult());
     }
 }
