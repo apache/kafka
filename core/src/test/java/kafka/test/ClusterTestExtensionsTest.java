evazca/kafka-tiered

Fix the replica fetcher thread and metadata versions tests.
Summary:
- The latest metadata version IBP_3_3_IV4 was not referred on the existing tests which lead to the failures.
- After the local and remote leader endpoint changes, the fetcher thread tests related to remote storage become stale, fixed them.
- Fixed the issue while renaming the parent directory of the index files.

Test Plan:
./gradlew :core:test
./gradlew :metadata:test

Reviewers: abhijeek, satishd

Revert Plan: git revert

API Changes: NA

Monitoring and Alerts: NA

core/src/test/java/kafka/test/ClusterTestExtensionsTest.java
@@ -117,6 +117,6 @@ public class ClusterTestExtensionsTest {
 
     @ClusterTest
     public void testDefaults(ClusterConfig config) {
-        Assertions.assertEquals(MetadataVersion.IBP_3_3_IV3, config.metadataVersion());
+        Assertions.assertEquals(MetadataVersion.IBP_3_3_IV4, config.metadataVersion());
     }
 }

core/src/test/java/kafka/test/annotation/ClusterTest.java
@@ -41,6 +41,6 @@ public @interface ClusterTest {
     String name() default "";
     SecurityProtocol securityProtocol() default SecurityProtocol.PLAINTEXT;
     String listener() default "";
-    MetadataVersion metadataVersion() default MetadataVersion.IBP_3_3_IV3;
+    MetadataVersion metadataVersion() default MetadataVersion.IBP_3_3_IV4;
     ClusterConfigProperty[] serverProperties() default {};
 }

server-common/src/main/java/org/apache/kafka/server/common/MetadataVersion.java
@@ -274,7 +274,9 @@ public enum MetadataVersion {
     }
 
     public short fetchRequestVersion() {
-        if (this.isAtLeast(IBP_3_1_IV0)) {
+        if (this.isAtLeast(IBP_3_3_IV4)) {
+            return 14;
+        } else if (this.isAtLeast(IBP_3_1_IV0)) {
             return 13;
         } else if (this.isAtLeast(IBP_2_7_IV1)) {
             return 12;
@@ -316,7 +318,9 @@ public enum MetadataVersion {
     }
 
     public short listOffsetRequestVersion() {
-        if (this.isAtLeast(IBP_3_0_IV1)) {
+        if (this.isAtLeast(IBP_3_3_IV4)) {
+            return 8;
+        } else if (this.isAtLeast(IBP_3_0_IV1)) {
             return 7;
         } else if (this.isAtLeast(IBP_2_8_IV0)) {
             return 6;

apache/kafka

core/src/test/java/kafka/test/ClusterTestExtensionsTest.java
@@ -117,6 +117,6 @@ public class ClusterTestExtensionsTest {
 
     @ClusterTest
     public void testDefaults(ClusterConfig config) {
-        Assertions.assertEquals(MetadataVersion.IBP_3_4_IV0, config.metadataVersion());
+        Assertions.assertEquals(MetadataVersion.IBP_3_4_IV1, config.metadataVersion());
     }
 }

core/src/test/java/kafka/test/annotation/ClusterTest.java
@@ -41,6 +41,6 @@ public @interface ClusterTest {
     String name() default "";
     SecurityProtocol securityProtocol() default SecurityProtocol.PLAINTEXT;
     String listener() default "";
-    MetadataVersion metadataVersion() default MetadataVersion.IBP_3_4_IV0;
+    MetadataVersion metadataVersion() default MetadataVersion.IBP_3_4_IV1;
     ClusterConfigProperty[] serverProperties() default {};
 }

server-common/src/main/java/org/apache/kafka/server/common/MetadataVersion.java
@@ -288,7 +288,9 @@ public enum MetadataVersion {
     }
 
     public short fetchRequestVersion() {
-        if (this.isAtLeast(IBP_3_1_IV0)) {
+        if (this.isAtLeast(IBP_3_4_IV1)) {
+            return 14;
+        } else if (this.isAtLeast(IBP_3_1_IV0)) {
             return 13;
         } else if (this.isAtLeast(IBP_2_7_IV1)) {
             return 12;
@@ -330,7 +332,9 @@ public enum MetadataVersion {
     }
 
     public short listOffsetRequestVersion() {
-        if (this.isAtLeast(IBP_3_0_IV1)) {
+        if (this.isAtLeast(IBP_3_4_IV1)) {
+            return 8;
+        } else if (this.isAtLeast(IBP_3_0_IV1)) {
             return 7;
         } else if (this.isAtLeast(IBP_2_8_IV0)) {
             return 6;