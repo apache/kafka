dc792216bd6d4d01b9cc5d7ef54d530ec85a19e0
Michael Marshall <mmarshall@apache.org>
2022-12-21 10:50:46 -0600

    Fix the fix, improve tests

--- clients/src/test/java/org/apache/kafka/common/protocol/DataOutputStreamWritableTest.java
+++ clients/src/test/java/org/apache/kafka/common/protocol/DataOutputStreamWritableTest.java
@@ -26,22 +26,41 @@ import org.junit.jupiter.api.Test;
 public class DataOutputStreamWritableTest {
     @Test
     public void testWritingSlicedByteBuffer() {
-        ByteBuffer expectedBuffer = ByteBuffer.wrap(new byte[]{2, 3});
-        ByteBuffer originalBuffer = ByteBuffer.allocate(4);
-        for (int i = 0; i < originalBuffer.limit(); i++) {
-            originalBuffer.put(i, (byte) i);
-        }
-        // Move position forward to ensure slice is not whole buffer
-        originalBuffer.position(2);
-        ByteBuffer slicedBuffer = originalBuffer.slice();
+        byte[] expectedArray = new byte[]{2, 3, 0, 0};
+        ByteBuffer sourceBuffer = ByteBuffer.wrap(new byte[]{0, 1, 2, 3});
+        ByteBuffer resultBuffer = ByteBuffer.allocate(4);
 
-        ByteBuffer actualBuf = ByteBuffer.allocate(2);
-        Writable writable = new DataOutputStreamWritable(new DataOutputStream(new ByteBufferOutputStream(actualBuf)));
+        // Move position forward to ensure slice is not whole buffer
+        sourceBuffer.position(2);
+        ByteBuffer slicedBuffer = sourceBuffer.slice();
+
+        Writable writable = new DataOutputStreamWritable(
+                new DataOutputStream(new ByteBufferOutputStream(resultBuffer)));
 
         writable.writeByteBuffer(slicedBuffer);
 
-        assertEquals(2, actualBuf.position(), "Writing to the buffer moves the position forward");
-        assertEquals(expectedBuffer, actualBuf.position(0),
-                "Actual buffer should have expected elements");
+        assertEquals(2, resultBuffer.position(), "Writing to the buffer moves the position forward");
+        assertEquals(expectedArray, resultBuffer.array(), "Result buffer should have expected elements");
+    }
+
+    @Test
+    public void testWritingSlicedByteBufferWithNonZeroPosition() {
+        byte[] expectedArray = new byte[]{3, 0, 0, 0};
+        ByteBuffer originalBuffer = ByteBuffer.wrap(new byte[]{0, 1, 2, 3});
+        ByteBuffer resultBuffer = ByteBuffer.allocate(4);
+
+        // Move position forward to ensure slice is backed by heap buffer with non-zero offset
+        originalBuffer.position(2);
+        ByteBuffer slicedBuffer = originalBuffer.slice();
+        // Move the slice's position forward to ensure the writer starts reading at that position
+        slicedBuffer.position(1);
+
+        Writable writable = new DataOutputStreamWritable(
+                new DataOutputStream(new ByteBufferOutputStream(resultBuffer)));
+
+        writable.writeByteBuffer(slicedBuffer);
+
+        assertEquals(1, resultBuffer.position(), "Writing to the buffer moves the position forward");
+        assertEquals(expectedArray, resultBuffer.array(), "Result buffer should have expected elements");
     }
 }
