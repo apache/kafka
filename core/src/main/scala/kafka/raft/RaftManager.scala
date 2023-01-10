67c85081d60cf4fa3e2188f8111066f58abb7b78
José Armando García Sancio <jsancio@users.noreply.github.com>
2023-01-10 10:18:40 -0800

    KAFKA-14557; Lock metadata log dir (#13058)
    This change makes sure that Kafka grabs a log dir lock in the following additional cases:
    
    1. When a Kafka node runs in controller only. The current implementation doesn't grab a file lock because the LogManager is never instantiated.
    2. When the metadata log dir is different from the log dir(s). The current implementation of LogManager doesn't load or grab a lock on the metadata dir.
    
    Reviewers: Ron Dagostino <rdagostino@confluent.io> , dengziming <dengziming1993@gmail.com>

--- core/src/main/scala/kafka/raft/RaftManager.scala
+++ core/src/main/scala/kafka/raft/RaftManager.scala
@@ -18,15 +18,25 @@ package kafka.raft
 
 import java.io.File
 import java.nio.file.Files
+import java.nio.file.Paths
 import java.util
 import java.util.OptionalInt
 import java.util.concurrent.CompletableFuture
+import kafka.log.LogManager
 import kafka.log.UnifiedLog
 import kafka.raft.KafkaRaftManager.RaftIoThread
+import kafka.server.KafkaRaftServer.ControllerRole
 import kafka.server.{KafkaConfig, MetaProperties}
+import kafka.utils.CoreUtils
+import kafka.utils.FileLock
+import kafka.utils.KafkaScheduler
+import kafka.utils.Logging
+import kafka.utils.ShutdownableThread
 import kafka.utils.timer.SystemTimer
-import kafka.utils.{KafkaScheduler, Logging, ShutdownableThread}
 import org.apache.kafka.clients.{ApiVersions, ManualMetadataUpdater, NetworkClient}
+import org.apache.kafka.common.KafkaException
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.Uuid
 import org.apache.kafka.common.metrics.Metrics
 import org.apache.kafka.common.network.{ChannelBuilders, ListenerName, NetworkReceive, Selectable, Selector}
 import org.apache.kafka.common.protocol.ApiMessage
@@ -34,7 +44,6 @@ import org.apache.kafka.common.requests.RequestHeader
 import org.apache.kafka.common.security.JaasContext
 import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.utils.{LogContext, Time}
-import org.apache.kafka.common.{TopicPartition, Uuid}
 import org.apache.kafka.raft.RaftConfig.{AddressSpec, InetAddressSpec, NON_ROUTABLE_ADDRESS, UnknownAddressSpec}
 import org.apache.kafka.raft.{FileBasedStateStore, KafkaRaftClient, LeaderAndEpoch, RaftClient, RaftConfig, RaftRequest, ReplicatedLog}
 import org.apache.kafka.server.common.serialization.RecordSerde
@@ -78,6 +87,19 @@ object KafkaRaftManager {
     Files.createDirectories(dir.toPath)
     dir
   }
+
+  private def lockDataDir(dataDir: File): FileLock = {
+    val lock = new FileLock(new File(dataDir, LogManager.LockFileName))
+
+    if (!lock.tryLock()) {
+      throw new KafkaException(
+        s"Failed to acquire lock on file .lock in ${lock.file.getParent}. A Kafka instance in another process or " +
+        "thread is using this directory."
+      )
+    }
+
+    lock
+  }
 }
 
 trait RaftManager[T] {
@@ -120,6 +142,23 @@ class KafkaRaftManager[T](
   scheduler.startup()
 
   private val dataDir = createDataDir()
+
+  private val dataDirLock = {
+    // Aquire the log dir lock if the metadata log dir is different from the log dirs
+    val differentMetadataLogDir = !config
+      .logDirs
+      .map(Paths.get(_).toAbsolutePath)
+      .contains(Paths.get(config.metadataLogDir).toAbsolutePath)
+    // Or this node is only a controller
+    val isOnlyController = config.processRoles == Set(ControllerRole)
+
+    if (differentMetadataLogDir || isOnlyController) {
+      Some(KafkaRaftManager.lockDataDir(new File(config.metadataLogDir)))
+    } else {
+      None
+    }
+  }
+
   override val replicatedLog: ReplicatedLog = buildMetadataLog()
   private val netChannel = buildNetworkChannel()
   private val expirationTimer = new SystemTimer("raft-expiration-executor")
@@ -147,13 +186,14 @@ class KafkaRaftManager[T](
   }
 
   def shutdown(): Unit = {
-    expirationService.shutdown()
-    expirationTimer.shutdown()
-    raftIoThread.shutdown()
-    client.close()
-    scheduler.shutdown()
-    netChannel.close()
-    replicatedLog.close()
+    CoreUtils.swallow(expirationService.shutdown(), this)
+    CoreUtils.swallow(expirationTimer.shutdown(), this)
+    CoreUtils.swallow(raftIoThread.shutdown(), this)
+    CoreUtils.swallow(client.close(), this)
+    CoreUtils.swallow(scheduler.shutdown(), this)
+    CoreUtils.swallow(netChannel.close(), this)
+    CoreUtils.swallow(replicatedLog.close(), this)
+    CoreUtils.swallow(dataDirLock.foreach(_.destroy()), this)
   }
 
   override def register(
