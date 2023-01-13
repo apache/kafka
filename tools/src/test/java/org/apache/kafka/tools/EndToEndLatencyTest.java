23dea5516ff1bc9dbb909a5c481c59060d8e7159
Sagar Rao <sagarrao@Sagars-MacBook-Pro.local>
2023-01-13 16:56:38 +0530

    Fixing checkstyle issues in tests

--- tools/src/test/java/org/apache/kafka/tools/EndToEndLatencyTest.java
+++ tools/src/test/java/org/apache/kafka/tools/EndToEndLatencyTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.tools;
 
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -30,12 +46,12 @@ public class EndToEndLatencyTest {
     @Test
     public void testUnexpectedArgs() {
         String[] args = new String[] {
-                "-b", "localhost:9092",
-                "-t", "test",
-                "-n", "10000",
-                "-a", "1",
-                "-s", "200",
-                "-e", "123"};
+            "-b", "localhost:9092",
+            "-t", "test",
+            "-n", "10000",
+            "-a", "1",
+            "-s", "200",
+            "-e", "123"};
         ArgumentParser parser = EndToEndLatency.addArguments();
         ArgumentParserException thrown = assertThrows(ArgumentParserException.class, () -> parser.parseArgs(args));
         assertEquals("unrecognized arguments: '-e'", thrown.getMessage());
@@ -44,11 +60,11 @@ public class EndToEndLatencyTest {
     @Test
     public void shouldFailWhenProducerAcksAreNotSynchronised() throws Exception {
         String[] args = new String[] {
-                "-b", "localhost:9092",
-                "-t", "test",
-                "-n", "10000",
-                "-a", "0",
-                "-s", "200"};
+            "-b", "localhost:9092",
+            "-t", "test",
+            "-n", "10000",
+            "-a", "0",
+            "-s", "200"};
 
         assertThrows(IllegalArgumentException.class, () -> EndToEndLatency.execute(args));
     }
