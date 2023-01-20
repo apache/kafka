7de68a42b41c20cdccefd77ae4d71a2938adf068
Kirk True <kirk@kirktrue.pro>
2023-01-20 12:04:06 -0800

    Added comment for Sender.completeBatch

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
@@ -647,6 +647,8 @@ public class Sender implements Runnable {
                 // thus it is not safe to reassign the sequence.
                 failBatch(batch, response, batch.attempts() < this.retries);
             }
+            // NetworkException is a subclass of InvalidMetadataException, but TimeoutException is not, so we
+            // we need to check for timeouts explicitly.
             if (error.exception() instanceof InvalidMetadataException || error.exception() instanceof TimeoutException) {
                 if (error.exception() instanceof UnknownTopicOrPartitionException) {
                     log.warn("Received unknown topic or partition error in produce request on partition {}. The " +
