ceaf3bde510973421d586c49ebe51823e4894e44
Colin P. McCabe <cmccabe@apache.org>
2023-01-27 15:00:34 -0800

    fix checkstyle

--- server-common/src/main/java/org/apache/kafka/server/util/FutureUtils.java
+++ server-common/src/main/java/org/apache/kafka/server/util/FutureUtils.java
@@ -19,7 +19,6 @@ package org.apache.kafka.server.util;
 import org.apache.kafka.common.utils.Time;
 import org.slf4j.Logger;
 
-import java.math.BigInteger;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
