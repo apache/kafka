0360577ccccb9103c43162d6b89afccd92adb2c1
Manikumar Reddy <manikumar.reddy@gmail.com>
2023-02-06 17:59:22 +0530

    MINOR: Add FeatureZNode ZK node path to ZK root paths

--- core/src/main/scala/kafka/zk/ZkData.scala
+++ core/src/main/scala/kafka/zk/ZkData.scala
@@ -1032,7 +1032,8 @@ object ZkData {
     ProducerIdBlockZNode.path,
     LogDirEventNotificationZNode.path,
     DelegationTokenAuthZNode.path,
-    ExtendedAclZNode.path) ++ ZkAclStore.securePaths
+    ExtendedAclZNode.path,
+    FeatureZNode.path) ++ ZkAclStore.securePaths
 
   // These are persistent ZK paths that should exist on kafka broker startup.
   val PersistentZkPaths = Seq(
