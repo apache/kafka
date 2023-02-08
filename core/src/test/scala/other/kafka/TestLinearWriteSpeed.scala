evazca/kafka-tiered

Moved ProducerStateManagerConfig to storage module.

core/src/test/scala/other/kafka/TestLinearWriteSpeed.scala
@@ -29,7 +29,7 @@ import kafka.utils._
 import org.apache.kafka.common.config.TopicConfig
 import org.apache.kafka.common.record._
 import org.apache.kafka.common.utils.{Time, Utils}
-import org.apache.kafka.server.log.internals.{LogConfig, LogDirFailureChannel}
+import org.apache.kafka.server.log.internals.{LogConfig, LogDirFailureChannel, ProducerStateManagerConfig}
 
 import scala.math._
 

core/src/test/scala/unit/kafka/log/ProducerStateManagerTest.scala
@@ -29,7 +29,7 @@ import org.apache.kafka.common.errors._
 import org.apache.kafka.common.internals.Topic
 import org.apache.kafka.common.record._
 import org.apache.kafka.common.utils.{MockTime, Utils}
-import org.apache.kafka.server.log.internals.{AppendOrigin, CompletedTxn, LogOffsetMetadata, ProducerAppendInfo, ProducerStateEntry, TxnMetadata}
+import org.apache.kafka.server.log.internals.{AppendOrigin, CompletedTxn, LogOffsetMetadata, ProducerAppendInfo, ProducerStateEntry, ProducerStateManagerConfig, TxnMetadata}
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.{AfterEach, BeforeEach, Test}
 import org.mockito.Mockito.{mock, when}

storage/src/main/java/org/apache/kafka/server/log/internals/ProducerStateManagerConfig.java
@@ -1,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.server.log.internals;
+
+import java.util.Collections;
+import java.util.Set;
+
+//todo-refactoring Add respective config changes once https://issues.apache.org/jira/browse/KAFKA-14478 is completed
+public class ProducerStateManagerConfig { // extends BrokerReconfigurable
+    //todo-refactoring replace later with KafkaConfig.ProducerIdExpirationMsProp
+    public static final Set<String> RECONFIGURABLE_CONFIGS = Collections.singleton("producer.id.expiration.ms");
+    private volatile int producerIdExpirationMs;
+
+    public ProducerStateManagerConfig(int producerIdExpirationMs) {
+        this.producerIdExpirationMs = producerIdExpirationMs;
+    }
+
+    public static Set<String> reconfigurableConfigs() {
+        //Set(KafkaConfig.ProducerIdExpirationMsProp)
+        return null;
+    }
+
+    // todo-refactoring
+//    public void reconfigure(KafkaConfig oldConfig, KafkaConfig newConfig) {
+//        if (producerIdExpirationMs != newConfig.producerIdExpirationMs) {
+//            info("Reconfigure ${KafkaConfig.ProducerIdExpirationMsProp} from $producerIdExpirationMs to ${newConfig.producerIdExpirationMs}");
+//            producerIdExpirationMs = newConfig.producerIdExpirationMs;
+//        }
+//    }
+
+//    public void validateReconfiguration(KafkaConfig newConfig) {
+//        if (newConfig.producerIdExpirationMs < 0)
+//            throw new ConfigException(s"${KafkaConfig.ProducerIdExpirationMsProp} cannot be less than 0, current value is $producerIdExpirationMs")
+//    }
+
+    public int producerIdExpirationMs() {
+        return producerIdExpirationMs;
+    }
+}

apache/kafka

core/src/test/scala/other/kafka/TestLinearWriteSpeed.scala
@@ -31,7 +31,7 @@ import org.apache.kafka.common.record._
 import org.apache.kafka.common.utils.{Time, Utils}
 import org.apache.kafka.server.util.{KafkaScheduler, Scheduler}
 import org.apache.kafka.server.util.CommandLineUtils
-import org.apache.kafka.storage.internals.log.{LogConfig, LogDirFailureChannel}
+import org.apache.kafka.storage.internals.log.{LogConfig, LogDirFailureChannel, ProducerStateManagerConfig}
 
 import scala.math._
 

core/src/test/scala/unit/kafka/log/ProducerStateManagerTest.scala
@@ -29,7 +29,7 @@ import org.apache.kafka.common.errors._
 import org.apache.kafka.common.internals.Topic
 import org.apache.kafka.common.record._
 import org.apache.kafka.common.utils.{MockTime, Utils}
-import org.apache.kafka.storage.internals.log.{AppendOrigin, CompletedTxn, LogOffsetMetadata, ProducerAppendInfo, ProducerStateEntry, TxnMetadata}
+import org.apache.kafka.storage.internals.log.{AppendOrigin, CompletedTxn, LogOffsetMetadata, ProducerAppendInfo, ProducerStateEntry, ProducerStateManagerConfig, TxnMetadata}
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.{AfterEach, BeforeEach, Test}
 import org.mockito.Mockito.{mock, when}

storage/src/main/java/org/apache/kafka/storage/internals/log/ProducerStateManagerConfig.java
@@ -1,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.storage.internals.log;
+
+import java.util.Collections;
+import java.util.Set;
+
+//todo-refactoring Add respective config changes once https://issues.apache.org/jira/browse/KAFKA-14478 is completed
+public class ProducerStateManagerConfig { // extends BrokerReconfigurable
+    //todo-refactoring replace later with KafkaConfig.ProducerIdExpirationMsProp
+    public static final Set<String> RECONFIGURABLE_CONFIGS = Collections.singleton("producer.id.expiration.ms");
+    private volatile int producerIdExpirationMs;
+
+    public ProducerStateManagerConfig(int producerIdExpirationMs) {
+        this.producerIdExpirationMs = producerIdExpirationMs;
+    }
+
+    public static Set<String> reconfigurableConfigs() {
+        //Set(KafkaConfig.ProducerIdExpirationMsProp)
+        return null;
+    }
+
+    // todo-refactoring
+//    public void reconfigure(KafkaConfig oldConfig, KafkaConfig newConfig) {
+//        if (producerIdExpirationMs != newConfig.producerIdExpirationMs) {
+//            info("Reconfigure ${KafkaConfig.ProducerIdExpirationMsProp} from $producerIdExpirationMs to ${newConfig.producerIdExpirationMs}");
+//            producerIdExpirationMs = newConfig.producerIdExpirationMs;
+//        }
+//    }
+
+//    public void validateReconfiguration(KafkaConfig newConfig) {
+//        if (newConfig.producerIdExpirationMs < 0)
+//            throw new ConfigException(s"${KafkaConfig.ProducerIdExpirationMsProp} cannot be less than 0, current value is $producerIdExpirationMs")
+//    }
+
+    public int producerIdExpirationMs() {
+        return producerIdExpirationMs;
+    }
+}