ec8ab9ce6b31ca3d12d86ee30c589990450296fd
David Mariassy <david.mariassy@shopify.com>
2023-02-09 14:15:01 -0500

    Resolve conflicts and errors

--- core/src/main/scala/kafka/server/KafkaConfig.scala
+++ core/src/main/scala/kafka/server/KafkaConfig.scala
@@ -48,11 +48,10 @@ import org.apache.kafka.server.common.{MetadataVersion, MetadataVersionValidator
 import org.apache.kafka.server.common.MetadataVersion._
 import org.apache.kafka.server.config.ServerTopicConfigSynonyms
 import org.apache.kafka.server.interceptors.ProduceRequestInterceptor
-import org.apache.kafka.server.log.internals.LogConfig
-import org.apache.kafka.server.log.internals.LogConfig.MessageFormatVersion
 import org.apache.kafka.server.log.remote.storage.RemoteLogManagerConfig
 import org.apache.kafka.server.record.BrokerCompressionType
 import org.apache.kafka.storage.internals.log.LogConfig
+import org.apache.kafka.storage.internals.log.LogConfig.MessageFormatVersion
 import org.apache.zookeeper.client.ZKClientConfig
 
 import scala.annotation.nowarn
--- core/src/test/scala/integration/kafka/api/ProducerSendTestHelpers.scala
+++ core/src/test/scala/integration/kafka/api/ProducerSendTestHelpers.scala
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package integration.kafka.api
 
 import kafka.integration.KafkaServerTestHarness
@@ -196,7 +213,10 @@ trait ProducerSendTestHelpers extends KafkaServerTestHarness {
 
       // make sure all of them end up in the same partition with increasing offset values
       for ((recordMetadata, offset) <- futures zip (0 until numRecords)) {
-        assertEquals(offset.toLong, recordMetadata.offset)
+        if (expectedRecords == numRecords) {
+          // This assertion only holds if we don't filter the records with an interceptor
+          assertEquals(offset.toLong, recordMetadata.offset)
+        }
         assertEquals(topic, recordMetadata.topic)
         assertEquals(partition, recordMetadata.partition)
       }
