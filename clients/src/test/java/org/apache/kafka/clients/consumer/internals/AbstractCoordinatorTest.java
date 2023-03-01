f7f376f6c162717e60e143b05fbd12ea2f347e3c
Philip Nee <pnee@confluent.io>
2023-02-28 17:36:37 -0800

    KAFKA-12639: Exit upon expired timer to prevent tight looping (#13190)
    In AbstractCoordinator#joinGroupIfNeeded - joinGroup request will be retried without proper backoff, due to the expired timer. This is an uncommon scenario and possibly only appears during the testing, but I think it makes sense to enforce the client to drive the join group via poll.
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinatorTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/AbstractCoordinatorTest.java
@@ -1561,6 +1561,45 @@ public class AbstractCoordinatorTest {
         }
     }
 
+    @Test
+    public void testBackoffAndRetryUponRetriableError() {
+        this.mockTime = new MockTime();
+        long currentTimeMs = System.currentTimeMillis();
+        this.mockTime.setCurrentTimeMs(System.currentTimeMillis());
+
+        setupCoordinator(); // note: uses 100ms backoff
+        mockClient.prepareResponse(groupCoordinatorResponse(node, Errors.NONE));
+        coordinator.ensureCoordinatorReady(mockTime.timer(0));
+
+        // Retriable Exception
+        mockClient.prepareResponse(joinGroupResponse(Errors.COORDINATOR_LOAD_IN_PROGRESS));
+        mockClient.prepareResponse(joinGroupResponse(Errors.NONE)); // Retry w/o error
+        mockClient.prepareResponse(syncGroupResponse(Errors.NONE));
+        coordinator.joinGroupIfNeeded(mockTime.timer(REQUEST_TIMEOUT_MS));
+
+        assertEquals(100, mockTime.milliseconds() - currentTimeMs, 1);
+    }
+
+    @Test
+    public void testReturnUponRetriableErrorAndExpiredTimer() throws InterruptedException {
+        setupCoordinator();
+        mockClient.prepareResponse(groupCoordinatorResponse(node, Errors.NONE));
+        coordinator.ensureCoordinatorReady(mockTime.timer(0));
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Timer t = mockTime.timer(500);
+        try {
+            Future<Boolean> attempt = executor.submit(() -> coordinator.joinGroupIfNeeded(t));
+            mockTime.sleep(500);
+            mockClient.prepareResponse(joinGroupResponse(Errors.COORDINATOR_LOAD_IN_PROGRESS));
+            assertFalse(attempt.get());
+        } catch (Exception e) {
+            fail();
+        } finally {
+            executor.shutdownNow();
+            executor.awaitTermination(1000, TimeUnit.MILLISECONDS);
+        }
+    }
+
     private AtomicBoolean prepareFirstHeartbeat() {
         final AtomicBoolean heartbeatReceived = new AtomicBoolean(false);
         mockClient.prepareResponse(body -> {
