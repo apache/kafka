42ce67413882c246e3f2223cb086ef18c1a48021
Sagar Rao <sagarrao@Sagars-MacBook-Pro.local>
2023-03-26 08:15:22 +0530

    Logging ignored status update

--- connect/runtime/src/main/java/org/apache/kafka/connect/storage/KafkaStatusBackingStore.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/storage/KafkaStatusBackingStore.java
@@ -588,13 +588,10 @@ public class KafkaStatusBackingStore extends KafkaTopicBasedBackingStore impleme
         synchronized (this) {
             log.trace("Received task {} status update {}", id, status);
             CacheEntry<TaskStatus> entry = getOrAdd(id);
-            // We add the status only if it's safe to do so. This is applicable
-            // when there are race conditions during frequent rebalances leading to
-            // the RUNNING status record with the latest generation followed by a stale
-            // UNASSIGNED status record belonging to the same or a previous generation
-            // from a worker which couldn't read the RUNNING record of the latest generation.
             if (entry.canWriteSafely(status)) {
                 entry.put(status);
+            } else {
+                log.trace("Ignoring stale status update {} for task {}", status, id);
             }
         }
     }
