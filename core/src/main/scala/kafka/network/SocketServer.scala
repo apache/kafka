255bf5a1d37ad404c9fe8ffd429c1331b9f60b6d
Chia-Ping Tsai <chia7712@gmail.com>
2023-03-29 22:08:00 +0800

    KAFKA-14774 the removed listeners should not be reconfigurable (#13472)
    Reviewers: Luke Chen <showuon@gmail.com>

--- core/src/main/scala/kafka/network/SocketServer.scala
+++ core/src/main/scala/kafka/network/SocketServer.scala
@@ -323,6 +323,7 @@ class SocketServer(val config: KafkaConfig,
       dataPlaneAcceptors.asScala.remove(endpoint).foreach { acceptor =>
         acceptor.beginShutdown()
         acceptor.close()
+        config.removeReconfigurable(acceptor)
       }
     }
   }
