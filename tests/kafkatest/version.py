a8f3172b9708794b34f48ac924fed2264d40fa07
Matthias J. Sax <matthias@confluent.io>
2023-05-02 17:11:44 -0700

    fix dev version

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -119,7 +119,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("3.5.0-SNAPSHOT")
+DEV_VERSION = KafkaVersion("3.6.0-SNAPSHOT")
 
 LATEST_METADATA_VERSION = "3.3"
 
@@ -249,7 +249,3 @@ LATEST_3_4 = V_3_4_0
 # 3.5.x versions
 V_3_5_0 = KafkaVersion("3.5.0")
 LATEST_3_5 = V_3_5_0
-
-# 3.6.x versions
-V_3_6_0 = KafkaVersion("3.6.0")
-LATEST_3_6 = V_3_6_0
