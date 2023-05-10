da7f6d3858677e9283be0bb26928f02a26705848
dengshuai <shuai.deng@dji.com>
2023-05-10 13:43:04 +0800

    SocketServer 注释:KafkaRequestHandler代码流程

--- core/src/main/scala/kafka/server/KafkaRequestHandler.scala
+++ core/src/main/scala/kafka/server/KafkaRequestHandler.scala
@@ -37,20 +37,41 @@ trait ApiRequestHandler {
 
 /**
  * A thread that answers kafka requests.
+ * comment by shuai 20230510
+ * 请求处理线程类。每个请求处理线程实例，负责从 SocketServer 的 RequestChannel 的请求队列中获取请求对象，并进行处理。
+ * @param id 请求处理线程的序号，类似于 Processor 线程的 ID 序号，仅仅用于标识这是线程池中的第几个线程。
+ * @param brokerId Broker 序号，用于标识这是哪个 Broker 上的请求处理线程。
+ * @param aggregateIdleMeter
+ * @param totalHandlerThreads
+ * @param requestChannel SocketServer 中的请求通道对象。KafkaRequestHandler 对象为什么要定义这个字段呢？我们说过，它是负责处理请求的类，
+ *                       请求恰恰是保存在 RequestChannel 中的请求队列中，因此，Kafka 在构造 KafkaRequestHandler 实例时，
+ *                       必须关联 SocketServer 组件中的 RequestChannel 实例，也就是说，要让 I/O 线程能够找到请求被保存的地方。
+ * @param apis 这是一个 KafkaApis 类。如果说 KafkaRequestHandler 是真正处理请求的，那么，KafkaApis 类就是真正执行请求处理逻辑的地方。
+ *             它有个 handle 方法，用于执行请求处理逻辑。
+ * @param time
  */
-class KafkaRequestHandler(id: Int,
-                          brokerId: Int,
+class KafkaRequestHandler(id: Int, // id: I/O线程序号；请求处理线程的序号，类似于 Processor 线程的 ID 序号，仅仅用于标识这是线程池中的第几个线程。
+                          brokerId: Int, // brokerId：所在Broker序号，即broker.id值；用于标识这是哪个 Broker 上的请求处理线程。
                           val aggregateIdleMeter: Meter,
-                          val totalHandlerThreads: AtomicInteger,
-                          val requestChannel: RequestChannel,
-                          apis: ApiRequestHandler,
+                          val totalHandlerThreads: AtomicInteger,// totalHandlerThreads：I/O线程池大小
+                          val requestChannel: RequestChannel,// requestChannel：请求处理通道
+                          apis: ApiRequestHandler,// apis：KafkaApis类，用于真正实现请求处理逻辑的类
                           time: Time) extends Runnable with Logging {
   this.logIdent = s"[Kafka Request Handler $id on Broker $brokerId], "
   private val shutdownComplete = new CountDownLatch(1)
   private val requestLocal = RequestLocal.withThreadConfinedCaching
   @volatile private var stopped = false
 
+  // KafkaRequestHandler 是一个线程类，那么，除去常规的 close、stop、initiateShutdown 和 awaitShutdown 方法，
+  // 最重要的当属 run 方法实现了
+  /**
+   * <img width="445" height="384" src="https://static001.geekbang.org/resource/image/b5/4d/b5f6d3b4ecea86a3e66a29953034dc4d.jpg" alt="">
+   * 第 1 步是从请求队列中获取下一个待处理的请求，同时更新一些相关的统计指标。如果本次循环没取到，那么本轮循环结束，进入到下一轮。
+   * 如果是 ShutdownRequest 请求，则说明该 Broker 发起了关闭操作。
+   **/
   def run(): Unit = {
+    // 只要该线程尚未关闭，循环运行处理逻辑
+    //它的所有执行逻辑都在 while 循环之下，因此，只要标志线程关闭状态的 stopped 为 false，run 方法将一直循环执行 while 下的语句。
     while (!stopped) {
       // We use a single meter for aggregate idle percentage for the thread pool.
       // Since meter is calculated as total_recorded_value / time_window and
@@ -58,28 +79,41 @@ class KafkaRequestHandler(id: Int,
       // time should be discounted by # threads.
       val startSelectTime = time.nanoseconds
 
+      // 从请求队列中获取下一个待处理的请求
       val req = requestChannel.receiveRequest(300)
       val endTime = time.nanoseconds
+      // 统计线程空闲时间
       val idleTime = endTime - startSelectTime
+      // 更新线程空闲百分比指标
       aggregateIdleMeter.mark(idleTime / totalHandlerThreads.get)
 
       req match {
+        // 关闭线程请求
         case RequestChannel.ShutdownRequest =>
           debug(s"Kafka request handler $id on broker $brokerId received shut down command")
+          // 关闭线程
+          // 而 Broker 关闭时会调用 KafkaRequestHandler 的 shutdown 方法，进而调用 initiateShutdown 方法，以及 RequestChannel 的 sendShutdownRequest 方法，而后者就是将 ShutdownRequest 写入到请求队列。
+          // 一旦从请求队列中获取到 ShutdownRequest，run 方法代码会调用 shutdownComplete 的 countDown 方法，正式完成对 KafkaRequestHandler 线程的关闭操作。
           completeShutdown()
           return
 
+        // 普通请求
         case request: RequestChannel.Request =>
           try {
             request.requestDequeueTimeNanos = endTime
             trace(s"Kafka request handler $id on broker $brokerId handling request $request")
+            // 由KafkaApis.handle方法执行相应处理逻辑
             apis.handle(request, requestLocal)
           } catch {
+            // 如果出现严重错误，立即关闭线程
             case e: FatalExitError =>
               completeShutdown()
               Exit.exit(e.statusCode)
+
+            // 如果是普通异常，记录错误日志
             case e: Throwable => error("Exception when handling request", e)
           } finally {
+            // 释放请求对象占用的内存缓冲区资源
             request.releaseBuffer()
           }
 
@@ -106,6 +140,7 @@ class KafkaRequestHandler(id: Int,
 
 // KafkaRequestHandlerPool 组件就是我们常说的 I/O 线程池，里面定义了若干个 I/O 线程，用于执行真实的请求处理逻辑。
 // KafkaRequestHandlerPool 线程池定义了多个 KafkaRequestHandler 线程，而 KafkaRequestHandler 线程是真正处理请求逻辑的地方。
+// 请求处理线程池，负责创建、维护、管理和销毁下辖的请求处理线程。
 class KafkaRequestHandlerPool(val brokerId: Int,
                               val requestChannel: RequestChannel,
                               val apis: ApiRequestHandler,
@@ -145,6 +180,13 @@ class KafkaRequestHandlerPool(val brokerId: Int,
     threadPoolSize.set(newSize)
   }
 
+  /**
+   * 而 Broker 关闭时会调用 KafkaRequestHandler 的 shutdown 方法，
+   * 进而调用 initiateShutdown 方法，以及 RequestChannel 的 sendShutdownRequest 方法，
+   * 而后者就是将 ShutdownRequest 写入到请求队列。
+   *
+   * 一旦从请求队列中获取到 ShutdownRequest，run 方法代码会调用 shutdownComplete 的 countDown 方法，正式完成对 KafkaRequestHandler 线程的关闭操作。
+   */
   def shutdown(): Unit = synchronized {
     info("shutting down")
     for (handler <- runnables)
@@ -155,6 +197,7 @@ class KafkaRequestHandlerPool(val brokerId: Int,
   }
 }
 
+//Broker 端与主题相关的监控指标的管理类。
 class BrokerTopicMetrics(name: Option[String]) {
   private val metricsGroup = new KafkaMetricsGroup(this.getClass)
 
@@ -273,6 +316,7 @@ class BrokerTopicMetrics(name: Option[String]) {
   def close(): Unit = metricTypeMap.values.foreach(_.close())
 }
 
+// 定义 Broker 端与主题相关的监控指标的管理操作。
 object BrokerTopicStats {
   val MessagesInPerSec = "MessagesInPerSec"
   val BytesInPerSec = "BytesInPerSec"
@@ -298,6 +342,8 @@ object BrokerTopicStats {
   private val valueFactory = (k: String) => new BrokerTopicMetrics(Some(k))
 }
 
+// BrokerTopicStats 的伴生对象类，定义 Broker 端与主题相关的监控指标，
+// 比如常见的 MessagesInPerSec 和 MessagesOutPerSec 等。
 class BrokerTopicStats extends Logging {
   import BrokerTopicStats._
 
