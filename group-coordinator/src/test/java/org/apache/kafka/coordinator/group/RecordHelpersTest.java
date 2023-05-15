ee662af9f26dc1d148e5e6e522e7b9d3621cbd6c
Jeff Kim <kimkb2011@gmail.com>
2023-05-15 11:21:14 -0400

    checkstyle

--- group-coordinator/src/test/java/org/apache/kafka/coordinator/group/RecordHelpersTest.java
+++ group-coordinator/src/test/java/org/apache/kafka/coordinator/group/RecordHelpersTest.java
@@ -54,7 +54,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
