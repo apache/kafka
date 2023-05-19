d1ada7a6e578ca2778c8f448da1b9a755166a719
Matthias J. Sax <matthias@confluent.io>
2023-05-19 15:32:37 -0700

    fix checkstyle

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamImplJoin.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamImplJoin.java
@@ -44,7 +44,6 @@ import org.apache.kafka.streams.state.internals.LeftOrRightValueSerde;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
