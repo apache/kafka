cf91d4eeefe9e92aef58a53704eb0c8da6649343
Alexandre Dupriez <duprie@amazon.com>
2023-05-24 15:51:23 +0100

    Simplify server.OffsetCommitRequestTest by using DTO directly.

--- core/src/test/scala/unit/kafka/server/OffsetCommitRequestTest.scala
+++ core/src/test/scala/unit/kafka/server/OffsetCommitRequestTest.scala
@@ -17,12 +17,14 @@
 
 package unit.kafka.server
 
-import kafka.server.KafkaApisTest.{NameAndId, newOffsetCommitRequestData, newOffsetCommitResponseData}
 import kafka.server.{BaseRequestTest, KafkaConfig}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, OffsetAndMetadata}
 import org.apache.kafka.common.protocol.{ApiKeys, Errors}
 import org.apache.kafka.common.requests.OffsetCommitRequestTest.assertResponseEquals
 import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.message.{OffsetCommitRequestData, OffsetCommitResponseData}
+import org.apache.kafka.common.message.OffsetCommitRequestData.{OffsetCommitRequestPartition, OffsetCommitRequestTopic}
+import org.apache.kafka.common.message.OffsetCommitResponseData.{OffsetCommitResponsePartition, OffsetCommitResponseTopic}
 import org.apache.kafka.common.requests.{OffsetCommitRequest, OffsetCommitResponse}
 import org.apache.kafka.common.utils.Utils
 import org.apache.kafka.common.utils.annotation.ApiKeyVersionsSource
@@ -31,8 +33,7 @@ import org.junit.jupiter.params.ParameterizedTest
 
 import java.util.Optional.empty
 import java.util.Properties
-import scala.collection.{Map, Seq}
-import scala.collection.immutable.ListMap
+import scala.collection.Seq
 import scala.jdk.CollectionConverters.{MapHasAsJava, SeqHasAsJava}
 
 class OffsetCommitRequestTest extends BaseRequestTest {
@@ -70,13 +71,52 @@ class OffsetCommitRequestTest extends BaseRequestTest {
     topicNames.foreach(createTopic(_))
     consumer.subscribe(topicNames.asJava)
 
-    val topics = getTopicIds(topicNames.toSeq)
+    val topicIds = getTopicIds(topicNames.toSeq)
 
-    sendOffsetCommitRequest(
-      topics.map { case(name, id) => (NameAndId(name, id), ListMap(0 -> offset)) }.toSeq,
-      topics.map { case(name, id) => (NameAndId(name, id), Map(0 -> Errors.NONE)) }.toSeq,
-      version
+    val requestData = new OffsetCommitRequestData()
+      .setGroupId(groupId)
+      .setTopics(List(
+        new OffsetCommitRequestTopic()
+          .setTopicId(topicIds("topic1"))
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava),
+        new OffsetCommitRequestTopic()
+          .setTopicId(topicIds("topic2"))
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava),
+        new OffsetCommitRequestTopic()
+          .setTopicId(topicIds("topic3"))
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava)
+      ).asJava)
+
+    val responseData = new OffsetCommitResponseData()
+      .setTopics(List(
+        new OffsetCommitResponseTopic()
+          .setTopicId(topicIds("topic1"))
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.NONE.code)).asJava),
+        new OffsetCommitResponseTopic()
+          .setTopicId(topicIds("topic2"))
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.NONE.code)).asJava),
+        new OffsetCommitResponseTopic()
+          .setTopicId(topicIds("topic3"))
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.NONE.code)).asJava),
+      ).asJava)
+
+    val response = connectAndReceive[OffsetCommitResponse](
+      new OffsetCommitRequest.Builder(requestData, true).build(version)
     )
+
+    assertResponseEquals(new OffsetCommitResponse(responseData), response)
   }
 
   @Test
@@ -94,17 +134,54 @@ class OffsetCommitRequestTest extends BaseRequestTest {
       return
     }
 
-    val topicNames = Seq("topic1", "topic2", "topic3")
+    val topicNames = Seq("topic1", "topic2")
     topicNames.foreach(createTopic(_))
     val topicIds = getTopicIds(topicNames)
 
-    sendOffsetCommitRequest(
-      topicIds.map { case(name, id) => (NameAndId(name, id), ListMap(0 -> offset)) }.toSeq
-        ++ Seq((NameAndId("unresolvable"), ListMap(0 -> offset))),
-      Seq((NameAndId("unresolvable"), ListMap(0 -> Errors.UNKNOWN_TOPIC_ID)))
-        ++ topicIds.map { case(name, id) => (NameAndId(name, id), Map(0 -> Errors.NONE)) }.toSeq,
-      version
+    val requestData = new OffsetCommitRequestData()
+      .setGroupId(groupId)
+      .setTopics(List(
+        new OffsetCommitRequestTopic()
+          .setTopicId(topicIds("topic1"))
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava),
+        new OffsetCommitRequestTopic()
+          .setTopicId(topicIds("topic2"))
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava),
+        new OffsetCommitRequestTopic()
+          .setName("unresolvable")
+          .setPartitions(List(new OffsetCommitRequestPartition()
+            .setPartitionIndex(0)
+            .setCommittedOffset(offset)).asJava)
+      ).asJava)
+
+    val responseData = new OffsetCommitResponseData()
+      .setTopics(List(
+        new OffsetCommitResponseTopic()
+          .setTopicId(topicIds("topic1"))
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.NONE.code)).asJava),
+        new OffsetCommitResponseTopic()
+          .setTopicId(topicIds("topic2"))
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.NONE.code)).asJava),
+        new OffsetCommitResponseTopic()
+          .setName("")
+          .setPartitions(List(new OffsetCommitResponsePartition()
+            .setPartitionIndex(0)
+            .setErrorCode(Errors.UNKNOWN_TOPIC_ID.code)).asJava),
+      ).asJava)
+
+    val response = connectAndReceive[OffsetCommitResponse](
+      new OffsetCommitRequest.Builder(requestData, true).build(version)
     )
+
+    assertResponseEquals(new OffsetCommitResponse(responseData), response)
   }
 
   @Test
@@ -120,25 +197,6 @@ class OffsetCommitRequestTest extends BaseRequestTest {
     } finally {
       Utils.closeQuietly(admin, "AdminClient")
     }
-  }
-
-  def sendOffsetCommitRequest(offsets: Seq[(NameAndId, Map[Int, Long])],
-                              responses: Seq[(NameAndId, Map[Int, Errors])],
-                              version: Short): Unit = {
-
-    val requestData = newOffsetCommitRequestData(
-      groupId = "group",
-      offsets = offsets
-    )
-
-    val expectedResponse = newOffsetCommitResponseData(
-      version,
-      topicPartitions = responses,
-    )
-    val response = connectAndReceive[OffsetCommitResponse](
-      new OffsetCommitRequest.Builder(requestData, true).build(version)
-    )
-    assertResponseEquals(new OffsetCommitResponse(expectedResponse), response)
   }
 
   private def offsetsToCommit(topics: Seq[String], offset: Long): java.util.Map[TopicPartition, OffsetAndMetadata] = {
