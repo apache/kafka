b3cda62a6fc3d1c029eb8c942631567b774e948c
Andrew Grant <agrant@confluent.io>
2023-05-31 17:15:05 -0400

    Dont use star import

--- metadata/src/test/java/org/apache/kafka/metadata/PartitionRegistrationTest.java
+++ metadata/src/test/java/org/apache/kafka/metadata/PartitionRegistrationTest.java
@@ -34,7 +34,11 @@ import org.junit.jupiter.api.Timeout;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @Timeout(40)
