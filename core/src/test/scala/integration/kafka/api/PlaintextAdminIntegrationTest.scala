4449de7e58ceec83af25251bc54752c7f47d43d3
Gantigmaa Selenge <tina.selenge@gmail.com>
2023-06-08 12:22:04 +0100

    Remove a test case that is not longer valid The test was expecting LeaderNotAvailableException when deleting records on non existant partition. This test case will no longer fail with this exception because now the metadata request will be retried and the delete records request will timeout instead. Therefore removed this particular test case so that the test does not wait until timeout.

--- core/src/test/scala/integration/kafka/api/PlaintextAdminIntegrationTest.scala
+++ core/src/test/scala/integration/kafka/api/PlaintextAdminIntegrationTest.scala
@@ -957,15 +957,9 @@ class PlaintextAdminIntegrationTest extends BaseAdminIntegrationTest {
       .lowWatermarks.get(topicPartition).get.lowWatermark)
 
     // OffsetOutOfRangeException if offset > high_watermark
-    var cause = assertThrows(classOf[ExecutionException],
+    val cause = assertThrows(classOf[ExecutionException],
       () => client.deleteRecords(Map(topicPartition -> RecordsToDelete.beforeOffset(20L)).asJava).lowWatermarks.get(topicPartition).get).getCause
     assertEquals(classOf[OffsetOutOfRangeException], cause.getClass)
-
-    val nonExistPartition = new TopicPartition(topic, 3)
-    // LeaderNotAvailableException if non existent partition
-    cause = assertThrows(classOf[ExecutionException],
-      () => client.deleteRecords(Map(nonExistPartition -> RecordsToDelete.beforeOffset(20L)).asJava).lowWatermarks.get(nonExistPartition).get).getCause
-    assertEquals(classOf[LeaderNotAvailableException], cause.getClass)
   }
 
   @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
