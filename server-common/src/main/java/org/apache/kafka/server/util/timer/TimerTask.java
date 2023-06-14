a5cb4f59bcae233a4d207e30630ba8387fd03996
David Jacot <djacot@confluent.io>
2023-06-14 13:30:10 +0200

    bug fix

--- server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTask.java
+++ server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTask.java
@@ -36,7 +36,7 @@ public abstract class TimerTask implements Runnable {
         synchronized (this) {
             // if this timerTask is already held by an existing timer task entry,
             // we will remove such an entry first.
-            if (timerTaskEntry != null && !timerTaskEntry.equals(entry)) {
+            if (timerTaskEntry != null && timerTaskEntry != entry) {
                 timerTaskEntry.remove();
             }
 
