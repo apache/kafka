bace6e2df5dcff1186db3ec4786fa051d4261f38
Greg Harris <greg.harris@aiven.io>
2023-06-23 09:49:52 -0700

    MINOR: Silence error logs for faulty plugins in integration tests
    Signed-off-by: Greg Harris <greg.harris@aiven.io>

--- connect/runtime/src/test/java/org/apache/kafka/connect/runtime/ErrorHandlingTaskTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/ErrorHandlingTaskTest.java
@@ -554,7 +554,7 @@ public class ErrorHandlingTaskTest {
     private abstract static class TestSinkTask extends SinkTask {
     }
 
-    static class FaultyConverter extends JsonConverter {
+    public static class FaultyConverter extends JsonConverter {
         private static final Logger log = LoggerFactory.getLogger(FaultyConverter.class);
         private int invocations = 0;
 
@@ -573,7 +573,7 @@ public class ErrorHandlingTaskTest {
         }
     }
 
-    static class FaultyPassthrough<R extends ConnectRecord<R>> implements Transformation<R> {
+    public static class FaultyPassthrough<R extends ConnectRecord<R>> implements Transformation<R> {
 
         private static final Logger log = LoggerFactory.getLogger(FaultyPassthrough.class);
 
