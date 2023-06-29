e6370512940a86aeee6b6d2bfe894fe529621d9d
Bruno Cadonna <cadonna@apache.org>
2023-06-29 14:39:20 +0200

    Consolidate standby retrieval

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
@@ -1566,11 +1566,7 @@ public class TaskManager {
     }
 
     Set<TaskId> standbyTaskIds() {
-        if (stateUpdater != null) {
-            return stateUpdater.getStandbyTasks().stream().map(Task::id).collect(Collectors.toSet());
-        } else {
-            return standbyTaskStream().map(Task::id).collect(Collectors.toSet());
-        }
+        return standbyTaskStream().map(Task::id).collect(Collectors.toSet());
     }
 
     Map<TaskId, Task> allTasks() {
