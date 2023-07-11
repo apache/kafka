c50f3bdbf79f1ca127355d2a9002b1fdbf0baa2f
Ritika Reddy <rreddy@confluent.io>
2023-07-11 16:30:32 -0700

    Removed extra lines

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/assignor/AssignmentMemberSpec.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/assignor/AssignmentMemberSpec.java
@@ -119,6 +119,5 @@ public class AssignmentMemberSpec {
             ", subscribedTopicIds=" + subscribedTopicIds +
             ", assignedPartitions=" + assignedPartitions +
             ')';
-
     }
 }
