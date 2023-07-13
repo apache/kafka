75d4dee84a12a9db686d0363324e00fdd920bba8
Ruslan Krivoshein <krivosheinruslan@hotmail.com>
2023-07-13 23:32:46 +0200

    KAFKA-14581: Print warning in stdout

--- tools/src/main/java/org/apache/kafka/tools/GetOffsetShell.java
+++ tools/src/main/java/org/apache/kafka/tools/GetOffsetShell.java
@@ -42,8 +42,6 @@ import org.apache.kafka.server.util.TopicFilter;
 import org.apache.kafka.server.util.TopicPartitionFilter;
 import org.apache.kafka.server.util.TopicPartitionFilter.CompositeTopicPartitionFilter;
 import org.apache.kafka.server.util.TopicPartitionFilter.TopicFilterAndPartitionFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -61,7 +59,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class GetOffsetShell {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GetOffsetShell.class);
     private static final Pattern TOPIC_PARTITION_PATTERN = Pattern.compile("([^:,]*)(?::(?:([0-9]*)|(?:([0-9]*)-([0-9]*))))?");
 
     public static void main(String... args) {
@@ -159,7 +156,7 @@ public class GetOffsetShell {
             } else {
                 effectiveBrokerListOpt = brokerListOpt;
 
-                LOGGER.warn("The 'broker-list' option is deprecated and will be removed in the next major release. Use the `bootstrap-server` option with the same syntax.");
+                System.out.println("WARNING: The 'broker-list' option is deprecated and will be removed in the next major release. Use the `bootstrap-server` option with the same syntax.");
             }
 
             CommandLineUtils.checkRequiredArgs(parser, options, effectiveBrokerListOpt);
