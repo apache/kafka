85cee4fcbcecebd3aae66fc074a560804601b84b
Said BOUDJELDA <bmscomp@gmail.com>
2023-07-15 01:37:07 +0200

    remove unused variable

--- examples/src/main/java/kafka/examples/KafkaProperties.java
+++ examples/src/main/java/kafka/examples/KafkaProperties.java
@@ -17,7 +17,6 @@
 package kafka.examples;
 
 public class KafkaProperties {
-    public static final String TOPIC = "topic1";
     public static final String KAFKA_SERVER_URL = "localhost";
     public static final int KAFKA_SERVER_PORT = 9092;
 
