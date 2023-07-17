094e53ea2fa764a9005f8df0d3a1517a8c1474e5
Owen Leung <owen.leung2@gmail.com>
2023-07-17 18:49:00 +0800

    refactor testHandleLoss

--- metadata/src/test/java/org/apache/kafka/image/writer/ImageWriterOptionsTest.java
+++ metadata/src/test/java/org/apache/kafka/image/writer/ImageWriterOptionsTest.java
@@ -21,8 +21,6 @@ import org.apache.kafka.server.common.MetadataVersion;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -57,24 +55,21 @@ public class ImageWriterOptionsTest {
 
     @Test
     public void testHandleLoss() {
-        PrintStream originalOut = System.out;
         String expectedMessage = "stuff";
-        Consumer<UnwritableMetadataException> customLossHandler = e -> System.out.println(e.getMessage());
 
         for (int i = MetadataVersion.MINIMUM_KRAFT_VERSION.ordinal();
              i < MetadataVersion.VERSIONS.length;
              i++) {
-            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
             MetadataVersion version = MetadataVersion.VERSIONS[i];
+            String formattedMessage = String.format("Metadata has been lost because the following could not be represented in metadata version %s: %s", version, expectedMessage);
+            Consumer<UnwritableMetadataException> customLossHandler = e -> {
+                assertEquals(formattedMessage, e.getMessage());
+            };
             ImageWriterOptions options = new ImageWriterOptions.Builder()
                     .setMetadataVersion(version)
                     .setLossHandler(customLossHandler)
                     .build();
-            System.setOut(new PrintStream(outContent));
             options.handleLoss(expectedMessage);
-            System.setOut(originalOut);
-            String formattedMessage = String.format("Metadata has been lost because the following could not be represented in metadata version %s: %s", version, expectedMessage);
-            assertEquals(formattedMessage, outContent.toString().trim());
         }
     }
 }
