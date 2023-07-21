2528dd41169a95f05117ba01302c5dcce0642ab1
David Jacot <djacot@confluent.io>
2023-07-21 20:09:06 +0200

    KAFKA-14499: [2/N] Add OffsetCommit record & related (#14047)
    This patch does a few things:
    1) It introduces the `OffsetAndMetadata` class which hold the committed offsets in the group coordinator.
    2) It adds methods to deal with OffsetCommit records to `RecordHelpers`.
    3) It adds `MetadataVersion#offsetCommitValueVersion` to get the version of the OffsetCommit value record that should be used.
    
    Reviewers: Jeff Kim <jeff.kim@confluent.io>, David Arthur <mumrah@gmail.com>, Justine Olshan <jolshan@confluent.io>

--- group-coordinator/src/test/java/org/apache/kafka/coordinator/group/RecordHelpersTest.java
+++ group-coordinator/src/test/java/org/apache/kafka/coordinator/group/RecordHelpersTest.java
@@ -40,6 +40,8 @@ import org.apache.kafka.coordinator.group.generated.ConsumerGroupTargetAssignmen
 import org.apache.kafka.coordinator.group.generated.ConsumerGroupTargetAssignmentMetadataValue;
 import org.apache.kafka.coordinator.group.generated.GroupMetadataKey;
 import org.apache.kafka.coordinator.group.generated.GroupMetadataValue;
+import org.apache.kafka.coordinator.group.generated.OffsetCommitKey;
+import org.apache.kafka.coordinator.group.generated.OffsetCommitValue;
 import org.apache.kafka.coordinator.group.generic.GenericGroup;
 import org.apache.kafka.coordinator.group.generic.GenericGroupMember;
 import org.apache.kafka.coordinator.group.generic.GenericGroupState;
@@ -48,6 +50,7 @@ import org.apache.kafka.server.common.MetadataVersion;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.ByteBuffer;
@@ -59,6 +62,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -647,5 +652,108 @@ public class RecordHelpersTest {
         );
 
         assertEquals(expectedRecord, groupMetadataRecord);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = MetadataVersion.class)
+    public void testNewOffsetCommitRecord(MetadataVersion metadataVersion) {
+        OffsetCommitKey key = new OffsetCommitKey()
+            .setGroup("group-id")
+            .setTopic("foo")
+            .setPartition(1);
+        OffsetCommitValue value = new OffsetCommitValue()
+            .setOffset(100L)
+            .setLeaderEpoch(10)
+            .setMetadata("metadata")
+            .setCommitTimestamp(1234L)
+            .setExpireTimestamp(-1L);
+
+        Record expectedRecord = new Record(
+            new ApiMessageAndVersion(
+                key,
+                (short) 1),
+            new ApiMessageAndVersion(
+                value,
+                metadataVersion.offsetCommitValueVersion(false)
+            )
+        );
+
+        assertEquals(expectedRecord, RecordHelpers.newOffsetCommitRecord(
+            "group-id",
+            "foo",
+            1,
+            new OffsetAndMetadata(
+                100L,
+                OptionalInt.of(10),
+                "metadata",
+                1234L,
+                OptionalLong.empty()),
+            metadataVersion
+        ));
+
+        value.setLeaderEpoch(-1);
+
+        assertEquals(expectedRecord, RecordHelpers.newOffsetCommitRecord(
+            "group-id",
+            "foo",
+            1,
+            new OffsetAndMetadata(
+                100L,
+                OptionalInt.empty(),
+                "metadata",
+                1234L,
+                OptionalLong.empty()),
+            metadataVersion
+        ));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = MetadataVersion.class)
+    public void testNewOffsetCommitRecordWithExpireTimestamp(MetadataVersion metadataVersion) {
+        Record expectedRecord = new Record(
+            new ApiMessageAndVersion(
+                new OffsetCommitKey()
+                    .setGroup("group-id")
+                    .setTopic("foo")
+                    .setPartition(1),
+                (short) 1),
+            new ApiMessageAndVersion(
+                new OffsetCommitValue()
+                    .setOffset(100L)
+                    .setLeaderEpoch(10)
+                    .setMetadata("metadata")
+                    .setCommitTimestamp(1234L)
+                    .setExpireTimestamp(5678L),
+                (short) 1 // When expire timestamp is set, it is always version 1.
+            )
+        );
+
+        assertEquals(expectedRecord, RecordHelpers.newOffsetCommitRecord(
+            "group-id",
+            "foo",
+            1,
+            new OffsetAndMetadata(
+                100L,
+                OptionalInt.of(10),
+                "metadata",
+                1234L,
+                OptionalLong.of(5678L)),
+            metadataVersion
+        ));
+    }
+
+    @Test
+    public void testNewOffsetCommitTombstoneRecord() {
+        Record expectedRecord = new Record(
+            new ApiMessageAndVersion(
+                new OffsetCommitKey()
+                    .setGroup("group-id")
+                    .setTopic("foo")
+                    .setPartition(1),
+                (short) 1),
+            null);
+
+        Record record = RecordHelpers.newOffsetCommitTombstoneRecord("group-id", "foo", 1);
+        assertEquals(expectedRecord, record);
     }
 }
