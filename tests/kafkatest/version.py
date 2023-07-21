78bcec7d1f2112fcd648db81226fa0b46e754c79
Alyssa Huang <ahuang@confluent.io>
2023-07-21 14:25:42 -0700

    update latest mv and temp setup change

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -122,7 +122,7 @@ def get_version(node=None):
 DEV_BRANCH = KafkaVersion("dev")
 DEV_VERSION = KafkaVersion("3.6.0-SNAPSHOT")
 
-LATEST_METADATA_VERSION = "3.3"
+LATEST_METADATA_VERSION = "3.6"
 
 # 0.8.2.x versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
