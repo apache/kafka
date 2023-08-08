b8732bb649e58f72af25dfc3fbedcf6858b6d767
Colin Patrick McCabe <cmccabe@apache.org>
2023-08-08 16:36:59 -0700

    HOFIX: fix missing import in SnapshottableHashTableTest.java

--- server-common/src/test/java/org/apache/kafka/timeline/SnapshottableHashTableTest.java
+++ server-common/src/test/java/org/apache/kafka/timeline/SnapshottableHashTableTest.java
@@ -30,6 +30,7 @@ import org.junit.jupiter.api.Timeout;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
