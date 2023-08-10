0b3a5d7061315a4bdfc2a3fa2c6cc79d96eaf9e9
Luke Chen <showuon@gmail.com>
2023-08-10 11:21:11 +0800

    MINOR: Update 3.4 branch version to 3.4.1-SNAPSHOT

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -119,7 +119,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("3.4.1-SNAPSHOT")
+DEV_VERSION = KafkaVersion("3.4.2-SNAPSHOT")
 
 LATEST_METADATA_VERSION = "3.3"
 
