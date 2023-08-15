f4381ccdba6162b4b872e83dc05e8ea76c5f3cae
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2023-08-15 14:56:51 +0300

    [aiven] fix: close merge gaps

--- core/src/test/java/kafka/log/remote/RemoteLogManagerTest.java
+++ core/src/test/java/kafka/log/remote/RemoteLogManagerTest.java
@@ -758,7 +758,7 @@ public class RemoteLogManagerTest {
         when(mockLog.logSegments(anyLong(), anyLong())).thenReturn(JavaConverters.collectionAsScalaIterable(Arrays.asList(oldSegment, activeSegment)));
         when(mockLog.lastStableOffset()).thenReturn(250L);
 
-        RemoteLogManager.RLMTask task = remoteLogManager.new RLMTask(leaderTopicIdPartition);
+        RemoteLogManager.RLMTask task = remoteLogManager.new RLMTask(leaderTopicIdPartition, 128);
         task.convertToLeader(0);
 
         RemoteLogManager.RLMTask spyTask = spy(task);
@@ -792,7 +792,7 @@ public class RemoteLogManagerTest {
         when(mockLog.logSegments(anyLong(), anyLong())).thenReturn(JavaConverters.collectionAsScalaIterable(Arrays.asList(oldSegment, activeSegment)));
         when(mockLog.lastStableOffset()).thenReturn(250L);
 
-        RemoteLogManager.RLMTask task = remoteLogManager.new RLMTask(leaderTopicIdPartition);
+        RemoteLogManager.RLMTask task = remoteLogManager.new RLMTask(leaderTopicIdPartition, 128);
         task.convertToLeader(0);
 
         RemoteLogManager.RLMTask spyTask = spy(task);
