e173a826bc40d8e95b2865149eaae119966a1dba
Omnia G.H Ibrahim <o.g.h.ibrahim@gmail.com>
2023-08-16 00:58:52 +0100

    KAFKA-15102: Add replication.policy.internal.topic.separator.enabled property to MirrorMaker 2 (KIP-949) (#14082)
    Reviewers: Chris Egerton <chrise@aiven.io>

--- connect/mirror-client/src/test/java/org/apache/kafka/connect/mirror/ReplicationPolicyTest.java
+++ connect/mirror-client/src/test/java/org/apache/kafka/connect/mirror/ReplicationPolicyTest.java
@@ -17,13 +17,24 @@
 
 package org.apache.kafka.connect.mirror;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ReplicationPolicyTest {
     private static final DefaultReplicationPolicy DEFAULT_REPLICATION_POLICY = new DefaultReplicationPolicy();
+
+    @BeforeEach
+    public void setUp() {
+        DEFAULT_REPLICATION_POLICY.configure(Collections.emptyMap());
+    }
 
     @Test
     public void testInternalTopic() {
@@ -38,5 +49,47 @@ public class ReplicationPolicyTest {
         assertTrue(DEFAULT_REPLICATION_POLICY.isInternalTopic("mm2-offsets-CLUSTER-internal"));
         // non-internal topic.
         assertFalse(DEFAULT_REPLICATION_POLICY.isInternalTopic("mm2-offsets_CLUSTER_internal"));
+    }
+
+
+    @Test
+    public void offsetSyncsTopic_shouldBeEffectedByInternalTopicSeparatorEnabled() {
+        Map<String, Object> config =  new HashMap<>();
+        config.put(MirrorClientConfig.REPLICATION_POLICY_SEPARATOR, "__");
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, false);
+        DEFAULT_REPLICATION_POLICY.configure(config);
+        assertEquals("mm2-offset-syncs.CLUSTER.internal", DEFAULT_REPLICATION_POLICY.offsetSyncsTopic("CLUSTER"));
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, true);
+        DEFAULT_REPLICATION_POLICY.configure(config);
+        assertEquals("mm2-offset-syncs__CLUSTER__internal", DEFAULT_REPLICATION_POLICY.offsetSyncsTopic("CLUSTER"));
+    }
+
+    @Test
+    public void checkpointsTopic_shouldBeEffectedByInternalTopicSeparatorEnabled() {
+        Map<String, Object> config =  new HashMap<>();
+        config.put(MirrorClientConfig.REPLICATION_POLICY_SEPARATOR, "__");
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, false);
+        DEFAULT_REPLICATION_POLICY.configure(config);
+        assertEquals("CLUSTER.checkpoints.internal", DEFAULT_REPLICATION_POLICY.checkpointsTopic("CLUSTER"));
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, true);
+        DEFAULT_REPLICATION_POLICY.configure(config);
+        assertEquals("CLUSTER__checkpoints__internal", DEFAULT_REPLICATION_POLICY.checkpointsTopic("CLUSTER"));
+    }
+
+    @Test
+    public void heartbeatsTopic_shouldNotBeEffectedByInternalTopicSeparatorConfig() {
+        Map<String, Object> config =  new HashMap<>();
+        config.put(MirrorClientConfig.REPLICATION_POLICY_SEPARATOR, "__");
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, true);
+        assertEquals("heartbeats", DEFAULT_REPLICATION_POLICY.heartbeatsTopic());
+
+        config.put(MirrorClientConfig.INTERNAL_TOPIC_SEPARATOR_ENABLED, false);
+        DEFAULT_REPLICATION_POLICY.configure(config);
+        assertEquals("heartbeats", DEFAULT_REPLICATION_POLICY.heartbeatsTopic());
     }
 }
