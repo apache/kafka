13633c98e1f7d90ca3ae796e947fa6de06d47d6a
Bruno Cadonna <cadonna@apache.org>
2023-08-15 16:50:30 +0200

    KAFKA-10199: Change to RUNNING if no pending task to init exist
    A stream thread should only change to RUNNING if there are no
    active tasks in restoration in the state updater and if there
    are no pending tasks to recycle and to init.
    
    Usually all pending tasks to init are added to the state updater
    in the same poll iteration that handles the assignment. However,
    if during an initialization of a task a LockException the task
    is re-added to the tasks to init and initialization is retried
    in the next poll iteration.
    
    A LockException might occur when a state directory is still locked
    by another thread, when the rebalance just happened.

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/TaskManagerTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/TaskManagerTest.java
@@ -528,7 +528,7 @@ public class TaskManagerTest {
 
         taskManager.handleAssignment(tasksToBeCreated, Collections.emptyMap());
 
-        Mockito.verify(tasks).addPendingTaskToInit(createdTasks);
+        Mockito.verify(tasks).addPendingTasksToInit(createdTasks);
         Mockito.verify(standbyTaskCreator).createTasks(Collections.emptyMap());
     }
 
@@ -550,7 +550,7 @@ public class TaskManagerTest {
         );
 
         Mockito.verify(activeTaskCreator).createTasks(consumer, Collections.emptyMap());
-        Mockito.verify(tasks).addPendingTaskToInit(createdTasks);
+        Mockito.verify(tasks).addPendingTasksToInit(createdTasks);
     }
 
     @Test
@@ -753,7 +753,7 @@ public class TaskManagerTest {
             .withInputPartitions(taskId01Partitions)
             .inState(State.RUNNING).build();
         final TasksRegistry tasks = mock(TasksRegistry.class);
-        when(tasks.drainPendingTaskToInit()).thenReturn(mkSet(task00, task01));
+        when(tasks.drainPendingTasksToInit()).thenReturn(mkSet(task00, task01));
         taskManager = setUpTaskManager(StreamsConfigUtils.ProcessingMode.AT_LEAST_ONCE, tasks, true);
 
         taskManager.checkStateUpdater(time.milliseconds(), noOpResetter);
@@ -773,7 +773,7 @@ public class TaskManagerTest {
             .withInputPartitions(taskId01Partitions)
             .inState(State.RUNNING).build();
         final TasksRegistry tasks = mock(TasksRegistry.class);
-        when(tasks.drainPendingTaskToInit()).thenReturn(mkSet(task00, task01));
+        when(tasks.drainPendingTasksToInit()).thenReturn(mkSet(task00, task01));
         final LockException lockException = new LockException("Where are my keys??");
         doThrow(lockException)
             .when(task00).initializeIfNeeded();
@@ -783,7 +783,7 @@ public class TaskManagerTest {
 
         Mockito.verify(task00).initializeIfNeeded();
         Mockito.verify(task01).initializeIfNeeded();
-        Mockito.verify(tasks).addPendingTaskToInit(Collections.singleton(task00));
+        Mockito.verify(tasks).addPendingTasksToInit(Collections.singleton(task00));
         Mockito.verify(stateUpdater).add(task01);
     }
 
@@ -1482,7 +1482,7 @@ public class TaskManagerTest {
             .withInputPartitions(taskId02Partitions).build();
         final TasksRegistry tasks = mock(TasksRegistry.class);
         final TaskManager taskManager = setUpTaskManager(ProcessingMode.EXACTLY_ONCE_V2, tasks, true);
-        when(tasks.drainPendingTaskToInit()).thenReturn(mkSet(statefulTask0, statefulTask1, statefulTask2));
+        when(tasks.drainPendingTasksToInit()).thenReturn(mkSet(statefulTask0, statefulTask1, statefulTask2));
         doThrow(new TaskCorruptedException(Collections.singleton(statefulTask0.id))).when(statefulTask0).initializeIfNeeded();
         doThrow(new TaskCorruptedException(Collections.singleton(statefulTask1.id))).when(statefulTask1).initializeIfNeeded();
 
@@ -2038,8 +2038,8 @@ public class TaskManagerTest {
         standbyTaskOrder.verify(corruptedStandbyTask).revive();
         Mockito.verify(tasks).removeTask(corruptedActiveTask);
         Mockito.verify(tasks).removeTask(corruptedStandbyTask);
-        Mockito.verify(tasks).addPendingTaskToInit(mkSet(corruptedActiveTask));
-        Mockito.verify(tasks).addPendingTaskToInit(mkSet(corruptedStandbyTask));
+        Mockito.verify(tasks).addPendingTasksToInit(mkSet(corruptedActiveTask));
+        Mockito.verify(tasks).addPendingTasksToInit(mkSet(corruptedStandbyTask));
     }
 
     @Test
