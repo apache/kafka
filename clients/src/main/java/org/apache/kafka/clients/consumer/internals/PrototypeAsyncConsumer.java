945d21953e93409290a68dced0d366380866fb26
Philip Nee <pnee@confluent.io>
2023-08-29 12:03:15 -0700

    KAFKA-14875: Implement wakeup (#14118)
    Summary
    Implemented wakeup() mechanism using a WakeupTrigger class to store the pending wakeup item, and when wakeup() is invoked, it checks whether there's an active task or a wakeup task.
    
    If there's an active task: the task will be completed exceptionally and the atomic reference will be freed up.
    If there an wakedup task, which means wakeup() was invoked before a blocking call was issued. Therefore, the current task will be completed exceptionally immediately.
    
    This PR also addressed minor issues such as:
    
    Throwing WakeupException at the right place: As wakeups are thrown by completing an active future exceptionally. The WakeupException is wrapped inside of the ExecutionException.
    
    mockConstruction is a thread-lock mock; therefore, we need to free up the reference before completing the test. Otherwise, other tests will continue using the thread-lock mock.
    
    Reviewers: Lianet Magrans <lianetmr@gmail.com>, Jun Rao <junrao@gmail.com>

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/PrototypeAsyncConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/PrototypeAsyncConsumer.java
@@ -42,6 +42,7 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.errors.InterruptException;
 import org.apache.kafka.common.errors.InvalidGroupIdException;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.internals.ClusterResourceListeners;
 import org.apache.kafka.common.metrics.Metrics;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -70,10 +71,10 @@ import java.util.regex.Pattern;
 
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.internals.ConsumerUtils.configuredConsumerInterceptors;
 import static org.apache.kafka.clients.consumer.internals.ConsumerUtils.createLogContext;
 import static org.apache.kafka.clients.consumer.internals.ConsumerUtils.createMetrics;
 import static org.apache.kafka.clients.consumer.internals.ConsumerUtils.createSubscriptionState;
-import static org.apache.kafka.clients.consumer.internals.ConsumerUtils.configuredConsumerInterceptors;
 import static org.apache.kafka.common.utils.Utils.closeQuietly;
 import static org.apache.kafka.common.utils.Utils.isBlank;
 import static org.apache.kafka.common.utils.Utils.join;
@@ -98,9 +99,10 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
     private final Metrics metrics;
     private final long defaultApiTimeoutMs;
 
-    public PrototypeAsyncConsumer(final Properties properties,
-                                  final Deserializer<K> keyDeserializer,
-                                  final Deserializer<V> valueDeserializer) {
+    private WakeupTrigger wakeupTrigger = new WakeupTrigger();
+    public PrototypeAsyncConsumer(Properties properties,
+                         Deserializer<K> keyDeserializer,
+                         Deserializer<V> valueDeserializer) {
         this(propsToMap(properties), keyDeserializer, valueDeserializer);
     }
 
@@ -197,6 +199,7 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
+        // TODO: Once we implement poll(), clear wakeupTrigger in a finally block: wakeupTrigger.clearActiveTask();
 
         return ConsumerRecords.empty();
     }
@@ -239,7 +242,7 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
 
     @Override
     public void commitAsync(Map<TopicPartition, OffsetAndMetadata> offsets, OffsetCommitCallback callback) {
-        CompletableFuture<Void> future = commit(offsets);
+        CompletableFuture<Void> future = commit(offsets, false);
         final OffsetCommitCallback commitCallback = callback == null ? new DefaultOffsetCommitCallback() : callback;
         future.whenComplete((r, t) -> {
             if (t != null) {
@@ -254,9 +257,13 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
     }
 
     // Visible for testing
-    CompletableFuture<Void> commit(Map<TopicPartition, OffsetAndMetadata> offsets) {
+    CompletableFuture<Void> commit(Map<TopicPartition, OffsetAndMetadata> offsets, final boolean isWakeupable) {
         maybeThrowInvalidGroupIdException();
         final CommitApplicationEvent commitEvent = new CommitApplicationEvent(offsets);
+        if (isWakeupable) {
+            // the task can only be woken up if the top level API call is commitSync
+            wakeupTrigger.setActiveTask(commitEvent.future());
+        }
         eventHandler.add(commitEvent);
         return commitEvent.future();
     }
@@ -317,6 +324,7 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
         }
 
         final OffsetFetchApplicationEvent event = new OffsetFetchApplicationEvent(partitions);
+        wakeupTrigger.setActiveTask(event.future());
         eventHandler.add(event);
         try {
             return event.future().get(timeout.toMillis(), TimeUnit.MILLISECONDS);
@@ -325,10 +333,11 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
         } catch (TimeoutException e) {
             throw new org.apache.kafka.common.errors.TimeoutException(e);
         } catch (ExecutionException e) {
-            // Execution exception is thrown here
+            if (e.getCause() instanceof WakeupException)
+                throw new WakeupException();
             throw new KafkaException(e);
-        } catch (Exception e) {
-            throw e;
+        } finally {
+            wakeupTrigger.clearActiveTask();
         }
     }
 
@@ -450,6 +459,7 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
 
     @Override
     public void wakeup() {
+        wakeupTrigger.wakeup();
     }
 
     /**
@@ -470,7 +480,7 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
 
     @Override
     public void commitSync(Map<TopicPartition, OffsetAndMetadata> offsets, Duration timeout) {
-        CompletableFuture<Void> commitFuture = commit(offsets);
+        CompletableFuture<Void> commitFuture = commit(offsets, true);
         try {
             commitFuture.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
         } catch (final TimeoutException e) {
@@ -478,9 +488,11 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
         } catch (final InterruptedException e) {
             throw new InterruptException(e);
         } catch (final ExecutionException e) {
+            if (e.getCause() instanceof WakeupException)
+                throw new WakeupException();
             throw new KafkaException(e);
-        } catch (final Exception e) {
-            throw e;
+        } finally {
+            wakeupTrigger.clearActiveTask();
         }
     }
 
@@ -559,6 +571,24 @@ public class PrototypeAsyncConsumer<K, V> implements Consumer<K, V> {
     @Deprecated
     public ConsumerRecords<K, V> poll(long timeout) {
         throw new KafkaException("method not implemented");
+    }
+
+    // Visible for testing
+    WakeupTrigger wakeupTrigger() {
+        return wakeupTrigger;
+    }
+
+    private static <K, V> ClusterResourceListeners configureClusterResourceListeners(
+            final Deserializer<K> keyDeserializer,
+            final Deserializer<V> valueDeserializer,
+            final List<?>... candidateLists) {
+        ClusterResourceListeners clusterResourceListeners = new ClusterResourceListeners();
+        for (List<?> candidateList: candidateLists)
+            clusterResourceListeners.maybeAddAll(candidateList);
+
+        clusterResourceListeners.maybeAdd(keyDeserializer);
+        clusterResourceListeners.maybeAdd(valueDeserializer);
+        return clusterResourceListeners;
     }
 
     // This is here temporary as we don't have public access to the ConsumerConfig in this module.
