4491b675feb7ba2eafe20cdb97df548e13b99118
Chris Egerton <chrise@aiven.io>
2023-08-29 11:42:59 -0400

    Add comment about skipping validation when class is null

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/ConnectorConfig.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/ConnectorConfig.java
@@ -429,6 +429,7 @@ public class ConnectorConfig extends AbstractConfig {
                 final ConfigDef.Validator typeValidator = ConfigDef.LambdaValidator.with(
                     (String name, Object value) -> {
                         validateProps(prefix);
+                        // The value will be null if the class couldn't be found; no point in performing follow-up validation
                         if (value != null) {
                             getConfigDefFromConfigProvidingClass(typeConfig, (Class<?>) value);
                         }
