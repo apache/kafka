3ca315646aa610f3863850e1eba84847c8d7a0d9
Deqi Hu <deqi.hu@shopee.com>
2023-08-30 20:34:41 +0800

    remove unused import

--- storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
@@ -46,7 +46,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
