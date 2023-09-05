3f4b2ba6bf3633f79cefc09d89d0985ce1b4529e
see-quick <maros.orsak159@gmail.com>
2023-09-05 10:56:51 +0200

    MINOR: Add missing versions to STs
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- tests/kafkatest/tests/streams/streams_upgrade_test.py
+++ tests/kafkatest/tests/streams/streams_upgrade_test.py
@@ -34,17 +34,18 @@ broker_upgrade_versions = [str(LATEST_0_11_0), str(LATEST_1_0), str(LATEST_1_1),
                            str(LATEST_2_0), str(LATEST_2_1), str(LATEST_2_2), str(LATEST_2_3),
                            str(LATEST_2_4), str(LATEST_2_5), str(LATEST_2_6), str(LATEST_2_7),
                            str(LATEST_2_8), str(LATEST_3_0), str(LATEST_3_1), str(LATEST_3_2),
-                           str(LATEST_3_3),
+                           str(LATEST_3_3), str(LATEST_3_4), str(LATEST_3_5), str(LATEST_3_6),
                            str(DEV_BRANCH)]
 
 metadata_1_versions = [str(LATEST_0_10_0)]
 metadata_2_versions = [str(LATEST_0_10_1), str(LATEST_0_10_2), str(LATEST_0_11_0), str(LATEST_1_0), str(LATEST_1_1),
                        str(LATEST_2_4), str(LATEST_2_5), str(LATEST_2_6), str(LATEST_2_7), str(LATEST_2_8),
-                       str(LATEST_3_0)]
+                       str(LATEST_3_0), str(LATEST_3_1), str(LATEST_3_2),str(LATEST_3_3), str(LATEST_3_4),
+                       str(LATEST_3_5), str(LATEST_3_6)]
 # upgrading from version (2.4...3.0) is broken and only fixed later in 3.1
 # we cannot test two bounce rolling upgrade because we know it's broken
 # instead we add version 2.4...3.0 to the `metadata_2_versions` upgrade list
-fk_join_versions = [str(LATEST_3_1), str(LATEST_3_2), str(LATEST_3_3)]
+fk_join_versions = [str(LATEST_3_1), str(LATEST_3_2), str(LATEST_3_3), str(LATEST_3_4), str(LATEST_3_5), str(LATEST_3_6)]
 
 """
 After each release one should first check that the released version has been uploaded to 
