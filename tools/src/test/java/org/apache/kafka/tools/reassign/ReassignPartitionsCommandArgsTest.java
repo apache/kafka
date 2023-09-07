2d76eca1bb1661111e044d24750c3d19bb454503
nizhikov <nizhikov@apache.org>
2023-09-07 20:37:03 +0300

    KAFKA-14595 Merge trunk

--- tools/src/test/java/org/apache/kafka/tools/reassign/ReassignPartitionsCommandArgsTest.java
+++ tools/src/test/java/org/apache/kafka/tools/reassign/ReassignPartitionsCommandArgsTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.kafka.tools.reassign;
 
-import kafka.admin.ReassignPartitionsCommand;
 import org.apache.kafka.common.utils.Exit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
