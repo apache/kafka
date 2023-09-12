d9c16b8a0f2596e4dab54b4b1a659a811090d9be
zhaohaiyuan <zhaohaiyuan@meituan.com>
2023-09-13 00:14:13 +0800

    Address comments

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupCoordinatorShard.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupCoordinatorShard.java
@@ -313,7 +313,7 @@ public class GroupCoordinatorShard implements CoordinatorShard<Record> {
      *
      * @param statesFilter The states of the groups we want to list. If empty all groups are returned with their state.
      * @param committedOffset A specified committed offset corresponding to this shard
-     * @return A Result containing the ListGroupsResponseData response
+     * @return A list containing the ListGroupsResponseData.ListedGroup
      */
     public List<ListGroupsResponseData.ListedGroup> listGroups(
         List<String> statesFilter,
