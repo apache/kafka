a7e865c0a756504cc7ae6f4eb0772cadd3333c53
Lianet Magrans <98415067+lianetm@users.noreply.github.com>
2023-09-13 10:49:41 -0400

    KAFKA-15115 - KAFKA-15163; Reset/Validate positions implementation & API integration (#14346)
    Implementation of the required functionality for resetting and validating positions in the new async consumer.
    
    This PR includes:
    1. New async application events ResetPositionsApplicationEvent and ValidatePositionsApplicationEvent, both handled by the same OfffsetsRequestManager.
    2. Integration of the reset/validate functionality in the new async consumer, to update fetch positions using the partitions offsets.
    3. Minor refactoring to extract functionality that is reused from both consumer implementations (moving logic without changes from OffsetFetcher into OffsetFetchUtils, and from OffsetsForLeaderEpochClient into OffsetsForLeaderEpochUtils)
    
    Reviewers: Philip Nee <pnee@confluent.io>, Kirk True <kirk@mustardgrain.com>, Jun Rao<junrao@gmail.com>

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/events/ValidatePositionsApplicationEvent.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/events/ValidatePositionsApplicationEvent.java
@@ -14,25 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.kafka.clients.consumer.internals.events;
 
 /**
- * This is the abstract definition of the events created by the KafkaConsumer API
+ * Event for validating offsets for all assigned partitions for which a leader change has been
+ * detected. This is an asynchronous event that generates OffsetForLeaderEpoch requests, and
+ * completes by validating in-memory positions against the offsets received in the responses.
  */
-abstract public class ApplicationEvent {
-    public final Type type;
+public class ValidatePositionsApplicationEvent extends CompletableApplicationEvent<Void> {
 
-    protected ApplicationEvent(Type type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return type + " ApplicationEvent";
-    }
-
-    public enum Type {
-        NOOP, COMMIT, POLL, FETCH_COMMITTED_OFFSET, METADATA_UPDATE, ASSIGNMENT_CHANGE,
-        LIST_OFFSETS, RESET_POSITIONS, VALIDATE_POSITIONS,
+    public ValidatePositionsApplicationEvent() {
+        super(Type.VALIDATE_POSITIONS);
     }
 }
