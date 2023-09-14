d7d65bfa209490184e0ad3f6623e05ee4860ff50
Hailey Ni <hni@confluent.io>
2023-09-14 16:26:49 -0700

    Add process.roles field to support killing controller/broker

--- bin/kafka-server-stop.sh
+++ bin/kafka-server-stop.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 SIGNAL=${SIGNAL:-TERM}
 
-if [[ "$1" == "--process-role" ]]; then
+if [[ "$1" == "--process.roles" ]]; then
     ProcessRole="$2"
 fi
 
@@ -28,22 +28,27 @@ if [[ "$OSNAME" == "OS/390" ]]; then
 elif [[ "$OSNAME" == "OS400" ]]; then
     PIDS=$(ps -Af | grep -i 'kafka\.Kafka' | grep java | grep -v grep | awk '{print $2}')
 else
-    PIDS=$(ps ax | grep ' kafka\.Kafka ' | grep java | grep -v grep | awk '{print $1}')
-    ConfigFiles=$(ps ax | grep ' kafka\.Kafka ' | grep java | grep -v grep | awk 'NF>1{print $NF}')
+    PIDS=$(ps ax | grep ' kafka\.Kafka ' | grep java | grep -v grep | awk '{print $1}'| xargs)
+    ConfigFiles=$(ps ax | grep ' kafka\.Kafka ' | grep java | grep -v grep | sed 's/--override property=[^ ]*//g' | awk 'NF>1{print $NF}' | xargs)
 fi
 
 if [ -z "$PIDS" ]; then
   echo "No kafka server to stop"
   exit 1
 else
-  if [ -z "$ConfigFile" ]; then
+  if [ -z "$ProcessRole" ]; then
     kill -s $SIGNAL $PIDS
   else
+    IFS=' ' read -ra ConfigFilesArray <<< "$ConfigFiles"
+    IFS=' ' read -ra PIDSArray <<< "$PIDS"
     keyword="process.roles="
-    for ((i = 0; i < ${#ConfigFiles[@]}; i++)); do
-        PRCRole=$(sed -n "s/$keyword//p" "${ConfigFiles[i]}")
+    for ((i = 0; i < ${#ConfigFilesArray[@]}; i++)); do
+        PRCRole=$(sed -n "/$keyword/ { s/$keyword//p; q; }" "${ConfigFilesArray[i]}")
         if [ "$PRCRole" == "$ProcessRole" ]; then
-          kill -s $SIGNAL ${PIDS[i]}
+          kill -s $SIGNAL ${PIDSArray[i]}
+#          echo "Killed ${PRCRole} with PID ${PIDSArray[i]}"
+#        else
+#          echo "Skip killing ${PRCRole} with PID ${PIDSArray[i]}"
         fi
     done
   fi
