09045619a5a0fc27f715bcb05c5bd6a45537dbcc
Jeel Jotaniya <jeeljo@amazon.co.uk>
2023-10-04 10:45:53 +0100

    KAFKA-15481: Fix concurrency bug in RemoteIndexCache

--- core/src/test/scala/unit/kafka/log/remote/RemoteIndexCacheTest.scala
+++ core/src/test/scala/unit/kafka/log/remote/RemoteIndexCacheTest.scala
@@ -555,21 +555,15 @@ class RemoteIndexCacheTest {
     val latchForCacheRemove = new CountDownLatch(1)
     val latchForTestWait = new CountDownLatch(1)
 
-    var markForCleanupCallCount = 0
-
     doAnswer((invocation: InvocationOnMock) => {
-      markForCleanupCallCount += 1
-
-      if (markForCleanupCallCount == 1) {
-        // Signal the CacheRead to unblock itself
-        latchForCacheRead.countDown()
-        // Wait for signal to start renaming the files
-        latchForCacheRemove.await()
-        // Calling the markForCleanup() actual method to start renaming the files
-        invocation.callRealMethod()
-        // Signal TestWait to unblock itself so that test can be completed
-        latchForTestWait.countDown()
-      }
+      // Signal the CacheRead to unblock itself
+      latchForCacheRead.countDown()
+      // Wait for signal to start renaming the files
+      latchForCacheRemove.await()
+      // Calling the markForCleanup() actual method to start renaming the files
+      invocation.callRealMethod()
+      // Signal TestWait to unblock itself so that test can be completed
+      latchForTestWait.countDown()
     }).when(spyEntry).markForCleanup()
 
     val removeCache = (() => {
