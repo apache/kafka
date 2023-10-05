2e043865cdd77c9b2d0fab1e4a677adf6ca903be
Matthias J. Sax <matthias@confluent.io>
2023-10-05 02:24:03 -0700

    HOTIFX: fix Kafka versions for system tests

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -205,35 +205,42 @@ LATEST_2_5 = V_2_5_1
 V_2_6_0 = KafkaVersion("2.6.0")
 V_2_6_1 = KafkaVersion("2.6.1")
 V_2_6_2 = KafkaVersion("2.6.2")
-LATEST_2_6 = V_2_6_2
+V_2_6_3 = KafkaVersion("2.6.3")
+LATEST_2_6 = V_2_6_3
 
 # 2.7.x versions
 V_2_7_0 = KafkaVersion("2.7.0")
 V_2_7_1 = KafkaVersion("2.7.1")
-LATEST_2_7 = V_2_7_1
+V_2_7_2 = KafkaVersion("2.7.2")
+LATEST_2_7 = V_2_7_2
 
 # 2.8.x versions
 V_2_8_0 = KafkaVersion("2.8.0")
 V_2_8_1 = KafkaVersion("2.8.1")
-LATEST_2_8 = V_2_8_1
+V_2_8_2 = KafkaVersion("2.8.2")
+LATEST_2_8 = V_2_8_2
 
 # 3.0.x versions
 V_3_0_0 = KafkaVersion("3.0.0")
 V_3_0_1 = KafkaVersion("3.0.1")
-LATEST_3_0 = V_3_0_1
+V_3_0_2 = KafkaVersion("3.0.2")
+LATEST_3_0 = V_3_0_2
 
 # 3.1.x versions
 V_3_1_0 = KafkaVersion("3.1.0")
 V_3_1_1 = KafkaVersion("3.1.1")
-LATEST_3_1 = V_3_1_1
+V_3_1_2 = KafkaVersion("3.1.2")
+LATEST_3_1 = V_3_1_2
 
 # 3.2.x versions
 V_3_2_0 = KafkaVersion("3.2.0")
-LATEST_3_2 = V_3_2_0
+V_3_2_1 = KafkaVersion("3.2.1")
+V_3_2_2 = KafkaVersion("3.2.2")
+V_3_2_3 = KafkaVersion("3.2.3")
+LATEST_3_2 = V_3_2_3
 
 # 3.3.x versions
 V_3_3_0 = KafkaVersion("3.3.0")
 V_3_3_1 = KafkaVersion("3.3.1")
 V_3_3_2 = KafkaVersion("3.3.2")
-V_3_3_3 = KafkaVersion("3.3.3")
-LATEST_3_3 = V_3_3_3
+LATEST_3_3 = V_3_3_2
