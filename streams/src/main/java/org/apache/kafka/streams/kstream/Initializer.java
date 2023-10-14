9c1a1eea5b3f74f668884174749eb11d06675dac
Matthias J. Sax <matthias@confluent.io>
2023-10-14 11:34:43 -0700

    MINOR: Fix `Consumed` to return new object instead of `this`
    We embrace immutability and thus should return a new object instead of
    `this`, similar to other config classed we use in the DSL.
    
    Side JavaDocs cleanup for a bunch of classes.

--- streams/src/main/java/org/apache/kafka/streams/kstream/Initializer.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/Initializer.java
@@ -16,12 +16,12 @@
  */
 package org.apache.kafka.streams.kstream;
 
-
 /**
  * The {@code Initializer} interface for creating an initial value in aggregations.
  * {@code Initializer} is used in combination with {@link Aggregator}.
  *
- * @param <VA> aggregate value type
+ * @param <VAag> aggregate value type
+ *
  * @see Aggregator
  * @see KGroupedStream#aggregate(Initializer, Aggregator)
  * @see KGroupedStream#aggregate(Initializer, Aggregator, Materialized)
@@ -30,12 +30,12 @@ package org.apache.kafka.streams.kstream;
  * @see SessionWindowedKStream#aggregate(Initializer, Aggregator, Merger)
  * @see SessionWindowedKStream#aggregate(Initializer, Aggregator, Merger, Materialized)
  */
-public interface Initializer<VA> {
+public interface Initializer<VAag> {
 
     /**
      * Return the initial value for an aggregation.
      *
      * @return the initial value for an aggregation
      */
-    VA apply();
+    VAag apply();
 }
