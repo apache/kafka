06234352e0a318dc036e33f9b647e7702ee8622e
Vedarth Sharma <vesharma@confluent.io>
2023-10-16 15:51:14 +0530

    Refactor scripts running in Dockerfile

--- docker/docker_build_test.py
+++ docker/docker_build_test.py
@@ -19,11 +19,11 @@ def run_jvm_tests(image, tag):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("image")
-    parser.add_argument("tag")
-    parser.add_argument("image_type", default="all")
-    parser.add_argument("-ku", "--kafka-url", dest="kafka_url")
-    parser.add_argument("-b", "--build", action="store_true", dest="build_only", default=False, help="Only builds the image, don't run tests")
+    parser.add_argument("image", help="Image name that you want to keep for the Docker image")
+    parser.add_argument("-tag", "--image-tag", default="latest", dest="tag", help="Image tag that you want to add to the image")
+    parser.add_argument("-type", "--image-type", default="all", dest="image_type", help="Image type you want to build. By default it's all")
+    parser.add_argument("-u", "--kafka-url", dest="kafka_url", help="Kafka url to be used to download kafka binary tarball in the docker image")
+    parser.add_argument("-b", "--build", action="store_true", dest="build_only", default=False, help="Only build the image, don't run tests")
     parser.add_argument("-t", "--test", action="store_true", dest="test_only", default=False, help="Only run the tests, don't build the image")
     args = parser.parse_args()
 
