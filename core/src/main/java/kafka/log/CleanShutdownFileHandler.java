0193ea58974403813cbbbe687856b66fec4ced86
CalvinConfluent <caliu@confluent.io>
2023-10-16 17:11:21 -0700

    Minor

--- core/src/main/java/kafka/log/CleanShutdownFileHandler.java
+++ core/src/main/java/kafka/log/CleanShutdownFileHandler.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
