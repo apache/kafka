f75cc5fa1f0f0f0c9684057d3b51c66d57e367a9
Vedarth Sharma <vesharma@confluent.io>
2023-10-18 19:07:57 +0530

    Update release script to support registry and add error handling

--- docker/docker_release.py
+++ docker/docker_release.py
@@ -29,21 +29,24 @@ from distutils.dir_util import copy_tree
 from datetime import date
 import shutil
 
-def push_jvm(docker_account, image_name, image_tag, kafka_url):
+def push_jvm(image, kafka_url):
     copy_tree("resources", "jvm/resources")
     subprocess.run(["docker", "buildx", "build", "-f", "jvm/Dockerfile", "--build-arg", f"kafka_url={kafka_url}", "--build-arg", f"build_date={date.today()}",
     "--push",
     "--platform", "linux/amd64,linux/arm64",
-    "--tag", f"{docker_account}/{image_name}:{image_tag}", "jvm"])
+    "--tag", image, "jvm"])
     shutil.rmtree("jvm/resources")
 
 def login():
-    subprocess.run(["docker", "login"])
+    status = subprocess.run(["docker", "login"])
+    if status.returncode != 0:
+        print("Docker login failed, aborting the docker release")
+        raise PermissionError
 
-def create():
+def create_builder():
     subprocess.run(["docker", "buildx", "create", "--name", "kafka-builder", "--use"])
 
-def remove():
+def remove_builder():
     subprocess.run(["docker", "buildx", "rm", "kafka-builder"])
 
 if __name__ == "__main__":
@@ -51,10 +54,27 @@ if __name__ == "__main__":
           This script will build and push docker images of apache kafka.\n\
           Please ensure that image has been sanity tested before pushing the image")
     login()
-    create()
-    docker_account = input("Enter the dockerhub account you want to push the image to: ")
+    docker_registry = input("Enter the docker registry you want to push the image to [docker.io]: ")
+    if docker_registry == "":
+        docker_registry = "docker.io"
+    docker_namespace = input("Enter the docker namespace you want to push the image to: ")
     image_name = input("Enter the image name: ")
+    if image_name == "":
+        raise ValueError("image name cannot be empty")
     image_tag = input("Enter the image tag for the image: ")
+    if image_tag == "":
+        raise ValueError("image tag cannot be empty")
     kafka_url = input("Enter the url for kafka binary tarball: ")
-    push_jvm(docker_account, image_name, image_tag, kafka_url)
-    remove()
+    if kafka_url == "":
+        raise ValueError("kafka url cannot be empty")
+    image = f"{docker_registry}/{docker_namespace}/{image_name}:{image_tag}"
+    print(f"Docker image containing kafka downloaded from {kafka_url} will be pushed to {image}")
+    proceed = input("Should we proceed? [y/N]: ")
+    if proceed == "y":
+        print("Building and pushing the image")
+        create_builder()
+        push_jvm(image, kafka_url)
+        remove_builder()
+        print(f"Image has been pushed to {image}")
+    else:
+        print("Image push aborted")
