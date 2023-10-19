33b8ee3aae15a4ad8b5118469f007c7c427f310d
Luke Chen <showuon@gmail.com>
2023-10-19 11:37:59 +0800

    remove unused import

--- core/src/test/java/kafka/log/remote/RemoteLogManagerTest.java
+++ core/src/test/java/kafka/log/remote/RemoteLogManagerTest.java
@@ -69,7 +69,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.mockito.MockedConstruction;
