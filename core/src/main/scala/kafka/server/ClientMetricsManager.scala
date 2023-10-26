275f3586327dea889e98b8581bb8c4a3d2af06b2
Apoorv Mittal <amittal@confluent.io>
2023-10-26 11:37:19 +0100

    Fixing variable names

--- core/src/main/scala/kafka/server/ClientMetricsManager.scala
+++ core/src/main/scala/kafka/server/ClientMetricsManager.scala
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package kafka.server
 
 import kafka.metrics.ClientMetricsConfig
--- core/src/test/scala/kafka/metrics/ClientMetricsTestUtils.scala
+++ core/src/test/scala/kafka/metrics/ClientMetricsTestUtils.scala
@@ -21,19 +21,19 @@ import ClientMetricsConfig.ClientMatchingParams.{CLIENT_SOFTWARE_NAME, CLIENT_SO
 import java.util.Properties
 
 object ClientMetricsTestUtils {
-  val defaultPushIntervalMs: Int = 30 * 1000 // 30 seconds
+  val DefaultPushIntervalMs: Int = 30 * 1000 // 30 seconds
 
-  val defaultMetrics =
+  val DefaultMetrics =
     "org.apache.kafka.client.producer.partition.queue.,org.apache.kafka.client.producer.partition.latency"
 
-  val defaultClientMatchPatters =
+  val DefaultClientMatchPatters =
     List(s"${CLIENT_SOFTWARE_NAME}=Java", s"${CLIENT_SOFTWARE_VERSION}=11.1.*")
 
   def getDefaultProperties: Properties = {
     val props = new Properties()
-    props.put(ClientMetricsConfig.ClientMetrics.SubscriptionMetrics, defaultMetrics)
-    props.put(ClientMetricsConfig.ClientMetrics.ClientMatchPattern, defaultClientMatchPatters.mkString(","))
-    props.put(ClientMetricsConfig.ClientMetrics.PushIntervalMs, defaultPushIntervalMs.toString)
+    props.put(ClientMetricsConfig.ClientMetrics.SubscriptionMetrics, DefaultMetrics)
+    props.put(ClientMetricsConfig.ClientMetrics.ClientMatchPattern, DefaultClientMatchPatters.mkString(","))
+    props.put(ClientMetricsConfig.ClientMetrics.PushIntervalMs, DefaultPushIntervalMs.toString)
     props
   }
 }
