c7eae56dfa17746bea242b81027208164ba66ae7
Matthias J. Sax <matthias@confluent.io>
2023-10-30 13:22:03 -0700

    HOTFIX: remove unused import to fix checkstyle error

--- clients/src/test/java/org/apache/kafka/common/serialization/SerializationTest.java
+++ clients/src/test/java/org/apache/kafka/common/serialization/SerializationTest.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Stack;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
