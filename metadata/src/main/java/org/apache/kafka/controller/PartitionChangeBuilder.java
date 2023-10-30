c67dd31e37e9fc374b4da72ea5c9d2086a6807a2
CalvinConfluent <caliu@confluent.io>
2023-10-30 13:40:38 -0700

    Minor

--- metadata/src/main/java/org/apache/kafka/controller/PartitionChangeBuilder.java
+++ metadata/src/main/java/org/apache/kafka/controller/PartitionChangeBuilder.java
@@ -483,7 +483,7 @@ public class PartitionChangeBuilder {
             record.setEligibleLeaderReplicas(targetElr);
         }
 
-        if (!targetLastKnownElr.equals(Replicas.toList(partition.lastKnownElr))) {
+        if (!useLastKnownLeaderInBalancedRecovery && !targetLastKnownElr.equals(Replicas.toList(partition.lastKnownElr))) {
             record.setLastKnownELR(targetLastKnownElr);
         }
     }
