f62803190c09915cab83afe4791c2d8220b3cfdc
Kirk True <kirk@kirktrue.pro>
2023-11-02 13:08:40 -0700

    Removed unnecessary import statement

--- clients/src/test/java/org/apache/kafka/clients/consumer/ConsumerConfigTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/ConsumerConfigTest.java
@@ -39,7 +39,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class ConsumerConfigTest {
 
