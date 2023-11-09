4de9c2fe07ff36ded313fc45e547373d46421cfd
David Arthur <mumrah@gmail.com>
2023-11-09 10:13:09 -0500

    MINOR Bump master to 7.7.0-0 (#979)

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -122,7 +122,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("7.6.0-0")
+DEV_VERSION = KafkaVersion("7.7.0-0")
 
 LATEST_METADATA_VERSION = "3.6"
 
