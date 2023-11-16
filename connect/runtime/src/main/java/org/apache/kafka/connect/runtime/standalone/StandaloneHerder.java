f20d993ca5fe800a87d670a7cd16bd7579590088
David Jameson <david.mck.jameson@gmail.com>
2023-11-16 15:19:41 +1300

    KAFKA-15821: Delete active topics after connector deletion in standalone mode

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/standalone/StandaloneHerder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/standalone/StandaloneHerder.java
@@ -515,6 +515,7 @@ public class StandaloneHerder extends AbstractHerder {
             synchronized (StandaloneHerder.this) {
                 configState = configBackingStore.snapshot();
             }
+            resetConnectorActiveTopics(connector);
         }
 
         @Override
