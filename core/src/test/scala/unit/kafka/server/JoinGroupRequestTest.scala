dd1157679933788e7ecb87677c049e8769a58215
dongnuolyu <dlyu@confluent.io>
2023-11-16 10:12:05 -0500

    leader and follower example

--- core/src/test/scala/unit/kafka/server/JoinGroupRequestTest.scala
+++ core/src/test/scala/unit/kafka/server/JoinGroupRequestTest.scala
@@ -19,9 +19,16 @@ package kafka.server
 import kafka.test.ClusterInstance
 import kafka.test.annotation.{ClusterConfigProperty, ClusterTest, ClusterTestDefaults, Type}
 import kafka.test.junit.ClusterTestExtensions
-import org.apache.kafka.common.protocol.{ApiKeys, Errors}
+import org.apache.kafka.clients.consumer.ConsumerPartitionAssignor
+import org.apache.kafka.clients.consumer.internals.ConsumerProtocol
+import org.apache.kafka.common.message.SyncGroupRequestData
 import org.junit.jupiter.api.{Tag, Timeout}
 import org.junit.jupiter.api.extension.ExtendWith
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.Collections
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 @Timeout(120)
 @ExtendWith(value = Array(classOf[ClusterTestExtensions]))
@@ -48,7 +55,6 @@ class JoinGroupRequestTest(cluster: ClusterInstance) extends GroupCoordinatorBas
     testJoinGroup()
   }
 
-
   private def testJoinGroup(): Unit = {
     // Creates the __consumer_offsets topics because it won't be created automatically
     // in this test because it does not use FindCoordinator API.
@@ -60,8 +66,70 @@ class JoinGroupRequestTest(cluster: ClusterInstance) extends GroupCoordinatorBas
       numPartitions = 3
     )
 
-    for (version <- ApiKeys.JOIN_GROUP.oldestVersion() to ApiKeys.JOIN_GROUP.latestVersion(isUnstableApiEnabled)) {
+    //    for (version <- ApiKeys.JOIN_GROUP.oldestVersion() to ApiKeys.JOIN_GROUP.latestVersion(isUnstableApiEnabled)) {
 
+    // Invalid group id.
+    //      val joinGroupResponseData = sendJoinRequest(groupId = "")
+    //      assertEquals(Errors.INVALID_GROUP_ID.code, joinGroupResponseData.errorCode)
+
+    val metadata = ConsumerProtocol.serializeSubscription(new ConsumerPartitionAssignor.Subscription(Collections.singletonList("foo"))).array
+
+    val joinGroupResponseData1 = sendJoinRequest(
+      groupId = "grp",
+      metadata = metadata
+    )
+
+    // Rejoin the group with the member id.
+    val rejoinGroupResponseData1 = sendJoinRequest(
+      groupId = "grp",
+      memberId = joinGroupResponseData1.memberId,
+      metadata = metadata
+    )
+
+    var assignments = List(
+      new SyncGroupRequestData.SyncGroupRequestAssignment().setMemberId(rejoinGroupResponseData1.memberId).setAssignment(Array[Byte](1))
+    )
+    syncGroupWithOldProtocol(
+      groupId = "grp",
+      memberId = rejoinGroupResponseData1.memberId,
+      generationId = rejoinGroupResponseData1.generationId,
+      assignments = assignments
+    )
+
+    // join the second member
+    val joinGroupResponseData2 = sendJoinRequest(
+      groupId = "grp",
+      metadata = metadata
+    )
+
+    val future2 = Future {
+      sendJoinRequest("grp", joinGroupResponseData2.memberId, metadata = metadata)
     }
+    val future3 = Future {
+      sendJoinRequest("grp", rejoinGroupResponseData1.memberId, metadata = metadata)
+    }
+    val rejoinGroupResponseData2 = Await.result(future2, Duration.Inf)
+    val rejoinGroupResponseData3 = Await.result(future3, Duration.Inf)
+
+
+    assignments = List(
+      new SyncGroupRequestData.SyncGroupRequestAssignment().setMemberId(rejoinGroupResponseData1.memberId).setAssignment(Array[Byte](1)),
+      new SyncGroupRequestData.SyncGroupRequestAssignment().setMemberId(rejoinGroupResponseData2.memberId).setAssignment(Array[Byte](2))
+    )
+    syncGroupWithOldProtocol(
+      groupId = "grp",
+      memberId = rejoinGroupResponseData1.memberId,
+      generationId = rejoinGroupResponseData3.generationId,
+      assignments = assignments
+    )
+    syncGroupWithOldProtocol(
+      groupId = "grp",
+      memberId = rejoinGroupResponseData2.memberId,
+      generationId = rejoinGroupResponseData2.generationId
+    )
+//    assertEquals(0, describeGroups(List("grp"), version = ApiKeys.DESCRIBE_GROUPS.latestVersion(isUnstableApiEnabled)))
+
+
+    //    }
   }
 }
