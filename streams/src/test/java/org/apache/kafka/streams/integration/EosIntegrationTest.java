9649b2be15a2d224f2a18c4aebb1a82f9b40c313
PzaThief <vwjdalsgkv@gmail.com>
2023-11-17 01:33:31 +0900

    MINOR: Fix toString method of IsolationLevel

--- streams/src/test/java/org/apache/kafka/streams/integration/EosIntegrationTest.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/EosIntegrationTest.java
@@ -88,7 +88,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -1170,7 +1169,7 @@ public class EosIntegrationTest {
                     valueDeserializer,
                     Utils.mkProperties(Collections.singletonMap(
                         ConsumerConfig.ISOLATION_LEVEL_CONFIG,
-                        IsolationLevel.READ_COMMITTED.name().toLowerCase(Locale.ROOT)))),
+                        IsolationLevel.READ_COMMITTED.toString()))),
                 topic,
                 numberOfRecords
             );
@@ -1202,7 +1201,7 @@ public class EosIntegrationTest {
                     valueDeserializer,
                     Utils.mkProperties(Collections.singletonMap(
                         ConsumerConfig.ISOLATION_LEVEL_CONFIG,
-                        IsolationLevel.READ_COMMITTED.name().toLowerCase(Locale.ROOT))
+                        IsolationLevel.READ_COMMITTED.toString())
                     )
                 ),
                 topic,
