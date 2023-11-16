14fff3a35f2c3b54c1a598fd1a9cd2d206264e5d
dongnuolyu <dlyu@confluent.io>
2023-11-16 10:46:23 -0500

    add quorum value

--- core/src/test/scala/integration/kafka/api/BaseAsyncConsumerTest.scala
+++ core/src/test/scala/integration/kafka/api/BaseAsyncConsumerTest.scala
@@ -32,7 +32,7 @@ class BaseAsyncConsumerTest extends AbstractConsumerTest {
 
   @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
   @ValueSource(strings = Array("zk", "kraft", "kraft+kip848"))
-  def testCommitAsync(): Unit = {
+  def testCommitAsync(quorum: String): Unit = {
     val props = new Properties();
     props.setProperty(ConsumerConfig.GROUP_PROTOCOL_CONFIG, GroupProtocol.CONSUMER.name());
     val consumer = createConsumer(configOverrides = props)
@@ -56,7 +56,7 @@ class BaseAsyncConsumerTest extends AbstractConsumerTest {
 
   @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
   @ValueSource(strings = Array("zk", "kraft", "kraft+kip848"))
-  def testCommitSync(): Unit = {
+  def testCommitSync(quorum: String): Unit = {
     val props = new Properties();
     props.setProperty(ConsumerConfig.GROUP_PROTOCOL_CONFIG, GroupProtocol.CONSUMER.name());
     val consumer = createConsumer(configOverrides = props)
