481e994c031205a29fd78e203894fb699cbb752f
Lucas Brutschy <lbrutschy@confluent.io>
2023-11-16 15:22:43 +0100

    updateAssignmentMetadataIfNeeded

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumer.java
@@ -1168,12 +1168,17 @@ public class AsyncKafkaConsumer<K, V> implements ConsumerDelegate<K, V> {
 
     @Override
     public boolean updateAssignmentMetadataIfNeeded(Timer timer) {
-        backgroundEventProcessor.process();
+        acquireAndEnsureOpen();
+        try {
+            backgroundEventProcessor.process();
 
-        // Keeping this updateAssignmentMetadataIfNeeded wrapping up the updateFetchPositions as
-        // in the previous implementation, because it will eventually involve group coordination
-        // logic
-        return updateFetchPositions(timer);
+            // Keeping this updateAssignmentMetadataIfNeeded wrapping up the updateFetchPositions as
+            // in the previous implementation, because it will eventually involve group coordination
+            // logic
+            return updateFetchPositions(timer);
+        } finally {
+            release();
+        }
     }
 
     @Override
