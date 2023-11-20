7657131e2222599e8340c9b67cfbb3686e2b7306
dongnuolyu <dlyu@confluent.io>
2023-11-20 11:29:46 -0500

    comments

--- core/src/test/scala/unit/kafka/server/HeartbeatRequestTest.scala
+++ core/src/test/scala/unit/kafka/server/HeartbeatRequestTest.scala
@@ -30,8 +30,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 
 import java.util.Collections
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 
 @Timeout(120)
 @ExtendWith(value = Array(classOf[ClusterTestExtensions]))
@@ -146,37 +145,33 @@ class HeartbeatRequestTest(cluster: ClusterInstance) extends GroupCoordinatorBas
         metadata = metadata
       )
 
-      val reJoinFollowerFuture = Future {
+      Future {
         sendJoinRequest(
           groupId = "grp",
           memberId = joinFollowerResponseData.memberId,
           metadata = metadata
         )
       }
-      val verifyAndRejoinLeaderFuture = Future {
-        TestUtils.waitUntilTrue(() => {
-          val described = describeGroups(groupIds = List("grp"), version = ApiKeys.DESCRIBE_GROUPS.latestVersion(isUnstableApiEnabled))
-          GenericGroupState.PREPARING_REBALANCE.toString == described.head.groupState
-        }, msg = s"The group is not in PREPARING_REBALANCE state.")
 
-        // Heartbeat PREPARING_REBALANCE group.
-        heartbeat(
-          groupId = "grp",
-          memberId = leaderMemberId,
-          generationId = leaderEpoch,
-          expectedError = Errors.REBALANCE_IN_PROGRESS,
-          version = version.toShort
-        )
-        
-        sendJoinRequest(
-          groupId = "grp",
-          memberId = leaderMemberId,
-          metadata = metadata
-        )
-      }
+      TestUtils.waitUntilTrue(() => {
+        val described = describeGroups(groupIds = List("grp"), version = ApiKeys.DESCRIBE_GROUPS.latestVersion(isUnstableApiEnabled))
+        GenericGroupState.PREPARING_REBALANCE.toString == described.head.groupState
+      }, msg = s"The group is not in PREPARING_REBALANCE state.")
 
-      Await.result(reJoinFollowerFuture, Duration.Inf)
-      Await.result(verifyAndRejoinLeaderFuture, Duration.Inf)
+      // Heartbeat PREPARING_REBALANCE group.
+      heartbeat(
+        groupId = "grp",
+        memberId = leaderMemberId,
+        generationId = leaderEpoch,
+        expectedError = Errors.REBALANCE_IN_PROGRESS,
+        version = version.toShort
+      )
+
+      sendJoinRequest(
+        groupId = "grp",
+        memberId = leaderMemberId,
+        metadata = metadata
+      )
 
       leaveGroup(
         groupId = "grp",
