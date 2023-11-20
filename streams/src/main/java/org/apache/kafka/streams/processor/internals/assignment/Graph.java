fd400f8110fa948812d101f3b879fbe4e2740242
Hao Li <1127478+lihaosky@users.noreply.github.com>
2023-11-20 10:12:53 -0800

    comment

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/assignment/Graph.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/assignment/Graph.java
@@ -108,8 +108,14 @@ public class Graph<V extends Comparable<V>> {
 
         @Override
         public int compare(final V o1, final V o2) {
-            if (o1 == null || o2 == null) {
+            if (o1 == null && o2 == null) {
+                return 0;
+            }
+            if (o1 == null) {
                 return -1;
+            }
+            if (o2 == null) {
+                return 1;
             }
             return o1.compareTo(o2);
         }
