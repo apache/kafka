f5ac59d6a1211abfdc3023263cc492509488b0e6
CalvinConfluent <caliu@confluent.io>
2023-11-21 08:43:42 -0800

    Minor

--- metadata/src/main/java/org/apache/kafka/controller/PartitionChangeBuilder.java
+++ metadata/src/main/java/org/apache/kafka/controller/PartitionChangeBuilder.java
@@ -466,7 +466,7 @@ public class PartitionChangeBuilder {
             partition.lastKnownElr[0] != partition.leader)) {
             // Only update the last known leader when the first time the partition becomes leaderless.
             record.setLastKnownELR(Arrays.asList(partition.leader));
-        } else if (record.leader() > 0 || (partition.leader != NO_LEADER && record.leader() != NO_LEADER)
+        } else if ((record.leader() > 0 || (partition.leader != NO_LEADER && record.leader() != NO_LEADER))
             && partition.lastKnownElr.length > 0) {
             // Clear the LastKnownElr field if the partition will have or continues to have a valid leader.
             record.setLastKnownELR(Collections.emptyList());
