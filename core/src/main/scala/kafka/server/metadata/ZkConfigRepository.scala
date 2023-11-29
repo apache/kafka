ae4181400c10a79a52bc6c84027806aa88491295
nizhikov <nizhikov@apache.org>
2023-11-29 13:24:19 +0300

    KAFKA-14588 ConfigType enum moved to server-common

--- core/src/main/scala/kafka/server/metadata/ZkConfigRepository.scala
+++ core/src/main/scala/kafka/server/metadata/ZkConfigRepository.scala
@@ -18,11 +18,11 @@
 package kafka.server.metadata
 
 import java.util.Properties
-
-import kafka.server.{ConfigEntityName, ConfigType}
+import kafka.server.ConfigEntityName
 import kafka.zk.{AdminZkClient, KafkaZkClient}
 import org.apache.kafka.common.config.ConfigResource
 import org.apache.kafka.common.config.ConfigResource.Type
+import org.apache.kafka.server.config.ConfigType
 
 
 object ZkConfigRepository {
@@ -33,8 +33,8 @@ object ZkConfigRepository {
 class ZkConfigRepository(adminZkClient: AdminZkClient) extends ConfigRepository {
   override def config(configResource: ConfigResource): Properties = {
     val configTypeForZk = configResource.`type` match {
-      case Type.TOPIC => ConfigType.Topic
-      case Type.BROKER => ConfigType.Broker
+      case Type.TOPIC => ConfigType.TOPIC
+      case Type.BROKER => ConfigType.BROKER
       case tpe => throw new IllegalArgumentException(s"Unsupported config type: $tpe")
     }
     // ZK stores cluster configs under "<default>".
--- server-common/src/main/java/org/apache/kafka/server/config/ConfigType.java
+++ server-common/src/main/java/org/apache/kafka/server/config/ConfigType.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,9 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.kafka.server.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Represents all the entities that can be configured via ZK
+ */
 public class ConfigType {
     public static final String TOPIC = "topics";
     public static final String CLIENT = "clients";
@@ -24,4 +29,6 @@ public class ConfigType {
     public static final String BROKER = "brokers";
     public static final String IP = "ips";
     public static final String CLIENT_METRICS = "client-metrics";
+
+    public static final List<String> ALL = Arrays.asList(TOPIC, CLIENT, USER, BROKER, IP, CLIENT_METRICS);
 }
