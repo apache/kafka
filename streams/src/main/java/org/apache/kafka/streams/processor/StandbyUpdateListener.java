a92006b8c3b518b7741d2227a52b7d51d27d4b84
Eduwer Camacaro <eduwerc@gmail.com>
2023-12-01 07:47:10 -0500

    Literal <n>  for StandbyUpdateListener javadoc
    Co-authored-by: A. Sophie Blee-Goldman <ableegoldman@gmail.com>

--- streams/src/main/java/org/apache/kafka/streams/processor/StandbyUpdateListener.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/StandbyUpdateListener.java
@@ -39,7 +39,7 @@ public interface StandbyUpdateListener {
     /**
      * Method called after restoring a batch of records. In this case the maximum size of the batch is whatever
      * the value of the MAX_POLL_RECORDS is set to.
-     *
+     * <n>
      * This method is called after restoring each batch and it is advised to keep processing to a minimum.
      * Any heavy processing will hold up recovering the next batch, hence slowing down the restore process as a
      * whole.
