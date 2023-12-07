676fd4bf1ac3ad80722b867a265d02469a7b83a6
jfaulkner <jfaulkner@expediagroup.com>
2023-12-07 09:47:51 +0000

    Bugfix: Prevent java.lang.UnsupportedOperationException.

--- clients/src/test/java/org/apache/kafka/clients/admin/MockAdminClient.java
+++ clients/src/test/java/org/apache/kafka/clients/admin/MockAdminClient.java
@@ -1014,7 +1014,9 @@ public class MockAdminClient extends AdminClient {
                 for (Node node : nodes) {
                     Map<String, LogDirDescription> logDirDescriptionMap = unwrappedResults.get(node.id());
                     LogDirDescription logDirDescription = logDirDescriptionMap.getOrDefault(partitionLogDirs.get(0), new LogDirDescription(null, new HashMap<>()));
-                    logDirDescription.replicaInfos().put(new TopicPartition(topicName, topicPartitionInfo.partition()), new ReplicaInfo(0, 0, false));
+                    Map<TopicPartition, ReplicaInfo> topicPartitionReplicaInfoMap = new HashMap<>(logDirDescription.replicaInfos());
+                    topicPartitionReplicaInfoMap.put(new TopicPartition(topicName, topicPartitionInfo.partition()), new ReplicaInfo(0, 0, false));
+                    logDirDescriptionMap.put(partitionLogDirs.get(0), new LogDirDescription(logDirDescription.error(), topicPartitionReplicaInfoMap));
                 }
             }
         }
