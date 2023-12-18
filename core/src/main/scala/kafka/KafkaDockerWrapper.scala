f24fd5c54276416951aa679bf0ec3983053d5884
Vedarth Sharma <vesharma@confluent.io>
2023-12-18 19:50:37 +0530

    Migrate all file operations to scala

--- core/src/main/scala/kafka/KafkaDockerWrapper.scala
+++ core/src/main/scala/kafka/KafkaDockerWrapper.scala
@@ -33,7 +33,8 @@ object KafkaDockerWrapper {
       case "setup" =>
         val defaultConfigsDir = arguments(0)
         val realConfigsDir = arguments(1)
-        prepareConfigs(defaultConfigsDir, realConfigsDir)
+        val mountedDir = arguments(2)
+        prepareConfigs(defaultConfigsDir, realConfigsDir, mountedDir)
 
         val formatCmd = formatStorageCmd(realConfigsDir)
         StorageTool.main(formatCmd)
@@ -49,13 +50,13 @@ object KafkaDockerWrapper {
     Array("format", "--cluster-id=" + sys.env.get("CLUSTER_ID"), "-c", s"$configsDir/server.properties")
   }
 
-  private def prepareConfigs(defaultConfigsDir: String, realConfigsDir: String): Unit = {
-    prepareServerConfigs(defaultConfigsDir, realConfigsDir)
-    prepareLog4jConfigs(realConfigsDir)
-    prepareToolsLog4jConfigs(realConfigsDir)
+  private def prepareConfigs(defaultConfigsDir: String, realConfigsDir: String, mountedDir: String): Unit = {
+    prepareServerConfigs(defaultConfigsDir, realConfigsDir, mountedDir)
+    prepareLog4jConfigs(defaultConfigsDir, realConfigsDir, mountedDir)
+    prepareToolsLog4jConfigs(defaultConfigsDir, realConfigsDir, mountedDir)
   }
 
-  private def prepareServerConfigs(defaultConfigsDir: String, realConfigsDir: String): Unit = {
+  private def prepareServerConfigs(defaultConfigsDir: String, realConfigsDir: String, mountedDir: String): Unit = {
     val serverPropsList = sys.env.map {
       case (key, value) =>
         if (key.startsWith("KAFKA_") && !ExcludeServerPropsEnv.contains(key)) {
@@ -69,19 +70,32 @@ object KafkaDockerWrapper {
         }
     }.toList
 
-    val realFile = s"$realConfigsDir/$ServerPropsFilename"
-    val defaultFile = s"$defaultConfigsDir/$ServerPropsFilename"
-    val propsToAdd = "\n" + serverPropsList.mkString("\n")
-    appendToFile(propsToAdd, realFile)
+    val realFilePath = s"$realConfigsDir/$ServerPropsFilename"
+    val defaultFilePath = s"$defaultConfigsDir/$ServerPropsFilename"
+    val mountedFilePath = s"$mountedDir/$ServerPropsFilename"
 
-    val source = scala.io.Source.fromFile(realFile)
+    val propsToAdd = "\n" + serverPropsList.mkString("\n")
+    if (Files.exists(Paths.get(mountedFilePath))) {
+      copyFile(mountedFilePath, realFilePath)
+      addToFile(propsToAdd, realFilePath)
+    } else {
+        addToFile(propsToAdd, realFilePath, StandardOpenOption.TRUNCATE_EXISTING)
+    }
+
+    val source = scala.io.Source.fromFile(realFilePath)
     val data = try source.mkString finally source.close()
     if (data.trim.isEmpty) {
-      Files.copy(Paths.get(defaultFile), Paths.get(realFile), StandardCopyOption.REPLACE_EXISTING)
+      copyFile(defaultFilePath, realFilePath)
     }
   }
 
-  private def prepareLog4jConfigs(configsDir: String): Unit = {
+  private def copyFile(source: String, destination: String) = {
+    if (Files.exists(Paths.get(source))) {
+      Files.copy(Paths.get(source), Paths.get(destination), StandardCopyOption.REPLACE_EXISTING)
+    }
+  }
+
+  private def prepareLog4jConfigs(defaultConfigsDir: String, configsDir: String, mountedDir: String): Unit = {
     val kafkaLog4jRootLogLevelProp = sys.env.get(KafkaLog4jRootLoglevelEnv)
       .map(kafkaLog4jRootLogLevel => s"log4j.rootLogger=$kafkaLog4jRootLogLevel, stdout")
       .getOrElse("")
@@ -90,25 +104,37 @@ object KafkaDockerWrapper {
       .map(kafkaLog4JLoggersString => kafkaLog4JLoggersString.split(",").mkString("\n"))
       .getOrElse("")
 
+    val realFilePath = s"$configsDir/$Log4jPropsFilename"
+    val mountedFilePath = s"$mountedDir/$Log4jPropsFilename"
+    val defaultFilePath = s"$defaultConfigsDir/$Log4jPropsFilename"
+
+    copyFile(defaultFilePath, realFilePath)
+    copyFile(mountedFilePath, realFilePath)
+
     val propsToAdd = "\n" + kafkaLog4jRootLogLevelProp + "\n" + kafkaLog4jLoggersProp
-    val filepath = s"$configsDir/$Log4jPropsFilename"
-    appendToFile(propsToAdd, filepath)
+    addToFile(propsToAdd, realFilePath)
   }
 
-  private def prepareToolsLog4jConfigs(configsDir: String): Unit = {
+  private def prepareToolsLog4jConfigs(defaultConfigsDir: String, configsDir: String, mountedDir: String): Unit = {
+    val realFilePath = s"$configsDir/$ToolsLog4jFilename"
+    val mountedFilePath = s"$mountedDir/$ToolsLog4jFilename"
+    val defaultFilePath = s"$defaultConfigsDir/$ToolsLog4jFilename"
+
+    copyFile(defaultFilePath, realFilePath)
+    copyFile(mountedFilePath, realFilePath)
+
     sys.env.get(KafkaToolsLog4jLoglevelEnv).foreach { kafkaToolsLog4jLogLevel =>
       val propToAdd = "\n" + s"log4j.rootLogger=$kafkaToolsLog4jLogLevel, stderr"
-      val filepath = s"$configsDir/$ToolsLog4jFilename"
-      appendToFile(propToAdd, filepath)
+      addToFile(propToAdd, realFilePath)
     }
   }
 
-  private def appendToFile(properties: String, filepath: String): Unit = {
+  private def addToFile(properties: String, filepath: String, option: StandardOpenOption = StandardOpenOption.APPEND): Unit = {
     val path = Paths.get(filepath)
     if (!Files.exists(path)) {
       Files.createFile(path)
     }
-    Files.write(Paths.get(filepath), properties.getBytes, StandardOpenOption.APPEND)
+    Files.write(Paths.get(filepath), properties.getBytes, option)
   }
 }
 
