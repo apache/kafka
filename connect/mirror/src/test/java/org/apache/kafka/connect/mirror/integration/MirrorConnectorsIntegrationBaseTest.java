ee044bb5f0909a27f4950b9fc6f913ca9383a70c
atusharm <atusharm@visa.com>
2023-12-21 12:15:03 +0530

    Adding lock for same topic names

--- connect/mirror/src/test/java/org/apache/kafka/connect/mirror/integration/MirrorConnectorsIntegrationBaseTest.java
+++ connect/mirror/src/test/java/org/apache/kafka/connect/mirror/integration/MirrorConnectorsIntegrationBaseTest.java
@@ -101,6 +101,7 @@ import org.junit.jupiter.api.BeforeEach;
  */
 @Tag("integration")
 public class MirrorConnectorsIntegrationBaseTest {
+    private static final Object TOPIC_LOCK = new Object();
     private static final Logger log = LoggerFactory.getLogger(MirrorConnectorsIntegrationBaseTest.class);
 
     protected static final int NUM_RECORDS_PER_PARTITION = 10;
@@ -748,133 +749,139 @@ public class MirrorConnectorsIntegrationBaseTest {
 
     @Test
     public void testSyncTopicConfigs() throws InterruptedException {
-        mm2Config = new MirrorMakerConfig(mm2Props);
+        synchronized (TOPIC_LOCK) {
+            mm2Config = new MirrorMakerConfig(mm2Props);
 
-        waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
+            waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
 
-        // create topic with configuration to test:
-        final Map<String, String> topicConfig = new HashMap<>();
-        topicConfig.put("delete.retention.ms", "1000"); // should be excluded (default value is 86400000)
-        topicConfig.put("retention.bytes", "1000"); // should be included, default value is -1
+            // create topic with configuration to test:
+            final Map<String, String> topicConfig = new HashMap<>();
+            topicConfig.put("delete.retention.ms", "1000"); // should be excluded (default value is 86400000)
+            topicConfig.put("retention.bytes", "1000"); // should be included, default value is -1
 
-        final String topic = "test-topic-with-config-new";
-        final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
+            final String topic = "test-topic-with-config-new";
+            final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
 
-        primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
-        waitForTopicCreated(backup, backupTopic);
+            primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
+            waitForTopicCreated(backup, backupTopic);
 
-        // alter configs on the target topic
-        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, backupTopic);
-        Collection<AlterConfigOp> ops = new ArrayList<>();
-        ops.add(new AlterConfigOp(new ConfigEntry("delete.retention.ms", "2000"), AlterConfigOp.OpType.SET));
-        ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "2000"), AlterConfigOp.OpType.SET));
-        Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
-        // alter configs on target cluster
-        backup.kafka().incrementalAlterConfigs(configOps);
-        // wait until the configs are changed
-        //waitForConfigValueChange(backup, backupTopic, "delete.retention.ms", "2000");
+            // alter configs on the target topic
+            ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, backupTopic);
+            Collection<AlterConfigOp> ops = new ArrayList<>();
+            ops.add(new AlterConfigOp(new ConfigEntry("delete.retention.ms", "2000"), AlterConfigOp.OpType.SET));
+            ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "2000"), AlterConfigOp.OpType.SET));
+            Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
+            // alter configs on target cluster
+            backup.kafka().incrementalAlterConfigs(configOps);
+            // wait until the configs are changed
+            //waitForConfigValueChange(backup, backupTopic, "delete.retention.ms", "2000");
 
-        waitForCondition(() -> {
-            String primaryConfig, backupConfig;
-            primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
-            assertNotEquals(primaryConfig, backupConfig,
-                    "`delete.retention.ms` should be different, because it's in exclude filter! ");
-            assertEquals("2000", backupConfig, "`delete.retention.ms` should be 2000, because it's explicitly defined on the target topic! ");
+            waitForCondition(() -> {
+                String primaryConfig, backupConfig;
+                primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
+                assertNotEquals(primaryConfig, backupConfig,
+                        "`delete.retention.ms` should be different, because it's in exclude filter! ");
+                assertEquals("2000", backupConfig, "`delete.retention.ms` should be 2000, because it's explicitly defined on the target topic! ");
 
-            // regression test for the config that are still supposed to be replicated
-            primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
-            assertEquals(primaryConfig, backupConfig,
-                    "`retention.bytes` should be the same, because it isn't in exclude filter! ");
-            return true;
-        }, 30000, "Topic configurations were not synced");
+                // regression test for the config that are still supposed to be replicated
+                primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
+                assertEquals(primaryConfig, backupConfig,
+                        "`retention.bytes` should be the same, because it isn't in exclude filter! ");
+                return true;
+            }, 30000, "Topic configurations were not synced");
+        }
     }
 
     @Test
     public void testReplicateSourceDefault() throws Exception {
-        mm2Props.put(DefaultConfigPropertyFilter.USE_DEFAULTS_FROM, "source");
-        mm2Config = new MirrorMakerConfig(mm2Props);
+        synchronized (TOPIC_LOCK) {
+            mm2Props.put(DefaultConfigPropertyFilter.USE_DEFAULTS_FROM, "source");
+            mm2Config = new MirrorMakerConfig(mm2Props);
 
-        waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
+            waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
 
-        // create topic with default configurations to test
-        final String topic = "test-topic-with-config";
-        final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
+            // create topic with default configurations to test
+            final String topic = "test-topic-with-config";
+            final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
 
-        primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, new HashMap<>());
-        waitForTopicCreated(backup, backupTopic);
+            primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, new HashMap<>());
+            waitForTopicCreated(backup, backupTopic);
 
-        // alter target topic configurations
-        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, backupTopic);
-        Collection<AlterConfigOp> ops = new ArrayList<>();
-        ops.add(new AlterConfigOp(new ConfigEntry("delete.retention.ms", "2000"), AlterConfigOp.OpType.SET));
-        ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "2000"), AlterConfigOp.OpType.SET));
-        Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
-        backup.kafka().incrementalAlterConfigs(configOps);
+            // alter target topic configurations
+            ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, backupTopic);
+            Collection<AlterConfigOp> ops = new ArrayList<>();
+            ops.add(new AlterConfigOp(new ConfigEntry("delete.retention.ms", "2000"), AlterConfigOp.OpType.SET));
+            ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "2000"), AlterConfigOp.OpType.SET));
+            Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
+            backup.kafka().incrementalAlterConfigs(configOps);
 
-        waitForCondition(() -> {
-            String primaryConfig, backupConfig;
-            // altered configuration of the target topic should be synced with the source cluster's default
-            primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
-            assertEquals(primaryConfig, backupConfig,
-                    "`retention.bytes` should be the same, because the source cluster default is being used! ");
-            assertEquals("-1", backupConfig,
-                    "`retention.bytes` should be synced with default value!");
+            waitForCondition(() -> {
+                String primaryConfig, backupConfig;
+                // altered configuration of the target topic should be synced with the source cluster's default
+                primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
+                assertEquals(primaryConfig, backupConfig,
+                        "`retention.bytes` should be the same, because the source cluster default is being used! ");
+                assertEquals("-1", backupConfig,
+                        "`retention.bytes` should be synced with default value!");
 
-            // when using the source cluster's default, the excluded configuration should still not be changed
-            primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
-            assertNotEquals(primaryConfig, backupConfig,
-                    "`delete.retention.ms` should be different, because it's in exclude filter! ");
-            assertEquals("2000", backupConfig, "`delete.retention.ms` should be 2000, because it's explicitly defined on the target topic! ");
-            return true;
-        }, 30000, "Topic configurations were not synced");
+                // when using the source cluster's default, the excluded configuration should still not be changed
+                primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
+                assertNotEquals(primaryConfig, backupConfig,
+                        "`delete.retention.ms` should be different, because it's in exclude filter! ");
+                assertEquals("2000", backupConfig, "`delete.retention.ms` should be 2000, because it's explicitly defined on the target topic! ");
+                return true;
+            }, 30000, "Topic configurations were not synced");
+        }
     }
 
     @Test
     public void testReplicateTargetDefault() throws Exception {
-        mm2Config = new MirrorMakerConfig(mm2Props);
+        synchronized (TOPIC_LOCK) {
+            mm2Config = new MirrorMakerConfig(mm2Props);
 
-        waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
+            waitUntilMirrorMakerIsRunning(backup, CONNECTOR_LIST, mm2Config, PRIMARY_CLUSTER_ALIAS, BACKUP_CLUSTER_ALIAS);
 
-        // create topic with configuration to test:
-        final Map<String, String> topicConfig = new HashMap<>();
-        topicConfig.put("retention.bytes", "1000");
+            // create topic with configuration to test:
+            final Map<String, String> topicConfig = new HashMap<>();
+            topicConfig.put("retention.bytes", "1000");
 
-        final String topic = "test-topic-with-config";
-        final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
+            final String topic = "test-topic-with-config";
+            final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
 
-        primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
-        waitForTopicCreated(backup, backupTopic);
+            primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
+            waitForTopicCreated(backup, backupTopic);
 
-        waitForCondition(() -> {
-            String primaryConfig, backupConfig;
-            primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
-            assertEquals(primaryConfig, backupConfig,
-                    "`retention.bytes` should be the same");
-            assertEquals("1000", backupConfig,
-                    "`retention.bytes` should be synced with default value!");
-            return true;
-        }, 30000, "Topic configurations were not synced");
+            waitForCondition(() -> {
+                String primaryConfig, backupConfig;
+                primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
+                assertEquals(primaryConfig, backupConfig,
+                        "`retention.bytes` should be the same");
+                assertEquals("1000", backupConfig,
+                        "`retention.bytes` should be synced with default value!");
+                return true;
+            }, 30000, "Topic configurations were not synced");
 
-        // delete the previously altered configuration of the source topic
-        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, topic);
-        Collection<AlterConfigOp> ops = new ArrayList<>();
-        ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "1000"), AlterConfigOp.OpType.DELETE));
-        Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
-        primary.kafka().incrementalAlterConfigs(configOps);
+            // delete the previously altered configuration of the source topic
+            ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, topic);
+            Collection<AlterConfigOp> ops = new ArrayList<>();
+            ops.add(new AlterConfigOp(new ConfigEntry("retention.bytes", "1000"), AlterConfigOp.OpType.DELETE));
+            Map<ConfigResource, Collection<AlterConfigOp>> configOps = Collections.singletonMap(configResource, ops);
+            primary.kafka().incrementalAlterConfigs(configOps);
 
-        waitForCondition(() -> {
-            String backupConfig;
-            // the configuration on the target topic should be changed to the target cluster's default
-            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
-            assertEquals("-1", backupConfig,
-                    "`retention.bytes` should be synced with target's default value!");
-            return true;
-        }, 30000, "Topic configurations were not synced");
+            waitForCondition(() -> {
+                String backupConfig;
+                // the configuration on the target topic should be changed to the target cluster's default
+                backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
+                assertEquals("-1", backupConfig,
+                        "`retention.bytes` should be synced with target's default value!");
+                return true;
+            }, 30000, "Topic configurations were not synced");
+        }
     }
 
     private TopicPartition remoteTopicPartition(TopicPartition tp, String alias) {
@@ -886,30 +893,32 @@ public class MirrorConnectorsIntegrationBaseTest {
      */
     void createAndTestNewTopicWithConfigFilter() throws Exception {
         // create topic with configuration to test:
-        final Map<String, String> topicConfig = new HashMap<>();
-        topicConfig.put("delete.retention.ms", "1000"); // should be excluded (default value is 86400000)
-        topicConfig.put("retention.bytes", "1000"); // should be included, default value is -1
+        synchronized (TOPIC_LOCK) {
+            final Map<String, String> topicConfig = new HashMap<>();
+            topicConfig.put("delete.retention.ms", "1000"); // should be excluded (default value is 86400000)
+            topicConfig.put("retention.bytes", "1000"); // should be included, default value is -1
 
-        final String topic = "test-topic-with-config";
-        final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
+            final String topic = "test-topic-with-config";
+            final String backupTopic = remoteTopicName(topic, PRIMARY_CLUSTER_ALIAS);
 
-        primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
-        waitForTopicCreated(backup, backupTopic);
+            primary.kafka().createTopic(topic, NUM_PARTITIONS, 1, topicConfig);
+            waitForTopicCreated(backup, backupTopic);
 
-        String primaryConfig, backupConfig;
+            String primaryConfig, backupConfig;
 
-        primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
-        backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
-        assertNotEquals(primaryConfig, backupConfig,
-                "`delete.retention.ms` should be different, because it's in exclude filter! ");
+            primaryConfig = getTopicConfig(primary.kafka(), topic, "delete.retention.ms");
+            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "delete.retention.ms");
+            assertNotEquals(primaryConfig, backupConfig,
+                    "`delete.retention.ms` should be different, because it's in exclude filter! ");
 
-        // regression test for the config that are still supposed to be replicated
-        primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
-        backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
-        assertEquals(primaryConfig, backupConfig,
-                "`retention.bytes` should be the same, because it isn't in exclude filter! ");
-        assertEquals("1000", backupConfig,
-                "`retention.bytes` should be the same, because it's explicitly defined! ");
+            // regression test for the config that are still supposed to be replicated
+            primaryConfig = getTopicConfig(primary.kafka(), topic, "retention.bytes");
+            backupConfig = getTopicConfig(backup.kafka(), backupTopic, "retention.bytes");
+            assertEquals(primaryConfig, backupConfig,
+                    "`retention.bytes` should be the same, because it isn't in exclude filter! ");
+            assertEquals("1000", backupConfig,
+                    "`retention.bytes` should be the same, because it's explicitly defined! ");
+        }
     }
 
     /*
