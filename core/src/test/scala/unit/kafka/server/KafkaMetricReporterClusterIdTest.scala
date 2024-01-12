3439ab18cb2cf552879668eb29fa3a6c4a7fd300
Chirag Wadhwa <cwadhwa@confluent.io>
2024-01-12 11:47:22 +0530

    Adding support for Kraft in test KafkaMetricReporterClusterIdTest

--- core/src/test/scala/unit/kafka/server/KafkaMetricReporterClusterIdTest.scala
+++ core/src/test/scala/unit/kafka/server/KafkaMetricReporterClusterIdTest.scala
@@ -1,31 +1,36 @@
 /**
-  * Licensed to the Apache Software Foundation (ASF) under one or more
-  * contributor license agreements.  See the NOTICE file distributed with
-  * this work for additional information regarding copyright ownership.
-  * The ASF licenses this file to You under the Apache License, Version 2.0
-  * (the "License"); you may not use this file except in compliance with
-  * the License.  You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kafka.server
 
-import java.util.concurrent.atomic.AtomicReference
+import kafka.integration.KafkaServerTestHarness
 
+import java.util.concurrent.atomic.AtomicReference
 import kafka.metrics.KafkaMetricsReporter
-import kafka.utils.{CoreUtils, TestUtils, VerifiableProperties}
-import kafka.server.QuorumTestHarness
+import kafka.utils.{TestUtils, VerifiableProperties}
 import org.apache.kafka.common.{ClusterResource, ClusterResourceListener}
 import org.apache.kafka.test.MockMetricsReporter
 import org.junit.jupiter.api.Assertions._
-import org.junit.jupiter.api.{AfterEach, BeforeEach, Test, TestInfo}
 import org.apache.kafka.test.TestUtils.isValidClusterId
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kafka.utils.TestInfoUtils
+
+import java.util.Properties
+import scala.collection.Seq
 
 object KafkaMetricReporterClusterIdTest {
   val setupError = new AtomicReference[String]("")
@@ -75,44 +80,34 @@ object KafkaMetricReporterClusterIdTest {
   }
 }
 
-class KafkaMetricReporterClusterIdTest extends QuorumTestHarness {
-  var server: KafkaServer = _
-  var config: KafkaConfig = _
+class KafkaMetricReporterClusterIdTest extends KafkaServerTestHarness {
 
-  @BeforeEach
-  override def setUp(testInfo: TestInfo): Unit = {
-    super.setUp(testInfo)
-    val props = TestUtils.createBrokerConfig(1, zkConnect)
-    props.setProperty(KafkaConfig.KafkaMetricsReporterClassesProp, "kafka.server.KafkaMetricReporterClusterIdTest$MockKafkaMetricsReporter")
-    props.setProperty(KafkaConfig.MetricReporterClassesProp, "kafka.server.KafkaMetricReporterClusterIdTest$MockBrokerMetricsReporter")
-    props.setProperty(KafkaConfig.BrokerIdGenerationEnableProp, "true")
-    props.setProperty(KafkaConfig.BrokerIdProp, "-1")
-    config = KafkaConfig.fromProps(props)
-    server = new KafkaServer(config, threadNamePrefix = Option(this.getClass.getName))
-    server.startup()
+  def generateConfigs: Seq[KafkaConfig] = {
+    val overridingProps = new Properties
+    overridingProps.setProperty(KafkaConfig.KafkaMetricsReporterClassesProp, "kafka.server.KafkaMetricReporterClusterIdTest$MockKafkaMetricsReporter")
+    overridingProps.setProperty(KafkaConfig.MetricReporterClassesProp, "kafka.server.KafkaMetricReporterClusterIdTest$MockBrokerMetricsReporter")
+    overridingProps.setProperty(KafkaConfig.BrokerIdGenerationEnableProp, "true")
+    TestUtils.createBrokerConfigs(1, zkConnectOrNull).
+      map(KafkaConfig.fromProps(_, overridingProps))
   }
 
-  @Test
-  def testClusterIdPresent(): Unit = {
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk","kraft"))
+  def testClusterIdPresent(quorum: String): Unit = {
     assertEquals("", KafkaMetricReporterClusterIdTest.setupError.get())
-
-    assertNotNull(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META)
-    isValidClusterId(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META.get().clusterId())
 
     assertNotNull(KafkaMetricReporterClusterIdTest.MockBrokerMetricsReporter.CLUSTER_META)
     isValidClusterId(KafkaMetricReporterClusterIdTest.MockBrokerMetricsReporter.CLUSTER_META.get().clusterId())
 
-    assertEquals(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META.get().clusterId(),
-      KafkaMetricReporterClusterIdTest.MockBrokerMetricsReporter.CLUSTER_META.get().clusterId())
+    if(!isKRaftTest()) {
+      assertNotNull(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META)
+      isValidClusterId(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META.get().clusterId())
 
-    server.shutdown()
+      assertEquals(KafkaMetricReporterClusterIdTest.MockKafkaMetricsReporter.CLUSTER_META.get().clusterId(),
+        KafkaMetricReporterClusterIdTest.MockBrokerMetricsReporter.CLUSTER_META.get().clusterId())
+    }
+
     TestUtils.assertNoNonDaemonThreads(this.getClass.getName)
   }
 
-  @AfterEach
-  override def tearDown(): Unit = {
-    server.shutdown()
-    CoreUtils.delete(config.logDirs)
-    super.tearDown()
-  }
 }
