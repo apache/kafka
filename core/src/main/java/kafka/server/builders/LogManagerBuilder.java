374ebe6b8c4519d171834033e87a92b91b94d355
David Arthur <mumrah@gmail.com>
2024-01-16 09:16:55 -0500

    fixup LogManagerBuilder

--- core/src/main/java/kafka/server/builders/LogManagerBuilder.java
+++ core/src/main/java/kafka/server/builders/LogManagerBuilder.java
@@ -35,7 +35,6 @@ import java.util.List;
 
 
 public class LogManagerBuilder {
-    private static final int PRODUCER_ID_EXPIRATION_CHECK_INTERVAL_MS = 600000;
     private List<File> logDirs = null;
     private List<File> initialOfflineDirs = Collections.emptyList();
     private ConfigRepository configRepository = null;
@@ -173,7 +172,7 @@ public class LogManagerBuilder {
                               retentionCheckMs,
                               maxTransactionTimeoutMs,
                               producerStateManagerConfig,
-                              PRODUCER_ID_EXPIRATION_CHECK_INTERVAL_MS,
+                              producerIdExpirationCheckIntervalMs,
                               interBrokerProtocolVersion,
                               scheduler,
                               brokerTopicStats,
