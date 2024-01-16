e4154f60b005940d38138c2075ef8827b7f3910c
David Arthur <mumrah@gmail.com>
2024-01-16 09:12:56 -0500

    fixup MetadataRequestBenchmark

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/metadata/MetadataRequestBenchmark.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/metadata/MetadataRequestBenchmark.java
@@ -96,29 +96,6 @@ import java.util.stream.IntStream;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 
 public class MetadataRequestBenchmark {
-    private final RequestChannel requestChannel = Mockito.mock(RequestChannel.class, Mockito.withSettings().stubOnly());
-    private final RequestChannel.Metrics requestChannelMetrics = Mockito.mock(RequestChannel.Metrics.class);
-    private final ReplicaManager replicaManager = Mockito.mock(ReplicaManager.class);
-    private final GroupCoordinator groupCoordinator = Mockito.mock(GroupCoordinator.class);
-    private final ZkAdminManager adminManager = Mockito.mock(ZkAdminManager.class);
-    private final TransactionCoordinator transactionCoordinator = Mockito.mock(TransactionCoordinator.class);
-    private final KafkaController kafkaController = Mockito.mock(KafkaController.class);
-    private final AutoTopicCreationManager autoTopicCreationManager = Mockito.mock(AutoTopicCreationManager.class);
-    private final KafkaZkClient kafkaZkClient = Mockito.mock(KafkaZkClient.class);
-    private final Metrics metrics = new Metrics();
-    private final int brokerId = 1;
-    private final ZkMetadataCache metadataCache = MetadataCache.zkMetadataCache(brokerId,
-        MetadataVersion.latestTesting(), BrokerFeatures.createEmpty(), null, false);
-    private final ClientQuotaManager clientQuotaManager = Mockito.mock(ClientQuotaManager.class);
-    private final ClientRequestQuotaManager clientRequestQuotaManager = Mockito.mock(ClientRequestQuotaManager.class);
-    private final ControllerMutationQuotaManager controllerMutationQuotaManager = Mockito.mock(ControllerMutationQuotaManager.class);
-    private final ReplicationQuotaManager replicaQuotaManager = Mockito.mock(ReplicationQuotaManager.class);
-    private final QuotaFactory.QuotaManagers quotaManagers = new QuotaFactory.QuotaManagers(clientQuotaManager,
-        clientQuotaManager, clientRequestQuotaManager, controllerMutationQuotaManager, replicaQuotaManager,
-        replicaQuotaManager, replicaQuotaManager, Option.empty());
-    private final FetchManager fetchManager = Mockito.mock(FetchManager.class);
-    private final BrokerTopicStats brokerTopicStats = new BrokerTopicStats(Optional.empty());
-    private final KafkaPrincipal principal = new KafkaPrincipal(KafkaPrincipal.USER_TYPE, "test-user");
     @Param({"500", "1000", "5000"})
     private int topicCount;
     @Param({"10", "20", "50"})
