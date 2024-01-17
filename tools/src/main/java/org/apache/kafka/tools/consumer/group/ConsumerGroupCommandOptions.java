70a0a4fc691c4504d94145ee0ea63b7582b20efb
nizhikov <nizhikov@apache.org>
2024-01-18 01:54:49 +0300

    KAFKA-14589 Rename partition

--- tools/src/main/java/org/apache/kafka/tools/consumer/group/ConsumerGroupCommandOptions.java
+++ tools/src/main/java/org/apache/kafka/tools/consumer/group/ConsumerGroupCommandOptions.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.kafka.tools.consumergroup;
+package org.apache.kafka.tools.consumer.group;
 
 import joptsimple.OptionSpec;
 import org.apache.kafka.server.util.CommandDefaultOptions;
