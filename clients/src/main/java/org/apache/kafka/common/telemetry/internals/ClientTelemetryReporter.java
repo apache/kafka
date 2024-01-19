8950ed10a7a869488d7f73821f619608caa11819
Apoorv Mittal <amittal@confluent.io>
2024-01-19 07:30:09 +0530

    KAFKA-16159: MINOR - Correcting logs to debug telemetry reporter (#15228)
    Removed debug log as next time to update runs in poll loop and excessive logging happens.
    
    Reviewers: Qichao Chu <qichao@uber.com>, Philip Nee <pnee@confluent.io>, Matthias J. Sax <matthias@confluent.io>

--- clients/src/main/java/org/apache/kafka/common/telemetry/internals/ClientTelemetryReporter.java
+++ clients/src/main/java/org/apache/kafka/common/telemetry/internals/ClientTelemetryReporter.java
@@ -362,7 +362,7 @@ public class ClientTelemetryReporter implements MetricsReporter {
                     throw new IllegalStateException("Unknown telemetry state: " + localState);
             }
 
-            log.debug("For telemetry state {}, returning the value {} ms; {}", localState, timeMs, msg);
+            log.trace("For telemetry state {}, returning the value {} ms; {}", localState, timeMs, msg);
             return timeMs;
         }
 
@@ -639,6 +639,7 @@ public class ClientTelemetryReporter implements MetricsReporter {
              signal to the broker that we need to have a client instance ID assigned.
             */
             Uuid clientInstanceId = (localSubscription != null) ? localSubscription.clientInstanceId() : Uuid.ZERO_UUID;
+            log.debug("Creating telemetry subscription request with client instance id {}", clientInstanceId);
 
             lock.writeLock().lock();
             try {
@@ -667,6 +668,7 @@ public class ClientTelemetryReporter implements MetricsReporter {
                 return Optional.empty();
             }
 
+            log.debug("Creating telemetry push request with client instance id {}", localSubscription.clientInstanceId());
             /*
              Don't send a push request if we don't have the collector initialized. Re-attempt
              the push on the next interval.
