b6e485fb3ef6b421f5b553871b69235f7fec3b94
Gantigmaa Selenge <tina.selenge@gmail.com>
2024-02-09 13:19:30 +0000

    Refactor SSL/SASL admin integration tests to not use a custom authorizer. This makes the test simpler to enable KRaft. Also unclear why a custom authoriser had to be used from the commit made this change and tests seem to work without it. The initial ACLs required by the tests are created with a super admin client (replacing AclAuthorizationAdmin). IDEMPOTENT_WRITE was added to the initial ACLs created in the SASL setup, because SSL and BaseAdmin tests that being extended or extending this class do not override the configuredClusterPermissions method therefore end up not matching the expected permissions set on the cluster initially. Overriding this method does not seem neccessary otherwise.

--- core/src/test/scala/integration/kafka/api/BaseAdminIntegrationTest.scala
+++ core/src/test/scala/integration/kafka/api/BaseAdminIntegrationTest.scala
@@ -71,7 +71,7 @@ abstract class BaseAdminIntegrationTest extends IntegrationTestHarness with Logg
 
   @Test
   def testCreateDeleteTopics(): Unit = {
-    client = Admin.create(createConfig)
+    client = createAdminClient
     val topics = Seq("mytopic", "mytopic2", "mytopic3")
     val newTopics = Seq(
       new NewTopic("mytopic", Map((0: Integer) -> Seq[Integer](1, 2).asJava, (1: Integer) -> Seq[Integer](2, 0).asJava).asJava),
@@ -163,7 +163,7 @@ abstract class BaseAdminIntegrationTest extends IntegrationTestHarness with Logg
 
   @Test
   def testAuthorizedOperations(): Unit = {
-    client = Admin.create(createConfig)
+    client = createAdminClient
 
     // without includeAuthorizedOperations flag
     var result = client.describeCluster
@@ -230,6 +230,13 @@ abstract class BaseAdminIntegrationTest extends IntegrationTestHarness with Logg
       adminClientSecurityConfigs(securityProtocol, trustStoreFile, clientSaslProperties)
     securityProps.forEach { (key, value) => config.put(key.asInstanceOf[String], value) }
     config
+  }
+
+  def createAdminClient: Admin = {
+    val props = new Properties()
+    props.putAll(createConfig)
+    val client = createAdminClient(configOverrides = props)
+    client
   }
 
   def waitForTopics(client: Admin, expectedPresent: Seq[String], expectedMissing: Seq[String]): Unit = {
