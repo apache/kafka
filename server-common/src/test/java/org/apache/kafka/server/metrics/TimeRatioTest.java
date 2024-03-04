09b3add784c5073b8f754d99f57e2e71d725269d
Jeff Kim <kimkb2011@gmail.com>
2024-03-04 11:33:22 -0500

    move test to server-common

--- server-common/src/test/java/org/apache/kafka/server/metrics/TimeRatioTest.java
+++ server-common/src/test/java/org/apache/kafka/server/metrics/TimeRatioTest.java
@@ -14,10 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.kafka.raft.internals;
+package org.apache.kafka.server.metrics;
 
 import org.apache.kafka.common.metrics.MetricConfig;
-import org.apache.kafka.server.metrics.TimeRatio;
 import org.apache.kafka.common.utils.MockTime;
 import org.junit.jupiter.api.Test;
 
