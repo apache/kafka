b5eb9c39b6ebc68575de571c99ce0e037c8e82f0
phuchong.tran <phuchong.tran@servicenow.com>
2024-03-08 21:36:02 +1100

    Revert "Fix unwanted spacing in OffsetsRequestManagerTest.java"
    This reverts commit 53fb53d79cede76766edf742e4e52d35da73ed40.

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/OffsetsRequestManagerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/OffsetsRequestManagerTest.java
@@ -86,7 +86,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class OffsetsRequestManagerTest {
-
     private OffsetsRequestManager requestManager;
     private ConsumerMetadata metadata;
     private SubscriptionState subscriptionState;
