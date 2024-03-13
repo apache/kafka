0d1b61fe981f7948265ec80f8c8c4d222ee975e1
Gaurav Narula <gaurav_narula2@apple.com>
2024-03-13 13:47:07 +0000

    Use @code instead of escaping angle brackets

--- core/src/main/java/kafka/log/remote/RemoteLogManager.java
+++ core/src/main/java/kafka/log/remote/RemoteLogManager.java
@@ -1278,7 +1278,8 @@ public class RemoteLogManager implements Closeable {
      *
      * For ex:
      * <pre>
-     *  &lt;epoch - start offset&gt;
+     * {@code
+     *  <epoch - start offset>
      *  0 - 0
      *  1 - 10
      *  2 - 20
@@ -1287,11 +1288,13 @@ public class RemoteLogManager implements Closeable {
      *  5 - 60  // epoch 5 does not have records or messages associated with it
      *  6 - 60
      *  7 - 70
+     * }
      * </pre>
      *
      *  When the above leaderEpochMap is passed to this method, it returns the following map:
      * <pre>
-     *  &lt;epoch - start offset&gt;
+     * {@code
+     *  <epoch - start offset>
      *  0 - 0
      *  1 - 10
      *  2 - 20
@@ -1299,6 +1302,7 @@ public class RemoteLogManager implements Closeable {
      *  4 - 40
      *  6 - 60
      *  7 - 70
+     * }
      * </pre>
      *
      * @param leaderEpochs The leader epoch map to be refined.
