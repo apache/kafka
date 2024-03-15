d089a7af4749b9248f103b8bee3b354648a76910
Sanskar Jhajharia <sjhajharia@confluent.io>
2024-03-15 12:46:09 +0530

    Fixed the comment for an updated request

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/runtime/PartitionWriter.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/runtime/PartitionWriter.java
@@ -128,6 +128,7 @@ public interface PartitionWriter<T> {
      * @param transactionalId   The transactional id.
      * @param producerId        The producer id.
      * @param producerEpoch     The producer epoch.
+     * @param apiVersion        The version of the Request used.
      * @return A future failed with any error encountered; or the {@link VerificationGuard}
      *         if the transaction required verification and {@link VerificationGuard#SENTINEL}
      *         if it did not.
