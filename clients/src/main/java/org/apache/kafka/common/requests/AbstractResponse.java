624c61887e70943ee58b6070bb72b74f98c3a12f
John Mannooparambil <jmannooparambil@confluent.io>
2024-03-15 12:11:21 -0400

    Squashed changes contributed by @mannoopj in https://github.com/apache/kafka/pull/15604
    add GetReplicaLogInfo Request and Response
    
    add GetReplicaLogInfo to apikeys.java
    
    add GetReplicaLogInfo Request and Response
    
    fix GetReplicaLogInfoResponse
    
    add test for GetReplicaLogInfo in RequestResponseTest.java
    
    add GetReplicaLogInfoResponse
    
    add latestVersionUnstable
    
    Conflicts:
    	clients/src/main/java/org/apache/kafka/common/protocol/ApiKeys.java
    	clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
    	clients/src/main/java/org/apache/kafka/common/requests/AbstractResponse.java
    	clients/src/test/java/org/apache/kafka/common/requests/RequestResponseTest.java
    	core/src/main/scala/kafka/network/RequestConvertToJson.scala
    
    cherry-pick of PR on top of origin/trunk.

--- clients/src/main/java/org/apache/kafka/common/requests/AbstractResponse.java
+++ clients/src/main/java/org/apache/kafka/common/requests/AbstractResponse.java
@@ -289,6 +289,8 @@ public abstract class AbstractResponse implements AbstractRequestResponse {
                 return AlterShareGroupOffsetsResponse.parse(responseBuffer, version);
             case DELETE_SHARE_GROUP_OFFSETS:
                 return DeleteShareGroupOffsetsResponse.parse(responseBuffer, version);
+            case GET_REPLICA_LOG_INFO:
+                return GetReplicaLogInfoResponse.parse(responseBuffer, version);
             default:
                 throw new AssertionError(String.format("ApiKey %s is not currently handled in `parseResponse`, the " +
                         "code should be updated to do so.", apiKey));
--- clients/src/test/java/org/apache/kafka/common/requests/RequestResponseTest.java
+++ clients/src/test/java/org/apache/kafka/common/requests/RequestResponseTest.java
@@ -157,6 +157,8 @@ import org.apache.kafka.common.message.FetchResponseData;
 import org.apache.kafka.common.message.FetchSnapshotRequestData;
 import org.apache.kafka.common.message.FetchSnapshotResponseData;
 import org.apache.kafka.common.message.FindCoordinatorRequestData;
+import org.apache.kafka.common.message.GetReplicaLogInfoRequestData;
+import org.apache.kafka.common.message.GetReplicaLogInfoResponseData;
 import org.apache.kafka.common.message.GetTelemetrySubscriptionsRequestData;
 import org.apache.kafka.common.message.GetTelemetrySubscriptionsResponseData;
 import org.apache.kafka.common.message.HeartbeatRequestData;
@@ -897,9 +899,9 @@ public class RequestResponseTest {
             new UnregisterBrokerRequestData()
         ).build((short) 0);
         String customerErrorMessage = "customer error message";
-        
+
         UnregisterBrokerResponse response = request.getErrorResponse(
-            0, 
+            0,
             new RuntimeException(customerErrorMessage)
         );
 
@@ -1110,6 +1112,7 @@ public class RequestResponseTest {
             case DESCRIBE_SHARE_GROUP_OFFSETS: return createDescribeShareGroupOffsetsRequest(version);
             case ALTER_SHARE_GROUP_OFFSETS: return createAlterShareGroupOffsetsRequest(version);
             case DELETE_SHARE_GROUP_OFFSETS: return createDeleteShareGroupOffsetsRequest(version);
+            case GET_REPLICA_LOG_INFO: return createGetReplicaLogInfoRequest(version);
             default: throw new IllegalArgumentException("Unknown API key " + apikey);
         }
     }
@@ -1205,8 +1208,24 @@ public class RequestResponseTest {
             case DESCRIBE_SHARE_GROUP_OFFSETS: return createDescribeShareGroupOffsetsResponse();
             case ALTER_SHARE_GROUP_OFFSETS: return createAlterShareGroupOffsetsResponse();
             case DELETE_SHARE_GROUP_OFFSETS: return createDeleteShareGroupOffsetsResponse();
+            case GET_REPLICA_LOG_INFO: return createGetReplicaLogInfoResponse();
             default: throw new IllegalArgumentException("Unknown API key " + apikey);
         }
+    }
+
+    private GetReplicaLogInfoRequest createGetReplicaLogInfoRequest(short version) {
+        GetReplicaLogInfoRequestData data = new GetReplicaLogInfoRequestData()
+            .setBrokerId(0)
+            .setTopicPartitions(singletonList(new GetReplicaLogInfoRequestData.TopicPartitions()
+                .setPartitions(singletonList(0))));
+        return new GetReplicaLogInfoRequest.Builder(data).build(version);
+    }
+
+    private GetReplicaLogInfoResponse createGetReplicaLogInfoResponse() {
+        GetReplicaLogInfoResponseData data = new GetReplicaLogInfoResponseData();
+        data.setBrokerEpoch(0);
+        data.setTopicPartitionLogInfoList(singletonList(new GetReplicaLogInfoResponseData.TopicPartitionLogInfo()));
+        return new GetReplicaLogInfoResponse(data);
     }
 
     private ConsumerGroupDescribeRequest createConsumerGroupDescribeRequest(short version) {
