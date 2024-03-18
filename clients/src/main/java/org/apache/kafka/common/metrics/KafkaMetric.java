547b01471068be7612a98ededc4efd8a92221415
Johnny Hsu <johnnyhsu@fb.com>
2024-03-18 21:11:14 +0800

    modify comment for config setter

--- clients/src/main/java/org/apache/kafka/common/metrics/KafkaMetric.java
+++ clients/src/main/java/org/apache/kafka/common/metrics/KafkaMetric.java
@@ -111,6 +111,7 @@ public final class KafkaMetric implements Metric {
 
     /**
      * Set the metric config.
+     * This is supposed to be used by server only, DO NOT use this for your metrics.
      * @param config configuration for this metrics
      */
     public void config(MetricConfig config) {
