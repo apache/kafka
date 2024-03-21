01d4e3a9ac9b24b5915cce93b66186542e26d0a9
Manikumar Reddy <manikumar.reddy@gmail.com>
2024-03-21 15:02:26 +0530

    MINOR: Minor update patch versions

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -142,10 +142,10 @@ get_kafka 2.4.1 2.12
 chmod a+rw /opt/kafka-2.4.1
 get_kafka 2.5.1 2.12
 chmod a+rw /opt/kafka-2.5.1
-get_kafka 2.6.2 2.12
-chmod a+rw /opt/kafka-2.6.2
-get_kafka 2.7.1 2.12
-chmod a+rw /opt/kafka-2.7.1
+get_kafka 2.6.3 2.12
+chmod a+rw /opt/kafka-2.6.3
+get_kafka 2.7.2 2.12
+chmod a+rw /opt/kafka-2.7.2
 get_kafka 2.8.2 2.12
 chmod a+rw /opt/kafka-2.8.2
 get_kafka 3.0.2 2.12
@@ -154,8 +154,8 @@ get_kafka 3.1.2 2.12
 chmod a+rw /opt/kafka-3.1.2
 get_kafka 3.2.3 2.12
 chmod a+rw /opt/kafka-3.2.3
-get_kafka 3.3.1 2.12
-chmod a+rw /opt/kafka-3.3.1
+get_kafka 3.3.2 2.12
+chmod a+rw /opt/kafka-3.3.2
 get_kafka 3.4.1 2.12
 chmod a+rw /opt/kafka-3.4.1
 get_kafka 3.5.2 2.12
