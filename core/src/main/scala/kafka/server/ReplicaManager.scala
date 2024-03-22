24be5e6899f060a7cbb6bbd2ff34ee081e6ee372
Sean Quah <squah@confluent.io>
2024-03-22 16:56:35 +0000

    fixup: explain that we want to avoid metadata refresh

--- core/src/main/scala/kafka/server/ReplicaManager.scala
+++ core/src/main/scala/kafka/server/ReplicaManager.scala
@@ -831,8 +831,9 @@ class ReplicaManager(val config: KafkaConfig,
             error match {
               case Errors.INVALID_TXN_STATE => Some(error.exception("Partition was not added to the transaction"))
               // Transaction verification can fail with a retriable error that older clients may not
-              // retry correctly. Translate these to `NOT_ENOUGH_REPLICAS`, which will cause such
-              // clients to retry the produce request.
+              // retry correctly. Translate these to an error which will cause such clients to retry
+              // the produce request. We use `NOT_ENOUGH_REPLICAS` because it does not trigger a
+              // metadata refresh.
               case Errors.CONCURRENT_TRANSACTIONS |
                    Errors.NETWORK_EXCEPTION |
                    Errors.COORDINATOR_LOAD_IN_PROGRESS |
