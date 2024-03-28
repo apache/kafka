997f0260e6ae606588d1680a8626d124a8821403
Greg Harris <greg.harris@aiven.io>
2024-03-27 18:06:29 -0700

    fixup: failing test due to Worker change
    Signed-off-by: Greg Harris <greg.harris@aiven.io>

--- connect/runtime/src/test/java/org/apache/kafka/connect/runtime/WorkerTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/WorkerTest.java
@@ -1167,9 +1167,7 @@ public class WorkerTest {
 
         Map<String, Object> connConfig = Collections.singletonMap("metadata.max.age.ms", "10000");
         Map<String, String> expectedConfigs = new HashMap<>(workerProps);
-        workerProps.keySet()
-                .stream().filter(key -> key.startsWith(AbstractConfig.CONFIG_PROVIDERS_CONFIG))
-                .forEach(expectedConfigs::remove);
+        expectedConfigs.remove(AbstractConfig.CONFIG_PROVIDERS_CONFIG);
         expectedConfigs.put("bootstrap.servers", "localhost:9092");
         expectedConfigs.put("client.id", "testid");
         expectedConfigs.put("metadata.max.age.ms", "10000");
