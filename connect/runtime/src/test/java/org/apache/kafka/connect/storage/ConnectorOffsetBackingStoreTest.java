f7323ab32c09f035af3bf435fdb6fa22299b790d
vamossagar12 <sagarmeansocean@gmail.com>
2024-04-10 16:43:15 +0530

    Update connect/runtime/src/test/java/org/apache/kafka/connect/storage/ConnectorOffsetBackingStoreTest.java
    Co-authored-by: Chris Egerton <fearthecellos@gmail.com>

--- connect/runtime/src/test/java/org/apache/kafka/connect/storage/ConnectorOffsetBackingStoreTest.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/storage/ConnectorOffsetBackingStoreTest.java
@@ -51,6 +51,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class ConnectorOffsetBackingStoreTest {
 
     private static final String NAMESPACE = "namespace";
