75f739d8ec531d82db17a57ea251dfb1f23b97ca
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2024-04-19 20:29:43 +0300

    fixup! KAFKA-14226: feat(connect:transform): Introduce FieldPath abstraction
    Move Trie-related tests to its right class.

--- connect/transforms/src/test/java/org/apache/kafka/connect/transforms/field/TrieTest.java
+++ connect/transforms/src/test/java/org/apache/kafka/connect/transforms/field/TrieTest.java
@@ -18,14 +18,13 @@ package org.apache.kafka.connect.transforms.field;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TrieTest {
@@ -38,39 +37,36 @@ class TrieTest {
     }
 
     @Test void shouldBuildMinimalTrie() {
-        Trie trie = new Trie();
-        trie.insert(new SingleFieldPath("test", FieldSyntaxVersion.V2));
+        Trie trie = trieWithPathsV2("test");
         assertFalse(trie.isEmpty());
         assertEquals(1, trie.size());
         assertFalse(trie.root.isLeaf());
         assertTrue(trie.get("test").isLeaf());
     }
 
+    @Test void shouldBuildTrieWithSinglePathV1() {
+        String path = "foo.bar.baz";
+        Trie trie = trieWithPathsV1(path);
+        assertFalse(trie.isEmpty());
+        assertEquals(1, trie.size());
+
+        Trie.Node maybeFoo = trie.get("foo.bar.baz");
+        assertNotNull(maybeFoo);
+        assertArrayEquals(new String[]{path}, maybeFoo.path.path());
+    }
+
     @Test void shouldBuildTrieWithSeparatePaths() {
-        Trie trie = new Trie();
-        trie.insert(new SingleFieldPath("foo", FieldSyntaxVersion.V2));
-        trie.insert(new SingleFieldPath("bar", FieldSyntaxVersion.V2));
+        Trie trie = trieWithPathsV2("foo", "bar");
         assertEquals(2, trie.size());
         assertFalse(trie.root.isLeaf());
         assertTrue(trie.get("foo").isLeaf());
         assertTrue(trie.get("bar").isLeaf());
     }
 
-    public static Stream<Arguments> overlappingPaths() {
-        final SingleFieldPath foo = new SingleFieldPath("foo", FieldSyntaxVersion.V2);
-        final SingleFieldPath fooBar = new SingleFieldPath("foo.bar", FieldSyntaxVersion.V2);
-        return Stream.of(
-            Arguments.of(foo, fooBar),
-            Arguments.of(fooBar, foo)
-        );
-    }
-
     @ParameterizedTest
-    @MethodSource("overlappingPaths")
-    void shouldFlatOverlappingPaths(SingleFieldPath first, SingleFieldPath second) {
-        Trie trie1 = new Trie();
-        trie1.insert(first);
-        trie1.insert(second);
+    @CsvSource({"foo,foo.bar", "foo.bar,foo"})
+    void shouldFlatOverlappingPaths(String first, String second) {
+        Trie trie1 = trieWithPathsV2(first, second);
         assertFalse(trie1.isEmpty());
         assertEquals(1, trie1.size());
 
@@ -79,5 +75,67 @@ class TrieTest {
         Trie.Node bar1 = foo1.get("bar");
         assertTrue(bar1.isLeaf());
         assertArrayEquals(new String[]{"foo", "bar"}, bar1.path.path());
+    }
+
+    @Test void shouldCreateTrieWithDups() {
+        Trie trie = trieWithPathsV2("test", "test");
+        assertEquals(1, trie.size());
+        Trie.Node test = trie.get("test");
+        assertNotNull(test);
+        assertArrayEquals(test.path.path(), new String[]{"test"});
+    }
+
+    @Test void shouldCreateTrieWithNestedNodes() {
+        Trie trie = trieWithPathsV2("foo.bar.baz");
+        assertFalse(trie.isEmpty());
+        assertEquals(1, trie.size());
+
+        Trie.Node maybeV1 = trie.get("foo.bar.baz");
+        assertNull(maybeV1);
+        Trie.Node maybeFoo = trie.get("foo");
+        assertNotNull(maybeFoo);
+        assertFalse(maybeFoo.isLeaf());
+        Trie.Node maybeBar = maybeFoo.get("bar");
+        assertNotNull(maybeBar);
+        assertFalse(maybeBar.isLeaf());
+        Trie.Node maybeBaz = maybeBar.get("baz");
+        assertNotNull(maybeBaz);
+        assertTrue(maybeBaz.isLeaf());
+        assertArrayEquals(new String[]{"foo", "bar", "baz"}, maybeBaz.path.path());
+    }
+
+    @Test void shouldCreateTrieWithMultipleBranchesV2() {
+        Trie trie = trieWithPathsV2("foo.bar", "foo.baz", "test");
+        assertFalse(trie.isEmpty());
+        assertEquals(3, trie.size());
+
+        Trie.Node maybeFoo = trie.get("foo");
+        assertNotNull(maybeFoo);
+        assertFalse(maybeFoo.isLeaf());
+        Trie.Node maybeBar = maybeFoo.get("bar");
+        assertNotNull(maybeBar);
+        assertTrue(maybeBar.isLeaf());
+        Trie.Node maybeBaz = maybeFoo.get("baz");
+        assertNotNull(maybeBaz);
+        assertTrue(maybeBaz.isLeaf());
+        Trie.Node maybeTest = trie.get("test");
+        assertNotNull(maybeTest);
+        assertTrue(maybeTest.isLeaf());
+    }
+
+    private static Trie trieWithPathsV2(String... paths) {
+        return trieWithPaths(FieldSyntaxVersion.V2, paths);
+    }
+
+    private static Trie trieWithPathsV1(String... paths) {
+        return trieWithPaths(FieldSyntaxVersion.V1, paths);
+    }
+
+    private static Trie trieWithPaths(FieldSyntaxVersion syntaxVersion, String... paths) {
+        Trie trie = new Trie();
+        for (String path : paths) {
+            trie.insert(new SingleFieldPath(path, syntaxVersion));
+        }
+        return trie;
     }
 }
