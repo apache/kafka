e5269ea1d1fdf83160a3410a6c30bf309b5fc3b3
Cerchie <lcerchie@confluent.io>
2024-05-03 12:52:39 -0700

    add correct link to cache.max.bytes.buffering alternative

--- streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
+++ streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
@@ -494,7 +494,7 @@ public class StreamsConfig extends AbstractConfig {
     private static final String BUILT_IN_METRICS_VERSION_DOC = "Version of the built-in metrics to use.";
 
     /** {@code cache.max.bytes.buffering}
-     * @deprecated since 3.4.0 Use {@link #CACHE_MAX_BYTES_CONFIG "cache.max.bytes"} instead. */
+     * @deprecated since 3.4.0 Use {@link #STATESTORE_CACHE_MAX_BYTES_CONFIG "statestore.cache.max.bytes"} instead. */
     @SuppressWarnings("WeakerAccess")
     @Deprecated
     public static final String CACHE_MAX_BYTES_BUFFERING_CONFIG = "cache.max.bytes.buffering";
