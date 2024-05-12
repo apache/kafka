10cddbaf6bd88279e5aabaa826eb8edaa4e238e9
chickenchickenlove <ojt90902@naver.com>
2024-05-12 09:29:10 +0900

    improve log description of QuorumController

--- metadata/src/main/java/org/apache/kafka/controller/ActivationRecordsGenerator.java
+++ metadata/src/main/java/org/apache/kafka/controller/ActivationRecordsGenerator.java
@@ -165,6 +165,8 @@ public class ActivationRecordsGenerator {
                         throw new RuntimeException("Should not have ZK migrations enabled on a cluster that was " +
                             "created in KRaft mode.");
                     }
+                    logMessageBuilder
+                            .append("since this is a de-novo KRaft cluster.");
                     break;
                 case PRE_MIGRATION:
                     if (!metadataVersion.isMetadataTransactionSupported()) {
