0dcada492ffe519f9337f15ff5fc7c9afdb25160
Justine <jolshan@confluent.io>
2024-05-15 14:17:07 -0700

    fix default for MV

--- metadata/src/main/java/org/apache/kafka/controller/ClusterControlManager.java
+++ metadata/src/main/java/org/apache/kafka/controller/ClusterControlManager.java
@@ -459,7 +459,8 @@ public class ClusterControlManager {
         FinalizedControllerFeatures finalizedFeatures,
         BrokerRegistrationRequestData.Feature feature
     ) {
-        short finalized = finalizedFeatures.getOrDefault(feature.name(), (short) 0);
+        int defaultVersion = feature.name().equals(MetadataVersion.FEATURE_NAME) ? 1 : 0; // The default value for MetadataVersion is 1 not 0.
+        short finalized = finalizedFeatures.getOrDefault(feature.name(), (short) defaultVersion);
         if (!VersionRange.of(feature.minSupportedVersion(), feature.maxSupportedVersion()).contains(finalized)) {
             throw new UnsupportedVersionException("Unable to register because the broker " +
                 "does not support version " + finalized + " of " + feature.name() +
