835fb15a20944da83ae6f669ef7cfdc2085bceed
TaiJu Wu <tjwu1217@gmail.com>
2024-05-16 22:58:54 +0800

    remove unnecessary import

--- core/src/test/java/kafka/test/ClusterTestExtensionsTest.java
+++ core/src/test/java/kafka/test/ClusterTestExtensionsTest.java
@@ -39,7 +39,6 @@ import java.util.Map;
 import java.util.List;
 import java.util.Collections;
 import java.util.concurrent.ExecutionException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
