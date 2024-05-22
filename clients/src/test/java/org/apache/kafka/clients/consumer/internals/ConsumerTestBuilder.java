c4916b5648597c4e5a252bb7336722add282fd07
Kirk True <kirk@kirktrue.pro>
2024-05-22 06:11:10 -1000

    KAFKA-15974: Enforce that event processing respects user-provided timeout (#15640)
    The intention of the CompletableApplicationEvent is for a Consumer to enqueue the event and then block, waiting for it to complete. The application thread will block up to the amount of the timeout. This change introduces a consistent manner in which events are expired out by checking their timeout values.
    
    The CompletableEventReaper is a new class that tracks CompletableEvents that are enqueued. Both the application thread and the network I/O thread maintain their own reaper instances. The application thread will track any CompletableBackgroundEvents that it receives and the network I/O thread will do the same with any CompletableApplicationEvents it receives. The application and network I/O threads will check their tracked events, and if any are expired, the reaper will invoke each event's CompletableFuture.completeExceptionally() method with a TimeoutException.
    
    On closing the AsyncKafkaConsumer, both threads will invoke their respective reapers to cancel any unprocessed events in their queues. In this case, the reaper will invoke each event's CompletableFuture.completeExceptionally() method with a CancellationException instead of a TimeoutException to differentiate the two cases.
    
    The overall design for the expiration mechanism is captured on the Apache wiki and the original issue (KAFKA-15848) has more background on the cause.
    
    Note: this change only handles the event expiration and does not cover the network request expiration. That is handled in a follow-up Jira (KAFKA-16200) that builds atop this change.
    
    This change also includes some minor refactoring of the EventProcessor and its implementations. This allows the event processor logic to focus on processing individual events rather than also the handling of batches of events.
    
    Reviewers: Lianet Magrans <lianetmr@gmail.com>, Philip Nee <pnee@confluent.io>, Bruno Cadonna <cadonna@apache.org>

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/ConsumerTestBuilder.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/ConsumerTestBuilder.java
@@ -38,7 +38,6 @@ import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.common.utils.Timer;
 
 import java.io.Closeable;
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.Properties;
@@ -270,7 +269,6 @@ public class ConsumerTestBuilder implements Closeable {
             );
         this.applicationEventProcessor = spy(new ApplicationEventProcessor(
                 logContext,
-                applicationEventQueue,
                 requestManagers,
                 metadata
             )
@@ -287,32 +285,6 @@ public class ConsumerTestBuilder implements Closeable {
     @Override
     public void close() {
         closeQuietly(requestManagers, RequestManagers.class.getSimpleName());
-        closeQuietly(applicationEventProcessor, ApplicationEventProcessor.class.getSimpleName());
-    }
-
-    public static class ConsumerNetworkThreadTestBuilder extends ConsumerTestBuilder {
-
-        final ConsumerNetworkThread consumerNetworkThread;
-
-        public ConsumerNetworkThreadTestBuilder() {
-            this(createDefaultGroupInformation());
-        }
-
-        public ConsumerNetworkThreadTestBuilder(Optional<GroupInformation> groupInfo) {
-            super(groupInfo);
-            this.consumerNetworkThread = new ConsumerNetworkThread(
-                    logContext,
-                    time,
-                    () -> applicationEventProcessor,
-                    () -> networkClientDelegate,
-                    () -> requestManagers
-            );
-        }
-
-        @Override
-        public void close() {
-            consumerNetworkThread.close(Duration.ZERO);
-        }
     }
 
     public static class GroupInformation {
