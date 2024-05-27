57217b4badd0858335129389c090945133f5d1bf
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2024-05-27 23:22:59 +0300

    fixup! KAFKA-15931: Reopen TransactionIndex if channel is closed
    Refactor search for aborted txns

--- storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
@@ -53,6 +53,7 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -523,7 +524,7 @@ public class RemoteIndexCache implements Closeable {
             return timeIndex;
         }
 
-        // Visible for testing
+        // Use to reopen txn index if channel is closed.
         public TransactionIndex txnIndex() {
             if (txnIndex.isClosed()) {
                 // Reopen txn index if closed
@@ -628,6 +629,17 @@ public class RemoteIndexCache implements Closeable {
             }
         }
 
+        public boolean completeAbortedTxnsSearch(
+            long startOffset,
+            long upperBoundOffset,
+            Consumer<List<AbortedTxn>> accumulator
+        ) {
+            TransactionIndex transactionIndex = txnIndex();
+            TxnIndexSearchResult searchResult = transactionIndex.collectAbortedTxns(startOffset, upperBoundOffset);
+            accumulator.accept(searchResult.abortedTransactions);
+            return searchResult.isComplete;
+        }
+
         @Override
         public void close() {
             entryLock.writeLock().lock();
@@ -647,6 +659,10 @@ public class RemoteIndexCache implements Closeable {
                     ", timeIndex=" + timeIndex.file().getName() +
                     ", txnIndex=" + txnIndex.file().getName() +
                     '}';
+        }
+
+        public boolean hasTxnIndex() {
+            return txnIndex.file().length() == 0;
         }
     }
 
