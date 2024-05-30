ef660246d2d0a6d9e5c5c9e5c117d7382eddf29e
Justine <jolshan@confluent.io>
2024-05-30 10:02:57 -0700

    add param javadoc

--- server-common/src/main/java/org/apache/kafka/server/common/Features.java
+++ server-common/src/main/java/org/apache/kafka/server/common/Features.java
@@ -83,9 +83,10 @@ public enum Features {
     /**
      * Creates a FeatureVersion from a level.
      *
-     * @param level   the level of the feature
-     * @return       the FeatureVersionUtils.FeatureVersion for the feature the enum is based on.
-     * @throws        IllegalArgumentException if the feature is not known.
+     * @param level                        the level of the feature
+     * @param allowUnstableFeatureVersions whether unstable versions can be used
+     * @return the FeatureVersionUtils.FeatureVersion for the feature the enum is based on.
+     * @throws IllegalArgumentException    if the feature is not known.
      */
     public FeatureVersion fromFeatureLevel(short level,
                                            boolean allowUnstableFeatureVersions) {
