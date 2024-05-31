02d859f4a6ac1e304f080af1ce27e03ef356b869
Kuan-Po Tseng <brandboat@gmail.com>
2024-05-31 02:00:08 +0000

    Address comment

--- core/src/test/scala/unit/kafka/log/UnifiedLogTest.scala
+++ core/src/test/scala/unit/kafka/log/UnifiedLogTest.scala
@@ -4265,6 +4265,9 @@ class UnifiedLogTest {
     seg1.append(1, 1000L, 1, MemoryRecords.withRecords(1, Compression.NONE, new SimpleRecord("one".getBytes)))
     seg2.append(2, 2000L, 1, MemoryRecords.withRecords(2, Compression.NONE, new SimpleRecord("two".getBytes)))
     assertEquals(Seq(1000L, 2000L), log.getFirstBatchTimestampForSegments(segments).asScala.toSeq)
+
+    seg1.close()
+    seg2.close()
   }
 
   private def appendTransactionalToBuffer(buffer: ByteBuffer,
