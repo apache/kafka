44c4043a2dd802a397ad241c01a4d051b7ee3111
Andrew Schofield <aschofield@confluent.io>
2024-06-13 21:25:18 +0100

    KAFKA-16950: Define Persister interfaces and RPCs

--- clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
@@ -340,6 +340,16 @@ public abstract class AbstractRequest implements AbstractRequestResponse {
                 return RemoveRaftVoterRequest.parse(buffer, apiVersion);
             case UPDATE_RAFT_VOTER:
                 return UpdateRaftVoterRequest.parse(buffer, apiVersion);
+            case INITIALIZE_SHARE_GROUP_STATE:
+                return InitializeShareGroupStateRequest.parse(buffer, apiVersion);
+            case READ_SHARE_GROUP_STATE:
+                return ReadShareGroupStateRequest.parse(buffer, apiVersion);
+            case WRITE_SHARE_GROUP_STATE:
+                return WriteShareGroupStateRequest.parse(buffer, apiVersion);
+            case DELETE_SHARE_GROUP_STATE:
+                return DeleteShareGroupStateRequest.parse(buffer, apiVersion);
+            case READ_SHARE_GROUP_STATE_SUMMARY:
+                return ReadShareGroupStateSummaryRequest.parse(buffer, apiVersion);
             default:
                 throw new AssertionError(String.format("ApiKey %s is not currently handled in `parseRequest`, the " +
                         "code should be updated to do so.", apiKey));
---
+++ server-common/src/main/java/org/apache/kafka/server/group/share/Persister.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kafka.server.group.share;
+
+import org.apache.kafka.common.annotation.InterfaceStability;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * This interface introduces methods which can be used by callers to interact with the
+ * persistence impl responsible for storing share group/partition states.
+ * For KIP-932, the persistence impl will be a share coordinator which stores information in
+ * an internal topic, but this allows for other variations as well.
+ */
+@InterfaceStability.Evolving
+public interface Persister {
+    /**
+     * Initialize the share partition state.
+     *
+     * @param request InitializeShareGroupStateParameters
+     * @return InitializeShareGroupStateResult
+     */
+    CompletableFuture<InitializeShareGroupStateResult> initializeState(InitializeShareGroupStateParameters request) throws IllegalArgumentException;
+
+    /**
+     * Read share-partition state from a persistence impl.
+     *
+     * @param request ReadShareGroupStateParameters
+     * @return ReadShareGroupStateResult
+     */
+    CompletableFuture<ReadShareGroupStateResult> readState(ReadShareGroupStateParameters request) throws IllegalArgumentException;
+
+    /**
+     * Write share-partition state to a persistence impl.
+     *
+     * @param request WriteShareGroupStateParameters
+     * @return WriteShareGroupStateResult
+     */
+    CompletableFuture<WriteShareGroupStateResult> writeState(WriteShareGroupStateParameters request) throws IllegalArgumentException;
+
+    /**
+     * Delete share-partition state from a persistence impl.
+     *
+     * @param request DeleteShareGroupStateParameters
+     * @return DeleteShareGroupStateResult
+     */
+    CompletableFuture<DeleteShareGroupStateResult> deleteState(DeleteShareGroupStateParameters request) throws IllegalArgumentException;
+
+    /**
+     * Read the offset information from share-partition state from a persistence impl.
+     *
+     * @param request ReadShareGroupOffsetsStateParameters
+     * @return ReadShareGroupOffsetsStateResult
+     */
+    CompletableFuture<ReadShareGroupStateSummaryResult> readSummary(ReadShareGroupStateSummaryParameters request) throws IllegalArgumentException;
+
+    /**
+     * Perform cleanup and interrupt any threads
+     */
+    void stop();
+}
