249917da06892dc8e2d090b243af40fcb4d61728
Justine <jolshan@confluent.io>
2024-06-19 10:46:43 -0700

    Fix test

--- server-common/src/test/java/org/apache/kafka/server/common/FeaturesTest.java
+++ server-common/src/test/java/org/apache/kafka/server/common/FeaturesTest.java
@@ -24,8 +24,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FeaturesTest {
 
@@ -95,8 +94,13 @@ public class FeaturesTest {
     @EnumSource(Features.class)
     public void testDefaultValueAllFeatures(Features feature) {
         for (FeatureVersion featureImpl : feature.featureVersions()) {
-            assertEquals(feature.defaultValue(featureImpl.bootstrapMetadataVersion()), featureImpl.featureLevel(),
-                    "Failed to get the correct default for " + featureImpl);
+            // If features have the same bootstrapMetadataVersion, the highest level feature should be chosen.
+            short defaultLevel = feature.defaultValue(featureImpl.bootstrapMetadataVersion());
+            if (defaultLevel != featureImpl.featureLevel()) {
+                FeatureVersion otherFeature = feature.fromFeatureLevel(defaultLevel, true);
+                assertEquals(featureImpl.bootstrapMetadataVersion(), otherFeature.bootstrapMetadataVersion());
+                assertTrue(defaultLevel > featureImpl.featureLevel());
+            }
         }
     }
 
