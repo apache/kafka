c12ca5f0e9208e0668067510ac599a76e0b9a14f
Justine <jolshan@confluent.io>
2024-06-20 11:54:05 -0700

    Fix imports

--- server-common/src/test/java/org/apache/kafka/server/common/FeaturesTest.java
+++ server-common/src/test/java/org/apache/kafka/server/common/FeaturesTest.java
@@ -24,7 +24,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FeaturesTest {
 
