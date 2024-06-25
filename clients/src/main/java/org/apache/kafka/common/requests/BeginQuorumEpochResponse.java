39faf9cd067d3041c0de8ef20245a9cdda70bda7
José Armando García Sancio <jsancio@users.noreply.github.com>
2024-06-25 16:45:15 -0400

    KAFKA-16527; Implement request handling for updated KRaft RPCs (#16235)
    Implement request handling for the updated versions of the KRaft RPCs (Fetch, FetchSnapshot, Vote,
    BeginQuorumEpoch and EndQuorumEpoch). This doesn't add support for KRaft replicas to send the new
    version of the KRaft RPCs. That will be implemented in KAFKA-16529.
    
    All of the RPCs responses were extended to include the leader's endpoint for the listener of the
    channel used in the request. EpochState was extended to include the leader's endpoint information
    but only the FollowerState and LeaderState know the leader id and its endpoint(s).
    
    For the Fetch request, the replica directory id was added. The leader now tracks the follower's log
    end offset using both the replica id and replica directory id.
    
    For the FetchSnapshot request, the replica directory id was added. This is not used by the KRaft
    leader and it is there for consistency with Fetch and for help debugging.
    
    For the Vote request, the replica key for both the voter (destination) and the candidate (source)
    were added. The voter key is checked for consistency. The candidate key is persisted when the vote
    is granted.
    
    For the BeginQuorumEpoch request, all of the leader's endpoints are included. This is needed so
    that the voters can return the leader's endpoint for all of the supported listeners.
    
    For the EndQuorumEpoch request, all of the leader's endpoints are included. This is needed so that
    the voters can return the leader's endpoint for all of the supported listeners. The successor list
    has been extended to include the directory id. Receiving voters can use the entire replica key when
    searching their position in the successor list.
    
    Updated the existing test in KafkaRaftClientTest and KafkaRaftClientSnapshotTest to execute using
    both the old version and new version of the RPCs.
    
    Reviewers: Luke Chen <showuon@gmail.com>, Colin P. McCabe <cmccabe@apache.org>

--- clients/src/main/java/org/apache/kafka/common/requests/BeginQuorumEpochResponse.java
+++ clients/src/main/java/org/apache/kafka/common/requests/BeginQuorumEpochResponse.java
@@ -17,14 +17,12 @@
 
 package org.apache.kafka.common.requests;
 
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.message.BeginQuorumEpochResponseData;
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.apache.kafka.common.protocol.ByteBufferAccessor;
 import org.apache.kafka.common.protocol.Errors;
 
 import java.nio.ByteBuffer;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,27 +45,6 @@ public class BeginQuorumEpochResponse extends AbstractResponse {
     public BeginQuorumEpochResponse(BeginQuorumEpochResponseData data) {
         super(ApiKeys.BEGIN_QUORUM_EPOCH);
         this.data = data;
-    }
-
-    public static BeginQuorumEpochResponseData singletonResponse(
-        Errors topLevelError,
-        TopicPartition topicPartition,
-        Errors partitionLevelError,
-        int leaderEpoch,
-        int leaderId
-    ) {
-        return new BeginQuorumEpochResponseData()
-                   .setErrorCode(topLevelError.code())
-                   .setTopics(Collections.singletonList(
-                       new BeginQuorumEpochResponseData.TopicData()
-                           .setTopicName(topicPartition.topic())
-                           .setPartitions(Collections.singletonList(
-                               new BeginQuorumEpochResponseData.PartitionData()
-                                   .setErrorCode(partitionLevelError.code())
-                                   .setLeaderId(leaderId)
-                                   .setLeaderEpoch(leaderEpoch)
-                           )))
-                   );
     }
 
     @Override
