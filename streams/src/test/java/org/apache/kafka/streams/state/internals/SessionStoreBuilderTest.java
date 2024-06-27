9b4f13efbc0674ec63835ceee728f5ec62e69171
Ken Huang <s7133700@gmail.com>
2024-06-27 16:11:32 +0900

    KAFKA-15623 Remove junit 4 from stream module  (#16447)
    Reviewers: Chia-Ping Tsai <chia7712@gmail.com>

--- streams/src/test/java/org/apache/kafka/streams/state/internals/SessionStoreBuilderTest.java
+++ streams/src/test/java/org/apache/kafka/streams/state/internals/SessionStoreBuilderTest.java
@@ -36,7 +36,7 @@ import java.util.Collections;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
