2597de30f8d50c627b905e33c5151e062fa969ac
frankvicky <kitingiao@gmail.com>
2024-06-30 23:30:13 +0800

    Beanchmark: Remove unnecessary method.

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/record/ConsumerRecordsBenchmark.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/record/ConsumerRecordsBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.jmh.record;
 
 
@@ -6,9 +22,25 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.apache.kafka.common.record.TimestampType;
-import org.openjdk.jmh.annotations.*;
 
-import java.util.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
@@ -43,21 +75,12 @@ public class ConsumerRecordsBenchmark {
 
     @Benchmark
     public void records() {
-        records.records("topic2");
-    }
-
-    @Benchmark
-    public void recordsForEach() {
-        records.recordsForEach("topic2");
-    }
-
-    @Benchmark
-    public void recordsWithEntrySetAndStream() {
-        records.recordsWithEntrySetAndStream("topic2");
+        // original one
+        records.recordsWithNestedList("topic2");
     }
 
     @Benchmark
     public void recordsWithFilterIterator() {
-        records.recordsWithFilterIterator("topic2");
+        records.records("topic2");
     }
 }
