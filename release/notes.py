519aeb5dd3622b628dc488c4c248ad7451d3ecc5
Igor Soarez <i@soarez.me>
2024-07-03 14:51:56 +0100

    Uniform license header

--- release/notes.py
+++ release/notes.py
@@ -1,3 +1,4 @@
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 """
 Usage: python notes.py <version> > RELEASE_NOTES.html
