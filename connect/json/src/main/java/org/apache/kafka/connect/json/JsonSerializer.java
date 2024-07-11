07b2c9a3a0dacaa011663231f7ce810a9a3dcb0e
Vikas Balani <123360241+vbalani002@users.noreply.github.com>
2024-07-11 17:47:27 +0530

    KAFKA-17111: explicitly register Afterburner module in JsonSerializer and JsonDeserializer (#16565)
    Reviewers: Mickael Maison <mickael.maison@gmail.com>, Josep Prat <josep.prat@aiven.io>, Greg Harris <greg.harris@aiven.io>

--- connect/json/src/main/java/org/apache/kafka/connect/json/JsonSerializer.java
+++ connect/json/src/main/java/org/apache/kafka/connect/json/JsonSerializer.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 import java.util.Collections;
 import java.util.Set;
@@ -51,12 +52,12 @@ public class JsonSerializer implements Serializer<JsonNode> {
     JsonSerializer(
         final Set<SerializationFeature> serializationFeatures,
         final JsonNodeFactory jsonNodeFactory,
-        final boolean enableModules
+        final boolean enableAfterburner
     ) {
         serializationFeatures.forEach(objectMapper::enable);
         objectMapper.setNodeFactory(jsonNodeFactory);
-        if (enableModules) {
-            objectMapper.findAndRegisterModules();
+        if (enableAfterburner) {
+            objectMapper.registerModule(new AfterburnerModule());
         }
     }
 
