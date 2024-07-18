0e133f72aab0455bd6175f99159832036b1c63d2
brenden20 <118419078+brenden20@users.noreply.github.com>
2024-07-18 11:32:05 -0500

    Checkstyle fixes

--- clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
@@ -1577,7 +1577,7 @@ public class KafkaAdminClient extends AdminClient {
                 log.trace("Entering KafkaClient#poll(timeout={})", pollTimeout);
                 List<ClientResponse> responses = client.poll(Math.max(0L, pollTimeout), now);
                 // Check if the client has been bootstrapped, if true, metadataUpdater.update()
-                if(client.isBootstrapped())
+                if (client.isBootstrapped())
                     metadataManager.update(Cluster.bootstrap(adminAddresses.addresses()), time.milliseconds());
                 log.trace("KafkaClient#poll retrieved {} response(s)", responses.size());
 
--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetcherTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetcherTest.java
@@ -143,7 +143,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
