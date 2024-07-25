61aebffdb7e9ce15a96afdc0abd5b56bd0e59acb
m1a2st <s7133700@gmail.com>
2024-07-25 16:12:25 +0800

    add test for OffsetsMessageFormatter and deprecate old Formatter

--- tools/src/main/java/org/apache/kafka/tools/consumer/OffsetsMessageFormatter.java
+++ tools/src/main/java/org/apache/kafka/tools/consumer/OffsetsMessageFormatter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.tools.consumer;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -73,7 +89,7 @@ public class OffsetsMessageFormatter implements MessageFormatter {
         if (version >= OffsetCommitValue.LOWEST_SUPPORTED_VERSION
                 && version <= OffsetCommitValue.HIGHEST_SUPPORTED_VERSION) {
             OffsetCommitValue value = new OffsetCommitValue(new ByteBufferAccessor(byteBuffer), version);
-            value.setLeaderEpoch(value.leaderEpoch() == NO_PARTITION_LEADER_EPOCH ? 0: value.leaderEpoch());
+            value.setLeaderEpoch(value.leaderEpoch() == NO_PARTITION_LEADER_EPOCH ? 0 : value.leaderEpoch());
             value.setExpireTimestamp(value.expireTimestamp() == DEFAULT_TIMESTAMP ? 0L : value.expireTimestamp());
             return Optional.of(value);
         } else {
