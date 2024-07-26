b5eb17e9ae567f06f99189360c882e5d30c4b4bc
Josep Prat <josep.prat@aiven.io>
2024-07-26 18:14:23 +0200

    MINOR: Update 3.8 branch version to 3.8.1-SNAPSHOT

--- docs/js/templateData.js
+++ docs/js/templateData.js
@@ -19,6 +19,6 @@ limitations under the License.
 var context={
     "version": "38",
     "dotVersion": "3.8",
-    "fullDotVersion": "3.8.0",
+    "fullDotVersion": "3.8.1-SNAPSHOT",
     "scalaVersion": "2.13"
 };
