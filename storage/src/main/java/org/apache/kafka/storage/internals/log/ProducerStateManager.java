654be943615957846397e04dd6a248c763e68407
Ming-Yen Chung <mingyen066@gmail.com>
2024-07-28 21:23:01 +0800

    Fix import

--- storage/src/main/java/org/apache/kafka/storage/internals/log/ProducerStateManager.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/ProducerStateManager.java
@@ -24,6 +24,7 @@ import org.apache.kafka.common.utils.ByteUtils;
 import org.apache.kafka.common.utils.Crc32C;
 import org.apache.kafka.common.utils.LogContext;
 import org.apache.kafka.common.utils.Time;
+import org.apache.kafka.common.utils.Utils;
 import org.apache.kafka.server.log.remote.metadata.storage.generated.ProducerSnapshot;
 
 import org.slf4j.Logger;
