8ef2b455c1bbc27f3e5328e4bf001819ac2a5a34
Josep Prat <josep.prat@aiven.io>
2024-07-30 09:19:48 +0200

    MINOR: Add 3.8.0 to system tests (#16714)
    Reviewers:  Manikumar Reddy <manikumar.reddy@gmail.com>

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -164,7 +164,8 @@ get_kafka 3.6.2 2.12
 chmod a+rw /opt/kafka-3.6.2
 get_kafka 3.7.1 2.12
 chmod a+rw /opt/kafka-3.7.1
-
+get_kafka 3.8.0 2.12
+chmod a+rw /opt/kafka-3.8.0
 
 # For EC2 nodes, we want to use /mnt, which should have the local disk. On local
 # VMs, we can just create it if it doesn't exist and use it like we'd use
