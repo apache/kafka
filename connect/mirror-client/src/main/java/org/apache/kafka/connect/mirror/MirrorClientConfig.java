442ac27c2a53bb5304980c9efebb1186925215c3
Bertalan Kondrat <kb.pcre@gmail.com>
2024-07-31 16:24:38 +0200

    modified docs
    Change-Id: I1206ac2bcbdf3c20b659806e1d69815208a28ae3

--- connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/MirrorClientConfig.java
+++ connect/mirror-client/src/main/java/org/apache/kafka/connect/mirror/MirrorClientConfig.java
@@ -73,7 +73,7 @@ public class MirrorClientConfig extends AbstractConfig {
     public static final String PRODUCER_CLIENT_PREFIX = "producer.";
 
     public static final String HEARTBEATS_TOPIC_NAME = "default.replication.policy.heartbeats.topic.name";
-    public static final String HEARTBEATS_TOPIC_NAME_DOC = "Name of the heartbeats topic";
+    public static final String HEARTBEATS_TOPIC_NAME_DOC = "Name of the heartbeats topic (It is discarded if the " + REPLICATION_POLICY_CLASS + " is set to anything else but " + REPLICATION_POLICY_CLASS_DEFAULT + ").";
     public static final String HEARTBEATS_TOPIC_NAME_DEFAULT = ReplicationPolicy.DEFAULT_HEARTBEATS_TOPIC_NAME;
     MirrorClientConfig(Map<?, ?> props) {
         super(CONFIG_DEF, props, Utils.castToStringObjectMap(props), true);
