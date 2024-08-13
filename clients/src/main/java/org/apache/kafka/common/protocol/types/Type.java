97c0637efd54c083b794591cd5b59535765c520e
Ivan Yurchenko <ivan@ivanyu.me>
2024-08-13 07:08:31 +0300

    KAFKA-17323: Document UINT16 instead of UINT32 in Protocol Guide
    `UINT16` is used in API messages, while `UINT32` is not.

--- clients/src/main/java/org/apache/kafka/common/protocol/types/Type.java
+++ clients/src/main/java/org/apache/kafka/common/protocol/types/Type.java
@@ -1113,7 +1113,7 @@ public abstract class Type {
     private static String toHtml() {
         DocumentedType[] types = {
             BOOLEAN, INT8, INT16, INT32, INT64,
-            UNSIGNED_INT32, VARINT, VARLONG, UUID, FLOAT64,
+            UINT16, VARINT, VARLONG, UUID, FLOAT64,
             STRING, COMPACT_STRING, NULLABLE_STRING, COMPACT_NULLABLE_STRING,
             BYTES, COMPACT_BYTES, NULLABLE_BYTES, COMPACT_NULLABLE_BYTES,
             RECORDS, new ArrayOf(STRING), new CompactArrayOf(COMPACT_STRING)};
