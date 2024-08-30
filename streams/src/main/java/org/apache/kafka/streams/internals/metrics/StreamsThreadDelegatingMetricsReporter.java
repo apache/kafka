22ebe92c21b7a4248ea182c1f98f7c2e5c1bcf84
bbejeck <bbejeck@gmail.com>
2024-08-30 19:01:04 -0400

    Added parameterized integration test

--- streams/src/main/java/org/apache/kafka/streams/internals/metrics/StreamsThreadDelegatingMetricsReporter.java
+++ streams/src/main/java/org/apache/kafka/streams/internals/metrics/StreamsThreadDelegatingMetricsReporter.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class StreamsThreadDelegatingMetricsReporter implements MetricsReporter {
 
@@ -33,36 +34,43 @@ public class StreamsThreadDelegatingMetricsReporter implements MetricsReporter {
     private static final Logger LOG = LoggerFactory.getLogger(StreamsThreadDelegatingMetricsReporter.class);
     private Consumer<?, ?> consumer;
     final String threadId;
+    final String stateUpdaterThreadId;
     private static final String THREAD_ID_TAG = "thread-id";
 
 
-    public StreamsThreadDelegatingMetricsReporter(final Consumer<?, ?> consumer, final String threadId) {
-        this.consumer = consumer;
-        this.threadId = threadId;
+    public StreamsThreadDelegatingMetricsReporter(final Consumer<?, ?> consumer, final String threadId, final String stateUpdaterThreadId) {
+        this.consumer = Objects.requireNonNull(consumer);
+        this.threadId = Objects.requireNonNull(threadId);
+        this.stateUpdaterThreadId = Objects.requireNonNull(stateUpdaterThreadId);
+        LOG.info("Creating MetricsReporter for threadId {} and stateUpdaterId {}", threadId, stateUpdaterThreadId);
     }
 
     @Override
     public void init(final List<KafkaMetric> metrics) {
-
+        metrics.forEach(this::metricChange);
     }
 
     @Override
     public void metricChange(final KafkaMetric metric) {
-        if (tagMatchesCurrentThread(metric)) {
-            LOG.info("Registering metric {} for thread={}", metric.metricName().name(), threadId);
+        if (tagMatchStreamOrStateUpdaterThreadId(metric)) {
+            LOG.info("Registering metric {}", metric.metricName());
             consumer.registerMetric(metric);
         }
     }
 
-    boolean tagMatchesCurrentThread(final KafkaMetric kafkaMetric) {
-        final Map<String, String> tags = kafkaMetric.metricName().tags();
-        return tags.containsKey(THREAD_ID_TAG) && tags.get(THREAD_ID_TAG).equals(threadId);
+    boolean tagMatchStreamOrStateUpdaterThreadId(final KafkaMetric metric) {
+        final Map<String, String> tags = metric.metricName().tags();
+        final boolean shouldInclude = tags.containsKey(THREAD_ID_TAG) && (tags.get(THREAD_ID_TAG).equals(threadId) || tags.get(THREAD_ID_TAG).equals(stateUpdaterThreadId));
+        if (!shouldInclude) {
+            LOG.warn("Rejecting metric {}", metric.metricName());
+        }
+        return shouldInclude;
     }
 
     @Override
     public void metricRemoval(final KafkaMetric metric) {
-        if (tagMatchesCurrentThread(metric)) {
-            LOG.info("Unregistering metric {} for thread={}", metric.metricName().name(), threadId);
+        if (tagMatchStreamOrStateUpdaterThreadId(metric)) {
+            LOG.info("Unregistering metric {}", metric.metricName());
             consumer.unregisterMetric(metric);
         }
     }
@@ -74,6 +82,6 @@ public class StreamsThreadDelegatingMetricsReporter implements MetricsReporter {
 
     @Override
     public void configure(final Map<String, ?> configs) {
-
+        // No op
     }
 }
