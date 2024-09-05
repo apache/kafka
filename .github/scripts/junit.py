84aa5d7a633826503ed69badd838b55552ef929c
David Arthur <mumrah@gmail.com>
2024-09-05 13:50:33 -0400

    KAFKA-17479 Relocate junit XML files [2/n] (#17098)
    Recently, we fixed caching for ":jar" and ":test" tasks. A side effect of this is that the test results will be restored as part of the Gradle cache resolution. This means test tasks which are skipped (as a result of FROM-CACHE) will still have test results in their build directory. To avoid incorrectly reporting these results in the job summary, this patch uses a doLast task handler to relocate JUnit XML files into a new directory.
    
    This patch also removes the "continue-on-error" from the JUnit test step which caused timed-out builds to appear successful.
    
    Reviewers: Chia-Ping Tsai <chia7712@gmail.com>

--- .github/scripts/junit.py
+++ .github/scripts/junit.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse JUnit XML results.")
     parser.add_argument("--path",
                         required=False,
-                        default="**/test-results/**/*.xml",
+                        default="build/junit-xml/**/*.xml",
                         help="Path to XML files. Glob patterns are supported.")
 
     if not os.getenv("GITHUB_WORKSPACE"):
