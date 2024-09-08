61ccf85ef77caac2afee0301c37b090344e00ea4
Chirag <apple@Apples-MacBook-Pro.local>
2024-09-08 15:32:15 +0530

    KAFKA:16745: removed persister.stop from BrokerServer shutdown

---
+++ share/src/test/java/org/apache/kafka/server/share/ShareGroupConfigTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.server.share;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShareGroupConfigTest {
+    public static ShareGroupConfig createShareGroupConfig(
+        boolean shareGroupEnable,
+        int shareGroupPartitionMaxRecordLocks,
+        int shareGroupDeliveryCountLimit,
+        short shareGroupsMaxGroups,
+        int shareGroupRecordLockDurationsMs,
+        int shareGroupMinRecordLockDurationMs,
+        int shareGroupMaxRecordLockDurationMs
+    ) {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ShareGroupConfig.SHARE_GROUP_ENABLE_CONFIG, shareGroupEnable);
+        configs.put(ShareGroupConfig.SHARE_GROUP_PARTITION_MAX_RECORD_LOCKS_CONFIG, shareGroupPartitionMaxRecordLocks);
+        configs.put(ShareGroupConfig.SHARE_GROUP_DELIVERY_COUNT_LIMIT_CONFIG, shareGroupDeliveryCountLimit);
+        configs.put(ShareGroupConfig.SHARE_GROUP_MAX_GROUPS_CONFIG, shareGroupsMaxGroups);
+        configs.put(ShareGroupConfig.SHARE_GROUP_RECORD_LOCK_DURATION_MS_CONFIG, shareGroupRecordLockDurationsMs);
+        configs.put(ShareGroupConfig.SHARE_GROUP_MIN_RECORD_LOCK_DURATION_MS_CONFIG, shareGroupMinRecordLockDurationMs);
+        configs.put(ShareGroupConfig.SHARE_GROUP_MAX_RECORD_LOCK_DURATION_MS_CONFIG, shareGroupMaxRecordLockDurationMs);
+
+        return createConfig(configs);
+    }
+
+    private static ShareGroupConfig createConfig(Map<String, Object> configs) {
+        return new ShareGroupConfig(
+            new AbstractConfig(Utils.mergeConfigs(Collections.singletonList(ShareGroupConfig.CONFIG_DEF)), configs, false));
+    }
+}
