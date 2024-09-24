b4e7e920c85b31dc26300d07c9ed9a649dfe570a
David Arthur <mumrah@gmail.com>
2024-09-24 11:04:44 -0400

    wip

--- .github/scripts/format-all-tests.py
+++ .github/scripts/format-all-tests.py
@@ -30,26 +30,8 @@ handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 
-if __name__ == "__main__":
-    """
-    """
-    parser = argparse.ArgumentParser(description="Convert tests from all-tests.txt into YAML.")
-    parser.add_argument("--path",
-                        required=False,
-                        default="**/build/all-tests.txt",
-                        help="Path to all-tests.txt files. Glob patterns are supported.")
-    parser.add_argument("--yaml-output-dir",
-                        required=False,
-                        default="data/all-tests",
-                        help="Directory to output YAML files")
-
-    if not os.getenv("GITHUB_WORKSPACE"):
-        print("This script is intended to by run by GitHub Actions.")
-        exit(1)
-
-    args = parser.parse_args()
-
-    reports = glob(pathname=args.path, recursive=True)
+def all_tests_to_yaml(glob_path: str, out_dir: str):
+    reports = glob(pathname=glob_path, recursive=True)
     logger.debug(f"Found {len(reports)} all-tests.txt files")
 
     all_tests = {}
@@ -57,18 +39,17 @@ if __name__ == "__main__":
         with open(report, "r") as fp:
             logger.debug(f"Parsing {report}")
             for line in fp:
-                toks = line.split()
-                module = toks[0]
+                line_tokens = line.split(maxsplit=1)
+                module = line_tokens[0]
                 if module not in all_tests:
                     all_tests[module] = OrderedDict()
-                toks = toks[1].split("#")
-                clazz = toks[0]
+                test_tokens = line_tokens[1].split("#", maxsplit=1)
+                clazz = test_tokens[0]
                 if clazz not in all_tests[module]:
                     all_tests[module][clazz] = []
-                method = toks[1].strip("()")
+                method = test_tokens[1].rstrip("()")
                 all_tests[module][clazz].append(method)
 
-    out_dir = args.yaml_output_dir
     if not os.path.exists(out_dir):
         logger.debug(f"Creating output directory {out_dir}.")
         os.makedirs(out_dir)
@@ -80,3 +61,58 @@ if __name__ == "__main__":
         logger.debug(f"Writing {len(tests)} tests for {module} into {out_path}.")
         stream = open(out_path, "w")
         yaml.dump(dict(tests), stream)
+
+
+def yaml_to_all_tests(glob_path: str, out_file: str):
+    yamls = glob(pathname=glob_path, recursive=True)
+    logger.debug(f"Found {len(yamls)} YAML files")
+
+    with open(out_file, "w") as fp:
+        for yaml_file in yamls:
+            with open(yaml_file, "r") as yamp_fp:
+                tests = yaml.safe_load(yamp_fp)
+                for clazz, methods in tests.items():
+                    for method in methods:
+                        fp.write(f"{clazz}#{method}\n")
+
+
+
+if __name__ == "__main__":
+    """
+    """
+    parser = argparse.ArgumentParser(description="Convert test suite to and from YAML.")
+    subparsers = parser.add_subparsers(dest="command")
+    to_yaml_parser = subparsers.add_parser("to-yaml", help="Convert the module test suite data to YAML files")
+    to_yaml_parser.add_argument("--path",
+                                required=False,
+                                default="**/build/all-tests.txt",
+                                help="Path to all-tests.txt files. Glob patterns are supported.")
+    to_yaml_parser.add_argument("--yaml-output-dir",
+                                required=False,
+                                default="data/all-tests",
+                                help="Directory to output YAML files")
+
+    from_yaml_parser = subparsers.add_parser("from-yaml", help="Convert the YAML files to a test suite descriptor")
+    from_yaml_parser.add_argument("--path",
+                                  required=False,
+                                  default="data/all-tests/*.yaml",
+                                  help="Path to module YAML files. Glob patterns are supported.")
+    from_yaml_parser.add_argument("--output-file",
+                                  required=False,
+                                  default="all-tests.txt",
+                                  help="Output file location")
+
+    if not os.getenv("GITHUB_WORKSPACE"):
+        print("This script is intended to by run by GitHub Actions.")
+        exit(1)
+
+    args = parser.parse_args()
+    if args.command == "to-yaml":
+        all_tests_to_yaml(args.path, args.yaml_output_dir)
+        exit(0)
+    elif args.command == "from-yaml":
+        yaml_to_all_tests(args.path, args.output_file)
+        exit(0)
+    else:
+        print(f"Unknown sub-command: {args.command}")
+        exit(1)
