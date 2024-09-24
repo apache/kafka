dfc286813a6e15658ca01cd295fc83ab595d7476
Matthias J. Sax <matthias@confluent.io>
2024-09-23 19:58:14 -0700

    checkstyle

--- streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
+++ streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
@@ -65,7 +65,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
