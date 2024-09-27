e0d488a15dd969443f4042135b634ca17d6cbcac
TaiJu Wu <tjwu1217@gmail.com>
2024-09-28 07:36:14 +0800

    fix build

--- server/src/main/java/org/apache/kafka/server/metrics/ClientMetricsConfigs.java
+++ server/src/main/java/org/apache/kafka/server/metrics/ClientMetricsConfigs.java
@@ -29,7 +29,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Pattern;
