bbed52c55ee66842e57b3312d1125d7df92c9261
Chirag Wadhwa <cwadhwa@confluent.io>
2024-10-01 10:12:39 -0700

    KAFKA-17532-2: Minor formatting in import statements

--- core/src/test/java/kafka/server/share/SharePartitionTest.java
+++ core/src/test/java/kafka/server/share/SharePartitionTest.java
@@ -85,9 +85,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SharePartitionTest {
 
