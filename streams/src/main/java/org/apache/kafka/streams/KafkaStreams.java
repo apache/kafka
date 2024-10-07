578b48a7409202e24a212716e5908a9a7ae665d8
bbejeck <bbejeck@gmail.com>
2024-10-07 16:55:49 -0400

    Fix constructor that got mangled during rebase

--- streams/src/main/java/org/apache/kafka/streams/KafkaStreams.java
+++ streams/src/main/java/org/apache/kafka/streams/KafkaStreams.java
@@ -1015,7 +1015,7 @@ public class KafkaStreams implements AutoCloseable {
         log.info("Kafka Streams commit ID: {}", ClientMetrics.commitId());
 
         metrics = createMetrics(applicationConfigs, time, clientId);
-        final StreamsClientMetricsDelegatingReporter reporter = new StreamsClientMetricsDelegatingReporter(adminClient);
+        final StreamsClientMetricsDelegatingReporter reporter = new StreamsClientMetricsDelegatingReporter(adminClient, clientId);
         metrics.addReporter(reporter);
         
         streamsMetrics = new StreamsMetricsImpl(
