a0b09885625bfbae8cda73ba97b9e8af34210986
aliehsaeedii <asaeedi@confluent.io>
2024-10-10 19:39:01 +0200

    update KafkaConsumerTest

--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
@@ -21,7 +21,6 @@ import org.apache.kafka.common.utils.AbstractIterator;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
