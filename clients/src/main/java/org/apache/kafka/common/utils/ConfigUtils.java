07a53b2db1f6657e5ebc157761ce5684db060de9
mingdaoy <mingdaoy@gmail.com>
2024-10-11 15:36:46 +0800

    remove Unused import

--- clients/src/main/java/org/apache/kafka/common/utils/ConfigUtils.java
+++ clients/src/main/java/org/apache/kafka/common/utils/ConfigUtils.java
@@ -25,14 +25,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ConfigUtils {
 
