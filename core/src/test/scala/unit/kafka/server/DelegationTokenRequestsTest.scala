c072f9634f4e8132f6a818a72ed361d8072b0b52
frankvicky <kitingiao@gmail.com>
2024-10-14 19:20:19 +0800

    KAFKA-17748: Remove scala-java8-compact

--- core/src/test/scala/unit/kafka/server/DelegationTokenRequestsTest.scala
+++ core/src/test/scala/unit/kafka/server/DelegationTokenRequestsTest.scala
@@ -30,9 +30,9 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 import java.util
-import scala.compat.java8.OptionConverters
 import scala.concurrent.ExecutionException
 import scala.jdk.CollectionConverters._
+import scala.jdk.javaapi.OptionConverters
 
 class DelegationTokenRequestsTest extends IntegrationTestHarness with SaslSetup {
   override protected def securityProtocol = SecurityProtocol.SASL_PLAINTEXT
