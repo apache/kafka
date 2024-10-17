58d424d621454681458de82da2276cff6952a2b4
asaeedi <asaeedi@confluent.io>
2024-10-17 23:24:51 +0200

    fix cs error

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetchCollectorTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/FetchCollectorTest.java
@@ -50,7 +50,6 @@ import org.mockito.quality.Strictness;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
