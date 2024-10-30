9444caefd0cecd7dda982d292c304c6c7f0a74f3
Mahsa Seifikar <m.seifikar93@gmail.com>
2024-10-30 11:20:46 -0400

    MINOR: Correct error message in reassign_partitions_test.py (#17632)
    Reviewers: Justine Olshan <jolshan@confluent.io>

--- tests/kafkatest/tests/core/reassign_partitions_test.py
+++ tests/kafkatest/tests/core/reassign_partitions_test.py
@@ -104,7 +104,7 @@ class ReassignPartitionsTest(ProduceConsumeValidateTest):
         # ensure all partitions have data so we don't hit OutOfOrderExceptions due to broker restarts
         wait_until(check_all_partitions,
                    timeout_sec=60,
-                   err_msg="Failed to produce to all partitions in 30s")
+                   err_msg="Failed to produce to all partitions in 60s")
 
         # send reassign partitions command
         self.kafka.execute_reassign_partitions(partition_info)
