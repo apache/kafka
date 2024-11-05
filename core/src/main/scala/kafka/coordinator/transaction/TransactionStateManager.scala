7ee8a31183bb31f5169eb0cc9ea198ad98b2efec
Vincent Jiang <84371940+vincent81jiang@users.noreply.github.com>
2024-11-05 14:22:20 -0800

    Fix TransactionStateManager handling of empty batch when loading transaction metadata (#17688)
    When loading transaction metadata from a transaction log partition, if the partition contains a segment ending with an empty batch, "currOffset" update logic at will be skipped for the last batch. Since "currOffset" is not advanced to next offset of last batch properly, TransactionStateManager.loadTransactionMetadata method will be stuck in the "while" loop.
    
    This change fixes the issue by updating "currOffset" after processing each batch, whether the batch is empty or not.
    
    Reviewers: Justine Olshan <jolshan@confluent.io>, Jun Rao <junrao@gmail.com>

--- core/src/main/scala/kafka/coordinator/transaction/TransactionStateManager.scala
+++ core/src/main/scala/kafka/coordinator/transaction/TransactionStateManager.scala
@@ -477,7 +477,6 @@ class TransactionStateManager(brokerId: Int,
                       case Some(txnMetadata) =>
                         loadedTransactions.put(transactionalId, txnMetadata)
                     }
-                    currOffset = batch.nextOffset
 
                   case unknownKey: UnknownKey =>
                     warn(s"Unknown message key with version ${unknownKey.version}" +
@@ -485,6 +484,7 @@ class TransactionStateManager(brokerId: Int,
                       "It could be a left over from an aborted upgrade.")
                 }
               }
+              currOffset = batch.nextOffset
             }
           }
         } catch {
