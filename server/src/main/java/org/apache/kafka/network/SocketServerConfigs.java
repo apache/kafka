4853a2ef76aa903644a27b740efc15811e8c6758
Chia-Chuan Yu <yujuan476@gmail.com>
2024-11-30 03:12:31 +0800

    KAFKA-16617 Add KRaft info for the `advertised.listeners` doc description (#17552)
    Reviewers: Chia-Ping Tsai <chia7712@gmail.com>

--- server/src/main/java/org/apache/kafka/network/SocketServerConfigs.java
+++ server/src/main/java/org/apache/kafka/network/SocketServerConfigs.java
@@ -78,8 +78,10 @@ public class SocketServerConfigs {
             " <code>PLAINTEXT://127.0.0.1:9092,SSL://[::1]:9092</code>%n";
 
     public static final String ADVERTISED_LISTENERS_CONFIG = "advertised.listeners";
-    public static final String ADVERTISED_LISTENERS_DOC = String.format(
-            "Listeners to publish to ZooKeeper for clients to use, if different than the <code>%s</code> config property." +
+    public static final String ADVERTISED_LISTENERS_DOC = String.format("Specifies the listener addresses that the Kafka brokers will advertise to clients and other brokers." +
+                    " The config is useful where the actual listener configuration <code>%s</code> does not represent the addresses that clients should" +
+                    " use to connect, such as in cloud environments. In environments using ZooKeeper, these addresses are published to ZooKeeper." +
+                    " In Kraft mode, the address would be published to and managed by kraft controller, the brokers would pull these data from controller as needed." +
                     " In IaaS environments, this may need to be different from the interface to which the broker binds." +
                     " If this is not set, the value for <code>%1$1s</code> will be used." +
                     " Unlike <code>%1$1s</code>, it is not valid to advertise the 0.0.0.0 meta-address.%n" +
