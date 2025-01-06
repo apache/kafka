0a828d6e5a9f01e732e8ee852b351ebe2499bdfe
Kirk True <kirk@kirktrue.pro>
2025-01-06 12:15:11 -0800

    Fixed lint failures

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/FetchRequestManager.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/FetchRequestManager.java
@@ -39,14 +39,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
-import static org.apache.kafka.clients.consumer.internals.AbstractMembershipManager.TOPIC_PARTITION_COMPARATOR;
 
 /**
  * {@code FetchRequestManager} is responsible for generating {@link FetchRequest} that represent the
