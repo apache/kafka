80741fe013caa34931a7e5e3a33178cd415d6d56
aliehsaeedii <asaeedi@confluent.io>
2025-01-13 08:00:11 +0100

    nit: add uid=502(aliehsaeedi) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh),400(com.apple.access_remote_ae),702(com.apple.sharepoint.group.2)

--- tools/src/main/java/org/apache/kafka/tools/streams/StreamsGroupCommand.java
+++ tools/src/main/java/org/apache/kafka/tools/streams/StreamsGroupCommand.java
@@ -211,7 +211,7 @@ public class StreamsGroupCommand {
                 } else {
                     String fmt = "%" + -groupLen + "s %-15s% %-15s%" + -maxMemberIdLen + "s %s %15s %" + -maxHostLen + "s %" + -maxClientIdLen + "s\n";
                     for (StreamsGroupMemberDescription member : members) {
-                        System.out.printf(fmt, "GROUP", "TARGET-ASSIGNMENT-EPOCH", "TOPOLOGY-EPOCH", "MEMBER", "MEMBER-PROTOCOL", "MEMBER-EPOCH", "PROCESS", "CLIENT");
+                        System.out.printf(fmt, "GROUP", "TARGET-ASSIGNMENT-EPOCH", "TOPOLOGY-EPOCH", "MEMBER", "MEMBER-PROTOCOL", "MEMBER-EPOCH", "PROCESS", "CLIENT-ID");
                         System.out.printf(fmt, description.groupId(), description.targetAssignmentEpoch(), description.topologyEpoch(), member.memberId(),
                             member.isClassic() ? "classic" : "streams", member.memberEpoch(), member.processId(), member.clientId());
                         printTasks(member.assignment(), false);
