6306f7bdd63eb6fe74b0ac8b09f0d0a216e81712
Mickael Maison <mickael.maison@gmail.com>
2025-01-14 18:03:29 +0100

    KAFKA-14485: Move LogCleaner exceptions to storage module

--- storage/src/main/java/org/apache/kafka/storage/internals/log/LogCleaningAbortedException.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/LogCleaningAbortedException.java
@@ -1,10 +1,10 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,11 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package kafka.common
+package org.apache.kafka.storage.internals.log;
 
 /**
  * Thrown when a log cleaning task is requested to be aborted.
  */
-class LogCleaningAbortedException extends RuntimeException() {
+public class LogCleaningAbortedException extends RuntimeException {
 }
