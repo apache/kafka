13bb5ef7a9cab111552583a7636644e83e5b84c3
Kuan-Po Tseng <brandboat@gmail.com>
2025-01-16 20:47:13 +0800

    Address comment

--- tools/src/main/java/org/apache/kafka/tools/consumer/ApiMessageFormatter.java
+++ tools/src/main/java/org/apache/kafka/tools/consumer/ApiMessageFormatter.java
@@ -38,7 +38,7 @@ public abstract class ApiMessageFormatter implements MessageFormatter {
     private static final String DATA = "data";
     private static final String KEY = "key";
     private static final String VALUE = "value";
-    public static final String UNKNOWN = "unknown";
+    protected static final String UNKNOWN = "unknown";
 
     @Override
     public void writeTo(ConsumerRecord<byte[], byte[]> consumerRecord, PrintStream output) {
