67166ee6a891264a7310c58d0d8cab1d70299ee4
pramithas <dhakalpramithas@gmail.com>
2025-01-20 18:22:55 +0545

    Make forceUnmap protected

--- storage/src/main/java/org/apache/kafka/storage/internals/log/AbstractIndex.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/AbstractIndex.java
@@ -400,7 +400,7 @@ public abstract class AbstractIndex implements Closeable {
      * Forcefully free the buffer's mmap.
      */
     // Visible for testing, we can make this protected once OffsetIndexTest is in the same package as this class
-    public void forceUnmap() throws IOException {
+    protected void forceUnmap() throws IOException {
         try {
             ByteBufferUnmapper.unmap(file.getAbsolutePath(), mmap);
         } finally {
