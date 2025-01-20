3f91f144fbf7713b3960c9f09fcbe5c7efcb184f
loganzhu <loganZhu96@outlook.com>
2025-01-20 11:39:40 +0800

    KAFKA-18597: Fix test issue

--- core/src/main/scala/kafka/log/LogCleaner.scala
+++ core/src/main/scala/kafka/log/LogCleaner.scala
@@ -117,10 +117,10 @@ class LogCleaner(initialConfig: CleanerConfig,
 
   /**
    * @param f to compute the result
-   * @return the max value (int value) or 0 if there is no cleaner
+   * @return the max value or 0 if there is no cleaner
    */
-  private[log] def maxOverCleanerThreads(f: CleanerThread => Double): Int =
-    cleaners.map(f).maxOption.getOrElse(0.0d).toInt
+  private[log] def maxOverCleanerThreads(f: CleanerThread => Double): Double =
+    cleaners.map(f).maxOption.getOrElse(0.0d)
 
   /* a metric to track the maximum utilization of any thread's buffer in the last cleaning */
   metricsGroup.newGauge(MaxBufferUtilizationPercentMetricName,
