6b95c7dc2812a4228d59cc8e10385cd28415deef
mingdaoy <mingdaoy@gmail.com>
2025-01-25 22:31:46 +0800

    KAFKA-18499 Clean up zookeeper from LogConfig (#18583)
    Reviewers: Chia-Ping Tsai <chia7712@gmail.com>

--- core/src/main/scala/kafka/server/ControllerConfigurationValidator.scala
+++ core/src/main/scala/kafka/server/ControllerConfigurationValidator.scala
@@ -118,7 +118,7 @@ class ControllerConfigurationValidator(kafkaConfig: KafkaConfig) extends Configu
             nullTopicConfigs.mkString(","))
         }
         LogConfig.validate(oldConfigs, properties, kafkaConfig.extractLogConfigMap,
-          kafkaConfig.remoteLogManagerConfig.isRemoteStorageSystemEnabled(), false)
+          kafkaConfig.remoteLogManagerConfig.isRemoteStorageSystemEnabled())
       case BROKER => validateBrokerName(resource.name())
       case CLIENT_METRICS =>
         val properties = new Properties()
