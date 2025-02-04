ec508ede5f61837c96d63399b9791c8dee483ab5
Sean Quah <squah@confluent.io>
2025-02-04 15:53:04 +0000

    run spotless

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerMetadata.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerMetadata.java
@@ -18,7 +18,6 @@ package org.apache.kafka.clients.consumer.internals;
 
 import org.apache.kafka.clients.Metadata;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.internals.ClusterResourceListeners;
 import org.apache.kafka.common.requests.MetadataRequest;
 import org.apache.kafka.common.utils.LogContext;
