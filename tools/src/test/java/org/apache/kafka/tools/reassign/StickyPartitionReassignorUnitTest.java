4653927201c676235848125aec505c968a958d74
ArneSchulze <arne.artur.schulze@sap.com>
2025-02-14 10:48:29 +0100

    Add copyright headers

--- tools/src/test/java/org/apache/kafka/tools/reassign/StickyPartitionReassignorUnitTest.java
+++ tools/src/test/java/org/apache/kafka/tools/reassign/StickyPartitionReassignorUnitTest.java
@@ -13,6 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * (c) Copyright 2025, SAP SE and apache/kafka contributors
  */
 package org.apache.kafka.tools.reassign;
 
