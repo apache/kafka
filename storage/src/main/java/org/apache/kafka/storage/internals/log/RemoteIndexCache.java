e554de1b7eeb172f0e31fc3a51f56ca2094251dc
TaiJu Wu <tjwu1217@gmail.com>
2025-02-18 12:38:45 +0000

    Revert "test comments"
    This reverts commit a3cb2395b1f22cd80aba23da1351a223fc6816cf.

--- storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/RemoteIndexCache.java
@@ -581,7 +581,6 @@ public class RemoteIndexCache implements Closeable {
             try {
                 if (!markedForCleanup) {
                     markedForCleanup = true;
-                    System.err.println("markForCleanup " + Utils.replaceSuffix(offsetIndex.file().getPath(), "", LogFileUtils.DELETED_FILE_SUFFIX));
                     offsetIndex.renameTo(new File(Utils.replaceSuffix(offsetIndex.file().getPath(), "", LogFileUtils.DELETED_FILE_SUFFIX)));
                     timeIndex.renameTo(new File(Utils.replaceSuffix(timeIndex.file().getPath(), "", LogFileUtils.DELETED_FILE_SUFFIX)));
                     txnIndex.renameTo(new File(Utils.replaceSuffix(txnIndex.file().getPath(), "", LogFileUtils.DELETED_FILE_SUFFIX)));
@@ -732,7 +731,6 @@ public class RemoteIndexCache implements Closeable {
 
     // Visible for testing
     public static String remoteDeletedSuffixIndexFileName(RemoteLogSegmentMetadata remoteLogSegmentMetadata) {
-        System.err.println("remoteDeletedSuffixIndexFileName " + generateFileNamePrefixForIndex(remoteLogSegmentMetadata) + LogFileUtils.DELETED_FILE_SUFFIX);
         return generateFileNamePrefixForIndex(remoteLogSegmentMetadata) + LogFileUtils.DELETED_FILE_SUFFIX;
     }
 
