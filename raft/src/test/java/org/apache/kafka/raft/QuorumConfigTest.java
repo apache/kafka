45a21a1d69635f24e6785163f2d0351830bc738c
TaiJu Wu <tjwu1217@gmail.com>
2025-02-21 21:45:36 +0000

    rename function

--- raft/src/test/java/org/apache/kafka/raft/QuorumConfigTest.java
+++ raft/src/test/java/org/apache/kafka/raft/QuorumConfigTest.java
@@ -27,15 +27,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class QuorumConfigTest {
     @Test
     public void testPositiveConfig() {
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_ELECTION_TIMEOUT_MS_CONFIG, "0"));
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_FETCH_TIMEOUT_MS_CONFIG, "0"));
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_ELECTION_BACKOFF_MAX_MS_CONFIG, "0"));
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_LINGER_MS_CONFIG, "0"));
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_REQUEST_TIMEOUT_MS_CONFIG, "0"));
-        verifyPositionConfig(Map.of(QuorumConfig.QUORUM_RETRY_BACKOFF_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_ELECTION_TIMEOUT_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_FETCH_TIMEOUT_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_ELECTION_BACKOFF_MAX_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_LINGER_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_REQUEST_TIMEOUT_MS_CONFIG, "0"));
+        verifyPositiveConfig(Map.of(QuorumConfig.QUORUM_RETRY_BACKOFF_MS_CONFIG, "0"));
     }
 
-    private void verifyPositionConfig(Map<String, Object> overrideConfig) {
+    private void verifyPositiveConfig(Map<String, Object> overrideConfig) {
         Map<String, Object> props = new HashMap<>();
         props.put(QuorumConfig.QUORUM_VOTERS_CONFIG, "1@localhost:9092");
         props.put(QuorumConfig.QUORUM_BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
