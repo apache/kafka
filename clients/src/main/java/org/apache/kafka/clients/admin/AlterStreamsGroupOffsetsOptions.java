193eeeb0cbc8db9f7ca70b58084f1cd96d885012
aliehsaeedii <asaeedi@confluent.io>
2025-03-05 17:00:32 +0100

    fix

--- clients/src/main/java/org/apache/kafka/clients/admin/AlterStreamsGroupOffsetsOptions.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/AlterStreamsGroupOffsetsOptions.java
@@ -18,9 +18,11 @@ package org.apache.kafka.clients.admin;
 
 import org.apache.kafka.common.annotation.InterfaceStability;
 
+import java.util.Map;
+
 
 /**
- * Options for the {@link Admin#alterStreamsGroupOffsets(String groupId, Map), AlterStreamsGroupOffsetsOptions)} call.
+ * Options for the {@link Admin#alterStreamsGroupOffsets(String, Map, AlterStreamsGroupOffsetsOptions)} call.
  * <p>
  * The API of this class is evolving, see {@link Admin} for details.
  */
