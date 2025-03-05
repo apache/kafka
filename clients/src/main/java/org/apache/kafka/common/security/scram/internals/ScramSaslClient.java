4581b72fff691f81a7a4db226a7cd4a6d01f5e83
Mingdao Yang <mingdaoy@gmail.com>
2025-03-06 04:32:21 +0800

    MINOR: Optimize password handling in ScramSaslClient

--- clients/src/main/java/org/apache/kafka/common/security/scram/internals/ScramSaslClient.java
+++ clients/src/main/java/org/apache/kafka/common/security/scram/internals/ScramSaslClient.java
@@ -190,7 +190,7 @@ public class ScramSaslClient implements SaslClient {
 
     private ClientFinalMessage handleServerFirstMessage(char[] password) throws SaslException {
         try {
-            byte[] passwordBytes = ScramFormatter.normalize(new String(password).toCharArray());
+            byte[] passwordBytes = ScramFormatter.normalize(password);
             this.saltedPassword = formatter.hi(passwordBytes, serverFirstMessage.salt(), serverFirstMessage.iterations());
 
             ClientFinalMessage clientFinalMessage = new ClientFinalMessage("n,,".getBytes(StandardCharsets.UTF_8), serverFirstMessage.nonce());
