426854a090c97d7b564ce0ef43cc6e87ad6243cc
adixitconfluent <adixit@confluent.io>
2025-03-08 21:50:46 +0530

    Addressed Apoorv'r round 1 review comment

--- server/src/main/java/org/apache/kafka/server/share/context/ShareSessionContext.java
+++ server/src/main/java/org/apache/kafka/server/share/context/ShareSessionContext.java
@@ -41,7 +41,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
-import java.util.Set;
 
 /**
  * The context for a share session fetch request.
