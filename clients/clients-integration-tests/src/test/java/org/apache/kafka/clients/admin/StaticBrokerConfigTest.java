cde4208dd41596d75dba54c65d18eee8773dd705
frankvicky <kitingiao@gmail.com>
2025-03-17 11:05:28 +0800

    refactor

--- clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/admin/StaticBrokerConfigTest.java
+++ clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/admin/StaticBrokerConfigTest.java
@@ -20,8 +20,8 @@ package org.apache.kafka.clients.admin;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.test.ClusterInstance;
-import org.apache.kafka.common.test.api.ClusterConfig;
-import org.apache.kafka.common.test.api.ClusterTemplate;
+import org.apache.kafka.common.test.api.ClusterConfigProperty;
+import org.apache.kafka.common.test.api.ClusterTest;
 import org.apache.kafka.common.test.api.Type;
 import org.apache.kafka.common.test.junit.ClusterTestExtensions;
 import org.apache.kafka.coordinator.group.GroupCoordinatorConfig;
@@ -30,12 +30,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(value = ClusterTestExtensions.class)
 public class StaticBrokerConfigTest {
@@ -47,100 +45,67 @@ public class StaticBrokerConfigTest {
      */
     private static final String LOG_SEGMENT_BYTES = "log.segment.bytes";
 
-    @ClusterTemplate("controllerTestConfig")
-    public void testDescribeConfigsShouldNotReturnTopicRelatedQuorumControllerConfigs(ClusterInstance cluster)
-        throws ExecutionException, InterruptedException {
-        try (Admin admin = cluster.admin()) {
-            CreateTopicsResult createResult = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)));
-            ConfigEntry config = createResult.config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG);
-            assertNotNull(config, "Create Topic result should include static topic config");
-            assertEquals(CUSTOM_VALUE, config.value(), "Config value should be custom value since controller have related static config");
-
-            ConfigResource resource = new ConfigResource(ConfigResource.Type.BROKER, "0");
-            Config keyToConfigEntry = admin.describeConfigs(List.of(resource)).all().get().get(resource);
-            assertNotEquals(CUSTOM_VALUE, keyToConfigEntry.get(LOG_SEGMENT_BYTES).value(),
-                "Config value should not be custom value since broker don't have related static config");
-        }
-    }
-
-    @ClusterTemplate("controllerTestConfig")
-    public void testDescribeConfigsShouldReturnTopicRelatedQuorumControllerConfigsWhenUsingBootstrapController(ClusterInstance cluster)
+    @ClusterTest(types = {Type.KRAFT},
+        serverProperties = {
+            @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_PARTITIONS_CONFIG, value = "1"),
+            @ClusterConfigProperty(id = 3000, key = LOG_SEGMENT_BYTES, value = CUSTOM_VALUE)
+        })
+    public void testTopicConfigsGetImpactedIfStaticConfigsAddToController(ClusterInstance cluster)
         throws ExecutionException, InterruptedException {
         try (
             Admin admin = cluster.admin();
-            Admin adminUsingBootstrapController = cluster.admin(Map.of(), true);
+            Admin adminUsingBootstrapController = cluster.admin(Map.of(), true)
         ) {
-            CreateTopicsResult createResult = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)));
-            ConfigEntry config = createResult.config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG);
-            assertNotNull(config, "Create Topic result should include static topic config");
-            assertEquals(CUSTOM_VALUE, config.value(), "Config value should be custom value since controller have related static config");
+            String value = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)))
+                .config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG).value();
+            assertEquals(CUSTOM_VALUE, value, "Config value should be custom value since controller have related static config");
 
-            ConfigResource resource = new ConfigResource(ConfigResource.Type.BROKER, "3000");
-            Config keyToConfigEntry = adminUsingBootstrapController.describeConfigs(List.of(resource)).all().get().get(resource);
-            assertEquals(CUSTOM_VALUE, keyToConfigEntry.get(LOG_SEGMENT_BYTES).value(),
+            ConfigResource brokerResource = new ConfigResource(ConfigResource.Type.BROKER, "0");
+            String valueFromBroker = admin.describeConfigs(List.of(brokerResource))
+                .all().get().get(brokerResource).get(LOG_SEGMENT_BYTES).value();
+            assertNotEquals(CUSTOM_VALUE,
+                valueFromBroker,
+                "Config value should not be custom value since broker don't have related static config");
+
+            ConfigResource controllerResource = new ConfigResource(ConfigResource.Type.BROKER, "3000");
+            String valueFromController = adminUsingBootstrapController.describeConfigs(List.of(controllerResource))
+                .all().get().get(controllerResource).get(LOG_SEGMENT_BYTES).value();
+            assertEquals(CUSTOM_VALUE,
+                valueFromController,
                 "Config value should be custom value since controller have related static config");
         }
     }
 
-    @ClusterTemplate("brokerTestConfig")
-    public void testDescribeConfigsShouldReturnTopicRelatedBrokerConfigsEvenIfNotInTopicCreation(ClusterInstance cluster)
+    @ClusterTest(types = {Type.KRAFT},
+        serverProperties = {
+            @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_PARTITIONS_CONFIG, value = "1"),
+            @ClusterConfigProperty(id = 0, key = LOG_SEGMENT_BYTES, value = CUSTOM_VALUE)
+        })
+    public void testTopicConfigsGetImpactedIfStaticConfigsAddToBroker(ClusterInstance cluster)
         throws ExecutionException, InterruptedException {
         try (
             Admin admin = cluster.admin();
+            Admin adminUsingBootstrapController = cluster.admin(Map.of(), true)
         ) {
-            CreateTopicsResult createResult = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)));
-            ConfigEntry config = createResult.config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG);
-            assertNotNull(config, "Create Topic result should include default topic config");
-            assertNotEquals(CUSTOM_VALUE, config.value(),
+            String value = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)))
+                .config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG).value();
+            assertNotEquals(CUSTOM_VALUE,
+                value,
                 "Config value should not be custom value since controller don't have static config");
 
-            ConfigResource resource = new ConfigResource(ConfigResource.Type.BROKER, "0");
-            Config configResourceConfigMap = admin.describeConfigs(List.of(resource)).all().get().get(resource);
-            ConfigEntry configValue = configResourceConfigMap.get(LOG_SEGMENT_BYTES);
-            assertNotNull(configValue, "Broker should include related static config");
-            assertEquals(CUSTOM_VALUE, configValue.value(), "Config value should be custom value since broker have related static config");
+            ConfigResource brokerResource = new ConfigResource(ConfigResource.Type.BROKER, "0");
+            String valueFromBroker = admin.describeConfigs(List.of(brokerResource))
+                .all().get().get(brokerResource).get(LOG_SEGMENT_BYTES).value();
+            assertEquals(CUSTOM_VALUE,
+                valueFromBroker,
+                "Config value should be custom value since broker have related static config");
+
+            ConfigResource controllerResource = new ConfigResource(ConfigResource.Type.BROKER, "3000");
+            String valueFromController = adminUsingBootstrapController.describeConfigs(List.of(controllerResource))
+                .all().get().get(controllerResource).get(LOG_SEGMENT_BYTES).value();
+            assertNotEquals(CUSTOM_VALUE,
+                valueFromController,
+                "Config value should not be custom value since controller don't have related static config");
         }
-    }
-
-    @ClusterTemplate("brokerTestConfig")
-    public void testDescribeConfigsShouldNotReturnTopicRelatedBrokerConfigsWhenUsingBootstrapController(ClusterInstance cluster)
-        throws ExecutionException, InterruptedException {
-        try (
-            Admin admin = cluster.admin();
-            Admin adminUsingBootstrapController = cluster.admin(Map.of(), true);
-        ) {
-            CreateTopicsResult createResult = admin.createTopics(List.of(new NewTopic(TOPIC, 1, (short) 1)));
-            ConfigEntry config = createResult.config(TOPIC).get().get(TopicConfig.SEGMENT_BYTES_CONFIG);
-            assertNotNull(config, "Create Topic result should include default topic config");
-            assertNotEquals(CUSTOM_VALUE, config.value(),
-                "Config value should not be custom value since controller don't have static config");
-
-            ConfigResource resource = new ConfigResource(ConfigResource.Type.BROKER, "3000");
-            Config configResourceConfigMap = adminUsingBootstrapController.describeConfigs(List.of(resource)).all().get().get(resource);
-            ConfigEntry configValue = configResourceConfigMap.get(LOG_SEGMENT_BYTES);
-            assertNotNull(configValue);
-            assertNotEquals(CUSTOM_VALUE, configValue.value(), "Config value should not be custom value since controller don't have related static config");
-        }
-    }
-
-
-    private static List<ClusterConfig> controllerTestConfig() {
-        return List.of(ClusterConfig.defaultBuilder()
-            .setTypes(Set.of(Type.KRAFT))
-            .setPerServerProperties(Map.of(3000, Map.of(LOG_SEGMENT_BYTES, CUSTOM_VALUE)))
-            .setServerProperties(Map.of(
-                GroupCoordinatorConfig.OFFSETS_TOPIC_PARTITIONS_CONFIG, "1", GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, "1")
-            )
-            .build());
-    }
-
-    private static List<ClusterConfig> brokerTestConfig() {
-        return List.of(ClusterConfig.defaultBuilder()
-            .setTypes(Set.of(Type.KRAFT))
-            .setPerServerProperties(Map.of(0, Map.of(LOG_SEGMENT_BYTES, CUSTOM_VALUE)))
-            .setServerProperties(Map.of(
-                GroupCoordinatorConfig.OFFSETS_TOPIC_PARTITIONS_CONFIG, "1", GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, "1")
-            )
-            .build());
     }
 }
