ecb5720f68f7a5dd0a6641ff13ac8a54cc66e068
clark <clark@hokplay.com>
2025-03-20 00:34:58 +0800

    feat: Cover rebootstrap of classic consumer test cases.

--- clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/ClientRebootstrapTest.java
+++ clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/ClientRebootstrapTest.java
@@ -17,11 +17,12 @@
 package org.apache.kafka.clients;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.GroupProtocol;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.test.ClusterInstance;
-import org.apache.kafka.common.test.TestUtils;
 import org.apache.kafka.common.test.api.ClusterConfigProperty;
 import org.apache.kafka.common.test.api.ClusterTest;
 import org.apache.kafka.common.test.api.Type;
@@ -101,6 +102,55 @@ public class ClientRebootstrapTest {
         admin.close(Duration.ZERO);
     }
 
+    public void consumerRebootstrap(ClusterInstance clusterInstance, GroupProtocol groupProtocol) throws InterruptedException, ExecutionException {
+        clusterInstance.createTopic(TOPIC, 1, (short) REPLICAS);
+
+        var part = 0;
+        var broker0 = 0;
+        var broker1 = 1;
+        var partitions = List.of(new TopicPartition(TOPIC, part));
+
+        try (var producer = clusterInstance.producer()) {
+            var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 0".getBytes(), "value 0".getBytes())).get();
+            assertEquals(0, recordMetadata.offset());
+            producer.flush();
+        }
+
+        clusterInstance.shutdownBroker(broker0);
+
+        try (var consumer = clusterInstance.consumer(Map.of(ConsumerConfig.GROUP_PROTOCOL_CONFIG, groupProtocol.name))) {
+            // Only the server 1 is available for the consumer during the bootstrap.
+            consumer.assign(partitions);
+            consumer.seekToBeginning(partitions);
+            assertEquals(1, consumer.poll(Duration.ofSeconds(1)).count());
+
+            // Bring back the server 0 and shut down 1.
+            clusterInstance.shutdownBroker(broker1);
+            clusterInstance.startBroker(broker0);
+
+            try (var producer = clusterInstance.producer()) {
+                var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 1".getBytes(), "value 1".getBytes())).get();
+                assertEquals(1, recordMetadata.offset());
+                producer.flush();
+            }
+
+            // The server 1 originally cached during the bootstrap, is offline.
+            // However, the server 0 from the bootstrap list is online.
+            assertEquals(1, consumer.poll(Duration.ofSeconds(1)).count());
+        }
+    }
+
+    @ClusterTest(
+        brokers = REPLICAS,
+        types = {Type.KRAFT},
+        serverProperties = {
+            @ClusterConfigProperty(key = TopicConfig.UNCLEAN_LEADER_ELECTION_ENABLE_CONFIG, value = "true"),
+            @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, value = "2"),
+        })
+    public void testClassicConsumerRebootstrap(ClusterInstance clusterInstance) throws InterruptedException, ExecutionException {
+        consumerRebootstrap(clusterInstance, GroupProtocol.CLASSIC);
+    }
+
     @ClusterTest(
         brokers = REPLICAS,
         types = {Type.KRAFT},
@@ -109,6 +159,10 @@ public class ClientRebootstrapTest {
             @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, value = "2"),
         })
     public void testConsumerRebootstrap(ClusterInstance clusterInstance) throws InterruptedException, ExecutionException {
+        consumerRebootstrap(clusterInstance, GroupProtocol.CONSUMER);
+    }
+
+    public void consumerRebootstrapDisabled(ClusterInstance clusterInstance, GroupProtocol groupProtocol) throws InterruptedException, ExecutionException {
         clusterInstance.createTopic(TOPIC, 1, (short) REPLICAS);
 
         var part = 0;
@@ -119,16 +173,19 @@ public class ClientRebootstrapTest {
         try (var producer = clusterInstance.producer()) {
             var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 0".getBytes(), "value 0".getBytes())).get();
             assertEquals(0, recordMetadata.offset());
+            producer.flush();
         }
 
         clusterInstance.shutdownBroker(broker0);
 
-        try (var consumer = clusterInstance.consumer()) {
+        try (var consumer = clusterInstance.consumer(Map.of(
+            CommonClientConfigs.METADATA_RECOVERY_STRATEGY_CONFIG, "none",
+            ConsumerConfig.GROUP_PROTOCOL_CONFIG, groupProtocol.name)
+        )) {
             // Only the server 1 is available for the consumer during the bootstrap.
             consumer.assign(List.of(tp));
             consumer.seekToBeginning(List.of(tp));
-
-            assertDoesNotThrow(() -> consumer.poll(Duration.ofMillis(100)).count() == 1);
+            assertEquals(1, consumer.poll(Duration.ofSeconds(1)).count());
 
             // Bring back the server 0 and shut down 1.
             clusterInstance.shutdownBroker(broker1);
@@ -137,11 +194,12 @@ public class ClientRebootstrapTest {
             try (var producer = clusterInstance.producer()) {
                 var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 1".getBytes(), "value 1".getBytes())).get();
                 assertEquals(1, recordMetadata.offset());
+                producer.flush();
             }
 
             // The server 1 originally cached during the bootstrap, is offline.
             // However, the server 0 from the bootstrap list is online.
-            assertDoesNotThrow(() -> consumer.poll(Duration.ofMillis(100)).count() == 1);
+            assertEquals(0, consumer.poll(Duration.ofSeconds(1)).count());
         }
     }
 
@@ -153,40 +211,19 @@ public class ClientRebootstrapTest {
             @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, value = "2")
         }
     )
+    public void testClassicConsumerRebootstrapDisabled(ClusterInstance clusterInstance) throws InterruptedException, ExecutionException {
+        consumerRebootstrapDisabled(clusterInstance, GroupProtocol.CLASSIC);
+    }
+
+    @ClusterTest(
+        brokers = REPLICAS,
+        types = {Type.KRAFT},
+        serverProperties = {
+            @ClusterConfigProperty(key = TopicConfig.UNCLEAN_LEADER_ELECTION_ENABLE_CONFIG, value = "true"),
+            @ClusterConfigProperty(key = GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, value = "2")
+        }
+    )
     public void testConsumerRebootstrapDisabled(ClusterInstance clusterInstance) throws InterruptedException, ExecutionException {
-        clusterInstance.createTopic(TOPIC, 1, (short) REPLICAS);
-
-        var part = 0;
-        var broker0 = 0;
-        var broker1 = 1;
-        var tp = new TopicPartition(TOPIC, part);
-
-        try (var producer = clusterInstance.producer()) {
-            var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 0".getBytes(), "value 0".getBytes())).get();
-            assertEquals(0, recordMetadata.offset());
-        }
-
-        clusterInstance.shutdownBroker(broker0);
-
-        try (var consumer = clusterInstance.consumer(Map.of(CommonClientConfigs.METADATA_RECOVERY_STRATEGY_CONFIG, "none"))) {
-            // Only the server 1 is available for the consumer during the bootstrap.
-            consumer.assign(List.of(tp));
-            consumer.seekToBeginning(List.of(tp));
-
-            TestUtils.waitForCondition(() -> consumer.poll(Duration.ofMillis(1000)).count() == 1, 500, "Timeout");
-
-            // Bring back the server 0 and shut down 1.
-            clusterInstance.shutdownBroker(broker1);
-            clusterInstance.startBroker(broker0);
-
-            try (var producer = clusterInstance.producer()) {
-                var recordMetadata = producer.send(new ProducerRecord<>(TOPIC, part, "key 1".getBytes(), "value 1".getBytes())).get();
-                assertEquals(1, recordMetadata.offset());
-            }
-
-            // The server 1 originally cached during the bootstrap, is offline.
-            // However, the server 0 from the bootstrap list is online.
-            TestUtils.waitForCondition(() -> consumer.poll(Duration.ofMillis(1000)).count() == 1, 500, "Timeout");
-        }
+        consumerRebootstrapDisabled(clusterInstance, GroupProtocol.CONSUMER);
     }
 }
