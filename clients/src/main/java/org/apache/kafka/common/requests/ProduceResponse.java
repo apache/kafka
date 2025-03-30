c20302d8e90d2197189c69120b743e3717181b9c
Lorcan <lorcanjames1@gmail.com>
2025-03-30 13:56:39 +0100

    more replacements of hashmap for enums

--- clients/src/main/java/org/apache/kafka/common/requests/ProduceResponse.java
+++ clients/src/main/java/org/apache/kafka/common/requests/ProduceResponse.java
@@ -27,7 +27,7 @@ import org.apache.kafka.common.record.RecordBatch;
 
 import java.nio.ByteBuffer;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -151,7 +151,7 @@ public class ProduceResponse extends AbstractResponse {
 
     @Override
     public Map<Errors, Integer> errorCounts() {
-        Map<Errors, Integer> errorCounts = new HashMap<>();
+        Map<Errors, Integer> errorCounts = new EnumMap<>(Errors.class);
         data.responses().forEach(t -> t.partitionResponses().forEach(p -> updateErrorCounts(errorCounts, Errors.forCode(p.errorCode()))));
         return errorCounts;
     }
