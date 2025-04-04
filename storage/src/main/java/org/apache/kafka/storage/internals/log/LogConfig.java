36e59c8af8ed8b1a010d3b92d7b52b67e0313606
m1a2st <s7133700@gmail.com>
2025-04-04 17:29:31 +0800

    use spotlessApply

--- storage/src/main/java/org/apache/kafka/storage/internals/log/LogConfig.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/LogConfig.java
@@ -26,7 +26,6 @@ import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.errors.InvalidConfigurationException;
 import org.apache.kafka.common.record.CompressionType;
-import org.apache.kafka.common.record.LegacyRecord;
 import org.apache.kafka.common.record.Records;
 import org.apache.kafka.common.record.TimestampType;
 import org.apache.kafka.common.utils.ConfigUtils;
