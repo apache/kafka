9c492800dbb0543f4c6c98c1f25e9142172d12cb
m1a2st <s7133700@gmail.com>
2025-04-05 09:49:24 +0800

    addressed by comment

--- streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
+++ streams/src/main/java/org/apache/kafka/streams/StreamsConfig.java
@@ -1050,6 +1050,11 @@ public class StreamsConfig extends AbstractConfig {
                     atLeast(0),
                     Importance.LOW,
                     COMMIT_INTERVAL_MS_DOC)
+            .define(CONFIG_PROVIDERS_CONFIG,
+                    Type.LIST,
+                    Collections.emptyList(),
+                    Importance.LOW, 
+                    CONFIG_PROVIDERS_DOC)
             .define(ENABLE_METRICS_PUSH_CONFIG,
                     Type.BOOLEAN,
                     true,
@@ -1236,11 +1241,7 @@ public class StreamsConfig extends AbstractConfig {
                     Type.LONG,
                     null,
                     Importance.LOW,
-                    WINDOW_SIZE_MS_DOC)
-            .define(CONFIG_PROVIDERS_CONFIG, 
-                    Type.LIST,
-                    Collections.emptyList(),
-                    Importance.LOW, CONFIG_PROVIDERS_DOC);
+                    WINDOW_SIZE_MS_DOC);
     }
 
     // this is the list of configs for underlying clients
