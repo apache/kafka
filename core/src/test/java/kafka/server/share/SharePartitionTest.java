95c9551008275024916aeb1c40d8bce41abdd366
Chirag Wadhwa <cwadhwa@confluent.io>
2025-04-07 18:15:04 +0530

    KAFKA-18796-3: Increased the acquisition lock timeout in SharePartitionTest to 2 seconds from 100 ms

--- core/src/test/java/kafka/server/share/SharePartitionTest.java
+++ core/src/test/java/kafka/server/share/SharePartitionTest.java
@@ -110,8 +110,8 @@ public class SharePartitionTest {
     private static final String MEMBER_ID = "member-1";
     private static final Time MOCK_TIME = new MockTime();
     private static final short MAX_IN_FLIGHT_MESSAGES = 200;
-    private static final int ACQUISITION_LOCK_TIMEOUT_MS = 100;
-    private static final int DEFAULT_MAX_WAIT_ACQUISITION_LOCK_TIMEOUT_MS = 300;
+    private static final int ACQUISITION_LOCK_TIMEOUT_MS = 2000;
+    private static final int DEFAULT_MAX_WAIT_ACQUISITION_LOCK_TIMEOUT_MS = 6000;
     private static final int BATCH_SIZE = 500;
     private static final int DEFAULT_FETCH_OFFSET = 0;
     private static final int MAX_FETCH_RECORDS = Integer.MAX_VALUE;
