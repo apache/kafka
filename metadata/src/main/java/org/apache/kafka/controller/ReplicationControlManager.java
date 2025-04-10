f82f5d564031a7e9b35fb231d5c55db382ffe6ba
Alyssa Huang <ahuang@confluent.io>
2025-04-10 11:31:56 -0700

    remove session

--- metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
+++ metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
@@ -136,6 +136,8 @@ import static org.apache.kafka.common.protocol.Errors.NO_REASSIGNMENT_IN_PROGRES
 import static org.apache.kafka.common.protocol.Errors.TOPIC_AUTHORIZATION_FAILED;
 import static org.apache.kafka.common.protocol.Errors.UNKNOWN_TOPIC_ID;
 import static org.apache.kafka.common.protocol.Errors.UNKNOWN_TOPIC_OR_PARTITION;
+import static org.apache.kafka.controller.BrokerControlState.CONTROLLED_SHUTDOWN;
+import static org.apache.kafka.controller.BrokerControlState.SHUTDOWN_NOW;
 import static org.apache.kafka.controller.PartitionReassignmentReplicas.isReassignmentInProgress;
 import static org.apache.kafka.controller.QuorumController.MAX_RECORDS_PER_USER_OP;
 import static org.apache.kafka.metadata.LeaderConstants.NO_LEADER;
@@ -1625,14 +1627,12 @@ public class ReplicationControlManager {
                     break;
             }
         }
-        // skip updating session if broker is about to shutdown
         if (states.current().equals(CONTROLLED_SHUTDOWN) && states.next().equals(SHUTDOWN_NOW)) {
-            // do nothing
-        } else {
-            heartbeatManager.touch(brokerId,
+            heartbeatManager.removeSession(new BrokerIdAndEpoch(brokerId, brokerEpoch));
+        }
+        heartbeatManager.touch(brokerId,
             states.next().fenced(),
             request.currentMetadataOffset());
-        }
         if (featureControl.metadataVersionOrThrow().isDirectoryAssignmentSupported()) {
             handleDirectoriesOffline(brokerId, brokerEpoch, request.offlineLogDirs(), records);
         }
