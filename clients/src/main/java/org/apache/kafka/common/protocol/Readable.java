a38817076045332bb0a6010d1ee1fa1879032d36
Vikas Singh <vikas@confluent.io>
2025-04-23 11:51:37 -0700

    Add javadoc

--- clients/src/main/java/org/apache/kafka/common/protocol/Readable.java
+++ clients/src/main/java/org/apache/kafka/common/protocol/Readable.java
@@ -38,6 +38,14 @@ public interface Readable {
     int readVarint();
     long readVarlong();
     int remaining();
+
+    /**
+     * Returns a new Readable object whose content will be shared with this object.
+     * <br>
+     * The content of the new Readable object will start at this Readable's current
+     * position. The two Readable position will be independent, so read from one will
+     * not impact the other.
+     */
     Readable slice();
 
     default String readString(int length) {
