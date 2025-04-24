14394f0921abf56eaa51cca805a0d19cb6776a15
Lucas Brutschy <lbrutschy@gmail.com>
2025-04-24 10:28:14 +0200

    Update clients/src/main/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceData.java
    Co-authored-by: PoAn Yang <yangpoan@gmail.com>

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceData.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceData.java
@@ -349,7 +349,7 @@ public class StreamsRebalanceData {
         return partitionsByHost.get();
     }
 
-    /** For the current stream thread to request a shutdown of all Streams clients belonging to the same applications. */
+    /** For the current stream thread to request a shutdown of all Streams clients belonging to the same application. */
     public void requestShutdown() {
         shutdownRequested.set(true);
     }
