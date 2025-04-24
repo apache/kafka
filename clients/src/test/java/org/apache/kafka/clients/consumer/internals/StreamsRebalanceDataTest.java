cda36b67b491b695b2d2e2ad8b5d838a74e181bc
Lucas Brutschy <lbrutschy@gmail.com>
2025-04-24 10:27:48 +0200

    Update clients/src/test/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceDataTest.java
    Co-authored-by: PoAn Yang <yangpoan@gmail.com>

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceDataTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/StreamsRebalanceDataTest.java
@@ -425,7 +425,7 @@ public class StreamsRebalanceDataTest {
     public void streamsRebalanceDataShouldBeConstructedWithEmptyStatuses() {
         final UUID processId = UUID.randomUUID();
         final Optional<StreamsRebalanceData.HostInfo> endpoint = Optional.of(new StreamsRebalanceData.HostInfo("localhost", 9090));
-        final Map<String, StreamsRebalanceData.Subtopology> subtopologies = new HashMap<>();
+        final Map<String, StreamsRebalanceData.Subtopology> subtopologies = Map.of();
         final Map<String, String> clientTags = Map.of("clientTag1", "clientTagValue1");
         final StreamsRebalanceData streamsRebalanceData = new StreamsRebalanceData(
             processId,
