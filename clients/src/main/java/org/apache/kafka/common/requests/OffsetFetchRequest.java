e3fc4a86a8a7f5d8027d2d0e2028c126d5056738
David Jacot <david.jacot@gmail.com>
2025-04-26 21:32:05 +0200

    fix imports

--- clients/src/main/java/org/apache/kafka/common/requests/OffsetFetchRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/OffsetFetchRequest.java
@@ -26,8 +26,8 @@ import org.apache.kafka.common.message.OffsetFetchResponseData;
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.apache.kafka.common.protocol.Errors;
 import org.apache.kafka.common.protocol.Readable;
-
 import org.apache.kafka.common.record.RecordBatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
