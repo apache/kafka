69d00352e35ced1c67ec6c589477f4ae1fe12960
Lucas Brutschy <lbrutschy@confluent.io>
2025-04-28 11:26:58 +0200

    remvoe zk

--- tests/kafkatest/tests/streams/base_streams_test.py
+++ tests/kafkatest/tests/streams/base_streams_test.py
@@ -17,8 +17,7 @@ from ducktape.tests.test import Test
 from ducktape.utils.util import wait_until
 from kafkatest.services.verifiable_consumer import VerifiableConsumer
 from kafkatest.services.verifiable_producer import VerifiableProducer
-from kafkatest.services.zookeeper import ZookeeperService
-from kafkatest.services.kafka import KafkaService, quorum
+from kafkatest.services.kafka import KafkaService
 
 
 class BaseStreamsTest(Test):
@@ -30,22 +29,18 @@ class BaseStreamsTest(Test):
     see tests/kafkatest/tests/kafka_test.py for more info
     """
     def __init__(self, test_context, topics, num_controllers=1, num_brokers=3):
-        self.num_zk = num_controllers
+        self.num_controllers = num_controllers
         self.num_brokers = num_brokers
         self.topics = topics
 
-        self.zk = ZookeeperService(test_context, self.num_zk) if quorum.for_test(test_context) == quorum.zk else None
-
         self.kafka = KafkaService(
             test_context, self.num_brokers,
-            self.zk, topics=self.topics,
-            controller_num_nodes_override=self.num_zk,
+            None, topics=self.topics,
+            controller_num_nodes_override=self.num_controllers,
             use_streams_groups=True
         )
 
     def setUp(self):
-        if self.zk:
-            self.zk.start()
         self.kafka.start()
 
     def get_consumer(self, client_id, topic, num_messages):
