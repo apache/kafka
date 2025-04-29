2ababd62100a988b6ff0ec68f39d920731af526f
m1a2st <s7133700@gmail.com>
2025-04-29 22:17:52 +0800

    fix some error

--- core/src/test/scala/integration/kafka/api/SaslSslAdminIntegrationTest.scala
+++ core/src/test/scala/integration/kafka/api/SaslSslAdminIntegrationTest.scala
@@ -567,7 +567,7 @@ class SaslSslAdminIntegrationTest extends BaseAdminIntegrationTest with SaslSetu
     client.createAcls(List(denyAcl).asJava, new CreateAclsOptions()).all().get()
 
     val topics = Seq(topic1, topic2)
-    val configsOverride = Map(TopicConfig.SEGMENT_BYTES_CONFIG -> "20000000").asJava
+    val configsOverride = Map(TopicConfig.SEGMENT_BYTES_CONFIG -> "1048576").asJava
     val newTopics = Seq(
       new NewTopic(topic1, 2, 3.toShort).configs(configsOverride),
       new NewTopic(topic2, Option.empty[Integer].toJava, Option.empty[java.lang.Short].toJava).configs(configsOverride))
@@ -580,8 +580,8 @@ class SaslSslAdminIntegrationTest extends BaseAdminIntegrationTest with SaslSetu
       assertEquals(3, result.replicationFactor(topic1).get())
       val topicConfigs = result.config(topic1).get().entries.asScala
       assertTrue(topicConfigs.nonEmpty)
-      val segmentBytesConfig = topicConfigs.find(_.name == TopicConfig.SEGMENT_BYTES_DOC).get
-      assertEquals(100000, segmentBytesConfig.value.toLong)
+      val segmentBytesConfig = topicConfigs.find(_.name == TopicConfig.SEGMENT_BYTES_CONFIG).get
+      assertEquals(1048576, segmentBytesConfig.value.toLong)
       assertEquals(ConfigEntry.ConfigSource.DYNAMIC_TOPIC_CONFIG, segmentBytesConfig.source)
       val compressionConfig = topicConfigs.find(_.name == TopicConfig.COMPRESSION_TYPE_CONFIG).get
       assertEquals(ServerLogConfigs.COMPRESSION_TYPE_DEFAULT, compressionConfig.value)
