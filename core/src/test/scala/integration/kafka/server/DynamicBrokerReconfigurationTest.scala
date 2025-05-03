1437ff915b90433c4605d3757d93ab76e776c4b8
Bolin Lin <linbolin1230@gmail.com>
2025-05-03 02:12:26 -0400

    feat: update DynamicBrokerReconfigurationTest and AbstractFetcherManagerTest

--- core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
+++ core/src/test/scala/integration/kafka/server/DynamicBrokerReconfigurationTest.scala
@@ -59,6 +59,7 @@ import org.apache.kafka.network.SocketServerConfigs
 import org.apache.kafka.raft.MetadataLogConfig
 import org.apache.kafka.server.config.{ReplicationConfigs, ServerConfigs, ServerLogConfigs, ServerTopicConfigSynonyms}
 import org.apache.kafka.server.metrics.{KafkaYammerMetrics, MetricConfigs}
+import org.apache.kafka.server.ReplicaState
 import org.apache.kafka.server.record.BrokerCompressionType
 import org.apache.kafka.server.util.ShutdownableThread
 import org.apache.kafka.storage.internals.log.{CleanerConfig, LogConfig, UnifiedLog}
@@ -934,8 +935,8 @@ class DynamicBrokerReconfigurationTest extends QuorumTestHarness with SaslSetup 
         assertEquals(1, fetcherThreads.size)
         assertEquals(replicaFetcherManager.getFetcherId(tp), fetcherThreads.head._1.fetcherId)
         val thread = fetcherThreads.head._2
-        assertEquals(Some(truncationOffset), thread.fetchState(tp).map(_.fetchOffset))
-        assertEquals(Some(Truncating), thread.fetchState(tp).map(_.state))
+        assertEquals(Some(truncationOffset), thread.fetchState(tp).map(_.getFetchOffset))
+        assertEquals(Some(ReplicaState.Truncating.getInstance), thread.fetchState(tp).map(_.getState))
       }
     }
   }
