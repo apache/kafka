e75a3c94781d2dfca45263f0135e36223fbd4c2c
Bolin Lin <linbolin1230@gmail.com>
2025-05-02 23:49:51 -0400

    feat: add PartitionState, ReplicaFetch, ReplicaState and ResultWithPartitions

---
+++ server/src/main/java/org/apache/kafka/server/PartitionFetchState.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.kafka.server;
+
+import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.utils.Time;
+
+import java.util.Optional;
+
+/**
+ * Class to keep partition offset and its state (truncatingLog, delayed)
+ * This represents a partition as being either:
+ * (1) Truncating its log, for example, having recently become a follower
+ * (2) Delayed, for example, due to an error, where we subsequently back off a bit
+ * (3) ReadyForFetch, the active state where the thread is actively fetching data.
+ */
+public class PartitionFetchState {
+    private final Optional<Uuid> topicId;
+    private final long fetchOffset;
+    private final Optional<Long> lag;
+    private final int currentLeaderEpoch;
+    private final Optional<Long> delay;
+    private final ReplicaState state;
+    private final Optional<Integer> lastFetchedEpoch;
+    private final Optional<Long> dueMs;
+
+
+    public static PartitionFetchState create(Optional<Uuid> topicId,
+                                                                   long offset,
+                                                                   Optional<Long> lag,
+                                                                   int currentLeaderEpoch,
+                                                                   ReplicaState state,
+                                                                   Optional<Integer> lastFetchedEpoch) {
+        return new PartitionFetchState(topicId, offset, lag, currentLeaderEpoch,
+                Optional.empty(), state, lastFetchedEpoch);
+    }
+
+    public PartitionFetchState(Optional<Uuid> topicId, long fetchOffset, Optional<Long> lag,
+                               int currentLeaderEpoch, Optional<Long> delay,
+                               ReplicaState state, Optional<Integer> lastFetchedEpoch) {
+        this.topicId = topicId;
+        this.fetchOffset = fetchOffset;
+        this.lag = lag;
+        this.currentLeaderEpoch = currentLeaderEpoch;
+        this.delay = delay;
+        this.state = state;
+        this.lastFetchedEpoch = lastFetchedEpoch;
+
+        // Initialize dueMs (equivalent to private val dueMs)
+        this.dueMs = delay.isPresent() ?
+                Optional.of(delay.get() + Time.SYSTEM.milliseconds()) :
+                Optional.empty();
+    }
+
+    public boolean isReadyForFetch() {
+        return state == ReplicaState.Fetching.getInstance() && !isDelayed();
+    }
+
+    public boolean isReplicaInSync() {
+        return lag.isPresent() && lag.get() <= 0;
+    }
+
+    public boolean isTruncating() {
+        return state == ReplicaState.Truncating.getInstance() && !isDelayed();
+    }
+
+    public boolean isDelayed() {
+        return dueMs.isPresent() && dueMs.get() > Time.SYSTEM.milliseconds();
+    }
+
+    public Optional<Uuid> getTopicId() {
+        return topicId;
+    }
+
+    public long getFetchOffset() {
+        return fetchOffset;
+    }
+
+    public Optional<Long> getLag() {
+        return lag;
+    }
+
+    public int getCurrentLeaderEpoch() {
+        return currentLeaderEpoch;
+    }
+
+    public Optional<Long> getDelay() {
+        return delay;
+    }
+
+    public ReplicaState getState() {
+        return state;
+    }
+
+    public Optional<Integer> getLastFetchedEpoch() {
+        return lastFetchedEpoch;
+    }
+
+    @Override
+    public String toString() {
+        return "FetchState(topicId=" + topicId +
+                ", fetchOffset=" + fetchOffset +
+                ", currentLeaderEpoch=" + currentLeaderEpoch +
+                ", lastFetchedEpoch=" + lastFetchedEpoch +
+                ", state=" + state +
+                ", lag=" + lag +
+                ", delay=" + (delay.isPresent() ? delay.get() : 0) + "ms)";
+    }
+
+    public PartitionFetchState updateTopicId(Optional<Uuid> newTopicId) {
+        return new PartitionFetchState(newTopicId, this.fetchOffset, this.lag,
+                this.currentLeaderEpoch, this.delay,
+                this.state, this.lastFetchedEpoch);
+    }
+}
