9882fbec15b338934b787de9b38936c695ad6f76
Kirk True <kirk@kirktrue.pro>
2025-05-06 16:10:48 -0700

    Fixed import linting issues

--- clients/src/main/java/org/apache/kafka/common/security/oauthbearer/internals/secured/BrokerJwtValidator.java
+++ clients/src/main/java/org/apache/kafka/common/security/oauthbearer/internals/secured/BrokerJwtValidator.java
@@ -31,7 +31,6 @@ import org.jose4j.keys.resolvers.VerificationKeyResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
--- clients/src/main/java/org/apache/kafka/common/security/oauthbearer/internals/secured/ClientJwtValidator.java
+++ clients/src/main/java/org/apache/kafka/common/security/oauthbearer/internals/secured/ClientJwtValidator.java
@@ -24,7 +24,6 @@ import org.apache.kafka.common.security.oauthbearer.internals.unsecured.OAuthBea
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
