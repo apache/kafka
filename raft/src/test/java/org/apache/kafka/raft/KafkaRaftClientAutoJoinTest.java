6e9604c02f7b0d5f76adec4efd90f2ddb8f533c0
Kevin Wu <kevin.wu2412@gmail.com>
2025-05-07 12:50:41 -0500

    fixing build

--- raft/src/test/java/org/apache/kafka/raft/KafkaRaftClientAutoJoinTest.java
+++ raft/src/test/java/org/apache/kafka/raft/KafkaRaftClientAutoJoinTest.java
@@ -18,9 +18,9 @@ package org.apache.kafka.raft;
 
 import org.apache.kafka.common.protocol.Errors;
 import org.apache.kafka.common.record.MemoryRecords;
-
 import org.apache.kafka.common.utils.BufferSupplier;
 import org.apache.kafka.server.common.KRaftVersion;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ThreadLocalRandom;
