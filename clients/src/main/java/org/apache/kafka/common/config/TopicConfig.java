f5d931ee198118c701b38dcfe818517739c69f66
Milly <32290488+shmily7829@users.noreply.github.com>
2025-05-07 23:58:20 +0800

    Update clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
    Co-authored-by: Luke Chen <showuon@gmail.com>

--- clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
+++ clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
@@ -112,13 +112,11 @@ public class TopicConfig {
 
     public static final String INDEX_INTERVAL_BYTES_CONFIG = "index.interval.bytes";
     public static final String INDEX_INTERVAL_BYTES_DOC = "This setting controls how frequently Kafka " +
-            "adds entries to its offset index and, conditionally, to its time index. After appending a batch of messages, " +
-            "if the total bytes written since the last index entry exceed this value, Kafka will: <br/>" +
-            "Add an entry to the offset index for the batch's last offset. <br/>" +
-            "Attempt to add an entry to the time index if the batch's maximum timestamp is greater than the last indexed timestamp. <br/>" +
+            "adds entries to its offset index and, conditionally, to its time index. " +      
             "The default setting ensures that we index a message roughly every 4096 bytes. " +
             "More frequent indexing allows reads to jump closer to the exact position in the log " +
-            "but results in larger index files. You probably don't need to change this.";
+            "but results in larger index files. You probably don't need to change this." 
+            "<p> Note: the time index will be inserted only when the timestamp is greater than the last indexed timestamp.";
 
     public static final String FILE_DELETE_DELAY_MS_CONFIG = "file.delete.delay.ms";
     public static final String FILE_DELETE_DELAY_MS_DOC = "The time to wait before deleting a file from the " +
