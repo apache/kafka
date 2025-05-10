b30fa886a4f524edaaabe8315d85a17a83e3e0fd
Alyssa Huang <ahuang@confluent.io>
2025-05-09 23:42:34 -0700

    MINOR: Fix some Request toString methods (#19655)
    Reviewers: Colin P. McCabe <cmccabe@apache.org>
    Conflicts:
        clients/src/main/java/org/apache/kafka/common/requests/AlterUserScramCredentialsRequest.java
        clients/src/main/java/org/apache/kafka/common/requests/IncrementalAlterConfigsRequest.java
        core/src/test/scala/unit/kafka/network/RequestChannelTest.scala
        core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
    
    Cherry-Picked-From: 042be5b9ac3f4be83069b18f17fda382e07d6539
    Cherry-Picked-By: Alyssa Huang <ahuang@confluent.io>
    Cherry-Picked-At: Mon May 12 13:48:31 2025 -0700

--- core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
+++ core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
@@ -6908,7 +6908,12 @@ class KafkaApisTest extends Logging {
 
   @Test
   def testEmptyIncrementalAlterConfigsRequestWithKRaft(): Unit = {
-    val request = buildRequest(new IncrementalAlterConfigsRequest(new IncrementalAlterConfigsRequestData(), 1.toShort))
+    val alterConfigsRequest = new IncrementalAlterConfigsRequest(new IncrementalAlterConfigsRequestData(), 1.toShort)
+    assertEquals(
+      "IncrementalAlterConfigsRequestData(resources=[], validateOnly=false)",
+      alterConfigsRequest.toString
+    )
+    val request = buildRequest(alterConfigsRequest)
     metadataCache = MetadataCache.kRaftMetadataCache(brokerId)
     when(clientRequestQuotaManager.maybeRecordAndGetThrottleTimeMs(any[RequestChannel.Request](),
       any[Long])).thenReturn(0)
@@ -6920,7 +6925,7 @@ class KafkaApisTest extends Logging {
 
   @Test
   def testLog4jIncrementalAlterConfigsRequestWithKRaft(): Unit = {
-    val request = buildRequest(new IncrementalAlterConfigsRequest(new IncrementalAlterConfigsRequestData().
+    val alterConfigsRequest = new IncrementalAlterConfigsRequest(new IncrementalAlterConfigsRequestData().
       setValidateOnly(true).
       setResources(new IAlterConfigsResourceCollection(asList(new IAlterConfigsResource().
         setResourceName(brokerId.toString).
@@ -6928,7 +6933,16 @@ class KafkaApisTest extends Logging {
         setConfigs(new IAlterableConfigCollection(asList(new IAlterableConfig().
           setName(Log4jController.ROOT_LOGGER).
           setValue("TRACE")).iterator()))).iterator())),
-        1.toShort))
+      1.toShort)
+    assertEquals(
+      "IncrementalAlterConfigsRequestData(resources=[" +
+        "AlterConfigsResource(resourceType=" + BROKER_LOGGER.id() + ", " +
+        "resourceName='"+ brokerId + "', " +
+        "configs=[AlterableConfig(name='" + Log4jController.ROOT_LOGGER + "', configOperation=0, value='REDACTED')])], " +
+        "validateOnly=true)",
+      alterConfigsRequest.toString
+    )
+    val request = buildRequest(alterConfigsRequest)
     metadataCache = MetadataCache.kRaftMetadataCache(brokerId)
     when(clientRequestQuotaManager.maybeRecordAndGetThrottleTimeMs(any[RequestChannel.Request](),
       any[Long])).thenReturn(0)
