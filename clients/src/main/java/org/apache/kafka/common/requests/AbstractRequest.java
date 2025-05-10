ac248e82eff3c06adac23e9967335e568b46277f
Alyssa Huang <ahuang@confluent.io>
2025-05-09 23:42:34 -0700

    MINOR: Fix some Request toString methods (#19655)
    Reviewers: Colin P. McCabe <cmccabe@apache.org>
    Conflicts:
        checkstyle/import-control.xml
        clients/src/main/java/org/apache/kafka/common/requests/AlterUserScramCredentialsRequest.java
        clients/src/main/java/org/apache/kafka/common/requests/IncrementalAlterConfigsRequest.java
        core/src/test/scala/unit/kafka/network/RequestChannelTest.scala
        core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
    
    Cherry-Picked-From: 042be5b9ac3f4be83069b18f17fda382e07d6539
    Cherry-Picked-By: Alyssa Huang <ahuang@confluent.io>
    Cherry-Picked-At: Mon May 12 15:20:00 2025 -0700

--- clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/AbstractRequest.java
@@ -136,7 +136,7 @@ public abstract class AbstractRequest implements AbstractRequestResponse {
     }
 
     @Override
-    public final String toString() {
+    public String toString() {
         return toString(true);
     }
 
