eae78562ad460ce9517228c932bd0567251c75e5
Federico Valeri <fedevaleri@gmail.com>
2025-05-13 11:51:42 +0200

    MINOR: Add JmxReporter note to metric.reporters
    According to KIP-830, in Kafka 4 users currently relying on JmxReporter and that are using additional reporters
    via metric.reporters will have to include org.apache.kafka.common.metrics.JmxReporter in metric.reporters.
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
+++ clients/src/main/java/org/apache/kafka/clients/CommonClientConfigs.java
@@ -125,7 +125,9 @@ public class CommonClientConfigs {
             "\n" +
             "TRACE level records all possible metrics, capturing every detail about the system's performance and operation. It's best for controlled environments where in-depth analysis is required, though it can introduce significant overhead.";
     public static final String METRIC_REPORTER_CLASSES_CONFIG = "metric.reporters";
-    public static final String METRIC_REPORTER_CLASSES_DOC = "A list of classes to use as metrics reporters. Implementing the <code>org.apache.kafka.common.metrics.MetricsReporter</code> interface allows plugging in classes that will be notified of new metric creation.";
+    public static final String METRIC_REPORTER_CLASSES_DOC = "A list of classes to use as metrics reporters. " +
+            "Implementing the <code>org.apache.kafka.common.metrics.MetricsReporter</code> interface allows plugging in classes that will be notified of new metric creation. " +
+            "When custom reporters are set and <code>org.apache.kafka.common.metrics.JmxReporter</code> is needed, it has to be explicitly added to the list.";
 
     public static final String METRICS_CONTEXT_PREFIX = "metrics.context.";
 
