2c4b0947b35802214e5af908b364b6cc1cc9df7b
Keshan <janindupathi@gmail.com>
2025-05-16 19:17:09 +0530

    KAFKA-18913: Removed an unused import

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/TaskManager.java
@@ -57,7 +57,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
