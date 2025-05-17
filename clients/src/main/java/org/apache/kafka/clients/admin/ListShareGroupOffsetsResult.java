b53e8fe4e2771f8ccad95bb6a9f1c50474c11ea2
loganzhu <loganZhu96@outlook.com>
2025-05-18 00:27:35 +0800

    remove redundant doc

--- clients/src/main/java/org/apache/kafka/clients/admin/ListShareGroupOffsetsResult.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/ListShareGroupOffsetsResult.java
@@ -68,9 +68,6 @@ public class ListShareGroupOffsetsResult {
     /**
      * Return a future which yields a map of topic partitions to offsets for the specified group. If the group doesn't
      * have a committed offset for a specific partition, the corresponding value in the returned map will be null.
-     *
-     * @param groupId The group ID.
-     * @return Future which yields a map of topic partitions to offsets for the specified group.
      */
     public KafkaFuture<Map<TopicPartition, OffsetAndMetadata>> partitionsToOffsetAndMetadata(String groupId) {
         if (!futures.containsKey(groupId)) {
