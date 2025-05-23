2e88e6da5737cc1c0bb55ca753ef17bfc4acb7e9
Apoorv Mittal <apoorvmittal10@gmail.com>
2025-05-23 22:56:45 +0100

    Updating comment

--- server-common/src/main/java/org/apache/kafka/server/purgatory/DelayedOperation.java
+++ server-common/src/main/java/org/apache/kafka/server/purgatory/DelayedOperation.java
@@ -29,7 +29,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * <br/>
  * The logic upon completing a delayed operation is defined in onComplete() and will be called exactly once.
  * Once an operation is completed, isCompleted() will return true. onComplete() is called from forceComplete(),
- * which is triggered once by either expiration if the operation is not completed after delayMs, or tryComplete()
+ * which is triggered by either expiration, if the operation is not completed after delayMs; or tryComplete(),
  * if the operation can be completed now.
  * <br/>
  * A subclass of DelayedOperation needs to provide an implementation of both onComplete() and tryComplete().
