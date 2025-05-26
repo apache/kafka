60ed7dc7d588d66c54a92c4efbb8c65e825ef60b
aliehsaeedii <asaeedi@confluent.io>
2025-05-26 23:12:39 +0200

    correct comments

--- clients/src/main/java/org/apache/kafka/clients/admin/ListStreamsGroupOffsetsSpec.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/ListStreamsGroupOffsetsSpec.java
@@ -33,7 +33,7 @@ public class ListStreamsGroupOffsetsSpec {
     private Collection<TopicPartition> topicPartitions;
 
     /**
-     * Set the topic partitions whose offsets are to be listed for a Streams group.
+     * Set the topic partitions whose offsets are to be listed for a streams group.
      */
     ListStreamsGroupOffsetsSpec topicPartitions(Collection<TopicPartition> topicPartitions) {
         this.topicPartitions = topicPartitions;
@@ -41,7 +41,7 @@ public class ListStreamsGroupOffsetsSpec {
     }
 
     /**
-     * Returns the topic partitions whose offsets are to be listed for a Streams group.
+     * Returns the topic partitions whose offsets are to be listed for a streams group.
      */
     Collection<TopicPartition> topicPartitions() {
         return topicPartitions;
