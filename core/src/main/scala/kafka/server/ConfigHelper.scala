ebee225f9c672558a39d1e230b1aab137931b5ff
Yunchi Pang <yunchipang@gmail.com>
2025-05-30 15:32:29 -0700

    refactor createResponseConfig to avoid creating extra collection

--- core/src/main/scala/kafka/server/ConfigHelper.scala
+++ core/src/main/scala/kafka/server/ConfigHelper.scala
@@ -89,15 +89,19 @@ class ConfigHelper(metadataCache: MetadataCache, config: KafkaConfig, configRepo
 
       def createResponseConfig(configs: Map[String, Any],
                                createConfigEntry: (String, Any) => DescribeConfigsResponseData.DescribeConfigsResourceResult): DescribeConfigsResponseData.DescribeConfigsResult = {
-        val filteredConfigPairs = if (resource.configurationKeys == null || resource.configurationKeys.isEmpty)
-          configs.toBuffer
-        else
-          configs.filter { case (configName, _) =>
-            resource.configurationKeys.asScala.contains(configName)
-          }.toBuffer
-
-        val configEntries = filteredConfigPairs.map { case (name, value) => createConfigEntry(name, value) }
-        new DescribeConfigsResponseData.DescribeConfigsResult().setErrorCode(Errors.NONE.code)
+        val configEntries = if (resource.configurationKeys == null || resource.configurationKeys.isEmpty) {
+          configs.view
+            .map { case (name, value) => createConfigEntry(name, value) }
+            .toList
+        } else {
+          val keys = resource.configurationKeys.asScala.toSet
+          configs.view
+            .filter { case (name, _) => keys.contains(name) }
+            .map { case (name, value) => createConfigEntry(name, value) }
+            .toList
+        }
+        new DescribeConfigsResponseData.DescribeConfigsResult()
+          .setErrorCode(Errors.NONE.code)
           .setConfigs(configEntries.asJava)
       }
 
