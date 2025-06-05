d12c86440246d24a27584c8a9eae313fd557233e
Yunchi Pang <yunchipang@gmail.com>
2025-06-04 18:10:17 -0700

    move files to correct locations

--- tools/src/test/java/org/apache/kafka/tools/LogCompactionTester.java
+++ tools/src/test/java/org/apache/kafka/tools/LogCompactionTester.java
@@ -1,20 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.kafka.tools;
 
@@ -33,13 +31,26 @@ import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.utils.Exit;
 import org.apache.kafka.server.util.CommandLineUtils;
+import org.apache.kafka.test.TestUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -47,7 +58,6 @@ import java.util.stream.Stream;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
-import static kafka.utils.TestUtils.waitUntilTrue;
 import static org.apache.kafka.tools.TestRecordUtils.valuesIterator;
 
 public class LogCompactionTester {
@@ -116,14 +126,19 @@ public class LogCompactionTester {
             );
             List<NewTopic> newTopics = Arrays.stream(topics)
                     .map(name -> new NewTopic(name, 1, (short) 1).configs(topicConfigs)).toList();
-
             adminClient.createTopics(newTopics).all().get();
 
-            List<String> pendingTopics = new ArrayList<>();
-            waitUntilTrue(() -> {
+            final List<String> pendingTopics = new ArrayList<>();
+            TestUtils.waitForCondition(() -> {
                 Set<String> allTopics = adminClient.listTopics().names().get();
-                return Arrays.stream(topics).allMatch(allTopics::contains);
-            }, "timed out waiting for topics to be created");
+                pendingTopics.clear();
+                pendingTopics.addAll(
+                        Arrays.stream(topics)
+                                .filter(topicName -> !allTopics.contains(topicName))
+                                .toList()
+                );
+                return pendingTopics.isEmpty();
+            }, "timed out waiting for topics: " + pendingTopics);
         }
     }
 
