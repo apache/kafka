ac49256b7720f85a119274bf78b0ee48156dc024
Bolin Lin <linbolin1230@gmail.com>
2025-06-07 11:07:12 -0400

    feat: update comment

--- test-common/test-common-runtime/src/main/java/org/apache/kafka/common/test/TestUtils.java
+++ test-common/test-common-runtime/src/main/java/org/apache/kafka/common/test/TestUtils.java
@@ -32,7 +32,9 @@ import java.util.function.Supplier;
 import static java.lang.String.format;
 
 /**
- * Helper functions for writing unit tests
+ * Helper functions for writing unit tests.
+ * <p>
+ * <b>Package-private:</b> Not intended for use outside {@code org.apache.kafka.common.test}.
  */
 class TestUtils {
     private static final Logger log = LoggerFactory.getLogger(TestUtils.class);
