337f7fc54472693d90713f1270abc03fb794cf09
Ming-Yen Chung <mingyen066@gmail.com>
2025-06-23 02:39:01 +0800

    update testPreferredReplicaAsLeader

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -1506,6 +1506,9 @@ class ReplicaManagerTest {
       val tp0 = new TopicPartition(topic, 0)
       val tidp0 = new TopicIdPartition(topicId, tp0)
 
+      val partition = replicaManager.createPartition(new TopicPartition(topic, 0))
+      partition.createLogIfNotExists(isNew = false, isFutureReplica = false,
+        new LazyOffsetCheckpoints(replicaManager.highWatermarkCheckpoints.asJava), None)
       // Make this replica the leader
       val leaderDelta = createLeaderDelta(
         topicId = topicId,
