34dd095c67aa1daafd7fac941cad035ea7bb9227
Lan Ding <isDing_L@163.com>
2025-06-25 16:57:52 +0800

    Update tools/src/test/java/org/apache/kafka/tools/reassign/ReassignPartitionsCommandTest.java
    Co-authored-by: Jhen-Yung Hsu <yungyung7654321@gmail.com>

--- tools/src/test/java/org/apache/kafka/tools/reassign/ReassignPartitionsCommandTest.java
+++ tools/src/test/java/org/apache/kafka/tools/reassign/ReassignPartitionsCommandTest.java
@@ -799,7 +799,7 @@ public class ReassignPartitionsCommandTest {
      */
     private void removeReplicationThrottleForPartitions(TopicPartition part) {
         try (Admin admin = Admin.create(Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, clusterInstance.bootstrapServers()))) {
-            removePartitionReplicaThrottles(admin, new HashSet<>(Set.of(part)));
+            removePartitionReplicaThrottles(admin, Set.of(part));
             assertDoesNotThrow(() -> throttleAllBrokersReplication(admin, List.of(0, 1, 2, 3), Integer.MAX_VALUE));
         }
     }
