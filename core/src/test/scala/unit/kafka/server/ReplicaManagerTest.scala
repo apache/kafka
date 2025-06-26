4957d50016ac08d826a54aeecc80f8646822f125
TaiJu Wu <tjwu1217@gmail.com>
2025-06-26 20:16:40 +0800

    fix build

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -43,7 +43,7 @@ import org.apache.kafka.common.metrics.Monitorable
 import org.apache.kafka.common.metrics.PluginMetrics
 import org.apache.kafka.common.network.ListenerName
 import org.apache.kafka.common.protocol.{ApiKeys, Errors}
-import org.apache.kafka.common.PartitionState
+import org.apache.kafka.common.{PartitionState => JPartitionState}
 import org.apache.kafka.common.record._
 import org.apache.kafka.common.replica.ClientMetadata.DefaultClientMetadata
 import org.apache.kafka.common.replica.ReplicaView.DefaultReplicaView
@@ -2660,8 +2660,8 @@ class ReplicaManagerTest {
                                          leaderEpoch: Int,
                                          leaderBrokerId: Int,
                                          aliveBrokerIds: Seq[Integer],
-                                         isNew: Boolean = false): PartitionState = {
-    new PartitionState()
+                                         isNew: Boolean = false): JPartitionState = {
+    new JPartitionState()
       .setTopicName(topic)
       .setPartitionIndex(topicPartition.partition)
       .setLeader(leaderBrokerId)
