2733572e8f66f69a794a63d7f66380c638707e47
Sanskar Jhajharia <sjhajharia@confluent.io>
2025-07-08 17:15:17 +0530

    Use modern java

--- connect/api/src/main/java/org/apache/kafka/connect/data/ConnectSchema.java
+++ connect/api/src/main/java/org/apache/kafka/connect/data/ConnectSchema.java
@@ -20,57 +20,55 @@ import org.apache.kafka.connect.errors.DataException;
 
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ConnectSchema implements Schema {
     /**
      * Maps {@link Schema.Type}s to a list of Java classes that can be used to represent them.
      */
-    private static final Map<Type, List<Class<?>>> SCHEMA_TYPE_CLASSES = new EnumMap<>(Type.class);
+    private static final Map<Type, List<Class<?>>> SCHEMA_TYPE_CLASSES = Collections.unmodifiableMap(new EnumMap<>(Map.ofEntries(
+        Map.entry(Type.INT8, List.of(Byte.class)),
+        Map.entry(Type.INT16, List.of(Short.class)),
+        Map.entry(Type.INT32, List.of(Integer.class)),
+        Map.entry(Type.INT64, List.of(Long.class)),
+        Map.entry(Type.FLOAT32, List.of(Float.class)),
+        Map.entry(Type.FLOAT64, List.of(Double.class)),
+        Map.entry(Type.BOOLEAN, List.of(Boolean.class)),
+        Map.entry(Type.STRING, List.of(String.class)),
+        // Bytes are special and have 2 representations. byte[] causes problems because it doesn't handle equals() and
+        // hashCode() like we want objects to, so we support both byte[] and ByteBuffer. Using plain byte[] can cause
+        // those methods to fail, so ByteBuffers are recommended
+        Map.entry(Type.BYTES, List.of(byte[].class, ByteBuffer.class)),
+        Map.entry(Type.ARRAY, List.of(List.class)),
+        Map.entry(Type.MAP, List.of(Map.class)),
+        Map.entry(Type.STRUCT, List.of(Struct.class))
+    )));
     /**
      * Maps known logical types to a list of Java classes that can be used to represent them.
      */
-    private static final Map<String, List<Class<?>>> LOGICAL_TYPE_CLASSES = new HashMap<>();
+    // We don't need to put these into JAVA_CLASS_SCHEMA_TYPES since that's only used to determine schemas for
+    // schemaless data and logical types will have ambiguous schemas (e.g. many of them use the same Java class) so
+    // they should not be used without schemas.
+    private static final Map<String, List<Class<?>>> LOGICAL_TYPE_CLASSES = Map.of(
+        Decimal.LOGICAL_NAME, List.of(BigDecimal.class),
+        Date.LOGICAL_NAME, List.of(java.util.Date.class),
+        Time.LOGICAL_NAME, List.of(java.util.Date.class),
+        Timestamp.LOGICAL_NAME, List.of(java.util.Date.class)
+    );
 
     /**
      * Maps the Java classes to the corresponding {@link Schema.Type}.
      */
-    private static final Map<Class<?>, Type> JAVA_CLASS_SCHEMA_TYPES = new HashMap<>();
-
-    static {
-        SCHEMA_TYPE_CLASSES.put(Type.INT8, List.of(Byte.class));
-        SCHEMA_TYPE_CLASSES.put(Type.INT16, List.of(Short.class));
-        SCHEMA_TYPE_CLASSES.put(Type.INT32, List.of(Integer.class));
-        SCHEMA_TYPE_CLASSES.put(Type.INT64, List.of(Long.class));
-        SCHEMA_TYPE_CLASSES.put(Type.FLOAT32, List.of(Float.class));
-        SCHEMA_TYPE_CLASSES.put(Type.FLOAT64, List.of(Double.class));
-        SCHEMA_TYPE_CLASSES.put(Type.BOOLEAN, List.of(Boolean.class));
-        SCHEMA_TYPE_CLASSES.put(Type.STRING, List.of(String.class));
-        // Bytes are special and have 2 representations. byte[] causes problems because it doesn't handle equals() and
-        // hashCode() like we want objects to, so we support both byte[] and ByteBuffer. Using plain byte[] can cause
-        // those methods to fail, so ByteBuffers are recommended
-        SCHEMA_TYPE_CLASSES.put(Type.BYTES, List.of(byte[].class, ByteBuffer.class));
-        SCHEMA_TYPE_CLASSES.put(Type.ARRAY, List.of(List.class));
-        SCHEMA_TYPE_CLASSES.put(Type.MAP, List.of(Map.class));
-        SCHEMA_TYPE_CLASSES.put(Type.STRUCT, List.of(Struct.class));
-
-        for (Map.Entry<Type, List<Class<?>>> schemaClasses : SCHEMA_TYPE_CLASSES.entrySet()) {
-            for (Class<?> schemaClass : schemaClasses.getValue())
-                JAVA_CLASS_SCHEMA_TYPES.put(schemaClass, schemaClasses.getKey());
-        }
-
-        LOGICAL_TYPE_CLASSES.put(Decimal.LOGICAL_NAME, List.of(BigDecimal.class));
-        LOGICAL_TYPE_CLASSES.put(Date.LOGICAL_NAME, List.of(java.util.Date.class));
-        LOGICAL_TYPE_CLASSES.put(Time.LOGICAL_NAME, List.of(java.util.Date.class));
-        LOGICAL_TYPE_CLASSES.put(Timestamp.LOGICAL_NAME, List.of(java.util.Date.class));
-        // We don't need to put these into JAVA_CLASS_SCHEMA_TYPES since that's only used to determine schemas for
-        // schemaless data and logical types will have ambiguous schemas (e.g. many of them use the same Java class) so
-        // they should not be used without schemas.
-    }
+    private static final Map<Class<?>, Type> JAVA_CLASS_SCHEMA_TYPES = SCHEMA_TYPE_CLASSES.entrySet()
+        .stream()
+        .flatMap(entry -> entry.getValue().stream().map(klass -> Map.entry(klass, entry.getKey())))
+        .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 
     // The type of the field
     private final Type type;
