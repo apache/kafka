763be97695937b26d45f6c6c002a71f6bdd4fef6
Jinhe Zhang <raidene1@outlook.com>
2025-07-11 11:58:33 -0400

    fix the handling source toic deletion integration test

--- streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/HandlingSourceTopicDeletionIntegrationTest.java
+++ streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/HandlingSourceTopicDeletionIntegrationTest.java
@@ -42,10 +42,13 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.kafka.streams.utils.TestUtils.safeUniqueTestName;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("integration")
 public class HandlingSourceTopicDeletionIntegrationTest {
@@ -124,12 +127,15 @@ public class HandlingSourceTopicDeletionIntegrationTest {
 
         CLUSTER.deleteTopic(INPUT_TOPIC);
 
-        TestUtils.waitForCondition(
-            () -> kafkaStreams1.state() == State.ERROR && kafkaStreams2.state() == State.ERROR,
-            TIMEOUT,
-            () -> "Kafka Streams clients did not reach state ERROR"
-        );
+        if (!useNewProtocol) {
+            TestUtils.waitForCondition(
+                    () -> kafkaStreams1.state() == State.ERROR && kafkaStreams2.state() == State.ERROR,
+                    TIMEOUT,
+                    () -> "Kafka Streams clients did not reach state ERROR"
+            );
 
+        }
+        TimeUnit.SECONDS.sleep(5);
         assertThat(calledUncaughtExceptionHandler1.get(), is(true));
         assertThat(calledUncaughtExceptionHandler2.get(), is(true));
     }
