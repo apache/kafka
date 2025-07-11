a433011db9dfbf211b6879439398de990d64ac58
lucliu1108 <lucliu@confluent.io>
2025-07-10 22:35:06 -0500

    list internal topic for reset command

--- tools/src/main/java/org/apache/kafka/tools/streams/StreamsGroupCommand.java
+++ tools/src/main/java/org/apache/kafka/tools/streams/StreamsGroupCommand.java
@@ -496,8 +496,9 @@ public class StreamsGroupCommand {
                                             if (e.getCause() instanceof UnknownTopicOrPartitionException) {
                                                 printError("Deleting internal topics for group '" + groupId + "' failed because the topics do not exist.", Optional.empty());
                                             } else if (e.getCause() instanceof UnsupportedVersionException) {
-                                                printError("Deleting internal topics is not supported by the broker version. " +
-                                                    "Use 'kafka-topics.sh' to delete the group's internal topics.", Optional.of(e.getCause()));
+                                                printError("Deleting internal topics is not supported by the broker version. \n Topics are " +
+                                                        String.join(", ", internalTopics) + "\n" +
+                                                        "Use 'kafka-topics.sh' to delete the group's internal topics.", Optional.of(e.getCause()));
                                             } else {
                                                 printError("Deleting internal topics for group '" + groupId + "' failed due to " + e.getMessage(), Optional.of(e));
                                             }
