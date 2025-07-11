0b79aee66a5d6dcf70eeca14daac277e02e11742
Kevin Wu <kevin.wu2412@gmail.com>
2025-07-11 08:55:36 -0500

    code review

--- core/src/test/scala/unit/kafka/tools/StorageToolTest.scala
+++ core/src/test/scala/unit/kafka/tools/StorageToolTest.scala
@@ -375,9 +375,10 @@ Found problem:
   def testFormatWithStandaloneFlagOnBrokerFails(): Unit = {
     val availableDirs = Seq(TestUtils.tempDir())
     val properties = new Properties()
-    properties.putAll(defaultStaticQuorumProperties)
-    properties.remove("controller.quorum.voters")
-    properties.put("controller.quorum.bootstrap.servers", "localhost:9093")
+    properties.setProperty("process.roles", "broker")
+    properties.setProperty("node.id", "0")
+    properties.setProperty("controller.listener.names", "CONTROLLER")
+    properties.setProperty("controller.quorum.bootstrap.servers", "localhost:9093")
     properties.setProperty("log.dirs", availableDirs.mkString(","))
     val stream = new ByteArrayOutputStream()
     val arguments = ListBuffer[String]("--release-version", "3.9-IV0", "--standalone")
@@ -387,7 +388,7 @@ Found problem:
   }
 
   @Test
-  def testFormatWithStandaloneFailsWithVotersConfig(): Unit = {
+  def testFormatWithStandaloneFailsWithStaticVotersConfig(): Unit = {
     val availableDirs = Seq(TestUtils.tempDir())
     val properties = new Properties()
     properties.putAll(defaultDynamicQuorumProperties)
@@ -405,7 +406,7 @@ Found problem:
   }
 
   @Test
-  def testFormatWithInitialControllersFailsWithVotersConfig(): Unit = {
+  def testFormatWithInitialControllersFailsWithStaticVotersConfig(): Unit = {
     val availableDirs = Seq(TestUtils.tempDir())
     val properties = new Properties()
     properties.putAll(defaultDynamicQuorumProperties)
