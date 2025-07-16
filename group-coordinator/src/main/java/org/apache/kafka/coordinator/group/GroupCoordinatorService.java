6a898da318aab16f95796ff552e6c11de2d37f75
Chirag Wadhwa <cwadhwa@confluent.io>
2025-07-16 15:16:03 +0530

    Corrected the logger name in PersisterStateManagerHandler

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupCoordinatorService.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupCoordinatorService.java
@@ -797,7 +797,7 @@ public class GroupCoordinatorService implements GroupCoordinator {
             }
             return performShareGroupStateMetadataInitialize(groupId, topicPartitionMap, defaultResponse);
         }
-        log.error("Received error while calling initialize state for {} on persister {}.", groupId, persisterError.code());
+        log.error("Received error while calling initialize state for {} on persister, errorCode: {}.", groupId, persisterError.code());
         return uninitializeShareGroupState(persisterError, groupId, topicPartitionMap);
     }
 
--- server-common/src/main/java/org/apache/kafka/server/share/persister/PersisterStateManager.java
+++ server-common/src/main/java/org/apache/kafka/server/share/persister/PersisterStateManager.java
@@ -221,7 +221,7 @@ public class PersisterStateManager {
     public abstract class PersisterStateManagerHandler implements RequestCompletionHandler {
         protected Node coordinatorNode;
         private final BackoffManager findCoordBackoff;
-        protected final Logger log = LoggerFactory.getLogger(getClass());
+        protected final Logger log = LoggerFactory.getLogger(getClass().getCanonicalName());
         private Consumer<ClientResponse> onCompleteCallback;
         protected final SharePartitionKey partitionKey;
 
