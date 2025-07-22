02bc493a2aa75e177167aafa5c00a83c937a10af
Federico Valeri <fedevaleri@gmail.com>
2025-07-22 09:50:06 +0200

    Address comments
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- core/src/main/scala/kafka/utils/Mx4jLoader.scala
+++ core/src/main/scala/kafka/utils/Mx4jLoader.scala
@@ -37,7 +37,7 @@ object Mx4jLoader extends Logging {
     val props = new VerifiableProperties(System.getProperties)
     if (!props.getBoolean("kafka_mx4jenable", default = false))
       return false
-    warn("MX4j is deprecated and will be removed in the next major release")
+    warn("MX4j is deprecated and will be removed in Kafka 5.0")
     val address = props.getString("mx4jaddress", "0.0.0.0")
     val port = props.getInt("mx4jport", 8082)
     try {
