bd6f2fad16feca2e8be9378cbdb88fc7f5919247
m1a2st <s7133700@gmail.com>
2025-08-11 20:43:08 +0800

    fix LogConfigTest and KRaftClusterTest

--- core/src/test/scala/unit/kafka/log/LogConfigTest.scala
+++ core/src/test/scala/unit/kafka/log/LogConfigTest.scala
@@ -295,7 +295,7 @@ class LogConfigTest {
     assertThrows(classOf[ConfigException], () => validateCleanupPolicy())
     logProps.put(TopicConfig.CLEANUP_POLICY_CONFIG, "compact,delete")
     assertThrows(classOf[ConfigException], () => validateCleanupPolicy())
-    logProps.put(TopicConfig.CLEANUP_POLICY_CONFIG, "delete,delete,delete")
+    logProps.put(TopicConfig.CLEANUP_POLICY_CONFIG, "delete")
     validateCleanupPolicy()
     logProps.put(TopicConfig.CLEANUP_POLICY_CONFIG, "")
     assertThrows(classOf[ConfigException], () => validateCleanupPolicy())
--- storage/src/main/java/org/apache/kafka/storage/internals/log/LogConfig.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/LogConfig.java
@@ -561,8 +561,7 @@ public class LogConfig extends AbstractConfig {
     @SuppressWarnings("unchecked")
     private static void validateRemoteStorageRequiresDeleteCleanupPolicy(Map<?, ?> props) {
         List<String> cleanupPolicy = (List<String>) props.get(TopicConfig.CLEANUP_POLICY_CONFIG);
-        Set<String> policySet = cleanupPolicy.stream().map(policy -> policy.toLowerCase(Locale.getDefault())).collect(Collectors.toSet());
-        if (!Set.of(TopicConfig.CLEANUP_POLICY_DELETE).equals(policySet)) {
+        if (cleanupPolicy.size() != 1 || !TopicConfig.CLEANUP_POLICY_DELETE.equals(cleanupPolicy.get(0))) {
             throw new ConfigException("Remote log storage only supports topics with cleanup.policy=delete");
         }
     }
