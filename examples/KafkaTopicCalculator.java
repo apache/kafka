1dc47e91f2c7181607e3ff888562c1619452be08
hasanAshraful <ashrafulhasan601@gmail.com>
2025-08-20 22:18:16 +0600

    chore: modify calculator to use addition

--- examples/KafkaTopicCalculator.java
+++ examples/KafkaTopicCalculator.java
@@ -1,18 +1,5 @@
 public class KafkaTopicCalculator {
-
-    public int totalParts(int partPerTopic, int topicCount) {
-        return partPerTopic * topicCount;
-    }
-
-    public double storageGB(int msgSize, int msgsPerDay, int days) {
-        double dailyBytes = msgSize * msgsPerDay;
-        double totalBytes = dailyBytes * days;
-        return totalBytes / (1024.0 * 1024.0 * 1024.0);
-    }
-
-    public static void main(String[] args) {
-        KafkaTopicCalculator calc = new KafkaTopicCalculator();
-        System.out.println("Total Partitions: " + calc.totalParts(6, 10));
-        System.out.println("Estimated Storage: " + calc.storageGB(1024, 1000000, 7) + " GB");
+    public int totalParts(int a, int b) {
+        return a + b;
     }
 }
