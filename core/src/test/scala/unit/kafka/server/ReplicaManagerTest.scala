2766f9db04c65e53d3102a37b331a431bde0cf46
tony tang <ztang@confluent.io>
2025-08-26 14:46:36 -0500

    fix unit test

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -5335,16 +5335,6 @@ class ReplicaManagerTest {
     delta
   }
 
-  @Test
-  def testTopicsDeltaCreateThenDelete(): Unit ={
-    val delta = topicsCreateDelta(0, isStartIdLeader = true, partitions = List(0), topicName = topic, topicId = FOO_UUID)
-    assertEquals(delta.createdTopicIds().contains(FOO_UUID),true)
-    assertEquals(delta.deletedTopicIds().contains(FOO_UUID),false)
-    delta.replay(new RemoveTopicRecord().setTopicId(FOO_UUID))
-    assertEquals(delta.deletedTopicIds().contains(FOO_UUID),false)
-    assertEquals(delta.createdTopicIds().contains(FOO_UUID),false)
-  }
-
   private def imageFromTopics(topicsImage: TopicsImage): MetadataImage = {
     val featuresImageLatest = new FeaturesImage(
       Collections.emptyMap(),
