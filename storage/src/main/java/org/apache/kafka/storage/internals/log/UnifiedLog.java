65877f2cf340ac8a378e8d1c75030087f8161bb0
m1a2st <s7133700@gmail.com>
2025-08-31 11:36:51 +0800

    add upgrade.html section

--- storage/src/main/java/org/apache/kafka/storage/internals/log/UnifiedLog.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/UnifiedLog.java
@@ -1918,15 +1918,15 @@ public class UnifiedLog implements AutoCloseable {
         } else if (config().compact) {
             return deleteLogStartOffsetBreachedSegments();
         } else {
-            // If cleanup.policy is empty and remote storage is enable, the local log segments will 
-            // be cleaned based on the values of log.local.retention.bytes and log.local.retention.ms.
+            // If cleanup.policy is empty and remote storage is enabled, the local log segments will 
+            // be cleaned based on the values of log.local.retention.bytes and log.local.retention.ms
             if (remoteLogEnabledAndRemoteCopyEnabled()) {
                 return deleteLogStartOffsetBreachedSegments() +
                         deleteRetentionSizeBreachedSegments() +
                         deleteRetentionMsBreachedSegments();
             } else {
                 // If cleanup.policy is empty and remote storage is disabled, we should not delete any local 
-                // log segments.
+                // log segments
                 return deleteLogStartOffsetBreachedSegments();
             }
         }
