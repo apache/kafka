ef2d28496a6d12fa767c6133979468cc8119fb9e
TaiJu Wu <tjwu1217@gmail.com>
2025-09-02 13:49:39 +0800

    move to java

---
+++ transaction-coordinator/src/main/java/org/apache/kafka/coordinator/transaction/TransactionLog.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.coordinator.transaction;
+
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.compress.Compression;
+import org.apache.kafka.common.protocol.ByteBufferAccessor;
+import org.apache.kafka.common.protocol.MessageUtil;
+import org.apache.kafka.common.record.RecordBatch;
+import org.apache.kafka.coordinator.transaction.generated.CoordinatorRecordType;
+import org.apache.kafka.coordinator.transaction.generated.TransactionLogKey;
+import org.apache.kafka.coordinator.transaction.generated.TransactionLogValue;
+import org.apache.kafka.server.common.TransactionVersion;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Messages stored for the transaction topic represent the producer id and transactional status of the corresponding
+ * transactional id, which have versions for both the key and value fields. Key and value
+ * versions are used to evolve the message formats:
+ *
+ * key version 0:               [transactionalId]
+ *    -> value version 0:       [producer_id, producer_epoch, expire_timestamp, status, [topic, [partition] ], timestamp]
+ */
+public class TransactionLog {
+
+    // enforce always using
+    //  1. cleanup policy = compact
+    //  2. compression = none
+    //  3. unclean leader election = disabled
+    //  4. required acks = -1 when writing
+    public static final Compression EnforcedCompression = Compression.NONE;
+    public static final short EnforcedRequiredAcks = (short) -1;
+
+    /**
+     * Generates the bytes for transaction log message key
+     *
+     * @return key bytes
+     */
+    static byte[] keyToBytes(String transactionalId) {
+        return MessageUtil.toCoordinatorTypePrefixedBytes(
+                new TransactionLogKey().setTransactionalId(transactionalId)
+        );
+    }
+
+    /**
+     * Generates the payload bytes for transaction log message value
+     *
+     * @return value payload bytes
+     */
+    static byte[] valueToBytes(TxnTransitMetadata txnMetadata,
+                               TransactionVersion transactionVersionLevel) {
+        if (txnMetadata.txnState() == TransactionState.EMPTY && !txnMetadata.topicPartitions().isEmpty()) {
+            throw new IllegalStateException("Transaction is not expected to have any partitions since its state is "
+                    + txnMetadata.txnState() + ": " + txnMetadata);
+        }
+
+        List<TransactionLogValue.PartitionsSchema> transactionPartitions = null;
+
+        if (txnMetadata.txnState() != TransactionState.EMPTY) {
+            transactionPartitions = txnMetadata.topicPartitions().stream()
+                    .collect(Collectors.groupingBy(TopicPartition::topic))
+                    .entrySet().stream()
+                    .map(entry -> {
+                        TransactionLogValue.PartitionsSchema schema = new TransactionLogValue.PartitionsSchema();
+                        schema.setTopic(entry.getKey());
+                        schema.setPartitionIds(
+                                entry.getValue().stream()
+                                        .map(tp -> Integer.valueOf(tp.partition()))
+                                        .collect(Collectors.toList())
+                        );
+                        return schema;
+                    })
+                    .collect(Collectors.toList());
+        }
+
+        return MessageUtil.toVersionPrefixedBytes(
+                transactionVersionLevel.transactionLogValueVersion(),
+                new TransactionLogValue()
+                        .setProducerId(txnMetadata.producerId())
+                        .setProducerEpoch(txnMetadata.producerEpoch())
+                        .setTransactionTimeoutMs(txnMetadata.txnTimeoutMs())
+                        .setTransactionStatus(txnMetadata.txnState().id())
+                        .setTransactionLastUpdateTimestampMs(txnMetadata.txnLastUpdateTimestamp())
+                        .setTransactionStartTimestampMs(txnMetadata.txnStartTimestamp())
+                        .setTransactionPartitions(transactionPartitions)
+                        .setClientTransactionVersion(txnMetadata.clientTransactionVersion().featureLevel())
+        );
+    }
+
+    /**
+     * Decodes the transaction log messages' key
+     *
+     * @return Either: left with the version if the key is not a transaction log key, right with the transactional id otherwise
+     */
+    static Object readTxnRecordKey(ByteBuffer buffer) {
+        short version = buffer.getShort();
+        if (version == CoordinatorRecordType.TRANSACTION_LOG.id()) {
+            return new TransactionLogKey(new ByteBufferAccessor(buffer), (short) 0).transactionalId();
+        } else {
+            return version; // like Scala Left(version)
+        }
+    }
+
+    /**
+     * Decodes the transaction log messages' payload and retrieves the transaction metadata from it
+     *
+     * @return a transaction metadata object from the message, or null if tombstone
+     */
+    static TransactionMetadata readTxnRecordValue(String transactionalId, ByteBuffer buffer) {
+        if (buffer == null) {
+            return null; // tombstone
+        } else {
+            short version = buffer.getShort();
+            if (version >= TransactionLogValue.LOWEST_SUPPORTED_VERSION
+                    && version <= TransactionLogValue.HIGHEST_SUPPORTED_VERSION) {
+
+                TransactionLogValue value = new TransactionLogValue(new ByteBufferAccessor(buffer), version);
+                TransactionState state = TransactionState.fromId(value.transactionStatus());
+
+                Set<TopicPartition> tps = new HashSet<>();
+                if (!state.equals(TransactionState.EMPTY)) {
+                    for (TransactionLogValue.PartitionsSchema partitionsSchema : value.transactionPartitions()) {
+                        for (Integer partitionId : partitionsSchema.partitionIds()) {
+                            tps.add(new TopicPartition(partitionsSchema.topic(), partitionId.intValue()));
+                        }
+                    }
+                }
+
+                return new TransactionMetadata(
+                        transactionalId,
+                        value.producerId(),
+                        value.previousProducerId(),
+                        value.nextProducerId(),
+                        value.producerEpoch(),
+                        RecordBatch.NO_PRODUCER_EPOCH,
+                        value.transactionTimeoutMs(),
+                        state,
+                        tps,
+                        value.transactionStartTimestampMs(),
+                        value.transactionLastUpdateTimestampMs(),
+                        TransactionVersion.fromFeatureLevel(value.clientTransactionVersion())
+                );
+            } else {
+                throw new IllegalStateException("Unknown version " + version + " from the transaction log message value");
+            }
+        }
+    }
+}
