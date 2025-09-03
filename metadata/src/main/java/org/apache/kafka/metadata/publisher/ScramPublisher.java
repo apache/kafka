b45fe8040b3a18d8e9f55721bf5777942cd9004b
see-quick <maros.orsak159@gmail.com>
2025-09-03 15:54:07 +0200

    checkstyle
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- metadata/src/main/java/org/apache/kafka/metadata/publisher/ScramPublisher.java
+++ metadata/src/main/java/org/apache/kafka/metadata/publisher/ScramPublisher.java
@@ -16,7 +16,6 @@
  */
 package org.apache.kafka.metadata.publisher;
 
-import org.apache.kafka.common.utils.LogContext;
 import org.apache.kafka.image.MetadataDelta;
 import org.apache.kafka.image.MetadataImage;
 import org.apache.kafka.image.ScramDelta;
@@ -24,8 +23,6 @@ import org.apache.kafka.image.loader.LoaderManifest;
 import org.apache.kafka.image.publisher.MetadataPublisher;
 import org.apache.kafka.server.common.CredentialProvider;
 import org.apache.kafka.server.fault.FaultHandler;
-
-import org.slf4j.Logger;
 
 import java.util.Optional;
 
