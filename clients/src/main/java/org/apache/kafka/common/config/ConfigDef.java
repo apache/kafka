8744f0b0d9431fa938bad3acb81d44de01aa08fc
Claude Warren <claude.warren@aiven.io>
2025-09-05 07:52:58 +0100

    updated javadoc

--- clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
+++ clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
@@ -1371,6 +1371,10 @@ public class ConfigDef {
             return type;
         }
 
+        /**
+         * Builder for ConfigDef.ConfigKey instances.
+         * @param <T> the class of the builder itself.
+         */
         public static class Builder<T extends Builder<?>> {
             private final String name;
             private ConfigDef.Type type;
@@ -1387,6 +1391,17 @@ public class ConfigDef {
             private boolean internalConfig;
             private String alternativeString;
 
+            /**
+             * Initializes the builder with the specified {@code name}.  Sets the following defaults:
+             * <ul>
+             *     <li>type = {@link ConfigDef.Type#STRING}</li>
+             *     <li>displayName = {@code name}</li>
+             *     <li>defaultValue = {@link #NO_DEFAULT_VALUE}</li>
+             *     <li>orderInGorup = -1</li>
+             *     <li>width = {@link ConfigDef.Width#NONE}</li>
+             * </ul>
+             * @param name the name of the ConfigKey instance.
+             */
             protected Builder(String name) {
                 this.name = name;
                 this.type = ConfigDef.Type.STRING;
@@ -1450,6 +1465,13 @@ public class ConfigDef {
                 return self();
             }
 
+            /**
+             * Adds the dependents to the dependents for the final ConfigDef.ConfigKey.
+             * Once added dependents can not be removed within the builder.
+             * Dependents are unique, Adding a dependent multiple times has no effect.
+             * @param dependents the collection of dependents to add.
+             * @return this
+             */
             public final T dependents(final Collection<String> dependents) {
                 if (this.dependents == null) {
                     this.dependents = new LinkedHashSet<>(dependents);
@@ -1459,6 +1481,13 @@ public class ConfigDef {
                 return self();
             }
 
+            /**
+             * Adds a dependent to the dependents for the final ConfigDef.ConfigKey.
+             * Once added dependents can not be removed within the builder.
+             * Dependents are unique, Adding a dependent multiple times has no effect.
+             * @param dependent the dependent to add.
+             * @return this
+             */
             public final T dependent(final String dependent) {
                 if (this.dependents == null) {
                     this.dependents = new LinkedHashSet<>();
