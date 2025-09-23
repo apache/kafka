8ee51308ec600af1368bf4d090771b226551e0d8
see-quick <maros.orsak159@gmail.com>
2025-09-23 16:13:31 +0200

    update review
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- tests/docker/run_tests.sh
+++ tests/docker/run_tests.sh
@@ -22,9 +22,8 @@ REBUILD=${REBUILD:f}
 
 # Auto-detect container runtime if not set
 if [[ -z "${CONTAINER_RUNTIME}" ]]; then
-    if command -v docker &> /dev/null; then
-        export CONTAINER_RUNTIME="docker"
-    elif command -v podman &> /dev/null; then
+    export CONTAINER_RUNTIME="docker"
+    if command -v podman &> /dev/null; then
         export CONTAINER_RUNTIME="podman"
     fi
 fi
