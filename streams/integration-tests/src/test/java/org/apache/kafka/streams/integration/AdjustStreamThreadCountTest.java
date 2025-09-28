02c828bda4b5e8d8a0f994b22a8584d906f25b7a
deepgolani4 <deegolani13@gmail.com>
2025-09-28 15:11:01 -0400

    reduce dummy data to 1k for aclCtlMgr tests

--- streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/AdjustStreamThreadCountTest.java
+++ streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/AdjustStreamThreadCountTest.java
@@ -146,7 +146,7 @@ public class AdjustStreamThreadCountTest {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         try (KafkaProducer<String, String> dummyProducer = new KafkaProducer<>(props)) {
-            for (int i = 0; i < 5000; i++) {
+            for (int i = 0; i < 1000; i++) {
                 final String key = "key-" + (i % 50);
                 final String value = "value-" + i;
                 dummyProducer.send(new ProducerRecord<>(inputTopic, key, value));
