a20c3cb8c403e303e38f3b195806c6d959d690fb
Jhen-Yung Hsu <jhenyunghsu@gmail.com>
2025-09-29 09:02:55 +0800

    Revert breaking change for bootstrap-server

--- tools/src/main/java/org/apache/kafka/tools/StreamsResetter.java
+++ tools/src/main/java/org/apache/kafka/tools/StreamsResetter.java
@@ -141,7 +141,10 @@ public class StreamsResetter {
             }
             Properties properties = (commandConfigFile != null) ? Utils.loadProps(commandConfigFile) : new Properties();
 
-            String bootstrapServerValue = options.bootstrapServer();
+            String bootstrapServerValue = "localhost:9092";
+            if (options.hasBootstrapServer()) {
+                bootstrapServerValue = options.bootstrapServer();
+            }
 
             properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerValue);
 
@@ -587,11 +590,10 @@ public class StreamsResetter {
                 .ofType(String.class)
                 .describedAs("id")
                 .required();
-            bootstrapServerOption = parser.accepts("bootstrap-server", "REQUIRED: The server(s) to connect to. The broker list string in the form HOST1:PORT1,HOST2:PORT2.")
+            bootstrapServerOption = parser.accepts("bootstrap-server", "The server(s) to connect to. The broker list string in the form HOST1:PORT1,HOST2:PORT2. (default: localhost:9092)")
                 .withRequiredArg()
                 .ofType(String.class)
-                .describedAs("server to connect to")
-                .required();
+                .describedAs("server to connect to");
             inputTopicsOption = parser.accepts("input-topics", "Comma-separated list of user input topics. For these topics, the tool by default will reset the offset to the earliest available offset. "
                     + "Reset to other offset position by appending other reset offset option, ex: --input-topics foo --shift-by 5")
                 .withRequiredArg()
@@ -677,6 +679,10 @@ public class StreamsResetter {
 
         public String commandConfig() {
             return options.valueOf(commandConfigOption);
+        }
+
+        public boolean hasBootstrapServer() {
+            return options.has(bootstrapServerOption);
         }
 
         public String bootstrapServer() {
