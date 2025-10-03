04313d742f6e5fcecfaf8508054f4dddd98e7f07
TaiJu Wu <tjwu1217@gmail.com>
2025-10-03 12:54:10 +0800

    remove brokerConfigBuilder

--- test-common/test-common-runtime/src/main/java/org/apache/kafka/common/test/TestUtils.java
+++ test-common/test-common-runtime/src/main/java/org/apache/kafka/common/test/TestUtils.java
@@ -156,10 +156,6 @@ public class TestUtils {
         throw new RuntimeException("Failed to create directory after 1000 attempts");
     }
 
-    public static Properties createBrokerConfig(int nodeId, int port) {
-        return new BrokerConfigBuilder(nodeId).withPort(port).build();
-    }
-
     public static MemoryRecords singletonRecords(byte[] value, byte[] key) {
         return singletonRecords(value, key, Compression.NONE, RecordBatch.NO_TIMESTAMP, RecordBatch.CURRENT_MAGIC_VALUE);
     }
@@ -238,110 +234,5 @@ public class TestUtils {
             builder.append(record);
         }
         return builder.build();
-    }
-
-    public static class BrokerConfigBuilder {
-        private final int nodeId;
-        private boolean enableControlledShutdown = true;
-        private boolean enableDeleteTopic = true;
-        private int port = -1;
-        private Optional<SecurityProtocol> interBrokerSecurityProtocol = Optional.empty();
-        private Optional<File> trustStoreFile = Optional.empty();
-        private Optional<Properties> saslProperties = Optional.empty();
-        private boolean enablePlaintext = true;
-        private boolean enableSaslPlaintext = false;
-        private int saslPlaintextPort = -1;
-        private boolean enableSsl = false;
-        private int sslPort = -1;
-        private boolean enableSaslSsl = false;
-        private int saslSslPort = -1;
-        private Optional<String> rack = Optional.empty();
-        private int logDirCount = 1;
-        private int numPartitions = 1;
-        private short defaultReplicationFactor = 1;
-        private boolean enableFetchFromFollower = false;
-
-        public BrokerConfigBuilder(int nodeId) {
-            this.nodeId = nodeId;
-        }
-
-        public BrokerConfigBuilder withPort(int port) {
-            this.enablePlaintext = true;
-            this.port = port;
-            return this;
-        }
-
-        public BrokerConfigBuilder withSsl(int port, File trustStoreFile) {
-            this.enableSsl = true;
-            this.sslPort = port;
-            this.trustStoreFile = Optional.of(trustStoreFile);
-            return this;
-        }
-
-        public Properties build() {
-            List<Map.Entry<SecurityProtocol, Integer>> protocolAndPorts = new ArrayList<>();
-
-            if (enablePlaintext || (interBrokerSecurityProtocol.isPresent() && interBrokerSecurityProtocol.get() == SecurityProtocol.PLAINTEXT))
-                protocolAndPorts.add(new AbstractMap.SimpleEntry<>(SecurityProtocol.PLAINTEXT, port));
-            if (enableSsl || (interBrokerSecurityProtocol.isPresent() && interBrokerSecurityProtocol.get() == SecurityProtocol.SSL))
-                protocolAndPorts.add(new AbstractMap.SimpleEntry<>(SecurityProtocol.SSL, sslPort));
-            if (enableSaslPlaintext || (interBrokerSecurityProtocol.isPresent() && interBrokerSecurityProtocol.get() == SecurityProtocol.SASL_PLAINTEXT))
-                protocolAndPorts.add(new AbstractMap.SimpleEntry<>(SecurityProtocol.SASL_PLAINTEXT, saslPlaintextPort));
-            if (enableSaslSsl || (interBrokerSecurityProtocol.isPresent() && interBrokerSecurityProtocol.get() == SecurityProtocol.SASL_SSL))
-                protocolAndPorts.add(new AbstractMap.SimpleEntry<>(SecurityProtocol.SASL_SSL, saslSslPort));
-
-            String listeners = protocolAndPorts.stream()
-                    .map(entry -> String.format("%s://localhost:%d", entry.getKey().name(), entry.getValue()))
-                    .collect(Collectors.joining(","));
-
-            Properties props = new Properties();
-            props.put(ServerConfigs.UNSTABLE_FEATURE_VERSIONS_ENABLE_CONFIG, "true");
-            props.put(ServerConfigs.UNSTABLE_API_VERSIONS_ENABLE_CONFIG, "true");
-            props.put(KRaftConfigs.SERVER_MAX_STARTUP_TIME_MS_CONFIG, String.valueOf(TimeUnit.MINUTES.toMillis(10)));
-            props.put(KRaftConfigs.NODE_ID_CONFIG, String.valueOf(nodeId));
-            props.put(ServerConfigs.BROKER_ID_CONFIG, String.valueOf(nodeId));
-            props.put(SocketServerConfigs.ADVERTISED_LISTENERS_CONFIG, listeners);
-            props.put(SocketServerConfigs.LISTENERS_CONFIG, listeners);
-            props.put(KRaftConfigs.CONTROLLER_LISTENER_NAMES_CONFIG, "CONTROLLER");
-
-            String securityProtocolMap = protocolAndPorts.stream()
-                    .map(entry -> String.format("%s:%s", entry.getKey().name(), entry.getKey().name()))
-                    .collect(Collectors.joining(",")) + ",CONTROLLER:PLAINTEXT";
-            props.put(SocketServerConfigs.LISTENER_SECURITY_PROTOCOL_MAP_CONFIG, securityProtocolMap);
-
-            if (logDirCount > 1) {
-                String logDirs = IntStream.range(0, logDirCount)
-                        .mapToObj(i -> tempDirectory().getAbsolutePath())
-                        .collect(Collectors.joining(","));
-                props.put(ServerLogConfigs.LOG_DIRS_CONFIG, logDirs);
-            } else {
-                props.put(ServerLogConfigs.LOG_DIR_CONFIG, tempDirectory().getAbsolutePath());
-            }
-
-            props.put(KRaftConfigs.PROCESS_ROLES_CONFIG, "broker");
-            props.put(QuorumConfig.QUORUM_VOTERS_CONFIG, "1000@localhost:0");
-            props.put(ServerConfigs.CONTROLLED_SHUTDOWN_ENABLE_CONFIG, String.valueOf(enableControlledShutdown));
-            props.put(ServerConfigs.DELETE_TOPIC_ENABLE_CONFIG, String.valueOf(enableDeleteTopic));
-
-            rack.ifPresent(r -> props.put(ServerConfigs.BROKER_RACK_CONFIG, r));
-
-            try {
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-            interBrokerSecurityProtocol.ifPresent(protocol ->
-                    props.put(ReplicationConfigs.INTER_BROKER_SECURITY_PROTOCOL_CONFIG, protocol.name()));
-
-            props.put(ServerLogConfigs.NUM_PARTITIONS_CONFIG, String.valueOf(numPartitions));
-            props.put(ReplicationConfigs.DEFAULT_REPLICATION_FACTOR_CONFIG, String.valueOf(defaultReplicationFactor));
-
-            if (enableFetchFromFollower) {
-                props.put(ServerConfigs.BROKER_RACK_CONFIG, String.valueOf(nodeId));
-                props.put(ReplicationConfigs.REPLICA_SELECTOR_CLASS_CONFIG, "org.apache.kafka.common.replica.RackAwareReplicaSelector");
-            }
-
-            return props;
-        }
     }
 }
