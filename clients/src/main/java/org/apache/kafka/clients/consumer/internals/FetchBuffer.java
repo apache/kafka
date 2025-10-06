ddf042306c92069407948c603808993001ef6569
Kirk True <kirk@kirktrue.pro>
2025-10-05 21:19:16 -0700

    Revert wakeup() method visibility back to package-private

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/FetchBuffer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/FetchBuffer.java
@@ -193,7 +193,7 @@ public class FetchBuffer implements AutoCloseable {
         }
     }
 
-    public void wakeup() {
+    void wakeup() {
         try {
             lock.lock();
             wokenup.set(true);
