a9b7f247276a0f6ca8fc620642f9320a2d955d22
Bill <bbejeck@apache.org>
2025-10-07 19:36:52 -0400

    Update `ClientTelemetryReporter` to handle failures by differentiating between retryable and fatal exceptions and add test cases

--- clients/src/main/java/org/apache/kafka/common/telemetry/internals/ClientTelemetryReporter.java
+++ clients/src/main/java/org/apache/kafka/common/telemetry/internals/ClientTelemetryReporter.java
@@ -20,6 +20,7 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.Uuid;
 import org.apache.kafka.common.errors.InterruptException;
+import org.apache.kafka.common.errors.RetriableException;
 import org.apache.kafka.common.message.GetTelemetrySubscriptionsRequestData;
 import org.apache.kafka.common.message.GetTelemetrySubscriptionsResponseData;
 import org.apache.kafka.common.message.PushTelemetryRequestData;
@@ -524,17 +525,41 @@ public class ClientTelemetryReporter implements MetricsReporter {
                 lock.writeLock().unlock();
             }
         }
+        
+        private boolean isRetryable(final KafkaException maybeFatalException) {
+            if (maybeFatalException == null) {
+                return true;
+            }
+
+            Throwable cause;
+            if (maybeFatalException.getClass() == KafkaException.class) {
+                if (maybeFatalException.getCause() == null) {
+                    return false;
+                } else {
+                    cause = maybeFatalException.getCause();
+                }
+            } else {
+                cause = maybeFatalException;
+            }
+            while (cause != null) {
+                if (!(cause instanceof RetriableException)) {
+                    return false;
+                }
+                cause = cause.getCause();
+            }
+            return true;
+        }
 
         @Override
         public void handleFailedGetTelemetrySubscriptionsRequest(KafkaException maybeFatalException) {
             log.debug("The broker generated an error for the get telemetry network API request", maybeFatalException);
-            handleFailedRequest(maybeFatalException != null);
+            handleFailedRequest(isRetryable(maybeFatalException));
         }
 
         @Override
         public void handleFailedPushTelemetryRequest(KafkaException maybeFatalException) {
             log.debug("The broker generated an error for the push telemetry network API request", maybeFatalException);
-            handleFailedRequest(maybeFatalException != null);
+            handleFailedRequest(isRetryable(maybeFatalException));
         }
 
         @Override
