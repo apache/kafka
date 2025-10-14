0d0be3cbbcbdb2389eddfc8158ae44095c23bdef
Andrew Schofield <aschofield@confluent.io>
2025-10-14 11:32:48 +0100

    MINOR: Tidy up comments in MetadataVersion

--- server-common/src/main/java/org/apache/kafka/server/common/MetadataVersion.java
+++ server-common/src/main/java/org/apache/kafka/server/common/MetadataVersion.java
@@ -108,8 +108,6 @@ public enum MetadataVersion {
     IBP_4_0_IV3(25, "4.0", "IV3", false),
 
     // Enables ELR by default for new clusters (KIP-966).
-    // Share groups are preview in 4.1 (KIP-932).
-    // Streams groups are early access in 4.1 (KIP-1071).
     IBP_4_1_IV0(26, "4.1", "IV0", false),
 
     // Send FETCH version 18 in the replica fetcher (KIP-1166)
@@ -121,24 +119,10 @@ public enum MetadataVersion {
     // Please move this comment when updating the LATEST_PRODUCTION constant.
     //
 
-    // Insert any additional IBP_4_1_IVx versions above this comment, and bump the feature level of
-    // IBP_4_2_IVx accordingly. When 4.2 development begins, IBP_4_2_IV0 will cease to be
-    // a placeholder.
-
     // Enables share groups by default for new clusters (KIP-932).
-    //
-    // *** THIS IS A PLACEHOLDER UNSTABLE VERSION WHICH IS USED TO DEFINE THE POINT AT WHICH   ***
-    // *** SHARE GROUPS BECOME PRODUCTION-READY IN THE FUTURE. ITS DEFINITION ALLOWS A SHARE   ***
-    // *** GROUPS FEATURE TO BE DEFINED IN 4.1 BUT TURNED OFF BY DEFAULT, ABLE TO BE TURNED ON ***
-    // *** DYNAMICALLY TO TRY OUT THE PREVIEW CAPABILITY.                                      ***
     IBP_4_2_IV0(28, "4.2", "IV0", false),
 
     // Enables "streams" groups by default for new clusters (KIP-1071).
-    //
-    // *** THIS IS A PLACEHOLDER UNSTABLE VERSION WHICH IS USED TO DEFINE THE POINT AT WHICH     ***
-    // *** STREAMS GROUPS BECOME PRODUCTION-READY IN THE FUTURE. ITS DEFINITION ALLOWS A STREAMS ***
-    // *** GROUPS FEATURE TO BE DEFINED IN 4.1 BUT TURNED OFF BY DEFAULT, ABLE TO BE TURNED ON   ***
-    // *** DYNAMICALLY TO TRY OUT THE EARLY ACCESS CAPABILITY.                                   ***
     IBP_4_2_IV1(29, "4.2", "IV1", false);
 
     // NOTES when adding a new version:
