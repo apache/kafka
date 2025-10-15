ef0620699bffe1d574a4f019940db85c25346f8e
TaiJu Wu <tjwu1217@gmail.com>
2025-10-15 19:38:55 +0800

    add test

--- tools/src/test/java/org/apache/kafka/tools/BrokerApiVersionsCommandTest.java
+++ tools/src/test/java/org/apache/kafka/tools/BrokerApiVersionsCommandTest.java
@@ -28,9 +28,11 @@ import org.apache.kafka.common.test.ClusterInstance;
 import org.apache.kafka.common.test.api.ClusterConfigProperty;
 import org.apache.kafka.common.test.api.ClusterTest;
 import org.apache.kafka.common.test.api.ClusterTestDefaults;
+import org.apache.kafka.common.test.api.Type;
 import org.apache.kafka.server.config.ServerConfigs;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -40,24 +42,48 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ClusterTestDefaults(serverProperties = {
+@ClusterTestDefaults(types = {Type.KRAFT}, serverProperties = {
     @ClusterConfigProperty(key = ServerConfigs.UNSTABLE_API_VERSIONS_ENABLE_CONFIG, value = "true"),
 })
 public class BrokerApiVersionsCommandTest {
     @ClusterTest
     public void testBrokerApiVersionsCommandOutput(ClusterInstance clusterInstance) {
-        String output = ToolsTestUtils.grabConsoleOutput(() ->
-                BrokerApiVersionsCommand.mainNoExit("--bootstrap-server", clusterInstance.bootstrapServers()));
+        testBrokerApiVersionsCommandOutput(clusterInstance, false);
+        testBrokerApiVersionsCommandOutput(clusterInstance, true);
+    }
+
+    private void testBrokerApiVersionsCommandOutput(ClusterInstance clusterInstance, boolean usingBootstrapController) {
+        String output = ToolsTestUtils.grabConsoleOutput(() -> {
+            if (usingBootstrapController) {
+                BrokerApiVersionsCommand.mainNoExit("--bootstrap-controller", clusterInstance.bootstrapControllers());
+            } else {
+                BrokerApiVersionsCommand.mainNoExit("--bootstrap-server", clusterInstance.bootstrapServers());
+            }
+        });
         Iterator<String> lineIter = Arrays.stream(output.split("\n")).iterator();
         assertTrue(lineIter.hasNext());
-        assertEquals(clusterInstance.bootstrapServers() + " (id: 0 rack: null isFenced: false) -> (", lineIter.next());
+        ApiMessageType.ListenerType listenerType;
 
-        ApiMessageType.ListenerType listenerType = ApiMessageType.ListenerType.BROKER;
+        if (usingBootstrapController) {
+            assertEquals(clusterInstance.bootstrapControllers() + " (id: 3000 rack: null isFenced: false) -> (", lineIter.next());
+            listenerType = ApiMessageType.ListenerType.CONTROLLER;
+        } else {
+            assertEquals(clusterInstance.bootstrapServers() + " (id: 0 rack: null isFenced: false) -> (", lineIter.next());
+            listenerType = ApiMessageType.ListenerType.BROKER;
+        }
+
+        EnumSet<ApiKeys> apiKeys = ApiKeys.clientApis();
+
+        // Controller will return all apis
+        if (listenerType == ApiMessageType.ListenerType.CONTROLLER) {
+            apiKeys.addAll(ApiKeys.controllerApis());
+        }
+
+        Iterator<ApiKeys> apiKeysIter = apiKeys.iterator();
 
         NodeApiVersions nodeApiVersions = new NodeApiVersions(
                 ApiVersionsResponse.filterApis(listenerType, true, true),
                 List.of());
-        Iterator<ApiKeys> apiKeysIter = ApiKeys.clientApis().iterator();
         while (apiKeysIter.hasNext()) {
             ApiKeys apiKey = apiKeysIter.next();
             String terminator = apiKeysIter.hasNext() ? "," : "";
