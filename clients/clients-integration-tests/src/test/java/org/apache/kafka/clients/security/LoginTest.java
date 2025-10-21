425f5782d2463125c417f29f1e3252113be97d96
m1a2st <s7133700@gmail.com>
2025-10-21 18:11:08 +0800

    completed the test

--- clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/security/LoginTest.java
+++ clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/security/LoginTest.java
@@ -54,27 +54,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
 
-    private static final String USERNAME = "userA";
-    private static final String PASSWORD = "pwd";
+    private static final String CONTROLLER_USERNAME = "userA";
+    private static final String CONTROLLER_PASSWORD = "pwdA";
+    private static final String EXTERNAL_USERNAME = "userB";
+    private static final String EXTERNAL_PASSWORD = "pwdB";
     private static final String CLIENT_ID = "test-login-client";
     private static final String LISTENER_PREFIX = "listener.name.controller.";
     private static final String EXTERNAL_PREFIX = "listener.name.external.";
     private static final String MECHANISMS = "PLAIN";
     private static final String MECHANISMS_PREFIX = "plain.";
-    private static final String SASL_JAAS = "org.apache.kafka.common.security.plain.PlainLoginModule required "
-        + "user_" + USERNAME + "=\"" + PASSWORD + "\";";
+    private static final String CONTROLLER_SASL_JAAS = "org.apache.kafka.common.security.plain.PlainLoginModule required "
+        + "user_" + CONTROLLER_USERNAME + "=\"" + CONTROLLER_PASSWORD + "\";";
+    private static final String EXTERNAL_SASL_JAAS = "org.apache.kafka.common.security.plain.PlainLoginModule required "
+        + "user_" + EXTERNAL_USERNAME + "=\"" + EXTERNAL_PASSWORD + "\";";
 
     @ClusterTest(
         types = {Type.CO_KRAFT},
         controllerSecurityProtocol = SecurityProtocol.SASL_PLAINTEXT,
         brokerSecurityProtocol = SecurityProtocol.SASL_PLAINTEXT,
         serverProperties = {
-            @ClusterConfigProperty(key = LISTENER_PREFIX + MECHANISMS_PREFIX + SASL_LOGIN_CLASS, value = "org.apache.kafka.clients.security.LoginTest$CustomerLogin"),
-            @ClusterConfigProperty(key = EXTERNAL_PREFIX + MECHANISMS_PREFIX + SASL_LOGIN_CLASS, value = "org.apache.kafka.clients.security.LoginTest$CustomerLogin"),
+            @ClusterConfigProperty(key = LISTENER_PREFIX + MECHANISMS_PREFIX + SASL_LOGIN_CLASS, value = "org.apache.kafka.clients.security.LoginTest$ControllerCustomerLogin"),
+            @ClusterConfigProperty(key = EXTERNAL_PREFIX + MECHANISMS_PREFIX + SASL_LOGIN_CLASS, value = "org.apache.kafka.clients.security.LoginTest$ExternalCustomerLogin"),
             @ClusterConfigProperty(key = SASL_ENABLED_MECHANISMS_CONFIG, value = MECHANISMS),
             @ClusterConfigProperty(key = SASL_MECHANISM_INTER_BROKER_PROTOCOL_CONFIG, value = MECHANISMS),
-            @ClusterConfigProperty(key = LISTENER_PREFIX + MECHANISMS_PREFIX + SASL_JAAS_CONFIG, value = SASL_JAAS),
-            @ClusterConfigProperty(key = SUPER_USERS_CONFIG, value = "User:" + USERNAME),
+            @ClusterConfigProperty(key = LISTENER_PREFIX + MECHANISMS_PREFIX + SASL_JAAS_CONFIG, value = CONTROLLER_SASL_JAAS),
+            @ClusterConfigProperty(key = EXTERNAL_PREFIX + MECHANISMS_PREFIX + SASL_JAAS_CONFIG, value = EXTERNAL_SASL_JAAS),
+            @ClusterConfigProperty(key = SUPER_USERS_CONFIG, value = "User:" + CONTROLLER_USERNAME + ";User:" + EXTERNAL_USERNAME),
         }
     )
     public void testCustomLoginWithKafkaCluster(ClusterInstance cluster) {
@@ -83,7 +88,12 @@ public class LoginTest {
             for (Metric metric : admin.metrics().values()) {
                 found += assertMetricName(
                     metric.metricName(),
-                    expectedTags(Map.of("client-id", CLIENT_ID))
+                    ExternalCustomerLogin.METRIC_NAME,
+                    ExternalCustomerLogin.METRIC_DESCRIPTION,
+                    expectedTags(Map.of(
+                        "class", ExternalCustomerLogin.class.getSimpleName(),
+                        "client-id", CLIENT_ID
+                    ))
                 );
             }
             assertEquals(1, found, "Expected to find 1 metric");
@@ -94,36 +104,52 @@ public class LoginTest {
             ).collect(HashMap::new, Map::putAll, Map::putAll);
             assertMetrics(
                 allMetrics,
+                ExternalCustomerLogin.METRIC_NAME,
+                ExternalCustomerLogin.METRIC_DESCRIPTION,
                 expectedTags(Map.of(
                     "mechanism", MECHANISMS,
                     "listener", "EXTERNAL",
-                    "networkProcessor", "0"
+                    "networkProcessor", "0",
+                    "class", ExternalCustomerLogin.class.getSimpleName()
                 ))
             );
             assertMetrics(
                 allMetrics,
+                ControllerCustomerLogin.METRIC_NAME,
+                ControllerCustomerLogin.METRIC_DESCRIPTION,
                 expectedTags(Map.of(
                     "node-id", "0",
-                    "component", "raft-channel"
+                    "component", "raft-channel",
+                    "class", ControllerCustomerLogin.class.getSimpleName()
                 ))
             );
         }
     }
 
-    private int assertMetricName(MetricName metricName, Map<String, String> expectedTags) {
+    private int assertMetricName(
+        MetricName metricName,
+        String expectedName,
+        String expectedDescription,
+        Map<String, String> expectedTags
+    ) {
         Map<String, String> tags = metricName.tags();
         if (metricName.group().equals("plugins") && expectedTags.equals(tags)) {
-            assertEquals(CustomerLogin.METRIC_NAME, metricName.name());
-            assertEquals(CustomerLogin.METRIC_DESCRIPTION, metricName.description());
+            assertEquals(expectedName, metricName.name());
+            assertEquals(expectedDescription, metricName.description());
             return 1;
         }
         return 0;
     }
 
-    private void assertMetrics(Map<MetricName, KafkaMetric> metrics, Map<String, String> expectedTags) {
+    private void assertMetrics(
+        Map<MetricName, KafkaMetric> metrics,
+        String expectedName,
+        String expectedDescription,
+        Map<String, String> expectedTags
+    ) {
         int found = 0;
         for (MetricName metricName : metrics.keySet()) {
-            found += assertMetricName(metricName, expectedTags);
+            found += assertMetricName(metricName, expectedName, expectedDescription, expectedTags);
         }
         assertEquals(1, found, "Expected to find 1 metric with the expected tags");
     }
@@ -131,7 +157,6 @@ public class LoginTest {
     private static Map<String, String> expectedTags(Map<String, String> extraTags) {
         Map<String, String> tags = new LinkedHashMap<>();
         tags.put("config", SASL_LOGIN_CLASS);
-        tags.put("class", CustomerLogin.class.getSimpleName());
         tags.putAll(extraTags);
         return tags;
     }
@@ -141,15 +166,15 @@ public class LoginTest {
             SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
             CLIENT_ID_CONFIG, CLIENT_ID,
             SASL_MECHANISM, MECHANISMS,
-            SASL_LOGIN_CLASS, LoginTest.CustomerLogin.class.getName(),
-            SASL_JAAS_CONFIG, SASL_JAAS
+            SASL_LOGIN_CLASS, LoginTest.ExternalCustomerLogin.class.getName(),
+            SASL_JAAS_CONFIG, CONTROLLER_SASL_JAAS
         );
     }
 
-    public static class CustomerLogin implements Login, Monitorable {
+    public static class ControllerCustomerLogin implements Login, Monitorable {
 
-        private static final String METRIC_NAME = "monitorable-custom-login-name";
-        private static final String METRIC_DESCRIPTION = "monitorable-custom-login-description";
+        private static final String METRIC_NAME = "monitorable-controller-custom-login-name";
+        private static final String METRIC_DESCRIPTION = "monitorable-controller-custom-login-description";
 
         private String contextName;
         private Configuration configuration;
@@ -179,8 +204,8 @@ public class LoginTest {
             subject = context.getSubject();
             subject.getPublicCredentials().clear();
             subject.getPrivateCredentials().clear();
-            subject.getPublicCredentials().add(USERNAME);
-            subject.getPrivateCredentials().add(PASSWORD);
+            subject.getPublicCredentials().add(CONTROLLER_USERNAME);
+            subject.getPrivateCredentials().add(CONTROLLER_PASSWORD);
             return context;
         }
 
@@ -191,7 +216,61 @@ public class LoginTest {
 
         @Override
         public String serviceName() {
-            return "customer login";
+            return "controller customer login";
+        }
+
+        @Override
+        public void close() {
+
+        }
+    }
+
+    public static class ExternalCustomerLogin implements Login, Monitorable {
+
+        private static final String METRIC_NAME = "monitorable-external-custom-login-name";
+        private static final String METRIC_DESCRIPTION = "monitorable-external-custom-login-description";
+
+        private String contextName;
+        private Configuration configuration;
+        private Subject subject;
+
+        @Override
+        public void withPluginMetrics(PluginMetrics metrics) {
+            MetricName metricName = metrics.metricName(METRIC_NAME, METRIC_DESCRIPTION, new LinkedHashMap<>());
+            metrics.addMetric(metricName, (Gauge<Integer>) (config, now) -> 1);
+        }
+
+        @Override
+        public void configure(
+                Map<String, ?> configs,
+                String contextName,
+                Configuration configuration,
+                AuthenticateCallbackHandler loginCallbackHandler
+        ) {
+            this.contextName = contextName;
+            this.configuration = configuration;
+        }
+
+        @Override
+        public LoginContext login() throws LoginException {
+            LoginContext context = new LoginContext(contextName, null, new AbstractLogin.DefaultLoginCallbackHandler(), configuration);
+            context.login();
+            subject = context.getSubject();
+            subject.getPublicCredentials().clear();
+            subject.getPrivateCredentials().clear();
+            subject.getPublicCredentials().add(EXTERNAL_USERNAME);
+            subject.getPrivateCredentials().add(EXTERNAL_PASSWORD);
+            return context;
+        }
+
+        @Override
+        public Subject subject() {
+            return subject;
+        }
+
+        @Override
+        public String serviceName() {
+            return "external customer login";
         }
 
         @Override
