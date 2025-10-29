1c70834d0e368a55e15f856bc15d8da06f8c1458
ChangYu Huang <jack.mainwork@gmail.com>
2025-10-29 19:00:11 -0400

    Fix

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
@@ -337,7 +337,8 @@ public class Sender implements Runnable {
 
         long currentTimeMs = time.milliseconds();
         long pollTimeout = sendProducerData(currentTimeMs);
-        client.poll(pollTimeout, currentTimeMs);
+        if (!forceClose)
+            client.poll(pollTimeout, currentTimeMs);
     }
 
     // We handle {@code TransactionalIdAuthorizationException} and {@code ClusterAuthorizationException} by first
