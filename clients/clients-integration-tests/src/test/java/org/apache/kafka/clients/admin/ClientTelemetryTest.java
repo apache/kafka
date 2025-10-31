21f98bcf78776c35494d638ea04d110741af9b23
see-quick <maros.orsak159@gmail.com>
2025-10-31 13:14:03 +0100

    checkstyle
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/admin/ClientTelemetryTest.java
+++ clients/clients-integration-tests/src/test/java/org/apache/kafka/clients/admin/ClientTelemetryTest.java
@@ -53,7 +53,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
