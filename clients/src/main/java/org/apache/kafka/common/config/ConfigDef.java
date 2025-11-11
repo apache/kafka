f0f103e2852f2a5983f4e255bc50241f404aaf19
m1a2st <s7133700@gmail.com>
2025-11-11 21:59:59 +0800

    format code

--- clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
+++ clients/src/main/java/org/apache/kafka/common/config/ConfigDef.java
@@ -18,6 +18,7 @@ package org.apache.kafka.common.config;
 
 import org.apache.kafka.common.config.types.Password;
 import org.apache.kafka.common.utils.Utils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
