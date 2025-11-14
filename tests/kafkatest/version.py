9988aa0acd7ece288f2f28754a69c1bd38d0cd79
Lianet Magrans <lmagrans@confluent.io>
2025-11-14 09:50:07 -0500

    add 4.0.1 and 4.1.1

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -239,11 +239,13 @@ LATEST_3_9 = V_3_9_1
 
 # 4.0.x version
 V_4_0_0 = KafkaVersion("4.0.0")
-LATEST_4_0 = V_4_0_0
+V_4_0_1 = KafkaVersion("4.0.1")
+LATEST_4_0 = V_4_0_1
 
 # 4.1.x version
 V_4_1_0 = KafkaVersion("4.1.0")
-LATEST_4_1 = V_4_1_0
+V_4_1_1 = KafkaVersion("4.1.1")
+LATEST_4_1 = V_4_1_1
 
 # 4.2.x version
 V_4_2_0 = KafkaVersion("4.2.0")
