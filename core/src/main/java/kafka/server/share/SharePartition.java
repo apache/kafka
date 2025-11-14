5395474620d85b5d1164f45d2e13eb6d393ea0a3
Sushant Mahajan <smahajan@confluent.io>
2025-11-14 23:02:07 +0530

    inc comments

--- core/src/main/java/kafka/server/share/SharePartition.java
+++ core/src/main/java/kafka/server/share/SharePartition.java
@@ -2600,7 +2600,7 @@ public class SharePartition {
             case FENCED_LEADER_EPOCH, FENCED_STATE_EPOCH ->
                 new NotLeaderOrFollowerException(errorMessage);
             case SASL_AUTHENTICATION_FAILED, UNSUPPORTED_VERSION ->
-                new UnknownServerException("Unable to complete operation due to internal error.");
+                new UnknownServerException("Unable to complete operation due to server error.");
             default ->
                 new UnknownServerException(errorMessage);
         };
