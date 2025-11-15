ad4314e5ee599ab998dd89a66728df782ce57d09
hermankj <hermankjakobsen@gmail.com>
2025-11-15 07:05:50 +0100

    fix: streamMetricsImpl creation

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/StreamTaskTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/StreamTaskTest.java
@@ -3486,7 +3486,7 @@ public class StreamTaskTest {
                 topology,
                 consumer,
                 new TopologyConfig(null,  config, new Properties()).getTaskConfig(),
-                new StreamsMetricsImpl(metrics, "test", "processId", time),
+                new StreamsMetricsImpl(metrics, "test", "processId", "applicationId", time),
                 stateDirectory,
                 cache,
                 time,
