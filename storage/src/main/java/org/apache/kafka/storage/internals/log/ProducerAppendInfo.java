b3f8ffc2d6277f1d16b13b834b94ea562e3d5444
rreddy-22 <rreddy@confluent.io>
2025-11-16 23:09:58 -0800

    minor

--- storage/src/main/java/org/apache/kafka/storage/internals/log/ProducerAppendInfo.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/ProducerAppendInfo.java
@@ -25,8 +25,8 @@ import org.apache.kafka.common.record.ControlRecordType;
 import org.apache.kafka.common.record.EndTransactionMarker;
 import org.apache.kafka.common.record.Record;
 import org.apache.kafka.common.record.RecordBatch;
-
 import org.apache.kafka.server.common.TransactionVersion;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
