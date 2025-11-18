df52b8d61acb97f8bc0bacfcc2860d8296774073
Andrew Schofield <aschofield@confluent.io>
2025-11-18 17:07:16 +0000

    KAFKA-19757: Interface stability and deprecation

--- clients/src/main/java/org/apache/kafka/clients/admin/DeleteShareGroupOffsetsOptions.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/DeleteShareGroupOffsetsOptions.java
@@ -17,15 +17,10 @@
 
 package org.apache.kafka.clients.admin;
 
-import org.apache.kafka.common.annotation.InterfaceStability;
-
 import java.util.Set;
 
 /**
  * Options for the {@link Admin#deleteShareGroupOffsets(String, Set, DeleteShareGroupOffsetsOptions)} call.
- * <p>
- * The API of this class is evolving, see {@link Admin} for details.
  */
-@InterfaceStability.Evolving
 public class DeleteShareGroupOffsetsOptions extends AbstractOptions<DeleteShareGroupOffsetsOptions> {
 }
--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaShareConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaShareConsumer.java
@@ -27,7 +27,6 @@ import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.TopicIdPartition;
 import org.apache.kafka.common.Uuid;
-import org.apache.kafka.common.annotation.InterfaceStability;
 import org.apache.kafka.common.errors.AuthenticationException;
 import org.apache.kafka.common.errors.AuthorizationException;
 import org.apache.kafka.common.errors.InterruptException;
@@ -299,7 +298,6 @@ import static org.apache.kafka.common.utils.Utils.propsToMap;
  * We have intentionally avoided implementing a particular threading model for processing. Various options for
  * multithreaded processing are possible, of which the most straightforward is to dedicate a thread to each consumer.
  */
-@InterfaceStability.Evolving
 public class KafkaShareConsumer<K, V> implements ShareConsumer<K, V> {
 
     private static final ShareConsumerDelegateCreator CREATOR = new ShareConsumerDelegateCreator();
