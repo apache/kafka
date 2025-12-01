8ebfdaf6cfe2f415c54417605af7237d58d94562
John Mannooparambil <jmannooparambil@confluent.io>
2025-12-01 14:07:02 -0500

    remove additionalBootstrapRecords

--- metadata/src/main/java/org/apache/kafka/metadata/storage/Formatter.java
+++ metadata/src/main/java/org/apache/kafka/metadata/storage/Formatter.java
@@ -46,7 +46,6 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.TreeMap;
@@ -101,11 +100,6 @@ public class Formatter {
      * The bootstrap metadata used to format the cluster.
      */
     private BootstrapMetadata bootstrapMetadata;
-
-    /**
-     * Additional bootstrap records that should be appended after the defaults.
-     */
-    private List<ApiMessageAndVersion> additionalBootstrapRecords = List.of();
 
     /**
      * True if we should enable unstable feature versions.
@@ -225,12 +219,6 @@ public class Formatter {
 
     public Formatter setHasDynamicQuorum(boolean hasDynamicQuorum) {
         this.hasDynamicQuorum = hasDynamicQuorum;
-        return this;
-    }
-
-    public Formatter setAdditionalBootstrapRecords(Collection<ApiMessageAndVersion> additionalBootstrapRecords) {
-        Objects.requireNonNull(additionalBootstrapRecords);
-        this.additionalBootstrapRecords = List.copyOf(additionalBootstrapRecords);
         return this;
     }
 
@@ -393,9 +381,6 @@ public class Formatter {
         BootstrapMetadata bootstrapMetadata = BootstrapMetadata.
             fromVersions(releaseVersion, featureLevels, "format command");
         List<ApiMessageAndVersion> bootstrapRecords = new ArrayList<>(bootstrapMetadata.records());
-        if (!additionalBootstrapRecords.isEmpty()) {
-            bootstrapRecords.addAll(additionalBootstrapRecords);
-        }
         if (!scramArguments.isEmpty()) {
             if (!releaseVersion.isScramSupported()) {
                 throw new FormatterException("SCRAM is only supported in " + MetadataVersion.FEATURE_NAME +
