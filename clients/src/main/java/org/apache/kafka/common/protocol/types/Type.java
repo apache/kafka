e2d2d2fa08e3fd2dc31523e3553bd82411f76b3d
Jonah Hooper <jhooper@confluent.io>
2025-12-04 18:22:18 +0200

    MINOR Clarified maximum length of COMPACT_STRING

--- clients/src/main/java/org/apache/kafka/common/protocol/types/Type.java
+++ clients/src/main/java/org/apache/kafka/common/protocol/types/Type.java
@@ -549,7 +549,8 @@ public abstract class Type {
         @Override
         public String documentation() {
             return "Represents a sequence of characters. First the length N + 1 is given as an UNSIGNED_VARINT " +
-                    ". Then N bytes follow which are the UTF-8 encoding of the character sequence.";
+                    ". Then N bytes follow which are the UTF-8 encoding of the character sequence. " +
+                    "The size of the string may not be more than 32767 bytes.";
         }
     };
 
