c32403aa76bf206c30ed9bc2f723061cfff41e1a
Chih-Yuan Chien <joshua2519@gmail.com>
2025-12-22 13:51:53 +0800

    draft

--- server/src/main/java/org/apache/kafka/server/NodeToControllerRequestThread.java
+++ server/src/main/java/org/apache/kafka/server/NodeToControllerRequestThread.java
@@ -17,31 +17,104 @@
  */
 
 package org.apache.kafka.server;
+import org.apache.kafka.clients.ClientResponse;
 import org.apache.kafka.clients.KafkaClient;
 import org.apache.kafka.clients.ManualMetadataUpdater;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.utils.Time;
-import org.apache.kafka.server.config.AbstractKafkaConfig;
 import org.apache.kafka.server.config.ReplicationConfigs;
 import org.apache.kafka.server.util.InterBrokerSendThread;
 import org.apache.kafka.server.util.RequestAndCompletionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class NodeToControllerRequestThread extends InterBrokerSendThread {
+    private static final Logger log = LoggerFactory.getLogger(NodeToControllerRequestThread.class);
+
+    private final LinkedBlockingDeque<NodeToControllerQueueItem> requestQueue = new LinkedBlockingDeque<>();
+    private final AtomicReference<Node> activeController = new AtomicReference<>(null);
+
+    // Used to testing
+    volatile boolean started = false;
+    private final Time time;
+    private long retryTimeoutMs;
 
     public NodeToControllerRequestThread(KafkaClient initialNetworkClient,
                                          ManualMetadataUpdater metadataUpdater,
                                          ControllerNodeProvider controllerNodeProvider,
-                                         AbstractKafkaConfig config,
+                                         AbstractConfig config,
                                          Time time,
                                          String threadName,
                                          Long retryTimeoutMs) {
         super(threadName, initialNetworkClient, Math.min(Integer.MAX_VALUE, (int) Math.min(config.getLong(ReplicationConfigs.CONTROLLER_SOCKET_TIMEOUT_MS_CONFIG), retryTimeoutMs)), time, false);
+        this.time = time;
+
+    }
+
+    public Optional<Node> activeControllerAddress() {
+        return Optional.ofNullable(activeController.get());
+    }
+
+    private void updateControllerAddress(Node newActiveController) {
+        activeController.set(newActiveController);
+    }
+
+    public void enqueue(NodeToControllerQueueItem request) {
+        if (!started) {
+            throw new IllegalStateException("Cannot enqueue a request if the request thread is not running");
+        }
+        requestQueue.add(request);
+        if (activeControllerAddress().isPresent()) {
+            wakeup();
+        }
+    }
+
+    public int queueSize() {
+        return requestQueue.size();
     }
 
     @Override
     public Collection<RequestAndCompletionHandler> generateRequests() {
-        return List.of();
+        final var currentTimeMs = time.milliseconds();
+        final var requestIter = requestQueue.iterator();
+        while (requestIter.hasNext()) {
+            var request = requestIter.next();
+            if (currentTimeMs - request.createdTimeMs() >= retryTimeoutMs) {
+                requestIter.remove();
+                request.callback().onTimeout();
+            } else {
+                var controllerAddress = activeControllerAddress();
+                if (controllerAddress.isPresent()) {
+                    requestIter.remove();
+                    return Collections.singletonList(new RequestAndCompletionHandler(
+                            time.milliseconds(),
+                            controllerAddress.get(),
+                            request.request(),
+                            response -> handleResponse(request, response)
+                    ));
+                }
+            }
+        }
+
+        return Collections.emptyList();
+    }
+
+    private void handleResponse(NodeToControllerQueueItem queueItem, ClientResponse response) {
+        log.debug("Request {} received {}", queueItem.request(), response);
+        if(response.authenticationException() != null) {
+            log.error("Request {} failed due to authentication error with controller. Disconnecting the " +
+                    "connection to the stale controller {}",
+                    queueItem.request(), activeControllerAddress().map(Node::idString).orElse("null"),
+                    response.authenticationException()
+            );
+        }
     }
 }
