cbd11b76b0e1b5557a55347b21452e2532780083
ChangYu Huang <jack.mainwork@gmail.com>
2025-12-28 01:01:39 -0500

    Fix

--- core/src/test/scala/integration/kafka/api/BaseProducerSendTest.scala
+++ core/src/test/scala/integration/kafka/api/BaseProducerSendTest.scala
@@ -89,11 +89,6 @@ abstract class BaseProducerSendTest extends KafkaServerTestHarness {
       groupProtocolFromTestParameters(),
       securityProtocol = SecurityProtocol.PLAINTEXT,
     )
-      consumer = TestUtils.createConsumer(
-        bootstrapServers(listenerName = ListenerName.forSecurityProtocol(SecurityProtocol.PLAINTEXT)),
-        groupProtocolFromTestParameters(),
-        securityProtocol = SecurityProtocol.PLAINTEXT,
-      )
   }
 
   @AfterEach
