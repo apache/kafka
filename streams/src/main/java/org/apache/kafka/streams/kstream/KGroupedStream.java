71473d1ab2e4a80b95ca16720fbdc2d90cc18573
mshijinksolves <m.shijin@ksolves.com>
2026-01-02 13:02:05 +0530

    Updated the java docs for the specified classes.  if a specific Serde is used earlier in the code, it will reuse that one instead of looking at the global defaults.

--- streams/src/main/java/org/apache/kafka/streams/kstream/KGroupedStream.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/KGroupedStream.java
@@ -383,7 +383,8 @@ public interface KGroupedStream<K, V> {
      * Thus, {@code aggregate(Initializer, Aggregator)} can be used to compute aggregate functions like
      * count (cf. {@link #count()}).
      * <p>
-     * The default value serde from config will be used for serializing the result.
+     * The <b>default value serde</b> from the config will be used for serializing the result.
+     * The <b>key serde</b> is derived from the upstream operator (i.e., the one used for the grouping).
      * If a different serde is required then you should use {@link #aggregate(Initializer, Aggregator, Materialized)}.
      * <p>
      * Not all updates might get sent downstream, as an internal cache is used to deduplicate consecutive updates to
--- streams/src/main/java/org/apache/kafka/streams/kstream/TimeWindowedKStream.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/TimeWindowedKStream.java
@@ -462,7 +462,8 @@ public interface TimeWindowedKStream<K, V> {
      * {@link Initializer}) and the record's value.
      * Thus, {@code aggregate()} can be used to compute aggregate functions like count (cf. {@link #count()}).
      * <p>
-     * The default key and value serde from the config will be used for serializing the result.
+     * The <b>default value serde</b> from the config will be used for serializing the result.
+     * The <b>key serde</b> is derived from the upstream operator (i.e., the one used for the grouping), wrapped in a windowed serde.
      * If a different serde is required then you should use {@link #aggregate(Initializer, Aggregator, Materialized)}.
      * Not all updates might get sent downstream, as an internal cache is used to deduplicate consecutive updates to
      * the same window and key.
