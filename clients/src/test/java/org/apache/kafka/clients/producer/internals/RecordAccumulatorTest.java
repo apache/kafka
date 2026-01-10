466bda9c74548bfe3c4fcb277a0ecc36f43e87bf
Donny Nadolny <donny.nadolny@hotmail.com>
2026-01-10 13:19:21 -0500

    KAFKA-19012 Fix rare producer message corruption, don't reuse buffers on the client in certain error cases (#21065)
    Client versions 2.8.0 and later are affected by a
    
    [change](https://github.com/apache/kafka/commit/30bc21ca35b165f04c472b4ce794893843809ccc)
    that exposes a latent bug in how BufferPool is used (BufferPool is a
    class used on the client side to allocate memory in ByteBuffers, for
    performance it will reuse them with the caller of the class doing manual
    memory management by calling free when they are done with the memory).
    The bug is that a pooled ByteBuffer can be freed while it is still in
    use by the network sending thread - this early freeing can happen when
    batches expire / brokers are disconnecting from clients. This bug has
    existed for more than a decade (since Kafka 0.x it seems), but never
    manifested because prior to 2.8.0 the pooled ByteBuffer (which contained
    record data aka your publishes) was copied into a freshly allocated
    ByteBuffer before any potential reuse and that fresh ByteBuffer was what
    got written over the network to the broker. With a change included in
    2.8.0, the pooled ByteBuffer remains as-is inside of a MemoryRecords
    instance and this pooled ByteBuffer (which in some cases can be reused
    and overwritten with other data) is written over the network. Two
    contributing factors are that the checksum for Kafka records only
    includes the key/value/headers/etc and not the topic so there is no
    protection there, and also an implementation detail is that, also newly
    in the commit that exposed the bug, the produce request header (which
    includes the topic and partition of a group of message batches) is
    serialized in a buffer separately from the messages themselves (and the
    latter is what gets put in the pooled ByteBuffer) which allows you to
    get messages misrouted to a random recently used topic as opposed to
    simple duplicate messages on their intended topic.
    
    The key change is in Sender.sendProducerData, we cannot allow the pooled
    ByteBuffer to be reused for expired in-flight batches until the request
    completes. For these batches we avoid deallocating the buffer in the
    normal failBatch call, deferring it until we call completeBatch (or a
    different path of failBatch).
    
    There are some automated tests to cover this, and also manual testing
    done to reproduce the issue from KAFKA-19012 and verify that this is
    sufficient to stop it.
    
    Reviewers: Justine Olshan <jolshan@confluent.io>, Jun Rao
     <junrao@gmail.com>, Chia-Ping Tsai <chia7712@gmail.com>

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordAccumulatorTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordAccumulatorTest.java
@@ -406,7 +406,7 @@ public class RecordAccumulatorTest {
                 for (ProducerBatch batch : batches) {
                     for (Record record : batch.records().records())
                         read++;
-                    accum.deallocate(batch);
+                    accum.completeAndDeallocateBatch(batch);
                 }
             }
         }
@@ -670,7 +670,7 @@ public class RecordAccumulatorTest {
 
         for (List<ProducerBatch> batches: results.values())
             for (ProducerBatch batch: batches)
-                accum.deallocate(batch);
+                accum.completeAndDeallocateBatch(batch);
 
         // should be complete with no unsent records.
         accum.awaitFlushCompletion();
@@ -1669,7 +1669,7 @@ public class RecordAccumulatorTest {
         assertEquals(1, batches.values().iterator().next().size());
         ProducerBatch batch = batches.values().iterator().next().get(0);
         int numSplitBatches = accum.splitAndReenqueue(batch);
-        accum.deallocate(batch);
+        accum.completeAndDeallocateBatch(batch);
 
         return numSplitBatches;
     }
@@ -1693,7 +1693,7 @@ public class RecordAccumulatorTest {
                     } else {
                         batch.complete(0L, 0L);
                     }
-                    accum.deallocate(batch);
+                    accum.completeAndDeallocateBatch(batch);
                 }
             }
         } while (batchDrained);
