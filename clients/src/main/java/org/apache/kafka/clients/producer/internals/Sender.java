189bec7a8e9fa86df842e4695a58bd234ba9a0fa
david-parkk <ji0513ji@naver.com>
2026-01-12 15:24:39 +0900

    MINOR: Fix typo in Sender class comment

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/Sender.java
@@ -80,7 +80,7 @@ public class Sender implements Runnable {
 
     private final Logger log;
 
-    /* the state of each nodes connection */
+    /* the state of each node's connection */
     private final KafkaClient client;
 
     /* the record accumulator that batches records */
