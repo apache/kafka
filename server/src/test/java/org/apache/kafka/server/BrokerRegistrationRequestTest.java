b0836c80ab05389d54de8026817456bbc6204bde
Chih-Yuan Chien <joshua2519@gmail.com>
2026-01-13 12:43:28 +0800

    fix compile error

--- server/src/test/java/org/apache/kafka/server/BrokerRegistrationRequestTest.java
+++ server/src/test/java/org/apache/kafka/server/BrokerRegistrationRequestTest.java
@@ -122,7 +122,7 @@ class BrokerRegistrationRequestTest {
         }
 
         @Override
-        public void close() {
+        public void close() throws InterruptedException {
             channelManager.shutdown();
             metrics.close();
         }
