55aff363f47d56e67c77c9f02231bb44e34a5671
cui <cuiweixie@gmail.com>
2026-01-13 14:39:20 +0800

    fix: review

--- clients/src/main/java/org/apache/kafka/common/utils/Shell.java
+++ clients/src/main/java/org/apache/kafka/common/utils/Shell.java
@@ -48,7 +48,7 @@ public abstract class Shell {
     private int exitCode;
     private Process process; // sub process used to execute the command
 
-    /* Whether or not the script finished executing */
+    /* Whether the script finished executing */
     private volatile AtomicBoolean completed;
 
     /**
