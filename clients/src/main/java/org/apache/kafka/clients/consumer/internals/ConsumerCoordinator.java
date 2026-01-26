1f47ae466d39776671bfbcd48b2f9d1e4ac82c8e
yangfeng <1790182087@qq.com>
2026-01-26 18:06:09 +0800

    Invoke OffsetCommitCallback immediately on commit failure

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerCoordinator.java
@@ -1113,7 +1113,9 @@ public final class ConsumerCoordinator extends AbstractCoordinator {
             @Override
             public void onFailure(RuntimeException e) {
                 inFlightAsyncCommits.decrementAndGet();
-
+                if (callback != null) {
+                    callback.onComplete(offsets, e);
+                }
                 Exception commitException = e;
 
                 if (e instanceof RetriableException) {
