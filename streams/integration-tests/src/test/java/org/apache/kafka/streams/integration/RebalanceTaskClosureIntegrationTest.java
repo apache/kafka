ef20d15450146441fe142d0f3639ac75d154264e
Nikita Shupletsov <nikita@shupletsov.ca>
2026-02-05 20:38:22 -0800

    Update streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/RebalanceTaskClosureIntegrationTest.java
    Co-authored-by: Matthias J. Sax <mjsax@apache.org>

--- streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/RebalanceTaskClosureIntegrationTest.java
+++ streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/RebalanceTaskClosureIntegrationTest.java
@@ -105,7 +105,7 @@ public class RebalanceTaskClosureIntegrationTest {
      * <li>starts another KS app which will trigger reassignment</li>
      * <li>waits for {@link CachedStateStore#clearCache} to be called(it's called during task recycle) and locks on it</li>
      * <li>sends a message with wrong types to crash the stream thread (this avoids a second rebalance, and enters shutdown directly)</li>
-     * <li>shutdowns the first KS app and waits for the stream tread to get into PENDING_SHUTDOWN state</li>
+     * <li>shutdowns the first KS app</li>
      * <li>releases the lock</li>
      * </ul><p>
      * At this point {@link org.apache.kafka.streams.processor.internals.TaskManager#shutdown} will be called,
