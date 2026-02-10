cbd986a1d5dc53b288a61a649a456e5659a79820
tirthooo7 <tjayswal@confluent.io>
2026-02-10 19:11:22 +0530

    MINOR: Remove unnecessary export of DEBIAN_FRONTEND in base.sh

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -52,7 +52,6 @@ if [[ -z "$JDK_FULL" || ! "$JDK_FULL" =~ ^[0-9]+(u[0-9]+|(\.[0-9]+)+)?-linux-(x6
 fi
 
 echo "JDK_MAJOR=$JDK_MAJOR JDK_ARCH=$JDK_ARCH JDK_FULL=$JDK_FULL"
-export DEBIAN_FRONTEND=noninteractive
 
 if [ -z `which javac` ]; then
     apt-get -y update
