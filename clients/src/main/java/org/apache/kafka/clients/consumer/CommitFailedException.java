af097ec96e4bce4380766e888cdf6d27535091c3
zoo-code <kyj20908@naver.com>
2026-02-10 18:27:08 +0900

    KAFKA-20136: Fix indentation in CommitFailedException

--- clients/src/main/java/org/apache/kafka/clients/consumer/CommitFailedException.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/CommitFailedException.java
@@ -42,10 +42,10 @@ public class CommitFailedException extends KafkaException {
      */
     public CommitFailedException() {
         super("Commit cannot be completed since the group has already " +
-            "rebalanced and assigned the partitions to another member. This means that the time " +
-            "between subsequent calls to poll() was longer than the configured max.poll.interval.ms, " +
-            "which typically implies that the poll loop is spending too much time message processing. " +
-            "You can address this either by increasing max.poll.interval.ms or by reducing the maximum " +
-            "size of batches returned in poll() with max.poll.records.");
+                "rebalanced and assigned the partitions to another member. This means that the time " +
+                "between subsequent calls to poll() was longer than the configured max.poll.interval.ms, " +
+                "which typically implies that the poll loop is spending too much time message processing. " +
+                "You can address this either by increasing max.poll.interval.ms or by reducing the maximum " +
+                "size of batches returned in poll() with max.poll.records.");
     }
 }
