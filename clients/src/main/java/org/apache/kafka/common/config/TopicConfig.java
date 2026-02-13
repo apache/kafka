32ca1385b38c631a5cc84d7547248fb4e59b7ce9
Jian <fujian1115@gmail.com>
2026-02-13 13:13:39 +0800

    Fix documentation for remote copy lag configs

--- clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
+++ clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
@@ -105,16 +105,16 @@ public class TopicConfig {
     public static final String REMOTE_COPY_LAG_MS_DOC = "Controls how long to delay uploading segments to remote storage. " +
             "When set to 0 (default), segments are uploaded as soon as they are eligible (no delay check). " +
             "When set to a positive value (ms), a segment can become eligible for upload only after the time since the latest record in the segment reaches the value. " +
-            "The value should not exceed the real local retention ms when the latter is non-negative. " +
-            "When set to -1, resolves to the real local retention ms (maximum delay, or no delay check when local retention ms is -1). " +
+            "The value should not exceed the real local retention ms when the latter is not save forever. " +
+            "When set to -1, resolves to the real local retention ms (maximum delay, or no delay check when the retention is save forever). " +
             "For how the real local retention time is computed, see <code>local.retention.ms</code>.";
 
     public static final String REMOTE_COPY_LAG_BYTES_CONFIG = "remote.copy.lag.bytes";
     public static final String REMOTE_COPY_LAG_BYTES_DOC = "Controls size-based delay for uploading segments to remote storage. " +
             "When set to 0 (default), segments are uploaded as soon as they are eligible (no delay check). " +
             "When set to a positive value (bytes), a segment can become eligible for upload only when the total bytes of log data after the segment reach the value. " +
-            "The value should not exceed the real local retention bytes when the latter is non-negative. " +
-            "When set to -1, resolves to the real local retention bytes (maximum delay, or no delay check when local retention bytes is -1). " +
+            "The value should not exceed the real local retention bytes when the latter is not save forever. " +
+            "When set to -1, resolves to the real local retention bytes (maximum delay, or no delay check when the retention is save forever). " +
             "For how the real local retention size is computed, see <code>local.retention.bytes</code>.";
 
     public static final String REMOTE_LOG_DELETE_ON_DISABLE_CONFIG = "remote.log.delete.on.disable";
