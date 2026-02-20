f7e90bf7bbda19686be9540ec358e833d8276319
Kirk True <kirk@kirktrue.pro>
2026-02-20 13:24:05 -0800

    KAFKA-20131: ClassicKafkaConsumer does not clear endOffsetRequested flag on failed LIST_OFFSETS calls (#21457)
    Updates the `ClassicKafkaConsumer` to clear out the `SubscriptionState`
    `endOffsetRequested` flag if the `LIST_OFFSETS` call fails.
    
    Reviewers: Viktor Somogyi-Vass <viktorsomogyi@gmail.com>, Lianet Magrans
    <lmagrans@confluent.io>, Andrew Schofield <aschofield@confluent.io>

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/SubscriptionState.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/SubscriptionState.java
@@ -645,6 +645,17 @@ public class SubscriptionState {
         return topicPartitionState.endOffsetRequested();
     }
 
+    public synchronized boolean maybeClearPartitionEndOffsetRequested(TopicPartition tp) {
+        TopicPartitionState topicPartitionState = assignedStateOrNull(tp);
+
+        if (topicPartitionState != null && topicPartitionState.endOffsetRequested()) {
+            topicPartitionState.clearEndOffset();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     synchronized Long partitionLead(TopicPartition tp) {
         TopicPartitionState topicPartitionState = assignedState(tp);
         return topicPartitionState.logStartOffset == null ? null : topicPartitionState.position.offset - topicPartitionState.logStartOffset;
@@ -980,6 +991,10 @@ public class SubscriptionState {
 
         public void requestEndOffset() {
             endOffsetRequested = true;
+        }
+
+        public void clearEndOffset() {
+            endOffsetRequested = false;
         }
 
         private void transitionState(FetchState newState, Runnable runIfTransitioned) {
