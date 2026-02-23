353f40175fc6fb69443d8fdfc725fa799e413cab
kevin-wu24 <kwu@confluent.io>
2026-02-23 15:51:40 +0100

    adding MetaProperties V2

--- metadata/src/main/java/org/apache/kafka/metadata/properties/MetaProperties.java
+++ metadata/src/main/java/org/apache/kafka/metadata/properties/MetaProperties.java
@@ -166,13 +166,11 @@ public final class MetaProperties {
         }
 
         public MetaProperties build() {
-            if (!version.equals(MetaPropertiesVersion.V0)) {
-                if (clusterId.isEmpty()) {
-                    throw new RuntimeException("cluster.id was not found.");
-                }
-                if (nodeId.isEmpty()) {
-                    throw new RuntimeException("node.id was not found.");
-                }
+            if (version.alwaysHasClusterId() && clusterId.isEmpty()) {
+                throw new RuntimeException("cluster.id was not found.");
+            }
+            if (version.alwaysHasNodeId() && nodeId.isEmpty()) {
+                throw new RuntimeException("node.id was not found.");
             }
             return new MetaProperties(version,
                 clusterId,
