196171287811835bb2f71225350fe4f6845800e5
lucliu1108 <lucliu@confluent.io>
2026-02-24 12:20:17 -0600

    remove redundant imports

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/assignor/AssignorBenchmarkUtils.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/assignor/AssignorBenchmarkUtils.java
@@ -37,13 +37,11 @@ import org.apache.kafka.image.MetadataProvenance;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
 public class AssignorBenchmarkUtils {
