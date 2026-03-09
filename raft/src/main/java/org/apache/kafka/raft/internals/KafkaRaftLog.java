6487e7fdc6f492904bae3dd79218a81bee64c03a
majialong <majialoong@gmail.com>
2026-03-10 00:54:41 +0800

    Clean import

--- raft/src/main/java/org/apache/kafka/raft/internals/KafkaRaftLog.java
+++ raft/src/main/java/org/apache/kafka/raft/internals/KafkaRaftLog.java
@@ -72,7 +72,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
