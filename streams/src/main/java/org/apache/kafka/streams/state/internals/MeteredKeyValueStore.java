4411d4e663a404e4a7e0079de00fe26f8f4be8a5
Evan Zhou <ezhou@confluent.io>
2026-03-12 13:31:30 -0500

    remove comment

--- streams/src/main/java/org/apache/kafka/streams/state/internals/MeteredKeyValueStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/MeteredKeyValueStore.java
@@ -177,7 +177,6 @@ public class MeteredKeyValueStore<K, V>
                 }
             }
         );
-        // Only register this metric if it is an in-memory store
         if (!persistent()) {
             StateStoreMetrics.addNumKeysGauge(taskId.toString(), metricsScope, name(), streamsMetrics,
                     (config, now) -> wrapped().approximateNumEntries());
