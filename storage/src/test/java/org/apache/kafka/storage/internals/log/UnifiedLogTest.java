02adab0774a5f81c8007ee92c670b24c8ea30b94
TaiJu Wu <tjwu1217@gmail.com>
2026-03-18 19:49:29 +0800

    fix build

--- storage/src/test/java/org/apache/kafka/storage/internals/log/UnifiedLogTest.java
+++ storage/src/test/java/org/apache/kafka/storage/internals/log/UnifiedLogTest.java
@@ -103,6 +103,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
