eab3922fbe7367faa856697599427ab0669a2666
Matthias J. Sax <mjsax@apache.org>
2026-03-20 14:18:30 -0700

    Apply suggestion from @mjsax

--- streams/src/main/java/org/apache/kafka/streams/state/internals/HeadersSerializer.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/HeadersSerializer.java
@@ -62,7 +62,7 @@ class HeadersSerializer {
     }
 
     // for testing
-    public static byte[] serialize(final Headers headers) {
+   static byte[] serialize(final Headers headers) {
         final PreSerializedHeaders prep = prepareSerialization(headers);
         final ByteBuffer buffer = ByteBuffer.allocate(prep.requiredBufferSizeForHeaders);
         serialize(prep, buffer);
