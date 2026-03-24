3d958978ea8a71542f008d1f8b74f628ab03dfb3
Eduwer Camacaro <eduwerc@gmail.com>
2026-03-24 11:22:32 -0500

    rollback changes in segmented bytes store

--- streams/src/main/java/org/apache/kafka/streams/state/internals/AbstractRocksDBSegmentedBytesStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/AbstractRocksDBSegmentedBytesStore.java
@@ -350,8 +350,6 @@ public class AbstractRocksDBSegmentedBytesStore<S extends Segment> implements Se
         return segments.allSegments(false);
     }
 
-
-
     // Visible for testing
     void restoreAllInternal(final Collection<ConsumerRecord<byte[], byte[]>> records) {
         synchronized (position) {
