443f370522d013adb3ef50bae226aa6e1bbae369
m1a2st <s7133700@gmail.com>
2026-03-30 18:43:52 +0800

    update the import order

--- core/src/main/scala/kafka/docker/KafkaDockerWrapper.scala
+++ core/src/main/scala/kafka/docker/KafkaDockerWrapper.scala
@@ -23,13 +23,13 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature
 import kafka.Kafka
 import kafka.tools.StorageTool
-import org.apache.kafka.server.util.TerseFailure
 import kafka.utils.Logging
 import net.sourceforge.argparse4j.ArgumentParsers
 import net.sourceforge.argparse4j.impl.Arguments.store
 import net.sourceforge.argparse4j.inf.Namespace
 import org.apache.kafka.common.utils.Exit
 import org.apache.kafka.raft.QuorumConfig
+import org.apache.kafka.server.util.TerseFailure
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path, Paths, StandardCopyOption, StandardOpenOption}
