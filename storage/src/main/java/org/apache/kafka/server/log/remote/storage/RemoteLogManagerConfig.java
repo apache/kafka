6aa8c91de581a46d98e65c7c0a0ebac105aef681
Jian <fujian1115@gmail.com>
2026-04-02 06:42:38 +0800

    improve the documents
    Signed-off-by: Jian <fujian1115@gmail.com>

--- storage/src/main/java/org/apache/kafka/server/log/remote/storage/RemoteLogManagerConfig.java
+++ storage/src/main/java/org/apache/kafka/server/log/remote/storage/RemoteLogManagerConfig.java
@@ -173,7 +173,7 @@ public final class RemoteLogManagerConfig {
             "When set to -2 (default), no delay check based on local retention ms. " +
             "When set to 0, segments are uploaded as soon as they are eligible without any delay check. " +
             "When set to a positive value (ms), a segment can't become eligible for upload util the time since the latest record in the segment reaches the value. " +
-            "The value should not exceed the real local retention ms except the latter is save forever. " +
+            "The value should not exceed the real local retention ms except the latter is retained indefinitely (-1). " +
             "When set to -1, resolves to the real local retention ms as maximum delay. " +
             "If the real local retention ms is configured as infinite, -1 is treated as an invalid configuration. " +
             "For how the real local retention time is computed, see <code>log.local.retention.ms</code>.";
@@ -184,7 +184,7 @@ public final class RemoteLogManagerConfig {
             "When set to -2 (default), no delay check based on local retention bytes. " +
             "When set to 0, segments are uploaded as soon as they are eligible without any delay check. " +
             "When set to a positive value (bytes), a segment can't become eligible for upload util the total bytes of log data after the segment reach the value. " +
-            "The value should not exceed the real local retention bytes except the latter is save forever. " +
+            "The value should not exceed the real local retention bytes except the latter is retained indefinitely (-1). " +
             "When set to -1, resolves to the real local retention bytes as maximum delay. " +
             "If the real local retention bytes is configured as infinite, -1 is treated as an invalid configuration. " +
             "For how the real local retention size is computed, see <code>log.local.retention.bytes</code>.";
