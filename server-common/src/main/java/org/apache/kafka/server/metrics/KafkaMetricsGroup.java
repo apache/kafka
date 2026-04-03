b7601ba33e9f327c722310e9cc71cf1bbcfb42fb
TaiJu Wu <tjwu1217@gmail.com>
2026-04-03 21:34:25 +0800

    Revert "check tags order"
    This reverts commit 0a35117aaf35bfe9d7bd0e006557de5af30ac303.

--- server-common/src/main/java/org/apache/kafka/server/metrics/KafkaMetricsGroup.java
+++ server-common/src/main/java/org/apache/kafka/server/metrics/KafkaMetricsGroup.java
@@ -24,7 +24,6 @@ import com.yammer.metrics.core.Meter;
 import com.yammer.metrics.core.MetricName;
 import com.yammer.metrics.core.Timer;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -59,8 +58,6 @@ public class KafkaMetricsGroup {
 
     private static MetricName explicitMetricName(String group, String typeName,
                                                 String name, Map<String, String> tags) {
-        if (tags.size() > 1 && !(tags instanceof LinkedHashMap<String, String>))
-            throw new RuntimeException("Tags must be of type LinkedHashMap. tags: " + tags + " type: " + tags.getClass().getName());
         StringBuilder nameBuilder = new StringBuilder(100);
         nameBuilder.append(group);
         nameBuilder.append(":type=");
