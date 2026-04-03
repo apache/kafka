cac0faeb7093b3a56af21d3de2c9d52f4cfca3ff
TaiJu Wu <tjwu1217@gmail.com>
2026-04-03 11:25:20 +0800

    fix build

--- storage/src/main/java/org/apache/kafka/storage/internals/log/LogCleaner.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/log/LogCleaner.java
@@ -35,7 +35,6 @@ import java.io.File;
 import java.io.IOException;
 import java.security.DigestException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
