49a96a0d59acb679f8ef9b4d6490248238f4c4a3
Nilesh Kumar <nileshkumar3@gmail.com>
2026-04-09 23:05:30 -0500

    Fixing reviewer comment

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumerTest.java
@@ -2091,7 +2091,7 @@ public class AsyncKafkaConsumerTest {
         doReturn(100L).when(applicationEventHandler).maximumTimeToWait();
         doAnswer(invocation -> {
             Timer pollTimer = invocation.getArgument(0, Timer.class);
-            ((MockTime) time).sleep(pollTimer.remainingMs());
+            time.sleep(pollTimer.remainingMs());
             return null;
         }).when(fetchBuffer).awaitWakeup(any(Timer.class));
 
@@ -2215,7 +2215,7 @@ public class AsyncKafkaConsumerTest {
 
         doAnswer(invocation -> {
             Timer pollTimer = invocation.getArgument(0, Timer.class);
-            ((MockTime) time).sleep(pollTimer.remainingMs());
+            time.sleep(pollTimer.remainingMs());
             return null;
         }).when(fetchBuffer).awaitWakeup(any(Timer.class));
 
