59f1ff740f4114ddb4a2d75d8f01c36b8d733229
m1a2st <s7133700@gmail.com>
2026-04-21 01:41:07 +0800

    fix the bug

--- tools/src/main/java/org/apache/kafka/tools/ConfigCommand.java
+++ tools/src/main/java/org/apache/kafka/tools/ConfigCommand.java
@@ -249,12 +249,15 @@ public class ConfigCommand {
             } else if (CLIENT_METRICS_TYPE.equals(entityType)) {
                 configResourceType = ConfigResource.Type.CLIENT_METRICS;
             } else if (BROKER_TYPE.equals(entityType)) {
+                if (!BROKER_DEFAULT_ENTITY_NAME.equals(entityName)) {
+                    validateBrokerId(entityName, entityType);
+                }
                 configResourceType = ConfigResource.Type.BROKER;
             } else {
                 configResourceType = ConfigResource.Type.GROUP;
             }
             try {
-                alterResourceConfig(adminClient, entityType, entityName, configsToBeDeleted, configsToBeAdded, configResourceType);
+                alterResourceConfig(adminClient, entityName, configsToBeDeleted, configsToBeAdded, configResourceType);
             } catch (ExecutionException ee) {
                 if (ee.getCause() instanceof UnsupportedVersionException) {
                     throw new UnsupportedVersionException("The " + ApiKeys.INCREMENTAL_ALTER_CONFIGS + " API is not supported by the cluster. The API is supported starting from version 2.3.0."
@@ -263,7 +266,7 @@ public class ConfigCommand {
                 throw ee;
             }
         } else if (BROKER_LOGGER_CONFIG_TYPE.equals(entityType)) {
-            List<String> validLoggers = getResourceConfig(adminClient, entityType, entityName, true, false).stream().map(ConfigEntry::name).toList();
+            List<String> validLoggers = getResourceConfig(adminClient, entityType, entityName, false).stream().map(ConfigEntry::name).toList();
             // fail the command if any of the configured broker loggers do not exist
             List<String> invalidBrokerLoggers = Stream.concat(
                     configsToBeDeleted.stream().filter(c -> !validLoggers.contains(c)),
@@ -568,7 +571,7 @@ public class ConfigCommand {
                 String entityTypeSingular = entityType.substring(0, entityType.length() - 1);
                 System.out.println(configSourceStr + " configs for " + entityTypeSingular + " " + entity + " are:");
             }
-            getResourceConfig(adminClient, entityType, entity, true, describeAll).forEach(entry -> {
+            getResourceConfig(adminClient, entityType, entity, describeAll).forEach(entry -> {
                 String synonyms = entry.synonyms().stream()
                         .map(synonym -> synonym.source() + ":" + synonym.name() + "=" + synonym.value())
                         .collect(Collectors.joining(", ", "{", "}"));
@@ -577,18 +580,7 @@ public class ConfigCommand {
         }
     }
 
-    private static void alterResourceConfig(Admin adminClient, String entityTypeHead, String entityNameHead, List<String> configsToBeDeleted, Map<String, ConfigEntry> configsToBeAdded, ConfigResource.Type resourceType) throws ExecutionException, InterruptedException, TimeoutException {
-        Map<String, ConfigEntry> oldConfig = getResourceConfig(adminClient, entityTypeHead, entityNameHead, false, false)
-                .stream()
-                .collect(Collectors.toMap(ConfigEntry::name, entry -> entry));
-
-        // fail the command if any of the configs to be deleted does not exist
-        List<String> invalidConfigs = configsToBeDeleted.stream()
-                .filter(config -> !oldConfig.containsKey(config))
-                .toList();
-        if (!invalidConfigs.isEmpty())
-            throw new InvalidConfigurationException("Invalid config(s): " + String.join(",", invalidConfigs));
-
+    private static void alterResourceConfig(Admin adminClient, String entityNameHead, List<String> configsToBeDeleted, Map<String, ConfigEntry> configsToBeAdded, ConfigResource.Type resourceType) throws ExecutionException, InterruptedException, TimeoutException {
         ConfigResource configResource = new ConfigResource(resourceType, entityNameHead);
         AlterConfigsOptions alterOptions = new AlterConfigsOptions().timeoutMs(30000).validateOnly(false);
         List<AlterConfigOp> addEntries = configsToBeAdded.values().stream().map(k -> new AlterConfigOp(k, AlterConfigOp.OpType.SET)).toList();
@@ -605,7 +597,7 @@ public class ConfigCommand {
         }
     }
 
-    private static List<ConfigEntry> getResourceConfig(Admin adminClient, String entityType, String entityName, boolean includeSynonyms, boolean describeAll) throws ExecutionException, InterruptedException, TimeoutException {
+    private static List<ConfigEntry> getResourceConfig(Admin adminClient, String entityType, String entityName, boolean describeAll) throws ExecutionException, InterruptedException, TimeoutException {
         ConfigResource.Type configResourceType;
         Optional<ConfigEntry.ConfigSource> dynamicConfigSource;
 
@@ -643,7 +635,7 @@ public class ConfigCommand {
         Optional<ConfigEntry.ConfigSource> configSourceFilter = describeAll ? Optional.empty() : dynamicConfigSource;
 
         ConfigResource configResource = new ConfigResource(configResourceType, entityName);
-        DescribeConfigsOptions describeOptions = new DescribeConfigsOptions().includeSynonyms(includeSynonyms);
+        DescribeConfigsOptions describeOptions = new DescribeConfigsOptions().includeSynonyms(true);
         Map<ConfigResource, Config> configs = adminClient.describeConfigs(Collections.singleton(configResource), describeOptions)
                     .all().get(30, TimeUnit.SECONDS);
 
