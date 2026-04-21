bc00c2c7a6a2be7b982a7e02cbcf49f7044fb92f
Kevin Wu <kwu@confluent.io>
2026-04-21 13:50:16 -0500

    cleanup

--- clients/src/main/java/org/apache/kafka/clients/admin/Admin.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/Admin.java
@@ -1689,7 +1689,6 @@ public interface Admin extends AutoCloseable {
      *
      * @return the {@link UnregisterControllerResult} containing the result
      */
-    @InterfaceStability.Unstable
     default UnregisterControllerResult unregisterController(int controllerId) {
         return unregisterController(controllerId, new UnregisterControllerOptions());
     }
@@ -1712,7 +1711,6 @@ public interface Admin extends AutoCloseable {
      *
      * @return the {@link UnregisterControllerResult} containing the result
      */
-    @InterfaceStability.Unstable
     UnregisterControllerResult unregisterController(int controllerId, UnregisterControllerOptions options);
 
     /**
