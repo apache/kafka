5d07053a443486096f0a3447f7033d461f82f33e
gabriellefu <100360154+gabriellefu@users.noreply.github.com>
2026-05-04 13:14:31 -0400

    KAFKA-13499: Avoid restoring outdated records (#22115)
    1. Expose the retentionPeriod length to storeMetadata
    2. In prepareChangelogs(), switch it from always seektobeginning if
    checkpoint doesn't exist to seek to certain timestamp to avoid restoring
    outdated records.
    3. Change from [the ](https://github.com/apache/kafka/pull/21901):
    Instead of the wall clock, use the latest timestamp in the changelog as
    the latest time, and seek from the timestamp of
    latest_changelog_stamp_time-rention_period.
    
    Reviewers: TengYao Chi <frankvicky@apache.org>, Bill Bejeck
     <bbejeck@apache.org>

--- streams/src/main/java/org/apache/kafka/streams/state/internals/InMemorySessionStore.java
+++ streams/src/main/java/org/apache/kafka/streams/state/internals/InMemorySessionStore.java
@@ -53,7 +53,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 import static org.apache.kafka.streams.StreamsConfig.InternalConfig.IQ_CONSISTENCY_OFFSET_VECTOR_ENABLED;
 
-public class InMemorySessionStore implements SessionStore<Bytes, byte[]> {
+public class InMemorySessionStore implements SessionStore<Bytes, byte[]>, WithRetentionPeriod {
 
     private static final Logger LOG = LoggerFactory.getLogger(InMemorySessionStore.class);
 
@@ -86,6 +86,11 @@ public class InMemorySessionStore implements SessionStore<Bytes, byte[]> {
         this.retentionPeriod = retentionPeriod;
         this.metricScope = metricScope;
         this.position = Position.emptyPosition();
+    }
+
+    @Override
+    public long retentionPeriod() {
+        return retentionPeriod;
     }
 
     @Override
