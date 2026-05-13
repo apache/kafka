58e49dff99b22b8bf18c49f0632682bbce421178
Jiayao Sun <jiayao.s@outlook.com>
2026-05-13 21:57:11 +1200

    move to server

--- server/src/test/java/org/apache/kafka/server/RaftClusterSnapshotTest.java
+++ server/src/test/java/org/apache/kafka/server/RaftClusterSnapshotTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package kafka.server;
+package org.apache.kafka.server;
 
 import org.apache.kafka.common.test.KafkaClusterTestKit;
 import org.apache.kafka.common.test.TestKitNodes;
