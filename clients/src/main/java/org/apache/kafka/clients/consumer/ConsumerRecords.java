8d2c59bc8f4050dbf68a246cbfdda19c6ceabc08
Evan Zhou <ezhou@confluent.io>
2026-06-04 22:02:13 -0500

    Revert "mark deprecated ConsumerRecords constructor with forRemoval = true"
    This reverts commit fd10d3fa5209734704e89a94a7c8ea83c59107ea.

--- clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/ConsumerRecords.java
@@ -52,7 +52,7 @@ public class ConsumerRecords<K, V> implements Iterable<ConsumerRecord<K, V>> {
     /**
      * @deprecated Since 4.0. Use {@link #ConsumerRecords(Map, Map)} instead.
      */
-    @Deprecated(since = "4.1", forRemoval = true)
+    @Deprecated
     public ConsumerRecords(Map<TopicPartition, List<ConsumerRecord<K, V>>> records) {
         this(records, Map.of(), true);
     }
