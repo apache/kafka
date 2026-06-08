528252636a94d270fda95a095a08f9caf224e48a
Andrew Schofield <aschofield@confluent.io>
2026-06-08 18:20:24 +0100

    Support coordinator connections and mark API as stable.

--- clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
+++ clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
@@ -1140,8 +1140,11 @@ public class NetworkClient implements KafkaClient {
                     int nodeId = Integer.parseInt(node);
                     // In order to allow separate connections to coordinators, the client uses large positive node ID values
                     // (Integer.MAX_VALUE - nodeId) for these connections which do not match the target broker's actual node ID.
-                    // To avoid those, only check if the node ID is less than half of Integer.MAX_VALUE.
-                    if (clusterId != null && nodeId >= 0 && nodeId < Integer.MAX_VALUE / 2) {
+                    // We can get the real node ID by subtracting from Integer.MAX_VALUE.
+                    if (nodeId > Integer.MAX_VALUE / 2) {
+                        nodeId = Integer.MAX_VALUE - nodeId;
+                    }
+                    if (clusterId != null && nodeId >= 0) {
                         apiVersionRequestBuilder.setClusterId(clusterId);
                         apiVersionRequestBuilder.setNodeId(nodeId);
                     }
