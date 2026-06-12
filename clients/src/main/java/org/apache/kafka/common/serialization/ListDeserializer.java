a983cd6a3c1c3f8a5893ddaf8999f537df75e5e8
Matthias J. Sax <matthias@confluent.io>
2026-06-12 12:09:15 -0700

    fix typo

--- clients/src/main/java/org/apache/kafka/common/serialization/ListDeserializer.java
+++ clients/src/main/java/org/apache/kafka/common/serialization/ListDeserializer.java
@@ -179,7 +179,7 @@ public class ListDeserializer<Inner> implements Deserializer<List<Inner>> {
             List<Integer> nullIndexList = null;
             if (serStrategy == SerializationStrategy.CONSTANT_SIZE) {
                 if (primitiveSize == null) {
-                    throw new SerializationException("Data is encode as constant size entries, but configured inner deserializer is not a known fixed-size deserializer.");
+                    throw new SerializationException("Data is encoded as constant size entries, but configured inner deserializer is not a known fixed-size deserializer.");
                 }
                 // In CONSTANT_SIZE strategy, indexes of null entries are decoded from a null index list
                 nullIndexList = deserializeNullIndexList(dis, data.length);
