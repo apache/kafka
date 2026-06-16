4280401976ce4ff7f287975a443fa47c8c381904
majialong <majialoong@gmail.com>
2026-06-16 23:27:35 +0800

    MINOR: Migrate AuthorizerIntegrationTest to ClusterInstance

--- tools/src/test/java/org/apache/kafka/tools/consumer/group/AuthorizerIntegrationTest.java
+++ tools/src/test/java/org/apache/kafka/tools/consumer/group/AuthorizerIntegrationTest.java
@@ -16,30 +16,82 @@
  */
 package org.apache.kafka.tools.consumer.group;
 
-import kafka.api.AbstractAuthorizerIntegrationTest;
-
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.acl.AccessControlEntry;
+import org.apache.kafka.common.acl.AccessControlEntryFilter;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclBindingFilter;
+import org.apache.kafka.common.acl.AclOperation;
 import org.apache.kafka.common.errors.GroupIdNotFoundException;
+import org.apache.kafka.common.internals.Topic;
+import org.apache.kafka.common.resource.PatternType;
+import org.apache.kafka.common.resource.ResourcePattern;
+import org.apache.kafka.common.resource.ResourceType;
+import org.apache.kafka.common.security.auth.AuthenticationContext;
+import org.apache.kafka.common.security.auth.KafkaPrincipal;
+import org.apache.kafka.common.security.authenticator.DefaultKafkaPrincipalBuilder;
+import org.apache.kafka.common.test.ClusterInstance;
+import org.apache.kafka.common.test.api.ClusterConfigProperty;
+import org.apache.kafka.common.test.api.ClusterTest;
+import org.apache.kafka.common.test.api.ClusterTestDefaults;
+import org.apache.kafka.common.test.api.Type;
+import org.apache.kafka.metadata.authorizer.StandardAuthorizer;
 
-import org.junit.jupiter.api.Test;
-
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
-import scala.jdk.javaapi.CollectionConverters;
-
+import static org.apache.kafka.common.acl.AclOperation.CREATE;
 import static org.apache.kafka.common.acl.AclOperation.DESCRIBE;
 import static org.apache.kafka.common.acl.AclPermissionType.ALLOW;
+import static org.apache.kafka.common.config.internals.BrokerSecurityConfigs.PRINCIPAL_BUILDER_CLASS_CONFIG;
+import static org.apache.kafka.coordinator.group.GroupCoordinatorConfig.OFFSETS_TOPIC_PARTITIONS_CONFIG;
+import static org.apache.kafka.coordinator.group.GroupCoordinatorConfig.OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG;
+import static org.apache.kafka.security.authorizer.AclEntry.WILDCARD_HOST;
+import static org.apache.kafka.server.config.ServerConfigs.AUTHORIZER_CLASS_NAME_CONFIG;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class AuthorizerIntegrationTest extends AbstractAuthorizerIntegrationTest {
-    @Test
-    public void testDescribeGroupCliWithGroupDescribe() throws Exception {
-        addAndVerifyAcls(CollectionConverters.asScala(Set.of(new AccessControlEntry(ClientPrincipal().toString(), "*", DESCRIBE, ALLOW))).toSet(), groupResource());
+@ClusterTestDefaults(
+    types = {Type.CO_KRAFT},
+    serverProperties = {
+        @ClusterConfigProperty(key = StandardAuthorizer.SUPER_USERS_CONFIG, value = "User:broker"),
+        @ClusterConfigProperty(key = OFFSETS_TOPIC_PARTITIONS_CONFIG, value = "1"),
+        @ClusterConfigProperty(key = OFFSETS_TOPIC_REPLICATION_FACTOR_CONFIG, value = "1"),
+        @ClusterConfigProperty(key = AUTHORIZER_CLASS_NAME_CONFIG, value = AuthorizerIntegrationTest.STANDARD_AUTHORIZER),
+        @ClusterConfigProperty(key = PRINCIPAL_BUILDER_CLASS_CONFIG, value = AuthorizerIntegrationTest.PRINCIPAL_BUILDER)
+    }
+)
+public class AuthorizerIntegrationTest {
+    public static final String STANDARD_AUTHORIZER = "org.apache.kafka.metadata.authorizer.StandardAuthorizer";
+    public static final String PRINCIPAL_BUILDER = "org.apache.kafka.tools.consumer.group.AuthorizerIntegrationTest$PrincipalBuilder";
 
-        String[] cgcArgs = new String[]{"--bootstrap-server", bootstrapServers(listenerName()), "--describe", "--group", group()};
+    private static final KafkaPrincipal BROKER_PRINCIPAL = new KafkaPrincipal(KafkaPrincipal.USER_TYPE, "broker");
+    private static final KafkaPrincipal CLIENT_PRINCIPAL = new KafkaPrincipal(KafkaPrincipal.USER_TYPE, "client");
+
+    private static final String CLIENT_LISTENER_NAME = "EXTERNAL";
+    private static final String CONTROLLER_LISTENER_NAME = "CONTROLLER";
+
+    private static final String GROUP = "my-group";
+    private static final ResourcePattern GROUP_RESOURCE = new ResourcePattern(ResourceType.GROUP, GROUP, PatternType.LITERAL);
+    private static final ResourcePattern OFFSETS_TOPIC_RESOURCE = new ResourcePattern(
+        ResourceType.TOPIC,
+        Topic.GROUP_METADATA_TOPIC_NAME,
+        PatternType.LITERAL
+    );
+
+    @ClusterTest
+    public void testDescribeGroupCliWithGroupDescribe(ClusterInstance clusterInstance) throws Exception {
+        setup(clusterInstance);
+        addAndVerifyAcls(Set.of(createAcl(DESCRIBE, CLIENT_PRINCIPAL)), GROUP_RESOURCE, clusterInstance);
+
+        String[] cgcArgs = new String[]{
+            "--bootstrap-server", clusterInstance.bootstrapServers(),
+            "--describe",
+            "--group", GROUP
+        };
         ConsumerGroupCommandOptions opts = ConsumerGroupCommandOptions.fromArgs(cgcArgs);
         try (ConsumerGroupCommand.ConsumerGroupService consumerGroupService = new ConsumerGroupCommand.ConsumerGroupService(opts, Map.of())) {
             consumerGroupService.describeGroups();
@@ -47,6 +99,47 @@ public class AuthorizerIntegrationTest extends AbstractAuthorizerIntegrationTest
         } catch (ExecutionException e) {
             assertInstanceOf(GroupIdNotFoundException.class, e.getCause(),
                 "Non-existent group should throw GroupIdNotFoundException");
+        }
+    }
+
+    private void setup(ClusterInstance clusterInstance) throws Exception {
+        addAndVerifyAcls(Set.of(createAcl(CREATE, CLIENT_PRINCIPAL)), OFFSETS_TOPIC_RESOURCE, clusterInstance);
+
+        NewTopic offsetTopic = new NewTopic(Topic.GROUP_METADATA_TOPIC_NAME, 1, (short) 1);
+        try (Admin admin = clusterInstance.admin()) {
+            admin.createTopics(List.of(offsetTopic)).all().get();
+            clusterInstance.waitTopicCreation(Topic.GROUP_METADATA_TOPIC_NAME, 1);
+        }
+    }
+
+    private static AccessControlEntry createAcl(AclOperation operation, KafkaPrincipal principal) {
+        return new AccessControlEntry(principal.toString(), WILDCARD_HOST, operation, ALLOW);
+    }
+
+    private void addAndVerifyAcls(Set<AccessControlEntry> acls, ResourcePattern resource, ClusterInstance clusterInstance) throws Exception {
+        List<AclBinding> aclBindings = acls.stream()
+            .map(acl -> new AclBinding(resource, acl))
+            .toList();
+
+        try (Admin admin = clusterInstance.admin(Map.of(), true)) {
+            admin.createAcls(aclBindings).all().get();
+        }
+
+        clusterInstance.waitAcls(new AclBindingFilter(resource.toFilter(), AccessControlEntryFilter.ANY), acls);
+    }
+
+    public static class PrincipalBuilder extends DefaultKafkaPrincipalBuilder {
+        public PrincipalBuilder() {
+            super(null, null);
+        }
+
+        @Override
+        public KafkaPrincipal build(AuthenticationContext context) {
+            return switch (context.listenerName()) {
+                case CLIENT_LISTENER_NAME -> CLIENT_PRINCIPAL;
+                case CONTROLLER_LISTENER_NAME -> BROKER_PRINCIPAL;
+                default -> throw new IllegalArgumentException("No principal mapped to listener " + context.listenerName());
+            };
         }
     }
 }
