8d195857260952cebb143426f4ac69d420a55d54
Matthias J. Sax <matthias@confluent.io>
2026-06-19 12:26:51 -0700

    checkstyle

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StateDirectory.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StateDirectory.java
@@ -67,7 +67,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
