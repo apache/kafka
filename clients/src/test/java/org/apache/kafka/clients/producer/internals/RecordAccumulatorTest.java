3e8540311fb3306f2ece54cfbd5738d30d785904
ChangYu Huang <jack.mainwork@gmail.com>
2026-06-19 10:03:01 -0700

    Test the producer's behavior when batch size is 0.

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordAccumulatorTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/RecordAccumulatorTest.java
@@ -22,6 +22,7 @@ import org.apache.kafka.clients.MetadataSnapshot;
 import org.apache.kafka.clients.NodeApiVersions;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.internals.RecordAccumulator.RecordAppendResult;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.Node;
@@ -1554,6 +1555,16 @@ public class RecordAccumulatorTest {
         Map<Integer, List<ProducerBatch>> batches = accum.drain(metadataCache,
             Set.of(node2), 999999 /* maxSize */, time.milliseconds());
         assertTrue(batches.get(node2.id()).isEmpty());
+    }
+
+    @Test
+    public void testBatchSizeZero() throws InterruptedException {
+        RecordAccumulator accum = createTestRecordAccumulator(1, 10 * 1024, Compression.NONE, Integer.MAX_VALUE);
+        for (int i = 0; i < 10; i++) {
+            RecordAppendResult result = accum.append(tp1.topic(), tp1.partition(), 0L, null, value, Record.EMPTY_HEADERS, null, 0, time.milliseconds(), cluster);
+            System.out.printf("%d - new batch: %s, batch full: %s\n", i, result.newBatchCreated, result.batchIsFull);
+        }
+        assertTrue(false);
     }
 
     private int prepareSplitBatches(RecordAccumulator accum, long seed, int recordSize, int numRecords)
