e6f9fe63a59f3da8a8ffa83397f7e24da92b4fca
Ken Huang <s7133700@gmail.com>
2026-06-23 14:25:30 +0800

    addressed by comments

--- server/src/test/java/org/apache/kafka/server/purgatory/DelayedProduceTest.java
+++ server/src/test/java/org/apache/kafka/server/purgatory/DelayedProduceTest.java
@@ -51,12 +51,12 @@ public class DelayedProduceTest {
                         && name.getMBeanName().contains("partition=0")
                         && name.getMBeanName().contains("name=ExpiresPerSec");
         assertTrue(
-                KafkaYammerMetrics.defaultRegistry()
-                        .allMetrics()
-                        .keySet()
-                        .stream()
-                        .anyMatch(isPartitionMetric),
-                "Partition metric should exist after recordExpiration"
+            KafkaYammerMetrics.defaultRegistry()
+                    .allMetrics()
+                    .keySet()
+                    .stream()
+                    .anyMatch(isPartitionMetric),
+            "Partition metric should exist after recordExpiration"
         );
 
         Predicate<MetricName> isAggregateMetric = name ->
@@ -74,12 +74,12 @@ public class DelayedProduceTest {
 
         // Verify the partition metric is removed from the registry
         assertFalse(
-                KafkaYammerMetrics.defaultRegistry()
-                        .allMetrics()
-                        .keySet()
-                        .stream()
-                        .anyMatch(isPartitionMetric),
-                "Partition metric should be removed after removePartitionMetrics"
+            KafkaYammerMetrics.defaultRegistry()
+                    .allMetrics()
+                    .keySet()
+                    .stream()
+                    .anyMatch(isPartitionMetric),
+            "Partition metric should be removed after removePartitionMetrics"
         );
 
         // Verify the aggregate metric is unaffected
@@ -90,9 +90,9 @@ public class DelayedProduceTest {
                 .filter(isAggregateMetric)
                 .count();
         assertEquals(
-                aggregateCountBefore,
-                aggregateCountAfter,
-                "Aggregate metric should be unaffected by removePartitionMetrics"
+            aggregateCountBefore,
+            aggregateCountAfter,
+            "Aggregate metric should be unaffected by removePartitionMetrics"
         );
     }
 
