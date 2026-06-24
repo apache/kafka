ae06a44c1ec8cecd305137a19d65be6833201462
cychiu <cychiu8@gmail.com>
2026-06-24 12:17:04 +0900

    remove empty line

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/ShareHeartbeatRequestManagerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/ShareHeartbeatRequestManagerTest.java
@@ -457,7 +457,6 @@ public class ShareHeartbeatRequestManagerTest
         assertEquals(0, pollResult.unsentRequests.size());
     }
 
-
     @Override
     protected ClientResponse createHeartbeatResponse(
             final NetworkClientDelegate.UnsentRequest request,
