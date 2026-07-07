38a0090cf481ac9cc487e8917d601f877c91ac9d
Jess Jin <jejin@confluent.io>
2026-07-07 09:17:23 -0400

    add @InterfaceAudience.Public

--- streams/src/main/java/org/apache/kafka/streams/query/TimestampedRangeWithHeadersQuery.java
+++ streams/src/main/java/org/apache/kafka/streams/query/TimestampedRangeWithHeadersQuery.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kafka.streams.query;
 
+import org.apache.kafka.common.annotation.InterfaceAudience;
 import org.apache.kafka.common.annotation.InterfaceStability.Evolving;
 import org.apache.kafka.streams.processor.api.ReadOnlyRecord;
 import org.apache.kafka.streams.state.ReadOnlyRecordIterator;
@@ -58,6 +59,7 @@ import java.util.Optional;
  * @param <V> Type of values
  */
 @Evolving
+@InterfaceAudience.Public
 public final class TimestampedRangeWithHeadersQuery<K, V> implements Query<ReadOnlyRecordIterator<K, V>> {
 
     private final Optional<K> lower;
