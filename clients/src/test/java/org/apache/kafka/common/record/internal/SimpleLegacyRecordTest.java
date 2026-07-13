6cd450e6773d0489cb4256b500b5f66525467f23
Eric Chang <e850506@gmail.com>
2026-07-13 22:14:37 +0800

    MINOR: Move ByteBufferInputStream and ByteBufferOutputStream to internal package (#22814)
    Moves ByteBufferInputStream and ByteBufferOutputStream to the internal
    utils package and updates Kafka callers.
    
    Reviewers: Ken Huang <s7133700@gmail.com>, Chia-Ping Tsai
    <chia7712@gmail.com>

--- clients/src/test/java/org/apache/kafka/common/record/internal/SimpleLegacyRecordTest.java
+++ clients/src/test/java/org/apache/kafka/common/record/internal/SimpleLegacyRecordTest.java
@@ -20,8 +20,8 @@ import org.apache.kafka.common.InvalidRecordException;
 import org.apache.kafka.common.compress.Compression;
 import org.apache.kafka.common.errors.CorruptRecordException;
 import org.apache.kafka.common.record.TimestampType;
-import org.apache.kafka.common.utils.ByteBufferOutputStream;
 import org.apache.kafka.common.utils.Utils;
+import org.apache.kafka.common.utils.internals.ByteBufferOutputStream;
 
 import org.junit.jupiter.api.Test;
 
