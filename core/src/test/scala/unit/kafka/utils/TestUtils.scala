336f6221dce42cf3c5bde726ecb0b1635172b991
Christo <lolovc@amazon.com>
2026-07-15 15:48:49 +0100

    Address clashes/errors

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -16,10 +16,7 @@
  */
 package kafka.utils
 
-import kafka.security.JaasTestUtils
-import com.yammer.metrics.core.{Histogram, Meter}
-import kafka.log.LogManager
-import kafka.network.RequestChannel
+import com.yammer.metrics.core.Meter
 import kafka.server._
 import kafka.utils.Implicits._
 import org.apache.kafka.clients.admin.AlterConfigOp.OpType
@@ -48,8 +45,7 @@ import org.apache.kafka.metadata.{ConfigRepository, LeaderAndIsr, MockConfigRepo
 import org.apache.kafka.network.SocketServerConfigs
 import org.apache.kafka.raft.{KRaftConfigs, QuorumConfig}
 import org.apache.kafka.security.JaasTestUtils
-import org.apache.kafka.server.authorizer.{AuthorizableRequestContext, Authorizer => JAuthorizer}
-import org.apache.kafka.server.common.{ControllerRequestCompletionHandler, TopicIdPartition}
+import org.apache.kafka.server.authorizer.{Authorizer => JAuthorizer}
 import org.apache.kafka.server.config.{DelegationTokenManagerConfigs, ReplicationConfigs, ServerConfigs, ServerLogConfigs}
 import org.apache.kafka.server.metrics.KafkaYammerMetrics
 import org.apache.kafka.server.util.MockTime
