9ae317d728b69fb363be9326ef07e2656765e211
Mickael Maison <mimaison@users.noreply.github.com>
2026-07-29 11:04:25 +0200

    KAFKA-17840: Move QuotaFactory to server module (#22721)
    Moving the last quota logic out of core.
    
    Reviewers: Ken Huang <s7133700@gmail.com>, Luke Chen <showuon@gmail.com>

--- core/src/main/scala/kafka/server/RequestHandlerHelper.scala
+++ core/src/main/scala/kafka/server/RequestHandlerHelper.scala
@@ -18,7 +18,7 @@
 package kafka.server
 
 import kafka.network.RequestChannel
-import kafka.server.QuotaFactory.QuotaManagers
+import org.apache.kafka.server.quota.QuotaFactory.QuotaManagers
 import org.apache.kafka.common.errors.ClusterAuthorizationException
 import org.apache.kafka.common.requests.AbstractResponse
 import org.apache.kafka.common.utils.Time
