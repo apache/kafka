dbc641ff863237cb436f3bc9a58daff798b2758f
Parker <parkerhiphop027@gmail.com>
2026-08-12 00:04:07 +0900

    Update metadata version

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -137,7 +137,7 @@ DEV_VERSION = KafkaVersion("4.5.0-SNAPSHOT")
 
 LATEST_STABLE_TRANSACTION_VERSION = 2
 # This should match the LATEST_PRODUCTION version defined in MetadataVersion.java
-LATEST_STABLE_METADATA_VERSION = "4.3-IV0"
+LATEST_STABLE_METADATA_VERSION = "4.4-IV2"
 
 # 2.1.x versions
 V_2_1_0 = KafkaVersion("2.1.0")
