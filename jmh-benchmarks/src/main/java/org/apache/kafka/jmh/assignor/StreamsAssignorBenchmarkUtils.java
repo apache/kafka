89a6c9eda143fd896793363e26d71002283f6a7b
gabriellafu <100360154+gabriellefu@users.noreply.github.com>
2026-08-12 15:15:53 -0400

    KAFKA-20790: add AssignmentConfigs interface (#23092)
    Adding AssignmentConfigs a public API for KIP-1357.
    
    Reviewers: Sean Quah <squah@confluent.io>, Matthias J. Sax
     <matthias@confluent.io>

--- jmh-benchmarks/src/main/java/org/apache/kafka/jmh/assignor/StreamsAssignorBenchmarkUtils.java
+++ jmh-benchmarks/src/main/java/org/apache/kafka/jmh/assignor/StreamsAssignorBenchmarkUtils.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kafka.jmh.assignor;
 
+import org.apache.kafka.coordinator.group.api.streams.assignor.AssignmentConfigs;
 import org.apache.kafka.coordinator.group.api.streams.assignor.GroupSpec;
 import org.apache.kafka.coordinator.group.streams.StreamsGroupMember;
 import org.apache.kafka.coordinator.group.streams.assignor.GroupSpecImpl;
@@ -45,7 +46,7 @@ public class StreamsAssignorBenchmarkUtils {
      */
     public static GroupSpec createGroupSpec(
         Map<String, StreamsGroupMember> members,
-        Map<String, String> assignmentConfigs,
+        AssignmentConfigs assignmentConfigs,
         Map<String, Map<String, Map<Integer, Long>>> taskOffsets
     ) {
         Map<String, MemberMetadataAndStateImpl> memberSpecs = new HashMap<>();
