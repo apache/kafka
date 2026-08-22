17e917bf3670abe6c65225affdaeb21837505ffb
Gavin Wang <z2552wan@uwaterloo.ca>
2026-08-21 20:10:32 -0400

    MINOR: Remove use_streams_groups from KafkaService
    The parameter dates back to the 4.1 early-access release of the streams rebalance
    protocol, when running a streams group required unstable.api.versions.enable and
    unstable.feature.versions.enable on the broker. None of that is needed anymore:
    group.coordinator.rebalance.protocols defaults to classic,consumer,streams since
    4.2, streams.version=1 (SV_1) is the bootstrap default from metadata version
    4.2-IV1 on, and neither StreamsGroupHeartbeat v1 nor StreamsGroupDescribe v1 is
    marked latestVersionUnstable. Setting the unstable flags was the parameter's only
    remaining effect, so it had become a no-op toggle that suggested streams groups
    need special enablement when they do not.
    
    Also remove the matching legacy 'kafka-features.sh upgrade streams.version 1' call
    from BaseStreamsTest.setUp, which upgraded the feature to a level it already has.
    No test using either construct runs a broker older than 4.2.

--- tests/kafkatest/tests/streams/base_streams_test.py
+++ tests/kafkatest/tests/streams/base_streams_test.py
@@ -34,13 +34,11 @@ class BaseStreamsTest(Test):
         self.num_controllers = num_controllers
         self.num_brokers = num_brokers
         self.topics = topics
-        self.use_streams_groups = True
 
         self.kafka = KafkaService(
             test_context, self.num_brokers,
             None, topics=self.topics,
             controller_num_nodes_override=self.num_controllers,
-            use_streams_groups=True,
             server_prop_overrides=[
                 [ "group.streams.min.session.timeout.ms", "10000" ], # Need to up the lower bound
                 [ "group.streams.session.timeout.ms", "10000" ], # As in classic groups, set this to 10s
@@ -50,8 +48,6 @@ class BaseStreamsTest(Test):
 
     def setUp(self):
         self.kafka.start()
-        if self.use_streams_groups:
-            self.kafka.run_features_command("upgrade", "streams.version", 1)
 
     def get_consumer(self, client_id, topic, num_messages):
         return VerifiableConsumer(self.test_context,
