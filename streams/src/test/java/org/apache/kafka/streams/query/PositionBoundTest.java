885a5fed4a60e93eeb5e454f2a60ff02747463bb
Mickael Maison <mickael.maison@gmail.com>
2026-08-27 09:45:27 +0200

    Remove unused import

--- streams/src/test/java/org/apache/kafka/streams/query/PositionBoundTest.java
+++ streams/src/test/java/org/apache/kafka/streams/query/PositionBoundTest.java
@@ -25,7 +25,6 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
