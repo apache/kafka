8fef03fa6dda1abded5346f95e22022b3335ad9c
Gergely Harmadas <harmadasg@cloudera.com>
2026-09-01 16:24:55 +0200

    KAFKA-20993: fix import order

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/assignment/GraphTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/assignment/GraphTest.java
@@ -25,9 +25,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
