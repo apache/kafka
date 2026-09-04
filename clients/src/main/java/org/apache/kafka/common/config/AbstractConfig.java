591e0b704dc990388248bf011d2f9fd58039ac3e
Christo <lolovc@amazon.com>
2026-09-04 14:51:57 +0100

    Address Chia-Ping's comments

--- clients/src/main/java/org/apache/kafka/common/config/AbstractConfig.java
+++ clients/src/main/java/org/apache/kafka/common/config/AbstractConfig.java
@@ -47,7 +47,7 @@ import java.util.stream.Collectors;
 @InterfaceAudience.Public
 public class AbstractConfig {
 
-    protected static final Logger log = LoggerFactory.getLogger(AbstractConfig.class);
+    private static final Logger log = LoggerFactory.getLogger(AbstractConfig.class);
 
     /**
      * Configs for which values have been requested, used to detect unused configs.
