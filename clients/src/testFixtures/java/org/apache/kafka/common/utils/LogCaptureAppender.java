a08d7afb33fba27693a8a60932b15a4326e17fe3
jim0987795064 <jim0987795064@gmail.com>
2026-09-07 23:37:51 +0800

    Rewrite getMessages with Stream API

--- clients/src/testFixtures/java/org/apache/kafka/common/utils/LogCaptureAppender.java
+++ clients/src/testFixtures/java/org/apache/kafka/common/utils/LogCaptureAppender.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class LogCaptureAppender extends AbstractAppender implements AutoCloseable {
     private final List<LogEvent> events = new LinkedList<>();
@@ -114,11 +115,10 @@ public class LogCaptureAppender extends AbstractAppender implements AutoCloseabl
     public List<String> getMessages(Level level) {
         final List<String> result = new LinkedList<>();
         synchronized (events) {
-            for (final LogEvent event : events) {
-                if (level.equals(event.getLevel())) {
-                    result.add(event.getMessage().getFormattedMessage());
-                }
-            }
+            events.stream()
+                .filter(event -> level.equals(event.getLevel()))
+                .map(event -> event.getMessage().getFormattedMessage())
+                .collect(Collectors.toList());
         }
         return result;
     }
