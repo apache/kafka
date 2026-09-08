6ec0ffde2e930cc87caf74939eb2599b1f94282f
Mickael Maison <mickael.maison@gmail.com>
2026-09-08 19:16:46 +0200

    Rename a few fields and methods

--- storage/src/test/java/org/apache/kafka/storage/internals/log/LocalLogTest.java
+++ storage/src/test/java/org/apache/kafka/storage/internals/log/LocalLogTest.java
@@ -162,7 +162,7 @@ class LocalLogTest {
         List<LogSegment> deletedSegments = log.deleteAllSegments();
         assertTrue(log.segments().isEmpty());
         assertEquals(segmentsBeforeDelete, deletedSegments);
-        assertThrows(KafkaStorageException.class, () -> log.checkIfMemoryMappedBufferClosed());
+        assertThrows(KafkaStorageException.class, () -> log.checkIfClosed());
         assertTrue(logDir.exists());
     }
 
@@ -272,26 +272,26 @@ class LocalLogTest {
     }
 
     @Test
-    public void testLogCloseFailureWhenInMemoryBufferClosed() throws IOException {
+    public void testLogCloseFailureWhenAlreadyClosed() throws IOException {
         List<KeyValue> keyValues = List.of(new KeyValue("abc", "ABC"), new KeyValue("de", "DE"));
         appendRecords(kvsToRecords(keyValues), 0L);
-        log.closeHandlers();
+        log.closeQuietly();
         assertThrows(KafkaStorageException.class, () -> log.close());
     }
 
     @Test
-    public void testLogCloseHandlers() throws IOException {
+    public void testCloseQuietly() throws IOException {
         List<KeyValue> keyValues = List.of(new KeyValue("abc", "ABC"), new KeyValue("de", "DE"));
         appendRecords(kvsToRecords(keyValues), 0L);
-        log.closeHandlers();
+        log.closeQuietly();
         assertThrows(ClosedChannelException.class, () -> appendRecords(kvsToRecords(keyValues), 2L));
     }
 
     @Test
-    public void testLogCloseHandlersIdempotent() {
-        log.closeHandlers();
-        // Check that LocalLog.closeHandlers() is idempotent
-        log.closeHandlers();
+    public void testCloseQuietlyIdempotent() {
+        log.closeQuietly();
+        // Check that LocalLog.closeQuietly() is idempotent
+        log.closeQuietly();
     }
 
     static class TestDeletionReason implements SegmentDeletionReason {
